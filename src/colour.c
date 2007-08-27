601332ea4850bdf9ce1759119c5f2588c3d50197
bobbens <>
2007-08-27 17:32:05 +0200

    branch:      naev [svn] new target graphics! new hyena graphic! split more colour stuff from player.c to colour.c minor gui tweak

--- src/colour.c
+++ src/colour.c
@@ -23,3 +23,22 @@ glColour cGreen      = { .r=0.20, .g=0.80, .b=0.20, .a=1. };
 glColour cDarkRed		= { .r=0.60, .g=0.10, .b=0.10, .a=1. };
 glColour cRed        = { .r=0.80, .g=0.20, .b=0.20, .a=1. };
 
+
+/*
+ * game specific
+ */
+glColour cConsole       =  { .r = 0.1, .g = 0.9, .b = 0.1, .a = 1. };
+/* objects */
+glColour cInert         =  { .r = 0.6, .g = 0.6, .b = 0.6, .a = 1. };
+glColour cNeutral       =  { .r = 0.9, .g = 1.0, .b = 0.3, .a = 1. };
+glColour cFriend        =  { .r = 0.0, .g = 1.0, .b = 0.0, .a = 1. };
+glColour cHostile       =  { .r = 0.9, .g = 0.2, .b = 0.2, .a = 1. };
+/* radar */
+glColour cRadar_player  =  { .r = 0.4, .g = 0.8, .b = 0.4, .a = 1. };
+glColour cRadar_targ    =  { .r = 0.0, .g = 0.7, .b = 1.0, .a = 1. };
+glColour cRadar_weap    =  { .r = 0.8, .g = 0.2, .b = 0.2, .a = 1. };
+/* health */
+glColour cShield        =  { .r = 0.2, .g = 0.2, .b = 0.8, .a = 1. };
+glColour cArmour        =  { .r = 0.5, .g = 0.5, .b = 0.5, .a = 1. };
+glColour cEnergy        =  { .r = 0.2, .g = 0.8, .b = 0.2, .a = 1. };
+
--- src/colour.h
+++ src/colour.h
@@ -18,7 +18,7 @@ typedef struct {
 extern glColour cWhite;
 #define cGrey  cGrey70
 extern glColour cBlack;
-
+/* greys */
 extern glColour cGrey90;
 extern glColour cGrey80;
 extern glColour cGrey70;
@@ -32,6 +32,28 @@ extern glColour cGrey10;
 extern glColour cGreen;
 extern glColour cDarkRed;
 extern glColour cRed;
+
+/*
+ * game specific
+ */
+/*
+ * game specific
+ */
+extern glColour cConsole;
+/* objects */
+extern glColour cInert;
+extern glColour cNeutral;
+extern glColour cFriend;
+extern glColour cHostile;
+/* radar */
+extern glColour cRadar_player;
+extern glColour cRadar_targ;
+extern glColour cRadar_weap;
+/* health */
+extern glColour cShield;
+extern glColour cArmour;
+extern glColour cEnergy;
+
 
 
 #endif /* COLOUR_H */
