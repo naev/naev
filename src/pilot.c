ab5018bfa186a79ece702601e0dd131d7ed18669
bobbens <bobbens@gmail.com>
2007-11-21 19:11:09 +0100

    branch:      naev weakened the knockback effect

--- src/pilot.c
+++ src/pilot.c
@@ -273,10 +273,11 @@ void pilot_hit( Pilot* p, const Solid* w, const unsigned int shooter,
 		}
 	}
 
-	/* knock back effect is dependent on both damage and mass of the weapon */
+	/* knock back effect is dependent on both damage and mass of the weapon 
+	 * should probably get turned into a partial conservative collision */
 	vect_cadd( &p->solid->vel,
-			w->vel.x * (dam_mod/4. + w->mass/p->solid->mass/4.),
-			w->vel.y * (dam_mod/4. + w->mass/p->solid->mass/4.) );
+			w->vel.x * (dam_mod/6. + w->mass/p->solid->mass/6.),
+			w->vel.y * (dam_mod/6. + w->mass/p->solid->mass/6.) );
 }
 
 
