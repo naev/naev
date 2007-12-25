b5ff8a9e811faf87cc7978145e6f43a7c82097f2
bobbens <bobbens@gmail.com>
2007-12-25 01:42:47 +0100

    branch:      naev tweaked the star deformation while hyperspacing

--- src/pilot.h
+++ src/pilot.h
@@ -22,8 +22,8 @@
 
 #define HYPERSPACE_ENGINE_DELAY	3000 /* time to warm up engine */
 #define HYPERSPACE_FLY_DELAY		5000 /* time it takes to hyperspace */
-#define HYPERSPACE_STARS_BLUR		3000 /* how long the stars blur */
-#define HYPERSPACE_STARS_LENGTH	500 /* length the stars blur to at max */
+#define HYPERSPACE_STARS_BLUR		2000 /* how long the stars blur */
+#define HYPERSPACE_STARS_LENGTH	1000 /* length the stars blur to at max */
 #define HYPERSPACE_FADEOUT			1000 /* how long the fade is */
 
 
