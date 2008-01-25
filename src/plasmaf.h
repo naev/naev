5568a378ceadb024317d299fc6b05bae28c09b5e
bobbens <bobbens@gmail.com>
2008-01-25 13:51:24 +0100

    first plasma fractal commit

---
+++ src/plasmaf.h
@@ -0,0 +1,16 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+
+
+#ifndef PLASMAF_H
+#  define PLASMAF_H
+
+
+#include "opengl.h"
+
+
+glTexture* pf_genFractal( const int w, const int h, const double rug );
+
+
+#endif /* PLASMAF_H */
