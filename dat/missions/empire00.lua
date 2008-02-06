6eda13b43cb84d740c8ff5ea18902e8c72d06c49
bobbens <bobbens@gmail.com>
2008-02-06 21:24:45 +0100

    updated mission text

--- dat/missions/empire00.lua
+++ dat/missions/empire00.lua
@@ -12,11 +12,12 @@ else -- default english
 	title[3] = "Mission Accomplished"
 	text = {}
 	text[1] = [[As you enter the bar you can't help to notice that a fellow at a table is looking at you since you came in.  You tend to your business as if you hadn't noticed.  A while later you feel a tap on your shoulder and see it's the same fellow.]]
-	text[2] = [["Hello, sorry about interrupting you.  I'm Lieutenant Czesc from the Empire Armada.  We're having another recruitment operation and would be interested in having another pilot among us.  Would be interesting in working for the Empire?"]]
+	text[2] = [["Hello, sorry about interrupting you.  I'm Lieutenant Czesc from the Empire Armada Shipping Division.  We're having another recruitment operation and would be interested in having another pilot among us.  Would be interesting in working for the Empire?"]]
 	text[3] = [["Welcome aboard.", says Czesc before giving you a firm handshake.  "At first you'll just be tested with cargo missions while we get data on your flying skills.  Later you could get called for more important mission.  Who knows?  You could be the next Yao Pternov, greatest pilot we ever had on the armada."
 He hits a couple buttons on his wrist computer that springs into action.
-"It looks like we already have a simple task for you. Deliver these parcels to %s.  The best pilots started delivering papers and ended up flying into combat against gigantic warships."]]
-	text[4] = [[You deliver the parcels to the Empire station at the %s spaceport.  Afterwards they make you do some paperwork to formalize your participation with the Empire.  You aren't too sure of what to make of your encounter with the Empire, only time will tell]]
+"It looks like we already have a simple task for you. Deliver these parcels to %s.  The best pilots started delivering papers and ended up flying into combat against gigantic warships with the Interception Division."]]
+	text[4] = [[You deliver the parcels to the Empire Shipping station at the %s spaceport.  Afterwards they make you do some paperwork to formalize your participation with the Empire.  They tell you to keep an eye out for missions labeled ES in the mission computer which stand for Empire Shipping which you now have access to.
+You aren't too sure of what to make of your encounter with the Empire, only time will tell...]]
 end
 
 
