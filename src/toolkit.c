082d9033e56500ff278a5c87a20a9e49477577d3
bobbens <bobbens@gmail.com>
2008-02-16 20:29:48 +0100

    minor map tweaks

--- src/toolkit.c
+++ src/toolkit.c
@@ -1201,8 +1201,8 @@ static void toolkit_renderCust( Widget* cst, double bx, double by )
             toolkit_colDark, NULL );
    }
 
-   toolkit_clip( x+1, y+1, cst->w, cst->h );
-   (*cst->dat.cst.render) ( x, y, cst->w, cst->h );
+   toolkit_clip( x, y+1, cst->w, cst->h );
+   (*cst->dat.cst.render) ( x, y, cst->w, cst->h+1 );
    toolkit_unclip();
 }
 
