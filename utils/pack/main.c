822b1b23f999971180748de4d3f06047318f55ef
bobbens <bobbens@gmail.com>
2008-03-04 23:18:50 +0100

    you can now use pack to list files in a packfile

--- utils/pack/main.c
+++ utils/pack/main.c
@@ -1,6 +1,7 @@
 
 #include <stdlib.h> /* exit() */
 #include <stdio.h> /* printf() */
+#include <stdint.h> /* uint32_t */
 
 #include "pack.h"
 
@@ -13,17 +14,29 @@ int main( int argc, char** argv )
 	switch (argc) {
 		case 1:
 			fprintf(stderr, "Missing output file\n");
-		case 2:
-			fprintf(stderr, "Missing input file/s\n");
 			goto usage;
 			break;
 	}
 
 	char* outfile = argv[1];
+   char** list;
+   uint32_t nlist;
+   int i;
 	uint32_t nfiles = (uint32_t)argc - 2;
 	argv+=2;
 
-	pack_files( outfile, (const char**)argv, nfiles );
+   if (nfiles == 0) { /* no files, list what it has */
+      list = pack_listfiles( outfile, &nlist );
+      fprintf(stdout, "Packfile '%s' contains:\n", outfile);
+      for (i=0; i<nlist; i++) {
+         fprintf(stdout, "   %03d) %s\n", i, list[i]);
+         free(list[i]);
+      }
+      free(list);
+   }
+   else {
+      pack_files( outfile, (const char**)argv, nfiles );
+   }
 	exit(EXIT_SUCCESS);
 
 usage:
