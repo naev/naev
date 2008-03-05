c3313c1123d03197788de98a580959db7718db19
bobbens <bobbens@gmail.com>
2008-03-05 20:35:04 +0100

    PACK: minor cleanuP

--- utils/pack/main.c
+++ utils/pack/main.c
@@ -10,37 +10,38 @@
 
 
 int main( int argc, char** argv )
-{  
-	switch (argc) {
-		case 1:
-			fprintf(stderr, "Missing output file\n");
-			goto usage;
-			break;
-	}
-
-	char* outfile = argv[1];
+{
+   char* packfile;
    char** list;
    uint32_t nlist;
    int i;
-	uint32_t nfiles = (uint32_t)argc - 2;
-	argv+=2;
+   uint32_t nfiles;
+
+   if (argc == 1) {
+      fprintf(stderr, "Missing output file\n");
+      goto usage;
+   }
+
+   packfile = argv[1];
+   nfiles = (uint32_t)argc - 2;
+   argv+=2;
 
    if (nfiles == 0) { /* no files, list what it has */
-      list = pack_listfiles( outfile, &nlist );
-      fprintf(stdout, "Packfile '%s' contains:\n", outfile);
+      list = pack_listfiles( packfile, &nlist );
+      fprintf(stdout, "Packfile '%s' contains:\n", packfile);
       for (i=0; i<nlist; i++) {
-         fprintf(stdout, "   %03d) %s\n", i, list[i]);
+         fprintf(stdout, "   %03d   %s\n", i, list[i]);
          free(list[i]);
       }
       free(list);
    }
-   else {
-      pack_files( outfile, (const char**)argv, nfiles );
+   else { /* create a packfile */
+      pack_files( packfile, (const char**)argv, nfiles );
    }
-	exit(EXIT_SUCCESS);
+   exit(EXIT_SUCCESS);
 
 usage:
-	printf(USAGE);
-	exit(EXIT_SUCCESS);
+   printf(USAGE);
+   exit(EXIT_SUCCESS);
 }
 
