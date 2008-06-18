75e08fe550bbcadf6d56d8c507690d816a93acde
bobbens <bobbens@gmail.com>
2008-06-18 19:53:53 +0200

    * Added script to easy updating ship graphics.

---
+++ gfx/ship/update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+#   Variables
+#
+ARTWORK_PATH="../../../naev-artwork/"
+
+
+#
+#   Copy them all over
+#
+ARTWORK_PATH=$ARTWORK_PATH"3d/final/"
+cp ${ARTWORK_PATH}"seaxbane.png" "drone.png"
+cp ${ARTWORK_PATH}"llama.png" "llama.png"
+cp ${ARTWORK_PATH}"lancelot.png" "lancelot.png"
+cp ${ARTWORK_PATH}"mule.png" "mule.png"
+cp ${ARTWORK_PATH}"pacifier.png" "pacifier.png"
+cp ${ARTWORK_PATH}"gawain0.png" "gawain.png"
+cp ${ARTWORK_PATH}"goddard.png" "goddard.png"
+cp ${ARTWORK_PATH}"hawking2.png" "hawking.png"
+cp ${ARTWORK_PATH}"hyena1.png" "hyena.png"
+cp ${ARTWORK_PATH}"admonisher0.png" "admonisher.png"
+cp ${ARTWORK_PATH}"admonisher2.png" "admonisher_pirate.png"
+
