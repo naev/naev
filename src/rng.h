08d7de084231c6c12d3aa782644e31fc05d37c03
bobbens <bobbens@gmail.com>
2008-06-23 12:08:44 +0200

    * Made RNG() more robust.

--- src/rng.h
+++ src/rng.h
@@ -8,7 +8,8 @@
 #  define RNG_H
 
 
-#define RNG(L,H)  ((int)L + (int)((double)(H-L+1) * randfp())) /* L <= RNG <= H */
+#define RNG(L,H)  (((L)>(H)) ? RNG_SANE((H),(L)) : RNG_SANE((L),(H)))
+#define RNG_SANE(L,H) ((int)L + (int)((double)(H-L+1) * randfp())) /* L <= RNG <= H */
 #define RNGF()    (randfp()) /* 0. <= RNGF <= 1. */
 
 
