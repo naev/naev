6e33fe88001d91ae7aedc7824cfbcb72c2e32fa3
bobbens <bobbens@gmail.com>
2008-07-01 20:57:50 +0200

    * WARN and ERR now generate asserts in DEBUG mode.

--- src/log.h
+++ src/log.h
@@ -10,9 +10,15 @@
 
 #include <stdio.h>
 
+/* We'll use asserts in DEBUG is defined */
+#ifndef DEBUG
+#  define NODEBUG
+#endif /* DEBUG */
+#include <assert.h>
+
 #define LOG(str, args...)  (fprintf(stdout,str"\n", ## args))
-#define WARN(str, args...) (fprintf(stderr,"Warning: "str"\n", ## args))
-#define ERR(str, args...)  (fprintf(stderr,"ERROR %s:%d [%s]: "str"\n", __FILE__, __LINE__, __func__, ## args))
+#define WARN(str, args...) (fprintf(stderr,"Warning: "str"\n", ## args), assert(0))
+#define ERR(str, args...)  (fprintf(stderr,"ERROR %s:%d [%s]: "str"\n", __FILE__, __LINE__, __func__, ## args), assert(0))
 #ifdef DEBUG
 #  undef DEBUG
 #  define DEBUG(str, args...) LOG(str, ## args)
