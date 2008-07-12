73f87997bbb989ba88662dac362da77086e9900e
bobbens <bobbens@gmail.com>
2008-07-12 17:50:30 +0200

    * Doxygenized collision.[ch].

--- src/collision.c
+++ src/collision.c
@@ -2,6 +2,11 @@
  * See Licensing and Copyright notice in naev.h
  */
 
+/**
+ * @file collision.c
+ *
+ * @brief Deals with 2d collisions.
+ */
 
 
 #include "collision.h"
@@ -12,18 +17,24 @@
 
 
 /**
- * @brief Collides sprite at (asx,asy) in at at pos ap with sprite at (bsx,bsy) in
- * bt at bp.
+ * @fn int CollideSprite( const glTexture* at, const int asx,
+ *                        const int asy, const Vector2d* ap,
+ *                        const glTexture* bt, const int bsx, 
+ *                        const int bsy, const Vector2d* bp )
  *
- * @param[in] at texture a.
- * @param[in] asx position of x of sprite a.
- * @param[in] asy position of y of sprita a.
- * @param[in] ap position in space of sprite a.
- * @param[in] bt texture b.
- * @param[in] bsx position of x of sprite b.
- * @param[in] bsy position of y of sprite b.
- * @param[in] bp position in space of sprite b.
- * @return 1 on collision, 0 else.
+ * @brief Checks whether or not two sprites collide.
+ *
+ * This function does pixel perfect checks.
+ *
+ *    @param[in] at texture a.
+ *    @param[in] asx position of x of sprite a.
+ *    @param[in] asy position of y of sprita a.
+ *    @param[in] ap position in space of sprite a.
+ *    @param[in] bt texture b.
+ *    @param[in] bsx position of x of sprite b.
+ *    @param[in] bsy position of y of sprite b.
+ *    @param[in] bp position in space of sprite b.
+ *    @return 1 on collision, 0 else.
  */
 int CollideSprite( const glTexture* at, const int asx, const int asy, const Vector2d* ap,
       const glTexture* bt, const int bsx, const int bsy, const Vector2d* bp )
@@ -77,90 +88,4 @@ int CollideSprite( const glTexture* at, const int asx, const int asy, const Vect
 
    return 0;
 }
-
-
-/**
- * @brief Calculates collision path corrections.
- * 
- * @param[in] p Position of object that's trying to collide.
- * @param[in] v Velocity of object that's trying to collide.
- * @param[in] tp Position of the target to collide into.
- * @param[in] tv Velocity of the target to collide into.
- * @param[in] limit Accuracy limit.
- * @return Direction object will need to adjust velocity to for collision.
- */
-/* 
- * Doesn't work as expected, needs to get fixed to allow missiles to use
- * the physics engine instead of hacking their velocities
- */
-#if 0
-double CollidePath( const Vector2d* p, Vector2d* v,
-      const Vector2d* tp, Vector2d* tv, double limit )
-{
-   double mx, my; /* Position modifiers */
-   double t, tt; /* Time to target */
-   Vector2d test, ttest; /* Test vector and target test vector */
-   double offset; /* Direction to face and error it produces */
-   double mod, moddir; /* Modifier for iteration */
-   int i;
-
-   /* Test vector */
-   vect_cset( &test, tp->x - p->x, tp->y - p->y ); /* Start by straight line */
-   t = VMOD(test) / VMOD(*v); /* d=v*t ==> t=d/v */
-
-   /* Target test vector */
-   vectnull( &ttest ); /* Starting from itself */
-   tt = VMOD(ttest) / VMOD(*tv);
-
-   /* Special case object isn't moving */
-   if (VMOD(*v) < 1e-6)
-      return VANGLE(test);
-
-   /* Target faster then object */
-   if (VMOD(*v) < VMOD(*tv)) {
-      vect_reflect( &test, v, tv );
-      return VANGLE(test);
-   }
-
-   /* Loop until error is minimal */
-   offset = tt - t;
-   moddir = 1.; /* Start off by positive increments */
-   mod = 10.; /* Start off by a 10 second jump */
-   i = 0;
-   while (FABS(offset) > limit) {
-      if (i>100) break;
-
-      /* Calculate position modifiers */
-      if (offset < -mod/2.) /* tt>t ==> major overshot */
-         moddir = -1.;
-      else if (offset < 0.) { /* tt>t ==> overshot */
-         /* invert direction and half */
-         moddir = -1.;
-         mod = mod/2.; 
-      }
-      else if (offset > mod) /* tt<t ==> undershot */
-         moddir = 1.; /* make sure it's positive */
-      else if (offset > 0.) { /* tt<t ==> minor undershot */
-         /* positive and shrink step */
-         moddir = 1.;
-         mod = FABS(mod)/2.;
-      }
-      mx = tv->x * mod * moddir;
-      my = tv->y * mod * moddir;
-
-      /* Increment test vectors */
-      vect_cadd( &test, mx, my );
-      vect_cadd( &ttest, mx, my );
-
-      /* Compare results */
-      t = VMOD(test) / VMOD(*v);
-      tt = VMOD(ttest) / VMOD(*tv);
-      offset = tt - t;
-
-      i++;
-   }
-
-   return VANGLE(test);
-}
-#endif
 
--- src/collision.h
+++ src/collision.h
@@ -16,9 +16,6 @@
 /* Returns 1 if collision is detected */
 int CollideSprite( const glTexture* at, const int asx, const int asy, const Vector2d* ap,
       const glTexture* bt, const int bsx, const int bsy, const Vector2d* bp );
-/* Gets direction to face the target */
-/*double CollidePath( const Vector2d* p, Vector2d* v,
-      const Vector2d* tp, Vector2d* tv, const double limit );*/
 
 
 #endif /* COLLISION_H */
