0de8d6a40b0fe18387a8bed58db051dab2c1fcb2
bobbens <bobbens@gmail.com>
2008-08-14 13:35:57 +0200

    * Doxygenization.

--- src/ntime.c
+++ src/ntime.c
@@ -2,6 +2,19 @@
  * See Licensing and Copyright notice in naev.h
  */
 
+/**
+ * @file time.c
+ *
+ * @brief Handles the NAEV time.
+ *
+ * The basic unit of time is the STU.  There are 1000 STU in a MTU.  The time
+ *  representation is generally UST which consists of MTU.STU.
+ *
+ * Acronyms:
+ *    - MTU : Major Time Unit (1000 STU)
+ *    - STU : Synchronized Time Unit
+ *    - UST : Universal Synchronized Time
+ */
 
 
 #include "ntime.h"
@@ -13,12 +26,14 @@
 #include "hook.h"
 
 
-static unsigned int naev_time = 0;
+static unsigned int naev_time = 0; /**< Contains the current time in STU. */
 
 
 
-/*
- * gets the current time
+/**
+ * @fn unsigned int ntime_get (void)
+ *
+ * @brief Gets the current time.
  */
 unsigned int ntime_get (void)
 {
@@ -26,25 +41,30 @@ unsigned int ntime_get (void)
 }
 
 
-/*
- * returns the time in pretty text
+/**
+ * @fn char* ntime_pretty( unsigned int t )
+ *
+ * @brief Gets the time in a pretty human readable format.
+ *
+ *    @param t Time to print (in STU), if 0 it'll use the current time.
+ *    @return The time in a human readable format (must free).
  */
 char* ntime_pretty( unsigned int t )
 {
    unsigned int nt;
-   int maj, stu;
+   int mtu, stu;
    char str[128], *ret;
 
    if (t==0) nt = naev_time;
    else nt = t;
 
    /* UST (Universal Synchronized Time) - unit is STU (Synchronized Time Unit) */
-   maj = nt / (1000*NTIME_UNIT_LENGTH);
+   mtu = nt / (1000*NTIME_UNIT_LENGTH);
    stu = (nt / (NTIME_UNIT_LENGTH)) % 1000;
-   if (maj == 0) /* only STU */
+   if (mtu == 0) /* only STU */
       snprintf( str, 128, "%03d STU", stu );
-   else /* full format */
-      snprintf( str, 128, "UST %d.%03d", maj, stu );
+   else /* UST format */
+      snprintf( str, 128, "UST %d.%03d", mtu, stu );
 
    ret = strdup(str);
 
@@ -52,8 +72,12 @@ char* ntime_pretty( unsigned int t )
 }
 
 
-/*
- * sets the time absolutely, does NOT generate an event, used at init
+/**
+ * @fn void ntime_set( unsigned int t )
+ *
+ * @brief Sets the time absolutely, does NOT generate an event, used at init.
+ *
+ *    @param t Absolute time to set to in STU.
  */
 void ntime_set( unsigned int t )
 {
@@ -61,8 +85,12 @@ void ntime_set( unsigned int t )
 }
 
 
-/*
- * sets the time relatively
+/**
+ * @fn void ntime_inc( unsigned int t )
+ *
+ * @brief Sets the time relatively.
+ *
+ *    @param t Time modifier in STU.
  */
 void ntime_inc( unsigned int t )
 {
