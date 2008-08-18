c1d3b67f5580fbb478c68dccdb16e4d2acf733c5
bobbens <bobbens@gmail.com>
2008-08-18 23:29:35 +0200

    * Added check.sh for checking outfit graphic usage.

---
+++ gfx/outfit/check.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+DATA="../../dat/outfit.xml"
+
+echo "Checking for unused graphics..."
+echo
+
+# Check unused space gfx
+echo "   Unused outfit store gfx"
+cd store
+for SPACE in *.png; do
+   if [ -z "`grep ${SPACE%.png} ../$DATA | grep "<gfx_store>"`" ]; then
+      echo "      $SPACE"
+   fi
+done
+cd ..
+
+# Check unused exterior gfx
+echo "   Unused outfit gfx"
+cd space
+for SPACE in *.png; do
+   if [ -z "`grep ${SPACE%.png} ../$DATA | grep "<gfx>"`" ]; then
+      echo "      $SPACE"
+   fi
+done
+cd ..
+
+echo
+echo
+echo "Checking for overused graphics..."
+echo
+
+# Check overused
+echo "   Overused outfit store gfx"
+cd store
+for SPACE in *.png; do
+   COUNT=`grep ${SPACE%.png} ../$DATA | grep -c "<gfx_store>"`
+   if [ $COUNT -gt 1 ]; then
+      echo "      $SPACE => $COUNT times"
+   fi
+done
+cd ..
+
+# Check unused exterior gfx
+echo "   Overused outfit gfx"
+cd space
+for SPACE in *.png; do
+   COUNT=`grep ${SPACE%.png} ../$DATA | grep -c "<gfx>"`
+   if [ $COUNT -gt 1 ]; then
+      echo "      $SPACE => $COUNT times"
+   fi
+done
+cd ..
