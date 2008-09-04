ec49cb71f6d220f9e9e1a592bd3ef07481acabf7
bobbens <bobbens@gmail.com>
2008-09-04 12:45:22 +0200

    * Some ai changes. * Added FLF fleets and FLF ai.

--- ai/empire.lua
+++ ai/empire.lua
@@ -16,13 +16,30 @@ function create ()
 end
 
 -- taunts
-function taunt ( target )
-   taunts = {
-         "You dare attack me!",
-         "You are no match for the Empire!",
-         "The Empire will have your head!",
-         "You'll regret this!"
-   }
+function taunt ( target, offense )
+
+   -- Only 50% of actually taunting.
+   if rnd.int(0,1) == 0 then
+      return
+   end
+
+   -- some taunts
+   if offense then
+      taunts = {
+            "There is no room in this universe for scum like you!",
+            "The Empire will enjoy your death!",
+            "Your head will make a fine gift for the Emperor!"
+      }
+   else
+      taunts = {
+            "You dare attack me!",
+            "You are no match for the Empire!",
+            "The Empire will have your head!",
+            "You'll regret this!"
+      }
+   end
+
    ai.comm(target, taunts[ rnd.int(1,#taunts) ])
 end
+
 
---
+++ ai/flf.lua
@@ -0,0 +1,39 @@
+include("ai/tpl/generic.lua")
+
+-- Settings
+aggressive = true
+safe_distance = 300
+armour_run = 100
+shield_return = 20
+land_planet = false
+
+
+function create ()
+      ai.setcredits(ai.shipprice()/1000 , ai.shipprice()/100 )
+end
+
+
+function taunt ( target, offense )
+
+   -- Only 50% of actually taunting.
+   if rnd.int(0,1) == 0 then
+      return
+   end
+
+   -- some taunts
+   if offense then
+      taunts = {
+            "For the Frontier!",
+            "You'll make great target practice!",
+            "Purge the oppressors!"
+      }
+   else
+      taunts = {
+            "You are no match for the FLF.",
+            "I've killed scum much more dangerous then you."
+      }
+   end
+
+   ai.comm(target, taunts[ rnd.int(1,#taunts) ])
+end
+
