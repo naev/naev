a4fa5683a5feaa094dff587c1c7dc5e5482e3dc0
bobbens <bobbens@gmail.com>
2008-12-10 19:51:58 +0100

    * Fixed segfault in volatile nebulae.

--- src/pilot.c
+++ src/pilot.c
@@ -587,11 +587,12 @@ void pilot_hit( Pilot* p, const Solid* w, const unsigned int shooter,
    }
 
 
-   /* knock back effect is dependent on both damage and mass of the weapon 
-    * should probably get turned into a partial conservative collision */
-   vect_cadd( &p->solid->vel,
-         knockback * (w->vel.x * (dam_mod/6. + w->mass/p->solid->mass/6.)),
-         knockback * (w->vel.y * (dam_mod/6. + w->mass/p->solid->mass/6.)) );
+   if (w != NULL)
+      /* knock back effect is dependent on both damage and mass of the weapon 
+       * should probably get turned into a partial conservative collision */
+      vect_cadd( &p->solid->vel,
+            knockback * (w->vel.x * (dam_mod/6. + w->mass/p->solid->mass/6.)),
+            knockback * (w->vel.y * (dam_mod/6. + w->mass/p->solid->mass/6.)) );
 }
 
 
