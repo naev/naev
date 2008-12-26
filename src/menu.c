bfead5f00ea58680b5ce367041935d9958ca4e5e
bobbens <bobbens@gmail.com>
2008-12-26 10:28:25 +0100

    * Version message should no longer get overlapped in small resolutions.

--- src/menu.c
+++ src/menu.c
@@ -117,7 +117,7 @@ void menu_main (void)
    window_addCust( bwid, 0, 0, SCREEN_W, SCREEN_H, "cstBG", 0,
          (void(*)(double,double,double,double)) nebu_render, NULL );
    window_addImage( bwid, (SCREEN_W-tex->sw)/2., -50, "imgLogo", tex, 0 );
-   window_addText( bwid, 0., 50, SCREEN_W, 30., 1, "txtBG", NULL,
+   window_addText( bwid, 0., 20, SCREEN_W, 30., 1, "txtBG", NULL,
          &cWhite, naev_version() );
 
    /* create menu window */
