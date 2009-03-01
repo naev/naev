c9ec72a569e64328b8a17fcb30802229a013fda6
Edgar Simo <bobbens@gmail.com>
2009-03-01 14:05:47 +0100

    * FLF now does bribes and refueling.

--- ai/flf.lua
+++ ai/flf.lua
@@ -1,15 +1,39 @@
 include("ai/tpl/generic.lua")
 
 -- Settings
-aggressive = true
-safe_distance = 300
-armour_run = 100
-shield_return = 20
-land_planet = false
+aggressive     = true
+safe_distance  = 300
+armour_run     = 100
+shield_return  = 20
+land_planet    = false
 
 
 function create ()
+
+   -- Give monies.
    ai.setcredits(ai.shipprice()/600 , ai.shipprice()/100 )
+
+   -- Get standing.
+   standing = ai.getstanding( ai.getPlayer() ) or -1
+
+   -- Handle bribes.
+   if standing < -30 then
+      mem.bribe_no = "\"The only way to deal with scum like you is with cannons!\""
+   else
+      mem.bribe = math.sqrt( ai.shipmass() ) * (300. * rnd.int() + 850.)
+      mem.bribe_prompt = string.format("\"It'll cost you %d credits for me to ignore your dirty presence.\"", mem.bribe)
+      mem.bribe_paid = "\"Begone before I change my mind.\""
+   end
+
+   -- Handle refueling.
+   if standing > 70 then
+      mem.refuel = rnd.rnd( 1000, 2000 )
+      mem.refuel_msg = string.format("\"I should be able to spare some fuel for %d credits.\"", mem.refuel)
+   else
+      mem.refuel_no = "\"I can't spare fuel for you.\""
+   end
+
+   -- Choose attack.
    attack_choose()
 end
 
