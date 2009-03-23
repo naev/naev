be98f9754f1ff2f665a5a1c55e2dc418ee639aff
Edgar Simo <bobbens@gmail.com>
2009-03-23 19:54:21 +0100

    * Simplified flee AI.

--- ai/flee.lua
+++ ai/flee.lua
@@ -1,98 +1,42 @@
 include("ai/include/basic.lua")
-include("ai/include/attack.lua")
 --[[
  ===FLEE AI===
 Mission specific AI to make pilots leave the system immediately.
 
   Settings
 --]]
-armour_run     = 100 -- At which damage to run at
-armour_return  = 0 -- At which armour to return to combat
-shield_run     = 100 -- At which shield to run
-shield_return  = 0 -- At which shield to return to combat
-aggressive     = false -- Should pilot actively attack enemies?
-safe_distance  = 0 -- Safe distance from enemies to jump
-land_planet    = false -- Should land on planets?
-distressrate   = 3 -- Number of ticks before calling for help
-distressmsg    = nil -- Message when calling for help
---[-[
 -- Required control rate
 control_rate   = 2
+
+
+-- Required "flee" function
+function create ()
+end
 
 -- Required "control" function
 function control ()
    task = ai.taskname()
    enemy = ai.getenemy()
 
-   -- Get new task
-   if task == "none" then
-      -- We'll first check enemy.
-      if enemy ~= nil and aggressive then
-         taunt(enemy, true)
-         ai.pushtask(0, "attack", enemy)
+   if task ~= "runaway" then
+
+      if enemy ~= nil then
+         ai.pushtask(0, "runaway", enemy)
       else
-         idle()
+         ai.pushtask(0, "hyperspace" )
       end
-
-   -- Don't stop boarding
-   elseif task == "board" or task == "boardstop" then
-      -- We want to think in case another attacker gets close
-      attack_think()
-
-   -- Think for attacking
-   elseif task == "attack" then
-      target = ai.target()
-
-      -- Needs to have a target
-      if not ai.exists(target) then
-         ai.poptask()
-         return
-      end
-
-      -- Runaway if needed
-      if (shield_run > 0 and ai.pshield() < shield_run
-               and ai.pshield() < ai.pshield(target) ) or
-            (armour_run > 0 and ai.parmour() < armour_run
-               and ai.parmour() < ai.parmour(target) ) then
-         ai.pushtask(0, "runaway", target)
-
-      -- Think like normal
-      else
-         attack_think()
-      end
-
-      -- Handle distress
-      gen_distress()
-
-   -- Pilot is running away
+   
    elseif task == "runaway" then
       target = ai.target()
 
-      -- Needs to have a target
       if not ai.exists(target) then
          ai.poptask()
          return
       end
 
-      dist = ai.dist( target )
-
-      -- Should return to combat?
-      if aggressive and ((shield_return > 0 and ai.pshield() >= shield_return) or
-            (armour_return > 0 and ai.parmour() >= armour_return)) then
-         ai.poptask() -- "attack" should be above "runaway"
-
-      -- Try to jump
-      elseif dist > safe_distance then
+      if ai.dist(target) > 300 then
          ai.hyperspace()
       end
-
-      -- Handle distress
-      gen_distress()
-
-   -- Enemy sighted, handled after running away
-   elseif enemy ~= nil and aggressive then
-      taunt(enemy, true)
-      ai.pushtask(0, "attack", enemy)
 
    -- Enter hyperspace if possible
    elseif task == "hyperspace" then 
@@ -106,23 +50,7 @@ function attacked ( attacker )
    task = ai.taskname()
    target = ai.target()
 
-   -- Notify that pilot has been attacked before
-   mem.attacked = true
-
-   if task ~= "attack" and task ~= "runaway" then
-
-      -- some taunting
-      taunt( attacker, false )
-
-      -- now pilot fights back
-      ai.pushtask(0, "attack", attacker)
-
-   elseif task == "attack" then
-      if ai.target() ~= attacker and 
-            ai.dist(attacker) < ai.dist(target) then
-         ai.pushtask(0, "attack", attacker)
-      end
-   elseif task == "runaway" then
+   if task == "runaway" then
       if ai.target() ~= attacker then
          ai.poptask()
          ai.pushtask(0, "runaway", attacker)
@@ -130,139 +58,3 @@ function attacked ( attacker )
    end
 end
 
--- Default task to run when idle
-function idle ()
-   planet = ai.landplanet()
-   -- planet must exist
-   if planet == nil or land_planet == false then
-      ai.settimer(0, rnd.int(1000, 3000))
-      ai.pushtask(0, "enterdelay")
-   else
-      mem.land = planet
-      ai.pushtask(0, "hyperspace")
-      ai.pushtask(0, "land")
-   end
-end
-
--- Delays the ship when entering systems so that it doesn't leave right away
-function enterdelay ()
-   if ai.timeup(0) then
-      ai.pushtask(0, "hyperspace")
-   end
-end
-
-function create ()
-   attack_choose()
-end
-
--- taunts
-function taunt ( target, offensive )
-   -- Empty stub
-end
-
-
--- Handle distress signals
-function distress ( pilot, attacker )
-
-   -- Make suree target exists
-   if not ai.exists( attacker ) then
-      ai.poptask()
-      return
-   end
-
-   -- Must be aggressive
-   if not aggressive then
-      return
-   end
-
-   -- Make sure pilot is setting his target properly
-   if pilot == attacker then
-      return
-   end
-
-   -- If ally, engage offender
-   if ai.isally(pilot) then
-      t = attacker
-   -- If enemy, engage distressed pilot
-   elseif ai.isenemy(pilot) then
-      t = pilot
-   -- Else ignore
-   else
-      return
-   end
-
-   task = ai.taskname()
-   -- If not attacking nor fleeing, begin attacking
-   if task ~= "attack" and task ~= "runaway" then
-      ai.pushtask( 0, "attack", t )
-   -- We're sort of busy
-   elseif task == "attack" then
-      target = ai.target()
-
-      if not ai.exists(target) or ai.dist(target) > ai.dist(t) then
-         ai.pushtask( 0, "attack", t )
-      end
-   end
-end
-
-
--- Handles generating distress messages
-function gen_distress ( target )
-
-   -- Must have a valid distress rate
-   if distressrate <= 0 then
-      return
-   end
-
-   -- Only generate distress if have been attacked before
-   if not mem.attacked then
-      return
-   end
-
-   -- Update distres counter
-   if mem.distressed == nil then
-      mem.distressed = 1
-   else
-      mem.distressed = mem.distressed + 1
-   end
-
-   -- See if it's time to trigger distress
-   if mem.distressed > distressrate then
-      ai.distress( distressmsg )
-      mem.distressed = 1
-   end
-
-end
---]]
-
---[[
--- Required "control" function
-function control ()
-   task = "runaway"
-   enemy = ai.getenemy()
-
-   if task == "runaway" then
-         ai.hyperspace()
-   end
-end
-
-
--- Required "attacked" function
-function attacked ( attacker )
-   task = "runaway"
-   target = ai.target()
-
-   if task == "runaway" then
-         ai.hyperspace()
-   end
-end
-
-function idle ()
-   task = "runaway"
-   target = ai.target()
-
-   if task == "runaway" then
-         ai.hyperspace()
-   end
-end
---]]
