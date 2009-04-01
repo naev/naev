5bec95d3786003a49927acc27535396d5d4da12d
Edgar Simo <bobbens@gmail.com>
2009-04-01 22:54:46 +0200

    * New pirate bounty mission preliminary implementation.

---
+++ dat/missions/empire/emp_pirbounty.lua
@@ -0,0 +1,164 @@
+--[[
+
+   Kill Pirate
+
+   Randomly appearing bar mission to kill a unique pirate.
+
+--]]
+
+-- Localization, choosing a language if naev is translated for non-english-speaking locales.
+lang = naev.lang()
+if lang == "es" then
+else -- Default to English
+   -- Mission details
+   misn_title  = "Pirate Bounty near %s"
+   misn_reward = "%d credits"
+   misn_desc   = "There is a bounty on the head of the pirate known as %s who was last seen near the %s system."
+
+   -- Text
+   title    = {}
+   text     = {}
+   title[1] = "Spaceport Bar"
+   text[1]  = [[You are enjoying your drinks when an Empire official bursts in and declares a bounty on the head of a pirate terrorizing the area known as %s for %d credits.  It seems like he was last seen in the %s system. Quite a few other mercenaries seem interested and it looks like you'll have to outrace them.
+   
+Will you take up the bounty?]]
+   text[2] = [[You roll up your sleeve and grab one of the pamphlets given out by the Empire official.]]
+
+   -- Messages
+   msg      = {}
+   msg[1]   = "MISSION SUCCESS!  Payment received."
+   msg[2]   = "Pursue %s!"
+end
+
+
+-- Scripts we need
+include("scripts/pilot/pirate.lua")
+
+
+--[[
+Mission entry point.
+--]]
+function create ()
+   -- Create the target pirate
+   pir_name, pir_ship, pir_outfits = pir_generate()
+
+   -- Get target system
+   cur_sys  = system.get()
+   adj_sys  = cur_sys:adjacentSystems()
+   near_sys = adj_sys[ rnd.rnd(1,#adj_sys) ]
+
+   -- Get credits
+   credits  = rnd.rnd(5,10) * 10000
+
+   -- Mission details:
+   if tk.yesno( title[1], string.format( text[1],
+         pir_name, credits, near_sys:name() ) ) then
+      misn.accept()
+
+      -- Set mission details
+      misn.setTitle( string.format( misn_title, near_sys:name()) )
+      misn.setReward( string.format( misn_reward, credits) )
+      misn.setDesc( string.format( misn_desc, pir_name, near_sys:name() ) )
+      misn.setMarker( near_sys, "misc" )
+
+      -- Some flavour text
+      tk.msg( title[1], text[2] )
+
+      -- Set hooks
+      hook.enter("sys_enter")
+   end
+end
+
+
+-- Player won, gives rewards.
+function give_rewards ()
+   -- Give monies
+   player.pay(credits)
+
+   -- Give factions
+   player.modFaction( "Empire", 5 )
+   
+   -- Finish mission
+   misn.finish(true)
+end
+
+
+-- Entering a system
+function sys_enter ()
+   cur_sys = system.get()
+   -- Check to see if reaching target system
+   if cur_sys == near_sys then
+
+      -- Create the badass enemy
+      p     = pilot.add(pir_ship)
+      pir   = p[1]
+      pir:rename(pir_name)
+      pir:rmOutfit("all") -- Start naked
+      pilot_outfitAddSet( pir, pir_outfits )
+      hook.pilot( pir, "death", "pir_dead" )
+      hook.pilot( pir, "jump", "pir_jump" )
+   end
+end
+
+
+-- Pirate is dead
+function pir_dead ()
+   player.msg( msg[1] )
+   give_rewards()
+end
+
+
+-- Pirate jumped away
+function pir_jump ()
+   player.msg( string.format(msg[2], pir_name) )
+
+   -- Basically just swap the system
+   adj_sys  = near_sys:adjacentSystems()
+   near_sys = adj_sys[ rnd.rnd(1,#adj_sys) ]
+end
+
+
+--[[
+Functions to create pirates based on difficulty more easily.
+--]]
+function pir_generate ()
+   -- Get the pirate name
+   pir_name = pirate_name()
+
+   -- Get the pirate details
+   rating = player.getRating()
+   if rating < 50 then
+      pir_ship, pir_outfits = pir_easy()
+   elseif rating < 150 then
+      pir_ship, pir_outfits = pir_medium()
+   else
+      pir_ship, pir_outfits = pir_hard()
+   end
+
+   -- Make sure to save the outfits.
+   pir_outfits["__save"] = true
+
+   return pir_name, pir_ship, pir_outfits
+end
+function pir_easy ()
+   if rnd.rnd() < 0.5 then
+      return pirate_createAncestor(false)
+   else
+      return pirate_createVendetta(false)
+   end
+end
+function pir_medium ()
+   if rnd.rnd() < 0.5 then
+      return pirate_createAdmonisher(false)
+   else
+      return pir_easy()
+   end
+end
+function pir_hard ()
+   if rnd.rnd() < 0.5 then
+      return pirate_createKestrel(false)
+   else
+      return pir_medium()
+   end
+end
+
