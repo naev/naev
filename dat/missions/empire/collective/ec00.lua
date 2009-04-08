237fdfa4dfd55da8f116eeeceebaa506b1ab1b29
Edgar Simo <bobbens@gmail.com>
2009-04-08 13:42:34 +0200

    * Fixed mission mistake.

--- dat/missions/empire/collective/ec00.lua
+++ dat/missions/empire/collective/ec00.lua
@@ -28,7 +28,7 @@ else -- default english
 "You've heard about the Collective right?  From what we know, the Collective seems to be a sort of 'hive' of robots.  They're a recent menace, had the timing to arrive more or less when the Incident happened, otherwise they would have been wiped out by the Emperor's Armada without sweating.  They completely wiped out all human life in the first worlds they hit like Eiroik.  We managed to stop them here, in %s and afterwards constructed this base.  Since then it's been more or less a stalemate."]]
    text[3] = [["This week Collective activity has increased heavily around NGC-7291.  We've been trying to contain them, but reports detect that a scout broke through.  The scout was last detected near %s.  We expect it to not have gone far.  You are to locate the scout and report back to %s in the %s system.  It seems like the Collective is planning something and we want to follow their game a bit more"
 "It is of vital important that you do not engage the drone.  Report back as soon as you locate it."]]
-   text[3] = [[After landing you head to the Empire military headquarters and find Sgt. Dimitri there.
+   text[4] = [[After landing you head to the Empire military headquarters and find Sgt. Dimitri there.
 "Well it seems like the drone has some strange fixation with %s.  We aren't quite sure what to make of it, but intelligence is on it.  Report back at the bar in bit and we'll see what we can do about the Collective"]]
    msg_killdrone = "Mission Failed: You weren't supposed to kill the drone!"
 end
@@ -52,7 +52,7 @@ function create()
       misn.setDesc( string.format(misn_desc[1],misn_nearby:name()))
 
       -- Flavour text and mini-briefing
-      tk.msg( title[2], string.format( text[2], misn_base_sys:name() )
+      tk.msg( title[2], string.format( text[2], misn_base_sys:name() ) )
       tk.msg( title[2], string.format( text[3], misn_nearby:name(),
             misn_base:name(), misn_base_sys:name() ))
 
@@ -87,7 +87,7 @@ function land()
    pnt = planet.get()
 
    if misn_stage == 1 and  pnt == misn_base then
-      tk.msg( title[3], string.format(text[3],misn_target:name()) )
+      tk.msg( title[3], string.format(text[4],misn_target:name()) )
       player.modFaction("Empire",5)
       misn.finish(true)
    end
