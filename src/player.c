fef50cffef016b307eef1b94be035b56fd0a240f
Edgar Simo <bobbens@gmail.com>
2009-05-09 19:22:33 +0200

    * Left in DEBUG.

--- src/player.c
+++ src/player.c
@@ -1021,7 +1021,6 @@ void player_think( Pilot* pplayer, const double dt )
 
    /* Gradually zoom in/out. */
    d  = CLAMP(-conf.zoom_speed, conf.zoom_speed, tz - z);
-   DEBUG("d: %f, tz: %f, z :%f", d, tz, z );
    d *= dt / dt_mod; /* Remove dt dependence. */
    if (d < 0) /** Speed up if needed. */
       d *= 2.;
