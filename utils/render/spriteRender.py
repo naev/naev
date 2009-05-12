64317af6f66b4e4389dafe235925cd671dc6b792
Edgar Simo <bobbens@gmail.com>
2009-05-12 18:56:48 +0200

    * Put warning that script is outdated.

--- utils/render/spriteRender.py
+++ utils/render/spriteRender.py
@@ -46,63 +46,66 @@ sliderLight = Create(0)
 # GUI
 ###########################
 def draw() :
-	global Button3, Button2, Button1 
-	global sliderWidth, sliderLight, sliderSpriteSize, sliderObjectSize
-	ypos = 0
+   global Button3, Button2, Button1 
+   global sliderWidth, sliderLight, sliderSpriteSize, sliderObjectSize
+   ypos = 0
 
 
-	glClearColor(0.753, 0.753, 0.753, 0.0)
-	Blender.BGL.glClear(Blender.BGL.GL_COLOR_BUFFER_BIT)
+   glClearColor(0.753, 0.753, 0.753, 0.0)
+   Blender.BGL.glClear(Blender.BGL.GL_COLOR_BUFFER_BIT)
 
-	glColor3f(0.000, 0.000, 0.000)
-	glRasterPos2i(10, ypos)
+   glColor3f(0.000, 0.000, 0.000)
+   glRasterPos2i(10, ypos)
 
-	Button('Exit', 1, 10, ypos, 50, 15, 'Exit script')
-	ypos = ypos + 20
-	Button('Render', 2, 10, ypos, 50, 15, 'Render the ship with current settings')
-	ypos = ypos + 20
-	Button('Test', 3, 10, ypos, 50, 15, 'Test your settings')
-	ypos = ypos + 20
-	Button('Initialize', 4, 10, ypos, 50, 15, 'Initialize the camera and lights')
-	ypos = ypos + 40
+   Button('Exit', 1, 10, ypos, 50, 15, 'Exit script')
+   ypos = ypos + 20
+   Button('Render', 2, 10, ypos, 50, 15, 'Render the ship with current settings')
+   ypos = ypos + 20
+   Button('Test', 3, 10, ypos, 50, 15, 'Test your settings')
+   ypos = ypos + 20
+   Button('Initialize', 4, 10, ypos, 50, 15, 'Initialize the camera and lights')
+   ypos = ypos + 40
 
-	sliderWidth = 300
+   sliderWidth = 300
 
-	sliderLight = Slider('Light Intensity   ', 5, 10, ypos, sliderWidth, 25, sliderLight.val, 1, 10, .1, 'Intensity of the light')
-	ypos = ypos + 40
+   sliderLight = Slider('Light Intensity   ', 5, 10, ypos, sliderWidth, 25, sliderLight.val, 1, 10, .1, 'Intensity of the light')
+   ypos = ypos + 40
 
-	glColor3f(0.000, 0.000, 0.000)
-	glRasterPos2i(10, ypos)
-	Text('3: Finally push the "Render" button to render your frames.')
-	ypos = ypos + 20
-	glRasterPos2i(10, ypos)
-	Text('2: Hit the "Test" button to see if your lighting is good.')
-	ypos = ypos + 20
-	glRasterPos2i(10, ypos)
-	Text('1: Put in your settings and hit the "initialization" button.')
-	ypos = ypos + 20
-	glRasterPos2i(10, ypos)
-	Text('Usage:')
-	ypos = ypos + 20
-	glRasterPos2i(10, ypos)
-	Text('Created for NAEV: https://trac.bobbens.dyndns.org/naev/')
-	ypos = ypos + 20
-	glRasterPos2i(10, ypos)
-	Text('NAEV Sprite Renderer')
+   glColor3f(0.000, 0.000, 0.000)
+   glRasterPos2i(10, ypos)
+   Text('3: Finally push the "Render" button to render your frames.')
+   ypos = ypos + 20
+   glRasterPos2i(10, ypos)
+   Text('2: Hit the "Test" button to see if your lighting is good.')
+   ypos = ypos + 20
+   glRasterPos2i(10, ypos)
+   Text('1: Put in your settings and hit the "initialization" button.')
+   ypos = ypos + 20
+   glRasterPos2i(10, ypos)
+   Text('Usage:')
+   ypos = ypos + 20
+   glRasterPos2i(10, ypos)
+   Text('Created for NAEV: https://trac.bobbens.dyndns.org/naev/')
+   ypos = ypos + 20
+   glRasterPos2i(10, ypos)
+   Text('NAEV Sprite Renderer')
+   ypos = ypos + 20
+   glRasterPos2i(10, ypos)
+   Text('THIS SCRIPT IS OUTDATED PLEASE USE THE STUFF AT http://darkshed.net:9999/bobbens/naev-artwork')
 
 def event(evt, val) :
-	if (evt == QKEY and not val) :
-		Exit()
+   if (evt == QKEY and not val) :
+      Exit()
 
 def bevent(evt) :
-	if evt == 1 :
-		Exit()
-	elif evt == 2 :
-		render()
-	elif evt == 3 :
-		test()
-	elif evt == 4 :
-		initialize()
+   if evt == 1 :
+      Exit()
+   elif evt == 2 :
+      render()
+   elif evt == 3 :
+      test()
+   elif evt == 4 :
+      initialize()
 
 Blender.Draw.Register(draw,event,bevent)
 
@@ -112,97 +115,97 @@ Blender.Draw.Register(draw,event,bevent)
 spriteSize = 500
 
 def initialize() :
-	objList = Blender.Object.Get()
-	
-	####################
-	# orienting the object(s) in the correct position
-	####################
-	for obj in objList :
-		if obj.getType() == 'Mesh' :
-			obj.RotZ = 0
-			obj.RotY = 0
-	#		obj.RotX = 45
+   objList = Blender.Object.Get()
+   
+   ####################
+   # orienting the object(s) in the correct position
+   ####################
+   for obj in objList :
+      if obj.getType() == 'Mesh' :
+         obj.RotZ = 0
+         obj.RotY = 0
+   #     obj.RotX = 45
 
-	##########################
-	# initializing the camera
-	##########################
-	cam = Blender.Object.Get("Camera")
-	cam.LocZ = 9.0
-	cam.LocX = -5.0
-	cam.LocY = 0
-	cam.RotY = 0
-	cam.RotX = 0
-	cam.RotZ = 0
-	cam.RotZ = -(pi / 2)
-	cam.RotX = (pi / 7)
+   ##########################
+   # initializing the camera
+   ##########################
+   cam = Blender.Object.Get("Camera")
+   cam.LocZ = 9.0
+   cam.LocX = -5.0
+   cam.LocY = 0
+   cam.RotY = 0
+   cam.RotX = 0
+   cam.RotZ = 0
+   cam.RotZ = -(pi / 2)
+   cam.RotX = (pi / 7)
 
-	###########################
-	# setting up the lighting, bitches!
-	###########################
-	for obj in objList :
-		if obj.getType() == 'Lamp' :
-			scn.objects.unlink(obj)
-	lamp = Blender.Lamp.New()
-	Lamp1 = Blender.Object.New('Lamp', 'Lamp1')
-	Lamp2 = Blender.Object.New('Lamp', 'Lamp2')
-	Lamp1.LocZ = 4.0
-	Lamp1.LocX = 4.0
-	Lamp1.LocY = 0
-	Lamp2.LocZ = 2.0
-	Lamp2.LocX = -4.0
-	Lamp2.LocY = 0
-	Lamp1.link(lamp)
-	Lamp2.link(lamp)
-	lamp.setEnergy(sliderLight.val)
-	scn.objects.link (Lamp1)
-	scn.objects.link (Lamp2)
+   ###########################
+   # setting up the lighting, bitches!
+   ###########################
+   for obj in objList :
+      if obj.getType() == 'Lamp' :
+         scn.objects.unlink(obj)
+   lamp = Blender.Lamp.New()
+   Lamp1 = Blender.Object.New('Lamp', 'Lamp1')
+   Lamp2 = Blender.Object.New('Lamp', 'Lamp2')
+   Lamp1.LocZ = 4.0
+   Lamp1.LocX = 4.0
+   Lamp1.LocY = 0
+   Lamp2.LocZ = 2.0
+   Lamp2.LocX = -4.0
+   Lamp2.LocY = 0
+   Lamp1.link(lamp)
+   Lamp2.link(lamp)
+   lamp.setEnergy(sliderLight.val)
+   scn.objects.link (Lamp1)
+   scn.objects.link (Lamp2)
 
 def test() :
-	context = scn.getRenderingContext()
-	Render.EnableDispWin()
+   context = scn.getRenderingContext()
+   Render.EnableDispWin()
 
-	context.imageSizeX(spriteSize)
-	context.imageSizeY(spriteSize)
-	context.render()
+   context.imageSizeX(spriteSize)
+   context.imageSizeY(spriteSize)
+   context.render()
 
 def render() :
-	###########################
-	# preparations
-	###########################
-	context = scn.getRenderingContext()
-	Render.EnableDispWin()
-	context.extensions = True
-	context.setImageType(Render.PNG)
-	context.enablePremultiply()
-	context.enableRGBAColor()
-	context.renderPath = "./"
+   ###########################
+   # preparations
+   ###########################
+   context = scn.getRenderingContext()
+   Render.EnableDispWin()
+   context.extensions = True
+   context.setImageType(Render.PNG)
+   context.enablePremultiply()
+   context.enableRGBAColor()
+   context.renderPath = "./"
 
-	objList = Blender.Object.Get()
+   objList = Blender.Object.Get()
 
-	origSX = context.imageSizeX()
-	origSY = context.imageSizeY()
-	context.imageSizeX(spriteSize)
-	context.imageSizeY(spriteSize)
-	
-	###########################
-	# rendering
-	###########################
-	for i in range(0,36) :
-		n = str(i)
-		for obj in objList :
-			if obj.getType() == 'Mesh' :
-				obj.RotZ = ((pi * 2) / 36) * i 
+   origSX = context.imageSizeX()
+   origSY = context.imageSizeY()
+   context.imageSizeX(spriteSize)
+   context.imageSizeY(spriteSize)
+   
+   ###########################
+   # rendering
+   ###########################
+   for i in range(0,36) :
+      n = str(i)
+      for obj in objList :
+         if obj.getType() == 'Mesh' :
+            obj.RotZ = ((pi * 2) / 36) * i 
 
-		spriteName = n.zfill(3)
-		context.render()
-		context.saveRenderedImage(spriteName)
+      spriteName = n.zfill(3)
+      context.render()
+      context.saveRenderedImage(spriteName)
 
-	###########################
-	# returning settings to normal
-	###########################
-	for obj in objList :
-		if obj.getType() == 'Mesh' :
-			obj.RotZ = 0
-	context.imageSizeX(origSX)
-	context.imageSizeY(origSY)
+   ###########################
+   # returning settings to normal
+   ###########################
+   for obj in objList :
+      if obj.getType() == 'Mesh' :
+         obj.RotZ = 0
+   context.imageSizeX(origSX)
+   context.imageSizeY(origSY)
 
