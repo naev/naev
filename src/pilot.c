c6e045e7809d1ee87332ace06dde4d1ce103f97b
Edgar Simo <bobbens@gmail.com>
2009-08-21 00:15:01 +0200

    * Multiple fighter bays work now.

--- src/pilot.c
+++ src/pilot.c
@@ -971,10 +971,8 @@ static int pilot_shootWeapon( Pilot* p, PilotOutfitSlot* w )
 
    /*
     * Fighter bays.
-    *
-    * Must be secondary weapon.
     */
-   else if (outfit_isFighterBay(w->outfit) && (w==p->secondary)) {
+   else if (outfit_isFighterBay(w->outfit)) {
 
       /* Must have ammo left. */
       if ((w->u.ammo.outfit == NULL) || (w->u.ammo.quantity <= 0))
@@ -986,7 +984,7 @@ static int pilot_shootWeapon( Pilot* p, PilotOutfitSlot* w )
 
       w->u.ammo.quantity -= 1; /* we just shot it */
       p->mass_outfit     -= w->u.ammo.outfit->mass;
-      p->secondary->u.ammo.deployed += 1; /* Mark as deployed. */
+      w->u.ammo.deployed += 1; /* Mark as deployed. */
       pilot_updateMass( p );
    }
 
@@ -1217,16 +1215,27 @@ int pilot_dock( Pilot *p, Pilot *target, int deployed )
 
    /* Check to see if target has an available bay. */
    for (i=0; i<target->noutfits; i++) {
+
+      /* Must have outfit. */
       if (target->outfits[i]->outfit == NULL)
          continue;
-      if (outfit_isFighterBay(target->outfits[i]->outfit)) {
-         o = outfit_ammo(target->outfits[i]->outfit);
-         if (outfit_isFighter(o) &&
-               (strcmp(p->ship->name,o->u.fig.ship)==0)) {
-            if (deployed)
-               target->outfits[i]->u.ammo.deployed -= 1;
-            break;
-         }
+
+      /* Must be fighter bay. */
+      if (!outfit_isFighterBay(target->outfits[i]->outfit))
+         continue;
+
+      /* Must have deployed. */
+      if (deployed && (target->outfits[i]->u.ammo.deployed <= 0))
+         continue;
+
+      o = outfit_ammo(target->outfits[i]->outfit);
+
+      /* Try to add fighter. */
+      if (outfit_isFighter(o) &&
+            (strcmp(p->ship->name,o->u.fig.ship)==0)) {
+         if (deployed)
+            target->outfits[i]->u.ammo.deployed -= 1;
+         break;
       }
    }
    if (i >= target->noutfits)
