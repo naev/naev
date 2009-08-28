4abeb9c6e7c1d68cf1a7d06b74c9a21b7eabc2b2
Alexandru Moșoi <brtzsnr@gmail.com>
2009-08-28 10:55:44 +0200

    * Added reading of material.

--- src/object.c
+++ src/object.c
@@ -1,15 +1,15 @@
 #include "object.h"
 
 #include <assert.h>
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <libgen.h>
 #include "SDL_image.h"
 
-#include "log.h"
 #include "array.h"
+#include "gui.h"
+#include "log.h"
 
-#define DELIM " \t"
+#define DELIM " \t\n"
 
 
 typedef struct {
@@ -18,7 +18,7 @@ typedef struct {
 } Vertex;
 
 
-static int readGLfloat(GLfloat *dest)
+static int readGLfloat(GLfloat *dest, int how_many)
 {
    char *token;
    int num = 0;
@@ -29,11 +29,107 @@ static int readGLfloat(GLfloat *dest)
       dest[num++] = d;
    }
 
+   if (how_many)
+      assert(num == how_many);
    return num;
 }
 
 
-static gl_vbo *mesh_loadFromFile( const char *filename, int *num_corners )
+static GLuint texture_loadFromFile( const char *filename )
+{
+   /* Reads image and converts it to RGBA */
+   SDL_Surface *brute = IMG_Load(filename);
+   if (brute == NULL)
+      ERR("Cannot load texture from %s", filename);
+   SDL_Surface *image = SDL_DisplayFormatAlpha(brute);
+
+   GLuint texture;
+   glGenTextures(1, &texture);
+   glBindTexture(GL_TEXTURE_2D, texture);
+
+   glTexImage2D(GL_TEXTURE_2D, 0, 4, image->w, image->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image->pixels);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+   SDL_FreeSurface(brute);
+   SDL_FreeSurface(image);
+   return texture;
+}
+
+static Material *materials_readFromFile( const char *filename )
+{
+   FILE *f = fopen(filename, "r");
+   if (!f)
+      ERR("Cannot open material file |%s| (%p)", filename, f);
+
+   Material *materials = array_create(Material);
+   Material *curr = NULL;
+
+   char line[256];
+   while (fgets(line, sizeof(line), f)) {
+      const char *token;
+      assert("Line too long" && (line[strlen(line) - 1] == '\n'));
+      token = strtok(line, DELIM);
+
+      if (token == NULL) {
+         /* Missing */
+      } else if (strcmp(token, "newmtl") == 0) {
+         token = strtok(NULL, DELIM);
+         curr = &array_grow(&materials);
+         memset(curr, 0x00, sizeof(Material));
+         curr->name = strdup(token);
+      } else if (strcmp(token, "Ns") == 0) {
+         readGLfloat(&curr->Ns, 1);
+      } else if (strcmp(token, "Ni") == 0) {
+         readGLfloat(&curr->Ni, 1);
+      } else if (strcmp(token, "d") == 0) {
+         readGLfloat(&curr->d, 1);
+      } else if (strcmp(token, "Ka") == 0) {
+         readGLfloat(curr->Ka, 3);
+      } else if (strcmp(token, "Kd") == 0) {
+         readGLfloat(curr->Kd, 3);
+      } else if (strcmp(token, "Ks") == 0) {
+         readGLfloat(curr->Ks, 3);
+      } else if (strcmp(token, "map_Kd") == 0) {
+         token = strtok(NULL, DELIM);
+         if (token[0] == '-')
+            ERR("Options not supported for map_Kd");
+
+         /* computes the path to texture */
+         char *copy_filename = strdup(filename);
+         char *dn = dirname(copy_filename);
+         char *texture_filename = malloc(strlen(filename) + 1 + strlen(token) + 1);
+         strcpy(texture_filename, dn);
+         strcat(texture_filename, "/");
+         strcat(texture_filename, token);
+
+         DEBUG("texture_filename = %s", texture_filename);
+         curr->texture = texture_loadFromFile(texture_filename);
+         free(copy_filename);
+         free(texture_filename);
+      } else if (token[0] == '#') {
+         /* Comment */
+      } else {
+         WARN("Can't understand token %s", token);
+      }
+   }
+
+   fclose(f);
+   return materials;
+}
+
+
+/**
+ * @brief Loads object
+ *
+ * Object file format is described here
+ * http://local.wasp.uwa.edu.au/~pbourke/dataformats/obj/
+ *
+ * @param filename base file name
+ * @return and Object containing the 3d model
+ */
+Object *object_loadFromFile( const char *filename )
 {
    GLfloat *vertex = array_create(GLfloat);   /**< vertex coordinates */
    GLfloat *texture = array_create(GLfloat);  /**< texture coordinates */
@@ -43,26 +139,59 @@ static gl_vbo *mesh_loadFromFile( const char *filename, int *num_corners )
    if (!f)
       ERR("Cannot open object file %s", filename);
 
+   Object *object = calloc(1, sizeof(Object));
+   object->meshes = array_create(Mesh);
+   object->materials = NULL;
+   Mesh *curr = &array_grow(&object->meshes);
+
    char line[256];
    while (fgets(line, sizeof(line), f)) {
-      assert("Line too long" && (line[strlen(line) - 1] == '\n'));
-
       const char *token;
+      assert("Line too long" && (line[strlen(line) - 1] == '\n'));
       token = strtok(line, DELIM);
 
-      if (strcmp(token, "v") == 0) {
-         (void)array_grow(&vertex);
-         (void)array_grow(&vertex);
-         (void)array_grow(&vertex);
+      if (token == NULL) {
+         /* Missing */
+      } else if (strcmp(token, "mtllib") == 0) {
+         if (object->materials)
+            ERR("Materials already loaded for object %s", filename);
 
-         int num = readGLfloat(array_end(vertex) - 3);
-         assert("Too few or too many coordinates in vertex." && (num == 3));
+         /* get filename containing the material */
+         token = strtok(NULL, DELIM);
+
+         /* computes the path to materials */
+         char *copy_filename = strdup(filename);
+         char *dn = dirname(copy_filename);
+         char *material_filename = malloc(strlen(filename) + 1 + strlen(token) + 1);
+         strcpy(material_filename, dn);
+         strcat(material_filename, "/");
+         strcat(material_filename, token);
+
+         DEBUG("material_filename = %s", material_filename);
+         object->materials = materials_readFromFile(material_filename);
+         free(copy_filename);
+         free(material_filename);
+      } else if (strcmp(token, "o") == 0) {
+         token = strtok(NULL, DELIM);
+
+         if (curr != NULL) {
+            curr->num_corners = array_size(corners);
+            curr->vbo = gl_vboCreateStatic(
+               array_size(corners) * sizeof(Vertex), corners);
+            array_clear(corners);
+         }
+
+         curr = &array_grow(&object->meshes);
+         curr->name = strdup(token);
+      } else if (strcmp(token, "v") == 0) {
+         (void)array_grow(&vertex);
+         (void)array_grow(&vertex);
+         (void)array_grow(&vertex);
+         readGLfloat(array_end(vertex) - 3, 3);
       } else if (strcmp(token, "vt") == 0) {
          (void)array_grow(&texture);
          (void)array_grow(&texture);
-
-         int num = readGLfloat(array_end(texture) - 2);
-         assert("Too few or too many coordinates in texture." && (num == 2));
+         readGLfloat(array_end(texture) - 2, 2);
       } else if (strcmp(token, "f") == 0) {
          /* XXX reads only the geometric & texture vertices.
           * The standards says corners can also include normal vertices.
@@ -90,71 +219,16 @@ static gl_vbo *mesh_loadFromFile( const char *filename, int *num_corners )
       }
    }
 
-   /* creates the vbo */
-   *num_corners = array_size(corners);
-   gl_vbo* vbo = gl_vboCreateStatic(
+   curr->num_corners = array_size(corners);
+   curr->vbo = gl_vboCreateStatic(
       array_size(corners) * sizeof(Vertex), corners);
+   array_clear(corners);
 
    /* cleans up */
    array_free(vertex);
    array_free(texture);
    array_free(corners);
    fclose(f);
-
-   return vbo;
-}
-
-
-static GLuint texture_loadFromFile( const char *filename )
-{
-   /* Reads image and converts it to RGBA */
-   SDL_Surface *brute = IMG_Load(filename);
-   if (brute == NULL)
-      ERR("Cannot load texture from %s", filename);
-   SDL_Surface *image = SDL_DisplayFormatAlpha(brute);
-
-   GLuint texture;
-   glGenTextures(1, &texture);
-   glBindTexture(GL_TEXTURE_2D, texture);
-
-   glTexImage2D(GL_TEXTURE_2D, 0, 4, image->w, image->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image->pixels);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-
-   SDL_FreeSurface(brute);
-   SDL_FreeSurface(image);
-   return texture;
-}
-
-
-/**
- * @brief Loads object
- *    mesh from filename + '.obj'
- *       and
- *    texture from filename + '.png'
- *
- * Object file format is described here
- * http://local.wasp.uwa.edu.au/~pbourke/dataformats/obj/
- *
- * Normals and material are not understood.
- *
- * @param filename base file name
- * @return and Object containing the 3d model
- */
-Object *object_loadFromFile( const char *filename )
-{
-   const int length = strlen(filename);
-   char temp[length + 5];
-   memcpy(temp, filename, length);
-
-   Object *object = (Object *)malloc(sizeof(Object));
-
-   memcpy(temp + length, ".obj", 5);
-   object->mesh = mesh_loadFromFile(temp, &object->num_corners);
-
-   memcpy(temp + length, ".png", 5);
-   object->texture = texture_loadFromFile(temp);
 
    return object;
 }
@@ -165,9 +239,8 @@ Object *object_loadFromFile( const char *filename )
  */
 void object_free( Object *object )
 {
-   gl_vboDestroy(object->mesh);
-   glDeleteTextures(1, &object->texture);
-   free(object);
+   (void)object;
+  /* XXX */
 }
 
 void object_render( Object *object )
@@ -176,7 +249,7 @@ void object_render( Object *object )
    int ver_offset = (int)(&((Vertex *)NULL)->ver);
    int tex_offset = (int)(&((Vertex *)NULL)->tex);
 
-   /* FIXME how much to scale the object */
+   /* FIXME how much to scale the object? */
    const double scale = 1. / 40.;
 
    /* rotates the object to match projection */
@@ -194,11 +267,12 @@ void object_render( Object *object )
    glPushMatrix();
    glScalef(+1., -1., +1.);
 
-   /* XXX wrong projection */
+   /* XXX changes the projection */
    glMatrixMode(GL_PROJECTION);
    glPushMatrix();
    glLoadIdentity();
 
+#if 0
    /* activates vertices and texture coords */
    gl_vboActivateOffset(object->mesh,
          GL_VERTEX_ARRAY, ver_offset, 3, GL_FLOAT, sizeof(Vertex));
@@ -211,7 +285,7 @@ void object_render( Object *object )
 
    glEnable(GL_TEXTURE_2D);
    glEnable(GL_DEPTH_TEST);
-   glDepthFunc(GL_LESS);  /* this changes the global DepthFunc */
+   glDepthFunc(GL_LESS);  /* XXX this changes the global DepthFunc */
 
    glColor4f(1.0, 1.0, 1.0, 1.0);
    glDrawArrays(GL_TRIANGLES, 0, object->num_corners);
@@ -219,11 +293,37 @@ void object_render( Object *object )
    gl_vboDeactivate();
    glDisable(GL_TEXTURE_2D);
    glDisable(GL_DEPTH_TEST);
+#endif
 
    /* restores all matrices */
    glPopMatrix();
    glMatrixMode(GL_TEXTURE);
    glPopMatrix();
    glMatrixMode(GL_MODELVIEW);
+   glPopMatrix();
+}
+
+
+void object_renderSolid( Object *object, const Solid *solid )
+{
+   double x, y, cx, cy, gx, gy, zoom;
+
+   glMatrixMode(GL_MODELVIEW);
+   glPushMatrix();
+
+   /* get parameters. */
+   gl_cameraGet(&cx, &cy);
+   gui_getOffset(&gx, &gy);
+   gl_cameraZoomGet(&zoom);
+
+   /* calculate position - we'll use relative coords to player */
+   x = (solid->pos.x - cx + gx) * zoom / gl_screen.nw * 2;
+   y = (solid->pos.y - cy + gy) * zoom / gl_screen.nh * 2;
+
+   glTranslatef(x, y, 0.);
+   glRotatef(solid->dir / M_PI * 180. + 90., 0., 0., 1.);
+
+   object_render(object);
+
    glPopMatrix();
 }
--- src/object.h
+++ src/object.h
@@ -6,21 +6,39 @@
 #ifndef OBJECT_H
 #  define OBJECT_H
 
-#include <GL/gl.h>
+#include "SDL.h"
 
 #include "opengl_vbo.h"
+#include "physics.h"
 
 struct gl_vbo;
 
-typedef struct {
-   int num_corners;
-   gl_vbo *object;
+typedef struct Material_ {
+   char *name;
+   GLfloat Ka[3], Kd[3], Ks[3];
+   GLfloat Ns, Ni, d;
    GLuint texture;
+
+   unsigned int has_texture:1;
+} Material;
+
+typedef struct Mesh_ {
+   char *name;
+   int num_corners;
+   gl_vbo *vbo;
+   Material *material;
+} Mesh;
+
+typedef struct Object_ {
+   Mesh *meshes;
+   Material *materials;
 } Object;
 
 
 Object *object_loadFromFile( const char *filename);
 void object_render( Object *object );
+void object_renderSolid( Object *object, const Solid *solid );
+void object_free( Object *object );
 
 
 
