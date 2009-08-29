54a5857991524fbd15fe4cdadbe506bf20632254
Edgar Simo <bobbens@gmail.com>
2009-08-29 18:30:42 +0200

    * Split equip.lua script. * Made more bits common.

---
+++ ai/equip/generic.lua
@@ -0,0 +1,197 @@
+--[[
+-- @brief Does generic pilot equipping
+--
+--    @param p Pilot to equip
+--]]
+function equip_generic( p )
+   -- Get ship info
+   local shiptype, shipsize = equip_getShipBroad( p )
+
+   -- Split by type
+   if shiptype == "civilian" then
+      equip_genericCivilian( p, shipsize )
+   elseif shiptype == "merchant" then
+      equip_genericMerchant( p, shipsize )
+   elseif shiptype == "military" then
+      equip_genericMilitary( p, shipsize )
+   elseif shiptype == "robotic" then
+      equip_genericRobotic( p, shipsize )
+   end
+end
+
+
+--[[
+-- @brief Equips a generic civilian type ship.
+--]]
+function equip_genericCivilian( p, shipsize )
+   local primary, secondary, medium, low, apu
+   local use_primary, use_secondary, use_medium, use_low
+   local nhigh, nmedium, nlow = p:shipSlots()
+
+   -- Defaults
+   medium      = { "Civilian Jammer" }
+   secondary   = { }
+   apu         = { }
+   use_primary = rnd.rnd(nhigh) -- Use fewer slots
+   use_secondary = 0
+   use_medium  = 0
+   use_low     = 0
+
+   -- Per ship type
+   if shipsize == "small" then
+      primary  = equip_forwardLow()
+      medium   = { "Civilian Jammer" }
+      if rnd.rnd() > 0.8 then
+         use_medium = 1
+      end
+   else
+      primary  = equip_turretLow()
+      medium   = { "Civilian Jammer" }
+      if rnd.rnd() > 0.5 then
+         use_medium = 1
+      end
+      low      = { "Plasteel Plating" }
+      if rnd.rnd() > 0.5 then
+         use_low = 1
+      end
+   end
+   equip_ship( p, true, primary, secondary, medium, low, apu,
+               use_primary, use_secondary, use_medium, use_low )
+end
+
+
+--[[
+-- @brief Equips a generic merchant type ship.
+--]]
+function equip_genericMerchant( p, shipsize )
+   local primary, secondary, medium, low, apu
+   local use_primary, use_secondary, use_medium, use_low
+   local nhigh, nmedium, nlow = p:shipSlots()
+
+   -- Defaults
+   medium      = { "Civilian Jammer" }
+   secondary   = { }
+   apu         = { }
+   use_primary = rnd.rnd(1,nhigh) -- Use fewer slots
+   use_secondary = 0
+   use_medium  = 0
+   use_low     = 0
+
+   -- Equip by size
+   if shipsize == "small" then
+      primary  = equip_forwardLow()
+      medium   = { "Civilian Jammer" }
+      if rnd.rnd() > 0.8 then
+         use_medium = 1
+      end
+   elseif shipsize == "medium" then
+      primary  = equip_turretLow()
+      medium   = { "Civilian Jammer" }
+      if rnd.rnd() > 0.6 then
+         use_medium = 1
+      end
+      low    = { "Plasteel Plating" }
+      if rnd.rnd() > 0.6 then
+         use_low = 1
+      end
+   else
+      primary     = equip_turretLow()
+      secondary   = { "EMP Grenade Launcher" }
+      use_seconadry = 1
+      medium = { "Civilian Jammer" }
+      if rnd.rnd() > 0.4 then
+         use_medium = 1
+      end
+      low    = { "Plasteel Plating" }
+      if rnd.rnd() > 0.6 then
+         use_low = 1
+      end
+   end
+   equip_ship( p, true, primary, secondary, medium, low, apu,
+               use_primary, use_secondary, use_medium, use_low )
+end
+
+
+--[[
+-- @brief Equips a generic military type ship.
+--]]
+function equip_genericMilitary( p, shipsize )
+   local primary, secondary, medium, low, apu
+   local use_primary, use_secondary, use_medium, use_low
+   local nhigh, nmedium, nlow = p:shipSlots()
+
+   -- Defaults
+   medium      = { "Civilian Jammer" }
+   secondary   = { }
+   apu         = { }
+
+   -- Equip by size and type
+   if shipsize == "small" then
+      local class = p:shipClass()
+
+      -- Scout
+      if class == "Scout" then
+         primary        = equip_forwardLow ()
+         use_primary    = rnd.rnd(1,#nhigh)
+         medium         = { "Generic Afterburner", "Milspec Jammer" }
+         use_medium     = 2
+         low            = { "Solar Panel" }
+
+      -- Fighter
+      elseif class == "Fighter" then
+         primary        = equip_forwardLow()
+         use_primary    = nhigh-1
+         secondary      = equip_secondaryLow()
+         use_secondary  = 1
+         medium         = equip_mediumLow()
+         low            = equip_lowLow()
+         apu            = equip_apuLow()
+
+      -- Bomber
+      elseif class == "Bomber" then
+         primary        = equip_forwardLow()
+         secondary      = equip_rangedLow()
+         use_primary    = rnd.rnd(1,2)
+         secondary      = nhigh - use_primary
+         use_secondary  = nhigh - use_primary
+         medium         = equip_mediumLow()
+         low            = equip_lowLow()
+         apu            = equip_apuLow()
+      end
+
+   elseif shipsize == "medium" then
+      if rnd.rnd() < 0.6 then
+         primary = equip_forwardMedLow()
+      else
+         primary = equip_turretLow()
+      end
+      secondary      = equip_secondaryMedLow()
+      use_secondary  = rnd.rnd(1,2)
+      use_primary    = nhigh - use_secondary
+      medium         = equip_mediumMed()
+      low            = equip_lowMed()
+      apu            = equip_apuMed()
+
+   else
+      primary        = equip_turretMedLow()
+      secondary      = equip_secondaryMedLow()
+      use_primary    = nhigh-2
+      use_secondary  = 2
+      medium         = equip_mediumHig()
+      low            = equip_lowHig()
+      apu            = equip_apuHig()
+   end
+   equip_ship( p, false, primary, secondary, medium, low, apu,
+               use_primary, use_secondary, use_medium, use_low )
+end
+
+
+--[[
+-- @brief Equips a generic robotic type ship.
+--]]
+function equip_genericRobotic( p, shipsize )
+   equip_fillSlots( p, { "Neutron Disruptor" }, { }, { } )
+end
+
+
+
