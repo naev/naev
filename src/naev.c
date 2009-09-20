49050e87726dd2e2479654708e69a274d96a2093
Unavowed <unavowed@vexillium.org>
2009-09-20 17:18:41 +0100

    Fixed missing feenableexcept linker errors.

--- src/naev.c
+++ src/naev.c
@@ -26,9 +26,11 @@
 
 /* global */
 #include <string.h> /* strdup */
-#if !(HAS_WIN32) && defined(DEBUGGING)
+
+#if defined(HAVE_FENV_H) && defined(DEBUGGING)
 #include <fenv.h>
-#endif /* !(HAS_WIN32) && defined(DEBUGGING) */
+#endif /* defined(HAVE_FENV_H) && defined(DEBUGGING) */
+
 #if HAS_LINUX && defined(DEBUGGING)
 #include <signal.h>
 #include <execinfo.h>
@@ -189,7 +191,7 @@ int main( int argc, char** argv )
    conf_parseCLI( argc, argv ); /* parse CLI arguments */
 
    /* Enable FPU exceptions. */
-#if !(HAS_WIN32) && defined(DEBUGGING)
+#if defined(HAVE_FEENABLEEXCEPT) && defined(DEBUGGING)
    if (conf.fpu_except)
       feenableexcept( FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW );
 #endif /* DEBUGGING */
