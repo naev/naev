25b1105a1a5fc0ea0380c0da993cc87958b62438
Edgar Simo <bobbens@gmail.com>
2009-09-22 22:04:25 +0200

    * Fancier documentation.

--- src/array.h
+++ src/array.h
@@ -85,6 +85,8 @@ __inline__ static void *_array_end_helper(void *a, size_t e_size)
 
 /** 
  * @brief Creates a new dynamic array of `basic_type'
+ *
+ *    @param basic_type Type of the array to create.
  */
 #define array_create(basic_type) \
       ((basic_type *)(_array_create_helper(sizeof(basic_type))))
@@ -92,6 +94,9 @@ __inline__ static void *_array_end_helper(void *a, size_t e_size)
  * @brief Resizes the array to accomodate new_size elements.
  *
  * @note Invalidates all iterators.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @param new_size New size to grow to (in number of elements).
  */
 #define array_resize(ptr_array, new_size) \
    (_array_resize_helper((void **)(ptr_array), sizeof((ptr_array)[0][0]), new_size))
@@ -106,13 +111,20 @@ __inline__ static void *_array_end_helper(void *a, size_t e_size)
  * @brief Adds a new element at the end of the array.
  *
  * @note Invalidates all iterators.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @param element Element being pushed to the back.
  */
 #define array_push_back(ptr_array, element) \
    do array_grow(ptr_array) = element; while (0)
 /**
- * @brief Erases elements in interval [first, last)
+ * @brief Erases elements in interval [first, last).
  * 
  * @note Invalidates all iterators.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @param first First iterator to erase.
+ *    @param last Last iteratior in erase section but is not erased.
  */
 #define array_erase(ptr_array, first, last) \
       (_array_erase_helper((void **)(ptr_array), sizeof((ptr_array)[0][0]), (void *)(first), (void *)(last)))
@@ -120,6 +132,8 @@ __inline__ static void *_array_end_helper(void *a, size_t e_size)
  * @brief Shrinks memory to fit only `size' elements.
  *
  * @note Invalidates all iterators.
+ *
+ *    @param ptr_array Array being manipulated.
  */
 #define array_shrink(ptr_array) \
       (_array_shrink_helper((void **)(ptr_array), sizeof((ptr_array)[0][0])))
@@ -127,34 +141,54 @@ __inline__ static void *_array_end_helper(void *a, size_t e_size)
  * @brief Frees memory allocated and sets array to NULL.
  * 
  * @note Invalidates all iterators.
+ *
+ *    @param ptr_array Array being manipulated.
  */
-#define array_free(array) \
-      _array_free_helper((void *)(array))
+#define array_free(ptr_array) \
+      _array_free_helper((void *)(ptr_array))
 
 /** 
  * @brief Returns number of elements in the array.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @return The size of the array (number of elements).
  */
 #define array_size(array) (_array_private_container(array)->_size)
 /**
  * @brief Returns number of elements reserved.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @return The size of the array (memory usage).
  */
 #define array_reserved(array) (_array_private_container(array)->_reserved)
 /**
  * @brief Returns a pointer to the begining of the reserved memory space.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @return Beginning of memory space.
  */
 #define array_begin(array) (array)
 /**
  * @brief Returns a pointer to the end of the reserved memory space.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @return End of memory space.
  */
 #define array_end(array) ((__typeof__(array))_array_end_helper((array), sizeof((array)[0])))
 /**
  * @brief Returns the first element in the array.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @return The first element in the array.
  */
-#define array_front(a) (*array_begin(a))
+#define array_front(ptr_array) (*array_begin(ptr_array))
 /** 
  * @brief Returns the last element in the array.
+ *
+ *    @param ptr_array Array being manipulated.
+ *    @return The last element in the array.
  */
-#define array_back(a) (*(array_end(a) - 1))
+#define array_back(ptr_array) (*(array_end(ptr_array) - 1))
 
 
 #endif /* ARRAY_H */
