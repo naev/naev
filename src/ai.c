026da29a55f61159095f63a269824fc3f55ab779
Edgar Simo <bobbens@gmail.com>
2009-11-08 19:55:27 +0100

    * Only run equip.lua once per pilot.

--- src/ai.c
+++ src/ai.c
@@ -827,8 +827,8 @@ static void ai_create( Pilot* pilot, char *param )
    /* Prepare AI. */
    ai_setPilot( pilot );
 
-   /* Create equipment first. */
-   if (!pilot_isFlag(pilot, PILOT_EMPTY)) {
+   /* Create equipment first - only if creating for the first time. */
+   if ((aiL_status==AI_STATUS_CREATE) || !pilot_isFlag(pilot, PILOT_EMPTY)) {
       L = equip_L;
       lua_getglobal(L, "equip");
       lp.pilot = cur_pilot->id;
