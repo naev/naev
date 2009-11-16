e81548642bdf56bbd1a1e5c7f848b3e0cd8f931a
Edgar Simo <bobbens@gmail.com>
2009-11-16 13:37:53 +0100

    * More tweaking function names. * Added follow function.

--- ai/tpl/escort.lua
+++ ai/tpl/escort.lua
@@ -16,43 +16,7 @@ end
 
 -- Just tries to guard mem.escort
 function idle ()
-   ai.pushtask(0, "escort")
-end
-
--- Escorts the target
-function escort ()
-   target = mem.escort
-
-   -- Will just float without a target to escort.
-   if not ai.exists(target) then
-      return
-   end
-   
-   dir   = ai.face(target)
-   dist  = ai.dist(target)
-   bdist = ai.minbrakedist()
-
-   -- Close enough.
-   if ai.isstopped() and dist < 300 then
-      return
-
-   -- Brake
-   elseif dist+100 < bdist then
-      ai.pushtask(0, "brake")
-
-   -- Must approach
-   elseif dir < 10 and dist > 300 then
-      ai.accel()
-
-   end
-end
-
--- Just brakes
-function brake ()
-   ai.brake()
-   if ai.isstopped() then
-      ai.poptask()
-   end
+   ai.pushtask(0, "follow", mem.escort)
 end
 
 
@@ -66,10 +30,10 @@ end
 
 -- Tries to fly back to carrier
 function flyback ()
-   target = mem.escort
-   dir    = ai.face(target)
-   dist   = ai.dist(target)
-   bdist  = ai.minbrakedist()
+   local target = mem.escort
+   local dir    = ai.face(target)
+   local dist   = ai.dist(target)
+   local bdist  = ai.minbrakedist()
 
    -- Try to brake
    if not ai.isstopped() and dist < bdist then
