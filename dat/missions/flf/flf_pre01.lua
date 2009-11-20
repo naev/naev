9d5cf3b34e6529f7458a0315dd38501924dc55cc
BTAxis <BTAxis@gmx.net>
2009-11-20 21:18:55 +0100

    * Simplified the mission to prevent inconsistencies.

--- dat/missions/flf/flf_pre01.lua
+++ dat/missions/flf/flf_pre01.lua
@@ -133,16 +133,11 @@ function land()
         misn.jetCargo(gregar)
         misn.finish(true)
     -- Case Dvaered planet
-    elseif planet.cur():faction():name() == "Dvaered" then
+    elseif planet.cur():faction():name() == "Dvaered" and not basefound then
         if tk.yesno(turnintitle[1], turnintext[1]) then
             tk.msg(turnintitle[2], turnintext[2])
             faction.get("Dvaered"):modPlayerRaw(5)
-            if basefound then
-                var.push("flfbase_intro", 3)
-                diff.remove("FLF_base")
-            else
-                var.push("flfbase_intro", 1)
-            end
+            var.push("flfbase_intro", 1)
             var.pop("flfbase_flfshipkilled")
             misn.jetCargo(gregar)
             misn.finish(true)
