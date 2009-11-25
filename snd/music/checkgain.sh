906e87f4c635f20fea50a92d2ddf0b37a4e3df0e
Edgar Simo <bobbens@gmail.com>
2009-11-25 20:20:41 +0100

    * New songs by Saturn6.

--- snd/music/checkgain.sh
+++ snd/music/checkgain.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ "$1" == "-h" ]; then
+   echo "Run vorbisgain on the non-replaygain files to enable replaygain."
+fi
+
 for FILE in *.ogg; do
    if [ -z "$(vorbiscomment -l $FILE | grep REPLAYGAIN_TRACK)" ]; then
       echo $FILE;
