7712ddd51dca5d7177372b2bdb3e2b51f63c703e
Edgar Simo <bobbens@gmail.com>
2010-02-11 18:49:47 +0100

    * Add a bit of custom widget infrastructure for the system editor.

--- src/dev_sysedit.c
+++ src/dev_sysedit.c
@@ -12,13 +12,30 @@
 
 #include "naev.h"
 
+#include "SDL.h"
+
 #include "space.h"
+#include "toolkit.h"
+#include "opengl.h"
+
+
+#define BUTTON_WIDTH    80 /**< Map button width. */
+#define BUTTON_HEIGHT   30 /**< Map button height. */
+
+
+/*
+ * Prototypes.
+ */
+/* Custom system editor widget. */
+static void sysedit_render( double bx, double by, double w, double h, void *data );
+static void sysedit_mouse( unsigned int wid, SDL_Event* event, double mx, double my,
+      double w, double h, void *data );
 
 
 /**
  * @brief Opens the system editor interface.
  */
-int sysedit_open( unsigned int wid_unused, char *unused )
+void sysedit_open( unsigned int wid_unused, char *unused )
 {
    (void) wid_unused;
    (void) unused;
@@ -31,5 +48,26 @@ int sysedit_open( unsigned int wid_unused, char *unused )
    window_addButton( wid, -20, 20, BUTTON_WIDTH, BUTTON_HEIGHT,
          "btnClose", "Close", window_close );
 
-   
+   /* Actual viewport. */
+   window_addCust( wid, 20, -40, SCREEN_W - 60 - BUTTON_WIDTH, SCREEN_H - 60 - BUTTON_HEIGHT,
+         "cstSysEdit", 1, sysedit_render, sysedit_mouse, NULL );
 }
+
+
+/**
+ * @brief System editor custom widget rendering.
+ */
+static void sysedit_render( double bx, double by, double w, double h, void *data )
+{
+}
+
+/**
+ * @brief System editor custom widget mouse handling.
+ */
+static void sysedit_mouse( unsigned int wid, SDL_Event* event, double mx, double my,
+      double w, double h, void *data )
+{
+}
+
+
+
