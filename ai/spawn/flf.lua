195f7043495d41dc2506e7e111fd897ad6350960
BTAxis <BTAxis@gmx.net>
2010-03-14 16:31:21 +0100

    * Spawn scripts for most factions (Sirius and Za'lek using Empire for now). * Added and removed fleets as necessary.

---
+++ ai/spawn/flf.lua
@@ -0,0 +1,88 @@
+include("ai/spawn/common.lua")
+
+
+-- @brief Spawns a small fleet.
+function spawn_patrol ()
+    local pilots = {}
+    local r = rnd.rnd()
+
+    if r < 0.5 then
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+    elseif r < 0.8 then
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+    else
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+    end
+
+    return pilots
+end
+
+
+-- @brief Spawns a medium sized squadron.
+function spawn_squad ()
+    local pilots = {}
+    local r = rnd.rnd()
+
+    if r < 0.5 then
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+    elseif r < 0.8 then
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+    else
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Lancelot", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+       scom.addPilot( pilots, "FLF Vendetta", 15 );
+    end
+
+    return pilots
+end
+
+
+-- @brief Creation hook.
+function create ( max )
+    local weights = {}
+
+    -- Create weights for spawn table
+    weights[ spawn_patrol  ] = 100
+    weights[ spawn_squad   ] = 0.33*max
+   
+    -- Create spawn table base on weights
+    spawn_table = scom.createSpawnTable( weights )
+
+    -- Calculate spawn data
+    spawn_data = scom.choose( spawn_table )
+
+    return scom.calcNextSpawn( 0, scom.presence(spawn_data), max )
+end
+
+
+-- @brief Spawning hook
+function spawn ( presence, max )
+    local pilots
+
+    -- Over limit
+    if presence > max then
+       return 5
+    end
+  
+    -- Actually spawn the pilots
+    pilots = scom.spawn( spawn_data )
+
+    -- Calculate spawn data
+    spawn_data = scom.choose( spawn_table )
+
+    return scom.calcNextSpawn( presence, scom.presence(spawn_data), max ), pilots
+end
+
