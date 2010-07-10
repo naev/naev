e9bf7e67134ec96dc6f96acc97ff0f073323d6d9
Edgar Simo <bobbens@gmail.com>
2010-07-10 11:44:40 +0200

    * Put csv in a directory. * .gitignore the csv directory.

--- src/dev.c
+++ src/dev.c
@@ -15,21 +15,32 @@
 #include "SDL.h"
 
 #include "log.h"
+#include "nfile.h"
 #include "dev_outfit.h"
 #include "dev_ship.h"
 
 
+#define CSV_DIR      "naev_csv"
+
+
 /**
+ * @brief Generates the naev CSV stuff.
  */
 void dev_csv (void)
 {
    DEBUG("Generating CSV data...");
 
+   /* Create directory. */
+   if (nfile_dirMakeExist( CSV_DIR )) {
+      WARN( "Unable to generate 'naev_csv' dir." );
+      return;
+   }
+
    DEBUG("   bolt.csv...");
-   dout_csvBolt( "bolt.csv" );
+   dout_csvBolt( CSV_DIR"/bolt.csv" );
 
    DEBUG("   ship.csv...");
-   dship_csv( "ship.csv" );
+   dship_csv( CSV_DIR"/ship.csv" );
 
    DEBUG("Generation complete!");
 }
