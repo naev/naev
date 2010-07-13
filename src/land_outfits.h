298ab0ee690b6320bd9ce76fa2ae258af4bdf8c6
Edgar Simo <bobbens@gmail.com>
2010-07-13 12:50:41 +0200

    * Split outfits code from land.c.

---
+++ src/land_outfits.h
@@ -0,0 +1,19 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+
+
+
+#ifndef LAND_OUTFITS_H
+#  define LAND_OUTFITS_H
+
+
+#include "outfit.h"
+
+
+void outfits_open( unsigned int wid );
+void outfits_updateQuantities( unsigned int wid );
+void outfits_update( unsigned int wid, char* str );
+
+
+#endif /* LAND_OUTFITS_H */
