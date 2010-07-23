8e600440dae1483738bdf10cfb0ac1170e88dac3
Edgar Simo <bobbens@gmail.com>
2010-07-23 20:13:57 +0200

    * You can now set pilot global hooks.

--- src/pilot.h
+++ src/pilot.h
@@ -481,6 +481,11 @@ void pilot_addHook( Pilot *pilot, int type, unsigned int hook );
 int pilot_runHook( Pilot* p, int hook_type );
 void pilots_rmHook( unsigned int hook );
 void pilot_clearHooks( Pilot *p );
+/* Global hooks. */
+void pilots_addGlobalHook( int type, unsigned int hook );
+void pilots_rmGlobalHook( unsigned int hook );
+void pilots_clearGlobalHooks (void);
+
 
 
 #endif /* PILOT_H */
