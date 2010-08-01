ca10d0bdc62cafbb706c69c371e8d52f4c417695
Edgar Simo <bobbens@gmail.com>
2010-08-01 13:49:42 +0200

    * Start trying to render the old GUI.

--- dat/gui/default.lua
+++ dat/gui/default.lua
@@ -6,9 +6,32 @@
    Run when the GUI is loaded.
 --]]
 function create()
+   -- Get the player
+   pp = player.pilot()
+
+   -- Get screen size
    screen_w, screen_h = gfx.dim()
 
-   tex = tex.open( "gfx/NAEV.png" )
+   -- Load graphics
+   local base = "gfx/gui/"
+   frame    = tex.open( base .. "minimal.png" )
+   energy   = tex.open( base .. "minimal_energy.png" )
+   fuel     = tex.open( base .. "minimal_fuel.png" )
+
+   -- Calculate positions
+   frame_w, frame_h = frame:dim()
+   frame_x  = screen_w - frame_w - 15
+   frame_y  = screen_h - frame_h - 15
+   shield_w = 128
+   shield_h = 7
+   shield_x, shield_y = relativize( 43, 185 )
+   armour_w = 128
+   armour_h = 7
+   armour_x, armour_y = relativize( 43, 199 )
+end
+
+function relativize( x, y )
+   return frame_x + x, frame_y + frame_h - y
 end
 
 
@@ -18,7 +41,15 @@ end
    Run every frame.
 --]]
 function render()
-   gfx.renderTex( tex, 0., 0. )
+
+   -- Frame
+   gfx.renderTex( frame, frame_x, frame_y )
+
+   -- Render health
+   local arm, shi = pp:health()
+   gfx.renderRect( shield_x, shield_y, shi*shield_w, shield_h )
+   gfx.renderRect( armour_x, armour_y, arm*armour_w, armour_h )
+
 end
 
 
