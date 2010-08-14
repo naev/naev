899a90bc26bc0d032c6847ad4ef2b0bbae6df7b4
Deiz <silverwraithii@gmail.com>
2010-08-14 13:27:43 -0400

    Background Lua script updated for use with the nebula images.

--- dat/bkg/default.lua
+++ dat/bkg/default.lua
@@ -1,3 +1,4 @@
+include("scripts/prng.lua")
 
 function background ()
 
@@ -8,16 +9,54 @@ function background ()
       return
    end
 
+   local nebulae = {
+      "nebula01.png",
+      "nebula02.png",
+      "nebula03.png",
+      "nebula04.png",
+      "nebula05.png",
+      "nebula06.png",
+      "nebula07.png",
+      "nebula08.png",
+      "nebula09.png",
+      "nebula10.png",
+      "nebula11.png",
+      "nebula12.png",
+      "nebula13.png",
+      "nebula14.png",
+      "nebula15.png",
+      "nebula16.png",
+      "nebula17.png",
+      "nebula18.png",
+      "nebula19.png",
+      "nebula20.png",
+      "nebula21.png",
+      "nebula22.png",
+      "nebula23.png",
+      "nebula24.png",
+      "nebula25.png",
+      "nebula26.png",
+      "nebula27.png",
+      "nebula28.png",
+      "nebula29.png",
+      "nebula30.png",
+      "nebula31.png",
+      "nebula32.png",
+      "nebula33.png"
+   }
+
+   prng.initHash( system.name(sys) )
 
    -- Set up parameters
-   path  = ""
-   x     = 0
-   y     = 0
+   path  = "gfx/bkg/"
+   local nebula = nebulae[ rnd.rnd(1,#nebulae) ]
+   x     = (prng.num( prng.z ) - .5) * 10000
+   y     = (prng.num( prng.z ) - .5) * 10000
    scale = 1
-   move  = 0.1
+   move  = 0.5
    col   = colour.new()
-   --img = tex.open( path )
-   --bkg.image( img, x, y, move, scale, col )
+   img = tex.open( path .. nebula )
+   bkg.image( img, x, y, move, scale, col )
 
 end
 
