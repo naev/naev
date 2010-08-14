14601648e8b8d9135db6c630c46918d01e288b3d
Edgar Simo <bobbens@gmail.com>
2010-08-14 20:20:20 +0200

    * Speed up script slightly. * Seperate generation.

--- dat/bkg/default.lua
+++ dat/bkg/default.lua
@@ -1,50 +1,46 @@
 include("scripts/prng.lua")
 
-function background ()
+nebulae = {
+   "nebula01.png",
+   "nebula02.png",
+   "nebula03.png",
+   "nebula04.png",
+   "nebula05.png",
+   "nebula06.png",
+   "nebula07.png",
+   "nebula08.png",
+   "nebula09.png",
+   "nebula10.png",
+   "nebula11.png",
+   "nebula12.png",
+   "nebula13.png",
+   "nebula14.png",
+   "nebula15.png",
+   "nebula16.png",
+   "nebula17.png",
+   "nebula18.png",
+   "nebula19.png",
+   "nebula20.png",
+   "nebula21.png",
+   "nebula22.png",
+   "nebula23.png",
+   "nebula24.png",
+   "nebula25.png",
+   "nebula26.png",
+   "nebula27.png",
+   "nebula28.png",
+   "nebula29.png",
+   "nebula30.png",
+   "nebula31.png",
+   "nebula32.png",
+   "nebula33.png"
+}
 
-   -- We can do systems without nebula
+
+function create_nebula ()
+   
+   -- Start up PRNG based on system name for deterministic nebula
    local sys = system.cur()
-   local nebud, nebuv = sys:nebula()
-   if nebud > 0 then
-      return
-   end
-
-   local nebulae = {
-      "nebula01.png",
-      "nebula02.png",
-      "nebula03.png",
-      "nebula04.png",
-      "nebula05.png",
-      "nebula06.png",
-      "nebula07.png",
-      "nebula08.png",
-      "nebula09.png",
-      "nebula10.png",
-      "nebula11.png",
-      "nebula12.png",
-      "nebula13.png",
-      "nebula14.png",
-      "nebula15.png",
-      "nebula16.png",
-      "nebula17.png",
-      "nebula18.png",
-      "nebula19.png",
-      "nebula20.png",
-      "nebula21.png",
-      "nebula22.png",
-      "nebula23.png",
-      "nebula24.png",
-      "nebula25.png",
-      "nebula26.png",
-      "nebula27.png",
-      "nebula28.png",
-      "nebula29.png",
-      "nebula30.png",
-      "nebula31.png",
-      "nebula32.png",
-      "nebula33.png"
-   }
-
    prng.initHash( system.name(sys) )
 
    -- Set up parameters
@@ -60,7 +56,19 @@ function background ()
    local scale = 1 + (prng.num()*0.5 + 0.5)*((2000+2000)/(w+h))
    if scale > 2 then scale = 2 end
    bkg.image( img, x, y, move, scale )
+end
 
+
+function background ()
+
+   -- We can do systems without nebula
+   local sys = system.cur()
+   local nebud, nebuv = sys:nebula()
+   if nebud > 0 then
+      return
+   end
+
+   create_nebula()
 end
 
 
