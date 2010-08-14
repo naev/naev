505deee6662865177caa0d71c5f2b127858672af
Edgar Simo <bobbens@gmail.com>
2010-08-14 17:18:18 +0200

    * Added API to initialize the prng from a string using a hash.

--- scripts/prng.lua
+++ scripts/prng.lua
@@ -12,8 +12,19 @@ function prng.init( num )
    prng.z = num
 end
 
+function prng.initHash( str )
+   hash  = 5381
+   i     = 1
+   bytes = { string.byte( str, 1, string.len(str) ) }
+   for _,c in ipairs(bytes) do
+      hash = hash * 33 + c
+   end
+   prng.z = math.abs( math.fmod( hash, 4294967295 ) )
+end
+
 function prng.num ()
    prng.z = math.abs( math.fmod( prng.z * 279470273, 4294967295 ) )
    return prng.z / 4294967295
 end
+
 
