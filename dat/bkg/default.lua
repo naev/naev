fa202b0023ba4084d8c7a3dbf77172d6bbb66e52
Edgar Simo <bobbens@gmail.com>
2010-08-14 18:16:16 +0200

    * Made variables local so they free themselves when the gc is run.

--- dat/bkg/default.lua
+++ dat/bkg/default.lua
@@ -2,21 +2,21 @@
 function background ()
 
    -- We can do systems without nebula
-   sys = system.cur()
-   nebud, nebuv = sys:nebula()
+   local sys = system.cur()
+   local nebud, nebuv = sys:nebula()
    if nebud > 0 then
       return
    end
 
 
    -- Set up parameters
-   path  = ""
-   x     = 0
-   y     = 0
-   scale = 1
-   move  = 0.1
-   col   = colour.new()
-   --img = tex.open( path )
+   local path  = ""
+   local x     = 0
+   local y     = 0
+   local scale = 1
+   local move  = 0.1
+   local col   = colour.new()
+   --local img = tex.open( path )
    --bkg.image( img, x, y, move, scale, col )
 
 end
