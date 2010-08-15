af2b8959c391da5c7da759bd7696691afde9e5ab
Edgar Simo <bobbens@gmail.com>
2010-08-15 14:52:25 +0200

    * Fixed radar.

--- src/gui.c
+++ src/gui.c
@@ -1037,9 +1037,9 @@ void gui_radarRender( double x, double y )
 
    gl_matrixPush();
    if (radar->shape==RADAR_RECT)
-      gl_matrixTranslate( x + radar->w/2., y - SCREEN_H/2. + radar->h/2. );
+      gl_matrixTranslate( x + radar->w/2., y + radar->h/2. );
    else if (radar->shape==RADAR_CIRCLE)
-      gl_matrixTranslate( x, y - SCREEN_H/2.);
+      gl_matrixTranslate( x, y );
 
    /*
     * planets
