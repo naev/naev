325fdf48eeb6fd82f3ef8c878b825b4754dee0b5
Edgar Simo <bobbens@gmail.com>
2010-08-18 20:07:24 +0200

    * Added Lua camera module basics.

---
+++ src/nlua_camera.h
@@ -0,0 +1,21 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+
+
+#ifndef NLUA_CAMERA_H
+#  define NLUA_CAMERA_H
+
+
+#include "lua.h"
+
+
+/*
+ * Library loading
+ */
+int nlua_loadCamera( lua_State *L, int readonly );
+
+
+#endif /* NLUA_CAMERA_H */
+
+
