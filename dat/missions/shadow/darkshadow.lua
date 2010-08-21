d63530fc5123cef45ea86806bdb336d4a7b6a9b3
BTAxis <BTAxis@gmx.net>
2010-08-21 11:54:02 +0200

    * WIP commit on Dark Shadow.

--- dat/missions/shadow/darkshadow.lua
+++ dat/missions/shadow/darkshadow.lua
@@ -1,4 +1,4 @@
-﻿--[
+--[[
 -- This is the third mission in the "shadow" series, featuring the return of SHITMAN.
 --]]
 
@@ -22,16 +22,30 @@ else -- default english
     You step onto the bridge, where captain Rebina is waiting for you. "Welcome back, %s," she says. "I'm pleased to see that you decided to respond to our communication. I doubt you would have come here if you weren't willing to continue to aid us. Your presence here confirms that you are a reliable partner, so I will treat you accordingly."
     the captain motions you to take a seat at what looks like a holotable in the center of the bridge. "Before I tell you what I've called you here for, I feel I should explain to you in full who we are, what we do and what your part in all this is." She takes a seat opposite from yours, and leans on the holotable. "As I've said before, we are the Four Winds. Our organization is a very secretive one, as you've experienced firsthand. Very few outside our know ranks of our existence, and now you're one of those few."]]
 
-    text[3] = [[    "The Four Winds are old, %s. Very old indeed. The movement dates back to old Earth, before the Space Age, even. We have been with human civilization throughout the ages, at first only in the Eastern nations, later establishing a foothold worldwide. Our purpose was to guide humanity, prevent it from making mistakes it could not afford to make. We were diplomats, scientists, journalists, politicians' spouses, sometimes even assassins. We used any means necessary to gather information and avert disaster, when we could.
+    text[3] = [[    "The Four Winds are old, %s. Very old indeed. The movement dates back to old Earth, before the Space Age, even. We have been with human civilization throughout the ages, at first only in the Eastern nations, later establishing a foothold worldwide. Our purpose was to guide humanity, prevent it from making mistakes it could not afford to make. We never came out in the open, we always worked behind the scenes, from the shadows. We were diplomats, scientists, journalists, politicians' spouses, sometimes even assassins. We used any means necessary to gather information and avert disaster, when we could.
     "Of course, we didn't always succeed. We couldn't prevent the nuclear strikes on Japan, though we managed to prevent several others. We foiled the sabotage attempts on several of the colony ships launched during the First Growth, but sadly failed to do so in Wendigo's case. We failed to stop the Faction Wars, though we managed to help the Empire gain the upper hand. Our most recent failure is the Incident - we should have seen it coming, but we were completely taken by surprise."]]
     
     text[4] = [[    Captain Rebina sits back in her chair and heaves a sigh. "I think that may have been when things started to change. We used to be committed to our purpose, but apparently things are different now. No doubt you remember what happened to the diplomatic exchange between the Empire and the Dvaered some time ago. Well, suffice to say that increasing the tension between the two is definitely not part of our mandate. In fact, it's completely at odds with what we stand for. And that was not just an isolated incident either. Things have been happening that suggest Four Winds involvement, things that bode ill."
-    She activates the holotable, and it displays four cruisers, all seemingly identical to the Seiryuu, though you notice subtle differences in the hull designs. "These are our flagships. Including this ship, they are the Seiryuu, Suzaku, Byakko and Genbu. I'm given to understand that these names, as well as our collective name, have their roots in ancient Oriental mythology." The captain touches another control, and four portraits appear, superimposed over the ships. "These are the four captains of the flagships, which by extension makes them the highest level of authority within the Four Winds. You know me. The other three are called Giornio, Zurike and Farett."]]
+    She activates the holotable, and it displays four cruisers, all seemingly identical to the Seiryuu, though you notice subtle differences in the hull designs. 
+    "These are our flagships. Including this ship, they are the Seiryuu, Suzaku, Byakko and Genbu. I'm given to understand that these names, as well as our collective name, have their roots in ancient Oriental mythology." The captain touches another control, and four portraits appear, superimposed over the ships. "These are the four captains of the flagships, which by extension makes them the highest level of authority within the Four Winds. You know me. The other three are called Giornio, Zurike and Farett."]]
     
     text[5] = [[    "It is my belief that one or more of my fellow captains have abandoned their mission, and are misusing their resources for a different agenda. I have been unable to find out the details of Four Winds missions that I did not order myself, which is a bad sign. I am being stonewalled, and I don't like it. I want to know what's going on, %s, and you're going to help me do it."
     The captain turns the holotable back off so she can have your undivided attention. "I have sent Jorek on a recon mission to the planet of %s in the %s system. He hasn't reported back to me so far, and that's bad news. Jorek is a reliable agent. If he fails to meet a deadline, then it means he is tied down by factors outside of his control, or worse. I want you to find him. Your position as an outsider will help you fly below the radar of potentially hostile Four Winds operatives. You must go to %s and contact Jorek if you can, or find out where he is if you can't."
     Captain Rebina stands up, a signal that this briefing is over. You are seen to your ship by a grey-uniformed crewman. You sit in your cockpit for a few minutes before disengaging the cocking clamp. What captain Rebina has told you is a lot to take in. A shadowy organization that guides humanity behind the scenes? And parts of that organization going rogue? The road ahead could well be a bumpy one.]]
-    
+
+    title[3] = "A tip from the barman"
+    text[6] = [[    You meet the barman's stare. He hesitates for a moment, then speaks up.
+    "Hey man. Are you %s by any chance?"
+    You tell him that yes, that's you, and ask how he knows your name.
+    "Well, your description was given to me by an old friend of mine. His name is Erkil. Do you know him?"
+    You tell him that you don't know anyone by the name of Erkil, but you do know a man named Jorek. The barman visibly relaxes when he hears that name.
+    "Ah, good. You're the real deal then. Can't be too careful in times like these, you know. Anyway, old Jorek was here, but he couldn't stay. He told me to keep an eye out for you, said you'd be coming to look for him." The barman glances around to make sure nobody is within earshot, even though any eavedroppers would have to be practically on top of you, given the bar's noise. "I have a message for you. Go to the %s system and land on %s. Jorek will be waiting for you there. But you better be ready for some trouble. I don't know what kind of trouble it is, but Jorek is never in any kind of minor trouble. Don't say I didn't warn you."
+    You thank the barman, pay for your drink and prepare to head back to your ship, wondering whether your armaments will be enough to deal with whatever trouble Jorek is in.]]
+
+    NPCtitle = "No Jorek"
+    NPCtext = [[You step into the bar, expecting to find Jorek McArthy sitting somewhere at a table. However, you don't see him anywhere. You decide to go for a drink to contemplate your next move. Then, you notice the barman is giving you a curious look.]]
+    NPCdesc = "The barman seems to be eyeing you in particular."
+        
     -- Mission info stuff
     osd_title = {}
     osd_msg   = {}
@@ -44,13 +58,18 @@ else -- default english
 end
 
 function create()
-    var.push("darkshadow_active")
+    var.push("darkshadow_active", true)
     
     seirplanet, seirsys = planet.get("Edergast")
-    jorekplanet1, joreksys1 = planet.get("Edergast") -- TODO
-    jorekplanet2, joreksys2 = planet.get("Edergast") -- TODO
+    jorekplanet1, joreksys1 = planet.get("Manis")
+    jorekplanet2, joreksys2 = planet.get("The Wringer")
     
-    tk.msg(title[1], text[1]:format(player.name(), seirplanet:name(), seirsys:name())
+    if not misn.claim ( {seirsys, joreksys1, joreksys2} ) then
+        print(STDERR, "Claim: aborting Dark Shadow!")
+        abort()
+    end
+    
+    tk.msg(title[1], text[1]:format(player.name(), seirplanet:name(), seirsys:name()))
     accept() -- The player automatically accepts this mission. If he doesn't want it, he'll have to manually abort.
 end
 
@@ -58,6 +77,7 @@ end
 function accept()
     misn.setDesc(misn_desc1:format(seirsys:name(), seirplanet:name()))
     misn.accept()
+    misn.osdDestroy() -- This is here because setDesc initializes the OSD.
 
     stage = 1
 
@@ -70,20 +90,8 @@ function accept2()
     misn.setReward(misn_reward)
     osd_msg[1] = osd_msg[1]:format(jorekplanet1, joreksys1)
     misn.osdCreate(osd_title, osd_msg) 
-end
-
--- Jumpin hook
-function jumpin()
-    if system.cur() == seirsys then
-        seiryuu = pilot.add("Seiryuu", nil, vec2.new(300, 300) + seirplanet:pos())[1]
-        seiryuu:setInvincible(true)
-        seiryuu:disable()
-        if stage == 1 or stage == 3 then
-            hook.pilot(seiryuu, "board", "board")
-        else
-            seiryuu:setNoboard(true)
-        end
-    end
+    marker = misn.markerAdd(joreksys1, "low");
+    landhook = hook.land("land", "bar")
 end
 
 -- Handle boarding of the Seiryuu
@@ -92,13 +100,61 @@ function board()
         tk.msg(title[2], text[2]:format(player.name()))
         tk.msg(title[2], text[3]:format(player.name()))
         tk.msg(title[2], text[4])
-        tk.msg(title[2], text[5]:format(player.name(), jorekplanet1, joreksys1, jorekplanet1))
+        tk.msg(title[2], text[5]:format(player.name(), jorekplanet1:name(), joreksys1:name(), jorekplanet1:name()))
+        player.unboard()
         accept2()
         stage = 2
-    elseif stage == 3 then -- Debriefing
+    elseif stage == 5 then -- Debriefing
         var.pop("darkshadow_active") 
         misn.finish(true)
     end
+end
+
+-- Jumpin hook
+function jumpin()
+    if system.cur() == seirsys then
+        seiryuu = pilot.add("Seiryuu", nil, vec2.new(300, 300) + seirplanet:pos())[1]
+        seiryuu:setInvincible(true)
+        seiryuu:disable()
+        if stage == 1 or stage == 5 then
+            hook.pilot(seiryuu, "board", "board")
+        else
+            seiryuu:setNoboard(true)
+        end
+    end
+end
+
+-- Land hook
+function land()
+    if planet.cur() == jorekplanet1 and stage == 2 then
+        -- Thank you player, but our SHITMAN is in another castle.
+        tk.msg(NPCtitle, NPCtext)
+        misn.npcAdd("barman", "Barman", "thief2", NPCdesc, 4)
+    elseif planet.cur() == jorekplanet2 and stage == 3 then
+        joreknpc = misn.npcAdd("jorek", "Jorek", "jorek", Jordesc, 4)
+    end
+end
+
+-- NPC hook
+function barman()
+    tk.msg(title[3], text[6]:format(player.name(), joreksys2:name(), jorekplanet2:name()))
+    osd_msg[1] = osd_msg[1]:format(jorekplanet2, joreksys2)
+    misn.osdCreate(osd_title, osd_msg)
+    misn.markerMove(marker, joreksys2)
+    stage = 3
+end
+
+-- NPC hook
+function jorek()
+    -- SHITMAN found, inititate blockade run sequence.
+    tk.msg("lele", "Hi, I'm SHITMAN.")
+    misn.npcRm(joreknpc)
+    stage = 4
+    hook.takeoff("takeoff")
+end
+
+-- Takeoff hook
+function takeoff()
 end
 
 -- Handle the unsuccessful end of the mission.
