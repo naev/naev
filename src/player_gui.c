ec00f0552e3b0eb9de673d737f60f8e7aa56fffd
Edgar Simo <bobbens@gmail.com>
2010-08-25 17:57:37 +0200

    * Basic GUI list implementation, never added though.

---
+++ src/player_gui.c
@@ -0,0 +1,97 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+
+/**
+ * @file player_gui.c
+ *
+ * @brief Handles the GUIs the player owns.
+ */
+
+
+#include "player_gui.h"
+
+#include "naev.h"
+
+#include "log.h"
+#include "array.h"
+
+
+
+static char** gui_list = NULL;
+
+
+
+/**
+ * @brief Cleans up the player's GUI list.
+ */
+void player_guiCleanup (void)
+{
+   array_free( &gui_list );
+   gui_list = NULL;
+}
+
+
+/**
+ * @brief Adds a gui to the player.
+ */
+int player_guiAdd( char* name )
+{
+   char *new;
+
+   /* Create new array. */
+   if (gui_list == NULL)
+      gui_list = array_create( char* );
+
+   /* Check if already exists. */
+   if (player_guiCheck(name))
+      return 1;
+
+   new = &array_grow( &gui_list );
+   new = strdup(name);
+   return 0;
+}
+
+
+/**
+ * @brief Removes a player GUI.
+ */
+void player_guiRm( char* name )
+{
+   if (gui_list == NULL)
+      return;
+}
+
+
+/**
+ * @brief Check if player has a GUI.
+ */
+int player_guiCheck( char* name )
+{
+   int i;
+
+   if (gui_list == NULL)
+      return 0;
+
+   for (i=0; i<array_size(gui_list); i++)
+      if (strcmp(gui_list[i],name)==0)
+         return 1;
+
+   return 0;
+}
+
+
+/**
+ * @brief Gets the list of GUIs.
+ */
+char** player_guiList( int *n )
+{
+   if (gui_list == NULL) {
+      *n = 0;
+      return NULL;
+   }
+
+   *n = array_size(gui_list);
+   return gui_list;
+}
+
