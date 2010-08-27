3cc81a759617dbad9d61d615da36b4d2ad8527cf
seanth <seanth@gmail.com>
2010-08-27 09:02:06 -0400

    added a helper script containing the ability to convert strings to tables (split)

---
+++ scripts/string_utils.lua
@@ -0,0 +1,21 @@
+--a function that lets one generate lua tables from a string
+
+function split(str, pat)
+   ---usage: split("1,2,3",",") --> {1,2,3}
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+	 table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
