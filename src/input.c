e06c15b844f73652c3eb4f220f0b5b369fc273ee
Edgar Simo <bobbens@gmail.com>
2010-09-12 20:32:24 +0200

    * Fixed possible issues with input handling and weapon groups.

--- src/input.c
+++ src/input.c
@@ -808,25 +808,25 @@ static void input_key( int keynum, double value, double kabs, int repeat )
          player_rmFlag(PLAYER_SECONDARY);
 
    /* Weapon sets. */
-   } else if (KEY("weapset1") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset1")) {
       player_weapSetPress( 0, value );
-   } else if (KEY("weapset2") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset2")) {
       player_weapSetPress( 1, value );
-   } else if (KEY("weapset3") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset3")) {
       player_weapSetPress( 2, value );
-   } else if (KEY("weapset4") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset4")) {
       player_weapSetPress( 3, value );
-   } else if (KEY("weapset5") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset5")) {
       player_weapSetPress( 4, value );
-   } else if (KEY("weapset6") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset6")) {
       player_weapSetPress( 5, value );
-   } else if (KEY("weapset7") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset7")) {
       player_weapSetPress( 6, value );
-   } else if (KEY("weapset8") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset8")) {
       player_weapSetPress( 7, value );
-   } else if (KEY("weapset9") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset9")) {
       player_weapSetPress( 8, value );
-   } else if (KEY("weapset0") && INGAME() && NODEAD()) {
+   } else if (KEY("weapset0")) {
       player_weapSetPress( 9, value );
 
    /*
