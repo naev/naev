8e867d55aa6892dac021e541157214d0ed4a4f16
Ludovic Bellière <xrogaan@users.sourceforge.net>
2010-10-03 04:03:37 +0200

    fleet reader now compile fleet names and find them at request

--- utils/in-sanity/readers/fleet.py
+++ utils/in-sanity/readers/fleet.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=80:
 
-import os
+import sys
 
 class fleet(readers):
-    def __init__(self, xmlFile, config):
+    def __init__(self, **config):
         fleetXml = os.path.join(config['datpath'], 'fleet.xml')
         readers.__init__(fleetXml, config['verbose'])
+
+        fleetsName = list()
+        sys.stdout.write('Compiling fleet list ...')
+        for fleet in self.xmlData.findall('fleet'):
+            fleetsName.append(fleet.attrib['name'])
+        print "\t\tDONE"
+
+    def find(self, name):
+        if name in self.fleetsName:
+            return True
+        else:
+            return False
+
+
