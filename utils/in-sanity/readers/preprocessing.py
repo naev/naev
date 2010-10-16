7b174f8cef2272e0f08c129e21c92b0ed84101ef
Ludovic Bellière <xrogaan@users.sourceforge.net>
2010-10-16 08:04:23 +0200

    let's comment !

--- utils/in-sanity/readers/preprocessing.py
+++ utils/in-sanity/readers/preprocessing.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=80:
+"""
+This one's got a lot of objects. It intends to create a validation process for
+several xml files. The data could be represented that way:
+    Techs validity:
+    all tech->name must be in assets xml (asset->tech[])
+     - tech->name from tech.xml
+     - asset->tech[] from asset.xml
 
+    Assets validity:
+    all asset->name must be in ssys xml (ssys->assets[])
+     - asset->name from asset.xml
+     - ssys->assets[] from ssys.xml
+
+Let describe them :
+items:
+    Used to be a model. It should makes life easier to tech and asset objects.
+    The ''find`` and ''show_unused`` methods aren't used here.
+
+tech:
+    That one must be instancied from outside, it will initialize all the
+    validation process.
+    Will use asset to validate itself.
+    Then, it will check if tech.xml has got all the ships and outfit available.
+
+asset:
+    Will first validate itself with ''ssys``, then will be used by ''tech`` to
+    validate techs.
+
+ssys:
+    Just used to list assets and be certain they are all used.
+    Is called by ''asset``.
+"""
 import os,sys
 from readers import readers
 
@@ -25,6 +56,9 @@ class items(readers):
             return False
 
     def show_unused(self):
+        """
+        Do not call this method before find !
+        """
         tmp = self.itemNames
         for name in self.usedItem:
             tmp.remove(name)
