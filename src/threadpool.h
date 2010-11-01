37f56e387b0b9c3ba6daa347285df0e479f8d7b2
Reynir Reynisson <reynirr@gmail.com>
2010-11-01 19:45:40 +0100

    Initial work on merging my threadpool into naev.

---
+++ src/threadpool.h
@@ -0,0 +1,42 @@
+/* Copyright 2010 Reynir Reynisson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef THREADPOOL_H
+#  define THREADPOOL_H
+
+typedef struct ThreadQueue_ *ThreadQueue;
+
+int threadpool_init( void );
+int threadpool_newJob( int (*function)(void *), void *data );
+
+/* Creates a new vpool queue */
+ThreadQueue vpool_create( void );
+
+/* Enqueue a job in the vpool queue. Do NOT enqueue a job that has to wait for
+ * another job to be done as this could lead to a deadlock. */
+void vpool_enqueue( ThreadQueue queue, int (*function)(void *), void *data );
+
+/* Run every job in the vpool queue and block untill every job in the queue is
+ * done. It destroys the queue when it's done. */
+void vpool_wait( ThreadQueue queue );
+
+
+
+#endif
+
+#ifndef LOG_H
+#define WARN(str, args...) (fprintf(stderr,"Warning: [%s] "str"\n", __func__, ## args))
+#endif
