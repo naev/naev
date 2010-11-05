ada999b4a8f49b824ff8903081a632b75c3bd61c
Ludovic Bellière <xrogaan@users.sourceforge.net>
2010-11-05 20:20:59 +0100

    first release

--- utils/csv2xml/main.py
+++ utils/csv2xml/main.py
@@ -20,7 +20,17 @@ def main(config):
             outfit = xmlReader.find('expr'.format(cLine['name']))
             for element in outfit.iter():
                 if element.tag in cLine.keys():
-                    # do stuff
+                    element.text = cLine[element.tag]
+                if len(element.attrib) > 0:
+                    for (k, v) in element.attrib:
+                        if k in cLine.keys():
+                            element[k] = cLine[k]
+        else:
+            print('If no outfit, what to do ?')
+    if config.outfit:
+        newxmlname = 'test_' + config.xmlfile
+        print("Since it's beta, please do a diff and validate "+newxmlname)
+        xmlReader.write( 'test_' + config.xmlfile)
 
 
 __version__ = '1.0'
@@ -33,11 +43,15 @@ if __name__ == '__main__':
                         version='%(prog)s '+__version__)
     parser.add_argument('--verbose', action='store_true', default=False,
                         help='Going verbose to see hidden secrets')
+    parser.add_argument('--outfit', '-o', action='store_true',
+                        help='Use this to operate on outfits')
     parser.add_argument('csvfile',
                         help='Path to csv files directory')
     parser.add_argument('xmlfile',
                         help='Path to xml file')
     args = parser.parse_args()
 
-    args.csvfile = os.path.abspath(args['csvfile'])
-    args.xmlfile = os.path.abspath(args['xmlfile'])
+    args.csvfile = os.path.abspath(args.csvfile)
+    args.xmlfile = os.path.abspath(args.xmlfile)
+
+    main(args)
