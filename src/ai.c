c1fb9a3456e9fea878c719935d8b9eb234229bcd
HatlessAtlas <pirierran@hotmail.com>
2010-11-13 07:27:51 -0600

    Function headers and documentation headers for the 0-1 map for target identification
    Signed-off-by: HatlessAtlas <pirierran@hotmail.com>

--- src/ai.c
+++ src/ai.c
@@ -148,6 +148,10 @@ static int ai_loadEquip (void);
 static Task* ai_createTask( lua_State *L, int subtask );
 static int ai_tasktarget( lua_State *L, Task *t );
 
+/*non-lua wrappers*/
+double relsize(int pilot_ID);
+double reldps(int pilot_ID);
+double relhp(int pilot_ID);
 
 /*
  * AI routines for Lua
@@ -228,9 +232,13 @@ static int aiL_shoot( lua_State *L ); /* shoot( number ); number = 1,2,3 */
 static int aiL_hasturrets( lua_State *L ); /* bool hasturrets() */
 static int aiL_getenemy( lua_State *L ); /* number getenemy() */
 static int aiL_getenemy_size( lua_State *L ); /* number getenemy_size() */
+static int aiL_getenemy_heuristic( lua_State *L ); /* number getenemy_heuristic() */
 static int aiL_hostile( lua_State *L ); /* hostile( number ) */
 static int aiL_getweaprange( lua_State *L ); /* number getweaprange() */
 static int aiL_canboard( lua_State *L ); /* boolean canboard( number ) */
+static int aiL_relsize( lua_State *L ); /* boolean relsize( number ) */
+static int aiL_reldps( lua_State *L ); /* boolean reldps( number ) */
+static int aiL_relhp( lua_State *L ); /* boolean relhp( number ) */
 
 /* timers */
 static int aiL_settimer( lua_State *L ); /* settimer( number, number ) */
@@ -324,9 +332,13 @@ static const luaL_reg aiL_methods[] = {
    { "shoot", aiL_shoot },
    { "getenemy", aiL_getenemy },
    { "getenemy_size", aiL_getenemy_size },
+   { "getenemy_heuristic", aiL_getenemy_heuristic },
    { "hostile", aiL_hostile },
    { "getweaprange", aiL_getweaprange },
    { "canboard", aiL_canboard },
+   { "relsize", aiL_relsize },
+   { "reldps", aiL_reldps },
+   { "relhp", aiL_relhp },
    /* timers */
    { "settimer", aiL_settimer },
    { "timeup", aiL_timeup },
@@ -2879,6 +2891,22 @@ static int aiL_getenemy_size( lua_State *L )
    return 1;
 }
 
+
+/*
+ * @brief gets the nearest enemy within specified heuristic
+ *
+ *  @param Mass goal mass map (0-1)
+ *  @param DPS goal DPS map (0-1)
+ *  @param HP goal HP map (0-1)
+ *  @luareturn the best fitting target
+ *  @luafunc getenemy_heuristic()
+ */
+static int aiL_getenemy_heuristic( lua_State *L )
+{
+    return 1;
+    }
+
+
 /*
  * sets the enemy hostile (basically notifies of an impending attack)
  */
@@ -2956,6 +2984,39 @@ static int aiL_canboard( lua_State *L )
    lua_pushboolean(L, !pilot_isFlag(p, PILOT_BOARDED));
    return 1;
 }
+
+/**
+ * @brief Gets the relative size(shipmass) between the current pilot and the specified target
+ *
+ * @param pilot_ID the ID of the pilot whose mass we will compare   
+ *    @luareturn A number from 0 to 1 mapping the relative masses
+ * relsize()
+ */
+static int aiL_relsize( lua_State *L )
+{return 1;}
+
+
+/**
+ * @brief Gets the relative damage output(total DPS) between the current pilot and the specified target
+ *
+ * @param pilot_ID the ID of the pilot whose DPS we will compare   
+ *    @luareturn A number from 0 to 1 mapping the relative DPS's
+ * relsize()
+ */
+static int aiL_reldps( lua_State *L )
+{return 1;}
+
+
+/**
+ * @brief Gets the relative HP(total shields and armor) between the current pilot and the specified target
+ *
+ * @param pilot_ID the ID of the pilot whose HP we will compare   
+ *    @luareturn A number from 0 to 1 mapping the relative HPs
+ * relsize()
+ */
+static int aiL_relhp( lua_State *L )
+{return 1;}
+
 
 
 /**
@@ -3142,6 +3203,42 @@ static int aiL_sysradius( lua_State *L )
    return 1;
 }
 
+
+/**
+ * @brief Gets the relative size(shipmass) between the current pilot and the specified target
+ *
+ * @param pilot_ID the ID of the pilot whose mass we will compare   
+ *    @luareturn A number from 0 to 1 mapping the relative masses
+ * relsize()
+ */
+double relsize(int pilot_ID)
+{
+return 0.5;
+    }
+
+/**
+ * @brief Gets the relative damage output(total DPS) between the current pilot and the specified target
+ *
+ * @param pilot_ID the ID of the pilot whose dps we will compare   
+ *    @luareturn A number from 0 to 1 mapping the relative damage output
+ * relsize()
+ */
+double reldps(int pilot_ID)
+{
+return 0.5;
+    }
+
+/**
+ * @brief Gets the relative hp(combined shields and armor) between the current pilot and the specified target
+ *
+ * @param pilot_ID the ID of the pilot whose shields/armor we will compare   
+ *    @luareturn A number from 0 to 1 mapping the relative HPs
+ * relsize()
+ */
+double relhp(int pilot_ID)
+{
+return 0.5;
+    }
 
 /**
  * @}
