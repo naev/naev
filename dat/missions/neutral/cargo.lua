41691060a20ada9eeb9cf10b7d2669e3d212ea63
Edgar Simo <bobbens@gmail.com>
2010-12-25 21:07:58 +0100

    * Extracted common stuff from cargo.lua and cargo_rush.lua.

--- dat/missions/neutral/cargo.lua
+++ dat/missions/neutral/cargo.lua
@@ -4,6 +4,7 @@
 --]]
 
 include "scripts/jumpdist.lua"
+include "scripts/cargo_common.lua"
 
 lang = naev.lang()
 if lang == "es" then
@@ -56,66 +57,8 @@ end
 
 -- Create the mission
 function create()
-    -- Note: this mission does not make any system claims. 
-    origin_p, origin_s = planet.cur()
-    
-    -- Select mission tier.
-    local tier = rnd.rnd(0, 4)
-    
-    -- Find an inhabited planet 0-3 jumps away.
-    -- Farther distances have a lower chance of appearing.
-    
-    local seed = rnd.rnd()
-    if     seed < 0.30 then missdist = 0
-    elseif seed < 0.60 then missdist = 1
-    elseif seed < 0.80 then missdist = 2
-    else                    missdist = 3
-    end
-    
-    local planets = {}
-    getsysatdistance(system.cur(), missdist, missdist,
-        function(s)
-            for i, v in ipairs(s:planets()) do
-                if v:services()["inhabited"] and v ~= planet.cur() and v:class() ~= 0 then
-                    planets[#planets + 1] = {v, s}
-                end
-           end
-           return true
-        end)
-
-    if #planets == 0 then
-        abort()
-    end
-    
-    index = rnd.rnd(1, #planets)
-    destplanet = planets[index][1]
-    destsys = planets[index][2]
-    
-    -- We have a destination, now we need to calculate how far away it is by simulating the journey there.
-    -- Assume shortest route with no interruptions.
-    -- This is used to calculate the reward.
-    local routesys = origin_s
-    local routepos = origin_p:pos()
-    traveldist = 0
-    numjumps = origin_s:jumpDist(destsys)
-    
-    while routesys ~= destsys do
-        -- We're not in the destination system yet.
-        -- So, get the next system on the route, and the distance between our entry point and the jump point to the next system.
-        -- Then, set the exit jump point as the next entry point.
-        local tempsys = getNextSystem(routesys, destsys)
-        traveldist = traveldist + vec2.dist(routepos, routesys:jumpPos(tempsys))
-        routepos = tempsys:jumpPos(routesys)
-        routesys = tempsys
-    end
-    -- We ARE in the destination system now, so route from the entry point to the destination planet.
-    traveldist = traveldist + vec2.dist(routepos, destplanet:pos())
-    
-    -- We now know where. But we don't know what yet. Randomly choose a commodity type.
-    -- TODO: I'm using the standard cargo types for now, but this should be changed to custom cargo once local-defined commodities are implemented.
-    local cargoes = {"Food", "Industrial Goods", "Medicine", "Luxury Goods", "Ore"}
-    cargo = cargoes[rnd.rnd(1, #cargoes)]
-    
+    -- Calculate the route, distance, jumps and cargo to take
+    destplanet, destsys, njumps, traveldist, cargo, tier = cargo_calculateRoute()
     
     -- Choose amount of cargo and mission reward. This depends on the mission tier.
     -- Note: Pay is independent from amount by design! Not all deals are equally attractive!
@@ -142,24 +85,6 @@ function accept()
     misn.osdCreate(osd_title, {osd_msg:format(destplanet:name(), destsys:name())})
     hook.land("land")
 end
-
--- Choose the next system to jump to on the route from system nowsys to system finalsys.
-function getNextSystem(nowsys, finalsys)
-    if nowsys == finalsys then
-        return nowsys
-    else
-        local neighs = nowsys:adjacentSystems()
-        local nearest = -1
-        local mynextsys = finalsys
-        for _, j in pairs(neighs) do
-            if nearest == -1 or j:jumpDist(finalsys) < nearest then
-                nearest = j:jumpDist(finalsys)
-                mynextsys = j
-            end
-        end
-        return mynextsys
-    end
-end 
 
 -- Land hook
 function land()
