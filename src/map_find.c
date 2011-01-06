74693951ec911489bac41f627a83d93dc6559378
Edgar Simo <bobbens@gmail.com>
2011-01-06 18:06:31 +0100

    * Find window is more sexy now.

--- src/map_find.c
+++ src/map_find.c
@@ -82,36 +82,45 @@ void map_inputFind( unsigned int parent, char* str )
 {
    (void) str;
    unsigned int wid;
-   int y;
+   int x, y;
 
    /* Create the window. */
-   wid = window_create( "Find...", -1, -1, 600, 400 );
+   wid = window_create( "Find...", -1, -1, 300, 220 );
    window_setAccept( wid, map_findSearch );
    window_setCancel( wid, map_findClose );
    window_setParent( wid, parent );
 
    /* Text. */
-   y = -20;
-   window_addText( wid, 20, y, 100, gl_defFont.h+4, 0,
+   y = -40;
+   window_addText( wid, 20, y, 300, gl_defFont.h+4, 0,
          "txtDescription", &gl_defFont, &cDConsole,
          "Enter keyword to search for:" );
-   y -= 20;
+   y -= 30;
 
    /* Create input. */
-   window_addInput( wid, 40, y, 100, gl_defFont.h+4,
+   window_addInput( wid, 30, y, 240, 20,
          "inpSearch", 32, 1, &gl_defFont );
+   y -= 40;
 
    /* Create buttons. */
-   window_addButton( wid, 160, y, BUTTON_WIDTH, BUTTON_HEIGHT,
+   window_addButton( wid,300-BUTTON_WIDTH-30, 20+BUTTON_HEIGHT+20, BUTTON_WIDTH, BUTTON_HEIGHT,
          "btnSearch", "Find", map_findSearch );
-   y -= 20;
+   window_addButton( wid,300-BUTTON_WIDTH-30, 20, BUTTON_WIDTH, BUTTON_HEIGHT,
+         "btnClose", "Close", map_findClose );
 
    /* Create check boxes. */
-   window_addCheckbox( wid, 20, y, 100, 20,
+   x = 40;
+   window_addCheckbox( wid, x, y, 100, 20,
          "chkSystem", "Systems", NULL, 1 );
    y -= 20;
-   window_addCheckbox( wid, 20, y, 100, 20,
+   window_addCheckbox( wid, x, y, 100, 20,
          "chkPlanet", "Planets", NULL, 1 );
+   y -= 20;
+   window_addCheckbox( wid, x, y, 100, 20,
+         "chkOutfit", "Outfits", NULL, 1 );
+   y -= 20;
+   window_addCheckbox( wid, x, y, 100, 20,
+         "chkShip", "Ships", NULL, 1 );
    y -= 20;
 }
 
