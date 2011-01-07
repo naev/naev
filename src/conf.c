abec09e70400b00912ebe773d610112c1eccd624
Edgar Simo <bobbens@gmail.com>
2011-01-07 20:08:33 +0100

    * Split nstrnstr to nstring.c.

--- src/conf.c
+++ src/conf.c
@@ -24,6 +24,7 @@
 #include "nebula.h"
 #include "ndata.h"
 #include "nfile.h"
+#include "nstring.h"
 
 
 #define  conf_loadInt(n,i)    \
@@ -676,47 +677,6 @@ static size_t quoteLuaString(char *str, size_t size, const char *text)
 
    /* return the amount of characters written */
    return count;
-}
-
-
-/**
- * @brief A bounded version of strstr
- *
- *    @param haystack The string to search in
- *    @param size The size of haystack
- *    @param needle The string to search for
- *    @return A pointer to the first occurrence of needle in haystack, or NULL
- */
-static const char *nstrnstr(const char *haystack, const char *needle, size_t size)
-{
-   size_t needlesize;
-   const char *i, *j, *k, *end, *giveup;
-
-   needlesize = strlen(needle);
-   /* We can give up if needle is empty, or haystack can never contain it */
-   if (needlesize == 0 || needlesize > size)
-      return NULL;
-   /* The pointer value that marks the end of haystack */
-   end = haystack + size;
-   /* The maximum value of i, because beyond this haystack cannot contain needle */
-   giveup = end - needlesize + 1;
-
-   /* i is used to iterate over haystack */
-   for (i = haystack; i != giveup; i++) {
-      /* j is used to iterate over part of haystack during comparison */
-      /* k is used to iterate over needle during comparison */
-      for (j = i, k = needle; j != end && *k != '\0'; j++, k++) {
-         /* Bail on the first character that doesn't match */
-         if (*j != *k)
-            break;
-      }
-      /* If we've reached the end of needle, we've found a match */
-      /* i contains the start of our match */
-      if (*k == '\0')
-         return i;
-   }
-   /* Fell through the loops, nothing found */
-   return NULL;
 }
 
 
---
+++ src/nstring.h
@@ -0,0 +1,17 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+
+
+#ifndef NSTRING_H
+#  define NSTRING_H
+
+
+#include <stdlib.h>
+
+
+const char *nstrnstr( const char *haystack, const char *needle, size_t size );
+
+
+#endif /* NSTRING_H */
+
