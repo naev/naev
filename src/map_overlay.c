c8754c75a6a93b7444b14438d4b8ffaedc8ed44b
Deiz <silverwraithii@gmail.com>
2011-03-21 22:09:18 -0400

    Improved overlay mouse input handling.

--- src/map_overlay.c
+++ src/map_overlay.c
@@ -69,8 +69,8 @@ int ovr_input( SDL_Event *event )
    unsigned int pid;
    Pilot *p;
    int mx, my;
-   double x, y, r;
-   double d;
+   double x, y, r, rp;
+   double d, dp;
    Planet *pnt;
    JumpPoint *jp;
    int pntid, jpid, ret;
@@ -94,41 +94,39 @@ int ovr_input( SDL_Event *event )
       x  = ((double)mx - SCREEN_W/2.) * ovr_res;
       y  = ((double)my - SCREEN_H/2.) * ovr_res;
 
-      /* Get closest pilot. */
+      /* Get nearest pilot and jump point/planet. */
       pid = pilot_getNearestPos( player.p, x, y, 1 );
-      p   = pilot_get(pid);
-      r   = MAX( 1.5 * PILOT_SIZE_APROX * p->ship->gfx_space->sw / 2, 100.*ovr_res );
-
-      /* Here we want the closest pilot, only if he isn't targetted. If he is we'll then
-       * focus on targetting features like planets or jump points. */
-      if ((player.p->target != pid) &&
-            (pow2(x-p->solid->pos.x) + pow2(y-p->solid->pos.y) < pow2(r))) {
-         player_targetSet( pid );
-         return 1;
-      }
-
-      /* Get closest planet and/or jump point. */
       system_getClosest( cur_system, &pntid, &jpid, x, y );
-      ret = 0;
-      /* Planet is closest. */
-      if (pntid >= 0) {
+      p   = pilot_get(pid);
+      rp = MAX( 1.5 * PILOT_SIZE_APROX * p->ship->gfx_space->sw / 2, 20.*ovr_res );
+      dp = pow2(x-p->solid->pos.x) + pow2(y-p->solid->pos.y);
+
+      if (pntid >=0) { /* Planet is closer. */
          pnt = cur_system->planets[ pntid ];
          d  = pow2(x-pnt->pos.x) + pow2(y-pnt->pos.y);
-         r  = MAX( 1.5 * pnt->radius, 100. * ovr_res );
-         if (d < pow2(r)) {
-            player_targetPlanetSet( pntid );
-            ret = 1;
-         }
+         r  = MAX( 1.5 * pnt->radius, 20. * ovr_res );
       }
-      /* Jump point is closest. */
       else if (jpid >= 0) {
          jp = &cur_system->jumps[ jpid ];
          d  = pow2(x-jp->pos.x) + pow2(y-jp->pos.y);
-         r  = MAX( 1.5 * jp->radius, 100. * ovr_res );
-         if (d < pow2(r)) {
-            player_targetHyperspaceSet( jpid );
-            ret = 1;
-         }
+         r  = MAX( 1.5 * jp->radius, 20. * ovr_res );
+      }
+
+      ret = 0;
+      /* Pilot is closest, or new jump point/planet is the same as the old. */
+      if ((dp < d && dp < pow2(rp) && player.p->target != pid) ||
+            ((pntid >=0 && player.p->nav_planet == pntid) ||
+            (jpid >=0 && player.p->nav_planet == jpid))) {
+         player_targetSet( pid );
+         ret = 1;
+      }
+      else if ((pntid >= 0) && (d < pow2(r))) { /* Planet is closest. */
+         player_targetPlanetSet( pntid );
+         ret = 1;
+      }
+      else if ((jpid >= 0) && (d < pow2(r))) { /* Jump point is closest. */
+         player_targetHyperspaceSet( jpid );
+         ret =  1;
       }
       return ret;
    }
