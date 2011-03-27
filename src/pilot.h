2af268c3e24f9c239a443f3b1b5df57a7a70d863
Deiz <silverwraithii@gmail.com>
2011-03-27 07:55:30 -0400

    Return distance when getting nearest pilot by distance or angle.

--- src/pilot.h
+++ src/pilot.h
@@ -398,8 +398,8 @@ unsigned int pilot_getNearestEnemy_size( const Pilot* p, int target_mass_LB, int
 unsigned int pilot_getNearestEnemy_heuristic(const Pilot* p, double mass_factor, double health_factor, double damage_factor, double range_factor);
 unsigned int pilot_getNearestHostile (void); /* only for the player */
 unsigned int pilot_getNearestPilot( const Pilot* p );
-unsigned int pilot_getNearestPos( const Pilot *p, double x, double y, int disabled );
-unsigned int pilot_getNearestAng( const Pilot *p, double ang, int disabled );
+double pilot_getNearestPos( const Pilot *p, unsigned int *tp, double x, double y, int disabled );
+double pilot_getNearestAng( const Pilot *p, unsigned int *tp, double ang, int disabled );
 int pilot_getJumps( const Pilot* p );
 
 /*non-lua wrappers*/
