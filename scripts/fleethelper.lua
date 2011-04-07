8943c0b715cf8e8f10a776e194461eae50bd95da
Deiz <silverwraithii@gmail.com>
2011-04-07 00:53:19 -0400

    Added helper script for generating and modifying raw ships.

---
+++ scripts/fleethelper.lua
@@ -0,0 +1,94 @@
+function _buildDupeTable( input, count )
+   local tmp = {}
+   if type(input) == "table" then
+      if #input ~= count then
+         print("Warning: Tables are different lengths.")
+      end
+      return input
+   else
+      for i=1,count do
+         tmp[i] = input
+      end
+      return tmp
+   end
+end
+
+
+-- Wrapper for pilot.add() that can operate on tables of fleets.
+function addShips( ship, ai, location, count )
+   local ais = {}
+   local locations = {}
+   local out = {}
+
+   if type(ship) ~= "table" and type(ship) ~= "string" then
+      print("addShips: Error, ship list is not a fleet or table of fleets!")
+      return
+   elseif type(ship) == "string" then -- Put lone fleet into table.
+      ship = { ship }
+   end
+   ais       = _buildDupeTable( ai, #ship )
+   locations = _buildDupeTable( location, #ship )
+
+   if count == nil then
+      count = 1
+   end
+   for i=1,count do -- Repeat the pattern as necessary.
+      for k,v in ipairs(ship) do
+         out[k+(i-1)*#ship] = pilot.add( ship[k], ais[k], locations[k] )[1]
+      end
+   end
+   return out
+end
+
+
+-- Wrapper for pilot.addRaw() that can operate on tables of ships.
+function addRawShips( ship, ai, location, faction, count )
+   local ais = {}
+   local locations = {}
+   local factions = {}
+   local out = {}
+
+   if type(ship) ~= "table" and type(ship) ~= "string" then
+      print("addRawShips: Error, ship list is not a ship or table of ships!")
+      return
+   elseif type(ship) == "string" then -- Put lone ship into table.
+      ship = { ship }
+   end
+   ais       = _buildDupeTable( ai, #ship )
+   locations = _buildDupeTable( location, #ship )
+   factions  = _buildDupeTable( faction, #ship )
+   if factions[1] == nil then
+      print("addRawShips: Error, raw ships must have factions!")
+      return
+   end
+
+   if count == nil then
+      count = 1
+   end
+   for i=1,count do -- Repeat the pattern as necessary.
+      for k,v in ipairs(ship) do
+         out[k+(i-1)*#ship] = pilot.addRaw( ship[k], ais[k], locations[k], factions[k] )[1]
+      end
+   end
+   return out
+end
+
+
+-- Renames ships with full regex support.
+function renameShips( ship, match, replace, limit )
+   if type(ship) ~= "table" and type(ship) ~= "userdata" then
+      print("renameShips: Error, ship list is not a pilot or table of pilots!")
+      return
+   elseif type(ship) == "userdata" then -- Put lone pilot into table.
+      ship = { ship }
+   end
+
+   if match == nil or replace == nil then
+      print("renameShips: Error, need a pattern to match and one to replace!")
+      return
+   end
+
+   for k,v in ipairs(ship) do
+      v:rename(string.gsub( tostring(v:name()), match, replace, limit ))
+   end
+end
