25e8a2b985acd082cca7dce8740ec6b270b18cf7
Deiz <silverwraithii@gmail.com>
2011-04-08 04:20:45 -0400

    Added position randomization to the helper script's pilot.add() wrapper.

--- scripts/fleethelper.lua
+++ scripts/fleethelper.lua
@@ -14,6 +14,41 @@ function _buildDupeTable( input, count )
 end
 
 
+function _mergeTables( old, new )
+   if type(old) ~= "table" or type(new) ~= "table" then
+      print("_mergeTables: Error, this function only accepts tables.")
+   end
+
+   for k,v in ipairs(new) do
+      table.insert(old, v )
+   end
+   return old
+end
+
+
+-- Randomize the locations of ships in the same manner than pilot.add() does.
+function _randomizePositions( ship, override )
+   if type(ship) ~= "table" and type(ship) ~= "userdata" then
+      print("_randomizePositions: Error, ship list is not a pilot or table of pilots!")
+      return
+   elseif type(ship) == "userdata" then -- Put lone pilot into table.
+      ship = { ship }
+   end
+
+   local x = 0
+   local y = 0
+   for k,v in ipairs(ship) do
+      if k ~= 1 and not override then
+         if vec2.dist( ship[1]:pos(), v:pos() ) == 0 then
+            x = x + rnd.rnd(75,150) * (rnd.rnd(0,1) - 0.5) * 2
+            y = y + rnd.rnd(75,150) * (rnd.rnd(0,1) - 0.5) * 2
+            v:setPos( v:pos() + vec2.new( x, y ) )
+         end
+      end
+   end
+end
+
+
 -- Wrapper for pilot.add() that can operate on tables of fleets.
 function addShips( ship, ai, location, count )
    local ais = {}
@@ -34,8 +69,11 @@ function addShips( ship, ai, location, count )
    end
    for i=1,count do -- Repeat the pattern as necessary.
       for k,v in ipairs(ship) do
-         out[k+(i-1)*#ship] = pilot.add( ship[k], ais[k], locations[k] )[1]
+         out = _mergeTables( out, pilot.add( ship[k], ais[k], locations[k] ) )
       end
+   end
+   if count > 1 then
+      _randomizePositions( out )
    end
    return out
 end
@@ -69,6 +107,9 @@ function addRawShips( ship, ai, location, faction, count )
       for k,v in ipairs(ship) do
          out[k+(i-1)*#ship] = pilot.addRaw( ship[k], ais[k], locations[k], factions[k] )[1]
       end
+   end
+   if count > 1 then
+      _randomizePositions( out )
    end
    return out
 end
