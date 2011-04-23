10caf762f665f120d8ebf61ef2abca247a152857
Edgar Simo Serra <bobbens@gmail.com>
2011-04-23 16:57:34 +0200

    * Removed unneeded line.

--- src/pilot.c
+++ src/pilot.c
@@ -1802,7 +1802,6 @@ void pilot_init( Pilot* pilot, Ship* ship, const char* name, int faction, const 
    pilot->shield = pilot->shield_max = 1.; /* ditto shield */
    pilot->energy = pilot->energy_max = 1.; /* ditto energy */
    pilot->fuel   = pilot->fuel_max   = 1.; /* ditto fuel */
-   pilot->crew   = pilot->ship->crew;
    pilot_calcStats(pilot);
 
    /* Allocate outfit memory. */
