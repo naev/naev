d58ae96b2111e1b52b841e7721b70f17e85e53a6
Edgar Simo Serra <bobbens@gmail.com>
2011-06-11 13:54:51 +0200

    * Whitespace. * Better doxygen. * Fixed mutex being destroyed too soon. * Minor paranoia.

--- src/threadpool.c
+++ src/threadpool.c
@@ -6,7 +6,10 @@
  *
  * The queue is inspired by this paper (look for the queue with two locks):
  *
- * Maged M. Michael and Michael L. Scott. 1998. Nonblocking algorithms and preemption-safe locking on multiprogrammed shared memory multiprocessors. J. Parallel Distrib. Comput. 51, 1 (May 1998), 1-26. DOI=10.1006/jpdc.1998.1446 http://dx.doi.org/10.1006/jpdc.1998.1446 
+ * Maged M. Michael and Michael L. Scott. 1998. Nonblocking algorithms and
+ * preemption-safe locking on multiprogrammed shared memory multiprocessors. J.
+ * Parallel Distrib. Comput. 51, 1 (May 1998), 1-26. DOI=10.1006/jpdc.1998.1446
+ * http://dx.doi.org/10.1006/jpdc.1998.1446 
  *
  * @ARTICLE{Michael98non-blockingalgorithms,
  *    author = {Maged M. Michael and Michael L. Scott},
@@ -16,6 +19,10 @@
  *    volume = {51},
  *    pages = {1--26},
  * }
+ *
+ * @note The algorithm/strategy for killing idle workers should be moved into
+ *       the threadhandler and it should also be improved (the current strategy
+ *       is probably not very good).
  */
 
 
@@ -114,7 +121,12 @@ static int vpool_worker( void *data );
 
 /**
  * @brief Creates a concurrent queue.
- * @return The ThreadQueue.
+ *
+ * The basic concept is that we have a separate lock for both head and tail, so
+ *  we can operate separately on either head or tail. This lets us enqueue at
+ *  tail and dequeue at the head in true FIFO fashion.
+ *
+ *    @return The ThreadQueue.
  */
 static ThreadQueue* tq_create (void)
 {
@@ -141,8 +153,8 @@ static ThreadQueue* tq_create (void)
 /**
  * @brief Enqueue data to the ThreadQueue q.
  *
- * @param q The queue to be inserted into.
- * @param data The element to be stored in the queue.
+ *    @param q The queue to be inserted into.
+ *    @param data The element to be stored in the queue.
  */
 static void tq_enqueue( ThreadQueue *q, void *data )
 {
@@ -168,11 +180,11 @@ static void tq_enqueue( ThreadQueue *q, void *data )
 
 /**
  * @brief Dequeue from the ThreadQueue q.
+ *
  * @attention The callee should ALWAYS have called SDL_SemWait() on the semaphore.
  *
- * @param q The queue to dequeue from.
- *
- * @return A void pointer to the element from the queue.
+ *    @param q The queue to dequeue from.
+ *    @return A void pointer to the element from the queue.
  */
 static void* tq_dequeue( ThreadQueue *q )
 {
@@ -183,16 +195,20 @@ static void* tq_dequeue( ThreadQueue *q )
    SDL_mutexP( q->h_lock );
 
    /* Start running. */
-   node = q->first;
+   node     = q->first;
+   newhead  = node->next;
 
-   if ((newhead = node->next) == NULL) {
+   /* Head not consistant. */
+   if (newhead == NULL) {
       WARN("Tried to dequeue while the queue was empty!");
       /* Ugly fix :/ */
+      /*
       SDL_mutexV(q->h_lock);
       return NULL;
-   }
-   while((newhead = node->next) == NULL) {
-       /* Wait until the cache updates :/ */
+      */
+      /* We prefer to wait until the cache updates :/ */
+      while(newhead == NULL) {
+      }
    }
 
    /* Remember the value and assign newhead as the new dummy element. */
@@ -207,22 +223,23 @@ static void* tq_dequeue( ThreadQueue *q )
 }
 
 /**
- * @brief Destroy and free a ThreadQueue.
+ * @brief Destroys and frees a ThreadQueue.
+ *
  * Frees all elements too.
  *
- * @param q The ThreadQueue to free.
- * This does not try to lock or anything */
+ *    @param q The ThreadQueue to free.
+ */
 static void tq_destroy( ThreadQueue *q )
 {
+   /* Iterate through the list and free the nodes */
+   while (q->first->next != NULL) {
+      free( tq_dequeue(q) ); /* Locks q->t_lock, so we must destroy mutex after. */
+   }
+
    /* Clean up threading structures. */
    SDL_DestroySemaphore( q->semaphore );
    SDL_DestroyMutex( q->h_lock );
    SDL_DestroyMutex( q->t_lock );
-
-   /* Iterate through the list and free the nodes */
-   while(q->first->next != NULL) {
-      free( tq_dequeue(q) );
-   }
 
    free( q->first );
    free( q );
@@ -231,13 +248,13 @@ static void tq_destroy( ThreadQueue *q )
 
 /**
  * @brief Enqueues a new job for the threadpool.
+ *
  * @warning Do NOT enqueue a job that has to wait for another job to be done as
- * this could lead to a deadlock.
+ *          this could lead to a deadlock.
  *
- * @param function The function (job) to be called (executed).
- * @param data The arguments for the function.
- *
- * @return Returns 0 on success and -2 if there was no threadpool.
+ *    @param function The function (job) to be called (executed).
+ *    @param data The arguments for the function.
+ *    @return Returns 0 on success and -2 if there was no threadpool.
  */
 int threadpool_newJob( int (*function)(void *), void *data )
 {
@@ -263,10 +280,10 @@ int threadpool_newJob( int (*function)(void *), void *data )
  * @brief The worker function for the threadpool.
  *
  * It waits for a signal from the handler. If it receives THREADSIG_STOP it
- * means the worker thread should stop. Else it dequeues a job from the
- * global_queue and executes it.
+ *  means the worker thread should stop. Else it dequeues a job from the
+ *  global_queue and executes it.
  *
- * @param data A pointer to the ThreadData struct used for a lot of stuff.
+ *    @param data A pointer to the ThreadData struct used for a lot of stuff.
  */
 static int threadpool_worker( void *data )
 {
@@ -301,12 +318,8 @@ static int threadpool_worker( void *data )
 
 /**
  * @brief Handles assigning jobs to the workers and killing them if necessary.
- * Stopping the threeadpool_handler is not yet implemented.
  *
- * @param data Not used. SDL threading requires functions to take a void
- * pointer as argument.
- *
- * @return Not really implemented yet.
+ * @note Stopping the threeadpool_handler is not yet implemented.
  *
  * Process is:
  *
@@ -318,6 +331,10 @@ static int threadpool_worker( void *data )
  *      else if stopped threads, reactivate stopped thread
  *      else wait for running thread
  * 4) Go to 1)
+ *
+ *    @param data Not used. SDL threading requires functions to take a void
+ *                pointer as argument.
+ *    @return Not really implemented yet.
  */
 static int threadpool_handler( void *data )
 {
@@ -452,7 +469,7 @@ static int threadpool_handler( void *data )
 /**
  * @brief Initialize the global threadpool.
  *
- * @return Returns 0 on success and -1 if there's already a threadpool.
+ *    @return Returns 0 on success and -1 if there's already a threadpool.
  */
 int threadpool_init (void)
 {
@@ -473,15 +490,17 @@ int threadpool_init (void)
 
 /**
  * @brief Creates a new vpool queue.
- * This is just an interface to make running a number of jobs and then wait for
- * them to finish more pleasant. You should not nest vpools as of now as there
- * are only a limit number of worker threads and we can't have them wait for a
- * thread to finish that doesn't exist.
- * If you really want to sort of nest vpools, you should start a new thread
- * instead of using the threadpool. I might add a vpool_waitInANewThread
- * function some day.
  *
- * @return Returns a ThreadQueue to be used.
+ * This is just an interface to make running a number of jobs and then wait for
+ *  them to finish more pleasant. You should not nest vpools as of now as there
+ *  are only a limit number of worker threads and we can't have them wait for a
+ *  thread to finish that doesn't exist.
+ *
+ * If you really want to sort of nest vpools, you should start a new thread
+ *  instead of using the threadpool. I might add a vpool_waitInANewThread
+ *  function some day.
+ *
+ *    @return Returns a ThreadQueue to be used.
  */
 ThreadQueue* vpool_create (void)
 {
@@ -490,10 +509,12 @@ ThreadQueue* vpool_create (void)
 
 /**
  * @brief Enqueue a job in the vpool queue.
+ *
  * @warning Do NOT enqueue jobs that wait for another job to be done, as this
- * could lead to a deadlock.
+ *          could lead to a deadlock.
+ *
  * @warning Do NOT enqueue jobs that wait for a vpool, as this could lead to a
- * deadlock.
+ *          deadlock.
  */
 void vpool_enqueue( ThreadQueue *queue, int (*function)(void *), void *data )
 {
@@ -504,13 +525,15 @@ void vpool_enqueue( ThreadQueue *queue, int (*function)(void *), void *data )
    node->data     = data;
    node->function = function;
 
+   /* Add to vpool. */
    tq_enqueue( queue, node );
 }
 
 /**
  * @brief A special vpool worker that signals the waiting thread when all jobs
- * are done.
- * It uses a mutex+condition variable+counter
+ *        are done.
+ *
+ * It uses a mutex+condition variable+counter.
  */
 static int vpool_worker( void *data )
 {
@@ -524,7 +547,7 @@ static int vpool_worker( void *data )
    /* Decrement the counter and signal vpool_wait if all threads are done */
    SDL_mutexP( work->mutex );
    *(work->count) = *(work->count) - 1;
-   if (*(work->count) == 0)             /* All jobs are done */
+   if (*(work->count) <= 0)             /* All jobs are done */
       SDL_CondSignal( work->cond );     /* Signal waiting thread */
    SDL_mutexV( work->mutex );
 
@@ -532,7 +555,8 @@ static int vpool_worker( void *data )
 }
 
 /* @brief Run every job in the vpool queue and block until every job in the
- * queue is done.
+ *        queue is done.
+ *
  * @note It destroys the queue when it's done.
  */
 void vpool_wait( ThreadQueue *queue )
@@ -580,15 +604,7 @@ void vpool_wait( ThreadQueue *queue )
    SDL_DestroyMutex( mutex );
    SDL_DestroyCond( cond );
    tq_destroy( queue );
-   free(arg);
+   free( arg );
 }
-
-/**
- * Notes
- *
- * The algorithm/strategy for killing idle workers should be moved into the
- * threadhandler and it should also be improved (the current strategy is
- * probably not very good).
- */
 
 
