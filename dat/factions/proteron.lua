5bd4677832bf95c087907956efe99fc414674871
BTAxis <BTAxis@gmail.com>
2011-06-13 20:22:21 +0200

    * Removed useless distress caps.

--- dat/factions/proteron.lua
+++ dat/factions/proteron.lua
@@ -3,7 +3,6 @@
 include "dat/factions/skel.lua"
 
 
-_fcap_distress = 5 -- Distress cap
 _fcap_kill     = 5 -- Kill cap
 _fdelta_distress = {-0.5, 0} -- Maximum change constraints
 _fdelta_kill     = {-5, 1} -- Maximum change constraints
