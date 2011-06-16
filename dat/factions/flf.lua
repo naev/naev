1e338f3be3fb3d7c99b4443932ea0742f375fdf8
Deiz <silverwraithii@gmail.com>
2011-06-16 16:43:27 -0400

    As FLF now starts neutral, they should have non-zero standing deltas.

--- dat/factions/flf.lua
+++ dat/factions/flf.lua
@@ -4,8 +4,8 @@ include "dat/factions/skel.lua"
 
 
 _fcap_kill     = 25 -- Kill cap
-_fdelta_distress = {0, 0} -- Maximum change constraints
-_fdelta_kill     = {0, 0} -- Maximum change constraints
+_fdelta_distress = {-0.5, 0} -- Maximum change constraints
+_fdelta_kill     = {-5, 1.5} -- Maximum change constraints
 _fcap_misn     = 50 -- Starting mission cap, gets overwritten
 _fcap_misn_var = "_fcap_flf"
 _fthis         = faction.get("FLF")
