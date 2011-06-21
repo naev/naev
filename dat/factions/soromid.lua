9e785dd232c062717a07bde496ff50857f59cbbe
BTAxis <BTAxis@gmail.com>
2011-06-21 12:36:19 +0200

    * Populated another Soromid system. * Soromid reputation used empire cap var, fixed.

--- dat/factions/soromid.lua
+++ dat/factions/soromid.lua
@@ -7,7 +7,7 @@ _fcap_kill     = 15 -- Kill cap
 _fdelta_distress = {-1, 0} -- Maximum change constraints
 _fdelta_kill     = {-5, 1} -- Maximum change constraints
 _fcap_misn     = 30 -- Starting mission cap, gets overwritten
-_fcap_misn_var = "_fcap_empire"
+_fcap_misn_var = "_fcap_soromid"
 _fthis         = faction.get("Soromid")
 
 
