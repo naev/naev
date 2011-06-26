38da03f0b04b0cf3cb0b3841775ad858b70aa92e
BTAxis <BTAxis@gmail.com>
2011-06-26 23:41:19 +0200

    * Use Deiz' version of the soromid spawn script.

--- ai/spawn/soromid.lua
+++ ai/spawn/soromid.lua
@@ -3,106 +3,107 @@ include("ai/spawn/common.lua")
 
 -- @brief Spawns a small patrol fleet.
 function spawn_patrol ()
-    local pilots = {}
-    local r = rnd.rnd()
+   local pilots = {}
+   local r = rnd.rnd()
 
-    if r < 0.5 then
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-    elseif r < 0.8 then
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-    else
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Shaman", 25 );
-    end
+   if r < 0.5 then
+      scom.addPilot( pilots, "Soromid Reaver", 25 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, "Soromid Brigand", 20 );
+      scom.addPilot( pilots, "Soromid Marauder", 25 );
+   else
+      scom.addPilot( pilots, "Soromid Nyx", 75 );
+   end
 
-    return pilots
+   return pilots
 end
 
 
 -- @brief Spawns a medium sized squadron.
 function spawn_squad ()
-    local pilots = {}
-    local r = rnd.rnd()
+   local pilots = {}
+   local r = rnd.rnd()
 
-    if r < 0.5 then
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Shaman", 25 );
-       scom.addPilot( pilots, "Sirius Preacher", 45 );
-    elseif r < 0.8 then
-       scom.addPilot( pilots, "Sirius Preacher", 45 );
-       scom.addPilot( pilots, "Sirius Preacher", 45 );
-    else
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Shaman", 25 );
-       scom.addPilot( pilots, "Sirius Preacher", 45 );
-    end
+   if r < 0.5 then
+      scom.addPilot( pilots, "Soromid Brigand", 20 );
+      scom.addPilot( pilots, "Soromid Marauder", 25 );
+      scom.addPilot( pilots, "Soromid Odium", 45 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, "Soromid Reaver", 25 );
+      scom.addPilot( pilots, "Soromid Odium", 45 );
+   else
+      scom.addPilot( pilots, "Soromid Brigand", 20 );
+      scom.addPilot( pilots, "Soromid Reaver", 25 );
+      scom.addPilot( pilots, "Soromid Nyx", 75 );
+   end
 
-    return pilots
+   return pilots
 end
 
 
 -- @brief Spawns a capship with escorts.
 function spawn_capship ()
-    local pilots = {}
-    local r = rnd.rnd()
+   local pilots = {}
+   local r = rnd.rnd()
 
-    -- Generate the capship
-    if r < 0.5 then
-        scom.addPilot( pilots, "Sirius Dogma", 140 )
-    else
-        scom.addPilot( pilots, "Sirius Divinity", 120 );
-    end
+   -- Generate the capship
+   if r < 0.7 then
+      scom.addPilot( pilots, "Soromid Ira", 140 )
+   else
+      scom.addPilot( pilots, "Soromid Arx", 165 )
+   end
 
-    -- Generate the escorts
-    r = rnd.rnd()
-    if r < 0.5 then
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Shaman", 25 );
-    else
-       scom.addPilot( pilots, "Sirius Fidelity", 20 );
-       scom.addPilot( pilots, "Sirius Preacher", 45 );
-    end
+   -- Generate the escorts
+   r = rnd.rnd()
+   if r < 0.5 then
+      scom.addPilot( pilots, "Soromid Brigand", 20 );
+      scom.addPilot( pilots, "Soromid Marauder", 25 );
+      scom.addPilot( pilots, "Soromid Reaver", 25 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, "Soromid Reaver", 25 );
+      scom.addPilot( pilots, "Soromid Odium", 45 );
+   else
+      scom.addPilot( pilots, "Soromid Reaver", 25 );
+      scom.addPilot( pilots, "Soromid Nyx", 75 );
+   end
 
-    return pilots
+   return pilots
 end
 
 
 -- @brief Creation hook.
 function create ( max )
-    local weights = {}
+   local weights = {}
 
-    -- Create weights for spawn table
+   -- Create weights for spawn table
     weights[ spawn_patrol  ] = 100
     weights[ spawn_squad   ] = math.max(1, -80 + 0.80 * max)
     weights[ spawn_capship ] = math.max(1, -500 + 1.70 * max)
    
-    -- Create spawn table base on weights
-    spawn_table = scom.createSpawnTable( weights )
+   -- Create spawn table base on weights
+   spawn_table = scom.createSpawnTable( weights )
 
-    -- Calculate spawn data
-    spawn_data = scom.choose( spawn_table )
+   -- Calculate spawn data
+   spawn_data = scom.choose( spawn_table )
 
-    return scom.calcNextSpawn( 0, scom.presence(spawn_data), max )
+   return scom.calcNextSpawn( 0, scom.presence(spawn_data), max )
 end
 
 
 -- @brief Spawning hook
 function spawn ( presence, max )
-    local pilots
+   local pilots
 
-    -- Over limit
-    if presence > max then
-       return 5
-    end
+   -- Over limit
+   if presence > max then
+      return 5
+   end
   
-    -- Actually spawn the pilots
-    pilots = scom.spawn( spawn_data )
+   -- Actually spawn the pilots
+   pilots = scom.spawn( spawn_data )
 
-    -- Calculate spawn data
-    spawn_data = scom.choose( spawn_table )
+   -- Calculate spawn data
+   spawn_data = scom.choose( spawn_table )
 
-    return scom.calcNextSpawn( presence, scom.presence(spawn_data), max ), pilots
+   return scom.calcNextSpawn( presence, scom.presence(spawn_data), max ), pilots
 end
