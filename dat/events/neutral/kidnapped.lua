6929bf41e89b279d5768d1690201f947ca189829
Edgar Simo Serra <bobbens@gmail.com>
2011-07-03 18:30:41 +0200

    * First installment of the kidnap campaign by Superkoop.

---
+++ dat/events/neutral/kidnapped.lua
@@ -0,0 +1,34 @@
+--[[ 
+--Event for kidnapped mission.
+--]]
+
+--Create Mom and Dad in their spaceship, and have them come from the planet Brooks in Arcturus system, following the player.
+function create ()
+    panma = pilot.add("Civilian Llama", "civilian", planet.get("Brooks"))[1]
+    panma:control()
+    panma:follow(player.pilot())
+    hook.pilot(panma, "jump", "finish")
+    hook.pilot(panma, "death", "finish")
+    hook.land("finish")
+    hook.jumpout("finish")
+    
+    yohail = hook.timer( 2000., "hailme" );
+end
+
+--Pa and Ma are hailing the player!
+function hailme()
+    panma:hailPlayer()
+    hook.pilot(panma, "hail", "hail")
+end
+
+--Pa and Ma have been hailed. The mission can begin, and panma should land on the planet Brooks
+function hail()
+    panma:control(false)
+    naev.missionStart("Kidnapped")
+    evt.finish(true)
+end
+
+function finish()
+    hook.rm(yohail)
+    evt.finish()
+end
