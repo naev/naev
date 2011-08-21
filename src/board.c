053ce61f6069e445aab2faf68b77e8a387855197
Edgar Simo Serra <bobbens@gmail.com>
2011-08-21 16:08:33 +0200

    * LLVM.

--- src/board.c
+++ src/board.c
@@ -389,11 +389,11 @@ static void board_update( unsigned int wdw )
    /* Fuel. */
    if (p->fuel <= 0.) {
       if (j < PATH_MAX)
-         j += snprintf( &str[j], PATH_MAX-j, "none\n" );
+         snprintf( &str[j], PATH_MAX-j, "none\n" );
    }
    else {
       if (j < PATH_MAX)
-         j += snprintf( &str[j], PATH_MAX-j, "%.0f Units\n", p->fuel );
+         snprintf( &str[j], PATH_MAX-j, "%.0f Units\n", p->fuel );
    }
 
    window_modifyText( wdw, "txtData", str );
