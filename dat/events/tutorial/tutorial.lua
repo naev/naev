bd35a0076ab1ddac7a572bc02475ea6ab4290fb4
BTAxis <BTAxis@gmail.com>
2011-09-14 20:55:03 +0200

    * Shortened tutorial menu code.

--- dat/events/tutorial/tutorial.lua
+++ dat/events/tutorial/tutorial.lua
@@ -36,43 +36,16 @@ function create()
     player.pilot():setNoJump(false)
 
     -- Create menu.
-    _, selection = tk.choice(menutitle, menutext, menubasic, menuinterstellar, menucomms, menubasiccombat, menumisscombat, menudisable, menuplanet, menutrade, menumissions, menux)
+    _, selection = tk.choice(menutitle, menutext, menubasic, menuinterstellar, menucomms, menubasiccombat, menumisscombat, menudisable, menuplanet, menumissions, menux)
     
-    if selection == menubasic then
-        startModule(menubasic)
-    elseif selection == menuinterstellar then
-        startModule(menuinterstellar)
-    elseif selection == menucomms then
-        startModule(menucomms)
-    elseif selection == menubasiccombat then
-        startModule(menubasiccombat)
-    elseif selection == menumisscombat then
-        startModule(menumisscombat)
-    elseif selection == menudisable then
-        startModule(menudisable)
-    elseif selection == menuplanet then
-        startModule(menuplanet)
-    elseif selection == menutrade then
-        placeholder()
-    elseif selection == menumissions then
-        startModule(menumissions)
-    elseif selection == menux then -- Quit to main menu
-        tut.main_menu();
-    else
-        -- This point should never be reached!
-        tk.msg("Error", "You've apparently selected an invalid menu option. This shouldn't happen. Please report.")
-        tut.main_menu();
-    end
-end
-
--- Placeholder not-implemented function
-function placeholder()
-    tk.msg("Not Implemented", "This tutorial has not been implemented yet because the subject matter is still in development.")
-    create()
+    startModule(_G["selection"])
 end
 
 -- Helper function for starting the tutorial modules
 function startModule(module)
+    if selection == menux then -- Quit to main menu
+        tut.main_menu()
+    end
     naev.eventStart(module)
     evt.finish(true) -- While the module is running, the event should not.
 end
