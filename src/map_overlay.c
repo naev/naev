b0165861cc9b2fbd0d6d97d6c8ea836b7847ae42
nloewen <loewen.nathan@gmail.com>
2011-11-30 22:12:08 -0600

    jump point type 3 is invalid. should be 2.

--- src/map_overlay.c
+++ src/map_overlay.c
@@ -273,7 +273,7 @@ void ovr_render( double dt )
 
    /* Render jump points. */
    for (i=0; i<cur_system->njumps; i++)
-      if ((i != player.p->nav_hyperspace) && (cur_system->jumps[i].type != 3))
+      if ((i != player.p->nav_hyperspace) && (cur_system->jumps[i].type != 2))
          gui_renderJumpPoint( i, RADAR_RECT, w, h, res, 1 );
    if (player.p->nav_hyperspace > -1)
       gui_renderJumpPoint( player.p->nav_hyperspace, RADAR_RECT, w, h, res, 1 );
