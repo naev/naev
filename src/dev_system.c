6ea4a20364d0ff4c6d5d59bc5ed1e5c8cafcccda
Edgar Simo-Serra <bobbens@gmail.com>
2012-01-22 11:28:18 +0100

    * Save exitonly property.

--- src/dev_system.c
+++ src/dev_system.c
@@ -153,6 +153,8 @@ static int dsys_saveSystem( xmlTextWriterPtr writer, const StarSystem *sys )
          xmlw_elemEmpty( writer, "autopos" );
       if (jp->flags & JP_HIDDEN)
          xmlw_elemEmpty( writer, "hidden" );
+      if (jp->flags & JP_EXITONLY)
+         xmlw_elemEmpty( writer, "exitonly" );
       xmlw_elem( writer, "hide", "%f", jp->hide );
       xmlw_endElem( writer ); /* "jump" */
    }
