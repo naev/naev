82d1504173374f5b414ebf685859e6ecb74cf622
BTAxis <BTAxis@gmail.com>
2012-02-05 11:07:37 +0100

    * Interstellar tutorial works again.

--- dat/events/tutorial/tutorial-interstellar.lua
+++ dat/events/tutorial/tutorial-interstellar.lua
@@ -17,7 +17,7 @@ Each star system is connected to one or more other star systems by means of "jum
 Let's select a jump point now. You can press %s to do this.]]
     message3 = [[Good. If you look at the bar at the bottom of the screen, you'll see that it has changed to show that you have selected a hyperspace target (next to "Nav"). Currently, the target is unknown, because you don't know anything about it yet. To actually use the jump point you've just selected you need to fly close to it and engage your hyperdrive. This is done with %s, but we're not going to do that right now, as there's an easier way.
 
-For now, press %s repeatedly to cycle through the jump points until you no longer have one targeted.]]
+For now, press %s to deselect your target. If you also have planets or ships targeted, you can use %s multiple times to select your most recent targets, in order.]]
     message4 = [[Another method of selecting a hyperspace target is by opening the galaxy map and clicking on the system you want to go to. The galaxy map is accessed by pressing %s, do so now.]]
     message5 = [[This is the galaxy map. It shows you a lot of information about the systems you've visited, and lets you plot jump routes to other star systems. Clicking a system will select it as your hyperspace target.
 
@@ -36,6 +36,7 @@ For the remainder of this tutorial, you will have unlimited fuel. Continue to ma
 Congratulations! This concludes the interstellar flight tutorial.]]
 
     thyperomsg = "Press %s to target a jump point"
+    clearomsg = "Press %s to deselect your target"
     starmapomsg = "Press %s to open the galaxy map"
     hyperomsg = "Select a hyperspace target with %s or by using the galaxy map, then press %s to engage the autonav (or jump manually with %s)"
     hyperomsg2 = "Continue exploring until you reach the Navajo system"
@@ -53,7 +54,7 @@ function create()
     tkMsg(title1, message2:format(tutGetKey"thyperspace"))
     omsg = player.omsgAdd(thyperomsg:format(tutGetKey"thyperspace"), 0)
 
-    waitthyper = 0
+    keystage = "thyperspace"
     hinput = hook.input("input")
     hook.jumpin("jumpin")
 
@@ -61,25 +62,22 @@ end
 
 -- Input hook.
 function input(inputname, inputpress)
-    if inputname == "thyperspace" and inputpress and not waitmap then
-        waitthyper = waitthyper + 1
-        nav, hyp = player.pilot():nav()
-        if waitthyper == 1 then
-            hook.rm(hinput)
-            tkMsg(title1, message3:format(tutGetKey("jump"), tutGetKey("thyperspace")))
-            hinput = hook.input("input")
-        elseif hyp == nil then
-            player.omsgRm(omsg)
-            waitmap = true
-            tkMsg(title1, message4:format(tutGetKey("starmap")))
-            omsg = player.omsgAdd(starmapomsg:format(tutGetKey("starmap")), 0)
-        end
-    elseif inputname == "starmap" and waitmap then
-        hook.rm(hinput)
-        tk.msg(title1, message5:format(tutGetKey("autonav")))
-        player.omsgChange(omsg, hyperomsg:format(tutGetKey("thyperspace"), tutGetKey("autonav"), tutGetKey("jump")), 0)
-        firstjump = true
-    end
+   nav, hyp = player.pilot():nav()
+   if inputname == "thyperspace" and keystage == "thyperspace" then
+      keystage = "deselect"
+      tkMsg(title1, message3:format(tutGetKey("jump"), tutGetKey("target_clear"), tutGetKey("target_clear")))
+   elseif hyp == nil and keystage == "deselect" then
+      keystage = "map"
+      player.omsgRm(omsg)
+      tkMsg(title1, message4:format(tutGetKey("starmap")))
+      omsg = player.omsgAdd(starmapomsg:format(tutGetKey("starmap")), 0)
+   elseif inputname == "starmap" and keystage == "map" then
+      keystage = nil
+      hook.rm(hinput)
+      tk.msg(title1, message5:format(tutGetKey("autonav")))
+      player.omsgChange(omsg, hyperomsg:format(tutGetKey("thyperspace"), tutGetKey("autonav"), tutGetKey("jump")), 0)
+      firstjump = true
+   end
 end
 
 -- Jumpin hook.
