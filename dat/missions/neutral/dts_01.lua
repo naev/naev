c0ae871da53670323bc5554f2701ef2bbfd959de
Deiz <silverwraithii@gmail.com>
2012-03-08 02:52:27 -0500

    Typo and phrasing fixes by Sudarshan S

--- dat/missions/neutral/dts_01.lua
+++ dat/missions/neutral/dts_01.lua
@@ -68,10 +68,10 @@ Into the confusion steps a steely-haired, upright, uniformed figure. Her stripes
    text[3] = [["That was good flying," the Commodore says with a tight smile. "Thank you all for your help. This gentleman has arranged a transfer of forty thousand credits to each of you. You can be proud of what you've done today."]]
 
 -- Other text for the mission
-   comm[8] = "You fled battle. The Empire wont forget."
+   comm[8] = "You fled from the battle. The Empire won't forget."
    comm[9] = "Comm Trader>You're a coward, %s. You better hope I never see you again."
    comm[10] = "Comm Trader>You're running away now, %s? The fight's finished, you know..."
-   title[4] = "Good job"
+   title[4] = "Good job!"
    text[4] = [[The debris from the battle disappears behind you in a blur of light. A moment after you emerge from hyperspace, a Imperial ship jumps in behind you and hails you.
 
 "Please hold course and confirm your identity, %s."  You send your license code and wait for a moment. "Ok, that's fine. We're just making sure no pirates escaped. You were part of the battle, weren't you?  Surprised you didn't return for the bounty, pilot. Listen, I appreciate what you did back there. I have family on %s. When I'm not flying overhead, it's good to know there are good samaritans like you who will step up. Thanks."
