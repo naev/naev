48aa394f91573a4cacb21622bf7919089cd4b4a5
Deiz <silverwraithii@gmail.com>
2012-03-29 04:32:33 -0400

    Cleaned up luadoc.sh a bit and made it work from any dir.

--- docs/luadoc.sh
+++ docs/luadoc.sh
@@ -1,48 +1,51 @@
-#run this from docs directory
-#create docs/lua directory
+# Need to be in the docs/ directory.
+cd "$(dirname $0)"
+
+# Create docs/lua/ directory
 test -d lua || mkdir lua
-#convert doxygen comments to luadoc comments
-for F in ../src/nlua_*.c
-do
-sed -n '
-	1 i -- This is file was generated automatically from C sources to feed luadoc
-#converts doxygens /** to luadocs ---
-	s|^ */\*\* *$|---|p 
-#convert special tags to lua expressions.
-#notice lines after @luafunc & @luamod will be ignored by luadoc
-#also doxygen comments that do not contain a any of this tags have no
-#influence on luadoc output
-	s|^ *\* *@luafunc|function|p
-	s|^ *\* *@luamod *\(.*\)|module "\1"|p
-#rename some tags:
-	s|^ *\* *@brief|-- @description|p
-	s|^ *\* *@luasee|-- @see|p
-	s|^ *\* *@luaparam|-- @param|p
-	s|^ *\* *@luareturn|-- @return|p #we accept both @luareturn & @return 
-#keep tags luadoc understands:
-#	s|^ *\* *@param|-- @param|p # use luaparam, param reserved for C arguments
-#	s|^ *\* *@see|-- @see|p # use luasee
-#	s|^ *\* *@return|-- @return|p # use luareturn
-	s|^ *\* *@usage|-- @usage|p
-	s|^ *\* *@description|-- @description|p
-	s|^ *\* *@name|-- @name|p
-	s|^ *\* *@class|-- @class|p
-	s|^ *\* *@field|-- @field|p
-	s|^ *\* *@release|-- @release|p
-#custom tags
-   s|^ *\* *@code|-- <pre>|p
-   s|^ *\* *@endcode|-- </pre>|p
-#remove other tags:
-	\|^ *\* *@.*|d
-#insert newline between comments, replace */ by \n
-	\|^ *\*/|c
-#keep other comments, replace * by --
-	s|^ *\*|--|p
-#delete everything else, just in case
-	d
-' $F > lua/`basename $F`.luadoc
+
+# Convert Doxygen comments to Luadoc comments
+for F in ../src/nlua_*.c; do
+   sed -n '
+         1 i -- This file was generated automatically from C sources to feed Luadoc.
+# Convert Doxygen /** to Luadoc ---
+         s|^ */\*\* *$|---|p
+# Convert special tags to Lua expressions.
+# Lines after @luafunc & @luamod will be ignored by Luadoc
+# Doxygen comments that do not contain any of these tags have no impact on
+# the Luadoc output.
+         s|^ *\* *@luafunc|function|p
+         s|^ *\* *@luamod *\(.*\)|module "\1"|p
+# Rename some tags:
+         s|^ *\* *@brief|-- @description|p
+         s|^ *\* *@luasee|-- @see|p
+         s|^ *\* *@luaparam|-- @param|p
+         s|^ *\* *@luareturn|-- @return|p #we accept both @luareturn & @return
+# Keep tags Luadoc understands:
+#        s|^ *\* *@param|-- @param|p # use luaparam, param reserved for C arguments
+#        s|^ *\* *@see|-- @see|p # use luasee
+#        s|^ *\* *@return|-- @return|p # use luareturn
+         s|^ *\* *@usage|-- @usage|p
+         s|^ *\* *@description|-- @description|p
+         s|^ *\* *@name|-- @name|p
+         s|^ *\* *@class|-- @class|p
+         s|^ *\* *@field|-- @field|p
+         s|^ *\* *@release|-- @release|p
+# Custom tags:
+         s|^ *\* *@code|-- <pre>|p
+         s|^ *\* *@endcode|-- </pre>|p
+# Remove other tags:
+         \|^ *\* *@.*|d
+# Insert newline between comments, replace */ with \n:
+         \|^ *\*/|c
+# Keep other comments, replace * with --
+         s|^ *\*|--|p
+# Delete everything else, just in case:
+         d
+         ' $F > lua/"$(basename $F)".luadoc
 done
-#run luadoc, put html files into lua dir
+
+# Run Luadoc, put HTML files into lua/ dir
 (
    cd lua
    luadoc --nofiles --taglet "naev-taglet" -t templates/ *.luadoc
