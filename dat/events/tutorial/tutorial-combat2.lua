2145a5933a65a328c62d924d561eee6f2e4123c0
xales <ravenxales@gmail.com>
2012-04-16 11:32:52 -0400

    Add "Play All" option to tutorial menu.
    When "Play All" is selected, var "tut_all" is set true. If "tut_all" is
    true and "tut_next" is not nil, the tutorial menu will pop "tut_all",
    then start the event named by "tut_next". If "tut_next" is nil,
    "tut_all" is cleared.
    
    Each tutorial sets var "tut_next" to the next tutorial in sequence
    during cleanup.

--- dat/events/tutorial/tutorial-combat2.lua
+++ dat/events/tutorial/tutorial-combat2.lua
@@ -210,6 +210,7 @@ end
 -- Cleanup function. Should be the exit point for the module in all cases.
 function cleanup()
     if not (omsg == nil) then player.omsgRm(omsg) end
+    var.push("var_next", "Tutorial: Heat")
     naev.keyEnableAll()
     naev.eventStart("Tutorial")
     evt.finish(true)
