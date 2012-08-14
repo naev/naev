2bb370b83180ef36be7e73b36215c2890e9cd3bb
Edgar Simo-Serra <bobbens@gmail.com>
2012-08-14 17:03:50 +0200

    * Added and using ndata_exists for lua voodoo.

--- src/ndata.h
+++ src/ndata.h
@@ -77,6 +77,7 @@ const char* ndata_name (void);
 /*
  * Individual file functions.
  */
+int ndata_exists( const char* filename );
 void* ndata_read( const char* filename, uint32_t *filesize );
 char** ndata_list( const char *path, uint32_t* nfiles );
 char** ndata_listRecursive( const char *path, uint32_t* nfiles );
--- src/nlua.c
+++ src/nlua.c
@@ -157,18 +157,21 @@ static int nlua_packfileLoader( lua_State* L )
    lua_pop(L,1);
 
    /* Try to locate the data directly */
-   buf = ndata_read( filename, &bufsize );
-   if (buf == NULL) {
+   if (ndata_exists( filename ))
+      buf = ndata_read( filename, &bufsize );
+   else {
       /* Try to locate the data in the data path */
-      len = strlen(LUA_INCLUDE_PATH)+strlen(filename)+2;
+      len           = strlen(LUA_INCLUDE_PATH)+strlen(filename)+2;
       path_filename = malloc( len * sizeof(char) );
       nsnprintf( path_filename, len, "%s%s", LUA_INCLUDE_PATH, filename );
-      
-      buf = ndata_read( path_filename, &bufsize );
-      if (buf == NULL) {
-         lua_pushfstring(L, "%s not found in ndata.", filename);
-         return 1;
-      }
+      if (ndata_exists( path_filename ))
+         buf = ndata_read( path_filename, &bufsize );
+   }
+
+   /* Must have buf by now. */
+   if (buf == NULL) {
+      lua_pushfstring(L, "%s not found in ndata.", filename);
+      return 1;
    }
 
    /* run the buffer */
