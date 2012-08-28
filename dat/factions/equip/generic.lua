bfeb442f38e276a9c4fa4b9fc0952d656c02edcc
BTAxis <BTAxis@gmail.com>
2012-08-28 21:04:44 +0200

    * Remove old cores prior to installing new ones.

--- dat/factions/equip/generic.lua
+++ dat/factions/equip/generic.lua
@@ -51,6 +51,7 @@ end
 --    @param p Pilot to equip
 --]]
 function equip_cores( p, engine, system, hull )
+   p:rmOutfit("cores")
    if p:addOutfit(hull,   1, false) == 0 then print("Warning: Could not equip " .. hull .. " on pilot " .. p:name() .. "!") end
    if p:addOutfit(engine, 1, false) == 0 then print("Warning: Could not equip " .. engine .. " on pilot " .. p:name() .. "!") end
    if p:addOutfit(system, 1, false) == 0 then print("Warning: Could not equip " .. system .. " on pilot " .. p:name() .. "!") end
