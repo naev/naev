0ca44c3cd20871e99891154c27371e712d4658ae
BariumBlue <alanvol7@gmail.com>
2013-06-16 07:08:23 -0700

    fixed planets.commoditiesSold()

--- src/nlua_planet.c
+++ src/nlua_planet.c
@@ -813,17 +813,21 @@ static int planetL_outfitsSold( lua_State *L )
 static int planetL_commoditiesSold( lua_State *L )
 {
    Planet *p;
-   int i, n;
+   int i;
    LuaCommodity lc;
    Commodity **c;
 
    /* Get result and tech. */
    p = luaL_validplanet(L,1);
-   c = tech_getCommodity( p->tech, &n );
+   if (p==NULL){
+      WARN("Argument 1 was not a valid planet");
+      return 0;
+   }
+   c = p->commodities;
 
    /* Push results in a table. */
    lua_newtable(L);
-   for (i=0; i<n; i++) {
+   for (i=0; i<p->ncommodities; i++) {
       lua_pushnumber(L,i+1); /* index, starts with 1 */
       lc.commodity = c[i];
       lua_pushcommodity(L,lc); /* value = LuaCommodity */
