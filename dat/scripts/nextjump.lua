a2d48e2a7e25e31924b7ec47b0abfd00ee090016
Edgar Simo-Serra <bobbens@gmail.com>
2013-07-13 19:13:59 -0500

    * Removed some debugging stuff.

--- dat/scripts/nextjump.lua
+++ dat/scripts/nextjump.lua
@@ -4,18 +4,15 @@ function getNextSystem( nowsys, finalsys, hidden )
        return nowsys
    end
 
-   print( string.format( "Getting next jump from %s en route to %s", nowsys:name(), finalsys:name() ) )
    local neighs    = nowsys:adjacentSystems( hidden )
    local nearest   = 1e9 -- Large value
    local mynextsys = finalsys
    for _, j in pairs(neighs) do
       local jdist = j:jumpDist( finalsys, hidden )
-      print( string.format( "   %s - %d", j:name(), jdist ) )
       if jdist < nearest then
          nearest     = jdist
          mynextsys   = j
       end
    end
-   print( string.format( "Chose %s - %d", mynextsys:name(), nearest ) )
    return mynextsys
 end
