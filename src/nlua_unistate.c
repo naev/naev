9a76600f9ebcb18884c1c3445e82c91285c435ad
Ambience <camjh99@gmail.com>
2014-01-25 16:45:24 +0000

    set up gamesaves from unistate.xml

--- src/nlua_unistate.c
+++ src/nlua_unistate.c
@@ -12,6 +12,7 @@
 
 #include <lauxlib.h>
 
+#include "console.h"
 #include "nlua.h"
 #include "nluadef.h"
 #include "nlua_planet.h"
@@ -29,7 +30,11 @@
 #include "map.h"
 #include "nmath.h"
 #include "nstring.h"
+#include "nfile.h"
 
+//functions
+assetStatePtr unistate_populateList(xmlNodePtr root);
+void unistate_freeList(assetStatePtr list);
 
 
 static int unistateL_changeowner(lua_State *L);
@@ -106,3 +111,112 @@ int unistateL_getpresence(lua_State *L)
    NLUA_ERROR(L, "Not implemented yet :(");
    return 0;
 }
+
+/**
+ * @brief Saves the current state of the universe
+ * 
+ * @param writer xml writer used when writing to save file
+ * @return  1 or 0 on success, -1 on failure
+ */
+int unistate_save(xmlTextWriterPtr writer)
+{
+   xmlDocPtr doc;
+   xmlNodePtr rootNode = NULL;
+   assetStatePtr list = NULL, curEntry = NULL;
+   char unistateFile[PATH_MAX], debugBuffer[PATH_MAX];
+  
+   //get the unistate file, if it doesn't exists return
+   snprintf(unistateFile, sizeof(char)*(PATH_MAX-1), "%s/unistate.xml", nfile_dataPath());
+   if(!(doc = xmlParseFile(unistateFile))) return 0;
+  
+   //if the root element isn't "unistate", then this is a different file than we want.
+   rootNode = xmlDocGetRootElement(doc);
+   if(!xml_isNode(rootNode,"unistate"))
+   {
+      WARN("Invalid unistate file at %s", unistateFile);
+      xmlFreeDoc(doc);
+      return -2;
+   }
+   
+   //get all the entries in our unistate file
+   list = unistate_populateList(rootNode);
+   //free our document pointer
+   xmlFreeDoc(doc);
+   //checks for empty list
+   if(!list) return 0;
+   else curEntry = list;
+   //iterate over entries, and save them to gamesave
+   xmlw_startElem(writer, "uni_state");
+   do {
+      //start code block
+      xmlw_startElem(writer, "asset_state");
+      //for debug purposes
+      snprintf(debugBuffer, sizeof(char) * (PATH_MAX - 1), "Adding entry to gamesave: %s, %s, %i\n", curEntry->name, curEntry->faction, curEntry->presence);
+      cli_addMessage(debugBuffer);
+      //print data 
+      xmlw_elem(writer, "name", "%s", curEntry->name);
+      xmlw_elem(writer, "faction", "%s", curEntry->faction);
+      xmlw_elem(writer, "presence", "%i", curEntry->presence);
+      //close code block
+      xmlw_endElem(writer);  
+   } while((curEntry = curEntry->next) != NULL);
+   xmlw_endElem(writer);
+   //free the memory allocated by the list
+   unistate_freeList(list);
+   //...and we're done here
+   return 1;
+   
+}
+
+/**
+ * @brief Allocates space for and populates a list of asset mods from unistate.xml.
+ * 
+ * @param rootNode A node pointer to the root node of unistate.xml
+ * @return A pointer to the first node of the linked list.
+ */
+assetStatePtr unistate_populateList(xmlNodePtr root)
+{
+   assetStatePtr listHead = NULL, curElement = NULL, lastElement = NULL;
+   xmlNodePtr elementNode = NULL, listNode = NULL;
+   
+   //no root node?
+   if(!root) return NULL;
+   //root has no children?
+   if(!(listNode = root->children)) return NULL;
+   //iterate over assets
+   do {
+      //if the node is named right
+      if(xml_isNode(listNode, "asset_state"))
+      {
+	 elementNode = listNode->xmlChildrenNode;
+	 //set up list element
+	 curElement = malloc(sizeof(assetState));
+	 if(!listHead) listHead = curElement;
+	 curElement->next = NULL;
+	 if(lastElement != NULL) lastElement->next = curElement;
+	 lastElement = curElement;
+	 //get info from file
+	 do {
+	    xmlr_strd(elementNode, "name", curElement->name);
+	    xmlr_strd(elementNode, "faction", curElement->faction);
+	    xmlr_int(elementNode, "presence", curElement->presence);
+	 } while(xml_nextNode(elementNode));
+      }
+   } while(xml_nextNode(listNode));
+   //return the list
+   return listHead;
+}
+
+/**
+ * @brief Frees a list made by unistate_populateList()
+ * 
+ * @param list list to be cleaned
+ */
+void unistate_freeList(assetStatePtr list)
+{
+   if(!list) return;
+   if(list->next != NULL) unistate_freeList(list->next);
+   free(list);
+   return;
+}
+
--- src/nlua_unistate.h
+++ src/nlua_unistate.h
@@ -1,13 +1,25 @@
 #include <lua.h>
+#include "nxml.h"
 
 #ifndef NLUA_UNISTATE_H
 #define NLUA_UNISTATE_H
 
 #define UNISTATE_METATABLE  "unistate"
 
+//structure for the list of asset mods. 
+//used when interacting with unistate.xml and saving games
+typedef struct unistate_entry {
+   char *name;
+   char *faction;
+   int presence;
+   struct unistate_entry *next;
+} assetState;
+
+//pointer version of the previous
+typedef assetState *assetStatePtr;
+
+//functions
 int nlua_loadUnistate( lua_State *L, int readonly );
-//int unistateL_changeowner(lua_State *L);
-//int unistateL_changepresence(lua_State *L);
-//int unistateL_getpresence(lua_State *L);
+int unistate_save(xmlTextWriterPtr writer);
 
 #endif
