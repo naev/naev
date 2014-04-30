e4eb32a94ea8210702183ab287ec4d5e0bbf8afe
Deiz <silverwraithii@gmail.com>
2014-04-29 23:06:16 -0400

    Mark energy usage as inverted

--- src/shipstats.c
+++ src/shipstats.c
@@ -123,7 +123,7 @@ static const ShipStatsLookup ss_lookup[] = {
    D__ELEM( SS_TYPE_D_MASS,               mass_mod,            "Mass" ),
 
    A__ELEM( SS_TYPE_A_ENERGY_FLAT,        energy_flat,         "Energy Capacity" ),
-   A__ELEM( SS_TYPE_A_ENERGY_REGEN_FLAT,  energy_usage,        "Energy Usage" ),
+   AI_ELEM( SS_TYPE_A_ENERGY_REGEN_FLAT,  energy_usage,        "Energy Usage" ),
    A__ELEM( SS_TYPE_A_CPU_MAX,            cpu_max,             "CPU Capacity" ),
 
    A__ELEM( SS_TYPE_A_ENGINE_LIMIT,       engine_limit,        "Engine Mass Limit" ),
