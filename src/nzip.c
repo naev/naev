5178a670f03787bd361c1b0dc012b1773ed807a7
nloewen <loewen.nathan@gmail.com>
2014-05-26 11:26:57 -0500

    SDL_RWops functions for nzip

--- src/nzip.c
+++ src/nzip.c
@@ -61,7 +61,7 @@ void* nzip_readFile ( struct zip* arc, const char* filename, uint32_t* size )
    uint32_t read;
    int flags = 0;
 
-   zip_stat_init(&stats);
+   zip_stat_init ( &stats );
    err = zip_stat ( arc, filename, flags, &stats );
 
    if ( err ) {
@@ -96,7 +96,8 @@ char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles )
 {
    struct zip_stat stats;
    char** filelist;
-   uint32_t i;
+   char** filelistshrunk;
+   uint32_t i, j;
    int err;
    int flags = 0;
 
@@ -104,21 +105,33 @@ char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles )
 
    filelist = malloc ( sizeof ( char* ) * ( *nfiles ) );
 
-   for ( i = 0; i < *nfiles; i++ ) {
-      zip_stat_init(&stats);
+   for ( i = 0, j = 0; i < *nfiles; i++ ) {
+      zip_stat_init ( &stats );
       err = zip_stat_index ( arc, i, flags, &stats );
-      
+
       if ( err ) {
-         WARN ( "Error getting file list from archive");
+         WARN ( "Error getting file list from archive" );
          WARN ( "%s", zip_strerror ( arc ) );
          return NULL;
       }
-      
-      filelist[i] = malloc(sizeof(char)*strlen(stats.name)+1);
-      strcpy(filelist[i],stats.name);
+
+      // If the name ends with a forward slash, it's a directory
+      if ( stats.name[strlen ( stats.name ) - 1] != '/' ) {
+         filelist[j] = malloc ( sizeof ( char ) * strlen ( stats.name ) + 1 );
+         strcpy ( filelist[j], stats.name );
+         j++;
+      }
    }
-   
-   return filelist;
+
+   *nfiles = j;
+
+   filelistshrunk = realloc ( filelist, sizeof ( char* ) *j );
+
+   if ( filelistshrunk == NULL )
+      return filelist;
+
+   else
+      return filelistshrunk;
 }
 
 void nzip_printError ( int err )
@@ -126,14 +139,18 @@ void nzip_printError ( int err )
    char *buf;
    int size;
 
-   size = zip_error_to_str ( NULL, 0, err,0 );
+   size = zip_error_to_str ( NULL, 0, err, 0 );
    buf = malloc ( sizeof ( char ) * size );
    zip_error_to_str ( buf, 50, err, 0 );
    WARN ( "%s", buf );
    free ( buf );
 }
 
-SDL_RWops* nzip_rwops(struct zip* arc, const char* filename)
+SDL_RWops* nzip_rwops ( struct zip* arc, const char* filename )
 {
-   return NULL;
+   void* data;
+   uint32_t size;
+
+   data = nzip_readFile ( arc, filename, &size );
+   return SDL_RWFromMem ( data, size );
 }
--- src/nzip.h
+++ src/nzip.h
@@ -12,10 +12,10 @@ int nzip_isZip ( const char* filename );
 struct zip* nzip_open ( const char* filename );
 void nzip_close ( struct zip* arc );
 
-int nzip_hasFile(struct zip* arc, const char* filename);
-void* nzip_readFile(struct zip* arc, const char* filename, uint32_t* size);
-char** nzip_listFiles(struct zip* arc, uint32_t* nfiles);
+int nzip_hasFile ( struct zip* arc, const char* filename );
+void* nzip_readFile ( struct zip* arc, const char* filename, uint32_t* size );
+char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles );
 
-SDL_RWops* nzip_rwops(struct zip* arc, const char* filename);
+SDL_RWops* nzip_rwops ( struct zip* arc, const char* filename );
 
 #endif
