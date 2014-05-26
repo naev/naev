e284683cdf699413995cc99fd187601274f64532
nloewen <loewen.nathan@gmail.com>
2014-05-26 13:28:19 -0500

    documentation and free some memory on errors

--- src/nzip.c
+++ src/nzip.c
@@ -4,10 +4,20 @@
 
 #include "log.h"
 
-
+/**
+ * Private function prototypes
+ */
 void nzip_printError ( int err );
 
 
+
+
+/**
+ * @brief Tests a file for zip like qualities
+ *
+ *    @param filename Name of the archive to check.
+ *    @return 0 if not zip, 1 if zip.
+ */
 int nzip_isZip ( const char* filename )
 {
    struct zip* arc;
@@ -22,6 +32,12 @@ int nzip_isZip ( const char* filename )
    return 1;
 }
 
+/**
+ * @brief Opens a zip file.
+ *
+ *    @param filename Name of the archive to open.
+ *    @return The archive, or NULL on error.
+ */
 struct zip* nzip_open ( const char* filename )
 {
    struct zip* arc;
@@ -38,6 +54,11 @@ struct zip* nzip_open ( const char* filename )
    return arc;
 }
 
+/**
+ * @brief Closes a zip file and free's resources
+ *
+ *    @param arc Archive to close
+ */
 void nzip_close ( struct zip* arc )
 {
    if ( zip_close ( arc ) ) {
@@ -46,12 +67,27 @@ void nzip_close ( struct zip* arc )
    }
 }
 
+/**
+ * @brief Check to see if a file exists in an archive
+ *
+ *    @param arc Archive to look in
+ *    @param filename File to look for
+ *    @return 1 if found, 0 otherwise
+ */
 int nzip_hasFile ( struct zip* arc, const char* filename )
 {
    int flags = 0;
    return zip_name_locate ( arc, filename, flags ) >= 0;
 }
 
+/**
+ * @brief Read the contents of a file from an archive
+ *
+ *    @param arc Archive to look in
+ *    @param filename File to read
+ *    @param[out] size Size of returned buffer
+ *    @return A pointer to the file contents in memory
+ */
 void* nzip_readFile ( struct zip* arc, const char* filename, uint32_t* size )
 {
    struct zip_file* file;
@@ -61,6 +97,7 @@ void* nzip_readFile ( struct zip* arc, const char* filename, uint32_t* size )
    uint32_t read;
    int flags = 0;
 
+   // Get info about file
    zip_stat_init ( &stats );
    err = zip_stat ( arc, filename, flags, &stats );
 
@@ -70,6 +107,7 @@ void* nzip_readFile ( struct zip* arc, const char* filename, uint32_t* size )
       return NULL;
    }
 
+   // Open the file
    file = zip_fopen_index ( arc, stats.index, flags );
 
    if ( file == NULL ) {
@@ -78,22 +116,36 @@ void* nzip_readFile ( struct zip* arc, const char* filename, uint32_t* size )
       return NULL;
    }
 
+   // Allocate based on reported size
    data = malloc ( stats.size );
 
+   // Read file data to buffer
    read = zip_fread ( file, data, stats.size );
 
+   // If we read less than the reported file size, something probably went wrong
    if ( read < stats.size ) {
       WARN ( "Error reading %s from archive", filename );
       WARN ( "%s", zip_strerror ( arc ) );
+      free ( data );
+      zip_fclose ( file );
       return NULL;
    }
 
+   // Close the file
    zip_fclose ( file );
 
+   // Return data
    *size = stats.size;
    return data;
 }
 
+/**
+ * @brief Return a list of all files in an archive
+ *
+ *    @param arc Archive to look through
+ *    @param[out] nfiles Number of files found
+ *    @return List of file names found
+ */
 char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles )
 {
    struct zip_stat stats;
@@ -103,10 +155,12 @@ char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles )
    int err;
    int flags = 0;
 
+   // Total number of archive entries (includes directories)
    *nfiles = zip_get_num_entries ( arc, flags );
 
    filelist = malloc ( sizeof ( char* ) * ( *nfiles ) );
 
+   // Get stats for each file, and store the name
    for ( i = 0, j = 0; i < *nfiles; i++ ) {
       zip_stat_init ( &stats );
       err = zip_stat_index ( arc, i, flags, &stats );
@@ -114,19 +168,23 @@ char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles )
       if ( err ) {
          WARN ( "Error getting file list from archive" );
          WARN ( "%s", zip_strerror ( arc ) );
+         free ( filelist );
          return NULL;
       }
 
       // If the name ends with a forward slash, it's a directory
       if ( stats.name[strlen ( stats.name ) - 1] != '/' ) {
+         // Copy file name to list
          filelist[j] = malloc ( sizeof ( char ) * strlen ( stats.name ) + 1 );
          strcpy ( filelist[j], stats.name );
          j++;
       }
    }
 
+   // Number of files excluding directories
    *nfiles = j;
 
+   // Shrink file list to needed size
    filelistshrunk = realloc ( filelist, sizeof ( char* ) *j );
 
    if ( filelistshrunk == NULL )
@@ -136,18 +194,34 @@ char** nzip_listFiles ( struct zip* arc, uint32_t* nfiles )
       return filelistshrunk;
 }
 
+/**
+ * @brief Print error message given a libzip error code
+ *
+ *    @param err The error code
+ */
 void nzip_printError ( int err )
 {
    char *buf;
    int size;
 
+   // Get length of message
    size = zip_error_to_str ( NULL, 0, err, 0 );
+   // Get message
    buf = malloc ( sizeof ( char ) * size );
    zip_error_to_str ( buf, 50, err, 0 );
+
    WARN ( "%s", buf );
+
    free ( buf );
 }
 
+/**
+ * @brief Return SDL_RWops for a file in an archive. This version works on a copy of the file in memory.
+ *
+ *    @param arc Archive to look in
+ *    @param filename File to get from archive
+ *    @return RWops for filename
+ */
 SDL_RWops* nzip_rwops ( struct zip* arc, const char* filename )
 {
    void* data;
