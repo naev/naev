6687ddccf3ceb5ebd416426f13805cb007914314
Julian Marchant <onpon4@riseup.net>
2014-08-06 17:25:10 -0400

    Marked FLF pre02 as GPLv3.
    I just realized that all of the Lua scripts link to the Naev API,
    which is under the GPLv3, so all of them must at least be under that
    license. I've marked flf_pre02 as such (inserting copyright notices
    based on the Git repo's commit history).

--- dat/missions/flf/flf_pre02.lua
+++ dat/missions/flf/flf_pre02.lua
@@ -1,9 +1,33 @@
 --[[
--- This is the second "prelude" mission leading to the FLF campaign.
--- stack variable flfbase_intro:
---      1 - The player has turned in the FLF agent or rescued the Dvaered crew. Conditional for dv_antiflf02
---      2 - The player has rescued the FLF agent. Conditional for flf_pre02
---      3 - The player has found the FLF base for the Dvaered, or has betrayed the FLF after rescuing the agent. Conditional for dv_antiflf03
+
+   FLF Prelude, part 2
+   Copyright (c) 2009, 2010 BTAxis
+   Copyright (c) 2009 Tim Mazid
+   Copyright (c) 2010 bobbens
+   Copyright (c) 2010, 2011 Deiz
+   Copyright (c) 2011 s0be
+   Copyright (c) 2011 nikai3d
+   Copyright (c) 2012 perey
+   Copyright (c) 2012 m1foley
+   Copyright (c) 2014 Julian Marchant
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+--
+
+   This is the second "prelude" mission leading to the FLF campaign.
+   stack variable flfbase_intro:
+        1 - The player has turned in the FLF agent or rescued the Dvaered crew. Conditional for dv_antiflf02
+        2 - The player has rescued the FLF agent. Conditional for flf_pre02
+        3 - The player has found the FLF base for the Dvaered, or has betrayed the FLF after rescuing the agent. Conditional for dv_antiflf03
+
 --]]
 
 include "fleethelper.lua"
