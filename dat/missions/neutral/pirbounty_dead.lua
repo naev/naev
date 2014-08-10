fa7d287d2ca1ababe92fcaf8b3c7f766796b20cd
Julian Marchant <onpon4@riseup.net>
2014-08-10 18:52:22 -0400

    Added copyright notices to the new bounty missions.

--- dat/missions/neutral/pirbounty_dead.lua
+++ dat/missions/neutral/pirbounty_dead.lua
@@ -1,12 +1,24 @@
 --[[
 
    Dead or Alive Pirate Bounty
+   Copyright 2014 Julian Marchant
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+--
 
    Generalized replacement for bobbens' Empire Pirate Bounty mission.
    Can work with any faction.
-
-   Author: onpon4
-           Based on Empire Pirate Bounty mission by bobbens.
 
 --]]
 
