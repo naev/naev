37548112fee56cf8bb23218cf22690d6b00a7643
Julian Marchant <onpon4@riseup.net>
2014-08-10 13:45:02 -0400

    Copied the copyright notice from naev.h to every other source file.
    Excluding those with their own copyright notices, of course.
    
    Conflicts:
    
    	src/nlua_economy.c
    	src/nlua_economy.h

--- src/dev_outfit.c
+++ src/dev_outfit.c
@@ -1,5 +1,17 @@
 /*
- * See Licensing and Copyright notice in naev.h
+ * Copyright 2006-2012 Edgar Simo Serra
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -7,6 +19,7 @@
  *
  * @brief Handles the outfit development routines.
  */
+
 
 #include "dev_outfit.h"
 
