027fa4f4d15a686854a25d49b90d5d3e0e5007a9
Julian Marchant <onpon4@riseup.net>
2014-08-25 12:48:02 -0400

    Added license notices to several Lua files.
    The Lua files under dat/ai (or at least the ones I checked) didn't
    have any commit history, so I had to do without adding copyright
    notices to them.

--- dat/landing.lua
+++ dat/landing.lua
@@ -1,4 +1,22 @@
 --[[
+
+   Copyright (c) 2011 bobbens
+   Copyright (c) 2011, 2012 BTAxis
+   Copyright (c) 2011, 2012 Deiz
+   Copyright (c) 2012 m1foley
+   Copyright (c) 2013 Lukc
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+--
+
    Prototype function:
 
       Parameter: pnt - Planet to set landing stuff about.
