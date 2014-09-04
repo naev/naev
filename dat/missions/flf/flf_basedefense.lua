47ba2e456179ffb382211a1895ecd218ade15b5d
Julian Marchant <onpon4@riseup.net>
2014-09-04 09:54:41 -0400

    Added some more license notices

--- dat/missions/flf/flf_basedefense.lua
+++ dat/missions/flf/flf_basedefense.lua
@@ -1,6 +1,18 @@
 --[[
--- This is the flf version of dv_antiflf03.  Instead of attacking the flf base, you will defend it.  Reuses code from dv_antiflf03
--- To Do (anyone): improve dialog, add flf dialog
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+--
+
+   This is the flf version of dv_antiflf03.  Instead of attacking the flf base, you will defend it.  Reuses code from dv_antiflf03
+   To Do (anyone): improve dialog, add flf dialog
 --]]
 
 include "fleethelper.lua" 
--- dat/missions/rehab_common.lua
+++ dat/missions/rehab_common.lua
@@ -1,10 +1,21 @@
 --[[
+
+   Rehabilitation Mission
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
 --
--- Rehabilitation Mission
---
--- This mission allows you to remain neutral with a faction until you've done services for them.
--- This file is used by the various faction missions, which must set the faction variable.
---
+
+   This mission allows you to remain neutral with a faction until you've done services for them.
+   This file is used by the various faction missions, which must set the faction variable.
+
 --]]
 
 include "numstring.lua"
