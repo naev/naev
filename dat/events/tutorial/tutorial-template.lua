0a085a61c9ad3c67b54408570edcab4116692b8e
Julian Marchant <onpon4@riseup.net>
2014-09-05 10:36:58 -0400

    Added license notices to the tutorials.

--- dat/events/tutorial/tutorial-template.lua
+++ dat/events/tutorial/tutorial-template.lua
@@ -1,6 +1,21 @@
--- Template for tutorial modules.
--- Each module should start by setting up the tutorial environment and enforcing rules.
--- Each module should clean up, set mission variable "var_next" to next tutorial in sequence, and return to the main tutorial menu when ending or aborting.
+--[[
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+--
+
+   Template for tutorial modules.
+   Each module should start by setting up the tutorial environment and enforcing rules.
+   Each module should clean up, set mission variable "var_next" to next tutorial in sequence, and return to the main tutorial menu when ending or aborting.
+
+--]]
 
 include("dat/events/tutorial/tutorial-common.lua")
 
--- dat/events/tutorial/tutorial.lua
+++ dat/events/tutorial/tutorial.lua
@@ -1,5 +1,20 @@
--- Master tutorial script.
--- This script allows the player to choose a tutorial module to run, or return to the main menu.
+--[[
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+--
+
+   Master tutorial script.
+   This script allows the player to choose a tutorial module to run, or return to the main menu.
+
+--]]
 
 -- localization stuff, translators would work here
 lang = naev.lang()
