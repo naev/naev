85d69d2e51490d47914e6f6d104068bc1087cef8
Julian Marchant <onpon4@riseup.net>
2014-09-11 14:32:04 -0400

    Added license notices to Lua in dat/scripts

--- dat/scripts/proximity.lua
+++ dat/scripts/proximity.lua
@@ -1,3 +1,16 @@
+--[[
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+--]]
+
 -- Poll for player proximity to a point in space.
 -- argument trigger: a table containing:
 -- location: The location, OR
