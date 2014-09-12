b7a00668a9c990adc2073fac2c83877d3bbbc16e
Julian Marchant <onpon4@riseup.net>
2014-09-12 10:23:41 -0400

    Added license notices to the rest of the files.

--- dat/missions/sirius/srs_rehab.lua
+++ dat/missions/sirius/srs_rehab.lua
@@ -1,7 +1,16 @@
 --[[
---
--- Rehabilitation Mission
---
+  
+   Rehabilitation Mission
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 3 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
 --]]
 
 include "dat/missions/rehab_common.lua"
