5a41fc327569920eee463392271bd2d08e265295
Deiz <silverwraithii@gmail.com>
2015-02-06 12:22:02 -0500

    Make refueling more resistant to interruptions
    The AI will now refuel before finding new targets to attack. This does
    have one side effect: The refuel task is pushed from C and preempts all
    other tasks, so a ship that's currently fighting will stop firing until
    it takes more damage.
    
    To fix this, the refuel conditions need to be determined dynamically.

--- dat/ai/tpl/generic.lua
+++ dat/ai/tpl/generic.lua
@@ -163,6 +163,11 @@ function control ()
 
    -- Enemy sighted, handled after running away
    elseif enemy ~= nil and mem.aggressive then
+      -- Don't start new attacks while refueling.
+      if task == "refuel" then
+         return
+      end
+
       local attack = false
 
       -- See if enemy is close enough to attack
@@ -320,19 +325,19 @@ function distress ( pilot, attacker )
    end
 
    local task = ai.taskname()
-   -- If not attacking nor fleeing, begin attacking
-   if task ~= "attack" and task ~= "runaway" then
-      if mem.aggressive then
-         ai.pushtask( "attack", t )
-      else
-         ai.pushtask( "runaway", t )
-      end
    -- We're sort of busy
-   elseif task == "attack" then
+   if task == "attack" then
       local target = ai.target()
 
       if not ai.exists(target) or ai.dist(target) > ai.dist(t) then
          ai.pushtask( "attack", t )
+      end
+   -- If not fleeing or refueling, begin attacking
+   elseif task ~= "runaway" and task ~= "refuel" then
+      if mem.aggressive then
+         ai.pushtask( "attack", t )
+      else
+         ai.pushtask( "runaway", t )
       end
    end
 end
