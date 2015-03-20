155a7fdaf07e1b5549e6314a433425b4187bb33f
Deiz <silverwraithii@gmail.com>
2015-03-20 16:00:37 -0400

    Don't use a pointer to a pointer

--- src/nlua_misn.c
+++ src/nlua_misn.c
@@ -620,7 +620,7 @@ static int misn_accept( lua_State *L )
    else if (i>=MISSION_MAX)
       ret = 1;
    else { /* copy it over */
-      memcpy( &player_missions[i], cur_mission, sizeof(Mission) );
+      memcpy( player_missions[i], cur_mission, sizeof(Mission) );
       memset( cur_mission, 0, sizeof(Mission) );
       cur_mission = player_missions[i];
       cur_mission->accepted = 1; /* Mark as accepted. */
