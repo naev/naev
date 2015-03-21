68447d8f936ef843093ac89b193d51e6f91e98e4
l0k1 <justi.n.icholson@gmail.com>
2015-03-20 21:14:51 -0700

    More progress. Still need to add in the in-flight dialogue.

--- dat/missions/goddard/corporate_war/corporate_war4.lua
+++ dat/missions/goddard/corporate_war/corporate_war4.lua
@@ -52,15 +52,15 @@ bmsg[1] = [[You find %s once again in the bar. He smiles as he sees you approach
 bmsg[2] = [[%s smiles happily. "Great! You can meet up with our people on %s in the %s system. They will transfer control over to you and probably be following you around in a ship of their own, just to observe. We got a nice little payment of %d waiting for you as well." %s grins as he gets up. He says, "Thank, %s." as he turns to leave.]]
 bmsg[3] = [[You land on %s, only to be greeted by a dock bustling with engineers and other people wearing the %s logo. Several engineers approach you as you leave your ship. "I assume you are %s," the lead engineer says. "I'm Giryn, lead engineer on the prototype project. We are going to let you fly the prototype ship. It is imperative that you follow my instructions and DO NOT LEAVE THE SYSTEM. We want to keep this project under wraps. I'll be flying beside you in another ship." He leads you to another part of the dock, partitioned off from the rest. Inside, you see the new prototype and your mouth starts watering. "This is the new bird," Giryn says. "Go ahead and climb in, and get yourself familiarized with the controls. You can takeoff whenever you are ready." And with that, the engineers turn and go, leaving you with the new ship.]]
 
-ifd[1] = [[Alright, just follow our commands and we should be done quick.]]
-ifd[2] = [[First thing, We need to check engines. Fly to point AF-27. We've highlighted it on your map.]]
-ifd[3] = [[Ok, good. Now, fly to point GE-83, then point TB-54. We've highlighted these on your map.]]
-ifd[4] = [[We need to monitor turret tracking. We've highlighted a drone on your map. Please attack it.]]
-ifd[5] = [[We need to check hull integrity. Please fly to point QH-09, and allow yourself to be attacked.]]
-ifd[6] = [[Next, we need to check heat diffusion. There are 2 groups of inactive drones at points MU-89.]]
-ifd[7] = [[Please destroy both groups.]]
-ifd[8] = [[Those aren't drones! Fireteams Bravo and Gamma, move in now!]]
-ifd[9] = [[%s! Defensive maneouvers! Take out as many as you can, but don't risk the ship!]]
+ifd[1] = [[This is Gyrin. We need to check engines first. Fly to point AF-27. We've highlighted it on your map.]]
+ifd[2] = [[Ok, good. Now, fly to point GE-83, then point TB-54. We've highlighted these on your map.]]
+ifd[3] = [[We need to monitor offensive systems. We've highlighted a drone on your map. Please destroy it.]]
+ifd[4] = [[We need to check defensive systems. Please hold your position, and allow yourself to be attacked.]]
+ifd[5] = [[Next, we need to check heat diffusion. There are 2 groups of inactive drones at point MU-89.]]
+ifd[6] = [[Please destroy both groups.]]
+ifd[7] = [[Those aren't drones! Fireteams Bravo and Gamma, move in now!]]
+ifd[8] = [[%s! Defensive maneouvers! Take out as many as you can, but don't risk the ship!]]
+
 
 fmsg[1] = [[%s doesn't look happy as you turn him down. "Well, if you change your mind, I'll be around here somewhere." %s gets up and wanders off through the bar.]]--player refuses mission.
 fmsg[2] = [[The comm on the bridge of the prototype ship suddenly displays a full hologram of %s. "I see our trust in you was misplaced. I'll assume you intend to keep that ship. You are now an enemy of %s, and we will not hesitate to shoot you on sight. Oh, and we are keeping your ship." The comm goes dark.]]
@@ -72,7 +72,13 @@ osd = {}
 osd[1] = "Land on %s in the %s system."
 osd[2] = "Follow instructions given to test the new ship."
 osd[3] = "Land on %s and transfer back to your ship."
-osd2temp = ""
+osdmini = {}
+osdmini[1] = "Fly to point AF-27, highlighted on the map."
+osdmini[2] = "Fly to point GE-83, then TB-54, highlighted on the map."
+osdmini[3] = "Destroy the drone highlighted on the map."
+osdmini[4] = "Allow the enemy craft to attack you. Do not fire back." --TODO: make sure player doesn't fire back. disable weaps?
+osdmini[5] = "Destroy groups of drones at MU-89, highlighted on the map."
+osdmini[6] = "Destroy attacking drones."
 
 function create ()
    
@@ -132,13 +138,13 @@ function accept ()
    
    hook.jumpin("jumper")
    hook.land("lander")
-   hook.takeoff("startTest")
 end
 
 function lander()
    if missionStatus == 1 and planet.cur() == testAsset then
       tk.msg(misn_title,bmsg[3])
       missionStatus = 2
+      hook.takeoff("setUpTest")
       --time to swap ships. hopefully i don't eff this up.
       playerShipModel = pilot.player():ship()
       playerShipName = player.ship()
@@ -200,9 +206,13 @@ function lander()
          pilot.addOutfit(pilot.player(),o)
       end  
    end
+   if missionStatus > 2 and missionStatus < 11 then
+      tk.msg(misn_title,fmsg[3])
+      misn.finish(false)
+   end
 end
 
-function startTest()
+function setUpTest()
    if player.ship() == "Prototype" then
       missionStatus = 3
       testControl = pilot.add("Gawain")
@@ -211,11 +221,15 @@ function startTest()
       testControl:follow(pilot.player())
 
       --set up drones, etc, used in test.
-      drone = pilot.add("Drone Lancelot","dummy",vec2.new(-4000,3000))
+      drone = pilot.add("Drone Lancelot",nil,vec2.new(-4000,3000))
       drone[1]:control()
       drone[1]:brake()
-
-      drone2 = pilot.add("Krain Kestrel","dummy",vec2.n32(-3700,2700))
+      
+      if friendlyFaction == "Krain" then
+         drone2 = pilot.add("Krain Kestrel",nil,vec2.new(-3700,2700))
+      else
+         drone2 = pilot.add("Goddard Goddard",nil,vec2.new(-3700,2700))
+      end
       drone2[1]:control()
       drone2[1]:brake()
 
@@ -231,13 +245,13 @@ function startTest()
          x = radius * math.cos(angle * i)
          y = radius * math.sin(angle * i) 
 
-         droneNew1 = pilot.add("Drone Lancelot","dummy",droneGroup1Coords)
+         droneNew1 = pilot.add("Drone Lancelot",nil,droneGroup1Coords)
          droneNew1[1]:control()
          droneNew1[1]:brake()
          droneNew1[1]:setPos(droneGroup1Coords + vec2.new(x,y))
          table.insert(droneGroup1, droneNew1[1])
 
-         droneNew2 = pilot.add("Drone Lancelot","dummy",droneGroup2Coords)
+         droneNew2 = pilot.add("Drone Lancelot",nil,droneGroup2Coords)
          droneNew2[1]:control()
          droneNew2[1]:brake()
          droneNew2[1]:setPos(droneGroup2Coords + vec2.new(x,y))
@@ -254,25 +268,19 @@ function startTest()
 --x=17000 y=18000
 --x=-18200,y=-12000
 --[[
-ifd[1] = [[This is Gyrin. We need to check engines first. Fly to point AF-27. We've highlighted it on your map.]]
-ifd[2] = [[Ok, good. Now, fly to point GE-83, then point TB-54. We've highlighted these on your map.]]
-ifd[3] = [[We need to monitor offensive systems. We've highlighted a drone on your map. Please destroy it.]]
-ifd[4] = [[We need to check defensive systems. Please fly to point QH-09, and allow yourself to be attacked.]]
-ifd[5] = [[Next, we need to check heat diffusion. There are 2 groups of inactive drones at point MU-89.]]
-ifd[6] = [[Please destroy both groups.]]
-ifd[7] = [[Those aren't drones! Fireteams Bravo and Gamma, move in now!]]
-ifd[8] = [[%s! Defensive maneouvers! Take out as many as you can, but don't risk the ship!]]
 --]]
    end
 end
 
 function startTest() --fly to a point.
+   missionStatus = 4
    testControl:broadcast(ifd[1])
    sysMrk = system.mrkAdd(14500,14500)
    testHook = hook.date(time.create(0,0,50),"proximity",{location=vec2.new(14500,14500), radius=250,funcname="testTwo"})
 end
 
 function testTwo() --fly through another point on your way to a third.
+   missionStatus = 5
    hook.rm(testHook)
    system.mrkRm(sysMrk)
    sysMrk = system.mrkAdd(-10000,-12000)
@@ -281,6 +289,7 @@ function testTwo() --fly through another point on your way to a third.
 end
 
 function testThree() --arrive at the third point.
+   missionStatus = 6
    hook.rm(testHook)
    system.mrkRm(sysMrk)
    sysMrk = system.mrkAdd(-10000,-12000)
@@ -289,6 +298,7 @@ end
 
 function testFour() --shoot a drone.
    --get the drone going
+   missionStatus = 7
    hook.rm(testHook)
    system.mrkRm(sysMrk)
    drone[1]:hilight()
@@ -299,6 +309,7 @@ function testFour() --shoot a drone.
 end
 
 function testFive() --getting shot! woo.
+   missionStatus = 8
    drone2[1]:hilight()
    drone2[1]:attack(pilot.player())
    testHook = hook.timer(100,"testSix")
@@ -307,6 +318,7 @@ end
 function testSix() --done getting shot.
    _,shield = pilot.player:health()
    if shield < 30 then
+      missionStatus = 9
       hook.rm(testHook)
       drone[2]:control()
       drone[2]:brake()
@@ -317,24 +329,48 @@ function testSix() --done getting shot.
 end
 
 function startTheAttack()
-   goddardGroup = {}
+   missionStatus = 10
+   enemyGroup = {}
    system.mrkRm(sysMrk)
    system.mrkRm(sysMrk2)
    for _,p in ipairs(droneGroup2) do
-      newPt = pilot.add("Goddard Lancelot",nil,p:pos())
+      newPt = pilot.add(enemyFaction .. " Lancelot",nil,p:pos())
       newPt[1]:setHostile()
+      newPt[1]:setVisible()
       p:destroy()
       hook.pilot(newPt[1],"exploded","countTheDead")
-      table.insert(goddardGroup, newPt[1])
+      table.insert(enemyGroup, newPt[1])
+   end
+   for i = 1,12 do 
+      newPt = pilot.add(friendlyFaction .. " Lancelot",nil,testAsset())
+      newPt[1]:setFriendly()
+      newPt[1]:setVisible()
    end
 end
 
 function countTheDead()
+   numExploded = numExploded or 1
+   numExploded = numExploded + 1
+   if numExploded >= #goddardGroup then
+      missionStatus = 11
+      misn.osdActive(3)
+   end
+end
 
+function osdUpdate(miniNum)
+   misn.osdDestroy()
+   osd[2] = osdmini[miniNum]
+   misn.osdCreate(misn_title,osd)
+   misn.osdActive(2)
 end
 
 function jumper()
-
+   if missionStatus > 2 and missionStatus < 11 then
+      tk.msg(misn_title,fmsg[2])
+      faction.modPlayer(friendlyFaction,-100)
+      var.push("corpWarCampaign",0)
+      misn.finish(true)
+   end
 end
 
 function abort ()
