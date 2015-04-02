de9b87f3a9ccde44779ab641622098375a83bf8b
onpon4 <onpon4@riseup.net>
2015-04-02 11:44:18 -0400

    Fixed some bugs with achack03.
    Specifically, police defending Harja, and a case where the OSD was
    being inappropriately reset.

--- dat/missions/sirius/achack/achack03.lua
+++ dat/missions/sirius/achack/achack03.lua
@@ -128,7 +128,9 @@ end
 
 -- Enter hook.
 function enter()
-   misn.osdActive(1)
+   if not harjatalked then
+      misn.osdActive(1)
+   end
 end
 
 -- Date hook.
@@ -145,6 +147,7 @@ function date()
 
       harja = addRawShips("Shark", "trader", spawnpoint, "Achack_sirius", 1)[1]
       harja:rename("Harja's Shark")
+      harja:memory("aggressive", true)
       harja:control()
       harja:follow(player.pilot())
       hook.timer(500, "proximityScan", {focus = harja, funcname = "detectHarja"})
