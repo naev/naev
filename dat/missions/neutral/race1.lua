d6898350d8cb75f29bf7cc1445a90180264ec60f
onpon4 <onpon4@riseup.net>
2015-09-04 14:41:33 -0400

    Some fixes to the race missions.
    Quite a lot here, but of note:
    
    - Fixed the strings to be more consistent with everything else (removed
      double-spaces, added use of numstring, etc).
    
    - Made the reward variable.
    
    - Made the guy stay behind when you say "No".

--- dat/missions/neutral/race1.lua
+++ dat/missions/neutral/race1.lua
@@ -5,6 +5,8 @@
 --
 --]]
 
+include "numstring.lua"
+
 -- Localization, choosing a language if naev is translated for non-english-speaking locales.
 lang = naev.lang()
 if lang == "es" then
@@ -16,37 +18,37 @@ else -- Default to English
     ftext = {}
 
     title[1] = "Looking for a 4th"
-    text[1] = [["Hey man, are you busy?  We are planning on having a race around this system, but we need a 4th yacht class ship to participate."]]
+    text[1] = [["Hey man, we are planning on having a race around this system. We need a 4th person to participate. You have to bring a Yacht class ship. There's a prize of %s credits if you win. Interested?"]]
 
-    title[2] = "Awesome "
-    text[2] = [["Thats great man.  Here is how it works.  We will all be in a yacht class ship.  Once we take off from %s, there will be a countdown.  At the end of the countdown, we will proceed to the various checkpoints in order, boarding them before proceeding to the next checkpoint.  After the last checkpoint has been boarded, head back to %s and land.  Let have some fun."]]
+    title[2] = "Awesome"
+    text[2] = [["Thats great man. Here is how it works. We will all be in a yacht class ship.  Once we take off from %s, there will be a countdown. At the end of the countdown, we will proceed to the various checkpoints in order, boarding them before proceeding to the next checkpoint. After the last checkpoint has been boarded, head back to %s and land. Let's have some fun!"]]
 
     title[3] = "Checkpoint %s reached"
     text[3] = "Proceed to Checkpoint %s"
 
-    text[4] = "Proceed to land at %s"
+    text[4] = "Land on %s"
     refusetitle = "Refusal"
     refusetext = [["I guess we'll need to find another pilot."]]
     
     wintitle = "You Won!"
-    wintext = [[The laid back man comes up to you.  
-"Congratulations man.  And guess what!  We got a sponsor!  Melendez Corporation has agreed to put up prize money.  He hands you a chip with 10000 credits.  Let's race again sometime soon."]]
+    wintext = [[The laid back man comes up to you and hands you a credit chip.  
+    "Congratulations, man! Here's your prize money. Let's race again sometime soon!"]]
 
     ftitle[1] = "Illegal ship!"
-    ftext[1] = [["You have switched to a ship that's not allowed in this race.  Mission failed."]]
+    ftext[1] = [["You have switched to a ship that's not allowed in this race. Mission failed."]]
 
     ftitle[2] = "You left the race!"
-    ftext[2] = [["You left the race.  The race continued without you."]]
+    ftext[2] = [["Because you left the race, you have been disqualified."]]
 
     ftitle[3] = "You failed to win the race."
     ftext[3] = [[As you congratulate the winner on a great race, the laid back man comes up to you.
-    "That was a lot of fun, if you ever have time, let's race again."]]
+    "That was a lot of fun! If you ever have time, let's race again. Maybe you'll win next time!"]]
 
     NPCname = "A laid back man"
-    NPCdesc = "You see a laid back man, who appears to be one of the locals, looking around the bar, apparently in search of a suitable pilot."
+    NPCdesc = "You see a laid back man, who appears to be one of the locals, looking around the bar."
 
     misndesc = "You're participating in a race!"
-    misnreward = "10000 credits"
+    misnreward = "%s credits"
 
     OSDtitle = "Racing Skills 1"
     OSD = {}
@@ -71,7 +73,6 @@ end
 
 
 function create ()
-
     this_planet, this_system = planet.cur()
     missys = {this_system}
     if not misn.claim(missys) then
@@ -81,25 +82,24 @@ function create ()
     curplanet = planet.cur()
     misn.setNPC(NPCname, "neutral/male1")
     misn.setDesc(NPCdesc)
-
-
+    credits = rnd.rnd(10000, 50000)
 end
 
 
 function accept ()
-    if tk.yesno(title[1], text[1]) then
+    if tk.yesno(title[1], text[1]:format(numstring(credits))) then
         misn.accept()
 	OSD[4] = string.format(OSD[4], curplanet:name())
         misn.setDesc(misndesc)
-        misn.setReward(misnreward)
+        misn.setReward(misnreward:format(numstring(credits)))
         misn.osdCreate(OSDtitle, OSD)
         tk.msg(title[2], string.format(text[2], curplanet:name(), curplanet:name()))
         hook.takeoff("takeoff")
     else
         tk.msg(refusetitle, refusetext)
-        misn.finish()
     end
 end
+
 
 function takeoff()
     if player.pilot():ship():class() ~= "Yacht" then
@@ -157,6 +157,8 @@ function takeoff()
     hook.jumpin("jumpin")
     hook.land("land")
 end
+
+
 function counter()
     countdown = countdown - 1
     if countdown == 0 then
@@ -179,12 +181,15 @@ function counter()
     end
 end
 
+
 function racer1idle(p)
     player.msg( string.format( positionmsg, p:name(),target[1]) )
     p:broadcast(string.format( chatter[4], target[1]))
     target[1] = target[1] + 1
     hook.timer(2000, "nexttarget1")
 end
+
+
 function nexttarget1()
     if target[1] == 4 then
         racers[1]:land(curplanet:name())
@@ -194,12 +199,15 @@ function nexttarget1()
     end
 end
 
+
 function racer2idle(p)
     player.msg( string.format( positionmsg, p:name(),target[2]) )
     p:broadcast(chatter[5])
     target[2] = target[2] + 1
     hook.timer(2000, "nexttarget2")
 end
+
+
 function nexttarget2()
     if target[2] == 4 then
         racers[2]:land(curplanet:name())
@@ -208,12 +216,16 @@ function nexttarget2()
         racers[2]:goto(checkpoint[target[2]]:pos())
     end
 end
+
+
 function racer3idle(p)
     player.msg( string.format( positionmsg, p:name(),target[3]) )
     p:broadcast(chatter[6])
     target[3] = target[3] + 1
     hook.timer(2000, "nexttarget3")
 end
+
+
 function nexttarget3()
     if target[3] == 4 then
         racers[3]:land(curplanet:name())
@@ -222,9 +234,13 @@ function nexttarget3()
         racers[3]:goto(checkpoint[target[3]]:pos())
     end
 end
+
+
 function stopcount()
     player.omsgRm(omsg)
 end
+
+
 function board(ship)
     for i,j in ipairs(checkpoint) do
         if ship == j and target[4] == i then
@@ -241,19 +257,23 @@ function board(ship)
     player.unboard()	    
 end
 
+
 function jumpin()
     tk.msg(ftitle[2], ftext[2])
     abort()
 end
 
+
 function racerland(p)
     player.msg( string.format(landmsg, p:name(),curplanet:name()))
 end
+
+
 function land()
     if target[4] == 4 then
         if racers[1]:exists() and racers[2]:exists() and racers[3]:exists() then
             tk.msg(wintitle, wintext)
-            player.pay(10000) 
+            player.pay(credits)
             misn.finish(true)
         else
             tk.msg(ftitle[3], ftext[3])
@@ -265,6 +285,7 @@ function land()
         abort()
     end
 end
+
 
 function abort ()
     misn.finish(false)
