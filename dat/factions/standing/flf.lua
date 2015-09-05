c06fc67fd564b2af96fd2d3755e71d8056f1c573
onpon4 <onpon4@riseup.net>
2015-09-05 18:39:37 -0400

    Changed the FLF's caps.
    This starts the FLF off with a very low mission cap (to reflect
    the FLF's distrust of outsiders indicated by flf_pre01 and flf_pre02),
    and then increases it upon completion of pre02.

--- dat/factions/standing/flf.lua
+++ dat/factions/standing/flf.lua
@@ -15,10 +15,10 @@
 include "dat/factions/standing/skel.lua"
 
 
-_fcap_kill     = 25 -- Kill cap
+_fcap_kill     = 5 -- Kill cap
 _fdelta_distress = {-0.5, 0} -- Maximum change constraints
 _fdelta_kill     = {-5, 1.5} -- Maximum change constraints
-_fcap_misn     = 50 -- Starting mission cap, gets overwritten
+_fcap_misn     = 5 -- Starting mission cap, gets overwritten
 _fcap_misn_var = "_fcap_flf"
 _fthis         = faction.get("FLF")
 
