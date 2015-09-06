99766c24af166dc281f321b83650141a64272b05
onpon4 <onpon4@riseup.net>
2015-09-06 18:47:23 -0400

    Made the Empire not care about its allies being attacked.
    The effect of this is that if the player attacks House Goddard or House
    Dvaered, the Empire will not become hostile to the player. Only attacks
    on the Empire's own ships will provoke this response.
    
    This change is primarily meant to make it possible to align with the FLF, but
    maintain good relations with the Empire.

--- dat/factions/standing/empire.lua
+++ dat/factions/standing/empire.lua
@@ -20,6 +20,10 @@ _fdelta_distress = {-1, 0} -- Maximum change constraints
 _fdelta_kill     = {-5, 1} -- Maximum change constraints
 _fcap_misn     = 30 -- Starting mission cap, gets overwritten
 _fcap_misn_var = "_fcap_empire"
+
+_fmod_distress_friend = 0
+_fmod_kill_friend     = 0
+
 _fthis         = faction.get("Empire")
 
 
