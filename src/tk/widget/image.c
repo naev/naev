08e9595a81f9757429c80c3c9aca8da9ae375fe3
Mutos <benoit.robin@hoshikaze.net>
2015-11-08 08:39:46 +0100

    Fixed 2 bugs in Image Widget : Widget border takes image size instead of widget size ; Entire widget not displayed when image is NULL

--- src/tk/widget/image.c
+++ src/tk/widget/image.c
@@ -66,29 +66,28 @@ static void img_render( Widget* img, double bx, double by )
    double x,y;
    double w,h;
 
-   /* Fanciness. */
-   if (img->dat.img.image == NULL)
-      return;
-
-   /* Values. */
+   /* Compute values. */
    x = bx + img->x;
    y = by + img->y;
    w = img->w;
    h = img->h;
 
-   /*
-    * image
-    */
-   gl_blitScale( img->dat.img.image, x, y,
-         w, h, &img->dat.img.colour );
+   /* Draw image */
+   if (img->dat.img.image == NULL) {
+      toolkit_drawRect(x, y, w, h, &img->dat.img.colour, NULL);
+   } else {
+      gl_blitScale( img->dat.img.image, x, y,
+            w, h, &img->dat.img.colour );
+   }
 
+   /* Draw border */
    if (img->dat.img.border) {
       /* inner outline (outwards) */
-      toolkit_drawOutline( x, y+1, img->dat.img.image->sw-1,
-         img->dat.img.image->sh-1, 1., toolkit_colLight, toolkit_col );
+      toolkit_drawOutline( x, y+1, w-1,
+         h-1, 1., toolkit_colLight, toolkit_col );
       /* outer outline */
-      toolkit_drawOutline( x, y+1, img->dat.img.image->sw-1,
-            img->dat.img.image->sh-1, 2., toolkit_colDark, NULL );
+      toolkit_drawOutline( x, y+1, w-1,
+            h-1, 2., toolkit_colDark, NULL );
    }
 }
 
