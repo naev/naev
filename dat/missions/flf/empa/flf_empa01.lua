100818798a69e334393a089d7b4475541c37d6f4
onpon4 <onpon4@riseup.net>
2015-11-29 18:03:17 -0500

    Finished flf_empa01.
    At least, I think. Needs testing.

--- dat/missions/flf/empa/flf_empa01.lua
+++ dat/missions/flf/empa/flf_empa01.lua
@@ -29,7 +29,7 @@ else -- default English
    text[1] = [[The woman looks up to you and smiles. She gestures for you to take a seat, which you do.
     "Greetings, %s", she says. "You're probably wondering who I am. My name is Cheryl, and I'm one of the higher-ranking officials of the FLF." You try to hide your disbelief in what she says, but judging from Cheryl's chuckle, she seems to see right through you. "Yes, I know. You probably imagined that I would be middle-aged and wearing a fancy uniform, didn't you?" You begin to respond to this statement, but she stops you, and her expression suddenly becomes serious.]]
 
-   text[2] = [["Our records show that you have been of serious help to the Empire. It seems you were one of the ones responsible for the destruction of the Collective, in particular. Is this correct?" You pause for a second, wondering why Cheryl would be asking such a question.
+   text[2] = [["Our records show that you have been of serious help to the Empire. It seems you were one of the people responsible for the destruction of the Collective, in particular. Is this correct?" You pause for a second, wondering why Cheryl would be asking such a question.
     Finally, you answer by agreeing that what she says is true, but assuring her that they didn't send you here. She laughs. "Don't be silly. I don't think you're a spy. We are not exactly friends with the Empire; the Empire and House Dvaered are allies, after all, at least on paper. But we are not enemies with them, either."]]
 
    text[3] = [["In fact, the Empire has become rather annoyed with the Dvaered lately. I think you may have witnessed some of this when you conducted a prisoner exchange for the Empire some time ago. If you will recall, the prisoner exchange was going perfectly smoothly until a squadron of Dvaered ships decided to attack us. In fact, many of our Empire prisoners died in the chaos, and I have been told that the Empire higher-ups were furious.
@@ -40,7 +40,20 @@ else -- default English
     You tell her that you will do your best, and she wishes you good luck before departing.]]
    
    title[5] = "First Contact"
-   text[5] = [[As you approach the officer, a grin shows up on his face. Before you can ask him about Commander Petridis, he starts talking.]]
+   text[5] = [[As you approach the officer, a grin shows up on his face. Before you can ask him about Commander Petridis, he starts talking.
+    "%s, right? You really are an interesting character, I gotta say. Never in a million years did I expect someone sympathetic to the FLF to help out the Empire as much as you did. If anything, I would have thought that the Collective and the FLF would make perfect allies. And yet here you are."]]
+
+   text[6] = [[You finally get a chance to tell the officer that you're looking for Commander Petridis. At this, he laughs. "Well, your search is over. That's me. I guess that means you've got a message to the Empire from the FLF, eh?"
+    Taking his cue, you explain to Petridis that the FLF wishes to establish friendly relations with the Empire.
+    "Friendly relations? That would really be something! You know, to be completely honest, a lot of us in the Empire are sick to death of the way House Dvaered handles things. It's always, 'Shoot first, ask questions later.' Why, just last month, those clowns blasted a thief's ship to smitherines because they stole a priceless work of art, and destroyed the art in the process!"]]
+
+   text[7] = [["That being said, House Dvaered is a part of the Empire, and while we don't really care so much about FLF ships engaging with the Dvaered military, you must understand that the Empire cannot join in on this. That is, not unless House Dvaered committed an act of treason against the Empire, which they haven't.
+    "Still, I will pass your message along. Even if it doesn't get us anywhere, with you involved in this, I'm sure the higher-ups would be more than willing to start a more friendly dialog. Perhaps we can eventually act as an intermediate between you and House Dvaered, and end this destructive conflict between you two."]]
+
+   text[8] = [[He stands and holds out his hand, which you shake. "It's been a pleasure to talk to you, %s. I hope to see you again in the future." He then leaves the bar. Time to report back to the FLF base!]]
+
+   title[9] = "A New Beginning"
+   text[9] = [[Cheryl waits for you as you land. "I trust your mission went well?" You confirm that, yes, you have spoken with Commander Petridis about the FLF's intentions. "Excellent," she remarks. She then gives you a firm handshake. "Congratulations, %s. You are now the official Empire ambassador of the FLF. Your payment for this first mission will be in your account shortly. Please check back later for your next mission." Cheryl then departs.]]
 
    misn_title = "Empire Negotiations"
    misn_desc = "You are to meet with Empire Commander Petridis and deliver a message that the FLF wishes to establish friendly relations with the Empire."
@@ -48,6 +61,9 @@ else -- default English
 
    npc_name = "A young woman"
    npc_desc = "This woman appears to be by herself, which is somewhat unusual for an FLF member. She regularly glances in your direction."
+
+   emp_name = "An Empire official"
+   emp_desc = "There is an Empire official sitting by himself, enjoying a drink. He seems to take special interest in you."
 
    osd_title   = "Empire Negotiations"
    osd_desc    = {}
@@ -97,18 +113,24 @@ end
 
 function land ()
    if job_done and planet.cur():faction():name() == "FLF" then
-      tk.msg( "", "" )
+      tk.msg( title[9], text[9]:format( player.name() ) )
       player.pay( credits )
       var.push( "_fcap_flf", math.max( 60, var.peek( "_fcap_flf" ) ) )
       faction.get("FLF"):modPlayer( reputation )
       faction.get("Empire"):modPlayer( emp_reputation )
       misn.finish( true )
    elseif planet.cur() == misplanet then
-      misn.npcAdd( "approach", "An Empire official" "empire/empire1", "There is an Empire official sitting by himself, enjoying a drink. He seems to take special interest in you." )
+      npc = misn.npcAdd( "approach", emp_name, "empire/empire1", emp_desc )
    end
 end
 
 
 function approach ()
+   tk.msg( title[5], text[5]:format( player.name() ) )
+   tk.msg( title[5], text[6] )
+   tk.msg( title[5], text[7] )
+   tk.msg( title[5], text[8]:format( player.name() ) )
+
+   misn.npcRm( npc )
    job_done = true
 end
