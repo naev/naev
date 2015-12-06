naev/naev

commit 05dc196080f165c8b720760cdc47b5cce90e7eac

new mission: The ruse. Not sure which context to put it in

dat/missions/baron/baron_baron.lua
@@ -141,7 +141,7 @@ function jumpin()
         pinnacle:setInvincible(true)
         pinnacle:control()
         pinnacle:setHilight(true)
-        pinnacle:goto(planet.get("Ulios"):pos() + vec2.new( 400, -400), false)
+        pinnacle:goto(broship:pos() + vec2.new( 400, -400), false)
         idlehook = hook.pilot(pinnacle, "idle", "idle")
         hook.pilot(pinnacle, "hail", "hail")
     end

dat/missions/neutral/crimelord.lua
@@ -5,7 +5,7 @@
            DESCRIPTION: Pirates chase you to Ogat.
 ]]--
 
-include "fleethelper.lua"
+include("dat/scripts/fleethelper.lua")
 
 lang = naev.lang()
 if lang == "es" then
@@ -97,9 +97,10 @@ function spawnBaddies ()
         ai = "baddie_norun"
     end
     thugs = addRawShips( "Admonisher", ai, last_system, "Thugs", 4 )
-    renameShips( thugs, "^.*", "Thug" )
+--    renameShips( thugs, "^.*", "Thug" )
     for pilot_number, pilot_object in ipairs(thugs) do
-        pilot_object:setHostile() --they don't like you
+	pilot_object:rename("Thug")
+        pilot_object:setHostile(true) --they don't like you
         pilot_object:rmOutfit("all") --strip them down
         pilot_object:addOutfit("Laser Cannon MK2") --add everything but rockets
         pilot_object:addOutfit("Plasma Blaster MK2", 2)

dat/missions/pirate/hitman3.lua
@@ -135,21 +135,23 @@ end
 
 -- Player won, gives rewards.
 function landed ()
-   tk.msg(title[2], text[3])
+   if planet.cur() == misn_base then
+   	tk.msg(title[2], text[3])
 
-   -- Give factions
-   local f = faction.playerStanding("Pirate")
-   if f < 0 then
-      f = 0 - f
-      faction.modPlayerSingle( "Pirate", f )
-   end
+   	-- Give factions
+   	local f = faction.playerStanding("Pirate")
+   	if f < 0 then
+      		f = 0 - f
+      		faction.modPlayerSingle( "Pirate", f )
+   	end
    
-   -- Give landing pass   
-   player.addOutfit("Pirate Landing Pass")
-   player.pay( 100000 ) -- 100k
+   	-- Give landing pass   
+   	player.addOutfit("Pirate Landing Pass")
+   	player.pay( 100000 ) -- 100k
 
-   -- Finish mission
-   misn.finish(true)
+   	-- Finish mission
+   	misn.finish(true)
+   end
 end
 
 

dat/missions/shark/sh07_pirates.lua
@@ -116,7 +116,7 @@ function accept()
       misn.accept()
       tk.msg(title[2], text[2]:format(gawname,gawsys:name(),kername1,kersys1:name(),kername2,kersys2:name(),godname,godsys:name()))
       
-      osd_msg[2] = osd_msg[2]:format(paypla:name())
+      osd_msg[2] = osd_msg[2]:format(pplname,psyname)
       
       misn.setTitle(misn_title)
       misn.setReward(misn_reward:format(numstring(reward)))

commit 0cde1accfc34aafe3d3d2760a4c6b4ced8202c99

Fixes Baron mission
Use of an undefined variable broke the Baron mission in
05dc196080f165c8b720760cdc47b5cce90e7eac such that a player could not
advance past the point where the Baron's ship needed to be hailed.

dat/missions/baron/baron_baron.lua
@@ -141,7 +141,7 @@ function jumpin()
       pinnacle:setInvincible(true)
       pinnacle:control()
       pinnacle:setHilight(true)
-      pinnacle:goto(broship:pos() + vec2.new( 400, -400), false)
+      pinnacle:goto(planet.get("Ulios"):pos() + vec2.new( 400, -400), false)
       idlehook = hook.pilot(pinnacle, "idle", "idle")
       hook.pilot(pinnacle, "hail", "hail")
    end