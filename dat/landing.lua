c172f5316b8d5c1cffe237c7e3c12981b7f60580
onpon4 <onpon4@riseup.net>
2016-06-06 09:24:19 -0400

    Revert "Removed copyright notices I added."
    This reverts commit 63fac0742da85f43fe1d5830b83091110c56c3fa.

--- dat/landing.lua
+++ dat/landing.lua
@@ -1,5 +1,11 @@
 --[[
 
+   Copyright (c) 2011 bobbens
+   Copyright (c) 2011, 2012 BTAxis
+   Copyright (c) 2011, 2012 Deiz
+   Copyright (c) 2012 m1foley
+   Copyright (c) 2013 Lukc
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License version 3 as
    published by the Free Software Foundation.
--- src/array.h
+++ src/array.h
@@ -1,4 +1,6 @@
 /*
+ * Copyright 2009 brtzsnr
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
  * published by the Free Software Foundation.
