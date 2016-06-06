3b1cefdadee076b18c1fad2c91d23a9a099d634d
onpon4 <onpon4@riseup.net>
2016-06-06 09:26:40 -0400

    Revert "Copied the copyright notice from naev.h to every other source file."
    This reverts commit 37548112fee56cf8bb23218cf22690d6b00a7643.

--- src/nlua_col.c
+++ src/nlua_col.c
@@ -1,17 +1,5 @@
 /*
- * Copyright 2006-2012 Edgar Simo Serra
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * See Licensing and Copyright notice in naev.h
  */
 
 /**
@@ -19,7 +7,6 @@
  *
  * @brief Handles colours.
  */
-
 
 #include "nlua_col.h"
 
--- src/perlin.h
+++ src/perlin.h
@@ -1,20 +1,3 @@
-/*
- * Copyright 2006-2012 Edgar Simo Serra
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
 #ifndef _TCOD_PERLIN_H
 #define _TCOD_PERLIN_H
 
