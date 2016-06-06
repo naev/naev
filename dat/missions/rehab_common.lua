b96b94411c32600973ca5b3291b043b41fb7b0af
onpon4 <onpon4@riseup.net>
2016-06-06 09:23:50 -0400

    Revert "Added some more license notices"
    This reverts commit 47ba2e456179ffb382211a1895ecd218ade15b5d.

--- dat/missions/rehab_common.lua
+++ dat/missions/rehab_common.lua
@@ -1,21 +1,10 @@
 --[[
-
-   Rehabilitation Mission
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 3 as
-   published by the Free Software Foundation.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
 --
-
-   This mission allows you to remain neutral with a faction until you've done services for them.
-   This file is used by the various faction missions, which must set the faction variable.
-
+-- Rehabilitation Mission
+--
+-- This mission allows you to remain neutral with a faction until you've done services for them.
+-- This file is used by the various faction missions, which must set the faction variable.
+--
 --]]
 
 include "dat/scripts/numstring.lua"
