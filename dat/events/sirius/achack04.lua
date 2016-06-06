f9c48903e33f1a3161182e001de3549c19788c52
onpon4 <onpon4@riseup.net>
2016-06-06 09:21:27 -0400

    Revert "More license notices"
    This reverts commit 544c6573570bb2d3512ca359b0e27f8a2f5c2da2.

--- dat/events/sirius/achack04.lua
+++ dat/events/sirius/achack04.lua
@@ -1,18 +1,5 @@
 --[[
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 3 as
-   published by the Free Software Foundation.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
---
-
-   This is a helper event for the fourth mission in the Academy Hack minor campaign.
-
+-- This is a helper event for the fourth mission in the Academy Hack minor campaign.
 --]]
 
 function create()
