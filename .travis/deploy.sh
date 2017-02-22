a11abd03a35b7cc4e5af5fb58628ed3b35c8213c
Ian Douglas Scott <ian@iandouglasscott.com>
2017-02-21 18:41:38 -0800

    Have travis push docs

---
+++ .travis/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy_key.pem
+ssh-add .travis/deploy_key.pem
+sync -e "ssh -o 'StrictHostKeyChecking no'" -rv --delete docs/html/ travis@iandouglasscott.com:/srv/naevdoc
