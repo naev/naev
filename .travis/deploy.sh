15aad228adf6c6eb308ac94604ffd9300360ed4d
Ian Douglas Scott <ian@iandouglasscott.com>
2017-02-26 13:05:34 -0800

    Travis: more decryption to deploy.sh
    Prevent error on PR, where key is not available

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+openssl aes-256-cbc -K $encrypted_18d5d806b7a8_key -iv $encrypted_18d5d806b7a8_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key.pem
 ssh-add .travis/deploy_key.pem
