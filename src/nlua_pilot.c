naev/naev

commit 0d1ca1d1d359064cd213c2c3e8ecb8d7dfdb2fb9

Remove TaskData, now that everything is in one Lua state

src/nlua_pilot.c
@@ -3758,8 +3758,8 @@ static int pilotL_goto( lua_State *L )
          tsk = "__goto_nobrake_raw";
    }
    t        = pilotL_newtask( L, p, tsk );
-   t->dtype = TASKDATA_VEC2;
-   t->dat.vec = *vec;
+   lua_pushvector( L, *vec );
+   t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
 
    return 0;
 }
@@ -3805,13 +3805,12 @@ static int pilotL_face( lua_State *L )
    else
       t     = pilotL_newtask( L, p, "__face" );
    if (pt != NULL) {
-      t->dtype = TASKDATA_PILOT;
-      t->dat.num = pt->id;
+      lua_pushnil(L);
    }
    else {
-      t->dtype = TASKDATA_VEC2;
-      t->dat.vec = *vec;
+      lua_pushvector(L, *vec);
    }
+   t->dat = luaL_ref(naevL, LUA_REGISTRYINDEX);
 
    return 0;
 }
@@ -3878,8 +3877,8 @@ static int pilotL_follow( lua_State *L )
    else
       t = pilotL_newtask( L, p, "follow_accurate" );
 
-   t->dtype = TASKDATA_PILOT;
-   t->dat.num = pt->id;
+   lua_pushinteger(L, pt->id);
+   t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
 
    return 0;
 }
@@ -3920,8 +3919,8 @@ static int pilotL_attack( lua_State *L )
 
    /* Set the task. */
    t        = pilotL_newtask( L, p, "attack" );
-   t->dtype = TASKDATA_PILOT;
-   t->dat.num = pid;
+   lua_pushpilot(L, pid);
+   t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
 
    return 0;
 }
@@ -3955,8 +3954,8 @@ static int pilotL_runaway( lua_State *L )
 
    /* Set the task. */
    t        = pilotL_newtask( L, p, (nojump) ? "__runaway_nojump" : "__runaway" );
-   t->dtype = TASKDATA_PILOT;
-   t->dat.num = pt->id;
+   lua_pushpilot(L, pt->id);
+   t->dat = luaL_ref(L, LUA_REGISTRYINDEX);;
 
    return 0;
 }
@@ -4021,13 +4020,13 @@ static int pilotL_hyperspace( lua_State *L )
       p->nav_hyperspace = i;
 
       /* Copy vector. */
-      t->dtype = TASKDATA_VEC2;
-      t->dat.vec = jp->pos;
+      lua_pushvector(L, jp->pos);
+      t->dat = luaL_ref(L, LUA_REGISTRYINDEX);;
 
       /* Introduce some error. */
       a     = RNGF() * M_PI * 2.;
       rad   = RNGF() * 0.5 * jp->radius;
-      vect_cadd( &t->dat.vec, rad*cos(a), rad*sin(a) );
+      vect_cadd( &jp->pos, rad*cos(a), rad*sin(a) );
    }
 
    return 0;
@@ -4077,15 +4076,15 @@ static int pilotL_land( lua_State *L )
 
       /* Copy vector. */
       p->nav_planet = i;
-      t->dtype = TASKDATA_VEC2;
-      t->dat.vec = pnt->pos;
+      lua_pushvector(L, pnt->pos);
+      t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
       if (p->id == PLAYER_ID)
          gui_setNav();
 
       /* Introduce some error. */
       a = RNGF() * 2. * M_PI;
       r = RNGF() * pnt->radius;
-      vect_cadd( &t->dat.vec, r*cos(a), r*sin(a) );
+      vect_cadd( &pnt->pos, r*cos(a), r*sin(a) );
    }
 
    return 0;

src/ai.c
@@ -892,8 +892,8 @@ void ai_refuel( Pilot* refueler, unsigned int target )
    /* Create the task. */
    t           = calloc( 1, sizeof(Task) );
    t->name     = strdup("refuel");
-   t->dtype    = TASKDATA_PILOT;
-   t->dat.num  = target;
+   lua_pushinteger(naevL, target);
+   t->dat      = luaL_ref(naevL, LUA_REGISTRYINDEX);
 
    /* Prepend the task. */
    t->next     = refueler->task;
@@ -1034,7 +1034,8 @@ Task *ai_newtask( Pilot *p, const char *func, int subtask, int pos )
    /* Create the new task. */
    t           = calloc( 1, sizeof(Task) );
    t->name     = strdup(func);
-   t->dtype    = TASKDATA_NULL;
+   lua_pushnil(naevL);
+   t->dat      = luaL_ref(naevL, LUA_REGISTRYINDEX);
 
    /* Handle subtask and general task. */
    if (!subtask) {
@@ -1078,8 +1079,7 @@ Task *ai_newtask( Pilot *p, const char *func, int subtask, int pos )
  */
 void ai_freetask( Task* t )
 {
-   if (t->dtype == TASKDATA_REF)
-       luaL_unref(t->L, LUA_REGISTRYINDEX, t->dat.num);
+   luaL_unref(t->L, LUA_REGISTRYINDEX, t->dat);
 
    /* Recursive subtask freeing. */
    if (t->subtask != NULL) {
@@ -1115,9 +1115,8 @@ static Task* ai_createTask( lua_State *L, int subtask )
 
    /* Set the data. */
    if (lua_gettop(L) > 1) {
-      t->dtype   = TASKDATA_REF;
-      t->dat.num = luaL_ref(L, LUA_REGISTRYINDEX);
-      t->L       = L;
+      t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
+      t->L   = L;
    }
 
    return t;
@@ -1129,27 +1128,8 @@ static Task* ai_createTask( lua_State *L, int subtask )
  */
 static int ai_tasktarget( lua_State *L, Task *t )
 {
-   /* Pass task type. */
-   switch (t->dtype) {
-      case TASKDATA_INT:
-         lua_pushnumber(L, t->dat.num);
-         return 1;
-
-      case TASKDATA_PILOT:
-         lua_pushpilot(L, t->dat.num);
-         return 1;
-
-      case TASKDATA_VEC2:
-         lua_pushvector(L, t->dat.vec);
-         return 1;
-
-      case TASKDATA_REF:
-         lua_rawgeti(L, LUA_REGISTRYINDEX, t->dat.num);
-         return 1;
-
-      default:
-         return 0;
-   }
+   lua_rawgeti(L, LUA_REGISTRYINDEX, t->dat);
+   return 1;
 }
 
 

src/ai.h
@@ -32,24 +32,6 @@ typedef struct Pilot_ Pilot;
 
 
 /**
- * @enum TaskData
- *
- * When task is created from Lua side, we can use TASKDATA_REF to directly
- * reference the Lua object. This allows the passing of arbitrary data types.
- * However, when created from C side, the data lives in C and such we have to use
- * the different provided TASKDATA types.
- *
- * @brief Task data types.
- */
-typedef enum TaskData_ {
-   TASKDATA_NULL,
-   TASKDATA_INT,
-   TASKDATA_VEC2,
-   TASKDATA_PILOT,
-   TASKDATA_REF
-} TaskData;
-
-/**
  * @struct Task
  *
  * @brief Basic AI task.
@@ -61,11 +43,7 @@ typedef struct Task_ {
 
    struct Task_* subtask; /**< Subtasks of the current task. */
 
-   TaskData dtype; /**< Data type. */
-   union {
-      unsigned int num; /**< Pilot ID, etc... */
-      Vector2d vec; /**< Vector. */
-   } dat; /**< Stores the data. */
+   int dat; /**< Lua reference to the data (index in registry). */
    lua_State *L;
 } Task;
 

commit 23e721dacc2fae299a9031be6c33d7ac24eb79a2

Fix bug in pilot.land() caused by 0d1ca1d1d359064cd213c2c3e8ecb8d7dfdb2fb9

src/nlua_pilot.c
@@ -3964,6 +3964,7 @@ static int pilotL_land( lua_State *L )
    Planet *pnt;
    int i;
    double a, r;
+   Vector2d v;
 
    NLUA_CHECKRW(L);
 
@@ -3988,17 +3989,20 @@ static int pilotL_land( lua_State *L )
          return 0;
       }
 
-      /* Copy vector. */
       p->nav_planet = i;
-      lua_pushvector(L, pnt->pos);
-      t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
       if (p->id == PLAYER_ID)
          gui_setNav();
+
+      /* Copy vector. */
+      v = pnt->pos;
 
       /* Introduce some error. */
       a = RNGF() * 2. * M_PI;
       r = RNGF() * pnt->radius;
-      vect_cadd( &pnt->pos, r*cos(a), r*sin(a) );
+      vect_cadd( &v, r*cos(a), r*sin(a) );
+
+      lua_pushvector(L, v);
+      t->dat = luaL_ref(L, LUA_REGISTRYINDEX);
    }
 
    return 0;