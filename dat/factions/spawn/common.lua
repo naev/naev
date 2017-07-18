98cd37e0537a8f270a1574a6fe249f4bf9821828
Edgar Simo-Serra <bobbens@gmail.com>
2017-07-18 15:31:41 +0900

    More gettext.

--- dat/factions/spawn/common.lua
+++ dat/factions/spawn/common.lua
@@ -40,7 +40,7 @@ function scom.createSpawnTable( weights )
 
    -- Sanity check
    if max == 0 then
-      error("No weight specified")
+      error(_("No weight specified"))
    end
 
    -- Normalize
@@ -61,7 +61,7 @@ function scom.choose( stable )
          return v["func"]()
       end
    end
-   error("No spawn function found")
+   error(_("No spawn function found"))
 end
 
 
@@ -83,7 +83,7 @@ function scom.spawn( pilots )
          p = scom.spawnRaw( v["pilot"][1], v["pilot"][2], v["pilot"][3], v["pilot"][4], v["pilot"][5])
       end
       if #p == 0 then
-         error("No pilots added")
+         error(_("No pilots added"))
       end
       local presence = v["presence"] / #p
       for _,vv in ipairs(p) do
