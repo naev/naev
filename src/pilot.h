2a2bdff0714e15aa8e672cb47d49ae8eb47da997
Edgar Simo-Serra <bobbens@gmail.com>
2017-07-27 17:15:31 +0900

    Refuel for 101. units not 100., which should fix possible round off errors (closes issue #739).

--- src/pilot.h
+++ src/pilot.h
@@ -36,7 +36,7 @@
 #define PILOT_TAKEOFF_DELAY      2. /**< Delay for takeoff animation. */
 /* Refueling. */
 #define PILOT_REFUEL_TIME        3. /**< Time to complete refueling. */
-#define PILOT_REFUEL_QUANTITY    100. /**< Amount transferred per refuel. */
+#define PILOT_REFUEL_QUANTITY    101. /**< Amount transferred per refuel. */
 #define PILOT_REFUEL_RATE        PILOT_REFUEL_QUANTITY/PILOT_REFUEL_TIME /**< Fuel per second. */
 /* Misc. */
 #define PILOT_SIZE_APROX         0.8   /**< approximation for pilot size */
