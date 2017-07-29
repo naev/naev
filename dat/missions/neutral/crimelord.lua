2facc083c422343658dd225a22180c717fb740d9
Edgar Simo-Serra <bobbens@gmail.com>
2017-07-30 07:36:59 +0900

    More gettext.

--- dat/missions/neutral/crimelord.lua
+++ dat/missions/neutral/crimelord.lua
@@ -7,30 +7,26 @@ DESCRIPTION: Pirates chase you to Ogat.
 
 include("dat/scripts/fleethelper.lua")
 
-lang = naev.lang()
-if lang == "es" then
-else --I guess you know this stuff...
-   NPC_name = "A detective" --NPC params
-   bar_desc = "A private detective is signalling you to come speak with him."
-   title = {}
-   title[0] = "Crimelord" --Mission title
-   title[1] = "Good luck" --dialogue title
-   title[2] = "Mission Accomplished" --finished title
-   title[3] = "He told you so..." --failed title
-   text = {}
-   text[0] = [[The private detective greets you and gets right down to business.
+NPC_name = _("A detective") --NPC params
+bar_desc = _("A private detective is signalling you to come speak with him.")
+title = {}
+title[0] = _("Crimelord") --Mission title
+title[1] = _("Good luck") --dialogue title
+title[2] = _("Mission Accomplished") --finished title
+title[3] = _("He told you so...") --failed title
+text = {}
+text[0] = _([[The private detective greets you and gets right down to business.
    "I have tracked down and collected evidence against a local crimelord," he says. "The evidence is on this data disk. He would love nothing more than to get his hands on this.
    I want you to bring this to my associates in the %s system. While the local authorities have proven corruptible, my associates will ensure that this man ends up in prison, where he belongs. I must warn you, however:
    He is a man of considerable influence. He has many friends, and no doubt will send some of his mercenaries to stop you. You'll need a fast ship to shake them off. My associates will compensate you generously when you reach %s.
    Regrettably, you are not the first pilot I've contacted regarding this matter. Your predecessor was intercepted when he landed en route to %s. The crime lord has many underlings lurking in nearby spaceports -- you must NOT land until you've delivered the data."
-   Given the dangers, you're not sure whether the reward will make this worth your while. Do you accept?]] --dialogue 1
-   text[1] = [[After quickly glancing around to make sure nobody's taken a particular interest, the detective presses the data stick into your hand.
-   "Be careful out there. I doubt you'll be able to get far without being noticed."]] --dialogue 2
-   text[2] = "\"Excellent work. This data will ensure an arrest and swift prosecution. You've certainly done your part towards cleaning up the region. As for your compensation, I've had %s credits transferred to you.\"" --finished
-   text[3] = "As you step out of your ship and seal the airlock, you spot a burly man purposefully heading towards you. You turn to flee, but there are others closing in on your position. Surrounded, and with several laser pistols trained on you, you see no option but to surrender the evidence."
-   misn_desc = "Evade the thugs and deliver the evidence to %s." --OSD text
-   reward_desc = "A generous compensation" --reward description
-end
+   Given the dangers, you're not sure whether the reward will make this worth your while. Do you accept?]]) --dialogue 1
+text[1] = _([[After quickly glancing around to make sure nobody's taken a particular interest, the detective presses the data stick into your hand.
+   "Be careful out there. I doubt you'll be able to get far without being noticed."]]) --dialogue 2
+text[2] = _("\"Excellent work. This data will ensure an arrest and swift prosecution. You've certainly done your part towards cleaning up the region. As for your compensation, I've had %s credits transferred to you.\"") --finished
+text[3] = _("As you step out of your ship and seal the airlock, you spot a burly man purposefully heading towards you. You turn to flee, but there are others closing in on your position. Surrounded, and with several laser pistols trained on you, you see no option but to surrender the evidence.")
+misn_desc = _("Evade the thugs and deliver the evidence to %s.") --OSD text
+reward_desc = _("A generous compensation") --reward description
 
 function create ()
    targetsystem = system.get("Ogat") --find target system
@@ -72,7 +68,7 @@ function enter () --aforementioned triggered function
       "Hawking", "Kestrel" }
       defenders = addRawShips( defenderships, "dvaered", jump.pos(targetsystem, last_system),
       "Associates" ) --add a defending force to help you
-      renameShips( defenders, "^", "Associate " )
+      renameShips( defenders, "^", _("Associate ") )
       for pilot_number, pilot_object in pairs(defenders) do
          pilot_object:setFriendly() --I think they like you
          pilot_object:setPos( pilot_object:pos() +
@@ -82,7 +78,7 @@ function enter () --aforementioned triggered function
       
       capship = defenders[#defenders]
       capship:setInvincible()
-      capship:comm("We've got your back. Engaging hostiles.", true )
+      capship:comm(_("We've got your back. Engaging hostiles."), true )
    end
 end
 
@@ -99,7 +95,7 @@ function spawnBaddies ()
    thugs = addRawShips( "Admonisher", ai, last_system, "Thugs", 4 )
    -- renameShips( thugs, "^.*", "Thug" )
    for pilot_number, pilot_object in ipairs(thugs) do
-      pilot_object:rename("Thug")
+      pilot_object:rename(_("Thug"))
       pilot_object:setHostile(true) --they don't like you
       pilot_object:rmOutfit("all") --strip them down
       pilot_object:addOutfit("Laser Cannon MK2") --add everything but rockets
@@ -119,10 +115,10 @@ function spawnBaddies ()
       end
    end
    threats = {
-      "Surrender now and we'll let you live.",
-      "You're dead!",
-      "You won't make it out alive!",
-      "Get back here!"
+      _("Surrender now and we'll let you live."),
+      _("You're dead!"),
+      _("You won't make it out alive!"),
+      _("Get back here!")
    }
    thugs[1]:comm(threats[rnd.rnd(1,#threats)],true)
 end
