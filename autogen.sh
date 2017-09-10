5a86c24339af486aa8a2ae98b4006158f0c13cb6
Edgar Simo-Serra <bobbens@gmail.com>
2017-09-10 20:28:47 +0900

    () not necessary for sh functions (might fix travis?)

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-function autogen_error () {
+function autogen_error {
    echo "Generating configuration files failed!"
    echo "Make sure you have the following packages installed:"
    echo ""
