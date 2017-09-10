5bc52b3eb28afc09f8760a83ef9e4d5ce386acc3
Edgar Simo-Serra <bobbens@gmail.com>
2017-09-10 11:29:07 +0900

    Added more verbose message when autogen.sh fails.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-autoreconf -vif
-intltoolize --force
+function autogen_error () {
+   echo "Generating configuration files failed!"
+   echo "Make sure you have the following packages installed:"
+   echo ""
+   echo "   automake"
+   echo "   autopoint"
+   echo "   inttool"
+   exit 1
+}
+autoreconf -vif || autogen_error
+intltoolize --force || autogen_error
