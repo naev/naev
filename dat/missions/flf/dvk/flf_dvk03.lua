cb2cde0da581b96343ea2ea28c62f029910df5d0
Julie Marchant <onpon4@riseup.net>
2018-10-09 21:38:10 -0400

    A little more on flf_dvk03.

--- dat/missions/flf/dvk/flf_dvk03.lua
+++ dat/missions/flf/dvk/flf_dvk03.lua
@@ -25,6 +25,16 @@ include "dat/missions/flf/flf_common.lua"
 title = {}
 text = {}
 
+title[1] = _("The Next Level")
+text[1] = _([["Hello there, %s! You're just in time. We were just discussing our next operation against the Dvaered oppressors! Do you want in?"]])
+
+title[2] = _("Not This Time")
+text[2] = _([["Okay. Just let me know if you change your mind."]])
+
+title[3] = _("A Decisive Strike")
+text[3] = _([[You take a seat among the group. "Fantastic!" says Benito. "Let me get you caught up, then. Do you remember that mission in Raelid you helped with a while back?" You nod. You were wondering what you were actually creating a diversion from. "Yes, well, I never got around to telling you what we actually did there. See, we've been wanting to destroy Raelid Outpost for some time, mostly because it's often used as a front for trying to scout us out. So while you were getting the attention of those idiot Dvaered oppressors, we rigged a special bomb and covertly installed it onto the outpost!
+    "Now, the bomb is not perfect. The bombs can only be detonated by mechanical means, so we will need to attack the station directly. Unfortunately, we didn't have guns quite powerful enough to deal the kind of damage necessary. But thanks to your efforts, we now do. After much negotiation, we have succeeded in convincing a few pilots with Kestrels to help us out!"]])
+
 misn_title = _("Assault on Raelid")
 misn_desc = _("Join with the other FLF pilots for the assault on Raelid Outpost.")
 misn_reward = _("A great victory against the Dvaereds")
@@ -37,3 +47,20 @@ osd_desc    = {}
 osd_desc[1] = _("Fly to the %s system")
 osd_desc[2] = _("Protect the Pirate Kestrel while it launches its attack on %s")
 osd_desc[3] = _("Return to FLF base")
+
+
+function create ()
+   missys = system.get( "Raelid" )
+   if not misn.claim( missys ) then
+      misn.finish( false )
+   end
+
+   misn.setNPC( npc_name, "flf/unique/benito" )
+   misn.setDesc( npc_desc )
+end
+
+
+function accept ()
+   
+end
+
