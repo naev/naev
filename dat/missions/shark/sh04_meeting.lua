a4b4388494920bed30b3a4411f2914aec096726e
Julie Marchant <onpon4@riseup.net>
2019-01-03 21:20:17 -0500

    Fixed some Shark campaign English errors.

--- dat/missions/shark/sh04_meeting.lua
+++ dat/missions/shark/sh04_meeting.lua
@@ -25,23 +25,22 @@ title[1] = _("Travel")
 text[1] = _([["Ok, are you ready for the travel to %s in %s?"]])
 
 refusetitle = _("Sorry, not interested")
-refusetext = _([["Ok, so come back when you are ready," Smith says.]])
+refusetext = _([["Ok, come back when you are ready."]])
 
 title[2] = _("Time to go")
 text[2] = _([["So let's go."]])
 
 title[3] = _("End of mission")
-text[3] = _([[Smith gets out of your ship and looks at you, smiling: "You know, it's like that in our kind of job. Sometimes it works and sometimes it fails. It's not our fault.
+text[3] = _([[Smith gets out of your ship and looks at you, smiling. "You know, it's like that in our kind of job. Sometimes it works and sometimes it fails. It's not our fault.
    Anyway, here is your pay, and goodbye."]])
 
 title[4] = _("The meeting")
-text[4] = _([[As you land, you see a group of people that were waiting for your ship. Smith hails them and says you to wait in the ship while he goes to a private part of the bar.
-   A few periods later, he comes back and explains you that he didn't manage to get the support of the councilor, which means that the Frontier will not buy the Sharks.
+text[4] = _([[As you land, you see a group of people that were waiting for your ship. Smith hails them and tells you to wait in the ship while he goes to a private part of the bar.
+   A few periods later, he comes back and explains that he didn't manage to get the support of the councilor, which means that the Frontier will not buy the Sharks.
    "Anyway," he says, "bring me back to %s in one piece and I will pay you."]])
 
 title[5] = _("What is going on?")
-text[5] = _([[That drone was behaving strangely, and now it is attacking you. As you wonder what to do, you hear a comm from a remote Za'lek ship: "Attention please, it seems some of our drones has been hacked. If a drone is attacking you and you aren't wanted by the authorities, you are hereby granted authorization to destroy it."
-   "Incredible, "Smith says, "they have managed to hire a Za'lek military engineer who has hacked some drones in order to make them attack our ship! That's strong," he says, admiringly.]])
+text[5] = _([[Suddenly, a Za'lek drone starts attacking you! As you wonder what to do, you hear a comm from a remote Za'lek ship. "Attention please, it seems some of our drones has been hacked. If a drone is attacking you and you aren't wanted by the authorities, you are hereby granted authorization to destroy it."]])
 
 -- Mission details
 misn_title = _("The Meeting")
