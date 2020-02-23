4fcf43bd87a33ff70211e820dfc7220a7c3bb4f8
Layla Marchant <onpon4@riseup.net>
2020-02-23 13:44:19 -0500

    Updated my info.

--- dat/missions/flf/dvk/flf_dvk01.lua
+++ dat/missions/flf/dvk/flf_dvk01.lua
@@ -1,7 +1,6 @@
 --[[
 
    Diversion from Raelid.
-   Copyright (C) 2014, 2015, 2018 Julie Marchant <onpon4@riseup.net>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
