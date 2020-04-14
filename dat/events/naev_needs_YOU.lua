4ffbb4254610ed4d9cc388a87ff2c62e86e6f955
Layla Marchant <onpon4@riseup.net>
2020-04-14 15:41:00 -0400

    Fixed spacing for naev_needs_YOU

--- dat/events/naev_needs_YOU.lua
+++ dat/events/naev_needs_YOU.lua
@@ -18,40 +18,27 @@ title = {}
 text = {}
 title[1] = _("Naev received SIGSEGV (address not mapped to object)!")
 text[1] = _([[As you step out of your ship, the sunlight suddenly flickers. The local star in the sky turns blue, then a deep purple, and then goes dark. The whole planet plunges into darkness.
-
-Then a voice speaks, louder than an earthquake. It seems to echo inside your bones and from the very center of the galaxy. "Oh crap..." it says.
-
-Another voice laughs musically, as if world itself were singing. "bobbens, did you just destroy the universe, again?"
-
-"Mmmm, hold on," the first voice rumbles. "...SIGSEGV... libc start main+0xe5... ah ha!  There's the problem."  You feel something fundamental in the fabric of reality change. The sensation is like sneezing and hiccuping at the same time. "There, that should fix it."
-
-A window opens in the darkness, infinitely far away and infinitely large. A face peeps down at you from the height of eternity.
-
-"Everything all right in there?"]]) --yes/no choice
+    Then a voice speaks, louder than an earthquake. It seems to echo inside your bones and from the very center of the galaxy. "Oh crap..." it says.
+    Another voice laughs musically, as if world itself were singing. "bobbens, did you just destroy the universe, again?"
+    "Mmmm, hold on," the first voice rumbles. "...SIGSEGV... libc start main+0xe5... ah ha!  There's the problem."  You feel something fundamental in the fabric of reality change. The sensation is like sneezing and hiccuping at the same time. "There, that should fix it."
+    A window opens in the darkness, infinitely far away and infinitely large. A face peeps down at you from the height of eternity.
+    "Everything all right in there?"]]) --yes/no choice
 
 -- If yes, the voice of the creator speaks to you:
 title[2] = _("The voice of the creator")
 text[2] = _([["Good. Sorry for the inconvenience. Please don't worry, I'll have reality reloaded again in just a sec."
-
-The creator turns to go, then pauses.
-
-"By the way, we're working hard on improving life, the universe, and all that, but we're a bit short handed. If you like this existence but think the details could be improved, the universe creation team could use your help. We'd be especially happy to see you if you have any special skill in drawing planets, designing ships, scripting the underlying laws of nature, or composing the music of the spheres. For information on how to contribute, just point your web-of-reality browser to naev.org."
-
-The watchmaker lifts his eyes up beyond your frame of reference and smiles.
-
-"OK then. Pushed. And we're back in 3... 2... 1..."]])
+    The creator turns to go, then pauses.
+    "By the way, we're working hard on improving life, the universe, and all that, but we're a bit short handed. If you like this existence but think the details could be improved, the universe creation team could use your help. We'd be especially happy to see you if you have any special skill in drawing planets, designing ships, scripting the underlying laws of nature, or composing the music of the spheres. For information on how to contribute, just point your web-of-reality browser to naev.org."
+    The watchmaker lifts his eyes up beyond your frame of reference and smiles.
+    "OK then. Pushed. And we're back in 3... 2... 1..."]])
 
 -- If no, the voice of the creator speaks to you:
 title[3] = _("The voice of the creator")
 text[3] = _([["Oh sorry about that. We're working hard on improving life, the universe, and all that, but the team is a bit short handed. Don't worry, I'll have reality reloaded again in just a sec."
-
-The creator turns to go, then pauses.
-
-"I tell you what: the universe creation team could really use your help. If you have any special skill in drawing planets, designing ships, scripting the underlying laws of nature, or composing the music of the spheres we'd be especially happy to see you. For information on how to contribute, just point your web-of-reality browser to naev.org."
-
-The watchmaker lifts his eyes up beyond your frame of reference and smiles.
-
-"OK then. Pushed. And we're back in 3... 2... 1..."]])
+    The creator turns to go, then pauses.
+    "I tell you what: the universe creation team could really use your help. If you have any special skill in drawing planets, designing ships, scripting the underlying laws of nature, or composing the music of the spheres we'd be especially happy to see you. For information on how to contribute, just point your web-of-reality browser to naev.org."
+    The watchmaker lifts his eyes up beyond your frame of reference and smiles.
+    "OK then. Pushed. And we're back in 3... 2... 1..."]])
 
 
 function create()
