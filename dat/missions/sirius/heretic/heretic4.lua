5b063d3191c8ffce66cd541a4a46d3fe05625ab5
Layla Marchant <onpon4@riseup.net>
2020-04-15 00:57:57 -0400

    More fixes. Finally all working.

--- dat/missions/sirius/heretic/heretic4.lua
+++ dat/missions/sirius/heretic/heretic4.lua
@@ -8,15 +8,17 @@
    to be very hard, and slightly combat oriented, but more supposed to
    involve smuggling elements.]]
 
+include "numstring.lua"
+
 --beginning messages
 bmsg = {}
 bmsg[1] = _([[You run up to Draga, who has a look of desperation on his face. "We need to go, now," he says. "The Sirii are overwhelming us, they're about to finish us off. Will you take me and as many Nasin as you can carry to %s in the %s system? This is our most desperate hour!"]])
 bmsg[2] = _([["Thank you! I knew you would do it!" Draga then proceeds to file as many people as can possibly fit onto your ship, enough to fill your ship's cargo hold to the brim. The number of Nasin members shocks you as they are packed into your ship.
-    As the Sirii approach ever closer, Draga yells at you to get the ship going and take off. As you get in the cabin and begin taking off, you see Draga under fire by a Sirian soldier. The last thing you see as you take off is Draga laying on the ground, lifeless.]])
+    As the Sirii approach ever closer, Draga yells at you to get the ship going and take off. You begin taking off just in time to see Draga under fire by a Sirian soldier who has infiltrated the base. The last thing you see as you take off is him lying on the ground, lifeless.]])
 
 --ending messages
 emsg = {}
-emsg[1] = _([[You land on %s, and you open the bay doors. You are still amazed at how many people Draga had helped get into the cargo hold. As you help everyone out of your ship, a man walks up to you. "Hello, my name is Jimmy. Thank you for helping all of these people. I am grateful. I've heard about you from Draga, and I will be forever in your debt. Here, please, take this." He presses a credit chip in your hand just as you finish helping everyone out of your ship. It seems it was a job well done.]])
+emsg[1] = _([[You land on %s and open the bay doors. You are still amazed at how many people Draga had helped get into the cargo hold. As you help everyone out of your ship, a man walks up to you. "Hello, my name is Jimmy. Thank you for helping all of these people. I am grateful. I've heard about you from Draga, and I will be forever in your debt. Here, please, take this." He presses a credit chip in your hand just as you finish helping everyone out of your ship. It seems it was a job well done.]])
 
 --mission osd
 osd = {}
@@ -41,8 +43,6 @@ function create()
    --set some mission stuff
    misn.setNPC(npc_name,"neutral/thief2")
    misn.setDesc(bar_desc)
-   misn.setTitle(misn_title)
-   misn.setReward(misn_reward:format(numstring(reward)))
 
    osd[1] = osd[1]:format(targetasset:name(),targetsys:name())
 end
@@ -55,16 +55,16 @@ function accept()
       misn.finish ()
    end
    misn.accept()
-   misn.setDesc(misn_desc:format( targetasset:name(), targetsys:name()))
-   misn.osdCreate(misn_title,osd)
-   misn.osdActive(1)
    player.allowSave(false) -- so the player won't get stuck with a mission they can't complete.
    tk.msg(misn_title,bmsg[2])
-   tk.msg(misn_title,bmsg[3])
    --convo over. time to finish setting the mission stuff.
    misn.markerAdd(targetsys,"high")
    free_cargo = player.pilot():cargoFree()
    people_carried =  (16 * free_cargo) + 7 --average weight per person is 62kg. one ton / 62 is 16. added the +7 for ships with 0 cargo.
+   misn.setTitle(misn_title)
+   misn.setReward(misn_reward:format(numstring(reward)))
+   misn.setDesc(misn_desc:format( targetasset:name(), targetsys:name()))
+   misn.osdCreate(misn_title,osd)
    refugees = misn.cargoAdd("Refugees",free_cargo)
    player.takeoff()
    --get the hooks.
@@ -121,7 +121,6 @@ function misn_over() --arent you glad thats over?
    if planet.cur() == planet.get("Ulios") then
       --introing one of the characters in the next chapter.
       tk.msg(misn_title,emsg[1]:format( targetasset:name() ))
-      tk.msg(misn_title,emsg[2])
       player.pay(reward)
       misn.cargoRm(refugees)
       misn_tracker = misn_tracker + 1
@@ -136,8 +135,7 @@ end
 function abort()
    tk.msg(misn_title,abort_msg:format(numstring(people_carried)))
    misn.cargoJet(refugees)
-   var.push("heretic_misn_tracker",-1) --if the player jettisons the peeps, the nasin will not let the player join there ranks anymore.
-   faction.modPlayerSingle("Nasin",-200) --big boost to the nasin, for completing the prologue
+   faction.modPlayerSingle("Nasin",-200)
    player.allowSave(true)
    misn.finish(true)
 end
