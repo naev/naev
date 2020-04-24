5c7ee2187aa7ff320527f38130b46ea21abe1a8d
Layla <diligentcircle@riseup.net>
2020-04-24 15:28:33 -0400

    Added BioPlasma Organ, fixed upgrade script errors

--- dat/events/bioship_upgrade.lua
+++ dat/events/bioship_upgrade.lua
@@ -67,7 +67,8 @@ bioship_parts = {
    { "Heavy Bioship Strong Fin Stage %s", 6 },
    { "Superheavy Bioship Strong Fin Stage %s", 7 },
 
-   -- Weapons (TODO)
+   -- Weapons
+   { "BioPlasma Organ Stage %s", 3 },
 }
 
 
@@ -133,7 +134,9 @@ function pay( amount )
          exp = exp - 100
          local parts = get_bioship_parts()
          if #parts > 0 then
-            local part, index = parts[ rnd.rnd( 1, #parts ) ]
+            local part_t = parts[ rnd.rnd( 1, #parts ) ]
+            local part = part_t[1]
+            local index = part_t[2]
             local current_level = 0
             local max_level = bioship_parts[index][2]
 
@@ -151,7 +154,7 @@ function pay( amount )
             if new_level > max_level then
                new_part = bioship_parts[index][1]:format( "X" ) 
             else
-               local sn = string.formmat( "%d", new_level )
+               local sn = string.format( "%d", new_level )
                new_part = bioship_parts[index][1]:format( sn )
             end
             player.pilot():addOutfit( new_part )
