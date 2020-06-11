3b89189f553939700cf191f4497d1f456eaa20fd
Layla Marchant <diligentcircle@riseup.net>
2020-06-11 13:59:49 -0400

    Added portraits for foodbiz

--- dat/missions/neutral/foodbiz/delivercake.lua
+++ dat/missions/neutral/foodbiz/delivercake.lua
@@ -45,7 +45,7 @@ function create () --No system shall be claimed by mission
 
    reward = 10000
 
-   misn.setNPC( npc_name, "neutral/female1" )
+   misn.setNPC( npc_name, "neutral/unique/paddy" )
    misn.setDesc( bar_desc )
 end
 
