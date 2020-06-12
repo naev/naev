5282c6c1ef8a3e58e876effa33a79e1d594ece4c
Layla Marchant <diligentcircle@riseup.net>
2020-06-11 23:15:59 -0400

    Fixed Soromid Brigand equip code.

--- dat/factions/equip/soromid.lua
+++ dat/factions/equip/soromid.lua
@@ -2,12 +2,14 @@ include("dat/factions/equip/generic.lua")
 
 
 equip_typeOutfits_weapons["Brigand"] = {
-   varied = true, probability = {
-      ["BioPlasma Organ Stage X"] = 3
-   };
-   "BioPlasma Organ Stage 1", "BioPlasma Organ Stage 2",
-   "BioPlasma Organ Stage 3", "BioPlasma Organ Stage X",
-   "Plasma Blaster MK3"
+   {
+      varied = true, probability = {
+         ["BioPlasma Organ Stage X"] = 3
+      };
+      "BioPlasma Organ Stage 1", "BioPlasma Organ Stage 2",
+      "BioPlasma Organ Stage 3", "BioPlasma Organ Stage X",
+      "Plasma Blaster MK3"
+   }
 }
 
 
