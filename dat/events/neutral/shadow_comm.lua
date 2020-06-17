17a00d1af6e76a4952837926742b9fff64962071
Layla Marchant <diligentcircle@riseup.net>
2020-06-17 11:23:17 -0400

    Added a reminder for shadow_comm every 500 periods.

--- dat/events/neutral/shadow_comm.lua
+++ dat/events/neutral/shadow_comm.lua
@@ -11,15 +11,18 @@ text = {}
 title[1] = _("An open invitation")
 text[1] = _([["Greetings, %s," the pilot of the Vendetta says to you as soon as you answer his hail. "I have been looking for you on behalf of an acquaintance of yours. She wishes to meet with you at a place of her choosing, and a time of yours. It involves a proposition that you might find interesting - if you don't mind sticking your neck out."
     You frown at that, but you ask the pilot where this acquaintance wishes you to go anyway.
-    "Fly to the %s system," he replies. "She will meet you there. There's no rush - but I suggest you go see her at the earliest opportunity."
-    The screen blinks out, and the Vendetta goes about its business, paying you no more attention. It seems there's someone out there who wants to see you, and there's only one way to find out what about. Perhaps you should make a note of the place you're supposed to meet her: the Pas system.]])
+    "Fly to the %s system," he replies. "She will meet you there. There's no rush, but I suggest you go see her at the earliest opportunity."
+    The screen blinks out and the Vendetta goes about its business, paying you no more attention. It seems there's someone out there who wants to see you, and there's only one way to find out what about. Perhaps you should make a note of the place you're supposed to meet her: the %s system.]])
+text[2] = _([["Greetings, %s," the pilot of the Vendetta says. "Sorry to bother you; I've just noticed that it's been quite some time since we contacted you and I wanted to check in on you.
+    "As was mentioned previously, the one sho wishes to meet you can be found in the %s system, which is near Empire space. Again, there's no rush, but I suggest you go to see her at the earliest opportunity."
+    The screen blinks out and the Vendetta goes about its business, paying you no more attention. You make a mental note again to try to remember to go to the %s system when you get the chance.]])
 
 function create ()
     sysname = "Pas"
     destsys = system.get(sysname)
 
     -- Create a Vendetta who hails the player after a bit
-    hailed = false
+    hail_time = nil
     vendetta = pilot.add("Four Winds Vendetta", nil, true)[1]
     vendetta:control()
     vendetta:follow(player.pilot())
@@ -43,26 +46,35 @@ end
 
 -- Triggered when the player hails the ship
 function hail(p)
-    hailed = true
-    tk.msg(title[1], string.format(text[1], player.name(), sysname))
-
-    -- The event should now remain active until Pas
-    -- Clear the hooks that would otherwise finish it
-    hook.rm(hook1)
-    hook.rm(hook2)
-    hook.rm(hook3)
-    hook.rm(hook4)
     hook.rm(hailhook)
-    
-    player.commClose()
 
-    vendetta:control()
-    vendetta:hyperspace()
+    if hail_time == nil then
+        hail_time = time.get()
+        tk.msg(title[1], text[1]:format(player.name(), sysname, sysname))
 
-    -- Catch the player jumping into Pas
-    -- The player may save between now and then, make sure our hook is saved too
-    evt.save(true)
-    hook.jumpin("jumpin")
+        -- The event should now remain active until Pas
+        -- Clear the hooks that would otherwise finish it
+        hook.rm(hook1)
+        hook.rm(hook2)
+        hook.rm(hook3)
+        hook.rm(hook4)
+
+        player.commClose()
+
+        vendetta:control()
+        vendetta:hyperspace()
+
+        -- Catch the player jumping into Pas
+        -- The player may save between now and then, make sure our hook is saved too
+        evt.save(true)
+        hook.enter("jumpin")
+    else
+        hail_time = time.get()
+        tk.msg(title[1], text[2]:format(player.name(), sysname, sysname))
+        player.commClose()
+        vendetta:control()
+        vendetta:hyperspace()
+    end
 end
 
 function jumpin()
@@ -74,6 +86,11 @@ function jumpin()
         seiryuu:setHilight(true)
         seiryuu:setVisplayer(true)
         hook.pilot(seiryuu, "board", "board")
+    elseif hail_time == nil or time.get() > hail_time + time.create(0, 500, 0) then
+        vendetta = pilot.add("Four Winds Vendetta", nil, true)[1]
+        vendetta:control()
+        vendetta:follow(player.pilot())
+        hook.timer(500, "proximityScan", {focus = vendetta, funcname = "hailme"})
     end
 end
 
@@ -88,7 +105,7 @@ end
 
 -- Clean up
 function finish()
-    if not hailed then
+    if hail_time == nil then
         var.pop("shadowvigil_active")
     end
     if hailhook then
