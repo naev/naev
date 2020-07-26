2668f237242a37ec52037934f0b901e6ddc1591e
nenau-again <renaud.ferrier@gmail.com>
2020-07-26 22:10:52 +0200

    No consideration about known or unknown JP with argument show_hidden in map_getJumpPath

--- src/map.c
+++ src/map.c
@@ -2247,8 +2247,8 @@ StarSystem** map_getJumpPath( int* njumps, const char* sysstart,
          if (jp_isFlag( jp, JP_EXITONLY ))
             continue;
 
-         /* Skip hidden jumps if they're unknown and not specifically requested */
-         if (!show_hidden && jp_isFlag( jp, JP_HIDDEN ) && !jp_isKnown(jp))
+         /* Skip hidden jumps if they're not specifically requested */
+         if (!show_hidden && jp_isFlag( jp, JP_HIDDEN ))
             continue;
 
          /* Check to see if it's already in the closed set. */
