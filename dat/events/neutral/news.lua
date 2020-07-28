e0d9c9b426c5edc71d62680636a337d4df11d3ee
Layla Marchant <diligentcircle@riseup.net>
2020-07-28 03:25:13 -0400

    Moved many "Generic" stories to faction sections.
    These are stories that reference either specific places or specific
    factions, so they shouldn't show up in the news feed of, say, the
    Proteron for example, or the FLF or Pirates.

--- dat/events/neutral/news.lua
+++ dat/events/neutral/news.lua
@@ -16,12 +16,12 @@ articles["Generic"] = {
    {
       tag = "Techs Probe for Sol",
       title = _("Techs Probe for Sol"),
-      desc = _("Technicians at the Omega Station recently returned from a new effort to understand the Sol Incident. They expect the first data to arrive in 50 decaperiods' time.")
+      desc = _("Technicians recently returned from a new effort to understand the Sol Incident. They expect the first data to arrive in 50 decaperiods' time.")
    },
    {
       tag = "Sand Monster Caught On Tape",
       title = _("Sand Monster Caught On Tape"),
-      desc = _("Local residents claim footage from an Arrakis security camera shows elusive beast walking through a desert storm. Exobiologists suspect a hoax.")
+      desc = _("Local residents claim footage from a security camera shows elusive beast walking through a desert storm. Exobiologists suspect a hoax.")
    },
    {
       tag = "New Handheld AI Debuts",
@@ -32,11 +32,6 @@ articles["Generic"] = {
       tag = "Experiment Produces Cold Fusion",
       title = _("Experiment Produces Cold Fusion"),
       desc = _("In an interview with Bleeding Edge anchor McKenzie Kruft, a researcher at Eureka labs says he has produced a tabletop atomic reaction. He hopes to publish his results in a science journal later this cycle.")
-   },
-   {
-      tag = "Bees Introduced to Emperor's Fist",
-      title = _("Bees Introduced to Emperor's Fist"),
-      desc = _("As they prepare the gardens of the future Imperial compound, entomologists have established the first colony of the Earth insects on the planet formerly known as G Scorpeii 5.")
    },
    {
       tag = "The Case for Sex",
@@ -66,97 +61,12 @@ articles["Generic"] = {
       title = _("Genetric Board Meets"),
       desc = _("In the wake of the Yavi Bartolo's departure as CSO of Genetric Technologies, the board of directors will meet today. The group is expected to appoint a new chief science officer.")
    },
-   {
-      tag = "Aerosys Earnings Drop",
-      title = _("Aerosys Earnings Drop"),
-      desc = _("The spaceways may swarm with Hyena-model craft, but today Aerosys recorded another quarterly loss. The company is investigating the discrepancy between the popularity of the craft and its sales figures.")
-   },
-   {
-      tag = "Aerosys Victim of Pirate Manufacturing",
-      title = _("Aerosys Victim of Pirate Manufacturing"),
-      desc = _("The ship manufacturer has released a study indicating its signature Hyena model is being produced by a hidden system of unlicensed manufacturers.")
-   },
-   {
-      tag = "Melendez CEO on Strategy",
-      title = _("Melendez CEO on Strategy"),
-      desc = _("The Chief Executive Officer of ship maker Melendez Inc. thinks manufacturers should follow his company's lead in keeping costs down and producing for the mass market.")
-   },
-   {
-      tag = "The Goddard Exception",
-      title = _("The Goddard Exception"),
-      desc = _("Why has a community with more expertise sailing than flying produced the Empire's elite civilian spacecraft?  Lord Warthon says the secret lies in his family's hands-on tradition of leadership.")
-   },
-   {
-      tag = "Nexus Contract Finalised",
-      title = _("Nexus Contract Finalised"),
-      desc = _("The Empire agreed to terms with shipbuilder Nexus for a new generation of military craft. The deal extends the partnership with the government for another 10 cycles.")
-   },
-   {
-      tag = "Sneak Peek: the Kestrel",
-      title = _("Sneak Peek: the Kestrel"),
-      desc = _("Our reporter took a tour through Krain's mysterious space craft. He says it poses a challenge to the Goddard.")
-   },
    --[[
       Politics
    --]]
-   {
-      tag = "FLF Responsible for Piracy",
-      title = _("FLF Responsible for Piracy"),
-      desc = _("Law enforcement expert Paet Dohmer's upcoming essay describes the group as \"more criminal gang than independence movement\", according to his publicist.")
-   },
-   {
-      tag = "Front Responsible for Shipping Woes",
-      title = _("Front Responsible for Shipping Woes"),
-      desc = _("A spokeswoman for the separatist group says they were behind the recent series of attacks on cargo ships operating between Dakron and Theras. Dvaered officials condemned the actions.")
-   },
-   {
-      tag = "Election on Caladan Marred by Fraud",
-      title = _("Election on Caladan Marred by Fraud"),
-      desc = _("As many as two of every hundred votes counted after the recent polling decaperiod may be falsified, an ombudsman reports. The opposition party demanded the election be annulled.")
-   },
-   {
-      tag = "Empire Relies on Prison Labour",
-      title = _("Empire Relies on Prison Labour"),
-      desc = _("A recent report by the Soromid House Ways and Means Committee suggests infrastructure may be too dependent the on the incarcerated population.")
-   },
-   {
-      tag = "Imperial Oversight Scandal",
-      title = _("Imperial Oversight Scandal"),
-      desc = _("Sources close to the Imperial Chancellor say they see the failure at the Department of Oversight, whose inspectors overlooked serious failings in other supervisory bodies, as a serious oversight.")
-   },
-   {
-      tag = "Governor Helmer Jailed",
-      title = _("Governor Helmer Jailed"),
-      desc = _("Imperial Auditors arrested governor Rex Helmer of Anecu on charges of corruption. He has been removed from office and transported to a holding facility awaiting trial.")
-   },
-   {
-      tag = "Jouvanin Tapped as Interim Chief",
-      title = _("Jouvanin Tapped as Interim Chief"),
-      desc = _("Following the arrest of Rex Helmer, former Anecu deputy governor Elene Jouvanin will be sworn in today. She will serve out the term as governor.")
-   },
-   {
-      tag = "Imperial Council Opens Doors",
-      title = _("Imperial Council Opens Doors"),
-      desc = _("The supreme advisory body invited undergraduates from six top schools to sit in on a day's deliberations. Topics required biodiversity strategy.")
-   },
-   {
-      tag = "FLF Terrorist Trial Ends",
-      title = _("FLF Terrorist Trial Ends"),
-      desc = _("FLF Terrorist Trial ended this cycle with an unsurprising death sentence for all five members of the Nor spaceport bombing. Execution is scheduled in 10 periods.")
-   },
-   {
-      tag = "New Challenges for New Times",
-      title = _("New Challenges for New Times"),
-      desc = _("The Dvaered council after a unanimous ruling decided to increase patrols in Dvaered space due to the recent uprising in FLF terrorism. The new measure is expected to start within the next cycle.")
-   },
    --[[
       Human interest.
    --]]
-   {
-      tag = "Emperor's Aid Gets Hitched",
-      title = _("Emperor's Aid Gets Hitched"),
-      desc = _("On Saturday, Imperial secretary Karil Lorenze married long time fiancee Rachid Baouda in the future palace gardens on Emperor's Fist. His Eminence the Bishop of Bao performed the ceremony.")
-   },
    {
       tag = "Eyeteeth Back in Fashion",
       title = _("Eyeteeth Back in Fashion"),
@@ -192,11 +102,6 @@ articles["Generic"] = {
       title = _("Games for Young Pilots"),
       desc = _("Want your child to have a chance at a career as a space pilot?  Games like Super Julio Omniverse and SpaceFox help your child develop twitch muscles.")
    },
-   {
-      tag = "Remembering the Past",
-      title = _("Remembering the Past"),
-      desc = _("The Emperor has scheduled a new monument to be constructed on Emperor's Fist in honour of all those dead in the Incident.")
-   }
 }
 
 articles["Dvaered"] = {
@@ -214,6 +119,31 @@ articles["Dvaered"] = {
    --[[
       Politics
    --]]
+   {
+      tag = "FLF Responsible for Piracy",
+      title = _("FLF Responsible for Piracy"),
+      desc = _("Law enforcement expert Paet Dohmer's upcoming essay describes the group as \"more criminal gang than independence movement\", according to his publicist.")
+   },
+   {
+      tag = "Front Responsible for Shipping Woes",
+      title = _("Front Responsible for Shipping Woes"),
+      desc = _("A spokeswoman for the separatist group says they were behind the recent series of attacks on cargo ships operating between Dakron and Theras. Dvaered officials condemned the actions.")
+   },
+   {
+      tag = "Jouvanin Tapped as Interim Chief",
+      title = _("Jouvanin Tapped as Interim Chief"),
+      desc = _("Following the arrest of Rex Helmer, former Anecu deputy governor Elene Jouvanin will be sworn in today. She will serve out the term as governor.")
+   },
+   {
+      tag = "FLF Terrorist Trial Ends",
+      title = _("FLF Terrorist Trial Ends"),
+      desc = _("FLF Terrorist Trial ended this cycle with an unsurprising death sentence for all five members of the Nor spaceport bombing. Execution is scheduled in 10 periods.")
+   },
+   {
+      tag = "New Challenges for New Times",
+      title = _("New Challenges for New Times"),
+      desc = _("The Dvaered council after a unanimous ruling decided to increase patrols in Dvaered space due to the recent uprising in FLF terrorism. The new measure is expected to start within the next cycle.")
+   },
    --[[
       Human interest.
    --]]
@@ -245,7 +175,32 @@ articles["Goddard"] = {
       tag = "Goddard Awarded Best Ship",
       title = _("Goddard Awarded Best Ship"),
       desc = _("Once again the Goddard Battlecruiser was awarded the Best Overall Ship prize by the Dvaered Armada's annual Ship Awards. \"Very few ships have reliability like the Goddard,\" said Lord Warthon upon receiving the award on behalf of House Goddard.")
-   }
+   },
+   {
+      tag = "Aerosys Earnings Drop",
+      title = _("Aerosys Earnings Drop"),
+      desc = _("The spaceways may swarm with Hyena-model craft, but today Aerosys recorded another quarterly loss. The company is investigating the discrepancy between the popularity of the craft and its sales figures.")
+   },
+   {
+      tag = "Aerosys Victim of Pirate Manufacturing",
+      title = _("Aerosys Victim of Pirate Manufacturing"),
+      desc = _("The ship manufacturer has released a study indicating its signature Hyena model is being produced by a hidden system of unlicensed manufacturers.")
+   },
+   {
+      tag = "Melendez CEO on Strategy",
+      title = _("Melendez CEO on Strategy"),
+      desc = _("The Chief Executive Officer of ship maker Melendez Inc. thinks manufacturers should follow his company's lead in keeping costs down and producing for the mass market.")
+   },
+   {
+      tag = "The Goddard Exception",
+      title = _("The Goddard Exception"),
+      desc = _("Why has a community with more expertise sailing than flying produced the Empire's elite civilian spacecraft?  Lord Warthon says the secret lies in his family's hands-on tradition of leadership.")
+   },
+   {
+      tag = "Sneak Peek: the Kestrel",
+      title = _("Sneak Peek: the Kestrel"),
+      desc = _("Our reporter took a tour through Krain's mysterious space craft. He says it poses a challenge to the Goddard.")
+   },
    --[[
       Politics
    --]]
@@ -281,7 +236,7 @@ articles["Sirius"] = {
       tag = "Words of Tranquility",
       title = _("Words of Tranquility"),
       desc = _("We welcome many new Touched who have recently begun ministering to the Shaira echelon after their long pilgrimage on Mutris. House Sirius is still a refugee for the orphans lost in this Universe.")
-   }
+   },
 }
 
 articles["Pirate"] = {
@@ -321,7 +276,7 @@ articles["Pirate"] = {
       tag = "Cats in New Haven",
       title = _("Cats in New Haven"),
       desc = _("An explosion in the cat population of New Haven has created an adoption campaign with the slogan, \"Pirate Cats, for those lonely space trips.\". Is your space vessel full of vermin? Adopt a cat today!")
-   }
+   },
 }
 
 articles["Empire"] = {
@@ -333,6 +288,11 @@ articles["Empire"] = {
       title = _("Terraforming Emperor's Fist"),
       desc = _("New bleeding-edge terraforming techniques to be tried on Emperor's Fist. Studies show that these techniques could speed up the terraforming process by as much as 40%.")
    },
+   {
+      tag = "Bees Introduced to Emperor's Fist",
+      title = _("Bees Introduced to Emperor's Fist"),
+      desc = _("As they prepare the gardens of the future Imperial compound, entomologists have established the first colony of the Earth insects on the planet formerly known as G Scorpeii 5.")
+   },
    --[[
       Business
    --]]
@@ -340,6 +300,11 @@ articles["Empire"] = {
       tag = "Empire Keeping Traders Safe",
       title = _("Empire Keeping Traders Safe"),
       desc = _("Recent studies show that reports of piracy on Trader vessels have gone down by up to 40% in some sectors. This is a demonstration of the Empire's commitment to eradicating piracy.")
+   },
+   {
+      tag = "Nexus Contract Finalised",
+      title = _("Nexus Contract Finalised"),
+      desc = _("The Empire agreed to terms with shipbuilder Nexus for a new generation of military craft. The deal extends the partnership with the government for another 10 cycles.")
    },
    --[[
       Politics
@@ -349,6 +314,16 @@ articles["Empire"] = {
       title = _("New Empire Recruits"),
       desc = _("Emperor's recruiting strategy a success. Many new soldiers joining the Empire Armada. \"We haven't had such a successful campaign in ages!\" - Raid Steele, spokesman for recruiting campaign.")
    },
+   {
+      tag = "Governor Helmer Jailed",
+      title = _("Governor Helmer Jailed"),
+      desc = _("Imperial Auditors arrested governor Rex Helmer of Anecu on charges of corruption. He has been removed from office and transported to a holding facility awaiting trial.")
+   },
+   {
+      tag = "Imperial Council Opens Doors",
+      title = _("Imperial Council Opens Doors"),
+      desc = _("The supreme advisory body invited undergraduates from six top schools to sit in on a day's deliberations. Topics required biodiversity strategy.")
+   },
    --[[
       Human interest.
    --]]
@@ -356,14 +331,54 @@ articles["Empire"] = {
       tag = "New Cat in the Imperial Family",
       title = _("New Cat in the Imperial Family"),
       desc = _("The Emperor's daughter was recently gifted a cat. Cat could be named \"Snuggles\" and seems to be all white.")
-   }
+   },
+   {
+      tag = "Emperor's Aid Gets Hitched",
+      title = _("Emperor's Aid Gets Hitched"),
+      desc = _("Imperial secretary Karil Lorenze married long time fiancee Rachid Baouda in the future palace gardens on Emperor's Fist. His Eminence the Bishop of Bao performed the ceremony.")
+   },
+   {
+      tag = "Remembering the Past",
+      title = _("Remembering the Past"),
+      desc = _("The Emperor has scheduled a new monument to be constructed on Emperor's Fist in honour of all those dead in the Incident.")
+   },
 }
+
+articles["Frontier"] = {
+   --[[
+      Science and technology
+   --]]
+   --[[
+      Business
+   --]]
+   --[[
+      Politics
+   --]]
+   {
+      tag = "Election on Caladan Marred by Fraud",
+      title = _("Election on Caladan Marred by Fraud"),
+      desc = _("As many as two of every hundred votes counted after the recent polling decaperiod may be falsified, an ombudsman reports. The opposition party demanded the election be annulled.")
+   },
+   {
+      tag = "Empire Relies on Prison Labour",
+      title = _("Empire Relies on Prison Labour"),
+      desc = _("A recent report by the Soromid House Ways and Means Committee suggests infrastructure may be too dependent the on the incarcerated population.")
+   },
+   {
+      tag = "Imperial Oversight Scandal",
+      title = _("Imperial Oversight Scandal"),
+      desc = _("Sources close to the Imperial Chancellor say they see the failure at the Department of Oversight, whose inspectors overlooked serious failings in other supervisory bodies, as a serious oversight.")
+   },
+   --[[
+      Human interest.
+   --]]
+}
+
 articles["Independent"] = {}
-articles["FLF"]={}
-articles["Frontier"]={}
-articles["Proteron"]={}
-articles["Za'lek"]={}
-articles["Thurion"]={}
+articles["FLF"] = {}
+articles["Proteron"] = {}
+articles["Za'lek"] = {}
+articles["Thurion"] = {}
 
 econ_articles = {
    {
