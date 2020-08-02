892f72a23bda1f1eb2e854b40e58c6a9fea1014e
Layla Marchant <diligentcircle@riseup.net>
2020-08-01 22:36:42 -0400

    Changed the gray text shade to a lighter color.

--- src/colour.c
+++ src/colour.c
@@ -101,7 +101,7 @@ const glColour cFontBlue      =  { .r = 0.2, .g = 0.4, .b = 0.8, .a = 1.  }; /**
 const glColour cFontOrange    =  { .r = 1.0, .g = 0.6, .b = 0.0, .a = 1.  }; /**< Orange font colour. */
 const glColour cFontYellow    =  { .r = 0.9, .g = 1.0, .b = 0.3, .a = 1.  }; /**< Yellow font colour. */
 const glColour cFontWhite     =  { .r = 0.95, .g = 0.95, .b = 0.95, .a = 1.  }; /**< White font colour. */
-const glColour cFontGrey      =  { .r = 0.6, .g = 0.6, .b = 0.6, .a = 1.  }; /**< Grey font colour. */
+const glColour cFontGrey      =  { .r = 0.7, .g = 0.7, .b = 0.7, .a = 1.  }; /**< Grey font colour. */
 const glColour cFontPurple    =  { .r = 0.9, .g = 0.3, .b = 0.9, .a = 1.  }; /**< Purple font colour. */
 
 
