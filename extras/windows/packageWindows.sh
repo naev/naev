bbc421d655942a76f41038b018368e0c88f991cb
Jack Greiner <jack@emoss.org>
2020-08-04 12:22:56 -0400

    Added nightly conf and fixed builds on mingw32

--- extras/windows/packageWindows.sh
+++ extras/windows/packageWindows.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 # WINDOWS PACKAGING SCRIPT FOR NAEV
-# Requires NSIS installed
+# Requires NSIS to be installed
 #
 # This script should be run after compiling Naev
 # It detects the current environment, and builds the appropriate NSIS installer
-# into the "extras/windows/installer" directory.
+# into the root naev directory.
 #
+
+# Checks if argument(s) are valid
+
+if [[ $1 == "--nightly" ]]; then
+    echo "Building for nightly release"
+    NIGHTLY=true
+    # Get Formatted Date
+    BUILD_DATE="$(date +%m_%d_%Y)"
+elif [[ $1 == "" ]]; then
+    echo "No arguments passed, assuming normal release"
+    NIGHTLY=false
+elif [[ $1 != "--nightly" ]]; then
+    echo "Please use argument --nightly if you are building this as a nightly build"
+    exit -1
+else
+    echo "Something went wrong."
+    exit -1
+fi
 
 # Check if we are running in the right place
 
 if [[ ! -f "naev.6" ]]; then
-   echo "Please run from Naev root directory."
-   exit -1
+    echo "Please run from Naev root directory."
+    exit -1
 fi
 
 # Rudementary way of detecting which environment we are packaging.. 
@@ -24,19 +42,19 @@ elif [[ $PATH == *"mingw64"* ]]; then
     echo "Detected MinGW64 environment"
     ARCH="64"
 else
-    echo "Welp, I don't know what environment this is.. "
-    exit 1
+    echo "Welp, I don't know what environment this is... Make sure you are running this in an MSYS2 MinGW environment"
+    exit -1
 fi
 
 VERSION="$(cat $(pwd)/VERSION)"
 # Get version, negative minors mean betas
-if [[ -n $(echo "${VERSION}" | grep "-") ]]; then
-    BASEVER=$(echo "${VERSION}" | sed 's/\.-.*//')
-    BETAVER=$(echo "${VERSION}" | sed 's/.*-//')
-    VERSION="${BASEVER}.0-beta${BETAVER}"
+if [[ -n $(echo "$VERSION" | grep "-") ]]; then
+    BASEVER=$(echo "$VERSION" | sed 's/\.-.*//')
+    BETAVER=$(echo "$VERSION" | sed 's/.*-//')
+    VERSION="$BASEVER.0-beta$BETAVER"
 else
     echo "could not find VERSION file"
-    exit 1
+    exit -1
 fi
 
 # Download Inetc
@@ -56,7 +74,7 @@ unzip ~/temp/Inetc.zip -d ~/temp
 
 echo "Installing Inetc"
 if [[ $ARCH == "32" ]]; then
-    cp -r ~/temp/Plugins/x86-unicode/* /mingw64/share/nsis/Plugins/unicode
+    cp -r ~/temp/Plugins/x86-unicode/* /mingw32/share/nsis/Plugins/unicode
     echo "Cleaning up temp folder"
     rm -rf ~/temp
 elif [[ $ARCH == "64" ]]; then
@@ -67,7 +85,7 @@ else
     echo "I'm afraid I can't do that Dave..."
     echo "Cleaning up temp folder"
     rm -rf ~/temp
-    exit 1
+    exit -1
 fi
 
 # Move compiled binary to staging folder.
@@ -75,19 +93,54 @@ fi
 echo "creating staging area"
 mkdir -p extras/windows/installer/bin
 
-echo "copying naev binary to staging area"
-cp src/naev.exe extras/windows/installer/bin/naev-${VERSION}-win$ARCH.exe
-
 # Collect DLLs
 
+if [[ $ARCH == "32" ]]; then
+for fn in `cygcheck src/naev.exe | grep -i "mingw32"`; do
+    echo "copying $fn to staging area"
+    cp $fn extras/windows/installer/bin
+done
+elif [[ $ARCH == "64" ]]; then
 for fn in `cygcheck src/naev.exe | grep -i "mingw64"`; do
     echo "copying $fn to staging area"
     cp $fn extras/windows/installer/bin
 done
+else
+    echo "Aw, man, I shot Marvin in the face..."
+    echo "Something went wrong while looking for DLLs to stage."
+    exit -1
+fi
 
-makensis -DVERSION=${BASEVER}.0 -DVERSION_SUFFIX=-beta${BETAVER} -DARCH=$ARCH extras/windows/installer/naev.nsi
+echo "copying naev binary to staging area"
+if [[ $NIGHTLY ]]; then
+cp src/naev.exe extras/windows/installer/bin/naev-$VERSION-$BUILD_DATE-win$ARCH.exe
+elif [[ ! $NIGHTLY ]]; then
+cp src/naev.exe extras/windows/installer/bin/naev-$VERSION-win$ARCH.exe
+else
+    echo "Cannot think of another movie quote."
+    echo "Something went wrong while copying binary to staging area."
+    exit -1
+fi
+
+# Build installer
+
+if [[ $NIGHTLY ]]; then
+makensis -DVERSION=$BASEVER.0 -DVERSION_SUFFIX=-beta$BETAVER-$BUILD_DATE -DARCH=$ARCH extras/windows/installer/naev-nightly.nsi
+# Move installer to root directory
+
+mv extras/windows/installer/naev-$VERSION-$BUILD_DATE-win$ARCH.exe naev-$VERSION-$BUILD_DATE-win$ARCH.exe
+elif [[ ! $NIGHTLY ]]; then
+makensis -DVERSION=$BASEVER.0 -DVERSION_SUFFIX=-beta$BETAVER -DARCH=$ARCH extras/windows/installer/naev.nsi
+# Move installer to root directory
+
+mv extras/windows/installer/naev-$VERSION-win$ARCH.exe naev-$VERSION-win$ARCH.exe
+else
+    echo "Cannot think of another movie quote.. again."
+    echo "Something went wrong.."
+    exit -1
+fi
 
 echo "Cleaning up staging area"
 rm -rf extras/windows/installer/bin
 
-echo "Successfully built Windows Installer"
+echo "Successfully built Windows Installer for win$ARCH"
