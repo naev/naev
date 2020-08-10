8e6ccd3eee921d037ff88302ff4d495e0d5f110f
Layla Marchant <diligentcircle@riseup.net>
2020-08-09 20:41:41 -0400

    Fixed up the Proteron Derivative's description, started on Proteron equips.

---
+++ dat/factions/equip/proteron.lua
@@ -0,0 +1,24 @@
+require "dat/factions/equip/generic.lua"
+
+
+equip_typeOutfits_coreSystems["Derivative"] = {
+   "Milspec Orion 2301 Core System"
+}
+
+equip_typeOutfits_engines["Derivative"] = {
+   "Tricon Zephyr Engine"
+}
+
+equip_typeOutfits_hulls["Derivative"] = {
+   "S&K Ultralight Combat Plating"
+}
+
+equip_typeOutfits_weapons["Derivative"] = {
+   {
+      num = 1;
+      "Unicorp Banshee Launcher", "Ripper Cannon"
+   },
+   {
+      "Ripper Cannon", "Laser Cannon MK3"
+   }
+}
