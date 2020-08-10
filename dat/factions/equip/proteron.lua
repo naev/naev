4a8183f13616d27a1d1f17630576840863ee1738
Layla Marchant <diligentcircle@riseup.net>
2020-08-09 21:40:51 -0400

    Fixed up equip script, improved Proteron kahan balancing.
    This replaces the heavier weapons and extra med utility slot with
    lighter size and an extra lge structural. Works better that way I
    think. The lighter mass (lightest mass of any cruiser) enables it
    to be faster than other cruisers while carrying more of a payload.
    Given this, it's had its time dilation value decreased as well,
    to 175% (compared to the 150% time dilation of corvettes), not
    unlike the Kestrel in that sense.

--- dat/factions/equip/proteron.lua
+++ dat/factions/equip/proteron.lua
@@ -2,23 +2,57 @@ require "dat/factions/equip/generic.lua"
 
 
 equip_typeOutfits_coreSystems["Derivative"] = {
-   "Milspec Orion 2301 Core System"
+   "Milspec Orion 2301 Core System",
+}
+equip_typeOutfits_coreSystems["Kahan"] = {
+   "Milspec Orion 5501 Core System"
 }
 
 equip_typeOutfits_engines["Derivative"] = {
-   "Tricon Zephyr Engine"
+   "Tricon Zephyr Engine",
+}
+equip_typeOutfits_engines["Kahan"] = {
+   "Tricon Cyclone II Engine"
 }
 
 equip_typeOutfits_hulls["Derivative"] = {
-   "S&K Ultralight Combat Plating"
+   "S&K Ultralight Combat Plating",
+}
+equip_typeOutfits_hulls["Kahan"] = {
+   "S&K Medium-Heavy Combat Plating"
 }
 
 equip_typeOutfits_weapons["Derivative"] = {
    {
       num = 1;
-      "Unicorp Banshee Launcher", "Ripper Cannon"
+      "Unicorp Banshee Launcher", "Ripper Cannon",
    },
    {
-      "Ripper Cannon", "Laser Cannon MK3"
+      "Ripper Cannon", "Laser Cannon MK3",
    }
 }
+equip_typeOutfits_weapons["Kahan"] = {
+   {
+      num = 1;
+      "Turbolaser", "Railgun Turret", "Heavy Laser",
+   },
+   {
+      num = 1;
+      "Ragnarok Beam", "Grave Beam", "Turbolaser", "Railgun Turret",
+      "Heavy Laser",
+   },
+   {
+      "Enygma Systems Turreted Fury Launcher",
+      "Enygma Systems Turreted Headhunter Launcher",
+   }
+}
+
+
+--[[
+-- @brief Does Proteron pilot equipping
+--
+--    @param p Pilot to equip
+--]]
+function equip( p )
+   equip_generic( p )
+end
