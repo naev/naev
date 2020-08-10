706caa214f407736c83c1c498c53d0f3123283f1
Layla Marchant <diligentcircle@riseup.net>
2020-08-09 22:12:03 -0400

    Added entries for the Archimedes in Proteron definition.

--- dat/factions/equip/proteron.lua
+++ dat/factions/equip/proteron.lua
@@ -7,19 +7,28 @@ equip_typeOutfits_coreSystems["Derivative"] = {
 equip_typeOutfits_coreSystems["Kahan"] = {
    "Milspec Orion 5501 Core System"
 }
+equip_typeOutfits_coreSystems["Archimedes"] = {
+   "Milspec Orion 9901 Core System"
+}
 
 equip_typeOutfits_engines["Derivative"] = {
    "Tricon Zephyr Engine",
 }
 equip_typeOutfits_engines["Kahan"] = {
-   "Tricon Cyclone II Engine"
+   "Tricon Cyclone II Engine",
+}
+equip_typeOutfits_engines["Archimedes"] = {
+   "Tricon Typhoon II Engine",
 }
 
 equip_typeOutfits_hulls["Derivative"] = {
    "S&K Ultralight Combat Plating",
 }
 equip_typeOutfits_hulls["Kahan"] = {
-   "S&K Medium-Heavy Combat Plating"
+   "S&K Medium-Heavy Combat Plating",
+}
+equip_typeOutfits_hulls["Archimedes"] = {
+   "Unicorp B-20 Heavy Plating", "S&K Superheavy Combat Plating",
 }
 
 equip_typeOutfits_weapons["Derivative"] = {
@@ -39,7 +48,25 @@ equip_typeOutfits_weapons["Kahan"] = {
    {
       num = 1;
       "Ragnarok Beam", "Grave Beam", "Turbolaser", "Railgun Turret",
-      "Heavy Laser",
+      "Heavy Laser", "TeraCom Imperator Launcher",
+   },
+   {
+      "Enygma Systems Turreted Fury Launcher",
+      "Enygma Systems Turreted Headhunter Launcher",
+   }
+}
+equip_typeOutfits_weapons["Archimedes"] = {
+   {
+      num = 1;
+      "Heavy Laser", "Heavy Ripper Turret",
+   },
+   {
+      num = 2;
+      "Turbolaser", "Railgun Turret", "Heavy Laser",
+   },
+   {
+      num = 2;
+      "Ragnarok Beam", "Grave Beam", "Turbolaser", "Railgun Turret",
    },
    {
       "Enygma Systems Turreted Fury Launcher",
