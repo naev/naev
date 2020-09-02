7d446eda66b283445c3062d61ab5478cda9b74f9
Edgar Simo-Serra <ess@waseda.jp>
2020-09-02 14:43:19 +0900

    Check for missing and duplicated songs.

--- utils/soundtrack.py
+++ utils/soundtrack.py
@@ -2,6 +2,8 @@
 
 import yaml
 import zipfile
+import glob
+import os
 
 from tinytag import TinyTag
 
@@ -11,9 +13,19 @@ with open('dat/snd/soundtrack.yaml','r') as f:
 with open('VERSION', 'r') as f:
     version = f.read().strip()
 
+all_songs = []
+for song in glob.glob("dat/snd/music/*.ogg"):
+    all_songs.append( os.path.basename(song) )
+
+duplicated_songs = []
 with zipfile.ZipFile(f"naev-{version}-soundtrack.zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
     i = 1
     for song in soundtrack:
+        if song['filename'] in all_songs:
+            all_songs.remove( song['filename'] )
+        else:
+            duplicated_songs.append( song['filename'] )
+
         filename = 'dat/snd/music/'+song['filename']
         tag = TinyTag.get(filename)
         length = "%d:%02d" % (tag.duration//60, tag.duration%60)
@@ -21,3 +33,10 @@ with zipfile.ZipFile(f"naev-{version}-soundtrack.zip", 'w', zipfile.ZIP_DEFLATED
         print( f"{number}. {song['filename']} ({length})" )
         zipf.write( filename, number+"_"+song['filename'] )
         i += 1
+
+if len(all_songs) > 0:
+    print( 'MISSING SONGS:' )
+    print( all_songs )
+if len(duplicated_songs) > 0:
+    print( 'DUPLICATED SONGS:' )
+    print( duplicated_songs )
