3586c5c86be0d20dfce180b3dff9763e7b3e4c41
Nathan Loewen <loewen.nathan@gmail.com>
2020-09-27 00:33:53 -0500

    Rework po generation a bit. This removes the dependency on itstool.

--- Readme.md
+++ Readme.md
@@ -39,7 +39,7 @@ Install compile-time dependencies on Ubuntu 16.04 (and hopefully later) with:
 ```bash
 apt-get install build-essential automake libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev \
 libgl1-mesa-dev libxml2-dev libfreetype6-dev libpng-dev libopenal-dev \
-libvorbis-dev binutils-dev libzip-dev libiberty-dev autopoint intltool libfontconfig-dev itstool autoconf-archive
+libvorbis-dev binutils-dev libzip-dev libiberty-dev autopoint intltool libfontconfig-dev autoconf-archive
 ```
 
 ### Other \*nix 
--- utils/update-po.sh
+++ utils/update-po.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/bash
 
-if [[ ! -f "naev.6" ]]; then
-   echo "Please run from Naev root directory."
+ROOT=${1-$(pwd)}
+TARGET=${2-"$ROOT/po/POTFILES.in"}
+
+if [[ ! -f "$ROOT/naev.6" ]]; then
+   echo "Please run from Naev root directory, or run with update-po.sh [source_root]"
    exit -1
 fi
+
+cd $ROOT
 
 set -x
 
@@ -12,10 +17,14 @@ TMPFILE=$(mktemp)
 echo "src/log.h" > "$TMPFILE"
 find src/ -name "*.c" | sort >> "$TMPFILE"
 find dat/ -name "*.lua" | sort >> "$TMPFILE"
-echo "po/xml.pot" >> "$TMPFILE"
+find dat/ -name "*.xml" -maxdepth 1 | sort >> "$TMPFILE"
+find dat/assets -name "*.xml" | sort >> "$TMPFILE"
+find dat/outfits -name "*.xml" | sort >> "$TMPFILE"
+find dat/ships -name "*.xml" | sort >> "$TMPFILE"
+find dat/ssys -name "*.xml" | sort >> "$TMPFILE"
 
 # Remove file if found
 sed -i '/src\/shaders.gen.c/d' "$TMPFILE"
 
-cat "$TMPFILE" | sort > po/POTFILES.in
+cat "$TMPFILE" | sort > $TARGET
 
