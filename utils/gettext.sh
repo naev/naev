ba21f081d96abbddbc023de44fbeb35d84f29c48
Edgar Simo-Serra <ess@waseda.jp>
2020-10-02 12:18:06 +0900

    should fix windows borking out because of funky bash

--- utils/gettext.sh
+++ utils/gettext.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-while read lang; do
+linguas=`cat ./dat/LANGUAGES`
+for lang in $linguas; do
+   lang=${lang:0:2}
    path="./dat/gettext/${lang}/LC_MESSAGES/"
    mkdir -p "${path}"
    cp "po/${lang}.gmo" "${path}naev.mo"
-done < ./dat/LANGUAGES
+done
