c2a5c31f880ee4fa60dfc3af0b950f05f9bc7433
Edgar Simo-Serra <ess@waseda.jp>
2020-10-02 12:00:48 +0900

    define stuff

--- src/naev.c
+++ src/naev.c
@@ -298,14 +298,12 @@ int main( int argc, char** argv )
       lang = conf.language;
 #if defined HAVE_DECL_SETENV
    setenv( "LANGUAGE", lang, 1 );
-#else /* defined HAVE_DECL_SETENV */
-#if defined HAVE_DECL__PUTENV_S
+#elif defined HAVE_DECL__PUTENV_S
    _putenv_s( "LANGUAGE", lang );
-#else /* defined HAVE_DECL__PUTENV_S */
+#else
    nsprintf( langbuf, sizeof(langbuf), "LANGUAGE=%s", lang );
    putenv( langbuf );
-#endif /* defined HAVE_DECL__PUTENV_S */
-#endif /* defined HAVE_DECL_SETENV */
+#endif
    /*
    if (setlocale( LC_ALL, lang )==NULL)
       WARN(_("Unable to set the locale to '%s'!"), lang );
