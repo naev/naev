27b06cd50210ee2b680e80b080eafb34b1326c7f
Edgar Simo-Serra <ess@waseda.jp>
2020-10-04 17:00:22 +0900

    Revert "Rework po generation a bit. This removes the dependency on itstool."
    This reverts commit e7181bf03304459693a8eac1a8f514de2448010a.

--- utils/update-po.sh
+++ utils/update-po.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/bash
 
-ROOT=${1-$(pwd)}
-TARGET=${2-"$ROOT/po/POTFILES.in"}
-
-if [[ ! -f "$ROOT/naev.6" ]]; then
-   echo "Please run from Naev root directory, or run with update-po.sh [source_root]"
+if [[ ! -f "naev.6" ]]; then
+   echo "Please run from Naev root directory."
    exit -1
 fi
-
-cd $ROOT
 
 set -x
 
@@ -17,14 +12,10 @@ TMPFILE=$(mktemp)
 echo "src/log.h" > "$TMPFILE"
 find src/ -name "*.c" | sort >> "$TMPFILE"
 find dat/ -name "*.lua" | sort >> "$TMPFILE"
-find dat/ -maxdepth 1 -name "*.xml" | sort >> "$TMPFILE"
-find dat/assets -name "*.xml" | sort >> "$TMPFILE"
-find dat/outfits -name "*.xml" | sort >> "$TMPFILE"
-find dat/ships -name "*.xml" | sort >> "$TMPFILE"
-find dat/ssys -name "*.xml" | sort >> "$TMPFILE"
+echo "po/xml.pot" >> "$TMPFILE"
 
 # Remove file if found
 sed -i '/src\/shaders.gen.c/d' "$TMPFILE"
 
-cat "$TMPFILE" | sort > $TARGET
+cat "$TMPFILE" | sort > po/POTFILES.in
 
