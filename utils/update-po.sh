fcaf534673c929f523abab0a2bb0760476ff4e5f
Edgar Simo-Serra <ess@waseda.jp>
2020-10-04 19:05:43 +0900

    Generate po/POTFILES_XMl and read from that for xml files.

--- utils/update-po.sh
+++ utils/update-po.sh
@@ -7,15 +7,21 @@ fi
 
 set -x
 
+# General file
 TMPFILE=$(mktemp)
-
 echo "src/log.h" > "$TMPFILE"
-find src/ -name "*.c" | sort >> "$TMPFILE"
-find dat/ -name "*.lua" | sort >> "$TMPFILE"
+find src/ -name "*.c" >> "$TMPFILE"
+find dat/ -name "*.lua" >> "$TMPFILE"
 echo "po/xml.pot" >> "$TMPFILE"
-
 # Remove file if found
 sed -i '/src\/shaders.gen.c/d' "$TMPFILE"
-
 cat "$TMPFILE" | sort > po/POTFILES.in
+rm "$TMPFILE" # clean-up
 
+# XML file
+find dat/ -maxdepth 1 -name "*.xml" > "$TMPFILE"
+find dat/assets -name "*.xml" >> "$TMPFILE"
+find dat/outfits -name "*.xml" >> "$TMPFILE"
+find dat/ships -name "*.xml" >> "$TMPFILE"
+cat "$TMPFILE" | sort > po/POTFILES_XML
+rm "$TMPFILE" # clean-up
