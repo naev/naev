73ea890e308ab478ca894adabf5d25d4530e3f58
Nathan Loewen <loewen.nathan@gmail.com>
2020-10-07 23:32:18 -0500

    Make gen_version.sh directory agnostic

--- utils/gen_version.sh
+++ utils/gen_version.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+cd "$MESON_SOURCE_ROOT"
 if [[ -d .git ]]; then
     # In the git repo. Build the tag from git describe
     VERSION=$(git describe --tags --match 'v*' --dirty | sed -E 's/v(.*)-([^-]*)-(g[^-]*)/\1+\2.\3/;s/-dirty/.dirty/')
-    echo $VERSION > dat/VERSION
+    echo $VERSION > "$MESON_SOURCE_ROOT/dat/VERSION"
     echo $VERSION
 else
     # In a source package. Version file should exist.
-    echo dat/VERSION
+    cat "$MESON_SOURCE_ROOT/dat/VERSION"
 fi
