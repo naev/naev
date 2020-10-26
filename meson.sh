1072e55b2c2b3605ba0f0dd6c77eb79fe6ce7349
Jack Greiner <jack@emoss.org>
2020-10-25 23:12:48 -0400

    Add support for passing meson through meson.sh Also added version detection so it will only pass through if compatible.

--- meson.sh
+++ meson.sh
@@ -2,19 +2,37 @@
 
 BUILDDIR="builddir"
 MESONDIR="meson-bin"
-MESON="$MESONDIR/meson.py"
 
-VERSION="0.55.3"
-PACKAGE="meson-$VERSION.tar.gz"
+grab_meson () {
+    VERSION="0.55.3"
+    PACKAGE="meson-$VERSION.tar.gz"
 
-if [ ! -d "$MESONDIR" ]; then
-    mkdir "$MESONDIR"
+    if [ ! -d "$MESONDIR" ]; then
+        mkdir "$MESONDIR"
+    fi
+
+    if [ ! -f "$MESON" ]; then
+        curl -L "https://github.com/mesonbuild/meson/releases/download/$VERSION/$PACKAGE" -o $PACKAGE
+        tar -xf $PACKAGE -C "$MESONDIR" --strip 1
+        rm $PACKAGE
+    fi
+}
+
+if [ ! "meson" ]; then
+    echo "You don't have Meson in PATH, grabbing from online"
+    MESON="$MESONDIR/meson.py"
+    grab_meson
+else
+    currentver="$(meson --version)"
+    requiredver="0.55.0"
+    if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
+            echo "Greater than or equal to ${requiredver}"
+            MESON="meson"
+    else
+            echo "Meson version is lower than ${requiredver}, grabbing from online"
+            MESON="$MESONDIR/meson.py"
+            grab_meson
+    fi
 fi
 
-if [ ! -f "$MESON" ]; then
-    curl -L "https://github.com/mesonbuild/meson/releases/download/$VERSION/$PACKAGE" -o $PACKAGE
-    tar -xf $PACKAGE -C "$MESONDIR" --strip 1
-    rm $PACKAGE
-fi
-
-./meson-bin/meson.py $@
+./$MESON $@
