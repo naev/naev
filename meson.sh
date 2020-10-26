88b162ddfd9cf14fc925880358a690b301527b96
Jack Greiner <jack@emoss.org>
2020-10-25 23:22:14 -0400

    Fix wording to it makes more sense.

--- meson.sh
+++ meson.sh
@@ -12,24 +12,27 @@ grab_meson () {
     fi
 
     if [ ! -f "$MESON" ]; then
+        echo "grabbing from online"
         curl -L "https://github.com/mesonbuild/meson/releases/download/$VERSION/$PACKAGE" -o $PACKAGE
         tar -xf $PACKAGE -C "$MESONDIR" --strip 1
         rm $PACKAGE
+    else
+        echo "using cached version $VERSION"
     fi
 }
 
 if ! [ -x "$(command -v meson)" ]; then
-    echo "You don't have Meson in PATH, grabbing from online"
+    echo "You don't have Meson in PATH"
     MESON="$MESONDIR/meson.py"
     grab_meson
 else
     currentver="$(meson --version)"
     requiredver="0.55.0"
     if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
-            echo "Greater than or equal to ${requiredver}"
+            echo "Meson version is greater than or equal to ${requiredver}"
             MESON="meson"
     else
-            echo "Meson version is lower than ${requiredver}, grabbing from online"
+            echo "Meson version is lower than ${requiredver}"
             MESON="$MESONDIR/meson.py"
             grab_meson
     fi
