c2e6c6f8fcbbccea9d5a5a4b9d6be9af5f13235f
Jack Greiner <jack@emoss.org>
2020-11-19 13:39:11 -0500

    Fix some variables, and the issue with tar

--- utils/ci/steam/SteamDeploy.sh
+++ utils/ci/steam/SteamDeploy.sh
@@ -15,6 +15,7 @@ set -e
 
 # Defaults
 NIGHTLY="false"
+BETA="false"
 
 while getopts dns:t:o: OPTION "$@"; do
     case $OPTION in
@@ -42,40 +43,38 @@ else
     echo "The VERSION file is missing from $SOURCEROOT."
     exit 1
 fi
-BETA=false
 
 # Get version, negative minors mean betas
 if [ -n $(echo "$VERSION" | grep "beta") ]; then
-    BETA=true
+    BETA="true"
 else
     echo "could not find VERSION file"
     exit 1
 fi
 
 # Make Steam dist path if it does not exist
-mkdir -p "$STEAMPATH/content/{lin64,win64,ndata}"
+mkdir -p "$STEAMPATH"/content/{lin64,win64,ndata}
 
 # Move Depot Scripts to Steam staging area
 cp -r "$SOURCEROOT/utils/ci/steam/scripts" "$STEAMPATH"
 
 # Move all build artefacts to deployment locations
 # Move Linux binary and set as executable
-mv "$TEMPPATH/naev-steamruntime/naev.x64" "$STEAMPATH/content/lin64/naev.x64"
+cp "$TEMPPATH/naev-steamruntime/naev.x64" "$STEAMPATH/content/lin64"
 chmod +x "$STEAMPATH/content/lin64/naev.x64"
           
 # Move macOS bundle to deployment location (Bye Bye for now)
 # unzip "$TEMPPATH/naev-macOS/naev-macos.zip" -d "$STEAMPATH/content/macos/"
 
 # Unzip Windows binary and DLLs and move to deployment location
-tar -Jxvf "$TEMPPATH/naev-win64/steam-win64.tar.xz" "$STEAMPATH/content/win64/"
+tar -Jxvf "$TEMPPATH/naev-win64/steam-win64.tar.xz" -C "$STEAMPATH/content/win64"
 mv "$STEAMPATH"/content/win64/naev*.exe "$STEAMPATH/content/win64/naev.exe"
 
 # Move data to deployment location
 cp -r "$SOURCEROOT/dat" "$STEAMPATH/content/ndata"
 
 tree "$STEAMPATH"
-#
-#
+
 ## Runs STEAMCMD, and builds the app as well as all needed depots.
 #
 ## Trigger 2FA request and get 2FA code
@@ -84,20 +83,20 @@ tree "$STEAMPATH"
 ## Wait a bit for the email to arrive
 #sleep 60s
 #python3 "$SOURCEROOT/utils/ci/steam/2fa/get_2fa.py"
-#STEAMCMD_TFA="$(<"$SOURCEROOT/extras/steam/2fa/2fa.txt")"
+#STEAMCMD_TFA="$(cat "$SOURCEROOT/extras/steam/2fa/2fa.txt")"
 #
-#if [[ "$NIGHTLY" == "true" ]]; then
+#if [ "$NIGHTLY" == "true" ]; then
 #    # Run steam upload with 2fa key
 #    steamcmd +login $STEAMCMD_USER $STEAMCMD_PASS $STEAMCMD_TFA +run_app_build_http "$STEAMPATH/scripts/app_build_598530_nightly.vdf" +quit
 #else
-#    if [[ $BETA == true ]]; then 
+#    if [ "$BETA" == "true" ]; then 
 #        # Run steam upload with 2fa key
 #        steamcmd +login $STEAMCMD_USER $STEAMCMD_PASS $STEAMCMD_TFA +run_app_build_http "$STEAMPATH/scripts/app_build_598530_prerelease.vdf" +quit
 #
-#    elif [[ $BETA == false ]]; then 
+#    elif [ "$BETA" == "false" ]; then 
 #        # Move soundtrack stuff to deployment area
-#        mv naev-soundtrack/soundtrack/*.mp3 "$STEAMPATH/content/soundtrack"
-#        mv naev-soundtrack/soundtrack/*.png "$STEAMPATH/content/soundtrack"
+#        cp naev-soundtrack/soundtrack/*.mp3 "$STEAMPATH/content/soundtrack"
+#        cp naev-soundtrack/soundtrack/*.png "$STEAMPATH/content/soundtrack"
 #
 #        # Run steam upload with 2fa key
 #        steamcmd +login $STEAMCMD_USER $STEAMCMD_PASS $STEAMCMD_TFA +run_app_build_http "$STEAMPATH/scripts/app_build_598530_release.vdf" +quit
@@ -107,3 +106,4 @@ tree "$STEAMPATH"
 #        echo "Something went wrong determining if this is a beta or not."
 #    fi
 #fi
+#
