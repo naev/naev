be5044bc3d0cbfa12d4248301a7fda3687ef66cf
Jack Greiner <jack@emoss.org>
2020-11-20 00:35:37 -0500

    Remove verbose extraction for the sake of log reading..

--- utils/ci/steam/SteamDeploy.sh
+++ utils/ci/steam/SteamDeploy.sh
@@ -77,11 +77,11 @@ chmod +x "$STEAMPATH"/content/lin64/naev.x64
 # unzip "$TEMPPATH/naev-macOS/naev-macos.zip" -d "$STEAMPATH/content/macos/"
 
 # Unzip Windows binary and DLLs and move to deployment location
-tar -Jxvf "$TEMPPATH/naev-win64/steam-win64.tar.xz" -C "$STEAMPATH/content/win64"
+tar -Jxf "$TEMPPATH/naev-win64/steam-win64.tar.xz" -C "$STEAMPATH/content/win64"
 mv "$STEAMPATH"/content/win64/naev*.exe "$STEAMPATH/content/win64/naev.exe"
 
 # Move data to deployment location
-tar -Jxvf "$TEMPPATH/naev-ndata/steam-ndata.tar.xz" -C "$STEAMPATH/content/ndata"
+tar -Jxf "$TEMPPATH/naev-ndata/steam-ndata.tar.xz" -C "$STEAMPATH/content/ndata"
 
 # Runs STEAMCMD, and builds the app as well as all needed depots.
 
