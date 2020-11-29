c00ec184534a87fa141149be9cad7036a2fe7aa2
Layla Marchant <diligentcircle@riseup.net>
2020-11-29 14:30:21 -0500

    Revert "Added quicktest.sh, made hrc.sh executable."
    This reverts commit a0c81649e7290e3eab09c2313b13cf1f38791bdf.

--- quicktest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Very quickly compile with meson, launch for testing, and return. For
-# use with git bisect. (Should be used only for Naev specifically.)
-
-cd builddir
-meson compile
-cd ..
-./builddir/naev
