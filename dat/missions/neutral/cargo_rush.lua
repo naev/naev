428057f45fa93581030512ac37f44220578e2603
Layla Marchant <diligentcircle@riseup.net>
2020-11-29 16:56:30 -0500

    Removed the distance warning. (It's a bit misleading.)

--- dat/missions/neutral/cargo_rush.lua
+++ dat/missions/neutral/cargo_rush.lua
@@ -147,16 +147,6 @@ function accept()
             destplanet:name() ) ) then
          misn.finish()
       end
-   elseif player.jumps() < numjumps then
-      if not tk.yesno( _("Short on fuel"), string.format(
-            _("You will require at least %s to reach %s. Your ship can only make %s, so you will have to refuel at least %s, which will take additional time and may cause you to miss the deadline. Accept the mission anyway?"),
-            jumpstring(numjumps), destplanet:name(), jumpstring(player.jumps()),
-            -- Note: math.ceil() - 1 because for example 6/3=2, but that
-            -- only requires 1 refuel (i.e. 2 trips). math.floor() would
-            -- only catch this for non-integer results.
-            timestring(math.ceil(numjumps / player.jumps()) - 1) ) ) then
-         misn.finish()
-      end
    elseif system.cur():jumpDist(destsys, false, true) == nil
          or system.cur():jumpDist(destsys, false, true) < numjumps then
       if not tk.yesno( _("Unknown route"), string.format(
