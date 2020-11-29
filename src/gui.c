55ab117f4f5cbcd26500cf82c19b78077149d625
Layla Marchant <diligentcircle@riseup.net>
2020-11-29 09:32:16 -0500

    Slightly better outlines

--- src/gui.c
+++ src/gui.c
@@ -1483,11 +1483,12 @@ void gui_renderPlayer( double res, int overlay )
 
    /* Render the cross. */
    // gl_renderCross( x, y, r, &cRadar_player );
-   glLineWidth( 4.5 );
-   gl_renderTriangleEmpty( x, y, player.p->solid->dir, r, 2., &cBlack );
-   glLineWidth( 3. );
+   gl_renderTriangleEmpty( x - 1, y, player.p->solid->dir, r, 2., &cBlack );
+   gl_renderTriangleEmpty( x + 1, y, player.p->solid->dir, r, 2., &cBlack );
+   gl_renderTriangleEmpty( x, y - 1, player.p->solid->dir, r, 2., &cBlack );
+   gl_renderTriangleEmpty( x, y + 1, player.p->solid->dir, r, 2., &cBlack );
+
    gl_renderTriangleEmpty( x, y, player.p->solid->dir, r, 2., &cRadar_player );
-   glLineWidth( 1. );
 }
 
 
@@ -1667,10 +1668,15 @@ void gui_renderPlanet( int ind, RadarShape shape, double w, double h, double res
    glDrawArrays( GL_LINE_STRIP, 0, 5 );
    gl_endSolidProgram();
    */
-   glLineWidth(4.5);
-   gl_drawCircle( cx, cy, vr/2.5, &cBlack, 0 );
-   gl_renderCross( cx, cy, vr/2.5, &cBlack );
-   glLineWidth(3.);
+   gl_drawCircle( cx - 1, cy, vr/2.5, &cBlack, 0 );
+   gl_renderCross( cx - 1, cy, vr/2.5, &cBlack );
+   gl_drawCircle( cx + 1, cy, vr/2.5, &cBlack, 0 );
+   gl_renderCross( cx + 1, cy, vr/2.5, &cBlack );
+   gl_drawCircle( cx, cy - 1, vr/2.5, &cBlack, 0 );
+   gl_renderCross( cx, cy - 1, vr/2.5, &cBlack );
+   gl_drawCircle( cx, cy + 1, vr/2.5, &cBlack, 0 );
+   gl_renderCross( cx, cy + 1, vr/2.5, &cBlack );
+
    gl_drawCircle( cx, cy, vr/2.5, &col, 0 );
    gl_renderCross( cx, cy, vr/2.5, &col );
    glLineWidth(1.);
@@ -1768,11 +1774,12 @@ void gui_renderJumpPoint( int ind, RadarShape shape, double w, double h, double 
    if (!overlay)
       col.a = 1.-interference_alpha;
 
-   glLineWidth( 3.5 );
-   gl_renderTriangleEmpty( cx, cy, -jp->angle, vr, 2., &cBlack );
-   glLineWidth( 2. );
+   gl_renderTriangleEmpty( cx - 1, cy, -jp->angle, vr, 2., &cBlack );
+   gl_renderTriangleEmpty( cx + 1, cy, -jp->angle, vr, 2., &cBlack );
+   gl_renderTriangleEmpty( cx, cy - 1, -jp->angle, vr, 2., &cBlack );
+   gl_renderTriangleEmpty( cx, cy + 1, -jp->angle, vr, 2., &cBlack );
+
    gl_renderTriangleEmpty( cx, cy, -jp->angle, vr, 2., &col );
-   glLineWidth( 1. );
 
    /* Render name. */
    /* XXX: Hack to prevent the text from overly obscuring overlay
