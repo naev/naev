9433885d748113b33bea92b47f7070bce09a779e
Justin Blanchard <UncombedCoconut@gmail.com>
2020-12-05 11:16:09 -0500

    Fix the working translation of the formerly %ld string. Fix update-po and Autotools in-tree conspiring to screw up POTFILES*.in.

--- utils/update-po.sh
+++ utils/update-po.sh
@@ -13,7 +13,7 @@ set -x
 
 # General file
 TMPFILE=$(mktemp)
-find src/ -name "*.[ch]" -not \( -name "glue_macos.h" -or -name "khrplatform.h" -or -name "shaders*gen*" \) >> "$TMPFILE"
+find src/ -name "*.[ch]" -not \( -name config.h -or -name glue_macos.h -or -name khrplatform.h -or -name "shaders*gen*" \) >> "$TMPFILE"
 find dat/ -name "*.lua" >> "$TMPFILE"
 
 cat "$TMPFILE" | LC_ALL=C sort | tee "$ROOT/po/POTFILES_COMBINED.in" > "$ROOT/po/POTFILES.in"
