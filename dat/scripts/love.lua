8a152007f286da47b6ed31e9fcf8c7a8e997b2ff
Edgar Simo-Serra <ess@waseda.jp>
2020-12-16 21:29:21 +0900

    More misc love functionality.

--- dat/scripts/love.lua
+++ dat/scripts/love.lua
@@ -128,14 +128,16 @@ function love.window.setMode( width, height, flags )
    end
    return true
 end
+function love.window.getDisplayCount() return 1 end
+function love.window.getDisplayName( displayindex ) return "Naev" end
 function love.window.getDesktopDimensions() return naev.gfx.dim() end
+function love.window.getDimensions() return love.w, love.h end
+function love.window.getWidth() return love.w end
+function love.window.getHeight() return love.h end
 function love.window.getDPIScale() return 1 end -- TODO return scaling factor?
 function love.window.getMode()
    return love.w, love.h, { fullscreen=love.fullscreen, vsync=1, resizeable=false, borderless = false, centered=true, display=1, msaa=0 }
 end
-function love.window.getDimensions() return love.w, love.h end
-function love.window.getWidth() return love.w end
-function love.window.getHeight() return love.h end
 function love.window.setFullscreen( fullscreen )
    -- Skip unnecessary changing
    if (fullscreen and love.fullscreen) or (not fullscreen and not love.fullscreen) then return true end
@@ -341,6 +343,7 @@ end
 -- Math
 --]]
 local prng = require 'prng'
+local function clamp01(x) return math.min(math.max(x, 0), 1) end
 love.math = {}
 love.math.RandomGenerator = inheritsFrom( love.Object )
 love.math.RandomGenerator._type = "RandomGenerator"
@@ -382,6 +385,26 @@ function love.math.random( min, max )
    else
       return naev.rnd.rnd( min, max )
    end
+end
+function love.math.colorToBytes(r, g, b, a)
+	if type(r) == "table" then
+		r, g, b, a = r[1], r[2], r[3], r[4]
+	end
+	r = floor(clamp01(r) * 255 + 0.5)
+	g = floor(clamp01(g) * 255 + 0.5)
+	b = floor(clamp01(b) * 255 + 0.5)
+	a = a ~= nil and floor(clamp01(a) * 255 + 0.5) or nil
+	return r, g, b, a
+end
+function love.math.colorFromBytes(r, g, b, a)
+	if type(r) == "table" then
+		r, g, b, a = r[1], r[2], r[3], r[4]
+	end
+	r = clamp01(floor(r + 0.5) / 255)
+	g = clamp01(floor(g + 0.5) / 255)
+	b = clamp01(floor(b + 0.5) / 255)
+	a = a ~= nil and clamp01(floor(a + 0.5) / 255) or nil
+	return r, g, b, a
 end
 
 
