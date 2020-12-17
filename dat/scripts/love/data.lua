57b924210cf3044a5be6ae0226938cb0c82962ef
Edgar Simo-Serra <ess@waseda.jp>
2020-12-17 15:47:13 +0900

    Create data.Data and have FontData and ImageData inherit from it.

--- dat/scripts/love/data.lua
+++ dat/scripts/love/data.lua
@@ -1,6 +1,19 @@
 --[[
 -- Data
 --]]
+local class = require 'class'
+local object = require 'love.object'
+
 local data = {}
+
+data.Data = class.inheritsFrom( object.Object )
+data.Data._type = "Data"
+
+function data.Data:getSize()
+   return self.d:getSize()
+end
+function data.Data:getString()
+   return self.d:getString()
+end
 
 return data
--- dat/scripts/love/font.lua
+++ dat/scripts/love/font.lua
@@ -1,6 +1,12 @@
 --[[
 -- Font
 --]]
+local class = require 'class'
+local data = require 'love.data'
+
 local font = {}
+
+font.Font = class.inheritsFrom( data.Data )
+font.Font._type = "Font"
 
 return font
