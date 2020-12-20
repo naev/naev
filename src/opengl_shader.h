e9bb062697175a229fd60084b65ad23a97fc9137
Edgar Simo-Serra <ess@waseda.jp>
2020-12-20 17:25:51 +0900

    Include opengl.h in opengl_shader.h

--- src/opengl_shader.h
+++ src/opengl_shader.h
@@ -5,10 +5,14 @@
 #ifndef OPENGL_SHADER_H
 # define OPENGL_SHADER_H
 
+
+#include "opengl.h"
+
 GLuint gl_shader_read(GLuint type, const char *filename);
 int gl_program_link(GLuint program);
 int gl_program_vert_frag(const char *vert, const char *frag);
 void gl_uniformColor(GLint location, const glColour *c);
 void gl_uniformAColor(GLint location, const glColour *c, float a);
+
 
 #endif /* OPENGL_SHADER_H */
