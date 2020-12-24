d2074cfcccc8391a1469ed21b69cc7610a7f08ec
Edgar Simo-Serra <ess@waseda.jp>
2020-12-24 17:09:41 +0900

    Comment

--- src/nlua_data.h
+++ src/nlua_data.h
@@ -22,7 +22,7 @@ typedef enum LuaDataType_e {
  * @brief Wrapper to datas.
  */
 typedef struct LuaData_s {
-   size_t size; /**< Size of buffer. */
+   size_t size; /**< Size of buffer (already multiplied by elem). */
    size_t elem; /**< Size of an element. */
    char *data; /**< Actually allocated data. */
    LuaDataType_t type; /**< Type of the data. */
