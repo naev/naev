3002bc61f001040336c50d2c8d5c42851fb506d2
Edgar Simo-Serra <ess@waseda.jp>
2020-12-28 11:14:42 +0900

    setting up some stuff

--- dat/events/minerva/altercation1.lua
+++ dat/events/minerva/altercation1.lua
@@ -43,5 +43,60 @@ end
 
 -- Scuffle between Za'lek and Dvaered
 function takeoff ()
+   -- Set up system
+   pilot.clear()
+   pilot.toggleSpawn(false)
+
+   -- Create the ships
+   local pos = planet.get("Minerva Station"):pos()
+   local zlpos = pos + vec2.newP( 700, 120 )
+   local dvpos = pos + vec2.newP( 500, 25 )
+   zl = pilot.addRaw("Za'lek Sting", "zalek", pos, "Za'lek" )
+   dv = pilot.add("Dvaered Phalanx", "dvaered", pos, "Dvaered" )
+   zl:rename( _("Za'lek Belligerent") )
+   zl:control()
+   zl:attack( dv )
+   zl:setNoDisable( true )
+   dv:rename( _("Dvaered Hooligan") )
+   dv:control()
+   dv:attack( zl )
+   dv:setNoDisable( true )
+
+   -- Prepare hooks
+   angrytimer = hook.timer( 3000, "angrypeople" )
+   hook.pilot( zl, "death", "zl_dead" )
+   hook.pilot( dv, "death", "dv_dead" )
+
+   -- Set up hooks when it is over
+   hook.jumpout("leave")
+   hook.land("leave")
+end
+
+function angrypeople ()
+   -- Take turns yelling
+   if not zl_yelling then
+      zl_yelling = true
+      local msg = "asshole"
+      dv:broadcast( msg )
+   else
+      zl_yelling = false
+      local msg = "asshole"
+      zl:broadcast( msg )
+   end
+   angrytimer = hook.timer( 2000, "angrypeople" )
+end
+
+function zl_dead ()
+   hook.rm( angrytimer )
+   dv:broadcast( "yay" )
+end
+
+function dv_dead ()
+   hook.rm( angrytimer )
+   zl:broadcast( "yay" )
+end
+
+function leave () --event ends on player leaving the system or landing
+   evt.finish(true)
 end
 
