76b0464f7e906dc8ec95bb3e8ba0e5e4eed40002
Edgar Simo-Serra <ess@waseda.jp>
2020-12-29 12:31:58 +0900

    Starting to set up next missions.

---
+++ dat/missions/minerva/maikki1.lua
@@ -0,0 +1,61 @@
+--[[
+<?xml version='1.0' encoding='utf8'?>
+<mission name="Maikki's Father 1">
+  <flags>
+   <unique />
+  </flags>
+  <avail>
+   <priority>4</priority>
+   <chance>100</chance>
+   <location>Bar</location>
+   <planet>Minerva Station</planet>
+   <cond>player.evtDone("Minerva Station Altercation 1")</cond>
+  </avail>
+ </mission>
+--]]
+
+--[[
+-- Maikki (Maisie McPherson) asks you to find her father, the famous pilot Kex
+-- McPherson. She heard rumours he was still alive and at Minerva station.
+--
+-- 1. Player is sent to Doeston to try to find the whereabouts.
+-- 2. Guys in the bar talk about how he never came back from the Nebula,
+-- 3. Player sent to explore Arandon, finds some scavengers, they run away.
+-- 4. Player goes back to bar, comment how some have been trying to sell some ship scrap, apparently from Zerantix. Player told to follow scavengers.
+-- 5. Scavengers show up and talk about sensors not working well, talk through broadcast. Player has to follow without getting too close (maybe needs outfits to improve sensor range in nebula?)
+-- 6. They lead the player to some debris and start looting. Player gets to talk to them and convince to leave, or kill them. Learns that Za'lek have been buying the goods.
+-- 7. Player finds a picture among the debris.
+-- 8. Return to Maikki
+--]]
+local vn = require 'vn'
+
+npc_name = _("Distraught Young Woman")
+npc_portrait = "none"
+npc_description = _("yeah")
+
+misn_title = _("Finding Father")
+misn_reward = _("???")
+misn_desc = _("Maikki wants you to help her find her father.")
+
+function create ()
+   misn.setNPC( npc_name, npc_portrait )
+   misn.setDesc( npc_description )
+end
+
+
+function accept ()
+   vn.clear()
+   vn.scene()
+   local m = vn.newCharacter( npc_name, {} )
+   vn.fadein()
+   m(_("foo"))
+   vn.fadeout()
+   vn.run()
+
+   if true then
+      misn.finish(false)
+   end
+
+   misn.accept()
+end
+
---
+++ dat/missions/minerva/maikki2.lua
@@ -0,0 +1,62 @@
+--[[
+<?xml version='1.0' encoding='utf8'?>
+<mission name="Maikki's Father 2">
+  <flags>
+   <unique />
+  </flags>
+  <avail>
+   <priority>4</priority>
+   <chance>100</chance>
+   <location>Bar</location>
+   <planet>Minerva Station</planet>
+   <done>Maikki's Father 1</done>
+  </avail>
+ </mission>
+--]]
+
+--[[
+-- Maikki (Maisie McPherson) asks you to find her father, the famous pilot Kex
+-- McPherson. She heard rumours he was still alive and at Minerva station.
+-- Player found out that Za'lek doing stuff with the ship cargo.
+--
+-- 1. Told to try to find who could have been involved and given three places to look at.
+-- Hint 1: Jorlas in Regas (University)
+-- Hint 2: Cantina Station in Qulam (Trade Hub)
+-- Hint 3: Jurai in Hideyoshi's Star (University)
+-- 2. After talking to all three, the player is told to go to Naga in Damien (Underwater University)
+-- 3. Mentions eccentric colleague at Westhaven
+-- 4. Eccentric colleague makes the player mine some stupid stuff for him.
+-- 5. Tells you he saved him and sent him off to "Minerva Station".
+--
+-- Eccentric Scientist in "Westhaven" (slightly senile with dementia).
+--]]
+local vn = require 'vn'
+
+npc_name = _("Maikki")
+npc_portrait = "none"
+npc_description = _("yeah")
+
+misn_title = _("Finding Father")
+misn_reward = _("???")
+misn_desc = _("Maikki wants you to help her find her father.")
+
+function create ()
+   misn.setNPC( npc_name, npc_portrait )
+   misn.setDesc( npc_description )
+end
+
+
+function accept ()
+   vn.clear()
+   vn.scene()
+   local m = vn.newCharacter( npc_name, {} )
+   vn.fadein()
+   m( "Yeah" )
+   vn.fadeout()
+   vn.run()
+
+   misn.accept()
+end
+
+
+
