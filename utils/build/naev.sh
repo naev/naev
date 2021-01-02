ea1a9504a2e52357d9b8fb309efc5d8935b09610
Justin Blanchard <UncombedCoconut@gmail.com>
2021-01-02 02:22:09 -0500

    Keep naev.sh branches (gdb vs non-gdb) consistent.

--- utils/build/naev.sh
+++ utils/build/naev.sh
@@ -3,5 +3,5 @@ type "gdb" > /dev/null
 if [ "$?" == 0 ]; then
    gdb -x @source_root@/.gdbinit --args @naev_bin@ -d @source_root@/dat -d @source_root@ "$@"
 else
-   @naev_bin@ -d @source_root@/dat "$@"
+   @naev_bin@ -d @source_root@/dat -d @source_root@ "$@"
 fi
