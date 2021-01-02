773239f99b40ab92574829e0f3b29746b0de9bec
Edgar Simo-Serra <ess@waseda.jp>
2021-01-02 12:25:58 +0900

    naev.sh adds source directory also to path

--- utils/build/naev.sh
+++ utils/build/naev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 type "gdb" > /dev/null
 if [ "$?" == 0 ]; then
-   gdb -x @source_root@/.gdbinit --args @naev_bin@ -d @source_root@/dat "$@"
+   gdb -x @source_root@/.gdbinit --args @naev_bin@ -d @source_root@/dat -d @source_root@ "$@"
 else
    @naev_bin@ -d @source_root@/dat "$@"
 fi
