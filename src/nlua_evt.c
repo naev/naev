066baf1ec155dbe2ed8a23a3e7d4fa220e6c29df
Edgar Simo-Serra <ess@waseda.jp>
2021-01-03 17:45:34 +0900

    use luaL_optinteger

--- src/nlua_evt.c
+++ src/nlua_evt.c
@@ -188,10 +188,7 @@ static int evt_npcAdd( lua_State *L )
    desc = luaL_checkstring(L, 4);
 
    /* Optional priority. */
-   if (lua_gettop(L) > 4)
-      priority = luaL_checkint( L, 5 );
-   else
-      priority = 5;
+   priority = luaL_optinteger(L,5,5);
 
    /* Set path. */
    nsnprintf( portrait, PATH_MAX, GFX_PATH"portraits/%s.png", gfx );
