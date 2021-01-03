92007a342b4f3ceb7afff150b3547063a0629f83
Edgar Simo-Serra <ess@waseda.jp>
2021-01-03 16:28:29 +0900

    TODO

--- dat/events/travelling_merchant.lua
+++ dat/events/travelling_merchant.lua
@@ -30,9 +30,9 @@ broadcastmsg = {
    _("...and that's how I was able to get a third liver haha. Oops is this on? Er, nevermind that. Outfits for sale!"),
 }
 
-first_hail_title = trader_name
 first_hail_message = _('"Howdy Human! Er, I mean, Greetings! If you want to take a look at my wonderful, exquisite, propitious, meretricious, effulgent, ... wait, what was I talking about? Oh yes, please come see my wares on my ship. You are welcome to board anytime!"')
 
+-- TODO boarding VN stuff should allow talking to Misi and such.
 board_message = _("You open the airlock and are immediately greeted by an intense humidity and heat, almost like a jungle. As you advance through the dimly lit ship you can see all types of mold and plants crowing in crevices in the wall. Wait, was that a small animal scurrying around? Eventually you reach the cargo hold that has been re-adapted as a sort of bazaar. It is a mess of different wares and most don't seem of much value, there might be some interesting find.")
 
 function create ()
