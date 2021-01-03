5a0a49eeb188f95050c6854697dc6c3194829783
Edgar Simo-Serra <ess@waseda.jp>
2021-01-03 12:17:16 +0900

    Added naev-artwork-production submodule.

--- utils/build/naev.sh
+++ utils/build/naev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 type "gdb" > /dev/null
-NAEV="@naev_bin@ -d @source_root@/dat -d @source_root@"
+NAEV="@naev_bin@ -d @source_root@/dat -d @source_root@/artwork -d @source_root@"
 if [ "$?" == 0 ]; then
    gdb -x @source_root@/.gdbinit --args ${NAEV} "$@"
 else
