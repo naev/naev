83b387c1d31ec7e264dd1f238eb5285747a22d65
Edgar Simo-Serra <ess@waseda.jp>
2021-01-04 19:03:12 +0900

    typo in comments

--- dat/missions/minerva/maikki1.lua
+++ dat/missions/minerva/maikki1.lua
@@ -31,7 +31,7 @@
 --  591ish - maikki is born
 --  593:3726.4663 - the incident
 --  596ish - Kex disappears (maikki is 8ish)
---  603ish - game start (~15 yeasr after incident, maikki is 18ish)
+--  603ish - game start (~15 years after incident, maikki is 18ish)
 --]]
 local minerva = require "minerva"
 local portrait = require 'portrait'
