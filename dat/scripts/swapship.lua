5a66b3b34ee47f8518bce5b282c45839526bb1e6
Valeriy Sosnov <sigus@vivaldi.net>
2021-01-11 10:44:17 +1000

    Fix double ship stealing and wrong slots equipping

--- dat/scripts/swapship.lua
+++ dat/scripts/swapship.lua
@@ -100,9 +100,15 @@ function swapship.swap( template )
    player.swapShip( template:ship() )
    pp = player.pilot() -- Update struct to new pilot
 
+
+   -- Start with an empty ship
+   pp:rmOutfit( "all" )
+   pp:rmOutfit( "cores" )
+
    -- Copy equipment
    for k,v in ipairs( template:outfits() ) do
-      pp:addOutfit( v, 1, true )
+      pp = player.pilot()
+      pp:addOutfit( v, 1, false )
    end
 
    -- Delete pilot
