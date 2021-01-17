c6bc58d587640dfd18e764358361964b986e7461
Edgar Simo-Serra <ess@waseda.jp>
2021-01-17 13:47:22 +0900

    Don't automatically start.

--- dat/events/minerva/chicken_rendezvous.lua
+++ dat/events/minerva/chicken_rendezvous.lua
@@ -1,13 +1,16 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
 <event name="Minerva Station Altercation 1">
- <trigger>land</trigger>
- <chance>100</chance>
- <cond>planet.cur():name()=="Minerva Station" and player.misnDone("Maikki's Father 2")</cond>
+ <trigger>none</trigger>
+ <chance>0</chance>
  <flags>
   <unique />
  </flags>
 </event>
+--]]
+
+--[[
+-- Triggered from station.lua
 --]]
 
 function create()
