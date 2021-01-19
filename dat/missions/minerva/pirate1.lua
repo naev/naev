3f8ff117a4cade4479a6a3e7ef9abdeccb9ba6b9
Edgar Simo-Serra <ess@waseda.jp>
2021-01-19 17:03:16 +0900

    Fixed past tense.

--- dat/missions/minerva/pirate1.lua
+++ dat/missions/minerva/pirate1.lua
@@ -92,7 +92,7 @@ They beam a smile at you.]]),_(runawaysys)))
    end
 
    shiplog.createLog( logidstr, minerva.log.pirate.logname, minerva.log.pirate.logtype, true )
-   shiplog.appendLog( logidstr, _("You accept a job from a shady individual to harass Dvaered thugs and make it seem like the Za'lek were involved.") )
+   shiplog.appendLog( logidstr, _("You accepted a job from a shady individual to harass Dvaered thugs and make it seem like the Za'lek were involved.") )
 
    misn.accept()
    osd = misn.osdCreate( _("Thug Decoy"),
