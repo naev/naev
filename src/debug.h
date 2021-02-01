467b3cd2b025acb76f777baad22ecbdbfef70f44
Justin Blanchard <UncombedCoconut@gmail.com>
2021-02-01 09:07:11 -0500

    Migrate debug_* from naev.c to a dedicated file.

---
+++ src/debug.h
@@ -0,0 +1,15 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+
+
+#ifndef DEBUG_H
+#  define DEBUG_H
+
+
+void debug_sigInit (void);
+void debug_sigClose (void);
+
+
+#endif /* DEBUG_H */
+
