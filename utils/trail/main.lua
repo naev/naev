abace45d39133e197027e59c0a672d4a0301621c
Edgar Simo-Serra <ess@waseda.jp>
2021-02-07 21:54:09 +0900

    Added lightning bolt trail to utils/trail.

--- utils/trail/main.lua
+++ utils/trail/main.lua
@@ -10,6 +10,75 @@ local pixelcode = [[
 uniform float dt;
 uniform int type;
 
+//
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+//
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+1.0)*x);
+}
+float snoise(vec2 v)
+  {
+  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+
+// Other corners
+  vec2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+		+ i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+// Compute final noise value at P
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+
 /* Has a peak at 1/k */
 float impulse( float x, float k )
 {
@@ -31,7 +100,7 @@ float smoothbeam( float x, float k )
 
 float sharpbeam( float x, float k )
 {
-   return 1. - pow( min( cos( M_PI * x / 2. ), 1.0 - abs(x) ), k );
+   return pow( min( cos( M_PI * x / 2. ), 1.0 - abs(x) ), k );
 }
 
 float random (vec2 st) {
@@ -111,6 +180,34 @@ float trail_flame( float t, float y )
    return a;
 }
 
+float trail_arc( float t, float y )
+{
+   float a, m, p, v, s;
+	vec2 ncoord;
+
+   // Modulate alpha base on length
+   a = fastdropoff( t, 1. );
+
+   // Modulate alpha based on dispersion
+   //m = 0.5 + 0.5*impulse( 1.-t, 1. );
+   m = 0.5 + 0.5*impulse( 1.-t, 1. );
+	m *= 3;
+
+   // Modulate width
+	ncoord = vec2( 20*t, 7*dt );
+	s =  0.6 * smoothstep(0, 0.2, 1-t);
+	p = y + s * snoise( ncoord );
+	v = sharpbeam( p, m );
+	p = y + s * snoise( 1.5*ncoord );
+	v += sharpbeam( p, 2*m );
+	p = y + s * snoise( 2*ncoord );
+	v += sharpbeam( p, 4*m );
+
+	a *= v * 0.6;
+
+   return min(1, a);
+}
+
 float trail_nebula( float t, float y )
 {
    float a, m;
@@ -123,7 +220,7 @@ float trail_nebula( float t, float y )
 
    // Modulate width
    m *= 2-smoothstep( 0., 0.2, 1.-t );
-   a *= 1-sharpbeam( y, 3*m );
+   a *= sharpbeam( y, 3*m );
    a *= 0.2 + 0.8*smoothstep( 0., 0.05, 1.-t );
 
    return a;
@@ -146,6 +243,8 @@ vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
       a = trail_flame( t, pos.y );
    else if (type==4)
       a = trail_nebula( t, pos.y );
+   else if (type==5)
+      a = trail_arc( t, pos.y );
    else
       a = trail_default( t, pos.y );
 
@@ -154,7 +253,7 @@ vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
 	return color_out;
 }
 ]]
- 
+
 local vertexcode = [[
 #pragma language glsl3
 
@@ -166,7 +265,7 @@ vec4 position( mat4 transform_projection, vec4 vertex_position )
 	return transform_projection * vertex_position;
 }
 ]]
- 
+
 
 function love.load()
    love.window.setTitle( "Naev Trail Demo" )
