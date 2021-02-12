b5eafc7b62013effdaaafde652e18a5d7903b229
Edgar Simo-Serra <ess@waseda.jp>
2021-02-12 20:38:50 +0900

    Added more math constants and good random function to math.glsl.

--- dat/glsl/lib/math.glsl
+++ dat/glsl/lib/math.glsl
@@ -1,7 +1,21 @@
 #ifndef _MATH_H
 #define _MATH_H
 
-const float M_PI = 3.141592502593994140625;
+/* Taken from /usr/include/math.h */
+const float M_E         = 2.7182818284590452354;   /* e */
+const float M_LOG2E     = 1.4426950408889634074;   /* log_2 e */
+const float M_LOG10E    = 0.43429448190325182765;  /* log_10 e */
+const float M_LN2       = 0.69314718055994530942;  /* log_e 2 */
+const float M_LN10      = 2.30258509299404568402;  /* log_e 10 */
+const float M_PI        = 3.14159265358979323846;  /* pi */
+const float M_PI_2      = 1.57079632679489661923;  /* pi/2 */
+const float M_PI_4      = 0.78539816339744830962;  /* pi/4 */
+const float M_1_PI      = 0.31830988618379067154;  /* 1/pi */
+const float M_2_PI      = 0.63661977236758134308;  /* 2/pi */
+const float M_2_SQRTPI  = 1.12837916709551257390;  /* 2/sqrt(pi) */
+const float M_SQRT2     = 1.41421356237309504880;  /* sqrt(2) */
+const float M_SQRT1_2   = 0.70710678118654752440;  /* 1/sqrt(2) */
+#
 
 // Modulo 289 without a division (only multiplications)
 vec4 mod289(vec4 x) {
@@ -37,6 +51,18 @@ vec3 fade(vec3 t) {
 }
 vec2 fade(vec2 t) {
   return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// http://byteblacksmith.com/improvements-to-the-canonical-one-liner-glsl-rand-for-opengl-es-2-0/
+float random(vec2 co)
+{
+   const highp float seed = 12.9898;
+   highp float a = seed;
+   highp float b = 78.233;
+   highp float c = 43758.5453;
+   highp float dt= dot(co.xy ,vec2(a,b));
+   highp float sn= mod(dt,3.14);
+   return fract(sin(sn) * c);
 }
 
 #endif /* _MATH_H */
