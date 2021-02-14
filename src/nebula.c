dd3ab5cebbb67590e61f8a811a43a7d7e666583e
Justin Blanchard <UncombedCoconut@gmail.com>
2021-02-14 00:57:56 -0500

    Revert: Doubled nebula visibility (shader). As far as I can tell, current code is rendering everything to scale and the code at the time wasn't (causing the radius to be way too small without the doubling).

--- src/nebula.c
+++ src/nebula.c
@@ -273,7 +273,7 @@ void nebu_update( double dt )
       mod = player.p->ew_detect;
 
    /* At density 1000 you have zero visibility. */
-   nebu_view = (1000. - nebu_density) * mod * 2;
+   nebu_view = (1000. - nebu_density) * mod;
 }
 
 
