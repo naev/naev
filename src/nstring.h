7e8fed15b465171c46944bdfe5efcee91b56f71b
Justin Blanchard <UncombedCoconut@gmail.com>
2021-02-15 15:47:44 -0500

    Require a C99-conformant snprintf().

--- src/nstring.h
+++ src/nstring.h
@@ -38,11 +38,7 @@ const char *nstrcasestr( const char *haystack, const char *needle );
 char* nstrndup( const char *s, size_t n );
 #endif /* HAS_POSIX && defined(_GNU_SOURCE) */
 
-#if HAS_SNPRINTF
 #define nsnprintf       snprintf
-#else /* HAS_SNPRINTF */
-PRINTF_FORMAT( 3, 4 ) int nsnprintf( char *text, size_t maxlen, const char *fmt, ... );
-#endif /* HAS_SNPRINTF */
 
 int strsort( const void *p1, const void *p2 );
 
