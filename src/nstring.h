a1b7e51c8109c3ab172eec88360ed576fbc5b9e4
Justin Blanchard <UncombedCoconut@gmail.com>
2021-02-15 17:21:23 -0500

    Accept that PRINTF_FORMAT(i, j) need not imply NONNULL(i).

--- src/nstring.h
+++ src/nstring.h
@@ -25,9 +25,8 @@
 /**
  * @brief A function which passes on a format string to vsnprintf or the like should have PRINTF_FORMAT( i, j )
  *        in front of its prototype, where i is the 1-based index of the format-string argument and j is the
- *        1-based index where the formatted items (if any) start.
- * @todo We should declare the fmt argument nonnull, but we have to rip out some null checks first. */
-#define PRINTF_FORMAT( i, j ) FORMAT( BUILTIN_PRINTF_FORMAT, i, j) /*NONNULL( i )*/
+ *        1-based index where the formatted items (if any) start. */
+#define PRINTF_FORMAT( i, j ) FORMAT( BUILTIN_PRINTF_FORMAT, i, j)
 
 const char *nstrnstr( const char *haystack, const char *needle, size_t size );
 #if HAS_POSIX && defined(_GNU_SOURCE)
