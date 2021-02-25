99b01306d38f6590ad12fcc7af24458d483463d2
Edgar Simo-Serra <ess@waseda.jp>
2021-02-25 14:31:43 +0900

    paper shader should compile now.

--- dat/scripts/love_shaders.lua
+++ dat/scripts/love_shaders.lua
@@ -29,14 +29,14 @@ function love_shaders.paper()
 
 uniform float u_r;
 
-vec4 effect( vec4 color, Image tex, vec2 uv, vec2 screen_coords )
+vec4 effect( vec4 color, Image tex, vec2 uv, vec2 px )
 {
    vec4 texcolor = color * texture2D( tex, uv );
 
    float n = 0.0;
    for (float i=1.0; i<8.0; i=i+1.0) {
       float m = pow( 2.0, i );
-      n += snoise( st * m + 1000.0 * u_r ) * (1.0 / m);
+      n += snoise( px * m + 1000.0 * u_r ) * (1.0 / m);
    }
 
    texcolor.rgb *= 0.68 + 0.3 * n;
