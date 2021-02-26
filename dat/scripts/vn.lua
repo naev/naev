397bff45701d23261c379eb980d3bd0eb44ec668
Edgar Simo-Serra <ess@waseda.jp>
2021-02-26 13:49:17 +0900

    [VN] More transitions, can't have enough, eh?

--- dat/scripts/vn.lua
+++ dat/scripts/vn.lua
@@ -1112,6 +1112,108 @@ vec4 effect( vec4 color, Image tex, vec2 uv, vec2 screen_coords )
          );
 }
 ]]
+   elseif name=="pixelate" then
+      _pixelcode = [[
+// Adapted from https://gl-transitions.com/editor/pixelize
+// Author: gre
+// License: MIT
+// forked from https://gist.github.com/benraziel/c528607361d90a072e98
+
+#include "lib/math.glsl"
+
+uniform Image texprev;
+uniform float progress;
+
+const ivec2 squaresMin = ivec2(20);
+const int steps = 50;
+
+vec4 effect( vec4 color, Image tex, vec2 uv, vec2 screen_coords ) {
+   float d = min(progress, 1.0 - progress);
+   float dist = steps>0 ? ceil(d * float(steps)) / float(steps) : d;
+   vec2 squareSize = 2.0 * dist / vec2(squaresMin);
+   vec2 p = dist>0.0 ? (floor(uv / squareSize) + 0.5) * squareSize : uv;
+   return mix( Texel( texprev, p ), Texel( MainTex, p ), progress);
+}
+]]
+   elseif name=="hexagon" then
+      _pixelcode = [[
+// Author: Fernando Kuteken
+// License: MIT
+// Hexagonal math from: http://www.redblobgames.com/grids/hexagons/
+
+uniform Image texprev;
+uniform float progress;
+
+const int steps = 50;
+uniform float horizontalHexagons = 20;
+float ratio = love_ScreenSize.x / love_ScreenSize.y;
+
+struct Hexagon {
+   float q;
+   float r;
+   float s;
+};
+
+Hexagon createHexagon(float q, float r) {
+   Hexagon hex;
+   hex.q = q;
+   hex.r = r;
+   hex.s = -q - r;
+   return hex;
+}
+
+Hexagon roundHexagon(Hexagon hex) {
+
+   float q = floor(hex.q + 0.5);
+   float r = floor(hex.r + 0.5);
+   float s = floor(hex.s + 0.5);
+
+   float deltaQ = abs(q - hex.q);
+   float deltaR = abs(r - hex.r);
+   float deltaS = abs(s - hex.s);
+
+   if (deltaQ > deltaR && deltaQ > deltaS)
+      q = -r - s;
+   else if (deltaR > deltaS)
+      r = -q - s;
+   else
+      s = -q - r;
+
+   return createHexagon(q, r);
+}
+
+Hexagon hexagonFromPoint(vec2 point, float size) {
+
+   point.y /= ratio;
+   point = (point - 0.5) / size;
+
+   float q = (sqrt(3.0) / 3.0) * point.x + (-1.0 / 3.0) * point.y;
+   float r = 0.0 * point.x + 2.0 / 3.0 * point.y;
+
+   Hexagon hex = createHexagon(q, r);
+   return roundHexagon(hex);
+
+}
+
+vec2 pointFromHexagon(Hexagon hex, float size) {
+
+   float x = (sqrt(3.0) * hex.q + (sqrt(3.0) / 2.0) * hex.r) * size + 0.5;
+   float y = (0.0 * hex.q + (3.0 / 2.0) * hex.r) * size + 0.5;
+
+   return vec2(x, y * ratio);
+}
+
+vec4 effect( vec4 color, Image tex, vec2 uv, vec2 screen_coords ) {
+
+   float dist = 2.0 * min(progress, 1.0 - progress);
+   dist = steps > 0 ? ceil(dist * float(steps)) / float(steps) : dist;
+   float size = (sqrt(3.0) / 3.0) * dist / horizontalHexagons;
+   vec2 point = dist > 0.0 ? pointFromHexagon(hexagonFromPoint(uv, size), size) : uv;
+
+   return mix( Texel( texprev, point ), Texel( MainTex, point ), progress);
+
+}
+]]
    else
       error( string.format(_("vn: unknown transition type'%s'"), name ) )
    end
