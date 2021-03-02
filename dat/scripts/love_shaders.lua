aff86e834ea9031ed390cad61db6fd1966f09989
Edgar Simo-Serra <ess@waseda.jp>
2021-03-02 12:16:28 +0900

    Added steam shader.

--- dat/scripts/love_shaders.lua
+++ dat/scripts/love_shaders.lua
@@ -292,4 +292,46 @@ vec4 effect( vec4 color, Image tex, vec2 uv, vec2 px ) {
    return shader
 end
 
+
+function love_shaders.steam( strength, speed )
+   strength = strength or 1.0
+   speed = speed or 1.0
+   local pixelcode = string.format([[
+#include "lib/math.glsl"
+#include "lib/simplex.glsl"
+
+uniform float u_time;
+
+const float strength = %f;
+const float speed    = %f;
+const float u_r      = %f;
+
+vec4 effect( vec4 color, Image tex, vec2 uv, vec2 px )
+{
+   vec4 texcolor = color * texture2D( tex, uv );
+
+   vec2 offset = vec2( 50.0*sin( M_PI*u_time * 0.001 * speed ), -0.3*u_time*speed );
+
+   float n = 0.0;
+   for (float i=1.0; i<4.0; i=i+1.0) {
+      float m = pow( 2.0, i );
+      n += snoise( offset +  px * strength * 0.0015 * m + 1000.0 * u_r ) * (1.0 / m);
+   }
+
+   texcolor.a *= 0.68 + 0.3 * n;
+
+   return color * texcolor;
+}
+]], strength, speed, love_math.random() )
+
+   local shader = graphics.newShader( pixelcode, _vertexcode )
+   shader._dt = 1000 * love_math.random()
+   shader.update = function (self, dt)
+      self._dt = self._dt + dt
+      self:send( "u_time", self._dt )
+   end
+   return shader
+end
+
+
 return love_shaders
