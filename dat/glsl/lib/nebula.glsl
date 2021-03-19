08115d9b549ceeddf3b2403e4774d8e9a4772582
Edgar Simo-Serra <ess@waseda.jp>
2021-03-19 11:35:13 +0900

    Lowered the value of the colour of the nebula on the map.

--- dat/glsl/lib/nebula.glsl
+++ dat/glsl/lib/nebula.glsl
@@ -9,7 +9,7 @@ const int ITERATIONS = 3;
 const float SCALAR = pow(2., 4./3.);
 const float COLOUR_DISPERSION = 0.05;
 
-vec4 nebula( vec4 bg, vec2 rel_pos, float time, float hue, float brightness )
+vec4 nebula( vec4 bg, vec2 rel_pos, float time, float hue, float value, float brightness )
 {
    float f, scale;
    vec3 uv, rgb, hsv;
@@ -35,7 +35,7 @@ vec4 nebula( vec4 bg, vec2 rel_pos, float time, float hue, float brightness )
    /* Choose the colour. */
    hue += COLOUR_DISPERSION * snoise( 0.1 * uv );
    hue = fract(hue);
-   hsv = vec3( hue, 1.0, 1.0 );
+   hsv = vec3( hue, value, 1.0 );
    rgb = hsv2rgb( hsv );
 
    return mix( bg, vec4( rgb, 1.0 ), brightness+(1.0-brightness)*f );
--- dat/glsl/nebula_map.frag
+++ dat/glsl/nebula_map.frag
@@ -8,7 +8,9 @@ uniform vec2 globalpos;
 in vec2 localpos;
 out vec4 color_out;
 
-const float smoothness = 0.5;
+const float smoothness  = 0.5;
+const float value       = 0.4;
+const float brightness  = 0.5;
 
 void main(void) {
    float f = 0.0;
@@ -17,7 +19,7 @@ void main(void) {
    // Calculate coordinates
    vec2 rel_pos = gl_FragCoord.xy + projection[3].xy - globalpos;
    rel_pos /= eddy_scale;
-   color_out = nebula( vec4(0.0), rel_pos, time, hue, 0.5 );
+   color_out = nebula( vec4(0.0), rel_pos, time, hue, value, brightness );
 
    // Fallout
    float dist = length(localpos);
