5a58ba85a81b8e15c24151d5b2f83a14e7590193
nenau-again <renaud.ferrier@gmail.com>
2021-03-21 10:57:53 +0100

    probability of warlord battle

--- dat/events/dvaered/warlords_battle.lua
+++ dat/events/dvaered/warlords_battle.lua
@@ -2,7 +2,7 @@
 <?xml version='1.0' encoding='utf8'?>
 <event name="Warlords battle">
   <trigger>enter</trigger>
-  <chance>100</chance>
+  <chance>5</chance>
   <cond>system.cur():faction() == faction.get("Dvaered") and not player.evtActive ("Warlords battle")</cond>
   <flags>
   </flags>
