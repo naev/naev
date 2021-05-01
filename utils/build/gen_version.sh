851d4eafcdc210ba48548bebaa0ce4ed415e0b3c
Justin Blanchard <UncombedCoconut@gmail.com>
2021-04-30 22:56:10 -0400

    Update gen_version.sh fallback logic: try to extract a version until we get one.

--- utils/build/gen_version.sh
+++ utils/build/gen_version.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
 SOURCE_ROOT="$MESON_SOURCE_ROOT"
+VERSION=""
 
 if [ -d "$SOURCE_ROOT/.git/" ]; then
     # In the git repo. Build the tag from git info
     VERSION=$(git -C "$SOURCE_ROOT" describe --tags --match 'v*' --dirty | sed -E 's/^v//;s/^(.*)-([^-]*)-(g[^-]*)/\1+\2.\3/;s/-dirty/.dirty/')
     echo $VERSION > "$SOURCE_ROOT/dat/VERSION"
-    echo $VERSION
+fi
 
-elif [ -f "$SOURCE_ROOT/dat/VERSION" ]; then
+if [ -z "$VERSION" -a -f "$SOURCE_ROOT/dat/VERSION" ]; then
     # In a source package. Version file should exist.
-    cat "$SOURCE_ROOT/dat/VERSION"
+    VERSION=`cat "$SOURCE_ROOT/dat/VERSION"`
+fi
 
-else
+if [ -z "$VERSION" ]; then
     # Couldn't find git repo or a packaged VERSION file
     # Did you download the zip'd repo instead of a release?
     VERSION="$1+dev"
     echo $VERSION > "$SOURCE_ROOT/dat/VERSION"
-    echo $VERSION
-
 fi
+
+echo $VERSION
