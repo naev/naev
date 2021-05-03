6e4f0e58425de5ac4b47b15199205c53f758361b
Edgar Simo-Serra <bobbens@gmail.com>
2021-05-03 20:35:52 +0900

    comment

--- dat/events/bioship_upgrade.lua
+++ dat/events/bioship_upgrade.lua
@@ -169,7 +169,7 @@ function pay( amount )
                local sn = string.format( "%d", new_level )
                new_part = bioship_parts[index][1]:format( sn )
             end
-            local q = pp:addOutfit( new_part, 1, true )
+            local q = pp:addOutfit( new_part, 1, true ) -- Only check slot stuff, ignore CPU and the rest.
             if q<=0 then
                warn(string.format(_("Unable to upgrade Soromid outfit to %s!"), new_part))
                pp:addOutfit( part, 1, true ) -- Try to add previous one
