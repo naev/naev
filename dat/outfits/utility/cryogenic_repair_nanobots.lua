7195a8bdac8adb114c1e628dc294b7bb0e41074a
Edgar Simo-Serra <bobbens@gmail.com>
2021-05-23 23:33:31 +0900

    Added cryogenic repair nanobots outfit.

---
+++ dat/outfits/utility/cryogenic_repair_nanobots.lua
@@ -0,0 +1,16 @@
+function init( p, po )
+   po:clear()
+end
+
+function cooldown( p, po, done, opt )
+   if not done then
+      -- Initialize repairs, we calculate how much is needed to repair to full
+      local a, s = p:health(true)
+      local ps = p:stats()
+      local regen = (ps.armour - a) / opt
+      po:set( "armour_damage", -regen );
+   else
+      -- Stop repairing
+      po:clear()
+   end
+end
