eacbeb1265ebfd4053df08d92db96e6d48a18d48
Edgar Simo-Serra <bobbens@gmail.com>
2021-05-23 23:26:18 +0900

    Added nlua_refenvtype which allows limiting the accepted types.

--- src/nlua.h
+++ src/nlua.h
@@ -47,6 +47,7 @@ int nlua_loadStandard( nlua_env env );
 int nlua_errTrace( lua_State *L );
 int nlua_pcall( nlua_env env, int nargs, int nresults );
 int nlua_refenv( nlua_env env, const char *name );
+int nlua_refenvtype( nlua_env env, const char *name, int type );
 
 
 #endif /* NLUA_H */
