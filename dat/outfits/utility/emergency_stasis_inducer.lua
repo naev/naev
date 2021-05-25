a48ad3d8016880bc2046781e069f54aa6eb9fe88
Edgar Simo-Serra <bobbens@gmail.com>
2021-05-25 16:58:51 +0900

    Gave emergency stasis inducer it's own shader effect.

--- dat/outfits/utility/emergency_stasis_inducer.lua
+++ dat/outfits/utility/emergency_stasis_inducer.lua
@@ -1,12 +1,33 @@
+local pp_shaders = require 'pp_shaders'
+
 -- Global constant variables for the outfit
 cooldown = 8 -- cooldown period in seconds
 ontime = 3 -- powered on time in seconds (it gets modulated by time_mod)
+ppshader = pp_shaders.newShader([[
+#include "lib/blend.glsl"
+#include "lib/colour.glsl"
+const vec3 colmod = vec3( 1.0, 0.0, 0.0 );
+uniform float u_time = 0;
+vec4 effect( sampler2D tex, vec2 texcoord, vec2 pixcoord )
+{
+   vec4 color     = texture( tex, texcoord );
+   float opacity  = min( 2.0*u_time, 0.8 );
+   vec3 grayscale = vec3(rgb2lum(color.rgb));
+   color.rgb      = mix( color.rgb, grayscale, opacity );
+   return color;
+}
+]])
 
 -- Init function run on creation
 function init( p, po )
    mem.timer = 0
    mem.active = false
    po:state( "off" )
+   mem.isp = (p == player.pilot())
+   if mem.shader then
+      shader.rmPPShader( ppshader )
+   end
+   mem.shader = nil
 end
 
 function update( p, po, dt )
@@ -18,6 +39,11 @@ function update( p, po, dt )
          mem.active = false
          po:state( "cooldown" )
          po:progress(1)
+
+         if mem.shader then
+            shader.rmPPShader( ppshader )
+         end
+         mem.shader = nil
          return
       else
          po:progress( mem.timer / ontime )
@@ -38,5 +64,11 @@ function onhit( p, po, armour, shield )
       mem.active = true
       po:state( "on" )
       po:progress(1)
+
+      -- Visual effect
+      if mem.isp then
+         ppshader:send( "u_time", 0 )
+         mem.shader = shader.addPPShader( ppshader, "game" )
+      end
    end
 end
