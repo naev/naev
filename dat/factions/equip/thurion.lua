b8f1b8acc505c5c8fde7d19f3daf97ae024b7858
Edgar Simo-Serra <bobbens@gmail.com>
2021-05-27 17:55:12 +0900

    Nerfed thurion ship nebula resistance, but gave them outfit to compensate.

--- dat/factions/equip/thurion.lua
+++ dat/factions/equip/thurion.lua
@@ -145,5 +145,7 @@ equip_typeOutfits_structurals["Apprehension"] = {
 --    @param p Pilot to equip
 --]]
 function equip( p )
+   -- They all get the nebula resistant coating
+   equip_set( p, { { "Nebula Resistant Coating", } } )
    equip_generic( p )
 end
