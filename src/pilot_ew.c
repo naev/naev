51a2475fe6347e6b6b879458ee9233e1fd6fd788
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-02 00:27:51 +0900

    Clamped tracking.

--- src/pilot_ew.c
+++ src/pilot_ew.c
@@ -321,5 +321,5 @@ int pilot_inRangeJump( const Pilot *p, int i )
 double pilot_ewWeaponTrack( const Pilot *p, const Pilot *t, double trackmin, double trackmax )
 {
    double mod = p->stats.ew_track * p->stats.ew_detect;
-   return (t->ew_evasion * mod - trackmin) / (trackmax - trackmin);
+   return CLAMP( 0., 1., (t->ew_evasion * mod - trackmin) / (trackmax - trackmin) );
 }
