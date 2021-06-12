8c36c739a70a68a68ec727fc5f24f4953eb6faab
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-12 13:41:07 +0900

    Crimson gauntlet logo.

--- dat/missions/dvaered/gauntlet/gui.lua
+++ dat/missions/dvaered/gauntlet/gui.lua
@@ -165,10 +165,9 @@ function love.load ()
    -- Window and top details
    local w, h = 800, 400
    local wdw = luatk.newWindow( nil, nil, w, h )
-   --local iw, ih = 100, 80
-   local iw, ih = 30, 30
+   local iw, ih = 100, 80
    local y = 5
-   --luatk.newImage( wdw, (w-iw)/2, y, iw, ih, lg.newImage("crimson_gauntlet.webp") )
+   luatk.newImage( wdw, (w-iw)/2, y, iw, ih, lg.newImage("gfx/misc/crimson_gauntlet.webp") )
    luatk.newText( wdw, 0, (ih-24)/2+5, (w-iw)/2-10, 24, _("CRIMSON"), {0.9, 0.1, 0.25}, "right", largefont )
    luatk.newText( wdw, (w-iw)/2+iw+10, (ih-24)/2+5, w-(w-iw)/2-iw-10, 24, _("GAUNTLET"), {0.9, 0.1, 0.25}, "left", largefont )
    headerh = ih+10
