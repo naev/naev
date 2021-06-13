ac8340e87c08b3a636e486244b42e7d983f89f64
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-13 23:22:13 +0900

    Save features.

--- src/dev_system.c
+++ src/dev_system.c
@@ -111,6 +111,8 @@ int dsys_saveSystem( StarSystem *sys )
    xmlw_startElem( writer, "general" );
    if (sys->background != NULL)
       xmlw_elem( writer, "background", "%s", sys->background );
+   if (sys->features != NULL)
+      xmlw_elem( writer, "features", "%s", sys->features );
    xmlw_elem( writer, "radius", "%f", sys->radius );
    xmlw_elem( writer, "stars", "%d", sys->stars );
    xmlw_elem( writer, "interference", "%f", sys->interference );
