7c7abd95eb423337a76d3e5eb77a95f564a91961
Justin Blanchard <UncombedCoconut@gmail.com>
2021-06-25 13:43:49 -0400

    Annoy the translators some more. :)

--- dat/missions/minerva/pirate4.lua
+++ dat/missions/minerva/pirate4.lua
@@ -115,7 +115,7 @@ They beam you a smile.]]))
       vn.label("accept")
       vn.func( function () misn_state=0 end )
       pir(_([["Glad to have you onboard again! So we have tracked down the mole and know that they are infiltrated in the station from the intercepted messages. It appears that they are most likely working at the station. Now there are not many places to work at the station so it is likely that they are involved in the gambling facility."]]))
-      pir(_([["The bad news is we don't exactly know who the moles is. However, the good news is we were able to intercept a messenger. It was after a delivery so we weren't able to capture anything very interesting. But there was a small memo we found that could be a hint."
+      pir(_([["The bad news is we don't exactly know who the mole is. However, the good news is we were able to intercept a messenger. It was after a delivery so we weren't able to capture anything very interesting. But there was a small memo we found that could be a hint."
 They should you a crumpled up dirty piece of paper that has '10K 5-6-3-1' on it and hands it to you.]]))
       vn.func( function ()
          local c = misn.cargoNew( _("Crumpled Up Note"), _("This is a crumpled up note that says '10K 5-6-3-1' on it. How could this be related to the Dvaered spy on Minerva Station?") )
