e77f833879951e839772503167fe2892727d9e15
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-28 15:29:55 +0900

    More goodness.

--- dat/scripts/equipopt.lua
+++ dat/scripts/equipopt.lua
@@ -9,17 +9,18 @@ local equipopt = {}
       Goodness functions to rank how good each outfits are
 --]]
 function equipopt.goodness_default( o )
-   -- Constant value makes them prefer outfits rather than not
-   local g = 1 - 0.00001*o.price
-   -- Movement attributes
-   g = g + 0.01*o.thrust + 0.1*o.speed + 0.1*o.turn
    -- Base attributes
-   g = g - 0.01 * o.mass + 0.01 * o.energy
-   -- Defensive attributes
-   g = g + 0.01 * o.shield + 0.01 * o.armour
-   -- Offensive attributes
-   g = g + o.dps - 0.5 * o.eps
-   return g
+   base = -0.00001*o.price - 0.01*o.mass + 0.1*o.cargo - 0.5*o.cargo_inertia + 0.003*o.fuel
+   -- Movement attributes
+   move = 0.01*o.thrust + 0.01*o.speed + 0.05*o.turn
+   -- Health attributes
+   health = 0.005*o.shield + 0.005*o.armour + 0.005*o.energy + o.absorb
+   -- Weapon attributese
+   weap = o.dps - 0.5*o.eps
+   -- Ewarfare attributes
+   ew = (1-o.ew_detect) + (1-o.ew_hide)
+   -- Constant value makes them prefer outfits rather than not
+   return 1 + base + move + health + weap + ew
 end
 
 --[[
@@ -88,15 +89,19 @@ function equipopt.equip( p, cores, outfit_list, goodness )
       end
 
       -- We correct ship stats here and convert them to "relative improvements"
+      -- Movement
       oo.thrust = oo.thrust_mod * (oo.thrust + st.thrust) - st.thrust
       oo.speed  = oo.speed_mod  * (oo.speed  + st.speed)  - st.speed
       oo.turn   = oo.turn_mod   * (oo.turn   + st.turn)   - st.turn
+      -- Health
       oo.armour = oo.armour_mod * (oo.armour + st.armour) - st.armour
       oo.shield = oo.shield_mod * (oo.shield + st.shield) - st.shield
       oo.energy = oo.energy_mod * (oo.energy + st.energy) - st.energy
       oo.armour_regen = oo.armour_regen_mod * (oo.armour_regen + st.armour_regen) - oo.armour_damage - st.armour_regen
       oo.shield_regen = oo.shield_regen_mod * (oo.shield_regen + st.shield_regen) - oo.shield_usage  - st.shield_regen
       oo.energy_regen = oo.energy_regen_mod * (oo.energy_regen + st.energy_regen) - oo.energy_usage  - oo.energy_loss - st.energy_regen
+      -- Misc
+      oo.cargo = oo.cargo_mod * (oo.cargo + ss.cargo) - ss.cargo
 
       -- Cache it all so we don't hae to recompute
       outfit_cache[v] = oo
