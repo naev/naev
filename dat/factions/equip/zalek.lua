f61ac554b1793db3fe3d90e17cfa0b7f9ab11f90
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-29 19:30:38 +0900

    Za'lek equipment is now 100% optimization based.

--- dat/factions/equip/zalek.lua
+++ dat/factions/equip/zalek.lua
@@ -1,153 +1,51 @@
-require "factions/equip/generic"
+equipopt = require 'equipopt'
 
-
-equip_typeOutfits_coreSystems["Sting"] = {
-   "Milspec Orion 4801 Core System", "Milspec Thalos 4702 Core System",
-}
-equip_typeOutfits_coreSystems["Demon"] = {
-   "Milspec Orion 5501 Core System", "Milspec Thalos 5402 Core System",
-}
-equip_typeOutfits_coreSystems["Mephisto"] = {
-   "Milspec Orion 8601 Core System",
-}
-equip_typeOutfits_coreSystems["Diablo"] = {
-   "Milspec Thalos 9802 Core System",
-}
-equip_typeOutfits_coreSystems["Hephaestus"] = {
-   "Milspec Thalos 9802 Core System",
-}
-
-equip_typeOutfits_engines["Sting"] = {
-   "Tricon Cyclone Engine",
-}
-equip_typeOutfits_engines["Demon"] = {
-   "Tricon Cyclone II Engine",
-}
-equip_typeOutfits_engines["Mephisto"] = {
-   "Unicorp Eagle 7000 Engine", "Tricon Typhoon II Engine",
-}
-equip_typeOutfits_engines["Diablo"] = {
-   "Tricon Typhoon II Engine",
-}
-equip_typeOutfits_engines["Hephaestus"] = {
-   "Melendez Mammoth XL Engine",
-}
-
-equip_typeOutfits_hulls["Sting"] = {
-   "Nexus Medium Stealth Plating", "S&K Medium Combat Plating",
-}
-equip_typeOutfits_hulls["Demon"] = {
-   "Nexus Medium Stealth Plating", "S&K Medium-Heavy Combat Plating",
-}
-equip_typeOutfits_hulls["Mephisto"] = {
-   "Unicorp D-48 Heavy Plating", "Unicorp D-68 Heavy Plating",
-}
-equip_typeOutfits_hulls["Diablo"] = {
-   "Unicorp D-48 Heavy Plating", "Unicorp D-68 Heavy Plating",
-}
-equip_typeOutfits_hulls["Hephaestus"] = {
-   "Unicorp D-68 Heavy Plating", "S&K Superheavy Combat Plating",
+local zalek_outfits = {
+   -- Heavy Weapons
+   "Za'lek Light Drone Fighter Dock", "Za'lek Heavy Drone Fighter Dock",
+   "Za'lek Bomber Drone Fighter Dock",
+   "Ragnarok Beam", "Grave Beam",
+   -- Medium Weapons
+   "Za'lek Heavy Drone Fighter Bay", "Za'lek Light Drone Fighter Bay",
+   "Za'lek Bomber Drone Fighter Bay",
+   "Enygma Systems Turreted Fury Launcher",
+   "Enygma Systems Turreted Headhunter Launcher",
+   "Enygma Systems Spearhead Launcher", "TeraCom Fury Launcher",
+   "TeraCom Headhunter Launcher", "TeraCom Medusa Launcher",
+   "TeraCom Vengeance Launcher",
+   "Za'lek Hunter Launcher", "Za'lek Reaper Launcher",
+   "Grave Lance", "Orion Beam",
+   -- Small Weapons
+   "Particle Beam",
+   -- Utility
+   "Droid Repair Crew", "Milspec Scrambler",
+   "Targeting Array", "Agility Combat AI",
+   "Milspec Jammer", "Emergency Shield Booster",
+   "Weapons Ionizer", "Sensor Array",
+   "Faraday Tempest Coating", "Hive Combat AI",
+   -- Heavy Structural
+   "Battery III", "Shield Capacitor III", "Shield Capacitor IV",
+   "Reactor Class III",
+   -- Medium Structural
+   "Battery II", "Shield Capacitor II", "Reactor Class II",
+   -- Small Structural
+   "Improved Stabilizer", "Engine Reroute",
+   "Battery", "Shield Capacitor", "Reactor Class I",
 }
 
-equip_typeOutfits_weapons["Sting"] = {
-   {
-      probability = {
-         ["Za'lek Hunter Launcher"] = 4,
-      };
-      num = 1;
-      varied = true;
-      "Enygma Systems Spearhead Launcher", "TeraCom Fury Launcher",
-      "TeraCom Headhunter Launcher", "TeraCom Medusa Launcher",
-      "TeraCom Vengeance Launcher", "Za'lek Hunter Launcher",
-      "Za'lek Reaper Launcher",
-   },
-   {
-      probability = {
-         ["Za'lek Light Drone Fighter Bay"] = 3,
-         ["Za'lek Bomber Drone Fighter Bay"] = 2,
-      };
-      varied = true;
-      num = 2;
-      "Za'lek Heavy Drone Fighter Bay",
-      "Za'lek Light Drone Fighter Bay",
-      "Za'lek Bomber Drone Fighter Bay",
-      "Za'lek Reaper Launcher",
-      "Za'lek Hunter Launcher",
-   },
-   {
-      varied = true;
-      num = 3,
-      "Grave Lance",
-      "Orion Lance",
-      "Particle Beam",
-   },
-}
-equip_typeOutfits_weapons["Demon"] = {
-   {
-      "Grave Beam",
-   },
-   {
-      probability = {
-         ["Za'lek Light Drone Fighter Bay"] = 3,
-         ["Za'lek Bomber Drone Fighter Bay"] = 2,
-         ["Za'lek Hunter Launcher"] = 2,
-      };
-      varied = true;
-      "Za'lek Heavy Drone Fighter Bay",
-      "Za'lek Light Drone Fighter Bay",
-      "Za'lek Bomber Drone Fighter Bay",
-      "Za'lek Hunter Launcher",
-      "Enygma Systems Turreted Fury Launcher",
-      "Enygma Systems Turreted Headhunter Launcher",
-      "Grave Lance", "Orion Beam",
-   },
-}
-equip_typeOutfits_weapons["Diablo"] = {
-   {
-      "Ragnarok Beam",
-   },
-   {
-      "Grave Beam",
-   },
-   {
-      varied = true;
-      "Za'lek Light Drone Fighter Dock", "Za'lek Heavy Drone Fighter Dock",
-      "Za'lek Bomber Drone Fighter Dock",
-   },
-}
-equip_typeOutfits_weapons["Mephisto"] = {
-   {
-      "Grave Beam",
-   },
-}
-equip_typeOutfits_weapons["Hephaestus"] = {
-   {
-      varied = true;
-      "Za'lek Light Drone Fighter Dock", "Za'lek Heavy Drone Fighter Dock",
-      "Za'lek Bomber Drone Fighter Dock",
-   },
-   {
-      "Ragnarok Beam",
-   },
-   {
-      "Grave Beam",
-   },
+local zalek_skip = {
+   ["Za'lek Scout Drone"]  = true,
+   ["Za'lek Light Drone"]  = true,
+   ["Za'lek Heavy Drone"]  = true,
+   ["Za'lek Bomber Drone"] = true,
 }
 
-equip_typeOutfits_utilities["Demon"] = {
-   {
-      varied = true;
-      "Droid Repair Crew", "Milspec Scrambler", "Boarding Androids MK1"
-   }
-}
-
-local zlk_util_medplus = { { varied=true;
-      "Faraday Tempest Coating", "Hive Combat AI" } }
-equip_shipOutfits_utilities["Za'lek Sting"]     = zlk_util_medplus
-equip_shipOutfits_utilities["Za'lek Demon"]     = zlk_util_medplus
-equip_shipOutfits_utilities["Za'lek Mephisto"]  = zlk_util_medplus
-equip_shipOutfits_utilities["Za'lek Diablo"]    = zlk_util_medplus
-equip_shipOutfits_utilities["Za'lek Hephaestus"]= zlk_util_medplus
+local function zalek_params( params )
+   -- Prefer to use the Za'lek utilities
+   params.prefer["Hive Combat AI"]          = 100
+   params.prefer["Faraday Tempest Coating"] = 100
+   return params
+end
 
 --[[
 -- @brief Does Za'lek pilot equipping
@@ -155,5 +53,13 @@ equip_shipOutfits_utilities["Za'lek Hephaestus"]= zlk_util_medplus
 --    @param p Pilot to equip
 --]]
 function equip( p )
-   equip_generic( p )
+   local sname = p:ship():nameRaw()
+   if zalek_skip[sname] then return end
+
+   -- Choose parameters and make Za'lekish
+   params = equipopt.params.choose( p )
+   params = zalek_params( params )
+
+   -- Try to equip
+   equipopt.equip( p, cores, zalek_outfits, params )
 end
