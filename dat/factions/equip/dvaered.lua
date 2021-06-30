953e64fdaa02b43cf867a95c1ac6f193b00cbe9e
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-30 11:35:29 +0900

    More dvaered equip tweaks.

--- dat/factions/equip/dvaered.lua
+++ dat/factions/equip/dvaered.lua
@@ -1,134 +1,6 @@
 local equipopt = require 'equipopt'
 local mt = require 'merge_tables'
 
---[[
-equip_typeOutfits_hulls["Vendetta"] = {
-   "S&K Light Combat Plating",
-}
-equip_typeOutfits_hulls["Ancestor"] = {
-   "S&K Light Combat Plating",
-}
-equip_typeOutfits_hulls["Phalanx"] = {
-   "S&K Medium Combat Plating",
-}
-equip_typeOutfits_hulls["Vigilance"] = {
-   "S&K Medium-Heavy Combat Plating",
-}
-equip_typeOutfits_hulls["Goddard"] = {
-   "S&K Superheavy Combat Plating",
-}
-
-equip_typeOutfits_weapons["Vendetta"] = {
-   {
-      num = 2, probability = {
-         ["Vulcan Gun"] = 4, ["Gauss Gun"] = 4,
-      };
-      "Vulcan Gun", "Gauss Gun",
-      "Unicorp Mace Launcher", "TeraCom Mace Launcher",
-   },
-   {
-      num = 2, probability = {
-         ["Vulcan Gun"] = 4, ["Gauss Gun"] = 4,
-      };
-      "Vulcan Gun", "Gauss Gun",
-      "Unicorp Mace Launcher", "TeraCom Mace Launcher",
-   },
-   {
-      probability = {
-         ["Vulcan Gun"] = 4, ["Gauss Gun"] = 4,
-      };
-      "Vulcan Gun", "Gauss Gun",
-      "Unicorp Mace Launcher", "TeraCom Mace Launcher",
-   },
-}
-equip_typeOutfits_weapons["Ancestor"] = {
-   {
-      num = 2, varied = true;
-      "Unicorp Headhunter Launcher", "Unicorp Vengeance Launcher",
-   },
-   {
-      varied = true;
-      "Vulcan Gun", "TeraCom Mace Launcher",
-   },
-}
-equip_typeOutfits_weapons["Phalanx"] = {
-   {
-      num = 3, varied = true;
-      "Unicorp Headhunter Launcher", "Unicorp Vengeance Launcher",
-      "TeraCom Headhunter Launcher", "TeraCom Vengeance Launcher",
-      "Unicorp Caesar IV Launcher", "TeraCom Imperator Launcher",
-   },
-   {
-      probability = {
-         ["Shredder"] = 8,
-      };
-      "Shredder", "Vulcan Gun",
-   },
-}
-equip_typeOutfits_weapons["Vigilance"] = {
-   {
-      num = 2;
-      "Railgun",
-   },
-   {
-      num = 1;
-      "Enygma Systems Turreted Fury Launcher",
-      "Enygma Systems Turreted Headhunter Launcher",
-   },
-   {
-      num = 1;
-      "Mass Driver", "Turreted Vulcan Gun",
-      "Enygma Systems Turreted Fury Launcher",
-      "Enygma Systems Turreted Headhunter Launcher",
-   },
-   {
-      "Mass Driver", "Turreted Vulcan Gun",
-   },
-}
-equip_typeOutfits_weapons["Goddard"] = {
-   {
-      num = 1;
-      "Enygma Systems Turreted Fury Launcher",
-      "Enygma Systems Turreted Headhunter Launcher",
-   },
-   {
-      "Repeating Railgun",
-   },
-   {
-      "Railgun", "Heavy Laser Turret", "Grave Beam", "Heavy Ion Turret",
-   },
-}
-
-equip_typeOutfits_structurals["Phalanx"] = {
-   {
-      varied = true;
-      "Medium Fuel Pod", "Battery II", "Shield Capacitor II",
-      "Plasteel Plating", "Active Plating",
-   },
-}
-equip_typeOutfits_structurals["Vigilance"] = {
-   {
-      varied = true;
-      "Large Fuel Pod", "Battery III", "Shield Capacitor IV",
-      "Shield Capacitor III", "Nanobond Plating",
-   },
-   {
-      varied = true;
-      "Medium Fuel Pod", "Battery II", "Shield Capacitor II",
-      "Plasteel Plating", "Active Plating",
-   },
-}
-
-
-local dva_util_small = { { "MilSpec Impacto-Plastic Coating" } }
-local dva_util_medplus = { { varied = true;
-   "Cyclic Combat AI", "MilSpec Impacto-Plastic Coating" } }
-equip_shipOutfits_utilities["Dvaered Vendetta"] = dva_util_small
-equip_shipOutfits_utilities["Dvaered Ancestor"] = dva_util_small
-equip_shipOutfits_utilities["Dvaered Phalanx"]  = dva_util_medplus
-equip_shipOutfits_utilities["Dvaered Vigilance"]= dva_util_medplus
-equip_shipOutfits_utilities["Dvaered Goddard"]  = dva_util_medplus
---]]
 
 local dvaered_outfits = {
    -- Heavy Weapons
@@ -163,6 +35,21 @@ local dvaered_outfits = {
 
 
 local dvaered_params = {
+   ["Dvaered Vendetta"] = function () return {
+         type_range = {
+            ["Launcher"] = { max = rnd.rnd(0,4) },
+         }
+      } end,
+   ["Dvaered Vigilance"] = function () return {
+         type_range = {
+            ["Launcher"] = { max = rnd.rnd(0,1) },
+         }
+      } end,
+   ["Dvaered Goddard"] = function () return {
+         type_range = {
+            ["Launcher"] = { max = rnd.rnd(0,2) },
+         }
+      } end,
 }
 local function choose_one( t ) return t[ rnd.rnd(1,#t) ] end
 local dvaered_cores = {
@@ -197,7 +84,9 @@ function equip( p )
    -- Prefer to use the Za'lek utilities
    params.prefer["Cyclic Combat AI"] = 100
    params.prefer["MilSpec Impacto-Plastic Coating"] = 100
+   params.type_range["Bolt Weapon"] = { min = 1 }
    params.max_same_stru = 3
+   params.min_energy_regen = 1.0 -- Dvaereds want more energy
    params.max_mass = 0.95 + 0.2*rnd.rnd()
    -- Per ship tweaks
    local sp = dvaered_params[ sname ]
