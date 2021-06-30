7011fc95fd70e8dde517f248710e451f3b864857
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-30 18:06:04 +0900

    Empire linopt equip script

--- dat/factions/equip/empire.lua
+++ dat/factions/equip/empire.lua
@@ -1,144 +1,120 @@
-require "factions/equip/generic"
+local equipopt = require 'equipopt'
+local mt = require 'merge_tables'
 
 
-equip_typeOutfits_coreSystems["Shark"] = {
-   "Milspec Orion 2301 Core System",
+local empire_outfits = {
+   -- Heavy Weapons
+   "Empire Lancelot Fighter Bay",
+   "Turbolaser", "Heavy Ripper Turret", "Ragnarok Beam",
+   "Heavy Laser Turret", "Railgun",
+   -- Medium Weapons
+   "Heavy Ripper Cannon", "Laser Turret MK2", "Orion Beam",
+   "TeraCom Fury Launcher", "Unicorp Headhunter Launcher",
+   "Unicorp Vengeance Launcher", "Enygma Systems Spearhead Launcher",
+   "Unicorp Caesar IV Launcher", "Enygma Systems Turreted Fury Launcher",
+   -- Small Weapons
+   "Unicorp Banshee Launcher", "TeraCom Banshee Launcher",
+   --"Unicorp Mace Launcher", "TeraCom Mace Launcher",
+   "Laser Cannon MK1", "Plasma Blaster MK1", "Ion Cannon",
+   "Ripper Cannon",
+   -- Utility
+   "Hunting Combat AI", "Photo-Voltaic Nanobot Coating",
+   "Unicorp Scrambler", "Unicorp Light Afterburner",
+   "Sensor Array", "Hellburner", "Emergency Shield Booster",
+   "Unicorp Medium Afterburner", "Droid Repair Crew",
+   -- Heavy Structural
+   "Battery IV", "Large Fuel Pod", "Battery III", "Reactor Class III",
+   "Shield Capacitor III", "Nanobond Plating",
+   -- Medium Structural
+   "Medium Fuel Pod", "Battery II", "Shield Capacitor II",
+   "Active Plating", "Reactor Class II",
+   -- Small Structural
+   "Plasteel Plating", "Battery I", "Improved Stabilizer", "Engine Reroute",
+   "Reactor Class I", "Small Fuel Pod",
 }
-equip_typeOutfits_coreSystems["Lancelot"] = equip_shipOutfits_coreSystems["Empire Lancelot"]
-equip_typeOutfits_coreSystems["Admonisher"] = {
-   "Milspec Orion 4801 Core System",
-}
-equip_typeOutfits_coreSystems["Pacifier"] = {
-   "Milspec Orion 5501 Core System",
-}
-equip_typeOutfits_coreSystems["Hawking"] = {
-   "Milspec Orion 8601 Core System",
-}
-equip_typeOutfits_coreSystems["Peacemaker"] = {
-   "Milspec Orion 9901 Core System",
+local empire_outfits_collective = false
+
+
+local empire_params = {
+   ["Empire Lancelot"] = function () return {
+         type_range = {
+            ["Launcher"] = { min = 1 },
+         },
+      } end,
+   ["Empire Peacemaker"] = function () return {
+         ["turret"] = 2,
+      } end,
 }
 
-equip_typeOutfits_engines["Shark"] = {
-   "Tricon Zephyr Engine",
+local function choose_one( t ) return t[ rnd.rnd(1,#t) ] end
+local empire_cores = {
+   ["Empire Shark"] = function () return {
+         "Milspec Orion 2301 Core System",
+         "Tricon Zephyr Engine",
+         choose_one{ "Nexus Light Stealth Plating", "S&K Ultralight Combat Plating" },
+      } end,
+   ["Empire Lancelot"] = function () return {
+         "Milspec Orion 3701 Core System",
+         "Tricon Zephyr II Engine",
+         choose_one{ "Nexus Light Stealth Plating", "S&K Light Combat Plating" },
+      } end,
+   ["Empire Admonisher"] = function () return {
+         "Milspec Orion 4801 Core System",
+         "Tricon Cyclone Engine",
+         choose_one{ "Nexus Medium Stealth Plating", "S&K Medium Combat Plating" },
+      } end,
+   ["Empire Pacifier"] = function () return {
+         "Milspec Orion 5501 Core System",
+         "Tricon Cyclone II Engine",
+         "S&K Medium-Heavy Combat Plating",
+      } end,
+   ["Empire Hawking"] = function () return {
+         "Milspec Orion 8601 Core System",
+         "Tricon Typhoon Engine",
+         choose_one{ "Unicorp D-48 Heavy Plating", "Unicorp D-68 Heavy Plating" },
+      } end,
+   ["Empire Peacemaker"] = function () return {
+         "Milspec Orion 9901 Core System",
+         "Melendez Mammoth XL Engine",
+         "S&K Superheavy Combat Plating",
+      } end,
 }
-equip_typeOutfits_engines["Lancelot"] = equip_shipOutfits_engines["Empire Lancelot"]
-equip_typeOutfits_engines["Admonisher"] = {
-   "Tricon Cyclone Engine",
-}
-equip_typeOutfits_engines["Pacifier"] = {
-   "Tricon Cyclone II Engine",
-}
-equip_typeOutfits_engines["Hawking"] = {
-   "Tricon Typhoon Engine",
-}
-equip_typeOutfits_engines["Peacemaker"] = {
-   "Melendez Mammoth XL Engine",
-}
-
-equip_typeOutfits_hulls["Shark"] = {
-   "Nexus Light Stealth Plating", "S&K Ultralight Combat Plating",
-}
-equip_typeOutfits_hulls["Lancelot"] = equip_shipOutfits_hulls["Empire Lancelot"]
-equip_typeOutfits_hulls["Admonisher"] = {
-   "Nexus Medium Stealth Plating", "S&K Medium Combat Plating",
-}
-equip_typeOutfits_hulls["Pacifier"] = {
-   "Nexus Medium Stealth Plating", "S&K Medium-Heavy Combat Plating",
-}
-equip_typeOutfits_hulls["Hawking"] = {
-   "Unicorp D-48 Heavy Plating", "Unicorp D-68 Heavy Plating"
-}
-equip_typeOutfits_hulls["Peacemaker"] = {
-   "S&K Superheavy Combat Plating",
-}
-
-equip_typeOutfits_weapons["Shark"] = {
-   {
-      num = 1;
-      "Unicorp Banshee Launcher", "TeraCom Banshee Launcher",
-      "Unicorp Mace Launcher", "TeraCom Mace Launcher",
-   },
-   {
-      "Laser Cannon MK1", "Plasma Blaster MK1", "Ion Cannon",
-   },
-}
-equip_typeOutfits_weapons["Lancelot"] = equip_shipOutfits_weapons["Empire Lancelot"]
-equip_typeOutfits_weapons["Admonisher"] = {
-   {
-      num = 1;
-      "TeraCom Fury Launcher", "Unicorp Headhunter Launcher",
-      "Unicorp Vengeance Launcher", "Enygma Systems Spearhead Launcher"
-   },
-   {
-      "Heavy Ripper Cannon"
-   },
-   {
-      "Ripper Cannon"
-   }
-}
-equip_typeOutfits_weapons["Pacifier"] = {
-   {
-      num = 2;
-      "Heavy Laser Turret", "Railgun"
-   },
-   {
-      num = 1;
-      "TeraCom Fury Launcher", "Unicorp Headhunter Launcher",
-      "Unicorp Vengeance Launcher", "Enygma Systems Spearhead Launcher",
-      "Unicorp Caesar IV Launcher", "Enygma Systems Turreted Fury Launcher"
-   },
-   {
-      num = 1;
-      "Heavy Ripper Cannon", "Laser Turret MK2", "Orion Beam",
-   },
-   {
-      "Heavy Ripper Cannon", "Laser Turret MK2"
-   }
-}
-
-local emp_util_small = { { "Photo-Voltaic Nanobot Coating" } }
-local emp_util_medplus = { { varied=true;
-   "Hunting Combat AI", "Photo-Voltaic Nanobot Coating" } }
-equip_shipOutfits_utilities["Empire Shark"]     = emp_util_small
-equip_shipOutfits_utilities["Empire Lancelot"]  = emp_util_small
-equip_shipOutfits_utilities["Empire Admonisher"]= emp_util_medplus
-equip_shipOutfits_utilities["Empire Pacifier"]  = emp_util_medplus
-equip_shipOutfits_utilities["Empire Hawking"]   = emp_util_medplus
-equip_shipOutfits_utilities["Empire Peacemaker"]= emp_util_medplus
 
 --[[
--- @brief Does empire pilot equipping
+-- @brief Does Dvaered pilot equipping
 --
 --    @param p Pilot to equip
 --]]
 function equip( p )
-   if diff.isApplied( "collective_dead" ) then
-      equip_typeOutfits_weapons["Peacemaker"] = {
-         {
-            varied = true;
-            "Empire Lancelot Fighter Bay", "Drone Fighter Bay",
-         },
-         {
-            "Turbolaser", "Heavy Ripper Turret", "Ragnarok Beam",
-         },
-         {
-            "Heavy Laser Turret",
-         },
-      }
-   else
-      equip_typeOutfits_weapons["Peacemaker"] = {
-         {
-            varied = true;
-            "Empire Lancelot Fighter Bay",
-         },
-         {
-            "Turbolaser", "Heavy Ripper Turret", "Ragnarok Beam",
-         },
-         {
-            "Heavy Laser Turret",
-         },
-      }
+   if diff.isApplied( "collective_dead" ) and not empire_outfits_collective then
+      table.insert( empire_outfits, "Drone Fighter Bay" )
+      empire_outfits_collective = true
    end
 
-   equip_generic( p )
+   local sname = p:ship():nameRaw()
+   --if empire_skip[sname] then return end
+
+   -- Choose parameters and make Empire-ish
+   local params = equipopt.params.choose( p )
+   -- Prefer to use the Empire utilities
+   params.prefer["Hunting Combat AI"] = 100
+   params.prefer["Photo-Voltaic Nanobot Coating"] = 100
+   params.type_range["Bolt Weapon"] = { min = 1 }
+   params.type_range["Launcher"] = { max = 2 }
+   params.max_same_stru = 3
+   params.bolt = 1.5
+   params.min_energy_regen = 1.0 -- Empire loves energy
+   params.max_mass = 0.95 + 0.1*rnd.rnd()
+   -- Per ship tweaks
+   local sp = empire_params[ sname ]
+   if sp then
+      params = mt.merge_tables_recursive( params, sp() )
+   end
+
+   -- See cores
+   local cores = empire_cores[ sname ]()
+
+   -- Try to equip
+   equipopt.equip( p, cores, empire_outfits, params )
 end
+
