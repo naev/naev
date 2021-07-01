9c7c3f8b12aa876432c9cdf70f34e108318745bc
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-02 00:37:10 +0900

    optimal thurions

--- dat/factions/equip/thurion.lua
+++ dat/factions/equip/thurion.lua
@@ -1,144 +1,60 @@
-require "factions/equip/generic"
+local equipopt = require 'equipopt'
+local mt = require 'merge_tables'
+local ecores = require 'factions.equip.cores'
 
-
-equip_typeOutfits_weapons["Perspicacity"] = {
-   {
-      "Laser Cannon MK1", "Razor MK1", "TeraCom Mace Launcher",
-      "TeraCom Banshee Launcher", "Electron Burst Cannon",
-   },
-}
-equip_typeOutfits_weapons["Ingenuity"] = {
-   {
-      "Laser Cannon MK1", "Razor MK1", "TeraCom Mace Launcher",
-      "TeraCom Banshee Launcher", "Electron Burst Cannon",
-   },
-}
-equip_typeOutfits_weapons["Scintillation"] = {
-   {
-      varied = true;
-      "TeraCom Fury Launcher", "TeraCom Medusa Launcher",
-      "Unicorp Headhunter Launcher", "Convulsion Launcher",
-   },
-   {
-      "Laser Cannon MK2", "Razor MK2",
-   },
-}
-equip_typeOutfits_weapons["Virtuosity"] = {
-   {
-      varied = true;
-      "Unicorp Fury Launcher", "Unicorp Headhunter Launcher",
-      "Unicorp Medusa Launcher", "Unicorp Vengeance Launcher",
-      "Enygma Systems Spearhead Launcher", "Unicorp Caesar IV Launcher",
-      "TeraCom Fury Launcher", "TeraCom Headhunter Launcher",
-      "TeraCom Medusa Launcher", "TeraCom Vengeance Launcher",
-      "TeraCom Imperator Launcher", "Convulsion Launcher",
-   },
-   {
-      probability = {
-         ["Ripper Cannon"] = 8, ["Slicer"] = 8,
-      };
-      "Ripper Cannon", "Slicer", "Laser Cannon MK2", "Razor MK2",
-   },
-}
-equip_typeOutfits_weapons["Taciturnity"] = {
-   {
-      "Heavy Ripper Turret", "Grave Beam", "Heavy Ion Turret",
-   },
-   {
-      num = 1;
-      "Enygma Systems Turreted Fury Launcher",
-      "Turreted Convulsion Launcher",
-   },
-   {
-      "Laser Turret MK2", "Razor Turret MK2", "Orion Beam",
-      "EMP Grenade Launcher", "Enygma Systems Turreted Fury Launcher",
-      "Turreted Convulsion Launcher",
-   },
-}
-equip_typeOutfits_weapons["Apprehension"] = {
-   {
-      "Heavy Ripper Turret", "Grave Beam", "Heavy Ion Turret",
-   },
-   {
-      num = 1;
-      "Enygma Systems Turreted Fury Launcher",
-      "Turreted Convulsion Launcher",
-   },
-   {
-      "Laser Turret MK2", "Razor Turret MK2", "Orion Beam",
-      "EMP Grenade Launcher", "Enygma Systems Turreted Fury Launcher",
-      "Turreted Convulsion Launcher",
-   },
-}
-equip_typeOutfits_weapons["Certitude"] = {
-   {
-      num = 1;
-      "Enygma Systems Turreted Fury Launcher",
-      "Turreted Convulsion Launcher",
-   },
-   {
-      "Heavy Laser Turret", "Ragnarok Beam",
-      "Thurion Perspicacity Fighter Bay",
-   },
-   {
-      "Heavy Ripper Turret", "Grave Beam", "Heavy Ion Turret",
-   },
+local thurion_outfits = {
+   -- Heavy Weapons
+   "Heavy Laser Turret", "Ragnarok Beam",
+   "Thurion Perspicacity Fighter Bay",
+   "Heavy Ripper Turret", "Grave Beam", "Heavy Ion Turret",
+   -- Medium Weapons
+   "Unicorp Fury Launcher", "Unicorp Headhunter Launcher",
+   "Unicorp Medusa Launcher", "Unicorp Vengeance Launcher",
+   "Enygma Systems Spearhead Launcher", "Unicorp Caesar IV Launcher",
+   "TeraCom Fury Launcher", "TeraCom Headhunter Launcher",
+   "TeraCom Medusa Launcher", "TeraCom Vengeance Launcher",
+   "TeraCom Imperator Launcher", "Convulsion Launcher",
+   "Enygma Systems Turreted Fury Launcher",
+   "Turreted Convulsion Launcher",
+   "Laser Turret MK2", "Razor Turret MK2", "Orion Beam",
+   "EMP Grenade Launcher", "Enygma Systems Turreted Fury Launcher",
+   -- Small Weapons
+   "Ripper Cannon", "Slicer", "Laser Cannon MK2", "Razor MK2",
+   "Laser Cannon MK1", "Razor MK1", "TeraCom Mace Launcher",
+   "TeraCom Banshee Launcher", "Electron Burst Cannon",
+   -- Utility
+   "Droid Repair Crew", "Milspec Scrambler",
+   "Targeting Array", "Agility Combat AI",
+   "Milspec Jammer", "Emergency Shield Booster",
+   "Weapons Ionizer", "Sensor Array",
+   "Nebula Resistant Coating",
+   -- Heavy Structural
+   "Battery III", "Shield Capacitor III", "Shield Capacitor IV",
+   "Reactor Class III",
+   -- Medium Structural
+   "Battery II", "Shield Capacitor II", "Reactor Class II",
+   -- Small Structural
+   "Improved Stabilizer", "Engine Reroute",
+   "Battery I", "Shield Capacitor I", "Reactor Class I",
+   "Adaptive Stealth Plating",
 }
 
-equip_typeOutfits_structurals["Perspicacity"] = {
-   {
-      varied = true, probability = {
-         ["Small Fuel Pod"] = 4, ["Improved Stabilizer"] = 2
-      };
-      "Small Fuel Pod", "Improved Stabilizer", "Shield Capacitor I",
-      "Adaptive Stealth Plating",
-   },
+local thurion_params = {
+   --["Sirius Demon"] = function () return {
+   ["Thurion Apprehension"] = function () return {
+         type_range = {
+            ["Launcher"] = { max = 1 },
+         },
+      } end,
+   ["Thurion Certitude"] = function  () return {
+         type_range = {
+            ["Launcher"] = { max = 0 },
+         },
+      } end,
 }
-equip_typeOutfits_structurals["Ingenuity"] = {
-   {
-      varied = true, probability = {
-         ["Improved Stabilizer"] = 4, ["Engine Reroute"] = 4,
-      };
-      "Small Fuel Pod", "Improved Stabilizer", "Engine Reroute", "Battery I",
-      "Shield Capacitor I", "Adaptive Stealth Plating",
-   },
+local function choose_one( t ) return t[ rnd.rnd(1,#t) ] end
+local thurion_cores = {
 }
-equip_typeOutfits_structurals["Scintillation"] = {
-   {
-      varied = true;
-      "Small Fuel Pod", "Improved Stabilizer", "Engine Reroute", "Shield Capacitor I",
-      "Adaptive Stealth Plating",
-   },
-}
-equip_typeOutfits_structurals["Virtuosity"] = {
-   {
-      varied = true;
-      "Medium Fuel Pod", "Battery II", "Shield Capacitor II",
-      "Plasteel Plating", "Adaptive Stealth Plating",
-   },
-}
-equip_typeOutfits_structurals["Taciturnity"] = {
-   {
-      varied = true, probability = {
-         ["Small Cargo Pod"] = 15, ["Medium Fuel Pod"] = 3,
-      };
-      "Small Cargo Pod", "Medium Fuel Pod", "Battery II", "Shield Capacitor II",
-      "Plasteel Plating", "Adaptive Stealth Plating",
-   },
-}
-equip_typeOutfits_structurals["Apprehension"] = {
-   {
-      varied = true;
-      "Large Fuel Pod", "Battery III", "Shield Capacitor IV",
-      "Shield Capacitor III", "Nanobond Plating",
-   },
-   {
-      varied = true;
-      "Medium Fuel Pod", "Battery II", "Shield Capacitor II",
-      "Plasteel Plating", "Adaptive Stealth Plating",
-   },
-}
-
 
 --[[
 -- @brief Does Thurion pilot equipping
@@ -146,7 +62,31 @@ equip_typeOutfits_structurals["Apprehension"] = {
 --    @param p Pilot to equip
 --]]
 function equip( p )
-   -- They all get the nebula resistant coating
-   equip_set( p, {{ "Nebula Resistant Coating", }} )
-   equip_generic( p )
+   local ps    = p:ship()
+   local sname = ps:nameRaw()
+
+   -- Choose parameters and make Thurion-ish
+   local params = equipopt.params.choose( p )
+   -- Prefer to use the Thurion utilities
+   params.prefer["Nebula Resistant Coating"] = 100
+   params.max_same_stru = 3
+   params.max_same_weap = 3
+   params.max_mass = 0.95 + 0.1*rnd.rnd()
+   -- Per ship tweaks
+   local sp = thurion_params[ sname ]
+   if sp then
+      params = mt.merge_tables_recursive( params, sp() )
+   end
+
+   -- See cores
+   local cores
+   local thrcor = thurion_cores[ sname ]
+   if thrcor then
+      cores = thrcor()
+   else
+      cores = ecores.get( p, { all="elite" } )
+   end
+
+   -- Try to equip
+   equipopt.equip( p, cores, thurion_outfits, params )
 end
