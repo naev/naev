67f06505a40ea0892c5f81e879b9c7f7bd560980
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-01 23:53:04 +0900

    optimized miners

--- dat/factions/equip/miner.lua
+++ dat/factions/equip/miner.lua
@@ -1,3 +1,4 @@
+--[=[
 require "factions/equip/generic"
 
 
@@ -48,4 +49,46 @@ equip_classOutfits_weapons["Freighter"] = {
 --]]
 function equip( p )
    equip_generic( p )
+end
+--]=]
+local equipopt = require 'equipopt'
+local mt = require 'merge_tables'
+local ecores = require 'factions.equip.cores'
+
+local miner_outfits = {
+   -- Heavy Weapons
+   -- Medium Weapons
+   "Enygma Systems Turreted Fury Launcher",
+   "Laser Turret MK2", "Turreted Vulcan Gun", "Plasma Turret MK2",
+   "Orion Beam",
+   -- Small Weapons
+   "Laser Turret MK1", "Turreted Gauss Gun", "Plasma Turret MK1",
+   "Laser Cannon MK1", "Gauss Gun", "Plasma Blaster MK1",
+   -- Utility
+   "Unicorp Scrambler", "Unicorp Light Afterburner",
+   "Sensor Array", "Unicorp Medium Afterburner", "Droid Repair Crew",
+   -- Heavy Structural
+   "Battery III", "Shield Capacitor III", "Shield Capacitor IV",
+   "Reactor Class III",
+   -- Medium Structural
+   "Battery II", "Shield Capacitor II", "Reactor Class II",
+   -- Small Structural
+   "Improved Stabilizer", "Engine Reroute",
+   "Battery I", "Shield Capacitor I", "Reactor Class I",
+}
+
+--[[
+-- @brief Does Pirate pilot equipping
+--
+--    @param p Pilot to equip
+--]]
+function equip( p )
+   -- Choose parameters and make Pirateish
+   local params = equipopt.params.choose( p )
+
+   -- See cores
+   local cores = ecores.get( p, { all="normal" } )
+
+   -- Try to equip
+   equipopt.equip( p, cores, miner_outfits, params )
 end
