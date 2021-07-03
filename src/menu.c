59528d476a6763bdebe24b321053f8d1c62189f8
Justin Blanchard <UncombedCoconut@gmail.com>
2021-07-03 01:48:57 -0400

    Externalize Naev logo.

--- src/menu.c
+++ src/menu.c
@@ -188,7 +188,7 @@ void menu_main (void)
    music_choose("load");
 
    /* Load background and friends. */
-   tex = gl_newImage( GFX_PATH"Naev.png", 0 );
+   tex = gl_newImage( GFX_PATH"Naev.webp", 0 );
    main_naevLogo = tex;
    menu_main_bkg_system();
 
