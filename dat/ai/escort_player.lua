4399c45d90dc8da739ce3d9d00ff2a481593463c
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-09 20:29:57 +0900

    escort_player don't scan.

--- dat/ai/escort_player.lua
+++ dat/ai/escort_player.lua
@@ -3,7 +3,7 @@ require("ai/personality/patrol")
 
 -- Settings
 mem.aggressive = true
-
+mem.doscans = false
 
 function create ()
    mem.loiter = 3 -- This is the amount of waypoints the pilot will pass through before leaving the system
