0386c817d969671b48a82df796f2cf5e515fb2ef
Justin Blanchard <UncombedCoconut@gmail.com>
2021-07-09 21:37:15 -0400

    We can do *some* of the debug_* things in release mode, trivially.

--- src/debug.h
+++ src/debug.h
@@ -11,22 +11,25 @@ void debug_sigInit (void);
 void debug_sigClose (void);
 void debug_enableFPUExcept (void);
 
-
-#ifdef DEBUGGING
-#define debug_isFlag(f)     (debug_flags[f])     /**< Checks if flag f is set. */
-#define debug_setFlag(f)    (debug_flags[f] = 1) /**< Sets flag f. */
-#define debug_rmFlag(f)     (debug_flags[f] = 0) /**< Removes flag f. */
-
 enum {
    DEBUG_MARK_EMITTER,        /**< Mark the trail emitters with a cross. */
 
    /* Sentinel. */
    DEBUG_FLAGS_MAX      /**< Maximum number of flags. */
 };
+
+#if DEBUGGING
+#define debug_isFlag(f)     (debug_flags[f])     /**< Checks if flag f is set. */
+#define debug_setFlag(f)    (debug_flags[f] = 1) /**< Sets flag f. */
+#define debug_rmFlag(f)     (debug_flags[f] = 0) /**< Removes flag f. */
+
 typedef char DebugFlags[ DEBUG_FLAGS_MAX ];
 
 /* Initialize debugging flags. */
 extern DebugFlags debug_flags;
+#else /* DEBUGGING */
+#define debug_isFlag(f)     0                    /**< Checks if flag f is set. */
+#define debug_rmFlag(f)     do {} while(0)       /**< Removes flag f. */
 #endif /* DEBUGGING */
 
 
