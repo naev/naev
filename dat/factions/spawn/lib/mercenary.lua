71cfe3421811c4315bbf53eca0be2fc5585455b9
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-17 13:44:36 +0900

    whitespace

--- dat/factions/spawn/lib/mercenary.lua
+++ dat/factions/spawn/lib/mercenary.lua
@@ -3,93 +3,93 @@ local scom = require "factions/spawn/lib/common"
 
 local merc = {}
 
-pbm = 0.05   --5% mercenaries
+local pbm = 0.05   --5% mercenaries
 
 function merc.spawnLtMerc(faction)
-    local pilots = {}
-    local r = rnd.rnd()
+   local pilots = {}
+   local r = rnd.rnd()
 
-    if r < 0.1 then
-        scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Phalanx", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
-    elseif r < 0.3 then
-        scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
-        scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
-        scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-    elseif r < 0.5 then
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-    elseif r < 0.8 then
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-    else
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-        scom.addPilot( pilots, {"Admonisher", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
-    end
+   if r < 0.1 then
+      scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Phalanx", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
+   elseif r < 0.3 then
+      scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
+      scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
+      scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+   elseif r < 0.5 then
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+   else
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+      scom.addPilot( pilots, {"Admonisher", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
+   end
 
-    return pilots
+   return pilots
 end
 
 function spawnMdMerc(faction)
-    local pilots = {}
-    local r = rnd.rnd()
+   local pilots = {}
+   local r = rnd.rnd()
 
-    if r < 0.5 then
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-        scom.addPilot( pilots, {"Vigilance", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 70 );
-    elseif r < 0.8 then
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-        scom.addPilot( pilots, {"Pacifier", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 70 );
-    else
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Vigilance", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 70 );
-        scom.addPilot( pilots, {"Phalanx", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
-    end
+   if r < 0.5 then
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+      scom.addPilot( pilots, {"Vigilance", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 70 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+      scom.addPilot( pilots, {"Pacifier", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 70 );
+   else
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Vigilance", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 70 );
+      scom.addPilot( pilots, {"Phalanx", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
+   end
 
-    return pilots
+   return pilots
 end
 
 function spawnBgMerc(faction)
-    local pilots = {}
+   local pilots = {}
 
-    -- Generate the capship
-    r = rnd.rnd()
-    if r < 0.5 then
-        scom.addPilot( pilots, {"Kestrel", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 90 );
-    elseif r < 0.8 then
-        scom.addPilot( pilots, {"Hawking", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 105 );
-    else
-        scom.addPilot( pilots, {"Goddard", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 120 );
-    end
+   -- Generate the capship
+   local r = rnd.rnd()
+   if r < 0.5 then
+      scom.addPilot( pilots, {"Kestrel", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 90 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, {"Hawking", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 105 );
+   else
+      scom.addPilot( pilots, {"Goddard", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 120 );
+   end
 
-    -- Generate the escorts
-    r = rnd.rnd()
-    if r < 0.1 then
-        scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Phalanx", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
-    elseif r < 0.3 then
-        scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
-        scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
-        scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-    elseif r < 0.5 then
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-    elseif r < 0.8 then
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-    else
-        scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
-        scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
-        scom.addPilot( pilots, {"Admonisher", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
-    end
+   -- Generate the escorts
+   r = rnd.rnd()
+   if r < 0.1 then
+      scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Phalanx", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
+   elseif r < 0.3 then
+      scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
+      scom.addPilot( pilots, {"Hyena", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 15 );
+      scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+   elseif r < 0.5 then
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Shark", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+   elseif r < 0.8 then
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Lancelot", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+   else
+      scom.addPilot( pilots, {"Vendetta", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 25 );
+      scom.addPilot( pilots, {"Ancestor", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 20 );
+      scom.addPilot( pilots, {"Admonisher", _("Bounty Hunter"), "mercenary", "Civilian", faction}, 45 );
+   end
 
-    return pilots
+   return pilots
 end
 
 return merc
