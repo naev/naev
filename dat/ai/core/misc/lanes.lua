378e0cf8e761816073c2861405bf512d3f2cd2ae
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-18 17:53:20 +0900

    Handle case of disjoint safe lanes.

--- dat/ai/core/misc/lanes.lua
+++ dat/ai/core/misc/lanes.lua
@@ -88,7 +88,7 @@ local function djikstra( vertices, edges, source, target )
          end
          return S
       end
- 
+
       -- Get neighbours
       local N = {}
       for k,e in ipairs(E) do
@@ -102,6 +102,77 @@ local function djikstra( vertices, edges, source, target )
       -- Update distance
       for k,v in ipairs(N) do
          local alt = u.d + vertices[u.v]:dist( vertices[v.v] )
+         if alt < v.d then
+            v.d = alt
+            v.p = u
+         end
+      end
+   end
+
+   -- No path found
+   if target then
+      return nil
+   end
+   -- Return the entire structure if no target
+   return Q
+end
+
+--[[
+-- Djikstra but considering everything is fully connected with different
+-- penalty for travelling through non-safe areas.
+--]]
+local function djikstra_full( vertices, edges, source, target )
+   local Q = {}
+   local N = {}
+   for k,v in ipairs(vertices) do
+      local q = {
+         v=k, -- Index of vertex
+         d=math.huge, -- Gigantic initial distance
+         p=nil, -- No previous node
+      }
+      table.insert( Q, q )
+      table.insert( N, q )
+   end
+
+   -- Build penalty matrix
+   local P = {}
+   local n = #vertices
+   for i=1,n do
+      P[i] = {}
+      for j=1,n do
+         P[i][j] = 10 -- penalty
+      end
+   end
+   for k,e in ipairs(edges) do
+      P[e[1]][e[2]] = 1
+      P[e[2]][e[1]] = 1
+   end
+
+   -- Initialize source
+   Q[source].d = 0
+
+   -- Start iterating
+   while #Q > 0 do
+      -- Vertex with minimum distance
+      table.sort( Q, function(a, b) return a.d < b.d end )
+      u = Q[1]
+      table.remove( Q, 1 )
+
+      -- Search done
+      if u.v == target then
+         -- Create the path by iterating backwards
+         local S = {}
+         while u do
+            table.insert( S, vertices[u.v] )
+            u = u.p
+         end
+         return S
+      end
+
+      -- Fully connected, so all are neighbours
+      for k,v in ipairs(N) do
+         local p = P[u.v][k]
+         local alt = u.d + p*vertices[u.v]:dist( vertices[v.v] )
          if alt < v.d then
             v.d = alt
             v.p = u
@@ -133,8 +204,11 @@ end
 --[[
 -- Computes the route for the pilot to get to target.
 --]]
-function lanes.getRoute( target )
+function lanes.getRoute( target, pos )
+   pos = pos or ai.pilot():pos()
+
    -- We try to cache the lane graph per system
+   -- TODO handle hostile lanes and such
    local nc = naev.cache()
    if not nc.lanes then nc.lanes = {} end
    local ncl = nc.lanes
@@ -146,20 +220,25 @@ function lanes.getRoute( target )
    local lv, le = ncl.v, ncl.e
 
    -- Compute shortest path
-   local aip = ai.pilot():pos()
-   local sv = nearestVertex( lv, aip )
+   local sv = nearestVertex( lv, pos )
    local tv = nearestVertex( lv, target )
-   local S = djikstra( lv, le, sv, tv )
+   local S = djikstra_full( lv, le, tv, sv )
 
    -- No path so just go straight
    if #S == 0 then
       return { target }
    end
 
+   -- Add the final point if necessary (it is only approximated due to
+   -- djistra)
+   if S[#S]:dist( target ) > 1e-5 then
+      table.insert( S, target )
+   end
+
    -- Correct the first point, as we might want to start in the middle of a
    -- line segment
    if #S > 1 then
-      S[1] = closestPointLine( S[1], S[2], aip )
+      S[1] = closestPointLine( S[1], S[2], pos )
    end
 
    return S
