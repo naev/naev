7a7bbebed74eeddde383f2e49be5da05e62146d2
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-18 14:19:11 +0900

    Added circle-line segmentation collision and Lua API for it.

--- src/nlua_safelanes.c
+++ src/nlua_safelanes.c
@@ -15,6 +15,7 @@
 #include "nlua_safelanes.h"
 
 #include "array.h"
+#include "collision.h"
 #include "nlua_faction.h"
 #include "nlua_jump.h"
 #include "nlua_planet.h"
@@ -26,8 +27,10 @@
 
 /* diffs */
 static int safelanesL_get( lua_State *L );
+static int safelanesL_intersect( lua_State *L );
 static const luaL_Reg safelanesL_methods[] = {
    { "get", safelanesL_get },
+   { "intersect", safelanesL_intersect },
    {0,0}
 }; /**< Safelane Lua methods. */
 
@@ -62,7 +65,7 @@ int nlua_loadSafelanes( nlua_env env )
  *    @luatparam[opt] Faction f If present, only return this faction's lanes.
  *    @luatparam[opt=system.cur()] System s The system whose lanes we want.
  *    @luatreturn table The list of matching safe lanes, each of which is a table where:
- *                lane[1] and lane[2] are the endpoints (type Planet|Jump),
+ *                lane[1] and lane[2] are the endpoints (type vec2),
  *                and lane.faction is the owner's Faction.
  * @luafunc get
  */
@@ -124,3 +127,37 @@ static int safelanesL_get( lua_State *L )
 
    return 1;
 }
+
+
+/**
+ * @brief Computes the intersection of a line segment and a circle.
+ *
+ *    @luatparam Vector center Center of the circle.
+ *    @luatparam number radius Radius of the circle.
+ *    @luatparam Vector p1 First point of the line segment.
+ *    @luatparam Vector p2 Second point of the line segment.
+ *    @luatreturn Vector|nil First point of collision or nil if no collision.
+ *    @luatreturn Vector|nil Second point of collision or nil if single-point collision.
+ * @luafunc intersect
+ */
+static int safelanesL_intersect( lua_State *L )
+{
+   Vector2d *center, *p1, *p2, crash[2];
+   double radius;
+
+   center = luaL_checkvector( L, 1 );
+   radius = luaL_checknumber( L, 2 );
+   p1     = luaL_checkvector( L, 3 );
+   p2     = luaL_checkvector( L, 4 );
+
+   int cnt = CollideLineCircle( p1, p2, center, radius, crash );
+   if (cnt>0)
+      lua_pushvector( L, crash[0] );
+   if (cnt>1)
+      lua_pushvector( L, crash[1] );
+
+   return cnt;
+}
+
+
+
