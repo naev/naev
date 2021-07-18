afbfaf4dc7db320399e14cb4009826d7ac106db3
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-18 18:57:58 +0900

    whitespace

--- dat/ai/core/idle/civilian.lua
+++ dat/ai/core/idle/civilian.lua
@@ -4,16 +4,16 @@ local lanes = require 'ai.core.misc.lanes'
 function idle ()
    if mem.loiter == nil then mem.loiter = 3 end
    if mem.loiter == 0 then -- Try to leave. Civilians will always try to land on a planet if there is one.
-       local planet = ai.landplanet( mem.land_friendly )
-       -- planet must exist
-       if planet == nil then
-          ai.settimer(0, rnd.int(1000, 3000))
-          ai.pushtask("enterdelay")
-       else
-          mem.land = planet:pos()
-          ai.pushtask("hyperspace")
-          ai.pushtask("land")
-       end
+      local planet = ai.landplanet( mem.land_friendly )
+      -- planet must exist
+      if planet == nil then
+         ai.settimer(0, rnd.int(1000, 3000))
+         ai.pushtask("enterdelay")
+      else
+         mem.land = planet:pos()
+         ai.pushtask("hyperspace")
+         ai.pushtask("land")
+      end
    else -- Stay. Have a beer.
       if not mem.route then
          local target = lanes.getPointInterest()
--- dat/ai/core/idle/generic.lua
+++ dat/ai/core/idle/generic.lua
@@ -14,18 +14,18 @@ function idle ()
 
    if mem.loiter == nil then mem.loiter = 3 end
    if mem.loiter == 0 then -- Try to leave.
-       local planet = ai.landplanet( mem.land_friendly )
-       -- planet must exist
-       if planet == nil or mem.land_planet == false then
-          ai.settimer(0, rnd.int(1000, 3000))
-          ai.pushtask("enterdelay")
-       else
-          mem.land = planet:pos()
-          ai.pushtask("hyperspace")
-          if not mem.tookoff then
-             ai.pushtask("land")
-          end
-       end
+      local planet = ai.landplanet( mem.land_friendly )
+      -- planet must exist
+      if planet == nil or mem.land_planet == false then
+         ai.settimer(0, rnd.int(1000, 3000))
+         ai.pushtask("enterdelay")
+      else
+         mem.land = planet:pos()
+         ai.pushtask("hyperspace")
+         if not mem.tookoff then
+            ai.pushtask("land")
+         end
+      end
    else -- Stay. Have a beer.
       if mem.doscans then
          local target = __getscantarget()
