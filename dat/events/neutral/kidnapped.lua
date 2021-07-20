0766c85d7bcdf44c75158074c3a07c083f60c35e
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-21 00:03:33 +0900

    Fixed mission.

--- dat/events/neutral/kidnapped.lua
+++ dat/events/neutral/kidnapped.lua
@@ -16,7 +16,8 @@
 
 --Create Mom and Dad in their spaceship, and have them come from the planet Brooks in Arcturus system, following the player.
 function create ()
-    panma = pilot.add( "Llama", "Independent", planet.get("Brooks") )
+    local pnt = planet.get("Brooks")
+    panma = pilot.add( "Llama", "Independent", pnt )
     panma:control()
     panma:follow(player.pilot())
     hook.pilot(panma, "jump", "finish")
