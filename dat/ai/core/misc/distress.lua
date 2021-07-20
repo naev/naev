fef2e23c9e4ebad7a865a96c35a106188db93d97
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-20 12:42:00 +0900

    Simplification.

--- dat/ai/core/misc/distress.lua
+++ dat/ai/core/misc/distress.lua
@@ -9,16 +9,17 @@ mem.careful    = true
 -- Send a distress signal which causes faction loss
 function sos ()
    local plt = ai.pilot()
+   local fname = plt:faction():name()
    local msg = {
       _("Local security: requesting assistance!"),
       _("Mayday! We are under attack!"),
       _("Requesting assistance. We are under attack!"),
-      string.format(_("%s vessel under attack! Requesting help!"), plt:faction():name()),
+      string.format(_("%s vessel under attack! Requesting help!"), fname),
       _("Help! Ship under fire!"),
       _("Taking hostile fire! Need assistance!"),
       _("We are under attack, require support!"),
       _("Mayday! Ship taking damage!"),
-      string.format(_("Mayday! %s %s being assaulted!"), plt:faction():name(), string.lower( plt:ship():class() ))
+      string.format(_("Mayday! %s %s being assaulted!"), fname, string.lower( plt:ship():class() ))
    }
    ai.settarget( ai.taskdata() )
    ai.distress( msg[ rnd.int(1,#msg) ])
