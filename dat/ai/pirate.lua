207000f4a7583c2b36db2a3a44a1345e3371b534
Edgar Simo-Serra <bobbens@gmail.com>
2021-07-30 15:25:38 +0900

    Keep a static version of pirate taunts.

--- dat/ai/pirate.lua
+++ dat/ai/pirate.lua
@@ -17,6 +17,87 @@ mem.atk_board     = true
 mem.atk_kill      = false
 mem.careful       = true
 
+local bribe_prompt_list = {
+   _([["It'll cost you %s for me to ignore your pile of rubbish."]]),
+   _([["I'm in a good mood so I'll let you go for %s."]]),
+   _([["Send me %s or you're dead."]]),
+   _([["Pay up %s or it's the end of the line."]]),
+   _([["Your money or your life. %s and make the choice quickly."]]),
+   _([["Money talks bub. %s up front or get off my channel."]]),
+   _([["Shut up and give me your money! %s now."]]),
+   _([["You're either really desperate or really rich. %s or shut up."]]),
+   _([["If you're willing to negotiate I'll gladly take %s to not kill you."]]),
+   _([["You give me %s and I'll act like I never saw you."]]),
+   _([["So this is the part where you pay up or get shot up. Your choice. What'll be, %s or…"]]),
+   _([["Pay up or don't. %s now just means I'll wait till later to collect the rest."]]),
+   _([["This is a toll road, pay up %s or die."]]),
+}
+local bribe_paid_list = {
+   _([["You're lucky I'm so kind."]]),
+   _([["Life doesn't get easier than this."]]),
+   _([["Pleasure doing business."]]),
+   _([["See you again, real soon."]]),
+   _([["I'll be around if you get generous again."]]),
+   _([["Lucky day, lucky day!"]]),
+   _([["And I didn't even have to kill anyone!"]]),
+   _([["See, this is how we become friends."]]),
+   _([["Now if I kill you it'll be just for fun!"]]),
+   _([["You just made a good financial decision today."]]),
+   _([["Know what? I won't kill you."]]),
+   _([["Something feels strange. It's almost as if my urge to kill you has completely dissipated."]]),
+   _([["Can I keep shooting you anyhow? No? You sure? Fine."]]),
+   _([["And it only cost you an arm and a leg."]]),
+}
+local taunt_list_offensive = {
+   _("Prepare to be boarded!"),
+   _("Yohoho!"),
+   _("Arr!"),
+   _("What's a ship like you doing in a place like this?"),
+   _("Prepare to have your booty plundered!"),
+   _("Give me your credits or die!"),
+   _("Your ship's mine!"),
+   _("Oh ho ho, what do I see here?"),
+   _("You may want to send that distress signal now."),
+   _("It's time to die."),
+   _("Back so soon?"),
+   _("What? Were you expecting prince charming?"),
+   _("Long time no see."),
+   _("Nothing personal, just business."),
+   _("Nothing personal."),
+   _("Just business."),
+   _("I can already taste the rum."),
+   _("How else am I going to pay off my tab?"),
+   _("Seems you're being shot at."),
+   _("I'm trying to kill you. Is it working?"),
+   _("I'm sorry, I just don't like you."),
+   _("Sorry, but I'm a private tracker."),
+}
+local taunt_list_defensive = {
+   _("You dare attack me?!"),
+   _("You think that you can take me on?"),
+   _("Die!"),
+   _("You'll regret this!"),
+   _("You can either pray now or sit in hell later."),
+   _("Game over, you're dead!"),
+   _("I'm sorry things couldn't work out between us."),
+   _("Knock it off!"),
+   _("Shooting back isn't allowed!"),
+   _("You owe me 20 credits!"),
+   _("You owe me a new paint job!"),
+   _("Fred here said to shoot the red blip."),
+   _("Now you're in for it!"),
+   _("Did you really think you would get away with that?"),
+   _("I just painted this thing!"),
+   _("Rot."),
+   _("Burn."),
+   _("I can't wait to see you burn."),
+   _("Just. Stop. Moving."),
+   _("Die already!"),
+   _("Tell you what, if you can keep dodging for 20 hectoseconds I'll let you live."),
+   _("Stop dodging!"),
+   _("Okay, that's enough of that!"),
+   _("I'm gonna torrent you to bits!"),
+}
 
 function create ()
    local p = ai.pilot()
@@ -35,39 +116,8 @@ function create ()
       mem.bribe_no = _("\"You won't be able to slide out of this one!\"")
    else
       mem.bribe = math.sqrt( p:stats().mass ) * (300 * rnd.rnd() + 850)
-      local bribe_prompt = {
-            _([["It'll cost you %s for me to ignore your pile of rubbish."]]),
-            _([["I'm in a good mood so I'll let you go for %s."]]),
-            _([["Send me %s or you're dead."]]),
-            _([["Pay up %s or it's the end of the line."]]),
-            _([["Your money or your life. %s and make the choice quickly."]]),
-            _([["Money talks bub. %s up front or get off my channel."]]),
-            _([["Shut up and give me your money! %s now."]]),
-            _([["You're either really desperate or really rich. %s or shut up."]]),
-            _([["If you're willing to negotiate I'll gladly take %s to not kill you."]]),
-            _([["You give me %s and I'll act like I never saw you."]]),
-            _([["So this is the part where you pay up or get shot up. Your choice. What'll be, %s or…"]]),
-            _([["Pay up or don't. %s now just means I'll wait till later to collect the rest."]]),
-            _([["This is a toll road, pay up %s or die."]]),
-      }
-      mem.bribe_prompt = string.format(bribe_prompt[ rnd.rnd(1,#bribe_prompt) ], creditstring(mem.bribe))
-      local bribe_paid = {
-            _([["You're lucky I'm so kind."]]),
-            _([["Life doesn't get easier than this."]]),
-            _([["Pleasure doing business."]]),
-            _([["See you again, real soon."]]),
-            _([["I'll be around if you get generous again."]]),
-            _([["Lucky day, lucky day!"]]),
-            _([["And I didn't even have to kill anyone!"]]),
-            _([["See, this is how we become friends."]]),
-            _([["Now if I kill you it'll be just for fun!"]]),
-            _([["You just made a good financial decision today."]]),
-            _([["Know what? I won't kill you."]]),
-            _([["Something feels strange. It's almost as if my urge to kill you has completely dissipated."]]),
-            _([["Can I keep shooting you anyhow? No? You sure? Fine."]]),
-            _([["And it only cost you an arm and a leg."]]),
-      }
-      mem.bribe_paid = bribe_paid[ rnd.rnd(1,#bribe_paid) ]
+      mem.bribe_prompt = string.format(bribe_prompt_list[ rnd.rnd(1,#bribe_prompt_list) ], creditstring(mem.bribe))
+      mem.bribe_paid = bribe_paid_list[ rnd.rnd(1,#bribe_paid_list) ]
    end
 
    -- Deal with refueling
@@ -102,57 +152,9 @@ function taunt ( target, offense )
    -- some taunts
    local taunts
    if offense then
-      taunts = {
-            _("Prepare to be boarded!"),
-            _("Yohoho!"),
-            _("Arr!"),
-            _("What's a ship like you doing in a place like this?"),
-            _("Prepare to have your booty plundered!"),
-            _("Give me your credits or die!"),
-            _("Your ship's mine!"),
-            _("Oh ho ho, what do I see here?"),
-            _("You may want to send that distress signal now."),
-            _("It's time to die."),
-            _("Back so soon?"),
-            _("What? Were you expecting prince charming?"),
-            _("Long time no see."),
-            _("Nothing personal, just business."),
-            _("Nothing personal."),
-            _("Just business."),
-            _("I can already taste the rum."),
-            _("How else am I going to pay off my tab?"),
-            _("Seems you're being shot at."),
-            _("I'm trying to kill you. Is it working?"),
-            _("I'm sorry, I just don't like you."),
-            _("Sorry, but I'm a private tracker."),
-      }
+      taunts = taunt_list_offensive
    else
-      taunts = {
-            _("You dare attack me?!"),
-            _("You think that you can take me on?"),
-            _("Die!"),
-            _("You'll regret this!"),
-            _("You can either pray now or sit in hell later."),
-            _("Game over, you're dead!"),
-            _("I'm sorry things couldn't work out between us."),
-            _("Knock it off!"),
-            _("Shooting back isn't allowed!"),
-            _("You owe me 20 credits!"),
-            _("You owe me a new paint job!"),
-            _("Fred here said to shoot the red blip."),
-            _("Now you're in for it!"),
-            _("Did you really think you would get away with that?"),
-            _("I just painted this thing!"),
-            _("Rot."),
-            _("Burn."),
-            _("I can't wait to see you burn."),
-            _("Just. Stop. Moving."),
-            _("Die already!"),
-            _("Tell you what, if you can keep dodging for 20 hectoseconds I'll let you live."),
-            _("Stop dodging!"),
-            _("Okay, that's enough of that!"),
-            _("I'm gonna torrent you to bits!"),
-      }
+      taunts = taunt_list_defensive
    end
 
    ai.pilot():comm(target, taunts[ rnd.rnd(1,#taunts) ])
