3afd913d08351c3683028cb5cd0f0a04658ed4e7
Edgar Simo-Serra <bobbens@gmail.com>
2021-08-03 14:46:38 +0900

    Thresholds were the other way around.

--- dat/ai/marauder.lua
+++ dat/ai/marauder.lua
@@ -2,9 +2,9 @@ require 'ai.pirate'
 
 mem.armour_run    = 60
 mem.careful       = false
-mem.vulnrambo     = 1.3
-mem.vulnattack    = 0.8
-mem.vulnabort     = 0.4
+mem.vulnrambo     = 2.0
+mem.vulnattack    = 3.0
+mem.vulnabort     = 5.0
 
 create_pirate = create
 
