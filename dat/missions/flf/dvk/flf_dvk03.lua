a3c09873c696e553409cef4a021653f90b60ccc8
Justin Blanchard <UncombedCoconut@gmail.com>
2021-08-06 21:06:54 -0400

    RIP "Empire Lge Attack"

--- dat/missions/flf/dvk/flf_dvk03.lua
+++ dat/missions/flf/dvk/flf_dvk03.lua
@@ -174,7 +174,12 @@ function enter ()
          -- Spawn Dvaered and Empire ships
          dv_fleet = {}
 
-         nf = pilot.addFleet( "Empire Lge Attack", ms:pos(), {ai="empire_norun"} )
+	 local empire_lge_attack = {"Empire Lancelot", "Empire Lancelot", "Empire Lancelot", "Empire Lancelot", "Empire Lancelot", "Empire Lancelot", "Empire Lancelot",
+	                            "Empire Admonisher", "Empire Admonisher", "Empire Admonisher",
+				    "Empire Pacifier",
+				    "Empire Hawking",
+				    "Empire Peacemaker"}
+         nf = addShips( 1, empire_lge_attack, "Empire", ms:pos(), nil, {ai="empire_norun"} )
          for i, j in ipairs( nf ) do
             dv_fleet[ #dv_fleet + 1 ] = j
          end
