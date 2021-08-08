11901992a2b841e4cd801ddae5eda6789491e9fc
nenau-again <renaud.ferrier@gmail.com>
2021-08-08 13:15:01 +0200

    cut beam when used as instant

--- src/pilot_weapon.c
+++ src/pilot_weapon.c
@@ -49,6 +49,7 @@ static int pilot_weapSetFire( Pilot *p, PilotWeaponSet *ws, int level );
 static int pilot_shootWeaponSetOutfit( Pilot* p, PilotWeaponSet *ws, const Outfit *o, int level, double time );
 static int pilot_shootWeapon( Pilot* p, PilotOutfitSlot* w, double time );
 static void pilot_weapSetUpdateRange( const Pilot *p, PilotWeaponSet *ws );
+unsigned int pilot_weaponSetShootStop( Pilot* p, PilotWeaponSet *ws, int level );
 
 
 /**
@@ -274,6 +275,8 @@ void pilot_weapSetUpdate( Pilot* p )
       if (ws->type == WEAPSET_TYPE_WEAPON) {
          if (ws->active)
             pilot_weapSetFire( p, ws, -1 );
+         else
+            pilot_weaponSetShootStop( p, ws, -1 );
       }
    }
 }
@@ -792,12 +795,31 @@ int pilot_shoot( Pilot* p, int level )
  */
 void pilot_shootStop( Pilot* p, int level )
 {
-   int i, recalc;
    PilotWeaponSet *ws;
-   PilotOutfitSlot *slot;
 
-   /* Get active set. */
+   /* Find active set. */
    ws = pilot_weapSet( p, p->active_set );
+
+   /* Stop and see if must recalculate. */
+   if (pilot_weaponSetShootStop( p, ws, level ))
+      pilot_calcStats( p );
+}
+
+
+/**
+ * @brief Have pilot stop shooting a given weaponset.
+ *
+ * Only really deals with beam weapons.
+ *
+ *    @param p Pilot that was shooting.
+ *    @param level Level of the shot.
+ *    @param ws Weapon Set to stop shooting
+ *    @return 1 if some outfit has been deactivated
+ */
+unsigned int pilot_weaponSetShootStop( Pilot* p, PilotWeaponSet *ws, int level )
+{
+   int i, recalc;
+   PilotOutfitSlot *slot;
 
    /* Stop all beams. */
    recalc = 0;
@@ -839,9 +861,7 @@ void pilot_shootStop( Pilot* p, int level )
       }
    }
 
-   /* Must recalculate. */
-   if (recalc)
-      pilot_calcStats( p );
+   return recalc;
 }
 
 
