05fc58106a9fb7e098bb97f559c696fcde2794f5
Edgar Simo-Serra <bobbens@gmail.com>
2021-08-31 18:42:05 +0900

    Better visualization for editing.

--- src/dev_uniedit.c
+++ src/dev_uniedit.c
@@ -498,9 +498,11 @@ static void uniedit_renderFactionDisks( double x, double y, double r )
       /* draws the disk representing the faction */
       sr = 5.*sqrt(presence) * uniedit_zoom * 0.5;
 
-      glUseProgram(shaders.factiondisk.program);
-      glUniform1f(shaders.factiondisk.paramf, r / sr );
-      gl_renderShader( tx, ty, sr, sr, 0., &shaders.factiondisk, &c, 1 );
+      //glUseProgram(shaders.factiondisk.program);
+      //glUniform1f(shaders.factiondisk.paramf, r / sr );
+      //gl_renderShader( tx, ty, sr, sr, 0., &shaders.factiondisk, &c, 1 );
+      (void) r;
+      gl_drawCircle( tx, ty, sr, &c, 1 );
    }
 }
 
