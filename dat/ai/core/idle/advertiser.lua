c769f1817b093f6e03c4677beac8259faeb13f69
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-06 10:55:08 +0900

    Less ad spam.

--- dat/ai/core/idle/advertiser.lua
+++ dat/ai/core/idle/advertiser.lua
@@ -3,7 +3,12 @@ require 'ai.core.idle.civilian'
 local idle_generic = idle
 
 function idle ()
-   ai.pilot():broadcast(mem.ad)
+   local lastspammed = mem.adspamlast or 0
+   local curtime = naev.ticks()
+   if curtime - lastspammed > 15e3 then
+      ai.pilot():broadcast(mem.ad)
+      mem.adspamlast = curtime
+   end
    return idle_generic ()
 end
 
