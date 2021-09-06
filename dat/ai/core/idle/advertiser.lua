51a92258bc1c8f9f8a1548922fc149f48356fb43
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-06 13:27:25 +0900

    Accidently left in debug stuff.

--- dat/ai/core/idle/advertiser.lua
+++ dat/ai/core/idle/advertiser.lua
@@ -5,7 +5,6 @@ control_funcs.loiter = function ()
    local curtime = naev.ticks() / 1e3
    local delay = mem.adspamdelay or 15
 
-   print( string.format( "curtime-lastspammed = %.1f, delay = %.1f", curtime-lastspammed, delay) )
    if curtime - lastspammed > delay then
       ai.pilot():broadcast(mem.ad)
       mem.adspamlast = curtime
