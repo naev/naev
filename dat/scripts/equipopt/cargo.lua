5ec548cbb81454c39ff96a6ca888313a2b97ed64
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-07 22:17:28 +0900

    Tweaked cargo values.

--- dat/scripts/equipopt/cargo.lua
+++ dat/scripts/equipopt/cargo.lua
@@ -1,8 +1,11 @@
 local cargo = {}
 
+-- We assume standard cargo doesn't change over time
+local avail_cargo = commodity.getStandard()
+
 function cargo.add( p )
-   local avail_cargo = commodity.getStandard()
    if #avail_cargo <= 0 then
+      warn(_("No standard commodities!"))
       return
    end
 
@@ -10,9 +13,9 @@ function cargo.add( p )
    local ncargo
    local r = rnd.rnd()
    local cf = p:cargoFree()
-   if r < 0.6 or cf<30 then
+   if r < 0.6 or cf<50 then
       ncargo = 1
-   elseif r < 0.85 or cf < 60 then
+   elseif r < 0.85 or cf < 200 then
       ncargo = 2
    else
       ncargo = 3
@@ -21,7 +24,8 @@ function cargo.add( p )
    -- Add the cargo
    local q = 0
    for i=1,ncargo do
-      local amount = rnd.rnd( 0, math.floor(p:cargoFree()) )
+      local cf = p:cargoFree()
+      local amount = rnd.rnd( math.floor(0.5*cf/ncargo), math.floor(cf) )
       q = q + p:cargoAdd( avail_cargo[ rnd.rnd( 1, #avail_cargo ) ]:nameRaw(), amount )
    end
    return q
