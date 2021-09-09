21f98e74192751e5f0bf14aa087ce519406e9ce4
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-09 12:56:21 +0900

    Fighters don't kill by default.

--- dat/ai/escort.lua
+++ dat/ai/escort.lua
@@ -3,6 +3,8 @@ require 'ai.core.control.escort'
 
 -- Do not distress
 mem.distress = false
+-- TODO the options below here should be player-configurable
 mem.aggressive = true
 mem.enemyclose = 3000
 mem.leadermaxdist = 8000
+mem.atk_kill = false
