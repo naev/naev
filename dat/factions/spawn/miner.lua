e368da0722aac2cb0489a8785fd7e04e66f133db
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-14 13:28:59 +0900

    Rhino miners.

--- dat/factions/spawn/miner.lua
+++ dat/factions/spawn/miner.lua
@@ -3,17 +3,20 @@ local scom = require "factions.spawn.lib.common"
 local sllama   = ship.get("Llama")
 local skoala   = ship.get("Koala")
 local smule    = ship.get("Mule")
+local srhino   = ship.get("Rhino")
 
 -- @brief Spawns a small group of miners
-function spawn_patrol ()
+function spawn_lone_miner ()
    local pilots = {}
    local r = rnd.rnd()
-   if r < 0.5 then
+   if r < 0.4 then
       scom.addPilot( pilots, sllama, {name=_("Miner Llama"), ai="miner"}  )
-   elseif r < 0.8 then
+   elseif r < 0.7 then
       scom.addPilot( pilots, skoala, {name=_("Miner Koala"), ai="miner"} )
-   else
+   elseif r < 0.9 then
       scom.addPilot( pilots, smule,  {name=_("Miner Mule"), ai="miner"} )
+   else
+      scom.addPilot( pilots, srhino,  {name=_("Miner Rhino"), ai="miner"} )
    end
 
    return pilots
@@ -25,7 +28,7 @@ function create ( max )
    local weights = {}
 
     -- Create weights for spawn table
-   weights[ spawn_patrol  ] = 100
+   weights[ spawn_lone_miner  ] = 100
 
    return scom.init( fminer, weights, max )
 end
