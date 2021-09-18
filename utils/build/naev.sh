e797ac79e57ff8d9beff0015ac90e2d976dee3e3
Jack Greiner <jack@emoss.org>
2021-09-18 01:38:46 -0400

    Add ability to avoid gdb with naev.sh Was working on something more fancy but this works for now.

--- utils/build/naev.sh
+++ utils/build/naev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# export WITHGDB=NO to avoid using GDB where it is a hinderance.
+
 "@source_root@/meson.sh" compile -C "@build_root@" naev-gmo
 for mo_path in "@build_root@"/po/*.gmo; do
     mo_name="$(basename "$mo_path")"
@@ -10,10 +12,14 @@ done
 grep -q '%<PRI' "@build_root@"/po/*.gmo && echo "***WARNING: Naev can't translate 'sysdep' strings like %<PRIu64>. Try %.0f?"
 
 wrapper() {
-   if type "gdb" > /dev/null; then
-      gdb -x "@source_root@/.gdbinit" --args "$@"
-   else
+   if [[ "$WITHGDB" =~ "NO" ]]; then
       "$@"
+   else
+      if type "gdb" > /dev/null; then
+         gdb -x "@source_root@/.gdbinit" --args "$@"
+      else
+         "$@"
+      fi
    fi
 }
 
