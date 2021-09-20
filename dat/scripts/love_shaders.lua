0c4b547f685a9d3a56fe5b976152ff64df8f6e74
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-20 18:17:26 +0900

    Tweaked hologram shader to be more fine-grained.

--- dat/scripts/love_shaders.lua
+++ dat/scripts/love_shaders.lua
@@ -257,7 +257,7 @@ A hologram effect, mainly meant for VN characters.
 function love_shaders.hologram( params )
    params = params or {}
    strength = params.strength or 1.0
-   local pixelcode = [[
+   local pixelcode = string.format([[
 #include "lib/math.glsl"
 #include "lib/blur.glsl"
 #include "lib/blend.glsl"
@@ -273,84 +273,68 @@ float onOff(float a, float b, float c)
 
 vec4 effect( vec4 color, Image tex, vec2 uv, vec2 screen_coords )
 {
-   const vec3 bluetint  = vec3(0.3, 0.5, 0.8);
-   const float brightness = 0.4;
-   const float contrast = 2.0;
-#ifdef HOLOGRAM_STRONG
-   const float strength = 64.0; // 64.0
-   const float shadowspeed = 0.3;
-   const float shadowrange = 0.35;
-   const float shadowcount = 5.0;
-   const float highlightspeed = 0.3;
-   const float highlightrange = 0.35;
-   const float highlightcount = 5.0;
-   const float wobbleamplitude = 0.2;
-   const float wobblespeed = 0.3;
-   const float bluramplitude = 12.0;
-   const float blurspeed = 0.31;
-   const float scanlinemean = 0.85;
-   const float scanlineamplitude = 0.3;
-   const float scanlinespeed = 5.0;
-#else /* HOLOGRAM_STRONG */
-   const float strength = 32.0; // 64.0
-   const float shadowspeed = 0.2;
-   const float shadowrange = 0.2; // 0.35
-   const float shadowcount = 5.0;
-   const float highlightspeed = 0.2;
-   const float highlightrange = 0.2; // 0.35
-   const float highlightcount = 5.0;
-   const float wobbleamplitude = 0.2; // 0.4
-   const float wobblespeed = 0.9; // 0.3
-   const float bluramplitude = 9.0;
-   const float blurspeed = 0.13;
-   const float scanlinemean = 0.9;
-   const float scanlineamplitude = 0.2;
-   const float scanlinespeed = 3.0;
-#endif /* HOLOGRAM_STRONG */
+   const float strength = %f; /* Simple parameter to control everything. */
+
+   /* Strength-based constants. */
+   const float BLURAMPLITUDE     = 6.0 * strength;
+   const float BLURSPEED         = 0.15 * strength;
+   const float HIGHLIGHTSPEED    = 0.15 + 0.15 * strength;
+   const float HIGHLIGHTRANGE    = 0.25 + 0.15 * strength;
+   const float HIGHLIGHTCOUNT    = 5.0 + 2.5 * strength;
+   const float SHADOWSPEED       = 0.17 + 0.14 * strength;
+   const float SHADOWRANGE       = 0.07 + 0.05 * strength;
+   const float SHADOWCOUNT       = 4.7 + 2.3 * strength;
+   const float GRAINSTRENGTH     = 32.0 + 32.0 * strength;
+   const float WOBBLEAMPLITUDEX  = 0.005 * strength;
+   const float WOBBLEAMPLITUDEY  = 0.025 * strength;
+   const float WOBBLESPEED       = 0.1 + 0.1 * strength;
+
+   /* Truly constant values. */
+   const vec3 bluetint           = vec3( 0.075, 0.215, 0.604 );/* Gamma: vec3(0.3, 0.5, 0.8); */
+   const float BRIGHTNESS        = 0.5;
+   const float CONTRAST          = 2.0;
+   const float SCANLINEMEAN      = 0.9;
+   const float SCANLINEAMPLITUDE = 0.2;
+   const float SCANLINESPEED     = 3.0;
 
    /* Get the texture. */
-   vec2 look = uv;
-   float window = 1./(1.+20.*(look.y-mod(u_time/4.,1.))*(look.y-mod(u_time/4.,1.)));
-   look.x = look.x + 0.01 * sin(look.y*10. + u_time)*onOff(4.0,4.0,wobblespeed)*(1.+cos(u_time*80.))*window;
-   float vShift = wobbleamplitude * onOff(2.0,3.0,M_PI*wobblespeed)*(sin(u_time)*sin(u_time*20.)+(0.5 + 0.1*sin(u_time*200.0)*cos(u_time)));
+   vec2 look      = uv;
+   float tmod     = mod(u_time/4.0,1.0);
+   float window   = 1.0 / (1.0+20.0*(look.y-tmod)*(look.y-tmod));
+   look.x += WOBBLEAMPLITUDEX * sin(look.y*10.0 + u_time)*onOff(4.0,4.0,WOBBLESPEED)*(1.0+cos(u_time*80.0))*window;
+   float vShift   = WOBBLEAMPLITUDEY * onOff(2.0,3.0,M_PI*WOBBLESPEED)*(sin(u_time)*sin(u_time*20.0)+(0.5 + 0.1*sin(u_time*200.0)*cos(u_time)));
    look.y = mod( look.y + vShift, 1.0 );
 
    /* Blur a bit. */
-   vec4 texcolor = color * texture( tex, look );
-   float blurdir = snoise( vec2( blurspeed*u_time, 0 ) );
-   vec2 blurvec = bluramplitude * vec2( cos(blurdir), sin(blurdir) );
-   vec4 blurbg = blur9( tex, look, love_ScreenSize.xy, blurvec );
-   texcolor.rgb = blendSoftLight( texcolor.rgb, blurbg.rgb );
-   texcolor.a = max( texcolor.a, blurbg.a );
+   vec4 texcolor  = color * texture( tex, look );
+   float blurdir  = snoise( vec2( BLURSPEED*u_time, 0 ) );
+   vec2 blurvec   = BLURAMPLITUDE * vec2( cos(blurdir), sin(blurdir) );
+   vec4 blurbg    = blur9( tex, look, love_ScreenSize.xy, blurvec );
+   texcolor.rgb   = blendSoftLight( texcolor.rgb, blurbg.rgb );
+   texcolor.a     = max( texcolor.a, blurbg.a );
 
    /* Drop to greyscale while increasing brightness and contrast */
-   //float greyscale = dot( texcolor.xyz, vec3( 0.2126, 0.7152, 0.0722 ) ); // standard
-   float greyscale = rgb2lum( texcolor.rgb ); // percieved
-   texcolor.xyz = contrast*vec3(greyscale) + brightness;
+   float greyscale= rgb2lum( texcolor.rgb ); // percieved
+   texcolor.xyz   = CONTRAST * vec3(greyscale) + BRIGHTNESS;
 
    /* Shadows. */
-   float shadow = 1.0 - shadowrange + (shadowrange * sin((uv.y + (u_time * shadowspeed)) * shadowcount));
-   texcolor.xyz *= shadow;
+   float shadow   = 1.0 + SHADOWRANGE * sin((uv.y + (u_time * SHADOWSPEED)) * SHADOWCOUNT);
+   texcolor.xyz  *= shadow;
 
    /* Highlights */
-   float highlight = 1.0 - highlightrange + (highlightrange * sin((uv.y + (u_time * -highlightspeed)) * highlightcount));
-   texcolor.xyz += highlight;
+   float highlight= HIGHLIGHTRANGE * (0.5 + 0.5 * sin((uv.y + (u_time * -HIGHLIGHTSPEED)) * HIGHLIGHTCOUNT) );
+   texcolor.xyz  += highlight;
 
    // Other effects.
    float x = (uv.x + 4.0) * (uv.y + 4.0) * u_time * 10.0;
-   float grain = 1.0 - (mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01) - 0.005) * strength;
+   float grain = 1.0 - (mod((mod(x, 13.0) + 1.0) * (mod(x, 123.0) + 1.0), 0.01) - 0.005) * GRAINSTRENGTH;
    float flicker = max(1.0, random(u_time * uv) * 1.5);
-   //float scanlines = 0.85 * clamp(sin(uv.y * 400.0), 0.25, 1.0) * random(uv * vec2(0,sin(u_time * 0.2)) * 0.1) * 2.0;
-   float scanlines = scanlinemean + scanlineamplitude*step( 0.5, sin(0.5*screen_coords.y + scanlinespeed*u_time)-0.1 );
+   float scanlines = SCANLINEMEAN + SCANLINEAMPLITUDE*step( 0.5, sin(0.5*screen_coords.y + SCANLINESPEED*u_time)-0.1 );
 
    texcolor.xyz *= grain * flicker * scanlines * bluetint;
    return texcolor * color;
 }
-]]
-
-   if strength  >= 2.0 then
-      pixelcode = "#define HOLOGRAM_STRONG 1\n"..pixelcode
-   end
+]], strength )
 
    local shader = graphics.newShader( pixelcode, _vertexcode )
    shader._dt = 1000 * love_math.random()
