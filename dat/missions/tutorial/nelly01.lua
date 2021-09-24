3ebefeaa746f61e7f46c945af5d15c755126aca9
Edgar Simo-Serra <bobbens@gmail.com>
2021-09-24 13:15:33 +0900

    More updates.

--- dat/missions/tutorial/nelly01.lua
+++ dat/missions/tutorial/nelly01.lua
@@ -1,6 +1,6 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
-<mission name="Tutorial">
+<mission name="Helping Nelly Out 1">
  <flags>
   <unique />
  </flags>
@@ -48,8 +48,8 @@ local lmisn = require "lmisn"
 --]]
 misn_state = nil
 
-cargo_type = "Food"
-cargo_q = 5
+cargo_type  = "Food"
+cargo_q     = 5
 reward_amount = 40e3
 outfit_buy = outfit.get("Ion Cannon")
 
@@ -92,18 +92,44 @@ function accept ()
    vn.scene()
    local nel = tutnel.vn_nelly()
 
-   nel(_([[]]))
+   nel(fmt.f(_([[The lone individual lightens up when you near.
+"Say, you look like a pilot with a working ship. I'm in a bit of a mess. You see, I was supposed to deliver some {cargoname} to {pntname} in the {sysname} system, but my ship broke down and I don't think I'll be able to deliver it any time soon. Would you be willing to help me take the cargo there and come back? I'll pay you your fair share."]]),
+      {cargoname=cargo_type, pntname=destpnt:name(), sysname=:destsys:name()}))
+
    vn.menu{
-      {_("Help Nelly out"), "accept"},
+      {_("Help them out"), "accept"},
       {_("Decline to help"), "decline"},
    }
 
    vn.label("decline")
-   nel(_([[]]))
+   nel(_([[They look dejected.
+"That's a shame. If you change your mind I'll be waiting here."]]))
    vn.done()
-   
+
+   vn.label("nofreespace")
+   nel(fmt.f(_([["You don't have enough free space to help me out. You'll need to carry {amount} of {cargo}. Please make free space by either selling unwanted cargo, or getting rid of it from the info menu which you can open with {infokey}."]]),
+         {amount=fmt.tonnes(cargo_q), cargo=cargo_type, infokey=tut.getKey("info")}))
+   vn.done()
+
    vn.label("accept")
-   nel(_([[]]))
+   vn.func( function ()
+      if player.pilot():cargoFree() < cargo_q then
+         vn.jump("nofreespace")
+      end
+   end )
+   nel(_([["Great! My name is Nelly. Glad to make your acquaintance. I'll have the dock workers load up your ship and we can be off. This should be a piece of cake."
+They cock their head a bit at you.
+"Say, you wouldn't happen to be a novice pilot?"]]))
+   vn.menu{
+      {_([["Yes"]]), "novice_yes"},
+      {_([["No"]]), "novice_no"},
+   }
+
+   vn.label("novice_yes")
+   nel(_([["I knew it! You seem to have a nice fresh aura around you. Reminds me of back in the day when I was starting out. Starting out can be a bit tricky, so I hope you don't mind if I give you some advice on the road."]]))
+
+   vn.label("novice_no")
+   nel(_([["Weird. I could have sworn you had some sort of new pilot aura around you. Must have been my imagination. Let's get going!"]]))
 
    vn.run()
 
@@ -114,7 +140,12 @@ function accept ()
 
    misn_state = 0
 
-   misn.cargoAdd( "Food"
+   cargo_id = misn.cargoAdd( cargo_type, cargo_q )
+
+   misn.osdCreate( _("Helping Nelly Out"), {
+      fmt.f(_("Deliver cargo to {pntname} in {sysname}"), {sysname=destsys:name(), pntname=destpnt:name()} ),
+      fmt.f(_("Return to {pntname} in {sysname}"), {syname=retsys:name(), pntname=retpnt:name()} ),
+   } )
 
    hook.enter("enter")
    hook.land("land")
@@ -123,12 +154,18 @@ end
 function land ()
    local pcur = planet.cur()
    if misn_state==1 and pcur == destsys then
+      -- Delivered Cargo
       vn.clear()
       vn.scene()
       local nel = tutnel.vn_nelly()
       vn.run()
 
+      -- Get rid of cargo
+      misn.cargoRm( cargo_id )
+      misn_state = 2
+
    elseif misn_state==3 and pcur == retsys then
+      -- Finished mission
       vn.clear()
       vn.scene()
       local nel = tutnel.vn_nelly()
@@ -139,6 +176,27 @@ end
 
 function enter ()
    if misn_state==0 then
-      -- TODO add explanation about info window
+      vn.clear()
+      vn.scene()
+      local nel = tutnel.vn_nelly()
+      vn.na(fmt.f(_("After the dock workers load the cargo on your ship, you take off with Nelly aboard. On to {sysname}!"), {sysname:name()}))
+      nel(_([[Just after taking off Nelly pipes up.
+"Say, are you familiar with the information window? It shows all the important things about your ship and current missions."]]))
+      vn.menu{
+         {_("Hear about the Info window"), "info_yes"},
+         {_("Already know"), "info_no"},
+      }
+
+      vn.label("info_no")
+      nel(_([["OK, great! Then let's get going!"]]))
+      vn.done()
+
+      vn.label("info_yes")
+      nel(_([["The information window, which you can open with {infokey}, is critical to managing your ship and finding out where to go."]]))
+      vn.run()
+
+   elseif misn_state==2 and system.cur() == retsys then
+      -- TODO Show and talk about disabled ship
+
    end
 end
