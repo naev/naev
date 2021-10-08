946f27fb77cc7ffa84a3357cff89e3b8ecd23029
Edgar Simo-Serra <bobbens@gmail.com>
2021-10-08 19:37:29 +0900

    Improved the condition checks for the shadow comm missions.

--- dat/events/neutral/shadow_comm.lua
+++ dat/events/neutral/shadow_comm.lua
@@ -1,20 +1,20 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
 <event name="Shadowcomm">
-  <trigger>enter</trigger>
-  <chance>3</chance>
-  <cond>system.cur():presence("hostile") &lt; 300 and player.misnDone("Shadowrun") and not (player.misnDone("Shadow Vigil") or player.misnActive("Shadow Vigil")) and not (system.cur() == system.get("Pas"))</cond>
-  <flags>
-  </flags>
-  <notes>
-   <done_misn name="Shadowrun"/>
-   <campaign>Shadow</campaign>
-   <tier>3</tier>
-  </notes>
- </event>
- --]]
+ <trigger>enter</trigger>
+ <chance>3</chance>
+ <cond>system.cur():presence("hostile") &lt; 300 and player.misnDone("Shadowrun") and not (player.misnDone("Shadow Vigil") or player.misnActive("Shadow Vigil")) and not (system.cur() == system.get("Pas")) and system.cur():presence("Independent") &gt; 100</cond>
+ <flags>
+ </flags>
+ <notes>
+  <done_misn name="Shadowrun"/>
+  <campaign>Shadow</campaign>
+  <tier>3</tier>
+ </notes>
+</event>
+--]]
 --[[
--- Comm Event for the Shadow missions
+   Comm Event for the Shadow missions
 --]]
 
 require "proximity"
@@ -33,58 +33,59 @@ text[1] = _([["Greetings, %s," the pilot of the Vendetta says to you as soon as 
 
 log_text = _([[Someone has invited you to meet with her in the Pas system, supposedly an acquaintance of yours. The pilot who told you this said that there's no rush, "but I suggest you go see her at the earliest opportunity".]])
 
-yesnotxt = _("Do you intend to respond to the invitation?")
-
 
 function create ()
-     -- Claim: duplicates the claims in the mission.
-    misssys = {system.get("Qex"), system.get("Shakar"), system.get("Borla"), system.get("Doranthex")}
-    if not evt.claim(misssys) then
-        abort()
-    end
+   -- Make sure system isn't claimed, but we don't claim it
+   if not evt.claim( system.cur(), true ) then evt.finish() end
 
-    sysname = "Pas"
+    -- Claim: test the claims in the mission.
+   misssys = {system.get("Qex"), system.get("Shakar"), system.get("Borla"), system.get("Doranthex")}
+   if not evt.claim( misssys, true ) then
+      abort()
+   end
 
-    -- Create a Vendetta who hails the player after a bit
-    hail_time = nil
-    vendetta = pilot.add( "Vendetta", "Four Winds", nil, _("Four Winds Vendetta"), {ai="trader"} )
-    vendetta:control()
-    vendetta:follow(player.pilot())
-    hook.timer(0.5, "proximityScan", {focus = vendetta, funcname = "hailme"})
+   sysname = "Pas"
 
-    -- Clean up on events that remove the Vendetta from the game
-    hook1 = hook.pilot(vendetta, "jump", "finish")
-    hook2 = hook.pilot(vendetta, "death", "finish")
-    hook3 = hook.land("finish")
-    hook4 = hook.jumpout("finish")
+   -- Create a Vendetta who hails the player after a bit
+   hail_time = nil
+   vendetta = pilot.add( "Vendetta", "Four Winds", nil, _("Four Winds Vendetta"), {ai="trader"} )
+   vendetta:control()
+   vendetta:follow(player.pilot())
+   hook.timer(0.5, "proximityScan", {focus = vendetta, funcname = "hailme"})
+
+   -- Clean up on events that remove the Vendetta from the game
+   hook1 = hook.pilot(vendetta, "jump", "finish")
+   hook2 = hook.pilot(vendetta, "death", "finish")
+   hook3 = hook.land("finish")
+   hook4 = hook.jumpout("finish")
 end
 
 -- Make the ship hail the player
 function hailme()
-    vendetta:hailPlayer()
-    hailhook = hook.pilot(vendetta, "hail", "hail")
+   vendetta:hailPlayer()
+   hailhook = hook.pilot(vendetta, "hail", "hail")
 end
 
 -- Triggered when the player hails the ship
 function hail(p)
-    hook.rm(hailhook)
+   hook.rm(hailhook)
 
-    tk.msg(title[1], text[1]:format(player.name(), sysname, sysname))
-    player.commClose()
-    vendetta:control()
-    vendetta:hyperspace()
+   tk.msg(title[1], text[1]:format(player.name(), sysname, sysname))
+   player.commClose()
+   vendetta:control()
+   vendetta:hyperspace()
 
-    if tk.yesno( "", yesnotxt ) then
-        shadow.addLog( log_text )
-        naev.missionStart("Shadow Vigil")
-        evt.finish()
-    end
+   if tk.yesno( "", _("Do you intend to respond to the invitation?") ) then
+      shadow.addLog( log_text )
+      naev.missionStart("Shadow Vigil")
+      evt.finish()
+   end
 end
 
 -- Clean up
 function finish()
-    if hailhook then
-        hook.rm(hailhook)
-    end
-    evt.finish()
+   if hailhook then
+      hook.rm(hailhook)
+   end
+   evt.finish()
 end
