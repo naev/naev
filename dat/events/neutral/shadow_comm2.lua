277134099af5ebc0fb9094bbb9eb14f7480c367a
Edgar Simo-Serra <bobbens@gmail.com>
2021-10-08 19:37:29 +0900

    Improved the condition checks for the shadow comm missions.

--- dat/events/neutral/shadow_comm2.lua
+++ dat/events/neutral/shadow_comm2.lua
@@ -1,38 +1,38 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
 <event name="Shadowcomm2">
-  <trigger>enter</trigger>
-  <chance>3</chance>
-  <cond>system.cur():presence("hostile") &lt; 300 and player.misnDone("Shadow Vigil") and not (player.misnDone("Dark Shadow") or var.peek("darkshadow_active") == true)</cond>
-  <flags>
-  </flags>
-  <notes>
-   <done_misn name="Shadow Vigil"/>
-   <campaign>Shadow</campaign>
-  </notes>
- </event>
- --]]
---[[
--- Comm Event for the Shadow missions
+ <trigger>enter</trigger>
+ <chance>3</chance>
+ <cond>system.cur():presence("hostile") &lt; 300 and player.misnDone("Shadow Vigil") and not (player.misnDone("Dark Shadow") or var.peek("darkshadow_active") == true) and system.cur():presence("Independent") &gt; 100</cond>
+ <flags>
+ </flags>
+ <notes>
+  <done_misn name="Shadow Vigil"/>
+  <campaign>Shadow</campaign>
+ </notes>
+</event>
 --]]
-
+--[[
+   Comm Event for the Shadow missions
+--]]
 require "proximity"
 
 function create ()
-    hailie = hook.timer(20.0, "hailme")
+   -- Make sure system isn't claimed, but we don't claim it
+   if not evt.claim( system.cur(), true ) then evt.finish() end
 
-    landhook = hook.land("finish")
-    jumpouthook = hook.jumpout("finish")
+   hook.timer(20.0, "hailme")
+   hook.land("finish")
+   hook.jumpout("finish")
 end
 
 -- Make the ship hail the player
 function hailme()
-    naev.missionStart("Dark Shadow")
-    player.commClose()
-    evt.finish()
+   naev.missionStart("Dark Shadow")
+   player.commClose()
+   evt.finish()
 end
 
 function finish()
-    hook.rm(hailie)
-    evt.finish()
+   evt.finish()
 end
