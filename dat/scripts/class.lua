298232f9187bbcc4f0859ba24f82ae0fc95b2dcf
Justin Blanchard <UncombedCoconut@gmail.com>
2021-10-10 10:31:32 -0400

    De-sugar class.lua definitions to avoid unused-self warnings.

--- dat/scripts/class.lua
+++ dat/scripts/class.lua
@@ -6,7 +6,7 @@ function class.inheritsFrom( baseClass )
     local new_class = {}
     local class_mt = { __index = new_class }
 
-    function new_class:new()
+    function new_class.new( _self )
         local newinst = {}
         setmetatable( newinst, class_mt )
         return newinst
@@ -19,17 +19,17 @@ function class.inheritsFrom( baseClass )
     -- Implementation of additional OO properties starts here --
 
     -- Return the class object of the instance
-    function new_class:class()
+    function new_class.class( _self )
         return new_class
     end
 
     -- Return the super class object of the instance
-    function new_class:superClass()
+    function new_class.superClass( _self )
         return baseClass
     end
 
     -- Return true if the caller is an instance of theClass
-    function new_class:isa( theClass )
+    function new_class.isa( _self, theClass )
         local b_isa = false
 
         local cur_class = new_class
