533a3ae3dcf70146c10d957790c86cd6f85a5ea8
Edgar Simo-Serra <bobbens@gmail.com>
2021-10-16 22:37:13 +0900

    Revamped advertiser to be a bit faster hopefully.

--- dat/ai/advertiser.lua
+++ dat/ai/advertiser.lua
@@ -2,9 +2,56 @@ require 'ai.core.core'
 require 'ai.core.idle.advertiser'
 require 'ai.core.misc.distress'
 local fmt = require "format"
+local mt = require 'merge_tables'
 
 mem.lanes_useneutral = true
 mem.simplecombat = true
+
+local ads_generic = {
+   _("Fly safe, fly Milspec."),
+   _("Reynir's Hot Dogs: enjoy the authentic taste of tradition."),
+   _("Everyone is faster than light, but only Tricon engines are faster than thought!"),
+   _("Dare excellence! Dare Teracom rockets!"),
+   _("Most people are ordinary. For the others, Nexus designed the Shark fighter."),
+   _("Never take off without your courage. Never take off without your Vendetta."),
+   _("Unicorp: low price and high quality!"),
+   _("Life is short, spend it at Minerva Station in the Limbo System!"),
+   _("Insuperable Sleekness. Introducing the Krain Industries Starbridge."),
+}
+local ads_empire = {
+   _("Do you love your Emperor as much as he loves you?"),
+   _("You're quick and dependable? The Emperor needs you in the Armada!"),
+   _("Made money hauling cargo? Remember to file your EW-59831 every cycle or face the consequences."),
+   _("Need help with your EW-59831 form? Try Bob's Bureaucratic Bazaar at Semper!"),
+   _("Not filing your EE-91726 for unauthorized pet iguanas is a crime. Report to your Empire Animal Bureau now."),
+   _("Keep your documents properly filed. Unicorp Filing Cabinets."),
+}
+local ads_dvaered = {
+   _("Only your hard work will save the Dvaered economy!"),
+   _("Together, we will beat the economic crisis!"),
+   _("Bet on Totoran and win incredible sums thanks to the Crimson Gauntlet!"),
+   _("Mace rockets lacking shine? Try Lady Killington's premium rocket polish!"),
+   _("Other warlords not letting you enjoy bloodshed? Join Lord Easytrigger's battalion today!"),
+}
+local ads_soromid = {
+   _("Remember Sorom."),
+   _("Special offer on Crow: buy one IR-eye, and the second comes for free!"),
+   _("Looking to modify an entire species? Visit Dr. Hu's Gene Clinic at Point Zero Station!"),
+   _("Endogenous DNA damage hampering your Gene Drive? Drop by your local Chromosomal Rearrangement Laboratory for a check-up."),
+   _("Visit Bohr Laboratory for all your epistatic mutation woes. 10 locations and counting!"),
+   _("Worried about your bio-ship adenosine triphosphate output? Leave it to ATP Specialists!"),
+   _("Preemptively treat your bio-ship for space fleas with Dr. Nastya's Ointment!"),
+}
+local ads_zalek = {
+   _("Want to solve a large-dimensional stochastic PDE? The LMKSTD method is what you need!"),
+   _("Love non-convex minimization? Join Ruadan's Computation Science Lab!"),
+   _("Keeping your drones in top shape. Prof. Imarisha's Robotic Laboratory."),
+   _("Interested in Genetic Lifeforms research? Apply to Interstice Science!"), -- Reference to Aperture Science (synonyms) from Portal
+   _("Want to learn about Anti-Mass Spectometry? Join Ebony Plateau today!"), -- Reference to Black Mesa (synonyms) from Half-Life
+}
+local ads_sirius = {
+   _("Want a new look? Try Verrill's Ceremonial Robes at Burnan!"),
+}
 
 function create ()
    -- Credits.
@@ -24,39 +71,22 @@ function create ()
    mem.refuel_msg = string.format(_([["I'll supply your ship with fuel for %s."]]),
          fmt.credits(mem.refuel))
 
-   -- Selects an advertiser message
-   local msg = {
-      _("Fly safe, fly Milspec."),
-      _("Reynir's Hot Dogs: enjoy the authentic taste of tradition."),
-      _("Everyone is faster than light, but only Tricon engines are faster than thought!"),
-      _("Dare excellence! Dare Teracom rockets!"),
-      _("Most people are ordinary. For the others, Nexus designed the Shark fighter."),
-      _("Never take off without your courage. Never take off without your Vendetta."),
-      _("Unicorp: low price and high quality!"),
-      _("Life is short, spend it at Minerva Station in the Limbo System!"),
-      _("Insuperable Sleekness. Introducing the Krain Industries Starbridge."),
-   }
+   -- Set up potential advertiser messages in msg variable
+   local msg = mt.merge_tables( {}, ads_generic )
 
    -- Faction specific messages
    local fpres = system.cur():presences()
 
+   -- Empire messages
    local fem = fpres["Empire"] or 0
    if fem > 1 then
-      table.insert(msg, _("Do you love your Emperor as much as he loves you?"))
-      table.insert(msg, _("You're quick and dependable? The Emperor needs you in the Armada!"))
-      table.insert(msg, _("Made money hauling cargo? Remember to file your EW-59831 every cycle or face the consequences."))
-      table.insert(msg, _("Need help with your EW-59831 form? Try Bob's Bureaucratic Bazaar at Semper!"))
-      table.insert(msg, _("Not filing your EE-91726 for unauthorized pet iguanas is a crime. Report to your Empire Animal Bureau now."))
-      table.insert(msg, _("Keep your documents properly filed. Unicorp Filing Cabinets."))
+      msg = mt.merge_tables( msg, ads_empire )
    end
 
+   -- Dvaered messages
    local fdv = fpres["Dvaered"] or 0
    if fdv > 1 then
-      table.insert(msg, _("Only your hard work will save the Dvaered economy!"))
-      table.insert(msg, _("Together, we will beat the economic crisis!"))
-      table.insert(msg, _("Bet on Totoran and win incredible sums thanks to the Crimson Gauntlet!"))
-      table.insert(msg, _("Mace rockets lacking shine? Try Lady Killington's premium rocket polish!"))
-      table.insert(msg, _("Other warlords not letting you enjoy bloodshed? Join Lord Easytrigger's battalion today!"))
+      msg = mt.merge_tables( msg, ads_dvaered )
       local badwords = {
          _("Butthead"),
          _("Nincompoop"),
@@ -89,39 +119,33 @@ function create ()
             {butthead=butthead, badword=badwords[rnd.rnd(1,#badwords)], sponser=sponser}))
    end
 
+   -- Soromid messages
    local fsr = fpres["Soromid"] or 0
    if fsr > 1 then
-      table.insert(msg, _("Remember Sorom."))
-      table.insert(msg, _("Special offer on Crow: buy one IR-eye, and the second comes for free!"))
-      table.insert(msg, _("Looking to modify an entire species? Visit Dr. Hu's Gene Clinic at Point Zero Station!"))
-      table.insert(msg, _("Endogenous DNA damage hampering your Gene Drive? Drop by your local Chromosomal Rearrangement Laboratory for a check-up."))
-      table.insert(msg, _("Visit Bohr Laboratory for all your epistatic mutation woes. 10 locations and counting!"))
-      table.insert(msg, _("Worried about your bio-ship adenosine triphosphate output? Leave it to ATP Specialists!"))
-      table.insert(msg, _("Preemptively treat your bio-ship for space fleas with Dr. Nastya's Ointment!"))
+      msg = mt.merge_tables( msg, ads_soromid )
    end
 
+   -- Soromid+Empire messages
    if fsr > 1 and fem > 1 then
       table.insert(msg, _("Remember to fill in your EX-29528-B form if returning to the Empire from Soromid territory."))
    end
 
+   -- Za'lek messages
    local fzl = fpres["Za'lek"] or 0
    if fzl > 1 then
+      msg = mt.merge_tables( msg, ads_zalek )
       -- Note that when running in the main menu background, player.name() might not exist (==nil), so
       -- we need to add a check for that.
       local pn = player.name()
       if pn then
          table.insert(msg, string.format(_("Dear Prof. %s, your recent work has left a deep impression on us. Due to the advance, novelty, and possible wide application of your innovation, we invite you to contribute other unpublished papers of relevant fields to the Interstellar Pay-to-win Journal for Mathematics and Applications."),pn))
       end
-      table.insert(msg, _("Want to solve a large-dimensional stochastic PDE? The LMKSTD method is what you need!"))
-      table.insert(msg, _("Love non-convex minimization? Join Ruadan's Computation Science Lab!"))
-      table.insert(msg, _("Keeping your drones in top shape. Prof. Imarisha's Robotic Laboratory."))
-      table.insert(msg, _("Interested in Genetic Lifeforms research? Apply to Interstice Science!")) -- Reference to Aperture Science (synonyms) from Portal
-      table.insert(msg, _("Want to learn about Anti-Mass Spectometry? Join Ebony Plateau today!")) -- Reference to Black Mesa (synonyms) from Half-Life
    end
 
+   -- Sirius messages
    local fsi = fpres["Sirius"] or 0
    if fsi > 1 then
-      table.insert(msg, _("Want a new look? Try Verrill's Ceremonial Robes at Burnan!"))
+      msg = mt.merge_tables( msg, ads_sirius )
    end
 
    mem.ad = msg[rnd.rnd(1,#msg)]
