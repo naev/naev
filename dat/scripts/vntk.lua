96c51af54e93b00ba69ffccca31a5ab9f0cb85e0
Edgar Simo-Serra <bobbens@gmail.com>
2021-10-18 13:03:49 +0900

    Allow parameter usage in vntk.

--- dat/scripts/vntk.lua
+++ dat/scripts/vntk.lua
@@ -4,24 +4,28 @@
 local vn = require "vn"
 local vntk = {}
 
-function vntk.msg( title, text )
+function vntk.msg( title, text, params )
+   params = params or {}
+   local transition = params.transition or {"blur",0.2}
    if type(text) ~= "table" then
       text = {text}
    end
 
    vn.reset()
    vn.scene()
+   if params.pre then params.pre() end
    local c
    if title and title ~= "" then
       c = vn.newCharacter( title )
    else
       c = vn.na
    end
-   vn.transition( "blur", 0.2 )
+   vn.transition( table.unpack(transition) )
    for k,t in ipairs(text) do
       c( t )
    end
-   vn.done( "blur", 0.2 )
+   if params.post then params.post() end
+   vn.done( table.unpack(transition) )
    vn.run()
 end
 
