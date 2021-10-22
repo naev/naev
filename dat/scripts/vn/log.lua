e5e3e905a09585f0257200685f0b9466e3953f2e
Edgar Simo-Serra <bobbens@gmail.com>
2021-10-22 21:26:18 +0900

    Split vn sdf stuff into its own file.

--- dat/scripts/vn/log.lua
+++ dat/scripts/vn/log.lua
@@ -1,5 +1,5 @@
 local graphics = require 'love.graphics'
-local love_shaders = require "love_shaders"
+local sdf = require "vn.sdf"
 
 local log = {
    border = 100,
@@ -65,25 +65,7 @@ function log.open( font )
 
    -- Compile shaders
    if not log.shader.arrow then
-      log.shader.arrow = graphics.newShader( [[
-#include "lib/sdf.glsl"
-vec4 effect( vec4 colour, Image tex, vec2 pos, vec2 px )
-{
-   vec2 uv = pos*2.0-1.0;
-   uv = vec2(-uv.y,-uv.x);
-
-   float d1 = sdTriangleIsosceles( uv+vec2(0.0,-0.4), vec2(0.6,0.4) );
-   float d2 = sdTriangleIsosceles( uv+vec2(0.0,0.2),  vec2(0.6,0.4) );
-   float d3 = sdTriangleIsosceles( uv+vec2(0.0,0.8),  vec2(0.6,0.4) );
-
-   float d = min(min(d1, d2), d3);
-
-   d = abs(d)-0.01;
-
-   colour.a *= step( 0.0, -d ) + pow( 1.0-d, 20.0 );
-   return colour;
-}
-]], love_shaders.vertexcode)
+      log.shader.arrow = sdf.arrow
    end
 end
 
---
+++ dat/scripts/vn/sdf.lua
@@ -0,0 +1,25 @@
+local love_shaders = require "love_shaders"
+
+local sdf = {}
+
+sdf.arrow = graphics.newShader( [[
+#include "lib/sdf.glsl"
+vec4 effect( vec4 colour, Image tex, vec2 pos, vec2 px )
+{
+   vec2 uv = pos*2.0-1.0;
+   uv = vec2(-uv.y,-uv.x);
+
+   float d1 = sdTriangleIsosceles( uv+vec2(0.0,-0.4), vec2(0.6,0.4) );
+   float d2 = sdTriangleIsosceles( uv+vec2(0.0,0.2),  vec2(0.6,0.4) );
+   float d3 = sdTriangleIsosceles( uv+vec2(0.0,0.8),  vec2(0.6,0.4) );
+
+   float d = min(min(d1, d2), d3);
+
+   d = abs(d)-0.01;
+
+   colour.a *= step( 0.0, -d ) + pow( 1.0-d, 20.0 );
+   return colour;
+}
+]], love_shaders.vertexcode)
+
+return sdf
