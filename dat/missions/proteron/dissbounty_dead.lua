b21231b5f874747712fddd1a51a532b44226f721
Justin Blanchard <UncombedCoconut@gmail.com>
2021-10-23 21:47:04 -0400

    Inline bounty mission title texts.

--- dat/missions/proteron/dissbounty_dead.lua
+++ dat/missions/proteron/dissbounty_dead.lua
@@ -21,7 +21,6 @@
 
 require "missions.neutral.pirbounty_dead"
 
-subdue_title   = _("Captured Alive")
 subdue_text    = {}
 subdue_text[1] = _("You and your crew infiltrate the ship's pathetic security and subdue the dissident. You transport them to your ship.")
 subdue_text[2] = _("Your crew has a difficult time getting past the ship's security, but eventually succeeds and subdues the dissident.")
@@ -29,14 +28,11 @@ subdue_text[3] = _("The ship's security system turns out to be no match for your
 subdue_text[4] = _("Your crew infiltrates the ship and captures the dissident.")
 subdue_text[5] = _("Getting past this ship's security was surprisingly easy. Didn't this dissident know that they were wanted?")
 
-subdue_fail_title   = _("Capture Failed")
 subdue_fail_text    = {}
 subdue_fail_text[1] = _("Try as you might, you cannot get past the dissident's security system. Defeated, you and your crew return to the ship.")
 subdue_fail_text[2] = _("The ship's security system locks you out.")
 subdue_fail_text[3] = _("Your crew comes close to getting past the dissident's security system, but ultimately fails.")
 subdue_fail_text[4] = _("It seems your crew is no match for this ship's security system. You return to your ship.")
-
-pay_title   = _("Mission Completed")
 
 pay_kill_text    = {}
 pay_kill_text[1] = _("After verifying that you killed your target, an officer hands you your pay.")
@@ -52,7 +48,6 @@ pay_capture_text[3] = _("The officer you deal with seems to especially dislike d
 pay_capture_text[4] = _("An officer rushes the fearful-looking dissident into a secure hold, pays you the appropriate bounty, and takes the dissident into custody.")
 pay_capture_text[5] = _("The officer you greet gives you a puzzled look when you say that you captured your target alive. Nonetheless, they politely take the dissident off of your hands and hand you your pay.")
 
-share_title   = _("A Smaller Reward")
 share_text    = {}
 share_text[1] = _([["Greetings. I can see that you were trying to collect a bounty. Well, as you can see, I earned the bounty, but I don't think I would have succeeded without your help, so I've transferred a portion of the bounty into your account."]])
 share_text[2] = _([["Sorry about getting in the way of your bounty. I don't really care too much about the money, but I just wanted to make sure the galaxy would be rid of that scum. So as an apology, I would like to offer you the portion of the bounty you clearly earned. The money will be in your account shortly."]])
@@ -62,7 +57,6 @@ share_text[5] = _([["Ha ha ha, looks like I beat you to it this time, eh? Well, 
 
 
 -- Mission details
-misn_title  = _("PD: Dead or Alive Bounty in %s")
 misn_desc   = _("A political dissident was recently seen in the %s system. %s authorities want this dissident dead or alive.")
 
 -- Messages
@@ -111,8 +105,10 @@ function create ()
    bounty_setup()
 
    -- Set mission details
-   misn.setTitle( misn_title:format( missys:name() ) )
+   misn.setTitle( _("PD: Dead or Alive Bounty in %s"):format( missys:name() ) )
    misn.setDesc( misn_desc:format( missys:name(), paying_faction:name() ) )
+   --TODO: redo as
+   --misn.setDesc( fmt.f( misn_desc, {sysname=missys:name(), fctname=paying_faction:name()} ) )
    misn.setReward( fmt.credits( credits ) )
    marker = misn.markerAdd( missys, "computer" )
 end
