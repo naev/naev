b375d777a8a4fe6bb62c879f6515cbe617ebe3aa
Edgar Simo-Serra <bobbens@gmail.com>
2021-10-28 23:26:16 +0900

    Added vntk.yesno.

--- dat/scripts/vntk.lua
+++ dat/scripts/vntk.lua
@@ -21,12 +21,52 @@ function vntk.msg( title, text, params )
       c = vn.na
    end
    vn.transition( table.unpack(transition) )
-   for k,t in ipairs(text) do
+   for _k,t in ipairs(text) do
       c( t )
    end
    if params.post then params.post() end
    vn.done( table.unpack(transition) )
    vn.run()
+end
+
+function vntk.yesno( title, text, params )
+   params = params or {}
+   local transition = params.transition or {"blur",0.2}
+   if type(text) ~= "table" then
+      text = {text}
+   end
+   local menu = params.menu or 1
+   local option
+
+   vn.reset()
+   vn.scene()
+   if params.pre then params.pre() end
+   local c
+   if title and title ~= "" then
+      c = vn.newCharacter( title )
+   else
+      c = vn.na
+   end
+   vn.transition( table.unpack(transition) )
+   for k,t in ipairs(text) do
+      c( t )
+      if k==menu then
+         vn.menu{ 
+            {_("Yes"), "yes"},
+            {_("No"), "no"},
+         }
+         vn.label("yes")
+         vn.func( function () option = true end )
+         vn.jump("cont")
+         vn.label("no")
+         vn.func( function () option = false end )
+         vn.label("cont")
+      end
+   end
+   if params.post then params.post() end
+   vn.done( table.unpack(transition) )
+   vn.run()
+   return option
 end
 
 return vntk
