86da0e7d8346358e4d37b422f30feb6a7551b147
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-09 21:36:00 +0900

    Reorganized a bit.

---
+++ utils/model-view-c/gltf.h
@@ -0,0 +1,15 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+#pragma once
+
+struct Object_;
+typedef struct Object_ Object;
+
+int object_init (void);
+void object_exit (void);
+
+Object *object_loadFromFile( const char *filename );
+void object_free( Object *obj );
+
+void object_render( const Object *obj );
