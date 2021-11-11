f7e7363d3990f29d6fa9e593b8a127ccd7c2ed04
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-11 13:34:54 +0900

    Improved handling of randomness.

--- dat/bkg/default.lua
+++ dat/bkg/default.lua
@@ -1,11 +1,11 @@
 --[[
    Some sort of stellar wind type background.
 --]]
-
--- We use the default background too!
 local bgshaders = require "bkg.bgshaders"
 local love_shaders = require 'love_shaders'
 local graphics = require "love.graphics"
+local prng_lib = require "prng"
+prng = prng_lib.new()
 
 local starfield = [[
 #include "lib/gamma.glsl"
@@ -18,38 +18,36 @@ local starfield = [[
 uniform vec2 u_resolution;
 uniform vec4 u_camera = vec4(1.0); /* xy corresponds to screen space */
 uniform sampler2D u_prevtex;
-uniform vec2 u_r  = vec2( 400.0, 700.0 );
+const vec2 R      = vec2( %f, %f );
+const float THETA = %f;
+const mat2 ROT    = mat2( cos(THETA), -sin(THETA), sin(THETA), cos(THETA) );
 
-/* Arbitrary rotation
-const float theta = 1.0;
-const mat2 rotate = mat2( cos(theta), -sin(theta), sin(theta), cos(theta) );
-
-#define iterations 17
-#define volsteps 8
-#define sparsity 0.7  // 0.4 to 0.5 (sparse)
-#define stepsize 0.2
-#define frequencyVariation   1.8 // 0.5 to 2.0
-#define brightness 0.0010
-#define distfading 0.6800
+#define ITERATIONS   17
+#define VOLSTEPS     8
+#define SPARSITY     0.7  // 0.4 to 0.5 (sparse)
+#define STEPSIZE     0.2
+#define FREQVAR      1.8 // 0.5 to 2.0
+#define BRIGHTNESS   0.0010
+#define DISTFADING   0.6800
 
 vec4 effect( vec4 colour_in, Image tex, vec2 texture_coords, vec2 screen_coords )
 {
-   vec2 uv = (texture_coords - 0.5) * love_ScreenSize.xy * u_camera.w + u_camera.xy + u_r;
+   vec2 uv = (texture_coords - 0.5) * love_ScreenSize.xy * u_camera.w + u_camera.xy + R;
 
    vec3 dir = vec3(uv, 1.0);
-   dir.xy *= rotate;
+   dir.xy *= ROT;
 
    float s = 0.1, fade = 0.01;
    vec4 colour = vec4( vec3(0.0), 1.0 );
 
-   for (int r=0; r < volsteps; r++) {
+   for (int r=0; r < VOLSTEPS; r++) {
       vec3 p = vec3(1.0) + u_camera.xyz + dir * (s * 0.5);
-      p = abs(vec3(frequencyVariation) - mod(p, vec3(frequencyVariation * 2.0)));
+      p = abs(vec3(FREQVAR) - mod(p, vec3(FREQVAR * 2.0)));
 
       float prevlen = 0.0, a = 0.0;
-      for (int i=0; i < iterations; i++) {
+      for (int i=0; i < ITERATIONS; i++) {
          p = abs(p);
-         p = p * (1.0 / dot(p, p)) + (-sparsity); // the magic formula
+         p = p * (1.0 / dot(p, p)) + (-SPARSITY); // the magic formula
          float len = length(p);
          a += abs(len - prevlen); // absolute sum of average change
          prevlen = len;
@@ -58,9 +56,9 @@ vec4 effect( vec4 colour_in, Image tex, vec2 texture_coords, vec2 screen_coords 
       a *= a * a; // add contrast
 
       /* Colouring based on distance. */
-      colour.rgb += (vec3(s, s*s, s*s*s) * a * brightness + 1.0) * fade;
-      fade *= distfading; /* Distance fading. */
-      s += stepsize;
+      colour.rgb += (vec3(s, s*s, s*s*s) * a * BRIGHTNESS + 1.0) * fade;
+      fade *= DISTFADING; /* Distance fading. */
+      s += STEPSIZE;
    }
    colour.rgb = min(colour.rgb, vec3(1.2));
 
@@ -88,9 +86,15 @@ function background ()
    -- Scale factor that controls computation cost. As this shader is really
    -- really expensive, we can't compute it at full resolution
    sf = naev.conf().nebu_scale * 0.5
+   
+   -- Per system parameters
+   prng:setSeed( system.cur():name() )
+   local theta = rnd.rnd()*2*math.pi
+   local rx = 500+500*rnd.rnd()
+   local ry = 500+500*rnd.rnd()
 
    -- Initialize shader
-   shader = graphics.newShader( starfield, love_shaders.vertexcode )
+   shader = graphics.newShader( string.format(starfield, rx, ry, theta), love_shaders.vertexcode )
    bgshaders.init( shader, sf, {usetex=true} )
 end
 
