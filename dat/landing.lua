22b1fba51877f0da4eee2b588f19aeff722ef734
Justin Blanchard <UncombedCoconut@gmail.com>
2021-11-11 23:01:08 -0500

    a few more missed locals

--- dat/landing.lua
+++ dat/landing.lua
@@ -194,14 +194,14 @@ end
 -- Helper function for determining the bribe cost multiplier for the player's current ship.
 -- Returns the factor the bribe cost is multiplied by when the player tries to bribe.
 -- NOTE: This should be replaced by something better in time.
-function getshipmod()
+local function getshipmod()
    return player.pilot():ship():size()
 end
 
 -- Helper function for calculating bribe availability and cost.
 -- Expects the faction, the minimum standing to land, the minimum standing to bribe, and a going rate for bribes.
 -- Returns whether the planet can be bribed, and the cost for doing so.
-function getcost(fct, land_floor, bribe_floor, rate)
+local function getcost(fct, land_floor, bribe_floor, rate)
    local standing = fct:playerStanding()
    if standing < bribe_floor then
       return _("\"I'm not dealing with dangerous criminals like you!\"")
