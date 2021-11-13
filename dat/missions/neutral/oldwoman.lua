0853ebe9f710ef0677d0e1d941726b215b08fb7e
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-13 17:47:30 +0900

    Whitespace and fixes.

--- dat/missions/neutral/oldwoman.lua
+++ dat/missions/neutral/oldwoman.lua
@@ -40,83 +40,79 @@ complaints[6] = _([["Time passes by so quickly. I remember when this was all spa
 complaints[7] = _([["All this automation is making people lax, I tell you. My uncle ran a tight ship. If he caught you cutting corners, you'd be defragmenting the sub-ion matrix filters for a week!"]])
 
 function create ()
-    mem.cursys = system.cur()
+   mem.cursys = system.cur()
 
-    local planets = {}
-    lmisn.getSysAtDistance( mem.cursys, 1, 6,
-        function(s)
-            for i, v in ipairs(s:planets()) do
-                if v:faction() == faction.get("Sirius") and v:class() == "M" and v:canLand() then
-                    planets[#planets + 1] = {v, s}
-                end
-            end
-            return false
-        end )
+   local planets = {}
+   lmisn.getSysAtDistance( mem.cursys, 1, 6,
+      function(s)
+         for i, v in ipairs(s:planets()) do
+             if v:faction() == faction.get("Sirius") and v:class() == "M" and v:canLand() then
+                 planets[#planets + 1] = {v, s}
+             end
+          end
+          return false
+      end )
 
-    if #planets == 0 then abort() end -- In case no suitable planets are in range.
+   if #planets == 0 then abort() end -- In case no suitable planets are in range.
 
-    local index = rnd.rnd(1, #planets)
-    mem.destplanet = planets[index][1]
-    mem.destsys = planets[index][2]
+   local index = rnd.rnd(1, #planets)
+   mem.destplanet = planets[index][1]
+   mem.destsys = planets[index][2]
 
-    mem.curplanet = planet.cur()
-    misn.setNPC(_("An old woman"), "neutral/unique/oldwoman.webp", _("You see a wrinkled old lady, a somewhat unusual sight in a spaceport bar. She's purposefully looking around."))
+   mem.curplanet = planet.cur()
+   misn.setNPC(_("An old woman"), "neutral/unique/oldwoman.webp", _("You see a wrinkled old lady, a somewhat unusual sight in a spaceport bar. She's purposefully looking around."))
 end
 
 
 function accept ()
-    if tk.yesno(_("An elderly lady"), fmt.f(_([[You decide to ask the old woman if there's something you can help her with.
-    "As a matter of fact, there is," she creaks. "I want to visit my cousin, she lives on {pnt}, you know, in the {sys} system, it's a Sirian place. But I don't have a ship and those blasted passenger lines around here don't fly on Sirius space! I tell you, customer service really has gone down the gutter over the years. In my space faring days, there would always be some transport ready to take you anywhere! But now look at me, I'm forced to get to the spaceport bar to see if there's a captain willing to take me! It's a disgrace, that's what it is. What a galaxy we live in! But I ramble. You seem like you've got time on your hands. Fancy making a trip down to {pnt}? I'll pay you a decent fare, of course."]]), {pnt=mem.destplanet, sys=mem.destsys})) then
-        tk.msg(_("An elderly lady"), _([["Oh, that's good of you." The old woman gives you a wrinkly smile. "I haven't seen my cousin in such a long time, it'll be great to see how she's doing, and we can talk about old times. Ah, old times. It was all so different then. The space ways were much safer, for one. And people were politer to each other too, oh yes!"
-    You escort the old lady to your ship, trying not to listen to her rambling. Perhaps it would be a good idea to get her to her destination as quickly as you can.]]))
-        local c = misn.cargoNew( N_("Old Woman"), N_("A grumbling old woman.") )
-        mem.oldwoman = misn.cargoAdd(c, 0)
+   if tk.yesno(_("An elderly lady"), fmt.f(_([[You decide to ask the old woman if there's something you can help her with.
+   "As a matter of fact, there is," she creaks. "I want to visit my cousin, she lives on {pnt}, you know, in the {sys} system, it's a Sirian place. But I don't have a ship and those blasted passenger lines around here don't fly on Sirius space! I tell you, customer service really has gone down the gutter over the years. In my space faring days, there would always be some transport ready to take you anywhere! But now look at me, I'm forced to get to the spaceport bar to see if there's a captain willing to take me! It's a disgrace, that's what it is. What a galaxy we live in! But I ramble. You seem like you've got time on your hands. Fancy making a trip down to {pnt}? I'll pay you a decent fare, of course."]]), {pnt=mem.destplanet, sys=mem.destsys})) then
+      tk.msg(_("An elderly lady"), _([["Oh, that's good of you." The old woman gives you a wrinkly smile. "I haven't seen my cousin in such a long time, it'll be great to see how she's doing, and we can talk about old times. Ah, old times. It was all so different then. The space ways were much safer, for one. And people were politer to each other too, oh yes!"
+   You escort the old lady to your ship, trying not to listen to her rambling. Perhaps it would be a good idea to get her to her destination as quickly as you can.]]))
+      local c = misn.cargoNew( N_("Old Woman"), N_("A grumbling old woman.") )
+      mem.oldwoman = misn.cargoAdd(c, 0)
 
-        misn.accept()
-        misn.setDesc(fmt.f(_("An aging lady has asked you to ferry her to {pnt} in the {sys} system."), {pnt=mem.destplanet, sys=mem.destsys}))
-        misn.setReward(_("Fair monetary compensation"))
-        misn.osdCreate(_("The old woman"), {
-            fmt.f(_("Take the old woman to {pnt} ({sys} system)"), {pnt=mem.destplanet, sys=mem.destsys}),
-        })
-        misn.markerAdd(mem.destplanet, "high")
+      misn.accept()
+      misn.setDesc(fmt.f(_("An aging lady has asked you to ferry her to {pnt} in the {sys} system."), {pnt=mem.destplanet, sys=mem.destsys}))
+      misn.setReward(_("Fair monetary compensation"))
+      misn.osdCreate(_("The old woman"), {
+         fmt.f(_("Take the old woman to {pnt} ({sys} system)"), {pnt=mem.destplanet, sys=mem.destsys}),
+      })
+      misn.markerAdd(mem.destplanet, "high")
 
-        mem.dist_total = car.calculateDistance(system.cur(), planet.cur():pos(), mem.destsys, mem.destplanet)
-        mem.complaint = 0
+      mem.dist_total = car.calculateDistance(system.cur(), planet.cur():pos(), mem.destsys, mem.destplanet)
+      mem.complaint = 0
 
-        hook.date(time.create(0, 0, 300), "date")
-        hook.land("land")
-    else
-        misn.finish()
-    end
+      hook.date(time.create(0, 0, 300), "date")
+      hook.land("land")
+   else
+      misn.finish()
+   end
 end
 
 -- Date hook.
 function date()
-    local dist_now = car.calculateDistance(system.cur(), player.pos(), mem.destsys, mem.destplanet)
-    local complaint_now = math.floor(((mem.dist_total - dist_now) / mem.dist_total) * #complaints + 0.5)
-    if complaint_now > mem.complaint then
-        mem.complaint = complaint_now
-        tk.msg(_("Grumblings from the old lady"), complaints[mem.complaint])
-    end
-    -- Uh... yeah.
+   local dist_now = car.calculateDistance(system.cur(), player.pos(), mem.destsys, mem.destplanet)
+   local complaint_now = math.floor(((mem.dist_total - dist_now) / mem.dist_total) * #complaints + 0.5)
+   if complaint_now > mem.complaint then
+      mem.complaint = complaint_now
+      tk.msg(_("Grumblings from the old lady"), complaints[mem.complaint])
+   end
+   -- Uh... yeah.
 end
 
 -- Land hook.
 function land()
-    if planet.cur() == mem.destplanet then
-        tk.msg(_("Delivery complete"), _([[You help the old lady to the spacedock elevator. She keeps grumbling about how spaceports these days are so inconvenient and how advertisement holograms are getting quite cheeky of late, they wouldn't allow that sort of thing in her day. But once you deliver her to the exit terminal, she smiles at you.
-    "Thank you, young captain, I don't know what I would have done without you. It seems there are still decent folk out there even now. Take this, as a token of my appreciation."
-    The lady hands you a credit chip. Then she disappears through the terminal. Well, that was quite a passenger!]]))
-        player.pay( reward )
-        neu.addMiscLog( _([[You escorted an old woman to her cousin in Sirian space. She was nice, albeit somewhat overly chatty.]]) )
-        misn.finish(true)
-    end
+   if planet.cur() == mem.destplanet then
+      tk.msg(_("Delivery complete"), _([[You help the old lady to the spacedock elevator. She keeps grumbling about how spaceports these days are so inconvenient and how advertisement holograms are getting quite cheeky of late, they wouldn't allow that sort of thing in her day. But once you deliver her to the exit terminal, she smiles at you.
+   "Thank you, young captain, I don't know what I would have done without you. It seems there are still decent folk out there even now. Take this, as a token of my appreciation."
+   The lady hands you a credit chip. Then she disappears through the terminal. Well, that was quite a passenger!]]))
+      player.pay( reward )
+      neu.addMiscLog( _([[You escorted an old woman to her cousin in Sirian space. She was nice, albeit somewhat overly chatty.]]) )
+      misn.finish(true)
+   end
 end
 
 function abort ()
-    -- Remove the passenger.
-    if mem.oldwoman then
-        misn.cargoRm(mem.oldwoman)
-    end
-    misn.finish(false)
+   misn.finish(false)
 end
