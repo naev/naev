a6430873ad73a77215d3fb906d9d4fbbd37222b3
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-16 12:23:52 +0900

    Made nebula a library.

--- dat/bkg/nebula1.lua
+++ dat/bkg/nebula1.lua
@@ -1,44 +1,11 @@
 --[[
-   One jump away from the nebula, so we get a bit nebula in the background.
+   Nearby Nebula background
 --]]
-local lg = require "love.graphics"
-local lf = require 'love.filesystem'
-local love_shaders = require 'love_shaders'
-local prng_lib = require "prng"
-local prng = prng_lib.new()
--- We use the default background too!
 local starfield = require "bkg.starfield"
-
-local nebulafrag = lf.read('bkg/shaders/nebula.frag')
-
-local function R()
-   return (2*prng:random()-1)*1000
-end
+local nebula = require "bkg.nebula"
 
 function background ()
-   -- Initialize shader
-   --local w, h = 1024, 1024
-   local w, h = 1024, 1024
-   local shader = lg.newShader(
-      string.format(nebulafrag, w, h, R(), R(), R()),
-      love_shaders.vertexcode )
-   local cvs = lg.newCanvas( w, h, {dpiscale=1} )
-
-   local oldcanvas = lg.getCanvas()
-   lg.setCanvas( cvs )
-   lg.clear( 0, 0, 0, 0 )
-   lg.setShader( shader )
-   lg.setColor( {1,1,1,1} )
-   love_shaders.img:draw( 0, 0, 0, w, h )
-   lg.setShader()
-   lg.setCanvas( oldcanvas )
-
-   local move = 0.03
-   local scale = 5
-   local angle = 1
-   naev.bkg.image( cvs.t.tex, 0, 0, move, scale, angle )
-
-   -- Default nebula background
+   nebula.init()
    starfield.init()
 end
 
