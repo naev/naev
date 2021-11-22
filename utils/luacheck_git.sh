cfbde47bc5f0a717adfad27319ee9b6913f47012
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-22 23:29:32 +0900

    pre-commit configuration seems to work well now.

--- utils/luacheck_git.sh
+++ utils/luacheck_git.sh
@@ -5,4 +5,4 @@
 # Usage: utils/luacheck_git.sh [--no-color]
 cd "$(git rev-parse --show-toplevel)/dat"
 # List all files that are modified/added/removed in the index, or modified in the working dir. Chop the "XY dat/" bit.
-git status --porcelain '**.lua' | grep '^[MAR]\|^.M' | cut -b 8- | xargs luacheck /dev/null "$@"
+git status --porcelain '**.lua' | grep '^[MAR]\|^.M' | cut -b 8- | xargs luacheck /dev/null
