a44569d1ed22919d9df2aa656f2dfdf6f0d99b1f
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-23 21:55:52 +0900

    More updates.

--- dat/missions/zalek/particlephysics/zpp03.lua
+++ dat/missions/zalek/particlephysics/zpp03.lua
@@ -29,7 +29,7 @@ local fmt = require "format"
 local zpp = require "common.zalek_physics"
 local sokoban = require "minigames.sokoban"
 
--- luacheck: globals land enter drone_board (Hook functions passed by name)
+-- luacheck: globals land enter drone_board heartbeat (Hook functions passed by name)
 
 local reward = zpp.rewards.zpp03
 local mainpnt, mainsys = planet.getS("Katar I")
@@ -46,19 +46,24 @@ function accept ()
    vn.scene()
    local n = vn.newCharacter( zpp.vn_noona() )
    vn.transition( zpp.noona.transition )
-   vn.na(_([[]]))
-   n(_([[""]]))
+   vn.na(_([[You approach Noona who doesn't seem too happy.]]))
+   n(_([["I have no idea what went wrong."
+She has trouble keeping her composure.
+"I double checked everything and still it seems like the drones failed to perform the procedure. It should have been a walk in the park!"]]))
+   n(_([[She looks at you with hope gleaming in her eyes.
+"Say, could you fly out and see what happened? The drones shouldn't be far from here and they don't seem to be moving. If you can, try to access the black box by opening the hull component. Could you look into this for me?"]]))
    vn.menu{
       {_("Accept"), "accept"},
       {_("Decline"), "decline"},
    }
 
    vn.label("decline")
-   n(_([[""]]))
+   n(_([["I see. You must be busy with other things."
+She goes back to ruminating on what to do.]]))
    vn.done( zpp.noona.transition )
 
    vn.label("accept")
-   n(_([[""]]))
+   n(_([["Thanks again. I'll give you the password to the hull so you can access the black box control panel. Depending on the damage, you may have to realign the memory to be able to retrieve the data, but I heard that you mastered that from my colleague. I've sent you the coordinates for the drones so you should be able to find them easy. Best of luck!"]]))
    vn.func( function () accepted = true end )
    vn.done( zpp.noona.transition )
    vn.run()
@@ -96,42 +101,79 @@ function land ()
    vn.scene()
    local n = vn.newCharacter( zpp.vn_noona() )
    vn.transition( zpp.noona.transition )
-   vn.na(_([[]]))
-   n(_([[""]]))
+   vn.na(_([[You land and find Noona waiting outside your ship expectantly.]]))
+   n(_([["That was scary! I have no idea what happened with the drone powering up and attacking you."]]))
    vn.sfxVictory()
    vn.na( fmt.reward(reward) )
    vn.done( zpp.noona.transition )
    vn.run()
 
    player.pay( reward )
-   zpp.log(_(""))
+   zpp.log(_("You helped Noona retrieve the black box from one of her drones that malfunctioned."))
    misn.finish(true)
 end
 
+local pdis, phost
 function enter ()
    if mem.state~=1 or system.cur() ~= mainsys then
       return
    end
 
    -- Temp faction
-   local fdrone = faction.dynAdd( "Za'lek", "haywire_drone", _("Za'lek") )
+   local fdrone = faction.dynAdd( "Za'lek", "haywire_drone", _("Za'lek"), {clear_allies=true, clear_enemies=true} )
 
    -- Spawn the drones
    -- TODO better location once testing center object is created
    local pkatar = planet.get("Katar"):pos()
    local pkatari = mainpnt:pos()
    local pos = (pkatar - pkatari)*3 + pkatar
-   local pdis = pilot.add( "Za'lek Light Drone", fdrone, pos, _("Haywire Drone") )
+   -- Disabled drone
+   pdis = pilot.add( "Za'lek Light Drone", fdrone, pos, _("Malfunctioned Drone") )
    pdis:setFriendly(true)
    pdis:setInvincible(true)
-   pdis:setActiveBoard(true)
+   pdis:setHilight(true)
+   pdis:setVisplayer(true)
    pdis:control(true)
    pdis:brake()
    hook.pilot( pdis, "disable", "drone_board" )
-
+   -- Hostile drone
    pos = pos + vec2.newP( 100*rnd.rnd(), rnd.angle() )
-   local phost = pilot.add( "Za'lek Heavy Drone", fdrone, pos, _("Haywire Drone") )
-   phost:setHostile(true)
+   phost = pilot.add( "Za'lek Heavy Drone", fdrone, pos, _("Malfunctioned Drone") )
+   phost:control(true)
+   phost:brake()
+   phost:setInvincible(true)
+   phost:setVisplayer(true)
+   phost:setHilight(true)
+
+   hook.timer( 5, "heartbeat" )
+end
+
+local stage = 0
+function heartbeat ()
+   if stage==0 then
+      pilot.comm(_("Noona"), _("I've sent you the drone positions, please get close to investigate."))
+      stage = 1
+   elseif stage==1 and  pdis:dist( player.pilot() ) < 500 then
+      pilot.comm(_("Noona"), _("That is weird, maybe a firmware bug? Wait… I'm detecting a power fluctuation!"))
+      stage = 2
+   elseif stage==3 then
+      stage = 3
+   elseif stage==4 then
+      pilot.comm(_("Noona"), _("Watch out!"))
+      phost:control(false)
+      phost:setInvincible(false)
+      phost:setNoDisable(true)
+      phost:rename(_("Haywire Drone"))
+      stage = 5
+   elseif stage==5 and not phost:exists() then
+      pilot.comm(_("Noona"), _("That was close. Can you try boarding the other drone?") )
+      pdis:setActiveBoard(true)
+      stage = 6
+   end
+
+   if stage < 6 then
+      hook.timer( 3, "heartbeat" )
+   end
 end
 
 local hacked = false
