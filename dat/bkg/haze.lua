6bd116acb934f6f569ddf14d2f65fa98155ef9ab
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-25 23:20:52 +0900

    Added haze.

---
+++ dat/bkg/haze.lua
@@ -0,0 +1,72 @@
+--[[
+   Some sort of stellar wind type background.
+--]]
+local graphics = require "love.graphics"
+local bgshaders = require "bkg.lib.bgshaders"
+local love_shaders = require 'love_shaders'
+-- We use the default background too!
+require "bkg.default"
+
+local shader, sf, shaze
+
+local background_default = background
+function background ()
+   -- Scale factor that controls computation cost. As this shader is really
+   -- really expensive, we can't compute it at full resolution
+   sf = naev.conf().nebu_scale
+
+   -- Initialize shader
+   local pixelcode = string.format([[
+#include "lib/simplex.glsl"
+
+const int ITERATIONS = 5;
+const float SCALAR = 2.0;
+const float SCALE = 200.0;
+const float TIME_SCALE = 50.0;
+
+uniform float u_time = 0.0;
+uniform vec3 u_camera;
+
+vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
+{
+   vec3 uv = 100.0 * vec3( %f, %f, %f );
+
+   /* Calculate coordinates */
+   uv.xy += ((texture_coords - 0.5) * love_ScreenSize.xy * u_camera.z + u_camera.xy) / SCALE;
+   uv.z += u_time / TIME_SCALE;
+
+   /* Create the noise */
+   float f = 0.0;
+   for (int i=0; i<ITERATIONS; i++) {
+      float scale = pow(SCALAR, i);
+      f += (snoise( uv * scale )*0.5 + 0.2) / scale;
+   }
+
+   /* Give more transparency around the player. */
+   float d = min( 1.0, length( (texture_coords-0.5)*love_ScreenSize.xy )/200.0 );
+   //d = 1.0;
+
+   return mix( vec4(0.0), color, f*d );
+}
+]], rnd.rnd(), rnd.rnd(), rnd.rnd() )
+   shader = graphics.newShader( pixelcode, love_shaders.vertexcode )
+   shader._dt = 0
+   shader.update = function( self, dt )
+      self._dt = self._dt + dt
+      self:send( "u_time", self._dt )
+   end
+   shaze = bgshaders.init( shader, sf )
+
+   -- Default nebula background
+   background_default()
+end
+
+function renderov( dt )
+   -- Get camera properties
+   local x, y = camera.get():get()
+   local z = camera.getZoom()
+   print(string.format("%f x %f", x*0.5/sf, y*0.5/sf))
+   shader:send( "u_camera", x*0.5/sf, -y*0.5/sf, z )
+
+   shaze:render( dt, {0.9, 0.1, 0.4, 1.0} )
+end
--- utils/glsl/nebula/main.lua
+++ utils/glsl/nebula/main.lua
@@ -1060,6 +1060,38 @@ vec4 effect( vec4 colour_in, Image tex, vec2 texture_coords, vec2 screen_coords 
 }
 ]]
 
+local pixelcode_haze = [[
+const int ITERATIONS = 3;
+const float SCALAR = 2.0; // pow(2., 4./3.);
+
+uniform vec4 color;
+uniform mat4 projection;
+uniform float eddy_scale = 200.0;
+uniform float u_time = 0.0;
+uniform vec2 center = vec2(400, 225);
+out vec4 color_out;
+
+vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
+{
+   float f = 0.0;
+   vec3 uv;
+
+   // Calculate coordinates
+   uv.xy = screen_coords / eddy_scale;
+   uv.z = u_time / 20.0;
+
+   // Create the noise
+   for (int i=0; i<ITERATIONS; i++) {
+      float scale = pow(SCALAR, i);
+      f += (snoise( uv * scale )*0.5 + 0.2) / scale;
+   }
+
+   float d = min( 1.0, length( screen_coords - center ) / 200.0 );
+
+   return mix( vec4(0.0,0.0,0.0,1.0), color, f*d );
+}
+]]
+
 local vertexcode = [[
 #pragma language glsl3
 vec4 position( mat4 transform_projection, vec4 vertex_position )
@@ -1084,7 +1116,8 @@ function love.load()
    --shader   = love.graphics.newShader( pixelcode_noise..pixelcode_digital, vertexcode)
    --shader   = love.graphics.newShader( pixelcode_noise..pixelcode_marble, vertexcode)
    --shader   = love.graphics.newShader( pixelcode_noise..pixelcode_electric, vertexcode)
-   shader   = love.graphics.newShader( pixelcode_noise..pixelcode_starfield, vertexcode)
+   --shader   = love.graphics.newShader( pixelcode_noise..pixelcode_starfield, vertexcode)
+   shader   = love.graphics.newShader( pixelcode_noise..pixelcode_haze, vertexcode)
    set_shader( 0 )
    -- We need an image for the shader to work so we create a 1x1 px white image.
    local idata = love.image.newImageData( 1, 1 )
