989b4f6a8c259ec156682d21201c341fdc130e18
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-26 09:44:48 +0900

    Less overlap in outfitter window.

--- src/land_outfits.c
+++ src/land_outfits.c
@@ -425,7 +425,7 @@ void outfits_update( unsigned int wid, const char *str )
    window_moveWidget( wid, "txtSDesc", 20+iw+20, -40-th-gl_defFont.h );
    window_moveWidget( wid, "txtDDesc", 20+iw+20+90, -40-th-gl_defFont.h );
    th += gl_printHeightRaw( &gl_defFont, w - (20 + iw + 20) - 200 - 20, buf );
-   th = MAX( th + gl_defFont.h, 256-32+8+10 );
+   th = MAX( th+gl_defFont.h, 256+10 );
    window_moveWidget( wid, "txtDescription", 20+iw+20, -40-th-gl_defFont.h );
 }
 
