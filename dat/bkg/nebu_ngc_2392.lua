dd5f21f57a5ab4c573e63ce430c94288d99b61d2
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-26 18:23:31 +0900

    Updated sources and added new background.

--- dat/bkg/nebu_ngc_2392.lua
+++ dat/bkg/nebu_ngc_2392.lua
@@ -1,2 +1,2 @@
 local nebu_img = require "bkg.lib.nebula_image"
-nebu_img.init( "ncg-2392.webp" )
+nebu_img.init( "ngc-2392.webp" )
--- dat/bkg/nebu_ngc_6210.lua
+++ dat/bkg/nebu_ngc_6210.lua
@@ -1,2 +1,2 @@
 local nebu_img = require "bkg.lib.nebula_image"
-nebu_img.init( "ngc-2440-2.webp" )
+nebu_img.init( "ngc-6210.webp" )
