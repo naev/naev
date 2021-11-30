e23d85d5ef3aa84ad52adb5427e7ad69f2404500
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-30 19:23:39 +0900

    More lmisn luadoc.

--- dat/scripts/lmisn.lua
+++ dat/scripts/lmisn.lua
@@ -7,7 +7,6 @@ local lmisn = {}
 --[[
    Sound Effects
 --]]
-
 local audio
 local _sfx
 local function _sfx_load ()
@@ -19,7 +18,7 @@ local function _sfx_load ()
 end
 
 --[[--
-   Plays a happy sound.
+   Plays a happy victory sound. Should be used for unique mission completion.
 --]]
 function lmisn.sfxVictory ()
    if not _sfx then _sfx_load() end
@@ -29,7 +28,7 @@ function lmisn.sfxVictory ()
 end
 
 --[[--
-   Plays another happy sound.
+   Plays a money indicating money made. Should be used when the player receives payment for generic repeatable missions.
 --]]
 function lmisn.sfxMoney ()
    if not _sfx then _sfx_load() end
@@ -40,18 +39,16 @@ end
 
 
 --[[--
-   Returns a complete or filtered table of landable planets.
+   Returns a complete or filtered table of landable planets (that is, landable, inhabitable, and not restricted)
 
    @tparam[opt] System sys The system to search, defaulting to the current one.
-   @tparam[opt] Faction fct If nil, return all landable planets in the system, otherwise...
-   @tparam[opt=false] boolean fctmatch If true, restrict results to the given faction; if false, restrict to its enemies.
+   @tparam[opt] Faction fct If nil, return all landable planets in the system meeting the landable criteria, otherwise...
+   @tparam[opt=false] boolean fctmatch If true, restrict results to the given faction; if false, restricts it to factions not hostile with fct.
    @treturn table All matching planets in a list.
 --]]
 function lmisn.getLandablePlanets( sys, fct, fctmatch )
    sys = sys or system.cur()
-   if fct ~= nil then
-      fct = faction.get(fct)
-   end
+   fct = fct and faction.get(fct)
    local pnt_candidates = {}
    for _k,p in ipairs(sys:planets()) do
       local s = p:services()
@@ -75,7 +72,7 @@ end
 
    @tparam System nowsys Start point.
    @tparam System finalsys End point.
-   @tparam boolean hidden Whether the path may include hidden systems.
+   @tparam[opt=false] boolean hidden Whether the path may include hidden systems.
    @treturn System The next system to jump to, defaulting to nowsys if there's no path or nothing to do.
 --]]
 function lmisn.getNextSystem( nowsys, finalsys, hidden )
@@ -92,30 +89,45 @@ function lmisn.getNextSystem( nowsys, finalsys, hidden )
 end
 
 lmisn.sysFilters = {}
+--[[
+   Provides the default system filter. Always true regardless of input.
+
+   @treturn function The filter function.
+--]]
 function lmisn.sysFilters.default ()
    return function( _sys )
       return true
    end
 end
+--[[
+   Provides the faction system filter. Makes sure each system hasa minimum amount of presence of a certain faction.
+
+   @tparam Faction fct Faction to ensure minimum presence.
+   @tparam number threshold Minimum amount of presence necessary to return true.
+   @treturn function The filter function.
+--]]
 function lmisn.sysFilters.faction( fct, threshold )
-   if fct ~= nil then
-      fct = faction.get(fct)
-   end
+   fct = fct and faction.get(fct)
    threshold = threshold or 0
    local fctname = fct:nameRaw()
    return function( sys )
       local f = sys:presences()[fctname] or 0
-      return (f > threshold)
+      return (f >= threshold)
    end
 end
+--[[
+   Provides the landable system filter. Makes sure each system has at least on landable, inhabited, non-restricted planet and is landable by a certain faction.
+
+   @tparam Faction fct Faction to make sure can land.
+   @tparam[opt=false] boolean samefact Whether or not being non-hostile is sufficient, or we enforce exact match (true).
+   @treturn function The filter function.
+--]]
 function lmisn.sysFilters.factionLandable( fct, samefact )
-   if fct ~= nil then
-      fct = faction.get(fct)
-   end
+   fct = fct and faction.get(fct)
    return function( sys )
       for k,p in ipairs(sys:planets()) do
          local s = p:services()
-         if s.land and s.inhabited then
+         if s.land and s.inhabited and not p:tags().restricted then
             local f = p:faction()
             if (samefact and f==fct) or (not samefact and f and not f:areEnemies(fct)) then
                return true
@@ -160,12 +172,8 @@ end
 --]]
 function lmisn.getSysAtDistance( sys, min, max, filter, data, hidden )
    -- Get default parameters
-   if sys == nil then
-      sys = system.cur()
-   end
-   if max == nil then
-      max = min
-   end
+   sys = sys or system.cur ()
+   max = max or min
 
    local open  = { sys }
    local close = { [sys:nameRaw()]=sys }
@@ -204,6 +212,28 @@ end
    Works the same as lmisn.getSysAtDistance, but for planets.
 
    Filter is applied on a planet level.
+
+   @usage
+   -- Get random planet within 3 to 5 jumps of current system that is landable by independent pilots.
+   local candidates = lmisn.getPlanetAtDistance( system.cur(), 3, 5, "Independent", false )
+   -- Make sure there are candidates
+   if #candidates==0 then
+      error("There are no planets meeting the criteria!")
+   end
+   -- Sort candidates by some criteria
+   table.sort( candidates, my_planet_sort_function )
+   -- Get best by sorting criteria
+   local destpnt = candidates[1]
+
+   @tparam[opt=system.cur()] System sys System to base distance calculations off of.
+   @tparam number min Minimum jump distance to get planet at.
+   @tparam number max Maximum jump distance to get planet at.
+   @tparam[opt=nil] Faction fct What faction to do landing checks with.
+   @tparam[opt=false] boolean samefct Whether or not to only allow planets to belong exactly to fct.
+   @tparam[opt=nil] function filter Filtering function that returns a boolean and takes a planet being tested as a parameter.
+   @param[opt=nil] data Custom data that will be passed to filter.
+   @tparam[opt=false] boolean hidden Whether or not to consider hidden jumps when computing system distance.
+   @treturn table A table containing all the planets matching the criteria. Can be empty if no matches found.
 --]]
 function lmisn.getPlanetAtDistance( sys, min, max, fct, samefct, filter, data, hidden )
    local pnts = {}
@@ -225,17 +255,17 @@ end
 --[[--
    Gets a random planet at a distance. Only checks for inhabited, landable, and non-restricted planets.
 
-   @usage lmisn.getRandomPlanetAtDistance( system.cur(), 3, 5, "Independent", false ) -- Get random planet within 3 to 5 jumps of current system that is landable by independent pilots.
+   @usage destpnt = lmisn.getRandomPlanetAtDistance( system.cur(), 3, 5, "Independent", false ) -- Get random planet within 3 to 5 jumps of current system that is landable by independent pilots.
 
-      @tparam[opt=system.cur()] System sys System to base distance calculations off of.
-      @tparam number min Minimum jump distance to get planet at.
-      @tparam number max Maximum jump distance to get planet at.
-      @tparam[opt=nil] Faction fct What faction to do landing checks with.
-      @tparam[opt=false] boolean samefct Whether or not to only allow planets to belong exactly to fct.
-      @tparam[opt=nil] function filter Filtering function that returns a boolean and takes a planet being tested as a parameter.
-      @param[opt=nil] data Custom data that will be passed to filter.
-      @tparam[opt=false] boolean hidden
-      @treturn Planet A single planet matching all the criteria.
+   @tparam[opt=system.cur()] System sys System to base distance calculations off of.
+   @tparam number min Minimum jump distance to get planet at.
+   @tparam number max Maximum jump distance to get planet at.
+   @tparam[opt=nil] Faction fct What faction to do landing checks with.
+   @tparam[opt=false] boolean samefct Whether or not to only allow planets to belong exactly to fct.
+   @tparam[opt=nil] function filter Filtering function that returns a boolean and takes a planet being tested as a parameter.
+   @param[opt=nil] data Custom data that will be passed to filter.
+   @tparam[opt=false] boolean hidden Whether or not to consider hidden jumps when computing system distance.
+   @treturn Planet A single planet matching all the criteria.
 --]]
 function lmisn.getRandomPlanetAtDistance( sys, min, max, fct, samefct, filter, data, hidden )
    local candidates = lmisn.getPlanetAtDistance( sys, min, max, fct, samefct, filter, data, hidden )
@@ -250,8 +280,8 @@ end
 
    @usage if anyMissionActive( { "Cargo", "Cargo Rush" } ) then -- at least one Cargo or Cargo Rush is active
 
-      @param names Table of names of missions to check
-      @return true if any of the listed missions are active
+   @tparam table names Table of names (strings) of missions to check
+   @return true if any of the listed missions are active
 --]]
 function lmisn.anyMissionActive( names )
    for i, j in ipairs( names ) do
