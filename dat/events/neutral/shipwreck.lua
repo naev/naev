b1587c864b83995efd425cfc0bd0da1aded4cbe0
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-04 11:40:23 +0900

    Reindent and only truely end the shipwreck event if the player destroys it.

--- dat/events/neutral/shipwreck.lua
+++ dat/events/neutral/shipwreck.lua
@@ -1,17 +1,17 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
 <event name="Shipwreck">
-  <trigger>enter</trigger>
-  <chance>3</chance>
-  <cond>require("common.pirate").systemPresence() &gt; 0</cond>
-  <flags>
-   <unique />
-  </flags>
-  <notes>
-   <tier>1</tier>
-  </notes>
- </event>
- --]]
+ <trigger>enter</trigger>
+ <chance>3</chance>
+ <cond>require("common.pirate").systemPresence() &gt; 0</cond>
+ <flags>
+  <unique />
+ </flags>
+ <notes>
+  <tier>1</tier>
+ </notes>
+</event>
+--]]
 --[[
 -- Shipwreck Event
 --
@@ -31,59 +31,62 @@ function create ()
    -- Make sure system isn't claimed, but we don't claim it
    if not evt.claim( system.cur(), true ) then evt.finish() end
 
-    -- The _("Shipwrecked {plt}") will be a random trader vessel.
-    local r = rnd.rnd()
-    local dship
-    if r > 0.95 then
-        dship = "Gawain"
-    elseif r > 0.8 then
-        dship = "Mule"
-    elseif r > 0.5 then
-        dship = "Koala"
-    else
-        dship = "Llama"
-    end
+   -- The _("Shipwrecked {plt}") will be a random trader vessel.
+   local r = rnd.rnd()
+   local dship
+   if r > 0.95 then
+      dship = "Gawain"
+   elseif r > 0.8 then
+      dship = "Mule"
+   elseif r > 0.5 then
+      dship = "Koala"
+   else
+      dship = "Llama"
+   end
 
-    -- Create the derelict.
-    local pos   = vec2.newP( rnd.rnd(2000,3000), rnd.angle() )
-    derelict    = pilot.add( dship, "Derelict", pos, nil, {ai="dummy"} )
-    derelict:disable()
-    derelict:rename(fmt.f(_("Shipwrecked {plt}"), {plt=shipname}))
-    -- Added extra visibility for big systems (A.)
-    derelict:setVisplayer( true )
-    derelict:setHilight( true )
+   -- Create the derelict.
+   local pos   = vec2.newP( rnd.rnd(2000,3000), rnd.angle() )
+   derelict    = pilot.add( dship, "Derelict", pos, nil, {ai="dummy"} )
+   derelict:disable()
+   derelict:rename(fmt.f(_("Shipwrecked {plt}"), {plt=shipname}))
+   -- Added extra visibility for big systems (A.)
+   derelict:setVisplayer( true )
+   derelict:setHilight( true )
 
-    hook.timer(3.0, "broadcast")
+   hook.timer(3.0, "broadcast")
 
-    -- Set hooks
-    hook.pilot( derelict, "board", "rescue" )
-    hook.pilot( derelict, "death", "destroyevent" )
-    hook.enter("endevent")
-    hook.land("endevent")
+   -- Set hooks
+   hook.pilot( derelict, "board", "rescue" )
+   hook.pilot( derelict, "death", "destroyevent" )
+   hook.enter("endevent")
+   hook.land("endevent")
 end
 
 function broadcast ()
-    -- Ship broadcasts an SOS every 10 seconds, until boarded or destroyed.
-    if not derelict:exists() then
-        return
-    end
-    derelict:broadcast( fmt.f(_("SOS. This is {plt}. We are shipwrecked. Please #bboard#0 us by positioning your ship over ours and then #bdouble-clicking#0 on our ship."), {plt=shipname}), true )
-    timer_delay = timer_delay or 10
-    timer_delay = timer_delay + 5
-    bctimer = hook.timer(timer_delay, "broadcast")
+   -- Ship broadcasts an SOS every 10 seconds, until boarded or destroyed.
+   if not derelict:exists() then
+      return
+   end
+   derelict:broadcast( fmt.f(_("SOS. This is {plt}. We are shipwrecked. Please #bboard#0 us by positioning your ship over ours and then #bdouble-clicking#0 on our ship."), {plt=shipname}), true )
+   timer_delay = timer_delay or 10
+   timer_delay = timer_delay + 5
+   bctimer = hook.timer(timer_delay, "broadcast")
 end
 
 function rescue ()
-    -- Player boards the _("Shipwrecked {plt}") and rescues the crew, this spawns a new mission.
-    hook.rm(bctimer)
-    naev.missionStart("The Space Family")
-    evt.finish(true)
+   -- Player boards the _("Shipwrecked {plt}") and rescues the crew, this spawns a new mission.
+   hook.rm(bctimer)
+   naev.missionStart("The Space Family")
+   evt.finish(true)
 end
 
-function destroyevent ()
-    evt.finish(true)
+function destroyevent( _p, killer )
+   if killer:faction() == player.pilot():faction() then
+      evt.finish(true)
+   end
+   evt.finish()
  end
 
 function endevent ()
-    evt.finish()
+   evt.finish()
 end
