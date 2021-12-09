00acee19e1163e730f61f6212c32333323314098
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-09 19:59:25 +0900

    Figured out the latin (hopefully)

--- dat/outfits/bioship/generate.py
+++ dat/outfits/bioship/generate.py
@@ -3,10 +3,10 @@
 TEMPLATEPATH = "./templates/"
 
 def lerpr( a, b ):
-    return lambda x: a + x * (b-a)
+    return lambda x: int(round(a + x * (b-a)))
 
 def lerp( a, b ):
-    return lambda x: round(a + x * (b-a))
+    return lambda x: a + x * (b-a)
 
 class BioOutfit:
     def __init__( self, template, params ):
@@ -32,14 +32,51 @@ talon
 claw
 """
 
+"""
+Perlevis (ul)
+Laevis (l)
+Mediocris (m)
+Largus (hm)
+Ponderosus (h)
+Immanis (uh)
+
+Cerebrum -> nominative neutral
+    perleve
+    laevum
+    mediocre
+    largum
+    ponderosum
+    immane
+
+Cortex -> nominative masculine
+    perlevis
+    laevis
+    mediocris
+    largus
+    ponderosus
+    immanis
+"""
+
+desc = {}
+desc["brain"] = "The brain is a Soromid bioship's equivalent to core systems in synthetic ships. Possibly the most important organ, the brain provides processing power and allocates energy to the rest of the organism. All brains start off undeveloped, but over time, just like the ships themselves, they grow and improve."
 
 BioOutfit( "pincer.xml.template", {
-    "name"  : "Pincer Organ Stage I",
-    "price" : lerpr( 4500, 20000 ),
-    "damage": lerp( 8, 17 ),
-    "energy": 4,
-    "range" : lerp( 750, 900 ),
-    "falloff": lerp( 450, 750 ),
-    "speed" : lerp( 550, 700 ),
-    "heatup": lerp( 25, 40 ),
+    "price" :   lerpr(   0, 20e3 ),
+    "damage":   lerp(    8,  17 ),
+    "energy":   4,
+    "range" :   lerp(  750, 900 ),
+    "falloff":  lerp(  450, 750 ),
+    "speed" :   lerp(  550, 700 ),
+    "heatup":   lerp(   25,  40 ),
 } ).run( ["Pincer Organ Stage I", "Pincer Organ Stage II", "Pincer Organ Stage III"] )
+
+BioOutfit( "perleve_cerebrum.xml.template", {
+    "price":        lerpr( 0, 120e3 ),
+    "mass":         14,
+    "desc":         desc["brain"],
+    "cpu":          lerpr(   3,   8 ),
+    "shield" :      lerp(  160, 250 ),
+    "shield_regen": lerp(    5,   9 ),
+    "energy":       lerp(  130, 250 ),
+    "energy_regen": lerp(   12,  19 ),
+} ).run( ["Perleve Cerebrum Stage I", "Perleve Cerebrum Stage 2"] )
