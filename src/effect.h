f4555c8bb29815236515799e4050e906a234dce1
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-19 14:42:42 +0900

    More comments.

--- src/effect.h
+++ src/effect.h
@@ -6,20 +6,28 @@
 #include "opengl.h"
 #include "shipstats.h"
 
+/**
+ * @brief Pilot ship effect.
+ */
 typedef struct Effect_s {
-   char *name;
-   char *desc;
-   glTexture *tex;
-   double duration;
-   double timer;
-   ShipStatList *stats;
+   char *name;          /**< Name of the effect. */
+   char *desc;          /**< Description of the effect. */
+   glTexture *tex;      /**< Effect icon texture. */
+   double duration;     /**< Max duration of the effect. */
+   double timer;        /**< Time left on the effect. */
+   ShipStatList *stats; /**< Actual effect. */
 } Effect;
 
+/*
+ * Effect stuff.
+ */
 int effect_load (void);
 void effect_exit (void);
-
 const Effect *effect_get( const char *name );
 
+/*
+ * Effect list stuff.
+ */
 Effect *effect_init (void);
 int effect_update( Effect **efxlist, double dt );
 int effect_add( Effect **efxlist, const Effect *efx );
