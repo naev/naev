2234fc20b3e8795c78d26b9f271221f441623816
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-21 17:56:32 +0900

    Add missing files.

---
+++ dat/outfits/bioship/skills/bioplasma2.lua
@@ -0,0 +1,13 @@
+local damage
+function onload( o )
+   local s = o:specificstats()
+   damage = s.damage
+end
+
+function init( _p, _po )
+   -- TODO handle intrinsic bonus outfits here
+end
+
+function onhit( _p, _po, target )
+   target:effectAdd( "Plasma Burn II", damage )
+end
