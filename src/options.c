5f6b7abe9c1b47dc5d31464230cabb24c3e44d0c
Justin Blanchard <UncombedCoconut@gmail.com>
2021-12-22 19:59:43 -0500

    Fix a doc comment tpyo

--- src/options.c
+++ src/options.c
@@ -198,7 +198,7 @@ static void opt_close( unsigned int wid, const char *name )
 }
 
 /**
- * @brief Handles resize events nfor the options menu.
+ * @brief Handles resize events for the options menu.
  */
 void opt_resize (void)
 {
