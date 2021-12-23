4e51f03541bfaec291d48ad1b83fccf112db6241
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-23 17:56:57 +0900

    Some spacing tweaks for the info window, doesn't really handle multiline though.

--- src/info.c
+++ src/info.c
@@ -219,7 +219,7 @@ static void info_openMain( unsigned int wid )
 
    /* pilot generics */
    nt = ntime_pretty( ntime_get(), 2 );
-   k += scnprintf( &str[k], sizeof(str)-k, "#nPilot:" );
+   k += scnprintf( &str[k], sizeof(str)-k, "Pilot:" );
    k += scnprintf( &str[k], sizeof(str)-k, "\n%s", _("Date:") );
    k += scnprintf( &str[k], sizeof(str)-k, "\n\n%s", _("Money:") );
    k += scnprintf( &str[k], sizeof(str)-k, "\n%s", _("Ship:") );
@@ -231,7 +231,7 @@ static void info_openMain( unsigned int wid )
    k += scnprintf( &str[k], sizeof(str)-k, "\n%s", _("Damage done:") );
    k += scnprintf( &str[k], sizeof(str)-k, "\n%s", _("Damage taken:") );
    k += scnprintf( &str[k], sizeof(str)-k, "\n%s", _("Ships destroyed:") );
-   window_addText( wid, 40, 20, 120, h-80, 0, "txtDPilot", &gl_smallFont, NULL, str );
+   window_addText( wid, 20, 20, 120, h-80, 0, "txtDPilot", &gl_smallFont, &cFontGrey, str );
 
    credits2str( creds, player.p->credits, 2 );
    num2str( sdied, (double)player.death_counter, 0 );
@@ -255,8 +255,8 @@ static void info_openMain( unsigned int wid )
    l += scnprintf( &str[l], sizeof(str)-l, "\n%s", "" );
    l += scnprintf( &str[l], sizeof(str)-l, _("%s MJ"), sdmgtaken );
    l += scnprintf( &str[l], sizeof(str)-l, "\n%s", sdestroyed );
-   window_addText( wid, 180, 20,
-         w-80-200-40+20-180, h-80,
+   window_addText( wid, 160, 20,
+         w-80-160-40+20-180, h-80,
          0, "txtPilot", &gl_smallFont, NULL, str );
    free(nt);
 
@@ -283,9 +283,9 @@ static void info_openMain( unsigned int wid )
         licenses[i] = strdup( _(buf[i]) );
       qsort( licenses, nlicenses, sizeof(char*), strsort );
    }
-   window_addText( wid, -20, -40, w-80-200-40-40, 20, 1, "txtList",
+   window_addText( wid, -20, -40, w-80-240-40-40, 20, 1, "txtList",
          NULL, NULL, _("Licenses") );
-   window_addList( wid, -20, -70, w-80-200-40-40, h-110-BUTTON_HEIGHT,
+   window_addList( wid, -20, -70, w-80-240-40-40, h-110-BUTTON_HEIGHT,
          "lstLicenses", licenses, MAX(nlicenses, 1), 0, NULL, NULL );
    window_setFocus( wid, "lstLicenses" );
 }
@@ -485,7 +485,7 @@ static void info_openShip( unsigned int wid )
    l += scnprintf( &buf[l], sizeof(buf)-l, "\n%s", "" );
    l += scnprintf( &buf[l], sizeof(buf)-l, "\n%s", _("Stats:") );
    window_addText( wid, 20, -40, 100, h-60, 0, "txtSDesc", &gl_smallFont, &cFontGrey, buf );
-   window_addText( wid, 160, -40, w-20-20-20-180-180., h-60, 0, "txtDDesc", &gl_smallFont,
+   window_addText( wid, 160, -40, w-20-20-20-160-180., h-60, 0, "txtDDesc", &gl_smallFont,
          NULL, NULL );
 
    /* Custom widget. */
@@ -540,7 +540,7 @@ static void ship_update( unsigned int wid )
          pilot_getJumps(player.p), n_( "jump", "jumps", pilot_getJumps(player.p) ) );
    l += scnprintf( &buf[l], sizeof(buf)-l, "%s", "\n\n" );
 
-   equipment_shipStats( &buf[l], sizeof(buf)-l, player.p, 1, 0 );
+   equipment_shipStats( &buf[l], sizeof(buf)-l, player.p, 0, 0 );
    window_modifyText( wid, "txtDDesc", buf );
    free( hyp_delay );
 }
