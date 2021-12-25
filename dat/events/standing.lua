592c73c4aa4f4174ff0f8aeb6c8499733ee7f7f7
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-26 01:16:08 +0900

    Moved some more factions to the new framework.

--- dat/events/standing.lua
+++ dat/events/standing.lua
@@ -16,6 +16,10 @@ local factions = {
    require("factions.empire"),
    require("factions.soromid"),
    require("factions.zalek"),
+   require("factions.dvaered"),
+   require("factions.sirius"),
+   require("factions.frontier"),
+   require("factions.goddard"),
 }
 
 local cap_tags_list = {}
---
+++ dat/scripts/factions/dvaered.lua
@@ -0,0 +1,10 @@
+return {
+   fct            = faction.get("Dvaered"),
+   cap_kill       = 25,
+   delta_distress = {-0.5, 0},    -- Maximum change constraints
+   delta_kill     = {-5, 1.5},    -- Maximum change constraints
+   cap_misn_def   = 40,
+   cap_misn_var   = "_fcap_dvaered",
+   cap_tags       = {
+   },
+}
