831bbde7ce7bebd41988c5e8adc099a99c4f6cf9
Edgar Simo-Serra <bobbens@gmail.com>
2021-12-26 11:32:26 +0900

    Gave the Dvaered custom feudal titles.

--- dat/scripts/factions/dvaered.lua
+++ dat/scripts/factions/dvaered.lua
@@ -6,5 +6,22 @@ return {
    cap_misn_def   = 40,
    cap_misn_var   = "_fcap_dvaered",
    cap_tags       = {
+      --["dva_cap_ch01_sml"] = { val=1, max=50 },
+      --["dva_cap_ch01_med"] = { val=3, max=50 },
+      --["dva_cap_ch01_lrg"] = { val=5, max=50 },
    },
+   text = {
+      [100] = _("Lord"),
+      [90]  = _("Liege Lord"),
+      [70]  = _("Esquire"),
+      [50]  = _("Yeoman"),
+      [20]  = _("Bordar"),
+      [0]   = _("Vagabond"),
+      [-1]  = _("Offender"),
+      [-30] = _("Enemy"),
+   },
+   text_friendly  = _("Amicable"),
+   text_neutral   = _("Indifferent"),
+   text_hostile   = _("Warlike"),
+   text_bribed    = _("Bribed"), -- Default
 }
