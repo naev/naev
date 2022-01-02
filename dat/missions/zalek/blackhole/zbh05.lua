b674d30af85f262b342e7c68498672357b7aebd1
Edgar Simo-Serra <bobbens@gmail.com>
2022-01-02 18:35:24 +0900

    Hopefully zbh05 will work first try.

--- dat/missions/zalek/blackhole/zbh05.lua
+++ dat/missions/zalek/blackhole/zbh05.lua
@@ -25,6 +25,7 @@ local vn = require "vn"
 local vntk = require "vntk"
 local fmt = require "format"
 local zbh = require "common.zalek_blackhole"
+local fleet = require "fleet"
 
 -- luacheck: globals land enter heartbeat feral_hail (Hook functions passed by name)
 
@@ -47,13 +48,14 @@ end
 
 function accept ()
    local accepted = false
+   var.pop( "zach_badnoona" ) -- Just in case the player abort sand redoes it
 
    vn.clear()
    vn.scene()
    local z = vn.newCharacter( zbh.vn_zach() )
    vn.transition( zbh.zach.transition )
-   vn.na(_([[]]))
-   z(fmt.f(_([[""]]),{}))
+   vn.na(_([[Zach is browsing what looks like some charts on his cyberdeck. He notices you right away when you enter, and turns to you.]]))
+   z(fmt.f(_([["It looks like Icarus is in worse shape than I thought. I've tried figuring out what it eats, but haven't really had too much luck with that. It would be great to directly ask a Soromid engineer, but not only is it unlikely they would disclose any secrets, Soromid and House Za'lek are not on the best of terms. However, I was able to get in touch with someone who told me they could get us with some bioship supplies. Would you be willing to bring the supplies over from {pnt} in the {sys} system while I try to diagnose Icarus?"]]),{pnt=destpnt, sys=destsys}))
    vn.menu{
       {_("Accept"), "accept"},
       {_("Decline"), "decline"},
@@ -64,7 +66,21 @@ function accept ()
    vn.done( zbh.zach.transition )
 
    vn.label("accept")
-   z(_([[""]]))
+   z(fmt.f(_([["Great! {pnt} is not really one of the best places, but they do tend to have access to pretty rare stuff, and more often than not it's easier to go through the providers there than official channels. I wouldn't be surprised if the Za'lek Chairwoman herself!"
+He chuckles.]]),{pnt=destpnt}))
+   vn.menu{
+      {_([["Well actually…"]]), "cont01a"},
+      {_([[Say nothing about Noona.]]), "cont01b"},
+   }
+
+   vn.label("cont01a")
+   z(_([["You sure about that? Isn't the Hawking radiation getting to your head? There's no way Chairwoman Sanderaite would ever get involved with such dirty dealings."]]))
+   vn.func( function () var.push("zach_badnoona",true) end )
+
+   vn.label("cont01b")
+   z(_([["I'll be taking care of Icarus here. There isn't too much of a hurry, as I don't fear for Icarus' life, but it's probably better to try to get them back to shape sooner than later. Best of luck!"
+You can see the glee on his face when he goes back to looking at the charts on his cyberdeck. Nothing is more addictive to a Za'lek researcher than the potential of exploring and learning new things.]]))
+
    vn.func( function () accepted = true end )
    vn.done( zbh.zach.transition )
    vn.run()
@@ -115,8 +131,20 @@ function land ()
       vn.scene()
       local z = vn.newCharacter( zbh.vn_zach() )
       vn.transition( zbh.zach.transition )
-      vn.na(_([[]]))
-      z(_([[""]]))
+      vn.na(_([[You land amidst a curious Icarus looping and dancing around your ship. Loading drones quickly begin to make work of unloading all the cargo of the ship under the watchful eyes of Icarus.]]))
+      z(_([["How'd it go? … Damn, you met some hostile drones nearby? That can't be a good omen, and I wanted to think we were in the clear. At least you managed to bring all the cargo safe in one piece. Now I have to see how Icarus enjoys them."]]))
+      z(_([[He goes to a nearby container and opens it up, causing a cloud of frozen gas to come rolling out.
+"Very interesting. Let's give this a try."
+He picks up a thick glass cylindrical cannister with metallic stoppers on both end. There seems to be a thick viscous liquid in it, reminding you of some sort of primordial soup. Without much grace, he musters all his strength and flings it towards space.]]))
+      vn.na(_([[The cannister slowly rotates as it flies out the space dock. Icarus is still doing weird loops and chasing nearby drones, when suddenly it stops moving, and begins to look at the cannister. In the blink of an eye, Icarus lunges at it, opening what can only be described as a gaping maw, and crushes the cannister in a single impetuous chomp, with you and Zach staring dumbfounded.]]))
+      z(_([["Holy shit! Did you just see what I did!?"
+Zach looks pale and goes stiff.]]))
+      z(_([["That… was…   …"]]))
+      z(_([["AWESOME!"
+He points to Icarus excitedly.
+"Did you see that? Damn, I wish my drones could do that. Wait if…"
+He seems to be seriously considering how to modify his drones.]]))
+      z(_([["It looks like hopefully there will be no problem with them enjoying their treats. However, I still think we'll have to do some basic surgery to repair some of Icarus' internal damage. I think I have an idea of how to pull it off, but it's going to be tricky. Meet me at the bar when you're ready to help."]]))
       vn.sfxVictory()
       vn.na( fmt.reward(reward) )
       vn.done( zbh.zach.transition )
@@ -130,8 +158,20 @@ function land ()
 end
 
 function enter ()
-   --if mem.state==2 and system.cur() == atksys then
-   --end
+   if mem.state==2 and system.cur() == atksys then
+      -- Player will almost certainly go through these ships as shortcut isn't known yet
+      local ships = {
+         "Za'lek Heavy Drone",
+         "Za'lek Heavy Drone",
+         "Za'lek Light Drone",
+         "Za'lek Light Drone",
+         "Za'lek Light Drone",
+      }
+      local drones = fleet.add( 1, ships, zbh.evilpi(), jump.get(atksys, retsys):pos()*0.9, nil, {ai="baddiepos"} )
+      for k,p in ipairs(drones) do
+         p:memory().comm_no = _("ACCESS DENIED.")
+      end
+   end
 
    if system.cur() == retsys then
       local feral = zbh.plt_icarus( retpnt:pos() + vec2.newP(300,rnd.angle()) )
