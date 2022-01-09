75c907dbf0ff5844e67a23868312837bead589d1
Justin Blanchard <UncombedCoconut@gmail.com>
2022-01-09 14:04:37 -0500

    use lazyload in luaspfx.

--- dat/scripts/luaspfx.lua
+++ dat/scripts/luaspfx.lua
@@ -1,12 +1,2 @@
 -- Lazy loader for luaspfx
-local luaspfx
-local lazy = setmetatable( {}, {
-   __index = function( self, key )
-      if not luaspfx then
-         luaspfx = require "luaspfx.core"
-         self.__index = luaspfx
-      end
-      return luaspfx[key]
-   end
-} )
-return lazy
+return require("lazyload")("luaspfx.core")
