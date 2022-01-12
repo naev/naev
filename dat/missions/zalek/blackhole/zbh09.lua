2cf4b69bf84cf37006802fc5f683c51544bf7d01
Edgar Simo-Serra <bobbens@gmail.com>
2022-01-12 14:07:45 +0900

    Initial zbh09 skeleton.

---
+++ dat/missions/zalek/blackhole/zbh09.lua
@@ -0,0 +1,246 @@
+--[[
+<?xml version='1.0' encoding='utf8'?>
+<mission name="Za'lek Black Hole 9">
+ <flags>
+  <unique />
+ </flags>
+ <avail>
+  <priority>4</priority>
+  <chance>100</chance>
+  <spob>Research Post Sigma-13</spob>
+  <location>Bar</location>
+  <done>Za'lek Black Hole 8</done>
+ </avail>
+ <notes>
+  <campaign>Za'lek Black Hole</campaign>
+ </notes>
+</mission>
+--]]
+--[[
+   Za'lek Black Hole 09
+
+   Have to find the wormhole, go through it, and have an encounter with feral bioships
+   1. Go to insys and find  wormhole (cutscene)
+   2. Jump into outsys, have cutscene with feral bioships
+   3. After some criteria, Icarus jumps in and brings peace
+   4. Icarus leaves and the player goes back to sigma-13
+]]--
+local vn = require "vn"
+local fmt = require "format"
+local zbh = require "common.zalek_blackhole"
+local fleet = require "fleet"
+
+-- luacheck: globals land enter zach_say heartbeat_wormhole heartbeat_ferals feral_hail (Hook functions passed by name)
+
+local reward = zbh.rewards.zbh09
+
+local inwormhole, insys = spob.getS( "Wormhole NGC-13674" )
+local outsys = system.get( "NGC-1931" )
+local mainpnt, mainsys = spob.getS("Research Post Sigma-13")
+
+function create ()
+   misn.finish()
+   if not misn.claim( {mainsys, insys, outsys} ) then
+      misn.finish()
+   end
+
+   misn.setNPC( _("Zach"), zbh.zach.portrait, zbh.zach.description )
+end
+
+local wormholeknown = false
+function accept ()
+   wormholeknown = inwormhole:isKnown()
+   local accepted = false
+
+   vn.clear()
+   vn.scene()
+   local z = vn.newCharacter( zbh.vn_zach() )
+   vn.transition( zbh.zach.transition )
+   vn.na(_([[]]))
+   z(fmt.f(_([[""]])
+      {}))
+   vn.menu{
+      {_("Accept"), "accept"},
+      {_("Decline"), "decline"},
+   }
+
+   vn.label("decline")
+   z(_([["OK. I'll be here if you change your mind."]]))
+   vn.done( zbh.zach.transition )
+
+   vn.label("accept")
+   vn.func( function () accepted = true end )
+   z(_([[""]]))
+
+   -- Change text a bit depending if known
+   --[[
+   if inwormhole:isKnown() then
+   else
+   end
+   --]]
+
+   vn.done( zbh.zach.transition )
+   vn.run()
+
+   -- Must be accepted beyond this point
+   if not accepted then return end
+
+   misn.accept()
+
+   -- mission details
+   local title = _("Black Hole Mystery")
+   misn.setTitle( title )
+   misn.setReward( fmt.credits(reward) )
+   misn.setDesc( _("") )
+
+   mem.mrk = misn.markerAdd( insys )
+   mem.state = 1
+
+   misn.osdCreate( title, {
+      fmt.f(_("Go track down the signal in the {sys} system"), {sys=insys})
+   } )
+
+   hook.land( "land" )
+   hook.enter( "enter" )
+end
+
+function land ()
+   if mem.state==3 and spob.cur() == mainpnt then
+
+      vn.clear()
+      vn.scene()
+      local z = vn.newCharacter( zbh.vn_zach() )
+      vn.transition( zbh.zach.transition )
+      vn.na(_(""))
+      z(_([[""]]))
+      vn.sfxVictory()
+      vn.na( fmt.reward(reward) )
+      vn.done( zbh.zach.transition )
+      vn.run()
+
+      faction.modPlayer("Za'lek", zbh.fctmod.zbh09)
+      player.pay( reward )
+      zbh.log(_(""))
+      misn.finish(true)
+   end
+end
+
+local pack
+function enter ()
+   if mem.state==1 and system.cur() == insys then
+      player.allowLand( false, _("Zach is analyzing the wormhole signal.") )
+
+      if wormholeknown then
+         system.mrkAdd( inwormhole:pos(), _("Wormhole") )
+      else
+         system.mrkAdd( inwormhole:pos(), _("Suspicious Signal") )
+      end
+
+      hook.timer( 5, zach_say, _("Weird that Icarus didn't follow us through the jump…") )
+      hook.timer( 12, zach_say, _("I've marked the location on your system map.") )
+      hook.timer( 1, "heartbeat_wormhole" )
+
+   elseif mem.state==1 and system.cur() == outsys then
+      pilot.clear()
+      pilot.toggleSpawn(false)
+
+      player.allowLand( false, _("The wormhole seems to have become too weak to go through.") )
+
+      --local j = jump.get( outsys, "NGC-4771" )
+      local pp = player.pilot()
+
+      -- nohinohi, taitamariki, kauweke,
+      local ships
+      if pp:ship():size() >= 5 then
+         ships = { "Kauweke", "Kauweke", "Taitamariki", "Taitamariki", "Taitamariki", "Taitamariki" }
+      else
+         ships = { "Kauweke", "Taitamariki", "Taitamariki", "Taitamariki" }
+      end
+      local pos = vec2.pos( -6000, 3000 ) -- Halfway towards NGC-4771
+      pack = fleet.add( 1, ships, zbh.feralbioships(), pos )
+      for k,p in ipairs(pack) do
+         p:rename(_("Feral Bioship"))
+         p:setNoDeath()
+         p:setInvincible() -- in case the player does something silly like preemptively shoot torpedoes
+      end
+      local l = pack[1]
+      l:control()
+      l:brake()
+
+      hook.timer( 5, zach_say, _("Hot damn that was weird. Ugh, I feel sick.") )
+      hook.timer( 12, zach_say, _("I'm getting some ship readings. Whait, what is that?") )
+      hook.timer( 18, "heartbeat_ferals" )
+
+   --elseif system.cur() == mainsys then
+
+   end
+end
+
+function zach_say( msg )
+   player.autonavReset( 3 )
+   player.pilot():comm(fmt.f(_([[Zach: "{msg}"]]),{msg=msg}))
+end
+
+local zach_msg_known = {
+   _("It's… mesmerizing!"),
+   _("It's safe right? Maybe we should send a drone first."),
+   _("I guess we might as well try it. Experimental physics at it's finest."),
+   _("Try to go through it carefully!"),
+}
+local zach_msg_unknown = {
+   _("Damn, that the hell is that?" ),
+   _("One second, let me analyze the data." ),
+   _("I can't make much sense out of this, but…"),
+   _("It seems like some sort of space-time discontinuity…"),
+   _("It doesn't seem dangerous, at least the readings seem strangely fine."),
+   _("I guess we might as well try it. Experimental physics at it's finest."),
+   _("Try to go through it carefully!"),
+}
+
+local wstate = 0
+function heartbeat_wormhole ()
+   local msglist = (wormholeknown and zach_msg_known) or zach_msg_unknown
+   local pp = player.pilot()
+   local d = pp:pos():dist( inwormhole:pos() )
+   if wstate==0 and d < 5000 then
+      wstate = 1
+      zach_say( _("The sensor readings are off the charts!") )
+   elseif wstate==1 and d < 2500 then
+      wstate = 2
+      zach_say( _("I think I see something!") )
+   elseif wstate==2 and d < 500 then
+      wstate = 3
+      zach_say( msglist[wstate-2] )
+   else
+      wstate = wstate+1
+      local msg = msglist[ wstate-2 ]
+      if msg==nil then -- Out of messages
+         player.allowLand()
+         return
+      end
+      zach_say( msg )
+   end
+
+   -- Normalize time so it's independent of ship
+   hook.timer( 5 / player.dt_mod(), "heartbeat_wormhole" )
+end
+
+--local fstate = 0
+function heartbeat_ferals ()
+   local nexttime = 5
+   --local l = pack[1]
+   --if fstate == 0 then
+   --end
+
+   hook.timer( nexttime / player.dt_mod(), "heartbeat_ferals" )
+end
+
+local sfx_spacewhale = {
+   zbh.sfx.spacewhale1,
+   zbh.sfx.spacewhale2
+}
+function feral_hail ()
+   local sfx = sfx_spacewhale[ rnd.rnd(1,#sfx_spacewhale) ]
+   sfx:play()
+   player.commClose()
+end
