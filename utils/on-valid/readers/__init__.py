8c9a0398a58521a22634e5dc1fd9eabc21303a20
Justin Blanchard <UncombedCoconut@gmail.com>
2022-01-12 13:26:09 -0500

    RIP utils that will only bit-rot further as dynamic Lua items proliferate.

--- utils/on-valid/readers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=80:
-
-
-from .outfit import outfit
-from .unidiff import unidiff
-from .ship import ship
-
-__all__ = []
