85a239d4fde3a7052077a0f7c880a249e8bd102f
Edgar Simo-Serra <bobbens@gmail.com>
2022-02-06 18:25:40 +0900

    Added missing mining script.

---
+++ dat/scripts/minigames/mining.lua
@@ -0,0 +1,54 @@
+--[[
+   Wrapper for the Sokoban minigame
+--]]
+local love = require "love"
+local vn = require "vn"
+local mcore = require "minigames.mining.core"
+local mining = {}
+
+local function setup( params, standalone )
+   params = params or {}
+   local c = naev.cache()
+   c.mining = {}
+   c.mining.params = params
+   c.mining.standalone = standalone
+end
+
+--[[
+   Runs the Mining minigame as a standalone
+--]]
+function mining.love( params )
+   setup( params, true )
+   love.exec( 'scripts/minigames/mining' )
+end
+
+--[[
+   Runs the Mining minigame from the VN
+--]]
+function mining.vn( params )
+   local s = vn.custom()
+   s._init = function ()
+      setup( params, false )
+      return mcore.load()
+   end
+   s._draw = function ()
+      naev.gfx.clearDepth()
+      return mcore.draw()
+   end
+   s._keypressed = function( _self, key )
+      return mcore.keypressed( key )
+   end
+   s._update = function( self, dt )
+      if mcore.update( dt ) then
+         self.done = true
+      end
+   end
+   return s
+end
+
+function mining.completed ()
+   local c = naev.cache()
+   return (c.mining and c.mining.completed)
+end
+
+return mining
