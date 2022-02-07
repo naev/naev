0e63848109e045fe983cee1a56bacf5110b80f6b
Edgar Simo-Serra <bobbens@gmail.com>
2022-02-08 02:08:10 +0900

    Added S&K Heavy Plasma Drill.

---
+++ dat/outfits/utility/sk_heavy_plasma_drill.lua
@@ -0,0 +1,15 @@
+local drill = require "outfits.utility.mining_drill"
+
+function init( p, po )
+   -- Since this outfit is usually off, we use shipstats to forcibly set the
+   -- base stats
+   po:set( "asteroid_scan", 350 )
+   po:set( "mining_bonus", 80 )
+   mem.isp = (p == player.pilot())
+   drill.setup( p, po, {
+      speed = 0.8*math.pi,
+      shots_max = 4,
+   })
+end
+
+ontoggle = drill.ontoggle
