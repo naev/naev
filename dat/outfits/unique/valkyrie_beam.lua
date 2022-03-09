c8a9de0c3f69acd869e05baa5bd18a78012de284
Edgar Simo-Serra <bobbens@gmail.com>
2022-03-09 14:51:59 +0900

    Redid valkyrie beam to be less bad.

---
+++ dat/outfits/unique/valkyrie_beam.lua
@@ -0,0 +1,17 @@
+function init( _p, _po )
+   mem.state = false
+end
+
+function update( p, _po )
+   if mem.state then
+      p:effectAdd( "Ride of the Valkyries" )
+   end
+end
+
+function ontoggle( p, _po, on )
+   if on then
+      p:effectAdd( "Ride of the Valkyries" )
+   end
+   mem.state = on
+   return true -- Doesn't limit anything
+end
