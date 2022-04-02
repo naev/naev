9469ed198c6765a74817d19ef3de32518a9a891c
Edgar Simo-Serra <bobbens@gmail.com>
2022-04-02 19:28:38 +0900

    Started to work on proper hypergates.

--- dat/spob/lua/hypergate.lua
+++ dat/spob/lua/hypergate.lua
@@ -1,28 +1,93 @@
+--[[
+   Active hypergate
+--]]
 local lg = require "love.graphics"
+local love_shaders = require "love_shaders"
 
-local pos, tex
+local pos, tex, mask, cvs, shader
+local tw, th
+
+local pixelcode = [[
+#include "lib/simplex.glsl"
+
+uniform float u_time = 0.0;
+uniform sampler2D u_mask;
+const vec3 basecol = vec3( 0.2, 0.8, 0.8 );
+
+vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
+{
+   vec4 col = texture( u_mask, texture_coords );
+   col.rgb *= basecol;
+   col.a *= 0.3 + 0.7*snoise( vec3(texture_coords, u_time)*10. );
+   return col;
+}
+]]
+
+local function update_canvas ()
+   local oldcanvas = lg.getCanvas()
+   local oldshader = lg.getShader()
+   lg.setCanvas( cvs )
+   lg.clear( 0, 0, 0, 0 )
+   lg.setColor( 1, 1, 1, 1 )
+   lg.setBlendMode( "alpha", "premultiplied" )
+
+   -- Draw base hypergate
+   tex:draw( 0, 0 )
+
+   -- Draw active overlay shader
+   lg.setShader( shader )
+   love_shaders.img:draw( 0, 0, 0, tw, th )
+   lg.setShader( oldshader )
+
+   lg.setBlendMode( "alpha" )
+   lg.setCanvas( oldcanvas )
+end
+
 function load( p )
    if tex==nil then
-      tex = lg.newImage( "gfx/spob/space/hypergate_destroyed.webp" )
+      -- Set up texture stuff
+      local prefix = "gfx/spob/space/"
+      tex  = lg.newImage( prefix.."hypergate_neutral_activated.webp" )
+      mask = lg.newImage( prefix.."hypergate_mask.webp" )
+
+      -- Position stuff
       pos = p:pos()
-      local tw, th = tex:getDimensions()
+      tw, th = tex:getDimensions()
       pos = pos + vec2.new( -tw/2, th/2 )
+
+      -- The canvas
+      cvs  = lg.newCanvas( tw, th, {dpiscale=1} )
+
+      -- Set up shader
+      shader = lg.newShader( pixelcode, love_shaders.vertexcode )
+      shader._dt = -1000 * rnd.rnd()
+      shader.update = function( self, dt )
+         self._dt = self._dt + dt
+         self:send( "u_time", self._dt )
+      end
+      shader:send( "u_mask", mask )
+
+      update_canvas()
    end
 
-   return tex.tex
+   return cvs.t.tex, tw/2
 end
 
 function unload ()
-   tex = nil -- Should get GC'd
+   shader= nil
+   tex   = nil
+   mask  = nil
+   cvs   = nil
+   --sfx   = nil
 end
 
---[[
 function render ()
+   update_canvas() -- We want to do this here or it gets slow in autonav
    local z = camera.getZoom()
    local x, y = gfx.screencoords( pos, true ):get()
-   tex:draw( x, y, 0, 1/z )
+   z = 1/z
+   cvs:draw( x, y, 0, z, z )
 end
---]]
 
 function can_land ()
    return false
