6217aaab442a78f3b18f78db51eaae16db15acb1
Edgar Simo-Serra <bobbens@gmail.com>
2022-05-23 16:21:20 +0900

    Removed duplicate code.

--- dat/events/neutral/news.lua
+++ dat/events/neutral/news.lua
@@ -385,33 +385,6 @@ articles["Empire"] = {
       desc = _([[The Emperor has scheduled a new monument to be constructed on Emperor's Fist in honour of all those dead in the Incident.]])
    },
 }
-
-articles["Frontier"] = {
-   --[[
-      Science and technology
-   --]]
-   --[[
-      Business
-   --]]
-   --[[
-      Politics
-   --]]
-   {
-      tag = N_([[Election on Caladan Marred by Fraud]]),
-      desc = _([[As many as two of every hundred votes counted after the recent polling decaperiod may be falsified, an ombudsman reports. The opposition party demanded the election be annulled.]])
-   },
-   {
-      tag = N_([[Empire Relies on Prison Labour]]),
-      desc = _([[A recent report by the Soromid House Ways and Means Committee suggests infrastructure may be too dependent the on the incarcerated population.]])
-   },
-   {
-      tag = N_([[Imperial Oversight Scandal]]),
-      desc = _([[Sources close to the Imperial Chancellor say they see the failure at the Department of Oversight, whose inspectors overlooked serious failings in other supervisory bodies, as a serious oversight.]])
-   },
-   --[[
-      Human interest.
-   --]]
-}
 articles["Za'lek"] = {
    --[[
       Science and technology
