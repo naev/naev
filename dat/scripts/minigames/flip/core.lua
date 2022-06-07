74f4e84f9306ff36d25ee90619230856d31bd2a2
Edgar Simo-Serra <bobbens@gmail.com>
2022-06-07 21:45:52 +0900

    Improved flip game.

--- dat/scripts/minigames/flip/core.lua
+++ dat/scripts/minigames/flip/core.lua
@@ -7,22 +7,46 @@ local mg = {}
 
 local colours = {
    --bg        = {0x00/0xFF, 0x00/0xFF, 0x00/0xFF},
-   dark          = {0x1C/0xFF, 0x30/0xFF, 0x4A/0xFF},
    --dark     = {0x04/0xFF, 0x6B/0xFF, 0x99/0xFF},
-   --foobar      = {0xB3/0xFF, 0xEF/0xFF, 0xFF/0xFF},
    --text        = {0xFF/0xFF, 0xFF/0xFF, 0xFF/0xFF},
    --bad         = { 0.8, 0.2, 0.2 },
    --dark     = {0x10/0xFF, 0x4C/0xFF, 0x71/0xFF},
+   dark     = {0x1C/0xFF, 0x30/0xFF, 0x4A/0xFF},
    light    = {0x00/0xFF, 0xCF/0xFF, 0xFF/0xFF},
+   won      = {0xB3/0xFF, 0xEF/0xFF, 0xFF/0xFF},
 }
 
-local board_size, selected_x, selected_y, board--, headerfont, headertext
-local movekeys, alpha, done, bx, by, bgshader, standalone
+local board_size, selected_x, selected_y, board, headerfont, headertext
+local movekeys, alpha, done, bx, by, bgshader, standalone, game_won
+
+local function flip( x, y )
+   -- flip
+   for i=math.max(x-1,1), math.min(x+1,board_size) do
+   board[i][y] = not board[i][y]
+   end
+   for j=math.max(y-1,1), math.min(y+1,board_size) do
+   board[x][j] = not board[x][j]
+   end
+   board[x][y] = not board[x][y]
+end
+
+local function checkwon ()
+   for i=1,board_size do
+      for j=1,board_size do
+         if not board[i][j] then
+            return false
+         end
+      end
+   end
+   return true
+end
+
 function mg.load ()
    local c = naev.cache()
    local params = c.flip.params
    standalone = c.flip.standalone
    board_size = params.board_size or 3
+   headertext = params.header
 
    -- Get movement keys
    movekeys = {
@@ -47,40 +71,50 @@ function mg.load ()
    for i=1,board_size do
       board[i] = {}
       for j=1,board_size do
-         --board[i][j] = (rnd.rnd() > 0.5)
-         board[i][j] = (love.math.random() > 0.5)
+         --board[i][j] = (love.math.random() > 0.5)
+         board[i][j] = true
       end
    end
+   local n = board_size*board_size
+   for i=1,rnd.rnd( math.floor(n*0.4), math.floor(n*0.6) ) do
+      flip( rnd.rnd(1,board_size), rnd.rnd(1,board_size) )
+   end
 
-   selected_x = math.floor(board_size/2)
-   selected_y = math.floor(board_size/2)
+   selected_x = math.ceil(board_size/2)
+   selected_y = math.ceil(board_size/2)
 
    local lw, lh = love.window.getDesktopDimensions()
    local s = 40*board_size-8
    bx = (lw-s)*0.5
    by = (lh-s)*0.5
+   if headertext then
+      by = by - 40
+   end
    lg.setBackgroundColor(0, 0, 0, 0)
-   --headerfont = lg.newFont(24)
-   --headertext = params.header
+   lg.setNewFont( 16 )
+   headerfont = lg.newFont(24)
    bgshader = love_shaders.circuit()
    alpha = 0
+   game_won = false
 end
 
 function mg.keypressed( key )
    if key == "escape" then
       done = true
+      return
+   end
+
+   if game_won then
+      done = true
+      return
    end
 
    -- handle keys
    if key == movekeys.shoot then
-      -- flip
-      for i=math.max(selected_x-1,1), math.min(selected_x+1,board_size) do
-         board[i][selected_y] = not board[i][selected_y]
+      flip( selected_x, selected_y )
+      if checkwon() then
+         game_won = true
       end
-      for j=math.max(selected_y-1,1), math.min(selected_y+1,board_size) do
-         board[selected_x][j] = not board[selected_x][j]
-      end
-      board[selected_x][selected_y] = not board[selected_x][selected_y]
    elseif key == movekeys.left then
       selected_x = math.max( selected_x-1, 1 )
    elseif key == movekeys.right then
@@ -99,25 +133,34 @@ local function setcol( col )
 end
 
 function mg.draw ()
+   local x, y = 0, 0
    local nw, nh = naev.gfx.dim()
    setcol( {0.2,0.2,0.2,0.85} )
    lg.setShader( bgshader )
    love_shaders.img:draw( 0, 0, 0, nw, nh )
    lg.setShader()
 
-   setcol{ 1, 1, 1 }
-   --lg.printf( headertext, headerfont, bx+x, by+y, nw, "center" )
+   if headertext then
+      setcol{ 1, 1, 1 }
+      lg.printf( headertext, headerfont, 0, by, nw, "center" )
+      y = y + 60
+   end
 
    -- Draw selected
-   setcol{ 1, 1, 1 }
-   lg.rectangle( "fill", bx+40*(selected_x-1)-2, by+40*(selected_y-1)-2, 36, 36 )
+   setcol{ 0, 0, 0 }
+   lg.rectangle( "fill", bx+x-4, by+y-4, 40*board_size, 40*board_size )
+   if not game_won then
+      setcol{ 1, 1, 1 }
+      lg.rectangle( "fill", bx+40*(selected_x-1)-2+x, by+40*(selected_y-1)-2+y, 36, 36 )
+   end
 
    -- Draw grid
-   local x, y = 0, 0
    for i=1,board_size do
       for j=1,board_size do
          local col
-         if board[j][i] then
+         if game_won then
+            col = colours.won
+         elseif board[j][i] then
             col = colours.light
          else
             col = colours.dark
@@ -130,6 +173,10 @@ function mg.draw ()
       x = 0
       y = y + 40
    end
+
+   y = y + 20
+   setcol{ 1, 1, 1 }
+   lg.printf( "#n".._("Flip the blocks until they are all lit"), 0, by+y, nw, "center" )
 end
 
 function mg.update( dt )
