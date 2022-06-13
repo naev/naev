6ea3fc563a4808084442d5f5d7d0c8b036a68c6d
Edgar Simo-Serra <bobbens@gmail.com>
2022-06-13 23:39:44 +0900

    Created a not very impressive trail shader

---
+++ dat/scripts/luaspfx/shaders/trail.frag
@@ -0,0 +1,35 @@
+#include "lib/perlin.glsl"
+
+const int ITERATIONS = 2;
+const float SCALAR = pow(2., 4./3.);
+const float eddy_scale = 50.0;
+
+uniform float u_time = 0.0;
+uniform float u_size = 0.0;
+uniform float u_r = 0.0;
+
+float fbm( vec4 color, vec2 screen_coords )
+{
+   float f = 0.0;
+   vec3 uv;
+
+   // Calculate coordinates
+   uv.xy = screen_coords / eddy_scale + u_r;
+   uv.z = u_time / 15.0;
+
+   // Create the noise
+   for (int i=0; i<ITERATIONS; i++) {
+      float scale = pow(SCALAR, i);
+      f += cnoise( uv * scale ) / scale;
+   }
+	return 0.1 + f;
+}
+
+vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
+{
+   vec2 uv = (texture_coords*2.0-1.0);
+   float d = length(uv);
+	float n = fbm( color, uv * u_size * 0.5 );
+   color.a *= n * smoothstep( -1.0, 0.0, -d );
+   return color;
+}
--- dat/scripts/luaspfx/trail.lua
+++ dat/scripts/luaspfx/trail.lua
@@ -3,8 +3,7 @@ local lf = require 'love.filesystem'
 --local audio = require 'love.audio'
 local love_shaders = require 'love_shaders'
 
---local trail_bg_shader_frag = lf.read( "scripts/luaspfx/shaders/trail.frag" )
-local trail_bg_shader_frag = lf.read( "scripts/luaspfx/shaders/alert.frag" )
+local trail_bg_shader_frag = lf.read( "scripts/luaspfx/shaders/trail.frag" )
 local trail_shader
 
 local function update( s, dt )
@@ -16,8 +15,9 @@ local function render( sp, x, y, z )
    local d = sp:data()
    trail_shader:send( "u_time", d.timer )
    trail_shader:send( "u_size", d.size )
+   trail_shader:send( "u_r", d.r )
 
-   local s = d.size / z
+   local s = d.size * z
    local old_shader = lg.getShader()
    lg.setShader( trail_shader )
    lg.setColor( d.col )
@@ -35,8 +35,9 @@ local function trail( pos, params )
    local s = spfx.new( math.huge, update, nil, nil, render, pos )
    local d  = s:data()
    d.timer  = 0
-   d.size   = params.size or 100
-   d.col    = params.col or {1, 1, 0, 0.5}
+   d.size   = params.size or 300
+   d.col    = params.col or {0.8, 0.2, 0.7, 0.3}
+   d.r      = 1000*rnd.rnd()
    return s
 end
 
