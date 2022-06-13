8cf63911119a6868bfc2e4270f3e11c50c916e4b
Edgar Simo-Serra <bobbens@gmail.com>
2022-06-13 19:25:17 +0900

    Basic bad implementation of pules scanner.

---
+++ dat/outfits/unique/pulse_scanner.lua
@@ -0,0 +1,94 @@
+local osh = require 'outfits.shaders'
+local audio = require 'love.audio'
+local luaspfx = require 'luaspfx'
+
+local active = 10 -- active time in seconds
+local cooldown = 15 -- cooldown time in seconds
+local oshader = osh.new([[
+#include "lib/blend.glsl"
+#include "lib/colour.glsl"
+const vec3 colmod = vec3( 1.0, 0.0, 0.0 );
+uniform float progress = 0;
+vec4 effect( sampler2D tex, vec2 texcoord, vec2 pixcoord )
+{
+   vec4 color     = texture( tex, texcoord );
+   float opacity  = 0.8 * clamp( progress, 0.0, 1.0 );
+   vec3 grayscale = vec3(rgb2lum(color.rgb));
+   color.rgb      = mix( color.rgb, grayscale, opacity );
+   return color;
+}
+]])
+
+local sfx = audio.newSource( 'snd/sounds/activate4.ogg' )
+
+local function turnon( p, po )
+   -- Still on cooldown
+   if mem.timer and mem.timer > 0 then
+      return false
+   end
+   po:state("on")
+   po:progress(1)
+   mem.timer = active
+   mem.active = true
+
+   -- Visual effect
+   if mem.isp then
+      oshader:on()
+      luaspfx.sfx( true, nil, sfx )
+
+      -- Can trigger scan hooks
+      hook.trigger( "poi_scan" )
+   else
+      luaspfx.sfx( p:pos(), p:vel(), sfx )
+   end
+   return true
+end
+
+local function turnoff( _p, po )
+   if not mem.active then
+      return false
+   end
+   po:state("cooldown")
+   po:progress(1)
+   mem.timer = cooldown
+   mem.active = false
+   oshader:off()
+   return true
+end
+
+function init( p, po )
+   turnoff()
+   mem.timer = nil
+   po:state("off")
+   mem.isp = (p == player.pilot())
+   oshader:force_off()
+end
+
+function update( p, po, dt )
+   if not mem.timer then return end
+
+   mem.timer = mem.timer - dt
+   if mem.active then
+      oshader:update_on( dt )
+      po:progress( mem.timer / active )
+      if mem.timer < 0 then
+         turnoff( p, po )
+      end
+   else
+      oshader:update_cooldown( dt )
+      po:progress( mem.timer / cooldown )
+      if mem.timer < 0 then
+         po:state("off")
+         mem.timer = nil
+         oshader:force_off()
+      end
+   end
+end
+
+function ontoggle( p, po, on )
+   if on then
+      return turnon( p, po )
+   else
+      return turnoff( p, po )
+   end
+end
