449bc23c75d97256c2111018a79819248e4d425e
Edgar Simo-Serra <bobbens@gmail.com>
2022-06-15 13:23:25 +0900

    Move as much poi mission stuff as possible to library.

--- dat/missions/neutral/point_of_interest.lua
+++ dat/missions/neutral/point_of_interest.lua
@@ -22,13 +22,13 @@
 
 --]]
 local fmt = require "format"
-local luaspfx = require "luaspfx"
+--local luaspfx = require "luaspfx"
 --local tut = require "common.tutorial"
 local der = require 'common.derelict'
 local poi = require "common.poi"
 local vn = require "vn"
 
--- luacheck: globals enter scan heartbeat_nooutfit heartbeat board (Hook functions passed by name)
+-- luacheck: globals found board (Hook functions passed by name)
 
 function create ()
    mem.sys, mem.risk, mem.reward = poi.start()
@@ -37,9 +37,6 @@ function create ()
    if not misn.claim( {mem.sys}, nil, true ) then
       return
    end
-
-   local riskstr = "Low"
-   local rewardstr = "Unknown"
 
    -- Roll for rewards here to disallow save scumming
    local reward_list = {
@@ -61,119 +58,20 @@ function create ()
    -- Choose a random reward and stick to it
    mem.reward = reward_list[ rnd.rnd(1,#reward_list) ]
 
-   -- Accept and set up mission
-   misn.accept()
-
-   misn.setTitle(_("Point of Interest")) -- TODO maybe randomize somewhat?
-   misn.setReward(_("Unknown")) -- TODO give some hint?
-   misn.setDesc(fmt.f(_([[A point of interest has been found in the {sys} system. It is not clear what can be foound, however, it warrants investigation. You should bring an outfit that can perform scans such as a #bPulse Scanner#0.
-
-#nEstimated Risk:#0 {risk}
-#nEstimated Reward:#0 {reward}]]),
-      {sys=mem.sys, risk=riskstr, reward=rewardstr} ) )
-
-   misn.markerAdd( mem.sys, "low" )
-
-   hook.enter( "enter" )
-   if system.cur() == mem.sys then
-      enter()
-   end
+   poi.misnSetup{ sys=mem.sys, found="found" }
 end
 
-local pos, timer, path, goal, mrk
-function enter ()
-   if system.cur() ~= mem.sys then
-      pos = nil
-      goal = nil
-      if mem.chook then
-         hook.rm( mem.chook )
-         mem.chook = nil
-      end
-      if timer then
-         hook.rm( timer )
-         timer = nil
-      end
-      return
-   end
+function found ()
+   player.msg(_("You have found something!"),true)
 
-   -- Find the initial point of interest, and path to the real location
-   pos = vec2.newP( system.cur():radius()*0.6, rnd.angle() ) -- TODO better initialization
-   path = {}
-   local angle = rnd.angle()
-   local mpos = pos
-   for i=1,rnd.rnd(13,17) do -- should average 15*5505 = 8250 units
-      mpos = vec2.newP( 400+300*rnd.rnd(), angle ) + mpos
-      angle = angle + rnd.sigma()
-      table.insert( path, mpos )
-   end
-   goal = mpos + vec2.newP( 300+400*rnd.rnd(), angle )
-
-   mrk = system.mrkAdd( pos, _("Point of Interest") )
-
-   -- Custom hook for when the player scans
-   mem.chook = hook.custom( "poi_scan", "scan" )
-
-   -- If the player has no scanning outfit we have to help them out
-   local pp = player.pilot()
-   local haspoi = false
-   for k,v in ipairs(pp:outfits()) do
-      if v:tags().poi_scan then
-         haspoi = true
-         break
-      end
-   end
-   if not haspoi then
-      timer = hook.timer( 5, "heartbeat_nooutfit" )
-   end
-end
-
-function heartbeat_nooutfit ()
-   if player.pos():dist( pos ) < 3e3 then
-      -- TODO ship AI message
-      player.msg(_("You lack an outtfit no scan the point of interest."),true)
-      return
-   end
-   timer = hook.timer( 1, "heartbeat_nooutfit" )
-end
-
-local started, path_spfx
-function scan ()
-   -- Ignore if not close
-   if started or player.pos():dist( pos ) > 3e3 then
-      return
-   end
-
-   -- Starts marks
-   started = true
-   path_spfx = {}
-
-   timer = hook.timer( 1, "heartbeat" )
-end
-
-function heartbeat ()
-   for k,v in ipairs(path) do
-      -- Have to follow the path
-      if not path_spfx[k] and (k<=1 or path_spfx[k-1]) and player.pos():dist( v ) < 1e3 then
-         path_spfx[k] = luaspfx.trail( v, path[k+1] or goal )
-      end
-   end
-
-   -- Found goal
-   if path_spfx[ #path ] and player.pos():dist( goal ) < 1e3 then
-      player.msg(_("You have found something!"),true)
-
-      -- TODO something more interesting
-      local p = pilot.add( "Mule", "Derelict", goal )
-      p:rename(_("Pristine Derelict"))
-      p:disable()
-      p:setInvincible()
-      p:setHilight()
-      p:effectAdd( "Fade-In" )
-      hook.pilot( p, "board", "board" )
-      return
-   end
-
-   timer = hook.timer( 1, "heartbeat" )
+   -- TODO something more interesting
+   local p = pilot.add( "Mule", "Derelict", mem.goal )
+   p:rename(_("Pristine Derelict"))
+   p:disable()
+   p:setInvincible()
+   p:setHilight()
+   p:effectAdd( "Fade-In" )
+   hook.pilot( p, "board", "board" )
 end
 
 function board( p )
@@ -202,11 +100,8 @@ function board( p )
    end
 
    -- Clean up stuff
+   poi.misnCleanup()
    p:setHilight(false)
-   system.mrkRm( mrk )
-   for k,v in ipairs(path_spfx) do
-      v:rm()
-   end
    player.unboard()
    misn.finish(true)
 end
--- dat/scripts/common/poi.lua
+++ dat/scripts/common/poi.lua
@@ -2,7 +2,11 @@
    Support common functions for Points of Interest
 --]]
 local lmisn = require "lmisn"
+local fmt = require "format"
+local luaspfx = require "luaspfx"
 local poi = {}
+
+-- luacheck: globals _poi_enter _poi_scan _poi_heartbeat_nooutfit _poi_heartbeat (Hook functions passed by name)
 
 --[[
    @brief Tries to generate a new setting for a point of interest.
@@ -57,6 +61,133 @@ function poi.start ()
       sys = system.get( sys )
    end
    return sys, risk, reward
+end
+
+local pos, timer, path, goal, mrk
+function _poi_enter ()
+   if system.cur() ~= mem.poi.sys then
+      pos = nil
+      goal = nil
+      if mem.poi.chook then
+         hook.rm( mem.poi.chook )
+         mem.poi.chook = nil
+      end
+      if timer then
+         hook.rm( timer )
+         timer = nil
+      end
+      return
+   end
+
+   -- Find the initial point of interest, and path to the real location
+   pos = vec2.newP( system.cur():radius()*0.6, rnd.angle() ) -- TODO better initialization
+   path = {}
+   local angle = rnd.angle()
+   local mpos = pos
+   for i=1,rnd.rnd(13,17) do -- should average 15*5505 = 8250 units
+      mpos = vec2.newP( 400+300*rnd.rnd(), angle ) + mpos
+      angle = angle + rnd.sigma()
+      table.insert( path, mpos )
+   end
+   goal = mpos + vec2.newP( 300+400*rnd.rnd(), angle )
+   mem.goal = goal
+
+   mrk = system.mrkAdd( pos, _("Point of Interest") )
+
+   -- Custom hook for when the player scans
+   mem.poi.chook = hook.custom( "poi_scan", "_poi_scan" )
+
+   -- If the player has no scanning outfit we have to help them out
+   local pp = player.pilot()
+   local haspoi = false
+   for k,v in ipairs(pp:outfits()) do
+      if v:tags().poi_scan then
+         haspoi = true
+         break
+      end
+   end
+   if not haspoi then
+      timer = hook.timer( 5, "_poi_heartbeat_nooutfit" )
+   end
+end
+
+function _poi_heartbeat_nooutfit ()
+   if player.pos():dist( pos ) < 3e3 then
+      -- TODO ship AI message
+      player.msg(_("You lack an outtfit no scan the point of interest."),true)
+      return
+   end
+   timer = hook.timer( 1, "_poi_heartbeat_nooutfit" )
+end
+
+local started, path_spfx
+function _poi_scan ()
+   -- Ignore if not close
+   if started or player.pos():dist( pos ) > 3e3 then
+      return
+   end
+
+   -- Starts marks
+   started = true
+   path_spfx = {}
+
+   timer = hook.timer( 1, "_poi_heartbeat" )
+end
+
+function _poi_heartbeat ()
+   for k,v in ipairs(path) do
+      -- Have to follow the path
+      if not path_spfx[k] and (k<=1 or path_spfx[k-1]) and player.pos():dist( v ) < 1e3 then
+         path_spfx[k] = luaspfx.trail( v, path[k+1] or goal )
+      end
+   end
+
+   -- Found goal
+   if path_spfx[ #path ] and player.pos():dist( goal ) < 1e3 then
+      _G[ mem.poi.found ]()
+      return
+   end
+
+   timer = hook.timer( 1, "_poi_heartbeat" )
+end
+
+--[[
+   @brief Sets up a Point Of Interest (POI) mission
+
+      @tparam table params Table of parameters to use. `sys` and `found` must be defined, where `sys` is the system the POI takes place in, and `found` is the name of the global function to call when found.
+--]]
+function poi.misnSetup( params )
+   mem.poi = {
+      sys   = params.sys,
+      found = params.found,
+   }
+
+   local riskstr = "Low"
+   local rewardstr = "Unknown"
+
+   -- Accept and set up mission
+   misn.accept()
+   misn.setTitle(_("Point of Interest")) -- TODO maybe randomize somewhat?
+   misn.setReward(_("Unknown")) -- TODO give some hint?
+   misn.setDesc(fmt.f(_([[A point of interest has been found in the {sys} system. It is not clear what can be foound, however, it warrants investigation. You should bring an outfit that can perform scans such as a #bPulse Scanner#0.
+
+#nEstimated Risk:#0 {risk}
+#nEstimated Reward:#0 {reward}]]),
+      {sys=mem.poi.sys, risk=riskstr, reward=rewardstr} ) )
+
+   misn.markerAdd( mem.poi.sys, "low" )
+
+   hook.enter( "_poi_enter" )
+   if system.cur() == mem.poi.sys then
+      _poi_enter()
+   end
+end
+
+function poi.misnCleanup()
+   system.mrkRm( mrk )
+   for k,v in ipairs(path_spfx) do
+      v:rm()
+   end
 end
 
 return poi
