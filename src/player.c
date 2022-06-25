1196fba9392ae6916031b423ca6b115715923744
Edgar Simo-Serra <bobbens@gmail.com>
2022-06-25 11:19:16 +0900

    Delete pilot.

--- src/player.c
+++ src/player.c
@@ -3066,6 +3066,7 @@ int player_addEscorts (void)
 
       po = pilot_getDockSlot( pe );
       if (po == NULL) {
+         pilot_delete( pe );
          WARN(_("Escort is undeployed, removing."));
          escort_rmListIndex(player.p, i);
          i--;
@@ -3075,6 +3076,7 @@ int player_addEscorts (void)
       po->u.ammo.deployed++;
       q = po->u.ammo.deployed + po->u.ammo.quantity;
       if (q >= pilot_maxAmmoO(player.p,po->outfit)) {
+         pilot_delete( pe );
          WARN(_("Escort is deployed past outfit limits, removing."));
          escort_rmListIndex(player.p, i);
          i--;
