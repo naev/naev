51fcbedc9529e949774fddc65065c5a1f5e84ee1
Edgar Simo-Serra <bobbens@gmail.com>
2022-07-11 00:52:53 +0900

    Moved empire military assets to the new system.

---
+++ dat/spob/lua/empire_mil_restricted.lua
@@ -0,0 +1,20 @@
+local luaspob = require "spob.lua.lib.spob"
+
+function load( spb )
+   return luaspob.setup( spb, {
+      std_land = 50,
+      std_bribe = 100,
+      msg_granted = {
+         _("Landing access authorized."),
+      },
+      msg_notyet = {
+         _("You are not authorized to land here."),
+      },
+      msg_cantbribe = {
+         _("Don't attempt to bribe an Empire official, pilot."),
+      },
+   } )
+end
+
+can_land = luaspob.can_land
+comm = luaspob.comm
