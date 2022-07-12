c0e574e765113a81dc2096c7196d2cbaa4181418
Edgar Simo-Serra <bobbens@gmail.com>
2022-07-12 16:06:06 +0900

    Update to memory model.

--- dat/spob/lua/lib/spob.lua
+++ dat/spob/lua/lib/spob.lua
@@ -4,50 +4,46 @@ local ccomm = require "common.comm"
 
 local luaspob = {}
 
-local params, land_spb, land_fct, bribed, bribe_cost_function
-local msg_bribed, msg_denied, msg_granted, msg_notyet, msg_cantbribe, msg_trybribe, msg_dangerous
-local std_dangerous, std_land, std_bribe
-
 function luaspob.init( spb, init_params )
-   land_spb = spb
-   params = init_params or {}
-   std_land = params.std_land or 0 -- Needed for can_land
+   mem.spob = spb
+   mem.params = init_params or {}
+   mem.std_land = mem.params.std_land or 0 -- Needed for can_land
 end
 
 function luaspob.load ()
    -- Basic stuff
-   land_fct = land_spb:faction()
-   bribed = false
+   mem.fct = mem.spob:faction()
+   mem.bribed = false
 
-   bribe_cost_function = params.bribe_cost or function ()
-      local std = land_fct:playerStanding()
-      return (std_land-std) * 1e3 * player.pilot():ship():size() + 5e3
+   mem.bribe_cost_function = mem.params.bribe_cost or function ()
+      local std = mem.fct:playerStanding()
+      return (mem.std_land-std) * 1e3 * player.pilot():ship():size() + 5e3
    end
 
-   std_land = params.std_land or 0
-   std_bribe = params.std_bribe or -30
-   std_dangerous = params.std_dangerous or -30
+   mem.std_land = mem.params.std_land or 0
+   mem.std_bribe = mem.params.std_bribe or -30
+   mem.std_dangerous = mem.params.std_dangerous or -30
 
-   msg_bribed = params.msg_bribed or {
+   mem.msg_bribed = mem.params.msg_bribed or {
       _([["Make it quick."]]),
       _([["Don't let anyone see you."]]),
       _([["Be quiet about this."]]),
    }
-   msg_denied = params.msg_denied or {
+   mem.msg_denied = mem.params.msg_denied or {
       _([["Landing request denied."]]),
       _([["Landing not authorized."]]),
       _([["Landing denied."]]),
    }
-   msg_notyet = params.msg_notyet or msg_denied
-   msg_granted = params.msg_granted or {
+   mem.msg_notyet = mem.params.msg_notyet or mem.msg_denied
+   mem.msg_granted = mem.params.msg_granted or {
       _([["Permission to land granted."]]),
       _([["You are clear to land."]]),
       _([["Proceed to land."]]),
    }
-   msg_cantbribe = params.msg_cantbribe or {
+   mem.msg_cantbribe = mem.params.msg_cantbribe or {
       _([["We do not accept bribes."]]),
    }
-   msg_trybribe = params.msg_trybribe or {
+   mem.msg_trybribe = mem.params.msg_trybribe or {
       _([["I'll let you land for the modest price of {credits}."
 
 Pay {credits}?]]),
@@ -55,7 +51,7 @@ Pay {credits}?]]),
 
 Pay {credits}?]]),
    }
-   msg_dangerous = params.msg_dangerous or {
+   mem.msg_dangerous = mem.params.msg_dangerous or {
       _([["I'm not dealing with dangerous criminals like you!"]]),
    }
 
@@ -67,56 +63,56 @@ Pay {credits}?]]),
       end
       return msg
    end
-   msg_bribed     = choose( msg_bribed )
-   msg_denied     = choose( msg_denied )
-   msg_notyet     = choose( msg_notyet )
-   msg_granted    = choose( msg_granted )
-   msg_cantbribe  = choose( msg_cantbribe )
-   msg_trybribe   = choose( msg_trybribe )
-   msg_dangerous  = choose( msg_dangerous )
+   mem.msg_bribed     = choose( mem.msg_bribed )
+   mem.msg_denied     = choose( mem.msg_denied )
+   mem.msg_notyet     = choose( mem.msg_notyet )
+   mem.msg_granted    = choose( mem.msg_granted )
+   mem.msg_cantbribe  = choose( mem.msg_cantbribe )
+   mem.msg_trybribe   = choose( mem.msg_trybribe )
+   mem.msg_dangerous  = choose( mem.msg_dangerous )
 end
 
 function luaspob.unload ()
-   bribed = false
+   mem.bribed = false
 end
 
 function luaspob.can_land ()
-   local s = land_spb:services()
+   local s = mem.spob:services()
    if not s.land then
-      return false,msg_denied
+      return false,mem.msg_denied
    end
-   if bribed or land_spb:getLandOverride() then
-      return true, msg_granted
+   if mem.bribed or mem.spob:getLandOverride() then
+      return true, mem.msg_granted
    end
-   local fct = land_spb:faction()
+   local fct = mem.spob:faction()
    if not fct then
-      return false, msg_denied
+      return false, mem.msg_denied
    end
    local std = fct:playerStanding()
    if std < 0 then
-      return false, msg_denied
+      return false, mem.msg_denied
    end
-   if std < std_land then
-      return false, msg_notyet
+   if std < mem.std_land then
+      return false, mem.msg_notyet
    end
-   return true, msg_granted
+   return true, mem.msg_granted
 end
 
 function luaspob.comm ()
    vn.clear()
    vn.scene()
-   local spb = ccomm.newCharacterSpob( vn, land_spb, bribed )
+   local spb = ccomm.newCharacterSpob( vn, mem.spob, mem.bribed )
    vn.transition()
    vn.na(fmt.f(_("You establish a communication channel with the authorities at {spb}."),
-      {spb=land_spb}))
+      {spb=mem.spob}))
 
    vn.label("menu")
    vn.menu( function ()
       local opts = {
          { _("Close"), "leave" }
       }
-      local std = land_fct:playerStanding()
-      if std < std_land then
+      local std = mem.fct:playerStanding()
+      if std < mem.std_land then
          table.insert( opts, 1, { _("Bribe"), "bribe" } )
       end
       return opts
@@ -125,19 +121,19 @@ function luaspob.comm ()
    local bribe_cost
    vn.label("bribe")
    vn.func( function ()
-      local std = land_fct:playerStanding()
-      if std < std_dangerous then
+      local std = mem.fct:playerStanding()
+      if std < mem.std_dangerous then
          vn.jump("dangerous")
          return
       end
-      if std < std_bribe then
+      if std < mem.std_bribe then
          vn.jump("nobribe")
          return
       end
-      bribe_cost = bribe_cost_function( land_spb )
+      bribe_cost = mem.bribe_cost_function( mem.spob )
    end )
    spb( function ()
-      return fmt.f( msg_trybribe, {credits=fmt.credits( bribe_cost )} )
+      return fmt.f( mem.msg_trybribe, {credits=fmt.credits( bribe_cost )} )
    end )
    vn.menu( function ()
       return {
@@ -153,10 +149,10 @@ function luaspob.comm ()
          return
       end
       player.pay( -bribe_cost )
-      bribed = true
-      ccomm.nameboxUpdateSpob( land_spb, bribed )
+      mem.bribed = true
+      ccomm.nameboxUpdateSpob( mem.spob, mem.bribed )
    end )
-   spb( msg_bribed )
+   spb( mem.msg_bribed )
 
    vn.label("player_broke")
    vn.na( function ()
@@ -171,11 +167,11 @@ function luaspob.comm ()
    vn.jump("menu")
 
    vn.label("nobribe")
-   spb( msg_cantbribe )
+   spb( mem.msg_cantbribe )
    vn.jump("menu")
 
    vn.label("dangerous")
-   spb( msg_dangerous )
+   spb( mem.msg_dangerous )
    vn.jump("menu")
 
    vn.label("leave")
