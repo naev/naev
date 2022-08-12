20143c22b03cd85606c2ba56061f69aa12287f3c
Edgar Simo-Serra <bobbens@gmail.com>
2022-08-12 23:10:37 +0900

    Some documentation.

--- dat/ai/pers.lua
+++ dat/ai/pers.lua
@@ -1,6 +1,13 @@
+--[[
+AI for pers unique pilots.
+
+Meant to be pretty flexible. You probably want to define the following:
+* mem.ad -- message spammed across the system
+* mem.comm_greet -- greeting message when hailed
+* mem.taunt -- message to spam when engaging the enemy
+--]]
 require 'ai.core.core'
 require 'ai.core.idle.advertiser'
---require 'ai.core.misc.distress'
 local fmt = require "format"
 
 mem.lanes_useneutral = true
@@ -22,10 +29,7 @@ function create ()
    mem.refuel_msg = fmt.f(_([["I'll supply your ship with fuel for {credits}."]]),
          {credits=fmt.credits(mem.refuel)})
 
-   -- Custom greeting
-   mem.comm_greet = mem.ad
-
-   mem.loiter = rnd.rnd(5,20) -- This is the amount of waypoints the pilot will pass through before leaving the system
+   mem.loiter = rnd.rnd(15,20) -- This is the amount of waypoints the pilot will pass through before leaving the system
    create_post()
 end
 
