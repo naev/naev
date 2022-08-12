9c6b0d7be4872d67ac5f2082fbcbf4d4f948a2e2
Edgar Simo-Serra <bobbens@gmail.com>
2022-08-12 20:01:29 +0900

    Parametrized advertiser spam time.

--- dat/ai/core/idle/advertiser.lua
+++ dat/ai/core/idle/advertiser.lua
@@ -1,13 +1,16 @@
 require 'ai.core.idle.civilian'
 
+mem.adspamdelayalpha = 15
+mem.adspamdelaybeta = 30
+
 control_funcs.loiter = function ()
    local lastspammed = mem.adspamlast or 0
    local curtime = naev.ticks()
-   local delay = mem.adspamdelay or 15
+   local delay = mem.adspamdelay or (mem.adpamdelayalpha + mem.adspamdelaybeta*rnd.rnd())
 
    if curtime - lastspammed > delay then
       ai.pilot():broadcast(mem.ad)
       mem.adspamlast = curtime
-      mem.adspamdelay = 15 + 30*rnd.rnd()
+      mem.adspamdelay = mem.adspamdelayalpha + mem.adspamdelaybeta*rnd.rnd()
    end
 end
