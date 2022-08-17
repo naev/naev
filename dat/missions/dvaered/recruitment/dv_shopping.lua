3432c9861830b39e83286820ba962b178c598a60
Edgar Simo-Serra <bobbens@gmail.com>
2022-08-17 17:28:36 +0900

    More double quotations.

--- dat/missions/dvaered/recruitment/dv_shopping.lua
+++ dat/missions/dvaered/recruitment/dv_shopping.lua
@@ -64,20 +64,20 @@ function accept()
    local doaccept = false
 
    vn.transition()
-   sol(fmt.f(_("Hello, citizen. We finally got the formal agreement of The Goddard Company for the obtention of our second battlecruiser. We would like you to escort our convoy to and from Zhiru. I can pay you {credits} Are you interested for that mission?"), {credits=fmt.credits(pay)}))
+   sol(fmt.f(_([["Hello, citizen. We finally got the formal agreement of The Goddard Company for the obtention of our second battlecruiser. We would like you to escort our convoy to and from Zhiru. I can pay you {credits} Are you interested for that mission?"]]), {credits=fmt.credits(pay)}))
    vn.menu{
       {_("Accept"), "accept"},
       {_("Refuse"), "refuse"},
    }
 
    vn.label("refuse")
-   sol(_([[Really? Well… I was not expecting that. Anyway, come back if you change your mind.]]))
+   sol(_([["Really? Well… I was not expecting that. Anyway, come back if you change your mind."]]))
    vn.func( function () doaccept = false end )
    vn.done()
 
    vn.label("accept")
-   sol(_([[Thank you, citizen. Your help will be most appreciated as I can not mobilize our entire fleet for this escort mission. Many of our ships are needed to protect Lord Fatgun's planets from attacks.
-I would like you to specially take the anti-bomber role. For that, I recommend you to fly a light ship.]])) -- That's actually a pretext for the player to fly a ship that can run away when Okran decides to attack the player
+   sol(_([["Thank you, citizen. Your help will be most appreciated as I can not mobilize our entire fleet for this escort mission. Many of our ships are needed to protect Lord Fatgun's planets from attacks.
+I would like you to specially take the anti-bomber role. For that, I recommend you to fly a light ship."]])) -- That's actually a pretext for the player to fly a ship that can run away when Okran decides to attack the player
    vn.func( function () doaccept = true end )
    vn.done()
    vn.run()
@@ -212,8 +212,8 @@ The walls are covered with pictures of different versions of the Goddard battlec
 "Congratulations, citizen {player}. Our data suggests you have won your first honourable fight against a gentleman. Please reach the spaceport's military contact office for further information."
 When you arrive at the said office, a soldier greets you.]]), {player=player.name()}))
       local sol = vn.newCharacter( _("Dvaered Soldier"), { image=portrait.getFullPath(solPort) } )
-      sol(fmt.f(_([[Good day, citizen. We got the information that you destroyed the Colonel Okran in a fair space fight in the framework of a Warlords rivalry. This achievement gives you the respect of the Dvaered High Command. As such, you will be allowed to purchase the Heavy Combat Vessel License if you did not already have it. Moreover, our cleaning team did find a fancy key chain among the remains of Okran's ship. As the customs requires it, this item is now yours, for what it's worth.
-And finally, two Warlords, namely Lady Proserpina and Lord Richthofen, required us to give you the sum of {credits} and their greetings. I wish you to stay right, loyal and strong, citizen.]]), {credits=fmt.credits(amount)}))
+      sol(fmt.f(_([["Good day, citizen. We got the information that you destroyed the Colonel Okran in a fair space fight in the framework of a Warlords rivalry. This achievement gives you the respect of the Dvaered High Command. As such, you will be allowed to purchase the Heavy Combat Vessel License if you did not already have it. Moreover, our cleaning team did find a fancy key chain among the remains of Okran's ship. As the customs requires it, this item is now yours, for what it's worth."
+"And finally, two Warlords, namely Lady Proserpina and Lord Richthofen, required us to give you the sum of {credits} and their greetings. I wish you to stay right, loyal and strong, citizen."]]), {credits=fmt.credits(amount)}))
       vn.done()
       vn.run()
 
@@ -308,26 +308,26 @@ function veryBadThings()
    vn.na(_([[You get hailed by a Hyena and a communication channel is opened between you, Colonel Okran and the Hyena in question.]]))
    local sol = vn.newCharacter( _("Colonel Okran"), { image=portrait.getFullPath(agentPort) } )
    local trick = vn.newCharacter( _("???"), { image=portrait.getFullPath(trickPort) } )
-   trick(_([[Yo, Okran! How are you, mate? Enyoyin' your new toy? Hewhewhew!]]))
-   sol(_([[I am sorry, but I do not know you, citizen.]]))
-   trick(_([[Ya do not know me? Hewhewhew! Of course! But really, you were expectin' to do all your shameful manoeuvring without ever meeting me? Ya all know that you are still subjects to his Imperial Majesty, right? No! You forgot that! Luckily, I'm here to remind that to you!]]))
-   sol(_([[I see. You are the… trickster, true? Lord Fatgun told me once I could run into you at some point.]]))
-   trick(_([[Hewhewhew! Different people call me differently. You boars tend to use that name indeed. So? happy to finally meet me?]]))
-   sol(_([[Look. I am running some serious business here. And I am not in need of a buffoon right now, so please proceed getting lost.]]))
-   trick(_([[Your Fatgun bro got killed one period ago.]]))
+   trick(_([["Yo, Okran! How are you, mate? Enyoyin' your new toy? Hewhewhew!"]]))
+   sol(_([["I am sorry, but I do not know you, citizen."]]))
+   trick(_([["Ya do not know me? Hewhewhew! Of course! But really, you were expectin' to do all your shameful manoeuvring without ever meeting me? Ya all know that you are still subjects to his Imperial Majesty, right? No! You forgot that! Luckily, I'm here to remind that to you!"]]))
+   sol(_([["I see. You are the… trickster, true? Lord Fatgun told me once I could run into you at some point.]]))
+   trick(_([["Hewhewhew! Different people call me differently. You boars tend to use that name indeed. So? happy to finally meet me?"]]))
+   sol(_([["Look. I am running some serious business here. And I am not in need of a buffoon right now, so please proceed getting lost."]]))
+   trick(_([["Your Fatgun bro got killed one period ago."]]))
    sol(_([[…]]))
-   trick(_([[Ya all are jobless jerks, folks!]]))
+   trick(_([["Ya all are jobless jerks, folks!]]))
    sol(_([[…]]))
-   trick(_([[Hewhewhew! Proserpina and the other asshole, Richthofen, they conducted a raid on Castellan III knowing that you were away. They managed to shoot a volley of hypersonic bombs at his bunker. And… Kaboom! One less moronic warlord in this galaxy!]]))
-   sol(_([[You… you must be kidding? Right, you are kidding!]]))
-   trick(_([[I aint ever kidding when it's about hypersonic bombs. We got the confirmation of the death by your Dvaered High Command. Your guy is down, bro! Totally down. D.O.W.N.]]))
-   sol(_([[How did they get the information I was here?]]))
-   trick(fmt.f(_([[{player} told them.]]), {player=player.name()}))
+   trick(_([["Hewhewhew! Proserpina and the other asshole, Richthofen, they conducted a raid on Castellan III knowing that you were away. They managed to shoot a volley of hypersonic bombs at his bunker. And… Kaboom! One less moronic warlord in this galaxy!"]]))
+   sol(_([["You… you must be kidding? Right, you are kidding!"]]))
+   trick(_([["I aint ever kidding when it's about hypersonic bombs. We got the confirmation of the death by your Dvaered High Command. Your guy is down, bro! Totally down. D.O.W.N."]]))
+   sol(_([["How did they get the information I was here?"]]))
+   trick(fmt.f(_([["{player} told them."]]), {player=player.name()}))
    vn.na(_([[You scratch your head, trying to remember if you were part to a scheming to kill Lord Fatgun when you notice Colonel Okran has been ejected from the communication channel. It's only you and the wild newcomer now.]]))
-   trick(fmt.f(_([[I hope you got an afterburner, mate! Hewhewhew!
+   trick(fmt.f(_([["I hope you got an afterburner, mate! Hewhewhew!
 So, seriously, what do you think of that? You chose the Dvaered way, we make it the Dvaered way! Ya know what? Meet me in orbit of {pnt} in {sys} and I'll explain you why I actually just made you a huge favour!
 Oh. If ya manage to kill Okran right now, you won't need to meet me. Just land on any Dvaered planet and you'll understand everything! All ya got to do is destroying a Dvaered Goddard! Piece of cake, right?
-Bye, mate!]]), {pnt=mem.tripnt,sys=mem.trisys}))
+Bye, mate!"]]), {pnt=mem.tripnt,sys=mem.trisys}))
    vn.na(_([[The Hyena goes away, letting you alone with the group of angry Dvaered.]]))
    vn.done()
    vn.run()
@@ -363,73 +363,73 @@ function discussWithTrickster()
 
    -- Some recurrent text
    local leave = _("Leave")
-   local why   = _("Why did you tell Okran I had betrayed him?")
-   local who   = _("Who are you actually?")
-   local what  = _("What is your motivation?")
-   local how   = _("How am I supposed to destroy a Goddard now?")
-   local where = _("Where am I supposed to find him?")
+   local why   = _([["Why did you tell Okran I had betrayed him?"]])
+   local who   = _([["Who are you actually?"]])
+   local what  = _([["What is your motivation?"]])
+   local how   = _([["How am I supposed to destroy a Goddard now?"]])
+   local where = _([["Where am I supposed to find him?"]])
 
    vn.clear()
    vn.scene()
    local trick = vn.newCharacter( _("The Trickster"), { image=portrait.getFullPath(trickPort) } )
-   trick(fmt.f(_([[Aye, {player}, how do you do?]]), {player=player.name()}))
+   trick(fmt.f(_([["Aye, {player}, how do you do?"]]), {player=player.name()}))
    vn.menu{
-      {_("Very well, thank you. And you?"), "thank"},
-      {_("Very well for someone you just tried to get murdered"), "murder"},
-      {_("I will feel much better once I'll have killed you"), "kill"},
+      {_([["Very well, thank you. And you?"]]), "thank"},
+      {_([["Very well for someone you just tried to get murdered"]]), "murder"},
+      {_([["I will feel much better once I'll have killed you"]]), "kill"},
       {why, "why"},
       {who, "who"},
    }
 
    vn.label("thank")
-   trick(fmt.f(_([[Kof kof kof! For real? You asked how I am? It's… It's the first time someone cares about me.
+   trick(fmt.f(_([["Kof kof kof! For real? You asked how I am? It's… It's the first time someone cares about me.
 Hewhewhew! You're a singular person, {player}, for sure! I wish you to never end on the bad end of a firing gun. Oh yeah! Or at least the latest possible!
-So I guess you want to know why I told the pack of boars that you betrayed them, right?]]), {player=player.name()}))
+So I guess you want to know why I told the pack of boars that you betrayed them, right?"]]), {player=player.name()}))
    vn.jump("why")
 
    vn.label("murder")
-   trick(_([[Come on… Come on… You survived, right? I would not have put you in a situation where you could have been killed! Never! We are good ol' friends, after all, aren't we? Hewhewhew!]]))
+   trick(_([["Come on… Come on… You survived, right? I would not have put you in a situation where you could have been killed! Never! We are good ol' friends, after all, aren't we? Hewhewhew!"]]))
    vn.jump("why")
 
    vn.label("kill")
-   trick(fmt.f(_([[Hewhewhew! Aren't we overreacting a bit over there, {player}? Yes we are. You won't kill me… I mean… I cannot exclude that ya kill me one day, but not today. Instead, ya will rather kill good ol' Okran. You know why?]]), {player=player.name()}))
+   trick(fmt.f(_([["Hewhewhew! Aren't we overreacting a bit over there, {player}? Yes we are. You won't kill me… I mean… I cannot exclude that ya kill me one day, but not today. Instead, ya will rather kill good ol' Okran. You know why?"]]), {player=player.name()}))
    vn.jump("why")
 
    vn.label("why")
-   trick(_([[Guess what, mate? When I told Okran you betrayed him, I made you the greatest favour possible! Yep for real!
+   trick(_([["Guess what, mate? When I told Okran you betrayed him, I made you the greatest favour possible! Yep for real!
 You know how to work your way up among the Dvaereds? Hewhewhew! By killing Dvaereds! Totally true! Hundred percent fact-checked! Dvaereds weirdos are the only kind of weirdos that will love you more if you kill other Dvaered weirdos.
-But watch out, mate! For killing Dvaereds weirdos is a subtle art, ya know? You can't just attack random Vendettas in space and win the High Command's respect. You need to kill people in the context of a "honorable fight between two respectable gentlemen". Yo, because Dvaered are for sure respectable gentlemen!]]))
-   trick(_([[So, I gave you the occasion to fight a respectable asshol… hem… gentleman. And if you kill him, the Dvaered will fully accept you among them. You'll be like their mate, ya know? Hewhewhew! For what it's worth. But you decided to join them.
-So here I am! Your white knight on his white horse. I give you not only the occasion to fight one Colonel, but also one Colonel in a Goddard. Ya'll never have any better occasion!]]))
+But watch out, mate! For killing Dvaereds weirdos is a subtle art, ya know? You can't just attack random Vendettas in space and win the High Command's respect. You need to kill people in the context of a "honorable fight between two respectable gentlemen". Yo, because Dvaered are for sure respectable gentlemen!"]]))
+   trick(_([["So, I gave you the occasion to fight a respectable asshol… hem… gentleman. And if you kill him, the Dvaered will fully accept you among them. You'll be like their mate, ya know? Hewhewhew! For what it's worth. But you decided to join them.
+So here I am! Your white knight on his white horse. I give you not only the occasion to fight one Colonel, but also one Colonel in a Goddard. Ya'll never have any better occasion!"]]))
    vn.menu{
-      {_("Thanks, genius. And how do I destroy a Goddard now?"), "how"},
+      {_([["Thanks, genius. And how do I destroy a Goddard now?"]]), "how"},
       {where, "where"}, {who, "who"}, {what, "what"}, {leave, "leave"},
    }
 
    vn.label("who")
-   trick(_([[Who I am actually? Hewhewhew! Kof kof kof! I am a loyal servant of our Emperor! Some call me 'the flying clown', because I lead a squadron named the 'flying circus'. People are funny, ya know? I'm presently Laughing My Ass Off! And your Dvaered friends call me the trickster. No clue why, Hewhewhew! Oh, and some call me 'Captain Hew Hew'.
+   trick(_([["Who I am actually? Hewhewhew! Kof kof kof! I am a loyal servant of our Emperor! Some call me 'the flying clown', because I lead a squadron named the 'flying circus'. People are funny, ya know? I'm presently Laughing My Ass Off! And your Dvaered friends call me the trickster. No clue why, Hewhewhew! Oh, and some call me 'Captain Hew Hew'.
 But that's all bullshit! For I am a real human being, with a heart in my chest. A heart that cries when people use mean words against me… Hewhewhew! Did I make you sad? Don't lie! You were sympathizing! I saw you!
-I'm so annoying! Am I not? Hewhewhew! Yep I totally am!]]))
+I'm so annoying! Am I not? Hewhewhew! Yep I totally am!"]]))
    vn.menu{
       {how, "how"}, {where, "where"}, {why, "why"}, {what, "what"}, {leave, "leave"},
    }
 
    vn.label("how")
-   trick(_([[C'mon! You're a adult, no? Ya know how to obliterate a Goddard. Don't you? Oh, I'm dumb. Maybe you don't even have the Heavy Combat Vessel License! Hewhewhew! Did I put you in a shitty situation? Kof kof kof!
+   trick(_([["C'mon! You're a adult, no? Ya know how to obliterate a Goddard. Don't you? Oh, I'm dumb. Maybe you don't even have the Heavy Combat Vessel License! Hewhewhew! Did I put you in a shitty situation? Kof kof kof!
 No, of course not! Now that Lord Fatass… Fatgun died, all his soldiers have been put to the Dvaered Military Reserve, and Okran don't command them anymore. Officially, he was also sent to the Reserve, but custom needs him to avenge his lord first, by at least killing one person (you). So he will be flying an unescorted Goddard.
-And as they teach at school, an unescorted capship is a dead capship. I recommend to use a corvette with torpedoes. Unless of course you have an other capship stronger than a Dvaered Goddard. Of course. Hewhewhew!]]))
+And as they teach at school, an unescorted capship is a dead capship. I recommend to use a corvette with torpedoes. Unless of course you have an other capship stronger than a Dvaered Goddard. Of course. Hewhewhew!"]]))
    vn.menu{
       {who, "who"}, {where, "where"}, {why, "why"}, {what, "what"}, {leave, "leave"},
    }
 
    vn.label("where")
-   trick(_([[Ah! Good question! Okran wants to kill you. I am pretty sure he will stay at Zhiru and try to intercept you each time you enter the system. I would totally do that if I was him. The Goddard system is a very central place in our galaxy. And it's a good spot for the maintenance of his ship.]]))
+   trick(_([["Ah! Good question! Okran wants to kill you. I am pretty sure he will stay at Zhiru and try to intercept you each time you enter the system. I would totally do that if I was him. The Goddard system is a very central place in our galaxy. And it's a good spot for the maintenance of his ship."]]))
    vn.menu{
       {who, "who"}, {how, "how"}, {why, "why"}, {what, "what"}, {leave, "leave"},
    }
 
    vn.label("what")
-   trick(_([[My motivation? But… I just wanted to be kind to you. I am a philanthropist, you know? Hewhewhew!]])) -- He certainly has a doubtful motivation that has to be determined later
+   trick(_([["My motivation? But… I just wanted to be kind to you. I am a philanthropist, you know? Hewhewhew!"]])) -- He certainly has a doubtful motivation that has to be determined later
    vn.menu{
       {who, "who"}, {how, "how"}, {why, "why"}, {where, "where"}, {leave, "leave"},
    }
