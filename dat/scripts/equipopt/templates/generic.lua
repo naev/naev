7efbe32a85b905fec2d963f2bb480f942aa38221
Edgar Simo-Serra <bobbens@gmail.com>
2022-08-17 18:23:41 +0900

    equipopt.generic supports outfits_add.

--- dat/scripts/equipopt/templates/generic.lua
+++ dat/scripts/equipopt/templates/generic.lua
@@ -20,6 +20,9 @@ local function equip_generic( p, opt_params, cores, outfits )
    -- Get stuff
    local ocores = ecores.get( p, { all=cores } )
    local ooutfits = eoutfits[ outfits ].set
+   if opt_params.outfits_add then
+      ooutfits = eoutfits.merge{ ooutfits, opt_params.outfits_add }
+   end
 
    -- Set some pilot meta-data
    local mem = p:memory()
