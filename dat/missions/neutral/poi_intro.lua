3b063f2e923929fd4ff33cdeee8fec14f1d211fa
Edgar Simo-Serra <bobbens@gmail.com>
2022-08-18 16:48:30 +0900

    Nelly's nemesis flies a koala.

--- dat/missions/neutral/poi_intro.lua
+++ dat/missions/neutral/poi_intro.lua
@@ -319,7 +319,7 @@ function heartbeat ()
 
    elseif cutscene == 2 then
       if nelly:pos():dist( player.pos() ) < 1000 then
-         nemesis = pilot.add( "Ancestor", fct_nemesis, pos + vec2.new( 5000, rnd.angle() ), _("Nemesis") )
+         nemesis = pilot.add( "Koala", fct_nemesis, pos + vec2.new( 5000, rnd.angle() ), _("Nemesis") )
          nelly:broadcast(_("Wait? We were followed?"))
          cutscene = 3
          broadcasted = 0
