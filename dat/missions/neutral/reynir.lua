naev/naev

commit 8153ed2a0d2d27bc0fd5d31aad73e83981c849c9

Use new conditional API for reynir.

dat/missions/neutral/reynir.lua
@@ -5,15 +5,15 @@
  <priority>1</priority>
  <chance>2</chance>
  <location>Bar</location>
- <cond>(function ()
-           local count = 0
-           for i, p in pairs(system.cur():spobs()) do
-              if p:services()["inhabited"] then
-                 count=count+1
-              end
-           end
-           return count &gt; 1
-        end)()</cond>
+ <cond>
+   local count = 0
+   for i, p in pairs(system.cur():spobs()) do
+      if p:services()["inhabited"] then
+         return true
+      end
+   end
+   return false
+ </cond>
  <notes>
   <tier>1</tier>
  </notes>

commit d538ee3620010b2ba56b0fe9b7134e3ade134cf3

reynir.lua: fix off-by-one
An off by one error was introduced in 8153ed2a0d2d27bc0fd5d31aad73e83981c849c9.

dat/missions/neutral/reynir.lua
@@ -9,7 +9,10 @@
    local count = 0
    for i, p in pairs(system.cur():spobs()) do
       if p:services()["inhabited"] then
-         return true
+         count=count+1
+      end
+      if count == 2 then
+          return true
       end
    end
    return false