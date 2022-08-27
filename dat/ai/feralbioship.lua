827ca60839449bc371ddd633ff1f8fb53c3cb0bd
Edgar Simo-Serra <bobbens@gmail.com>
2022-08-28 01:18:40 +0900

    Feral bioships bite asteroids to death now.

--- dat/ai/feralbioship.lua
+++ dat/ai/feralbioship.lua
@@ -7,4 +7,55 @@ mem.aggressive    = true
 mem.formation     = "vee" -- TODO random
 mem.atk_kill      = true
 mem.atk_board     = false
-mem.comm_no = _("No response.")
+mem.comm_no       = _("No response.")
+mem.loiter        = math.huge
+
+-- luacheck: globals mine_bite (AI Task functions passed by name)
+function mine_bite( ast )
+   if not ast:exists() then
+      ai.poptask()
+      return
+   end
+
+   local p = ai.pilot()
+
+   ai.setasterotarget( ast )
+
+   local target = ast:pos()
+   local vel = ast:vel()
+   local _dist, angle = vec2.polar( p:pos() - target )
+
+   -- First task : place the ship close to the asteroid
+   local goal = ai.face_accurate( target, vel, 0, angle, mem.Kp, mem.Kd )
+
+   local dir  = ai.face(goal)
+   if dir < math.rad(20) then
+      local dtime = 3
+      if mem._o.bite_lust then
+         dtime = dtime+2
+      end
+      local s = p:stats()
+      if ai.dist( target ) < (s.speed + (s.thrust+800)/3)*dtime then
+         p:outfitToggle( mem._o.bite, true )
+      end
+   end
+
+   if dir < math.rad(10) then
+      ai.accel()
+   end
+end
+
+local idle_old = idle
+function idle ()
+   if not mem._o or not mem._o.bite then
+      return idle_old()
+   end
+
+   if rnd.rnd() < 0.5 then
+      local ast = asteroid.get( mem.mining_field ) -- Get a random asteroid in the system (or current mining field)
+      mem.mining_field = ast:field()
+      ai.pushtask( "mine_bite", ast )
+   else
+      return idle_old()
+   end
+end
