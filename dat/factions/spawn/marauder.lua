c6cbb8f79dbc629b6cdbaf5b2850767c20bc1673
Edgar Simo-Serra <bobbens@gmail.com>
2022-09-03 12:41:32 +0900

    Fixed marauder spawning script.

--- dat/factions/spawn/marauder.lua
+++ dat/factions/spawn/marauder.lua
@@ -4,8 +4,9 @@ local fmarauder = faction.get("Marauder")
 -- @brief Creation hook. (May be fine-tuned by reusing only spir.spawn_* and writing a custom create().)
 function create ( max )
    return spir.create( fmarauder, max, { reweight = function( weights )
-      weights[ spir.weights_loner_strong ] = weights[ spir.weights_loner_strong ] - 100
-      weights[ spir.weights_squad ] = weights[ spir.weights_squad ] - 100
-      weights[ spir.weights_capship ] = weights[ spir.weights_capship ] - 200
+      weights[ spir.spawn_loner_strong ] = weights[ spir.spawn_loner_strong ] - 100
+      weights[ spir.spawn_squad ]        = weights[ spir.spawn_squad ] - 100
+      weights[ spir.spawn_capship ]      = weights[ spir.spawn_capship ] - 200
+      return weights
    end } )
 end
