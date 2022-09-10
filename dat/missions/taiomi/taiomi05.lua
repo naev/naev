237727f0ccddb9566d154221c6fb8470f0c96def
Edgar Simo-Serra <bobbens@gmail.com>
2022-09-11 01:09:09 +0900

    Some more taiomi05. progress.

--- dat/missions/taiomi/taiomi05.lua
+++ dat/missions/taiomi/taiomi05.lua
@@ -28,8 +28,10 @@ local taiomi = require "common.taiomi"
 local reward = taiomi.rewards.taiomi05
 local title = _("Missing Drones")
 local base, basesys = spob.getS("One-Wing Goddard")
+-- The systems below backtrack from Taiomi to Haven
 local scenesys = system.get("Bastion")
 local fightsys = system.get("Gamel")
+local lastsys = system.get("Haven")
 
 --[[
    0: mission started
@@ -128,18 +130,61 @@ function land ()
 
    if mem.state==0 then
       local dead = taiomi.young_died()
-      --local alive = taiomi.young_alive()
+      local alive = taiomi.young_alive()
 
       vn.clear()
       vn.scene()
       local s = vn.newCharacter( taiomi.vn_scavenger() )
       vn.transition( taiomi.scavenger.transition )
       vn.na(_([[You disembark and are soon met with a Scavenger you can only describe as solemn.]]))
+      s(fmt.f(_([["It is irrational for us to leave the safety of Taiomi unless it is necessary. I have no idea what compelled {younga} and {youngb}. Minimization of risk is essential to our survival, which is why I entrusted the acquirement of Therite to you. Could this be a program fault?"]]),
+         {younga=taiomi.younga.name, youngb=taiomi.youngb.name}))
+      vn.menu{
+         {_([["Is that not curiosity?"]]), "cont01_curiosity"},
+         {_([["Maybe they wanted to see the outside world?"]]), "cont01_curiosity"},
+         {_([["Definitely a software bug."]]), "cont01_software"},
+         {_([[…]]), "cont01"},
+      }
+
+      vn.label("cont01_curiosity")
+      s(_([["Such irrationality does not compute. It is very unlikely of our kind to behave in such suboptimal behaviour. My simulations indicate that it is more probable that this be a degeneration of our computational cortex after excessive replication."]]))
+      vn.jump("cont01")
+
+      vn.label("cont01_software")
+      s(_([["It is statistically unlikely that our diagnostics did not pick up such an important issue. Maybe the diagnostic protocols need to be revised. It would not be the first time we had issues with them."]]))
+      vn.jump("cont01")
+
+      vn.label("cont01")
+      s(fmt.f(_([["Being too late to take preemptive measures, we have no other option than be proactive in searching for {dead}."]]),
+         {dead=dead}))
+      s(fmt.f(_([["The only information we were able to obtain from {alive} is that both {alive} and {dead} followed you. It is not exactly clear what happened, but they got surprised and fled. At that time, it seems like they got split up. While {alive} made it back, {dead} is still missing"]]),
+         {alive=alive, dead=dead}))
+      s(fmt.f(_([["We have tried to get more details, but {alive} seems to be stuck in a loop. Until their computational cortex recovers, we have no option but to infer the most probable situation from the available data to find {dead}."]]),
+         {alive=alive, dead=dead}))
+      s(fmt.f(_([["I have simulated the most likely situations and devised a plan that maximizes the chance of recovering {dead} safely by minimizing the amount of time to find them, however, this will come at a risk for us."]]),
+         {dead=dead}))
+      s(fmt.f(_([["The core idea is to backtrack the most probably path, starting with {scenesys}, then {fightsys}, and finally {lastsys}. At each system, we will make a run to an optimal position, and I will begin broadcasting a special code while listening to possible answers."]]),
+         {scenesys=scenesys, fightsys=fightsys, lastsys=lastsys}))
+      s(fmt.f(_([["There is no time to explain the details, but this code will allow detecting {dead}. However, I will remain largely immobile, and it is likely that it will attract unwanted attention in the system. I will need you to protect me for the duration of the signal."]]),
+         {dead=dead}))
+
+      vn.menu{
+         {_([["Understood."]]), "cont02"},
+         {_([["Wait, you'll be coming?"]]), "cont02_q"},
+         {_([[…]]), "cont02"},
+      }
+
+      vn.label("cont02_q")
+      s(fmt.f(_([["Yes. While I believe your piloting skills are excellent, they will not be sufficient to find {dead} in time. While I am not a fighter, this plan maximizes the probability of success. All other plans are suboptimal."]]),
+         {dead=dead}))
+
+      vn.label("cont02")
       s(_([[""]]))
+
       vn.done( taiomi.scavenger.transition )
       vn.run()
 
-      mem.marker = misn.markerMove( base )
+      mem.marker = misn.markerMove( scenesys )
 
       misn.osdCreate( title, {
          fmt.f(_("Search for {name} ({sys})"),{name=dead, sys=scenesys}),
