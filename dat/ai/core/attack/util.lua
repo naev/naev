99d84904ab94b4068ce8ec017ff2dd02c03ff7a8
Edgar Simo-Serra <bobbens@gmail.com>
2022-09-11 15:19:20 +0900

    Minor optimizations and have AI use preferred_enemy when attacking.

--- dat/ai/core/attack/util.lua
+++ dat/ai/core/attack/util.lua
@@ -123,8 +123,7 @@ end
 -- big game hunter attack pattern using heuristic target identification.
 --]]
 function atk.heuristic_big_game_think( target, _si )
-   local enemy         = ai.getenemy_heuristic(0.9, 0.9, 0.9, 20000)
-   local nearest_enemy = ai.getenemy()
+   local enemy = atk.preferred_enemy()
 
    local dist = ai.dist(target)
    local range = ai.getweaprange(3, 0)
@@ -135,8 +134,11 @@ function atk.heuristic_big_game_think( target, _si )
       if dist > range * mem.atk_changetarget then
          ai.pushtask("attack", enemy )
       end
+      return
+   end
 
-   elseif nearest_enemy ~= target and nearest_enemy ~= nil then
+   local nearest_enemy = ai.getenemy()
+   if nearest_enemy ~= target and nearest_enemy ~= nil then
       -- Shouldn't switch targets if close
       if dist > range * mem.atk_changetarget then
          ai.pushtask("attack", nearest_enemy )
