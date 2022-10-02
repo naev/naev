42bf79d9913c4d19847700d4de4b5b016559cc4f
Edgar Simo-Serra <bobbens@gmail.com>
2022-10-02 10:30:40 +0900

    Hide name of unknown system when adding notes. Fixes #2208

--- src/map.c
+++ src/map.c
@@ -2194,7 +2194,7 @@ static void map_buttonMarkSystem( unsigned int wid, const char* str )
       if (sys_isFlag(sys, SYSTEM_PMARKED))
          sys_rmFlag(sys, SYSTEM_PMARKED);
       else {
-         sys->note = dialogue_input(_("Add System Note"), 0, 60, _("Write a note about the #o%s#0 system:"), sys->name );
+         sys->note = dialogue_input(_("Add System Note"), 0, 60, _("Write a note about the #o%s#0 system:"), sys_isKnown(sys) ? sys->name : _("Unknown") );
          if (sys->note != NULL)
             sys_setFlag(sys, SYSTEM_PMARKED);
       }
