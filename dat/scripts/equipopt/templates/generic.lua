814ed4acc8f549cb919967c8b754ecf9b371a6aa
Edgar Simo-Serra <bobbens@gmail.com>
2022-10-04 22:00:14 +0900

    Properly initialize the opt_params for generic equipopt.

--- dat/scripts/equipopt/templates/generic.lua
+++ dat/scripts/equipopt/templates/generic.lua
@@ -9,7 +9,7 @@ local eoutfits = require 'equipopt.outfits'
 --    @param p Pilot to equip
 --]]
 local function equip_generic( p, opt_params, cores, outfits )
-   opt_params  = opt_params or nil
+   opt_params  = opt_params or {}
    cores       = cores or "standard"
    outfits     = outfits or cores
 
