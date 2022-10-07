7c8bcfb239af0b4b25e83e243e6670cb549a4d05
Edgar Simo-Serra <bobbens@gmail.com>
2022-10-07 13:45:07 +0900

    Made luacheckrc_extractor.py more suited for meson.

--- utils/luacheckrc_extractor.py
+++ utils/luacheckrc_extractor.py
@@ -1,40 +1,52 @@
 #!/usr/bin/python
 import argparse
 import re
+import os
+import sys
 
 def parse_api( filename ):
-	with open( filename, 'r' ) as f:
-		data = f.read()
+    with open( filename, 'r' ) as f:
+        data = f.read()
 
-	result = re.findall("static const luaL_Reg.*?\[\] = {(.+?)};", data, re.S)
-	assert( len(result)==1 )
-	result = re.findall( "\".*\"", result[0] )
-	funclist = []
-	for r in result:
-		funclist += [r.replace("\"","")]
+    result = re.findall("static const luaL_Reg.*?\[\] = {(.+?)};", data, re.S)
+    assert( len(result)==1 )
+    result = re.findall( "\".*\"", result[0] )
+    funclist = []
+    for r in result:
+        funclist += [r.replace("\"","")]
 
-	return funclist
+    return funclist
 
 def generate_api( modname, filename ):
-	api = parse_api( filename )
-	api.sort()
+    api = parse_api( filename )
+    api.sort()
 
-	apilist = ""
-	for a in api:
-		apilist += f"         {a} = {{}},\n"
+    apilist = ""
+    for a in api:
+        apilist += f"            {a} = {{}},\n"
 
-	return f"""return read_globals = {{
-   {modname} = {{
-      fields = {{
-{apilist}      }},
-   }},
+    return f"""stds.{modname} = {{
+   read_globals = {{
+      {modname} = {{
+         fields = {{
+{apilist}         }},
+      }},
+   }}
 }}"""
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser( description='Generates API for luacheckrc from a C source file.' )
-	parser.add_argument('modname',  metavar='MODNAME',  type=str, help='Name of the module to generate.')
-	parser.add_argument('filename', metavar='FILENAME', type=str, help='Name of the file name to parse.')
-	args = parser.parse_args()
-	print( generate_api( args.modname, args.filename ) )
+    parser = argparse.ArgumentParser( description='Generates API for luacheckrc from a C source file.' )
+    parser.add_argument('filename', metavar='FILENAME', nargs='+', type=str, help='Name of the file name to parse.')
+    parser.add_argument('--output', default=None, help="Name of the output file.")
+    args = parser.parse_args()
+
+    if not args.output:
+        output = sys.stdout
+    else:
+        output = open(args.output,'w')
+
+    for filename in args.filename:
+        modname = os.path.basename(filename).replace('nlua_','').replace('.c','')
+        output.write( generate_api( modname, filename )+'\n' )
 
 #api = generate_api( "news", "/home/ess/naev/src/nlua_news.c" )
