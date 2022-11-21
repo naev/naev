014cdc9c7fc7d8a1bfe7cbe62088820b14a99b4b
Edgar Simo-Serra <bobbens@gmail.com>
2022-11-21 18:49:22 +0900

    Plasma burst applies plasma effects.

--- dat/outfits/bioship/skills/plasma_burst.lua
+++ dat/outfits/bioship/skills/plasma_burst.lua
@@ -1,6 +1,6 @@
 local explosion = require "luaspfx.explosion"
 
-local damage = 300
+local damage = 100
 local penetration = 0.5
 local radius = 200
 local cooldown = 20
@@ -11,21 +11,28 @@ local function activate( p, po )
       return false
    end
 
+   local dur = 10
    local pos = p:pos()
    for k,t in ipairs(p:getEnemies( radius )) do
+      local ts = t:stats()
+      local dmg = damage * (1 - math.min( 1, math.max( 0, ts.absorb - penetration ) ))
       local norm, angle = (t:pos() - pos):polar()
       local mod = 1 - norm / radius
       local mass = math.pow( damage / 15, 2 )
       -- Damage and knockback
       t:damage( damage, 0, penetration, "impact", p )
       t:knockback( mass, vec2.newP( mod*radius, angle ), pos, 1 )
+      -- Nasty effects
+      t:effectAdd( "Plasma Burn", dur, dmg )
+      t:effectAdd( "Paralyzing Plasma", dur )
+      t:effectAdd( "Crippling Plasma", dur )
    end
 
    -- TODO visuals and sound
    explosion( pos, p:vel(), 400, nil, {
-      speed = 0.5,
-      grain = 0.3,
-      steps = 8,
+      speed     = 0.5,
+      grain     = 0.3,
+      steps     = 8,
       smokiness = 0.4,
       rollspeed = 0.3,
       smokefade = 1.6,
