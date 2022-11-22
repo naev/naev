14bb77ca1f6a9ec323e43c683ad194608f922658
Edgar Simo-Serra <bobbens@gmail.com>
2022-11-22 19:28:43 +0900

    Player will always be considered to be attacking (works better with AI)

--- dat/ai/player.lua
+++ dat/ai/player.lua
@@ -1,3 +1,7 @@
 require 'ai.core.core'
 mem.aggressive = true -- Player is an asshole
 mem.atk_kill = false -- Should propagate to fighters
+
+function should_attack( _enemy, _si, _aggressor )
+   return true -- should work better with escorts
+end
