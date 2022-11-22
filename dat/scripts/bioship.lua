79d228840e48d263775096f415a68e0f25c873ac
Edgar Simo-Serra <bobbens@gmail.com>
2022-11-22 11:14:07 +0900

    Added some annotations.

--- dat/scripts/bioship.lua
+++ dat/scripts/bioship.lua
@@ -184,12 +184,12 @@ local function skill_enable( p, s )
 end
 
 local _maxstageSize = {
-   5,
-   6,
-   7,
-   8,
-   9,
-  10
+   5, -- interceptor
+   6, -- fighter / bomber
+   7, -- corvette
+   8, -- destroyer
+   9, -- cruiser
+  10, -- carrier / battleship
 }
 function bioship.maxstage( p )
    return _maxstageSize[ p:ship():size() ]
