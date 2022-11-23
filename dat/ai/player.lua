15fd9658250f8fe30434ed470d7e3d2eccad76c1
Edgar Simo-Serra <bobbens@gmail.com>
2022-11-24 01:19:12 +0900

    Player should not error out as much with should_attack.

--- dat/ai/player.lua
+++ dat/ai/player.lua
@@ -2,6 +2,9 @@ require 'ai.core.core'
 mem.aggressive = true -- Player is an asshole
 mem.atk_kill = false -- Should propagate to fighters
 
-function should_attack( _enemy, _si, _aggressor )
+function should_attack( enemy, _si, _aggressor )
+   if not enemy or not enemy:exists() then
+      return false
+   end
    return true -- should work better with escorts
 end
