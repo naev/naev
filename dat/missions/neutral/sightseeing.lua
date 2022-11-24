d230d25f24e000e65a3d943a1cce14ecb9e20501
Edgar Simo-Serra <bobbens@gmail.com>
2022-11-25 01:11:19 +0900

    comment

--- dat/missions/neutral/sightseeing.lua
+++ dat/missions/neutral/sightseeing.lua
@@ -120,6 +120,7 @@ function create ()
 
    -- Set mission details
    misn.setTitle( fmt.f( _("Sightseeing in the {sys} System"), {sys=mem.missys} ) )
+   -- TODO should probably not just mention luxury yachts, but since they're the only luxury type ship, I guess it works for now.
    misn.setDesc( fmt.f(_([[Several passengers wish to go off-world and go on a sightseeing tour. Navigate to specified {amount} different attractions in the {sys} system.
 
 #nAttractions:#0 {amount}
