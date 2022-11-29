1ba556f9759e714830d06ae0b2ac1cecf3c069d7
Edgar Simo-Serra <bobbens@gmail.com>
2022-11-29 14:50:42 +0900

    Started work on racetrack spfx.

---
+++ dat/scripts/luaspfx/racetrack.lua
@@ -0,0 +1,74 @@
+local lg = require 'love.graphics'
+local lf = require 'love.filesystem'
+local love_shaders = require 'love_shaders'
+
+local track_shader, buoy_gfx, buoy_w, buoy_h
+
+local function update( s, dt )
+   local d = s:data()
+   d.timer = d.timer + dt
+
+   if d.ready then
+      local p = player.pos()
+      local ret = vec2.collideCircleLine( player.pos(), 30, d.seg1, d.seg2 )
+      if ret then
+         d.ready = false
+         d.activate()
+      end
+      d.ppos = p
+   end
+end
+
+local function render_buoy( v, z, w, h )
+   local x, y = gfx.screencoords( v, true ):get()
+   buoy_gfx:draw( x-w*0.5, y-h*0.5, 0, z )
+   buoy_gfx:draw( x-w*0.5, y-h*0.5, 0, z )
+end
+
+local function render( sp, x, y, z )
+   local d = sp:data()
+   track_shader:send( "u_time", d.timer )
+   track_shader:send( "u_size", d.size )
+
+   local sw = d.size * 0.2 * z
+   local sh = d.size * z
+   local old_shader = lg.getShader()
+   lg.setShader( track_shader )
+   lg.setColor( d.col )
+   love_shaders.img:draw( x-sw*0.5, y-sh*0.5, d.rot, sw, sh )
+   lg.setShader( old_shader )
+
+   lg.setColor{1,1,1}
+   local w, h = buoy_w*z, buoy_h*z
+   render_buoy( d.seg1, z, w, h )
+   render_buoy( d.seg2, z, w, h )
+end
+
+local function racetrack( pos, rot, activate, params )
+   params = params or {}
+   -- Lazy loading shader
+   if not track_shader then
+      local track_bg_shader_frag = lf.read( "scripts/luaspfx/shaders/track.frag" )
+      track_shader = lg.newShader( track_bg_shader_frag )
+
+      buoy_gfx = lg.newImage( "gfx/spob/space/jumpbuoy.webp" )
+      buoy_w, buoy_h = buoy_gfx:getDimensions()
+   end
+
+   local size = params.size or 500
+
+   local s = spfx.new( math.huge, update, nil, nil, render, pos, nil, nil, size )
+   local d  = s:data()
+   d.timer  = 0
+   d.size   = size
+   d.col    = params.col or {0, 1, 1, 0.3}
+   d.rot    = rot
+   d.ready  = false
+   d.seg1   = pos-vec2.newP(size*0.5,math.pi/2-rot)
+   d.seg2   = pos+vec2.newP(size*0.5,math.pi/2-rot)
+   d.ppos   = player.pos()
+   d.activate = activate
+   return s
+end
+
+return racetrack
---
+++ dat/scripts/luaspfx/shaders/track.frag
@@ -0,0 +1,9 @@
+#include "lib/sdf.glsl"
+
+uniform float u_time = 0.0;
+uniform float u_size = 100.0;
+
+vec4 effect( vec4 color, Image tex, vec2 uv, vec2 px )
+{
+   return color;
+}
