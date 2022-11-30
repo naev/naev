b236efade68b5ab3e6cf2b13e85a75b00a2681f6
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-01 00:05:42 +0900

    Fixed scans not passing scalars to vec2.newP

--- dat/ai/core/misc/scans.lua
+++ dat/ai/core/misc/scans.lua
@@ -47,7 +47,7 @@ function scans.investigate( target )
    -- Guess the pilot will be randomly between the current position and the
    -- future position if they go in the same direction with the same velocity
    local ttl = ai.dist(target) / p:stats().speed_max
-   local fpos = target:pos() + vec2.newP( target:vel()*ttl, target:dir() ) * rnd.rnd()
+   local fpos = target:pos() + vec2.newP( target:vel():mod()*ttl, target:dir() ) * rnd.rnd()
    ai.pushtask("inspect_moveto", fpos )
 end
 
