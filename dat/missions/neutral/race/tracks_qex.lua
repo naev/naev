f1e8c31a9cc51e587a8f5f7109c345c28a877dc4
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-01 17:28:22 +0900

    race_qex: split and organize stuff a bit

---
+++ dat/missions/neutral/race/tracks_qex.lua
@@ -0,0 +1,296 @@
+return {
+   {
+      name = _("Qex Tour"),
+      track = {
+         {
+            vec2.new(  5e3,  2e3 ), -- start near Qex IV
+            vec2.new( -1e3,  3e3 ),
+            vec2.new(  1e3, -1e3 ),
+            vec2.new(  2e3,  2e3 ),
+         }, {
+            vec2.new(  2e3,  2e3 ),
+            vec2.new( -1e3,  1e3 ),
+            vec2.new(  2e3,    0 ),
+            vec2.new( -1e3,  5e3 ), -- passes by Qex V
+         }, {
+            vec2.new( -1e3,  5e3 ),
+            vec2.new( -6e3,    0 ),
+            vec2.new(  1e3,  1e3 ),
+            vec2.new( -8e3,  2e3 ), -- circle Qex II
+         }, {
+            vec2.new( -8e3,  2e3 ),
+            vec2.new( -1e3, -1e3 ),
+            vec2.new( -1e3,  1e3 ),
+            vec2.new( -8e3,  4e3 ),
+         }, {
+            vec2.new( -8e3,  4e3 ),
+            vec2.new(  1e3, -1e3 ),
+            vec2.new(    0,  2e3 ),
+            vec2.new( -1e3,  2e3 ), -- Through asteroid field
+         }, {
+            vec2.new( -1e3,  2e3 ),
+            vec2.new(    0, -4e3 ),
+            vec2.new(  1e3, -3e3 ),
+            vec2.new(  5e3,  2e3 ),
+         }
+      },
+   }, {
+      name = _("Easy"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 939, 394 ),
+            vec2.new( 6, -128 ),
+            vec2.new( 70, 2 ),
+            vec2.new( 874, 134 ),
+         },
+         {
+            vec2.new( 874, 134 ),
+            vec2.new( -70, -2 ),
+            vec2.new( -34, -33 ),
+            vec2.new( 809, 208 ),
+         },
+         {
+            vec2.new( 809, 208 ),
+            vec2.new( 34, 39 ),
+            vec2.new( 118, 57 ),
+            vec2.new( 725, 270 ),
+         },
+         {
+            vec2.new( 725, 270 ),
+            vec2.new( -118, -57 ),
+            vec2.new( 39, -42 ),
+            vec2.new( 470, 250 ),
+         },
+         {
+            vec2.new( 470, 250 ),
+            vec2.new( -39, 42 ),
+            vec2.new( -91, 17 ),
+            vec2.new( 549, 338 ),
+         },
+         {
+            vec2.new( 549, 338 ),
+            vec2.new( 147, -31 ),
+            vec2.new( -144, 53 ),
+            vec2.new( 735, 395 ),
+         },
+         {
+            vec2.new( 735, 395 ),
+            vec2.new( 82, -37 ),
+            vec2.new( -15, 226 ),
+            vec2.new( 939, 394 ),
+         },
+      },
+   }, {
+      name = _("Race 2"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 942, 381 ),
+            vec2.new( 5, -104 ),
+            vec2.new( 69, -1 ),
+            vec2.new( 896, 165 ),
+         },
+         {
+            vec2.new( 896, 165 ),
+            vec2.new( -69, 1 ),
+            vec2.new( 61, 55 ),
+            vec2.new( 733, 177 ),
+         },
+         {
+            vec2.new( 733, 177 ),
+            vec2.new( -61, -55 ),
+            vec2.new( 86, 80 ),
+            vec2.new( 551, 176 ),
+         },
+         {
+            vec2.new( 551, 176 ),
+            vec2.new( -86, -80 ),
+            vec2.new( -169, -106 ),
+            vec2.new( 544, 322 ),
+         },
+         {
+            vec2.new( 544, 322 ),
+            vec2.new( 73, 42 ),
+            vec2.new( -154, 98 ),
+            vec2.new( 723, 379 ),
+         },
+         {
+            vec2.new( 723, 379 ),
+            vec2.new( 154, -99 ),
+            vec2.new( 288, -3 ),
+            vec2.new( 687, 465 ),
+         },
+         {
+            vec2.new( 687, 465 ),
+            vec2.new( -181, -1 ),
+            vec2.new( 36, -183 ),
+            vec2.new( 402, 398 ),
+         },
+         {
+            vec2.new( 402, 398 ),
+            vec2.new( -15, 66 ),
+            vec2.new( -81, 55 ),
+            vec2.new( 515, 506 ),
+         },
+         {
+            vec2.new( 515, 506 ),
+            vec2.new( 81, -56 ),
+            vec2.new( -105, 94 ),
+            vec2.new( 702, 532 ),
+         },
+         {
+            vec2.new( 702, 532 ),
+            vec2.new( 105, -94 ),
+            vec2.new( -83, 111 ),
+            vec2.new( 896, 503 ),
+         },
+         {
+            vec2.new( 896, 503 ),
+            vec2.new( 21, -30 ),
+            vec2.new( 0, 63 ),
+            vec2.new( 942, 381 ),
+         },
+      },
+   }, {
+      name = _("Race 3"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 942, 466 ),
+            vec2.new( 3, -288 ),
+            vec2.new( 132, -1 ),
+            vec2.new( 830, 176 ),
+         },
+         {
+            vec2.new( 830, 176 ),
+            vec2.new( -132, 1 ),
+            vec2.new( -2, -113 ),
+            vec2.new( 711, 291 ),
+         },
+         {
+            vec2.new( 711, 291 ),
+            vec2.new( 2, 113 ),
+            vec2.new( -56, 1 ),
+            vec2.new( 772, 409 ),
+         },
+         {
+            vec2.new( 772, 409 ),
+            vec2.new( 56, -1 ),
+            vec2.new( -7, -172 ),
+            vec2.new( 830, 577 ),
+         },
+         {
+            vec2.new( 830, 577 ),
+            vec2.new( -5, 178 ),
+            vec2.new( 144, -11 ),
+            vec2.new( 651, 698 ),
+         },
+         {
+            vec2.new( 651, 698 ),
+            vec2.new( -123, 1 ),
+            vec2.new( 1, 116 ),
+            vec2.new( 530, 585 ),
+         },
+         {
+            vec2.new( 530, 585 ),
+            vec2.new( -1, -116 ),
+            vec2.new( -114, -3 ),
+            vec2.new( 651, 466 ),
+         },
+         {
+            vec2.new( 651, 466 ),
+            vec2.new( 59, -1 ),
+            vec2.new( 2, -63 ),
+            vec2.new( 709, 528 ),
+         },
+         {
+            vec2.new( 709, 528 ),
+            vec2.new( 2, 51 ),
+            vec2.new( -170, 0 ),
+            vec2.new( 884, 579 ),
+         },
+         {
+            vec2.new( 884, 579 ),
+            vec2.new( 62, -1 ),
+            vec2.new( 4, 108 ),
+            vec2.new( 942, 471 ),
+         },
+      },
+   }, {
+      name = _("Race 4"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 1062, 350 ),
+            vec2.new( 3, -112 ),
+            vec2.new( 117, -4 ),
+            vec2.new( 945, 236 ),
+         },
+         {
+            vec2.new( 945, 236 ),
+            vec2.new( -117, 4 ),
+            vec2.new( 2, 60 ),
+            vec2.new( 825, 171 ),
+         },
+         {
+            vec2.new( 825, 171 ),
+            vec2.new( 4, -54 ),
+            vec2.new( 120, -4 ),
+            vec2.new( 706, 118 ),
+         },
+         {
+            vec2.new( 706, 118 ),
+            vec2.new( -240, -1 ),
+            vec2.new( 3, -108 ),
+            vec2.new( 471, 231 ),
+         },
+         {
+            vec2.new( 471, 231 ),
+            vec2.new( -3, 108 ),
+            vec2.new( -113, -1 ),
+            vec2.new( 587, 350 ),
+         },
+         {
+            vec2.new( 587, 350 ),
+            vec2.new( 113, 1 ),
+            vec2.new( 2, -113 ),
+            vec2.new( 709, 466 ),
+         },
+         {
+            vec2.new( 709, 466 ),
+            vec2.new( 1, 175 ),
+            vec2.new( 120, 3 ),
+            vec2.new( 590, 639 ),
+         },
+         {
+            vec2.new( 590, 639 ),
+            vec2.new( -120, -3 ),
+            vec2.new( 2, 121 ),
+            vec2.new( 474, 521 ),
+         },
+         {
+            vec2.new( 474, 521 ),
+            vec2.new( 0, -50 ),
+            vec2.new( -116, -2 ),
+            vec2.new( 589, 470 ),
+         },
+         {
+            vec2.new( 589, 470 ),
+            vec2.new( 116, 2 ),
+            vec2.new( -118, -2 ),
+            vec2.new( 946, 468 ),
+         },
+         {
+            vec2.new( 946, 468 ),
+            vec2.new( 118, 2 ),
+            vec2.new( -3, 112 ),
+            vec2.new( 1062, 350 ),
+         },
+      },
+   },
+}
--- dat/missions/neutral/race_qex.lua
+++ dat/missions/neutral/race_qex.lua
@@ -15,9 +15,11 @@
 --]]
 local fmt = require "format"
 local spfxtrack = require "luaspfx.racetrack"
+local vn = require "vn"
 local vntk = require "vntk"
 local luatk = require "luatk"
 local bezier = require "luatk.bezier"
+local portrait = require 'portrait'
 
 local elapsed_time, race_done
 
@@ -37,302 +39,11 @@ local function cubicBezier( t, p0, p1, p2, p3 )
    return lerp(l4, l5, t)
 end
 
-local track_list = {
-   {
-      name = _("Qex Tour"),
-      track = {
-         {
-            vec2.new(  5e3,  2e3 ), -- start near Qex IV
-            vec2.new( -1e3,  3e3 ),
-            vec2.new(  1e3, -1e3 ),
-            vec2.new(  2e3,  2e3 ),
-         }, {
-            vec2.new(  2e3,  2e3 ),
-            vec2.new( -1e3,  1e3 ),
-            vec2.new(  2e3,    0 ),
-            vec2.new( -1e3,  5e3 ), -- passes by Qex V
-         }, {
-            vec2.new( -1e3,  5e3 ),
-            vec2.new( -6e3,    0 ),
-            vec2.new(  1e3,  1e3 ),
-            vec2.new( -8e3,  2e3 ), -- circle Qex II
-         }, {
-            vec2.new( -8e3,  2e3 ),
-            vec2.new( -1e3, -1e3 ),
-            vec2.new( -1e3,  1e3 ),
-            vec2.new( -8e3,  4e3 ),
-         }, {
-            vec2.new( -8e3,  4e3 ),
-            vec2.new(  1e3, -1e3 ),
-            vec2.new(    0,  2e3 ),
-            vec2.new( -1e3,  2e3 ), -- Through asteroid field
-         }, {
-            vec2.new( -1e3,  2e3 ),
-            vec2.new(    0, -4e3 ),
-            vec2.new(  1e3, -3e3 ),
-            vec2.new(  5e3,  2e3 ),
-         }
-      },
-   }, {
-      name = _("Easy"),
-      scale = 15,
-      center = true,
-      track = {
-         {
-            vec2.new( 939, 394 ),
-            vec2.new( 6, -128 ),
-            vec2.new( 70, 2 ),
-            vec2.new( 874, 134 ),
-         },
-         {
-            vec2.new( 874, 134 ),
-            vec2.new( -70, -2 ),
-            vec2.new( -34, -33 ),
-            vec2.new( 809, 208 ),
-         },
-         {
-            vec2.new( 809, 208 ),
-            vec2.new( 34, 39 ),
-            vec2.new( 118, 57 ),
-            vec2.new( 725, 270 ),
-         },
-         {
-            vec2.new( 725, 270 ),
-            vec2.new( -118, -57 ),
-            vec2.new( 39, -42 ),
-            vec2.new( 470, 250 ),
-         },
-         {
-            vec2.new( 470, 250 ),
-            vec2.new( -39, 42 ),
-            vec2.new( -91, 17 ),
-            vec2.new( 549, 338 ),
-         },
-         {
-            vec2.new( 549, 338 ),
-            vec2.new( 147, -31 ),
-            vec2.new( -144, 53 ),
-            vec2.new( 735, 395 ),
-         },
-         {
-            vec2.new( 735, 395 ),
-            vec2.new( 82, -37 ),
-            vec2.new( -15, 226 ),
-            vec2.new( 939, 394 ),
-         },
-      },
-   }, {
-      name = _("Race 2"),
-      scale = 15,
-      center = true,
-      track = {
-         {
-            vec2.new( 942, 381 ),
-            vec2.new( 5, -104 ),
-            vec2.new( 69, -1 ),
-            vec2.new( 896, 165 ),
-         },
-         {
-            vec2.new( 896, 165 ),
-            vec2.new( -69, 1 ),
-            vec2.new( 61, 55 ),
-            vec2.new( 733, 177 ),
-         },
-         {
-            vec2.new( 733, 177 ),
-            vec2.new( -61, -55 ),
-            vec2.new( 86, 80 ),
-            vec2.new( 551, 176 ),
-         },
-         {
-            vec2.new( 551, 176 ),
-            vec2.new( -86, -80 ),
-            vec2.new( -169, -106 ),
-            vec2.new( 544, 322 ),
-         },
-         {
-            vec2.new( 544, 322 ),
-            vec2.new( 73, 42 ),
-            vec2.new( -154, 98 ),
-            vec2.new( 723, 379 ),
-         },
-         {
-            vec2.new( 723, 379 ),
-            vec2.new( 154, -99 ),
-            vec2.new( 288, -3 ),
-            vec2.new( 687, 465 ),
-         },
-         {
-            vec2.new( 687, 465 ),
-            vec2.new( -181, -1 ),
-            vec2.new( 36, -183 ),
-            vec2.new( 402, 398 ),
-         },
-         {
-            vec2.new( 402, 398 ),
-            vec2.new( -15, 66 ),
-            vec2.new( -81, 55 ),
-            vec2.new( 515, 506 ),
-         },
-         {
-            vec2.new( 515, 506 ),
-            vec2.new( 81, -56 ),
-            vec2.new( -105, 94 ),
-            vec2.new( 702, 532 ),
-         },
-         {
-            vec2.new( 702, 532 ),
-            vec2.new( 105, -94 ),
-            vec2.new( -83, 111 ),
-            vec2.new( 896, 503 ),
-         },
-         {
-            vec2.new( 896, 503 ),
-            vec2.new( 21, -30 ),
-            vec2.new( 0, 63 ),
-            vec2.new( 942, 381 ),
-         },
-      },
-   }, {
-      name = _("Race 3"),
-      scale = 15,
-      center = true,
-      track = {
-         {
-            vec2.new( 942, 466 ),
-            vec2.new( 3, -288 ),
-            vec2.new( 132, -1 ),
-            vec2.new( 830, 176 ),
-         },
-         {
-            vec2.new( 830, 176 ),
-            vec2.new( -132, 1 ),
-            vec2.new( -2, -113 ),
-            vec2.new( 711, 291 ),
-         },
-         {
-            vec2.new( 711, 291 ),
-            vec2.new( 2, 113 ),
-            vec2.new( -56, 1 ),
-            vec2.new( 772, 409 ),
-         },
-         {
-            vec2.new( 772, 409 ),
-            vec2.new( 56, -1 ),
-            vec2.new( -7, -172 ),
-            vec2.new( 830, 577 ),
-         },
-         {
-            vec2.new( 830, 577 ),
-            vec2.new( -5, 178 ),
-            vec2.new( 144, -11 ),
-            vec2.new( 651, 698 ),
-         },
-         {
-            vec2.new( 651, 698 ),
-            vec2.new( -123, 1 ),
-            vec2.new( 1, 116 ),
-            vec2.new( 530, 585 ),
-         },
-         {
-            vec2.new( 530, 585 ),
-            vec2.new( -1, -116 ),
-            vec2.new( -114, -3 ),
-            vec2.new( 651, 466 ),
-         },
-         {
-            vec2.new( 651, 466 ),
-            vec2.new( 59, -1 ),
-            vec2.new( 2, -63 ),
-            vec2.new( 709, 528 ),
-         },
-         {
-            vec2.new( 709, 528 ),
-            vec2.new( 2, 51 ),
-            vec2.new( -170, 0 ),
-            vec2.new( 884, 579 ),
-         },
-         {
-            vec2.new( 884, 579 ),
-            vec2.new( 62, -1 ),
-            vec2.new( 4, 108 ),
-            vec2.new( 942, 471 ),
-         },
-      },
-   }, {
-      name = _("Race 4"),
-      scale = 15,
-      center = true,
-      track = {
-         {
-            vec2.new( 1062, 350 ),
-            vec2.new( 3, -112 ),
-            vec2.new( 117, -4 ),
-            vec2.new( 945, 236 ),
-         },
-         {
-            vec2.new( 945, 236 ),
-            vec2.new( -117, 4 ),
-            vec2.new( 2, 60 ),
-            vec2.new( 825, 171 ),
-         },
-         {
-            vec2.new( 825, 171 ),
-            vec2.new( 4, -54 ),
-            vec2.new( 120, -4 ),
-            vec2.new( 706, 118 ),
-         },
-         {
-            vec2.new( 706, 118 ),
-            vec2.new( -240, -1 ),
-            vec2.new( 3, -108 ),
-            vec2.new( 471, 231 ),
-         },
-         {
-            vec2.new( 471, 231 ),
-            vec2.new( -3, 108 ),
-            vec2.new( -113, -1 ),
-            vec2.new( 587, 350 ),
-         },
-         {
-            vec2.new( 587, 350 ),
-            vec2.new( 113, 1 ),
-            vec2.new( 2, -113 ),
-            vec2.new( 709, 466 ),
-         },
-         {
-            vec2.new( 709, 466 ),
-            vec2.new( 1, 175 ),
-            vec2.new( 120, 3 ),
-            vec2.new( 590, 639 ),
-         },
-         {
-            vec2.new( 590, 639 ),
-            vec2.new( -120, -3 ),
-            vec2.new( 2, 121 ),
-            vec2.new( 474, 521 ),
-         },
-         {
-            vec2.new( 474, 521 ),
-            vec2.new( 0, -50 ),
-            vec2.new( -116, -2 ),
-            vec2.new( 589, 470 ),
-         },
-         {
-            vec2.new( 589, 470 ),
-            vec2.new( 116, 2 ),
-            vec2.new( -118, -2 ),
-            vec2.new( 946, 468 ),
-         },
-         {
-            vec2.new( 946, 468 ),
-            vec2.new( 118, 2 ),
-            vec2.new( -3, 112 ),
-            vec2.new( 1062, 350 ),
-         },
-      },
-   },
-}
+local track_list = require "missions.neutral.race.tracks_qex"
+
+local npc_portrait   = "minerva_terminal.png"
+local npc_description= _("A terminal to let you participate in the different available races.")
+local laid_back_portrait = portrait.getFullPath("neutral/unique/laidback.webp")
 
 function create ()
    if not var.peek("testing") then return end
@@ -341,7 +52,7 @@ function create ()
    if not misn.claim( system.cur() ) then
       misn.finish(false)
    end
-   misn.setNPC(_("A laid back person"), "neutral/unique/laidback.webp", _("You see a laid back person, who appears to be one of the locals, looking around the bar."))
+   misn.npcAdd( "approach_terminal", _("Racing Terminal"), npc_portrait, npc_description )
 end
 
 local function display_time( time )
@@ -352,9 +63,33 @@ local function display_time( time )
    })
 end
 
-function accept ()
+function approach_terminal ()
    local accept = false
    local track
+
+   if false and not var.peek( "racing_intro" ) then
+      vn.clear()
+      vn.scene()
+      local lbp = vn.newCharacter(_("Laid Back Person"), {image=laid_back_portrait})
+      vn.transition()
+
+      vn.na(_([[One of the locals appears as you get close to the racing terminal.]]))
+
+      lbp(_([["Hiya there! You seem like a new face. You want an explanation on how racing works?"]]))
+      vn.menu{
+         {_([[Get an explanation]]),"cont01_yes"},
+         {_([[Skip explanation]]),"cont01_no"},
+      }
+
+      vn.label("cont01_yes")
+      lbp(_([["Racing is quite simple, you have to go through all the gates in order until you reach the final goal. There are no limitations on what ship and outfits you can use, but you're best sticking to Yacht or Interceptor-class ships if you want to make good times."]]))
+      lbp(_([[""]]))
+
+      vn.label("cont01_no")
+      lbp(_([["OK, look forward to seeing your racing skills!"]]))
+      vn.run()
+      var.push("racing_intro", true)
+   end
 
    local w, h = 460, 400
    local wdw = luatk.newWindow( nil, nil, w, h )
