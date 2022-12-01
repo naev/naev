f54568425ebd73bf17c87fdaa5f0fcb990ac4ea4
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-01 11:33:44 +0900

    Added more race tracks and ability to scale/translate/center them.

--- dat/missions/neutral/race_qex.lua
+++ dat/missions/neutral/race_qex.lua
@@ -40,7 +40,6 @@ end
 local track_list = {
    {
       name = _("Qex Tour"),
-      laps = 1,
       track = {
          {
             vec2.new(  5e3,  2e3 ), -- start near Qex IV
@@ -73,6 +72,264 @@ local track_list = {
             vec2.new(  1e3, -3e3 ),
             vec2.new(  5e3,  2e3 ),
          }
+      },
+   }, {
+      name = _("Easy"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 939, 394 ),
+            vec2.new( 6, -128 ),
+            vec2.new( 70, 2 ),
+            vec2.new( 874, 134 ),
+         },
+         {
+            vec2.new( 874, 134 ),
+            vec2.new( -70, -2 ),
+            vec2.new( -34, -33 ),
+            vec2.new( 809, 208 ),
+         },
+         {
+            vec2.new( 809, 208 ),
+            vec2.new( 34, 39 ),
+            vec2.new( 118, 57 ),
+            vec2.new( 725, 270 ),
+         },
+         {
+            vec2.new( 725, 270 ),
+            vec2.new( -118, -57 ),
+            vec2.new( 39, -42 ),
+            vec2.new( 470, 250 ),
+         },
+         {
+            vec2.new( 470, 250 ),
+            vec2.new( -39, 42 ),
+            vec2.new( -91, 17 ),
+            vec2.new( 549, 338 ),
+         },
+         {
+            vec2.new( 549, 338 ),
+            vec2.new( 147, -31 ),
+            vec2.new( -144, 53 ),
+            vec2.new( 735, 395 ),
+         },
+         {
+            vec2.new( 735, 395 ),
+            vec2.new( 82, -37 ),
+            vec2.new( -15, 226 ),
+            vec2.new( 939, 394 ),
+         },
+      },
+   }, {
+      name = _("Race 2"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 942, 381 ),
+            vec2.new( 5, -104 ),
+            vec2.new( 69, -1 ),
+            vec2.new( 896, 165 ),
+         },
+         {
+            vec2.new( 896, 165 ),
+            vec2.new( -69, 1 ),
+            vec2.new( 61, 55 ),
+            vec2.new( 733, 177 ),
+         },
+         {
+            vec2.new( 733, 177 ),
+            vec2.new( -61, -55 ),
+            vec2.new( 86, 80 ),
+            vec2.new( 551, 176 ),
+         },
+         {
+            vec2.new( 551, 176 ),
+            vec2.new( -86, -80 ),
+            vec2.new( -169, -106 ),
+            vec2.new( 544, 322 ),
+         },
+         {
+            vec2.new( 544, 322 ),
+            vec2.new( 73, 42 ),
+            vec2.new( -154, 98 ),
+            vec2.new( 723, 379 ),
+         },
+         {
+            vec2.new( 723, 379 ),
+            vec2.new( 154, -99 ),
+            vec2.new( 288, -3 ),
+            vec2.new( 687, 465 ),
+         },
+         {
+            vec2.new( 687, 465 ),
+            vec2.new( -181, -1 ),
+            vec2.new( 36, -183 ),
+            vec2.new( 402, 398 ),
+         },
+         {
+            vec2.new( 402, 398 ),
+            vec2.new( -15, 66 ),
+            vec2.new( -81, 55 ),
+            vec2.new( 515, 506 ),
+         },
+         {
+            vec2.new( 515, 506 ),
+            vec2.new( 81, -56 ),
+            vec2.new( -105, 94 ),
+            vec2.new( 702, 532 ),
+         },
+         {
+            vec2.new( 702, 532 ),
+            vec2.new( 105, -94 ),
+            vec2.new( -83, 111 ),
+            vec2.new( 896, 503 ),
+         },
+         {
+            vec2.new( 896, 503 ),
+            vec2.new( 21, -30 ),
+            vec2.new( 0, 63 ),
+            vec2.new( 942, 381 ),
+         },
+      },
+   }, {
+      name = _("Race 3"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 942, 466 ),
+            vec2.new( 3, -288 ),
+            vec2.new( 132, -1 ),
+            vec2.new( 830, 176 ),
+         },
+         {
+            vec2.new( 830, 176 ),
+            vec2.new( -132, 1 ),
+            vec2.new( -2, -113 ),
+            vec2.new( 711, 291 ),
+         },
+         {
+            vec2.new( 711, 291 ),
+            vec2.new( 2, 113 ),
+            vec2.new( -56, 1 ),
+            vec2.new( 772, 409 ),
+         },
+         {
+            vec2.new( 772, 409 ),
+            vec2.new( 56, -1 ),
+            vec2.new( -7, -172 ),
+            vec2.new( 830, 577 ),
+         },
+         {
+            vec2.new( 830, 577 ),
+            vec2.new( -5, 178 ),
+            vec2.new( 144, -11 ),
+            vec2.new( 651, 698 ),
+         },
+         {
+            vec2.new( 651, 698 ),
+            vec2.new( -123, 1 ),
+            vec2.new( 1, 116 ),
+            vec2.new( 530, 585 ),
+         },
+         {
+            vec2.new( 530, 585 ),
+            vec2.new( -1, -116 ),
+            vec2.new( -114, -3 ),
+            vec2.new( 651, 466 ),
+         },
+         {
+            vec2.new( 651, 466 ),
+            vec2.new( 59, -1 ),
+            vec2.new( 2, -63 ),
+            vec2.new( 709, 528 ),
+         },
+         {
+            vec2.new( 709, 528 ),
+            vec2.new( 2, 51 ),
+            vec2.new( -170, 0 ),
+            vec2.new( 884, 579 ),
+         },
+         {
+            vec2.new( 884, 579 ),
+            vec2.new( 62, -1 ),
+            vec2.new( 4, 108 ),
+            vec2.new( 942, 471 ),
+         },
+      },
+   }, {
+      name = _("Race 4"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 1062, 350 ),
+            vec2.new( 3, -112 ),
+            vec2.new( 117, -4 ),
+            vec2.new( 945, 236 ),
+         },
+         {
+            vec2.new( 945, 236 ),
+            vec2.new( -117, 4 ),
+            vec2.new( 2, 60 ),
+            vec2.new( 825, 171 ),
+         },
+         {
+            vec2.new( 825, 171 ),
+            vec2.new( 4, -54 ),
+            vec2.new( 120, -4 ),
+            vec2.new( 706, 118 ),
+         },
+         {
+            vec2.new( 706, 118 ),
+            vec2.new( -240, -1 ),
+            vec2.new( 3, -108 ),
+            vec2.new( 471, 231 ),
+         },
+         {
+            vec2.new( 471, 231 ),
+            vec2.new( -3, 108 ),
+            vec2.new( -113, -1 ),
+            vec2.new( 587, 350 ),
+         },
+         {
+            vec2.new( 587, 350 ),
+            vec2.new( 113, 1 ),
+            vec2.new( 2, -113 ),
+            vec2.new( 709, 466 ),
+         },
+         {
+            vec2.new( 709, 466 ),
+            vec2.new( 1, 175 ),
+            vec2.new( 120, 3 ),
+            vec2.new( 590, 639 ),
+         },
+         {
+            vec2.new( 590, 639 ),
+            vec2.new( -120, -3 ),
+            vec2.new( 2, 121 ),
+            vec2.new( 474, 521 ),
+         },
+         {
+            vec2.new( 474, 521 ),
+            vec2.new( 0, -50 ),
+            vec2.new( -116, -2 ),
+            vec2.new( 589, 470 ),
+         },
+         {
+            vec2.new( 589, 470 ),
+            vec2.new( 116, 2 ),
+            vec2.new( -118, -2 ),
+            vec2.new( 946, 468 ),
+         },
+         {
+            vec2.new( 946, 468 ),
+            vec2.new( 118, 2 ),
+            vec2.new( -3, 112 ),
+            vec2.new( 1062, 350 ),
+         },
       },
    },
 }
@@ -123,9 +380,10 @@ function accept ()
       -- Length of the track
       local length = 0
       local lp = track.track[1][1]
+      local scale = track.scale or 1
       for k,trk in ipairs(track.track) do
          for t = 0,1,0.01 do -- only 100 samples
-            local p = cubicBezier( t, trk[1], trk[1]+trk[2], trk[4]+trk[3], trk[4] )
+            local p = cubicBezier( t, trk[1], trk[1]+trk[2], trk[4]+trk[3], trk[4] ) * scale
             length = length + p:dist(lp)
             lp = p
          end
@@ -151,8 +409,8 @@ function accept ()
 
    hook.load( "loaded" ) -- don't load into the race
 
-   -- TODO track selector
-   mem.track = track.track
+   mem.spob = spob.cur()
+   mem.track = track
    hook.safe("start_race")
    player.takeoff() -- take off and start the race!
 end
@@ -167,12 +425,30 @@ function start_race ()
    pilot.toggleSpawn(false)
    -- TODO add spectators / cameras?
 
+   local scale = mem.track.scale or 1
+   local translate = mem.track.translate or vec2.new()
+   if mem.track.center then
+      local minx, maxx, miny, maxy = math.huge, -math.huge, math.huge, -math.huge
+      for k,trk in ipairs(mem.track.track) do
+         for t = 0,1,0.05 do
+            local p = cubicBezier( t, trk[1], trk[1]+trk[2], trk[4]+trk[3], trk[4] ) * scale
+            local x,y = p:get()
+            minx = math.min( minx, x )
+            maxx = math.max( maxx, x )
+            miny = math.min( miny, y )
+            maxy = math.max( maxy, y )
+         end
+      end
+      local pos = mem.spob:pos()
+      translate = pos - vec2.new( minx, miny ) - vec2.new( (maxx-minx)*0.5, (maxy-miny)*0.5 )
+   end
+
    local lp
    local elapsed = 0
    local gates_p = {}
-   for k,trk in ipairs(mem.track) do
+   for k,trk in ipairs(mem.track.track) do
       for t = 0,1,0.005 do
-         local p = cubicBezier( t, trk[1], trk[1]+trk[2], trk[4]+trk[3], trk[4] )
+         local p = cubicBezier( t, trk[1], trk[1]+trk[2], trk[4]+trk[3], trk[4] ) * scale + translate
          local d = (lp and p:dist(lp)) or math.huge
          if d > 500 then
             table.insert( gates_p, p )
@@ -262,7 +538,7 @@ end
 
 function race_landed ()
    vntk.msg(_("Race Finished!"),fmt.f(_("You finished the race in {elapsed}. Congratulations!"), {
-      elapsed=display_time( elapsed_time ),
+      elapsed="#g"..display_time( elapsed_time ).."#0",
    }))
    misn.finish(false) -- false until we actually set up proper rewards and stuff
 end
