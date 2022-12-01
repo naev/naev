02a3885e480324d1df402020d0dd409db97a18d1
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-01 18:56:51 +0900

    Split and renamed race tracks a bit.

---
+++ dat/missions/neutral/race/tracks_zlk.lua
@@ -0,0 +1,141 @@
+return {
+   {
+      name = _("ZR-01"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 942, 466 ),
+            vec2.new( 3, -288 ),
+            vec2.new( 132, -1 ),
+            vec2.new( 830, 176 ),
+         },
+         {
+            vec2.new( 830, 176 ),
+            vec2.new( -132, 1 ),
+            vec2.new( -2, -113 ),
+            vec2.new( 711, 291 ),
+         },
+         {
+            vec2.new( 711, 291 ),
+            vec2.new( 2, 113 ),
+            vec2.new( -56, 1 ),
+            vec2.new( 772, 409 ),
+         },
+         {
+            vec2.new( 772, 409 ),
+            vec2.new( 56, -1 ),
+            vec2.new( -7, -172 ),
+            vec2.new( 830, 577 ),
+         },
+         {
+            vec2.new( 830, 577 ),
+            vec2.new( -5, 178 ),
+            vec2.new( 144, -11 ),
+            vec2.new( 651, 698 ),
+         },
+         {
+            vec2.new( 651, 698 ),
+            vec2.new( -123, 1 ),
+            vec2.new( 1, 116 ),
+            vec2.new( 530, 585 ),
+         },
+         {
+            vec2.new( 530, 585 ),
+            vec2.new( -1, -116 ),
+            vec2.new( -114, -3 ),
+            vec2.new( 651, 466 ),
+         },
+         {
+            vec2.new( 651, 466 ),
+            vec2.new( 59, -1 ),
+            vec2.new( 2, -63 ),
+            vec2.new( 709, 528 ),
+         },
+         {
+            vec2.new( 709, 528 ),
+            vec2.new( 2, 51 ),
+            vec2.new( -170, 0 ),
+            vec2.new( 884, 579 ),
+         },
+         {
+            vec2.new( 884, 579 ),
+            vec2.new( 62, -1 ),
+            vec2.new( 4, 108 ),
+            vec2.new( 942, 471 ),
+         },
+      },
+   }, {
+      name = _("ZR-02"),
+      scale = 15,
+      center = true,
+      track = {
+         {
+            vec2.new( 1062, 350 ),
+            vec2.new( 3, -112 ),
+            vec2.new( 117, -4 ),
+            vec2.new( 945, 236 ),
+         },
+         {
+            vec2.new( 945, 236 ),
+            vec2.new( -117, 4 ),
+            vec2.new( 2, 60 ),
+            vec2.new( 825, 171 ),
+         },
+         {
+            vec2.new( 825, 171 ),
+            vec2.new( 4, -54 ),
+            vec2.new( 120, -4 ),
+            vec2.new( 706, 118 ),
+         },
+         {
+            vec2.new( 706, 118 ),
+            vec2.new( -240, -1 ),
+            vec2.new( 3, -108 ),
+            vec2.new( 471, 231 ),
+         },
+         {
+            vec2.new( 471, 231 ),
+            vec2.new( -3, 108 ),
+            vec2.new( -113, -1 ),
+            vec2.new( 587, 350 ),
+         },
+         {
+            vec2.new( 587, 350 ),
+            vec2.new( 113, 1 ),
+            vec2.new( 2, -113 ),
+            vec2.new( 709, 466 ),
+         },
+         {
+            vec2.new( 709, 466 ),
+            vec2.new( 1, 175 ),
+            vec2.new( 120, 3 ),
+            vec2.new( 590, 639 ),
+         },
+         {
+            vec2.new( 590, 639 ),
+            vec2.new( -120, -3 ),
+            vec2.new( 2, 121 ),
+            vec2.new( 474, 521 ),
+         },
+         {
+            vec2.new( 474, 521 ),
+            vec2.new( 0, -50 ),
+            vec2.new( -116, -2 ),
+            vec2.new( 589, 470 ),
+         },
+         {
+            vec2.new( 589, 470 ),
+            vec2.new( 116, 2 ),
+            vec2.new( -118, -2 ),
+            vec2.new( 946, 468 ),
+         },
+         {
+            vec2.new( 946, 468 ),
+            vec2.new( 118, 2 ),
+            vec2.new( -3, 112 ),
+            vec2.new( 1062, 350 ),
+         },
+      },
+   },
+}
