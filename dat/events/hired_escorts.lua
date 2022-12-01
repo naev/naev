e258b89c1979e7dfdf0bce9463e2ef166e9e6571
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-02 00:59:06 +0900

    Disabled new escorts for hire from spawning. Only old ones will remain.

--- dat/events/hired_escorts.lua
+++ dat/events/hired_escorts.lua
@@ -16,18 +16,10 @@
 
 --]]
 local fmt = require "format"
-local portrait = require "portrait"
-local pir = require "common.pirate"
-local pilotname = require "pilotname"
 
 local npcs -- Non-persistent state
 
 local logidstr = "log_hiredescort"
-
-local npctext = {}
-npctext[1] = _([["Hi there! I'm looking to get some piloting experience. Here are my credentials. Would you be interested in hiring me?"]])
-npctext[2] = _([["Hello! I'm looking to join someone's fleet. Here's my credentials. What do you say, would you like me on board?"]])
-npctext[3] = _([["Hi! You look like you could use a pilot! I'm available and charge some of the best rates in the galaxy, and I promise you I'm perfect for the job! Here's my info. Well, what do you think? Would you like to add me to your fleet?"]])
 
 function create ()
    mem.lastplanet = nil
@@ -40,89 +32,6 @@ function create ()
    hook.jumpout("jumpout")
    hook.enter("enter")
    hook.pay("pay")
-end
-
-
-local function createPilotNPCs ()
-   local ship_choices = {
-      --{ ship = "Llama",       royalty = 0.05 },
-      { ship = "Hyena",       royalty = 0.1 },
-      { ship = "Shark",       royalty = 0.15 },
-      { ship = "Vendetta",    royalty = 0.2 },
-      { ship = "Lancelot",    royalty = 0.2 },
-      { ship = "Ancestor",    royalty = 0.25 },
-      { ship = "Admonisher",  royalty = 0.35 },
-      { ship = "Phalanx",     royalty = 0.35 },
-      { ship = "Pacifier",    royalty = 0.45 },
-      { ship = "Vigilance",   royalty = 0.45 },
-   }
-   local num_pilots = rnd.rnd(0, 5)
-   local fac = faction.get("Mercenary")
-   local name_func = pilotname.generic
-   local portrait_arg = nil
-
-   local pf = spob.cur():faction()
-   if pir.factionIsPirate( pf ) then
-      ship_choices = {
-         { ship = "Pirate Hyena",      royalty = 0.1 },
-         { ship = "Pirate Shark",      royalty = 0.15 },
-         { ship = "Pirate Vendetta",   royalty = 0.2 },
-         { ship = "Pirate Ancestor",   royalty = 0.25 },
-         { ship = "Pirate Admonisher", royalty = 0.35 },
-         { ship = "Pirate Phalanx",    royalty = 0.35 },
-      }
-      fac = faction.get("Pirate")
-      name_func = pilotname.pirate
-      portrait_arg = "Pirate"
-   elseif pf == faction.get("Thurion") then
-      ship_choices = {
-         { ship = "Thurion Ingenuity",     royalty = 0.15 },
-         { ship = "Thurion Scintillation", royalty = 0.25 },
-         { ship = "Thurion Virtuosity",    royalty = 0.35 },
-         { ship = "Thurion Apprehension",  royalty = 0.5 },
-      }
-      fac = faction.get("Thurion")
-      portrait_arg = "Thurion"
-   end
-
-   if fac == nil or fac:playerStanding() < 0 then
-      return
-   end
-
-   for i=1, num_pilots do
-      local newpilot = {}
-      local shipchoice = ship_choices[rnd.rnd(1, #ship_choices)]
-      local p = pilot.add(shipchoice.ship, fac)
-      local _n, deposit = p:ship():price()
-      newpilot.outfits = {}
-
-      for j, o in ipairs(p:outfitsList()) do
-         deposit = deposit + o:price()
-         newpilot.outfits[#newpilot.outfits + 1] = o:nameRaw()
-      end
-
-      deposit = math.floor((deposit + 0.1*deposit*rnd.sigma()) / 4)
-      if deposit <= player.credits() then
-         newpilot.ship = ship.get(shipchoice.ship)
-         newpilot.deposit = deposit
-         newpilot.deposit_text = fmt.credits(newpilot.deposit)
-         newpilot.royalty = (
-               shipchoice.royalty + 0.05*shipchoice.royalty*rnd.sigma() )
-         newpilot.royalty_percent = newpilot.royalty * 100
-         newpilot.name = name_func()
-         newpilot.portrait = portrait.get(portrait_arg)
-         newpilot.faction  = fac
-         newpilot.approachtext = npctext[rnd.rnd(1, #npctext)]
-         local id = evt.npcAdd( "approachPilot", _("Pilot for Hire"), newpilot.portrait,
-            fmt.f(_([[This pilot seems to be looking for work.
-
-Ship: {ship}
-Deposit: {deposit_text}
-Royalty: {royalty_percent:.1f}% of mission earnings]]), newpilot), 9 )
-
-         npcs[id] = newpilot
-      end
-   end
 end
 
 
@@ -192,7 +101,7 @@ function land ()
    end
 
    -- Create NPCs for pilots you can hire.
-   createPilotNPCs()
+   --createPilotNPCs()
 end
 
 
@@ -402,45 +311,4 @@ function approachEscort( npc_id )
    end
 
    pilot_askFire( edata, npc_id )
-end
-
--- Approaching unhired pilot at the bar
-function approachPilot( npc_id )
-   local pdata = npcs[npc_id]
-   if pdata == nil then
-      evt.npcRm(npc_id)
-      return
-   end
-
-   if not tk.yesno("", getOfferText(pdata.approachtext, pdata)) then
-      return -- Player rejected offer
-   end
-
-   if pdata.deposit and pdata.deposit > player.credits() then
-      tk.msg("", _("You don't have enough credits to pay for this pilot's deposit."))
-      return
-   end
-   if getTotalRoyalties() + pdata.royalty > 1 then
-      if not tk.yesno("", _("Hiring this pilot will lead to you paying more in royalties than you earn from missions, meaning you will lose credits when doing missions. Are you sure you want to hire this pilot?")) then
-         return
-      end
-   end
-
-   if pdata.deposit then
-      player.pay(-pdata.deposit, true)
-   end
-
-   local i = #mem.escorts + 1
-   pdata.alive = true
-   mem.escorts[i] = pdata
-   evt.npcRm(npc_id)
-   npcs[npc_id] = nil
-   local id = evt.npcAdd( "approachEscort", pdata.name, pdata.portrait,
-         _("This is one of the pilots currently under your wing."), 8 )
-   npcs[id] = pdata
-   evt.save(true)
-
-   local edata = mem.escorts[i]
-   shiplog.create( logidstr, _("Hired Escorts"), _("Hired Escorts") )
-   shiplog.append( logidstr, fmt.f(_("You hired a {ship} ship named '{name}' for {deposit_text} and {royalty_percent:.1f}% of mission earnings."), edata ) )
 end
