85580b2056c0f613a26abbd34a2d991486a4bb32
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-27 01:17:45 +0900

    kidnapped: finished up moving to vn and vntk

--- dat/missions/neutral/kidnapped/traffic_01.lua
+++ dat/missions/neutral/kidnapped/traffic_01.lua
@@ -31,11 +31,18 @@
 local fmt = require "format"
 local neu = require "common.neutral"
 local vntk = require "vntk"
+local vn = require "vn"
+local portrait = require "portrait"
+local ccomm = require "common.comm"
 
 local reward = 200e3
 
 local badguys, broship -- Non-persistent state
 local spawn_baddies -- Forward-declared functions
+
+local npc_name = _("Ordinary Woman")
+local npc_portrait = "neutral/unique/fakesister.webp"
+local npc_image = portrait.getFullPath( npc_portrait )
 
 function create ()
    mem.targetsys = {system.get("Mural"),system.get("Darkstone"),system.get("Haleb")}
@@ -49,7 +56,7 @@ function create ()
    end
 
    -- Spaceport bar stuff
-   misn.setNPC( _("Ordinary Woman"), "neutral/unique/fakesister.webp", _("The woman waves at you a bit desperately.") )
+   misn.setNPC( npc_name, npc_portrait, _("The woman waves at you a bit desperately.") )
 end
 
 
@@ -57,11 +64,30 @@ end
 Mission entry point.
 --]]
 function accept ()
-   -- Mission details:
-   if not vntk.yesno( _("In the Bar"), _([["I must find my dear brother! Please help me. I think he is in danger! I don't have a ship and he is the only family I have left. Could you please help me?"]]) ) then
-      vntk.msg(_("Sorry, I can't"), _([["How can you be such a heartless person? What has this universe become?…"]]))
-      return
-   end
+   local accepted = false
+
+   vn.clear()
+   vn.scene()
+   local w = vn.newCharacter( npc_name, {image=npc_image} )
+
+   w(_([["I must find my dear brother! Please help me. I think he is in danger! I don't have a ship and he is the only family I have left. Could you please help me?"]]) )
+   vn.menu{
+      {_("Help her"), "accept"},
+      {_("Decline"), "decline"},
+   }
+
+   vn.label("decline")
+   w(_([["How can you be such a heartless person? What has this universe become?…"]]))
+   vn.done()
+
+   vn.label("accept")
+   w(_([[The woman calms down as you signal your willingness to help. "Oh, thank goodness! I was told where he usually hangs around. Please take me there and tell him that I have to talk to him.
+And please hurry. Someone was sent to assassinate him. I don't have much to give, but whatever I have saved, you can have."]]) )
+
+   vn.run()
+
+   if not accepted then return end
+
    misn.accept()
 
    -- Some variables for keeping track of the mission
@@ -84,10 +110,6 @@ function accept ()
       misn.markerAdd( mem.targetsys[2], "low" ),
       misn.markerAdd( mem.targetsys[3], "low" )
    }
-
-   -- Some flavour text
-   vntk.msg( _("In the Bar"), _([[The woman calms down as you signal your willingness to help. "Oh, thank goodness! I was told where he usually hangs around. Please take me there and tell him that I have to talk to him.
-    And please hurry. Someone was sent to assassinate him. I don't have much to give, but whatever I have saved, you can have."]]) )
 
    -- Set hooks
    hook.jumpin("sys_enter")
@@ -158,7 +180,7 @@ function sys_enter ()
       broship:setFuel(true)
       broship:moveto(mem.bropla:pos() + vec2.new( 400, -400), false)
       -- just some moving around, stolen from baron missions ;D
-      mem.idlehook = hook.pilot(broship, "idle", "idle",broship,mem.bropla)
+      mem.idlehook = hook.pilot(broship, "idle", "idle",broship, mem.bropla)
       misn.osdActive(2)
       -- get point between jumpgate and broship to spawn mercenaries disencouraging him from following
       mem.jpt = get_nearest_jump(broship)
@@ -173,8 +195,14 @@ end
 
 -- if hailed: stop vessel let it be boarded
 function got_hailed(shipp)
-   vntk.msg(_("Comm Channel"), _([[You radio the ship with a message saying you have his sister on board and that she has a message for him.
-    "My sister? What the heck could she want from me? Prepare for docking."]]))
+   vn.clear()
+   vn.scene()
+   local p = ccomm.newCharacter( vn, shipp )
+   vn.transition()
+   p(_([[You radio the ship with a message saying you have his sister on board and that she has a message for him.
+"My sister? What the heck could she want from me? Prepare for docking."]]))
+   vn.run()
+
    shipp:taskClear()
    shipp:brake()
    shipp:setActiveBoard(true)
@@ -191,10 +219,10 @@ function got_boarded(shipp)
    --get nearest jumppoints and let ship escape in this direction
    shipp:hyperspace(mem.jpt:dest())
    vntk.msg(_("The Deception"), _([[The woman stands next to you while the airlock opens. You see the grin on the man's face change to a baffled expression, then hear the sound of a blaster. Before you even realize what has happened, the lady rushes past you and closes the airlock.
-    You find an arrangement of credit chips she left in your ship along with a note: "Sorry."]]))
+You find an arrangement of credit chips she left in your ship along with a note: "Sorry."]]))
    -- turn mercs hostile
    for i=1,#badguys do
-     badguys[i]:setHostile(true)
+      badguys[i]:setHostile(true)
    end
 
    player.pay(reward)
