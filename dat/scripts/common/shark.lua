1b10e36df63697daa3f561c3bcfd9ea3b5cdc06b
Edgar Simo-Serra <bobbens@gmail.com>
2022-12-27 21:26:54 +0900

    sh00: use vn

--- dat/scripts/common/shark.lua
+++ dat/scripts/common/shark.lua
@@ -3,6 +3,9 @@
    Shark Common Functions
 
 --]]
+local portrait = require "portrait"
+local vn = require "vn"
+
 local shark = {}
 
 function shark.pirateFaction ()
@@ -13,6 +16,23 @@ end
 function shark.addLog( text )
    shiplog.create( "shark", _("Nexus Shipyards"), _("Nexus Shipyards") )
    shiplog.append( "shark", text )
+end
+
+shark.arnold = {
+   name = _("Arnold Smith"),
+   portrait = "neutral/unique/arnoldsmith.webp",
+   colour = nil,
+   transition = "pixelize",
+   description = _(""),
+}
+shark.arnold.image = portrait.getFullPath(shark.arnold.portrait)
+
+function shark.vn_arnold( params )
+   return vn.Character.new( shark.arnold.name,
+      tmerge( {
+         image=shark.arnold.image,
+         color=shark.arnold.colour,
+      }, params) )
 end
 
 shark.rewards = {
