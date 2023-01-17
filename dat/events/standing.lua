7312b9f885dbb14a0a293a4b16b2e875caf26f16
Edgar Simo-Serra <bobbens@gmail.com>
2023-01-17 22:14:15 +0900

    Trivial whitespace.

--- dat/events/standing.lua
+++ dat/events/standing.lua
@@ -11,7 +11,6 @@
 local fmt = require "format"
 local lf = require "love.filesystem"
 
-
 local factions = {}
 for k,v in ipairs(lf.enumerate("scripts/factions")) do
    local e = require( "factions."..string.gsub(v,".lua","") )
