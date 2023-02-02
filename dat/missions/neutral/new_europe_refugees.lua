73f307195c06ba1e4e6ca6415f12b1d36f5e71f3
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-02 23:57:00 +0900

    Refugee mission works, TODO make more interesting.

--- dat/missions/neutral/new_europe_refugees.lua
+++ dat/missions/neutral/new_europe_refugees.lua
@@ -1,7 +1,7 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
 <mission name="New Europe Refugees">
- <priority>6</priority>
+ <priority>3</priority>
  <chance>100</chance>
  <location>Computer</location>
  <spob>New Europe</spob>
@@ -16,10 +16,10 @@ local fmt = require "format"
 local pir = require "common.pirate"
 
 function create ()
-   mem.dest_spob, mem.dest_sys = spob.get("Ohon")
+   mem.dest_spob, mem.dest_sys = spob.getS("Ohon")
    local numjumps = system.cur():jumpDist( mem.dest_sys )
 
-   mem.reward = numjumps*150
+   mem.reward = numjumps*250
 
    misn.setTitle(_("Refugee Evacuation"))
    misn.setDesc(fmt.f(_([[Nebula refugees want you to get them off world to {spob} in the {sys} system.
@@ -29,8 +29,7 @@ function create ()
       {spob=mem.dest_spob, sys=mem.dest_sys,
          jumps=fmt.f( n_( "#nJumps:#0 {jumps}", "#nJumps:#0 {jumps}", numjumps ), {jumps=numjumps} )}))
    misn.setReward( fmt.credits(mem.reward).." per tonne of refugees" )
-
-   hook.land("land")
+   misn.markerAdd( mem.dest_spob, "computer" )
 end
 
 function accept ()
@@ -48,8 +47,15 @@ function accept ()
 
    misn.setReward( fmt.credits(mem.reward*mem.amount) )
 
+   misn.osdCreate( _("Refugee Evacuation"), {
+      fmt.f(_("Take the refugees to {spob} ({sys} system"),
+         {spob=mem.dest_spob, sys=mem.dest_sys}),
+   } )
+
    vntk.msg(_("Nebula Refugees"), fmt.f(_([[The procession of refugees slowly boards your ship, with more then one person looking back at the dying planet they used to call home. They make themselves as comfortable as they can in your cargo holds and seem to resigned to hoping what they will find at {spob} is better than what they are leaving behind at {home}.]]),
       {spob=mem.dest_spob, home=spob.cur()}))
+
+   hook.land("land")
 end
 
 function land ()
