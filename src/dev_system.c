2c2efef9e3fcdfcd952f01990c7895cf6d72b1e7
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-04 01:27:23 +0900

    Don't save system nebula stuff unless non-zero density.

--- src/dev_system.c
+++ src/dev_system.c
@@ -120,12 +120,14 @@ int dsys_saveSystem( StarSystem *sys )
    xmlw_elem( writer, "radius", "%f", sys->radius );
    xmlw_elem( writer, "stars", "%d", sys->stars );
    xmlw_elem( writer, "interference", "%f", sys->interference );
-   xmlw_startElem( writer, "nebula" );
-   xmlw_attr( writer, "volatility", "%f", sys->nebu_volatility );
-   if (fabs(sys->nebu_hue*360.0 - NEBULA_DEFAULT_HUE) > 1e-5)
-      xmlw_attr( writer, "hue", "%f", sys->nebu_hue*360.0 );
-   xmlw_str( writer, "%f", sys->nebu_density );
-   xmlw_endElem( writer ); /* "nebula" */
+   if (sys->nebu_density > 0.) {
+      xmlw_startElem( writer, "nebula" );
+      xmlw_attr( writer, "volatility", "%f", sys->nebu_volatility );
+      if (fabs(sys->nebu_hue*360.0 - NEBULA_DEFAULT_HUE) > 1e-5)
+         xmlw_attr( writer, "hue", "%f", sys->nebu_hue*360.0 );
+      xmlw_str( writer, "%f", sys->nebu_density );
+      xmlw_endElem( writer ); /* "nebula" */
+   }
    xmlw_endElem( writer ); /* "general" */
 
    /* Position. */
