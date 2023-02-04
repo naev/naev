adbd6b1479f73f92c0a0515be3998137f24a1329
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-04 17:13:51 +0900

    Added Traders Society news stub.

---
+++ dat/events/news/trader.lua
@@ -0,0 +1,12 @@
+local head = {
+   _("Space Traders Society News Network"),
+}
+local greeting = {
+   _("Traders of the galaxy, nite!"),
+}
+local articles = {
+}
+
+return function ()
+   return "Traders Society", head, greeting, articles
+end
