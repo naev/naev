75fc21f04bd9301192308fc06102017a55da71e6
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-07 15:19:49 +0900

    Added another wormhole at ivella.

--- dat/spob/lua/wormhole_ivella.lua
+++ dat/spob/lua/wormhole_ivella.lua
@@ -1,7 +1,7 @@
 local wormhole = require "spob.lua.lib.wormhole"
 
 function init( p )
-   return wormhole.init( p, "Wormhole Ivella", {
+   return wormhole.init( p, "Wormhole Surano", {
       col_inner   = {0.2, 0.8, 1.0},
       col_outter  = {0.3, 0.2, 1.0},
    } )
