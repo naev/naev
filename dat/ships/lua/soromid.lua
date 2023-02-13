3850e3a4edca143a1b7550c0b7cdb9edc0d60957
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-13 19:40:45 +0900

    Gave soromid ships their own death animations.

---
+++ dat/ships/lua/soromid.lua
@@ -0,0 +1,58 @@
+local explosion = require "luaspfx.explosion"
+
+function explode_init( p )
+   local s = p:stats()
+   local armour_max, shield_max = s.armour, s.shield
+   local sw, sh = p:ship():dims()
+
+   mem.exploded = false
+   mem.dtimer = math.min( 1 + math.sqrt( armour_max * shield_max ) / 500, 3 + math.pow( armour_max * shield_max, 0.4) / 400 )
+   mem.timer = 0
+   mem.r = (sw+sh)*0.25 -- Radius
+end
+
+local function exp_params( size )
+   return {
+      colorbase = {1.0, 0.2, 0.2, 0.2},
+      smokiness = 0.4,
+      colorsmoke = {0.6, 0.3, 0.3, 0.25},
+      smokefade = 1.6,
+      rollspeed = 0.3,
+      grain = 0.1 + size*0.001,
+   }
+end
+
+function explode_update( p, dt )
+   mem.timer = mem.timer - dt
+   mem.dtimer = mem.dtimer - dt
+   if mem.timer < 0 then
+      mem.timer = 0.08 * (mem.dtimer - mem.timer) / mem.dtimer
+
+      local pos = p:pos() + vec2.newP( mem.r*rnd.rnd(), rnd.angle() )
+      local r = mem.r * (0.2 + 0.3 * rnd.rnd())
+
+      -- TODO maybe explosions should be under player when applicable...
+      if rnd.rnd() < 0.1 then
+         explosion( pos, p:vel(), r, nil, {silent=true} )
+      else
+         explosion( pos, p:vel(), r*1.2, nil, tmerge( {silent=true}, exp_params(r) ) )
+      end
+   end
+
+   if not mem.exploded and mem.dtimer < 0.25 then
+      mem.exploded = true
+      local a = math.sqrt( p:mass() )
+      local r = mem.r * 1.0 + a
+      local d = math.max( 0, 2 * (a * (1+math.sqrt(p:fuel()+1)/28)))
+      local params = {
+         penetration = 1,
+      }
+      spfx.debris( p:mass(), mem.r, p:pos(), p:vel() )
+      explosion( p:pos(), p:vel(), r, d, tmerge( params, exp_params(r) ) )
+      p:cargoJet("all")
+   end
+
+   if mem.dtimer < 0 then
+      p:rm()
+   end
+end
