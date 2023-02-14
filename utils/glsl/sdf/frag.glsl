027dd75a3d03f0057c6bd3fd5284068e4a836354
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-14 14:24:18 +0900

    Added electric effect to test shaders.

--- utils/glsl/sdf/frag.glsl
+++ utils/glsl/sdf/frag.glsl
@@ -11,6 +11,272 @@ const float M_SQRT1_2   = 0.70710678118654752440;  /* 1/sqrt(2) */
 float cro(in vec2 a, in vec2 b ) { return a.x*b.y - a.y*b.x; }
 float ndot( vec2 a, vec2 b ) { return a.x*b.x - a.y*b.y; }
 
+// Modulo 289 without a division (only multiplications)
+vec4 mod289(vec4 x) {
+   return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+vec3 mod289(vec3 x) {
+   return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+vec2 mod289(vec2 x) {
+   return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+// Modulo 7 without a division
+vec4 mod7(vec4 x) {
+   return x - floor(x * (1.0 / 7.0)) * 7.0;
+}
+vec3 mod7(vec3 x) {
+   return x - floor(x * (1.0 / 7.0)) * 7.0;
+}
+
+// Permutation polynomial: (34x^2 + x) mod 289
+vec4 permute(vec4 x) {
+   return mod289((34.0 * x + 10.0) * x);
+}
+vec3 permute(vec3 x) {
+   return mod289((34.0 * x + 10.0) * x);
+}
+
+vec4 taylorInvSqrt(vec4 r) {
+   return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+   return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+vec2 fade(vec2 t) {
+   return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+/* Returns a value in the [0,1] range. */
+float random(float n)
+{
+   return fract(sin(n) * 43758.5453123);
+}
+
+/* Returns a value in the [0,1] range.
+ http://byteblacksmith.com/improvements-to-the-canonical-one-liner-glsl-rand-for-opengl-es-2-0/
+ */
+float random(vec2 co)
+{
+   const highp float seed = 12.9898;
+   highp float a = seed;
+   highp float b = 78.233;
+   highp float c = 43758.5453;
+   highp float dt= dot(co.xy ,vec2(a,b));
+   highp float sn= mod(dt,M_PI);
+   return fract(sin(sn) * c);
+}
+
+float snoise(vec2 v)
+{
+   const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+         0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+         -0.577350269189626,  // -1.0 + 2.0 * C.x
+         0.024390243902439); // 1.0 / 41.0
+   // First corner
+   vec2 i  = floor(v + dot(v, C.yy) );
+   vec2 x0 = v -   i + dot(i, C.xx);
+
+   // Other corners
+   vec2 i1;
+   //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+   //i1.y = 1.0 - i1.x;
+   i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+   // x0 = x0 - 0.0 + 0.0 * C.xx ;
+   // x1 = x0 - i1 + 1.0 * C.xx ;
+   // x2 = x0 - 1.0 + 2.0 * C.xx ;
+   vec4 x12 = x0.xyxy + C.xxzz;
+   x12.xy -= i1;
+
+   // Permutations
+   i = mod289(i); // Avoid truncation effects in permutation
+   vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+         + i.x + vec3(0.0, i1.x, 1.0 ));
+
+   vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+   m = m*m ;
+   m = m*m ;
+
+   // Gradients: 41 points uniformly over a line, mapped onto a diamond.
+   // The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+   vec3 x = 2.0 * fract(p * C.www) - 1.0;
+   vec3 h = abs(x) - 0.5;
+   vec3 ox = floor(x + 0.5);
+   vec3 a0 = x - ox;
+
+   // Normalise gradients implicitly by scaling m
+   // Approximation of: m *= inversesqrt( a0*a0 + h*h );
+   m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+   // Compute final noise value at P
+   vec3 g;
+   g.x  = a0.x  * x0.x  + h.x  * x0.y;
+   g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+   return 130.0 * dot(m, g);
+}
+
+float snoise(vec3 v)
+{
+   const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+   const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+   // First corner
+   vec3 i  = floor(v + dot(v, C.yyy) );
+   vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+   // Other corners
+   vec3 g = step(x0.yzx, x0.xyz);
+   vec3 l = 1.0 - g;
+   vec3 i1 = min( g.xyz, l.zxy );
+   vec3 i2 = max( g.xyz, l.zxy );
+
+   //   x0 = x0 - 0.0 + 0.0 * C.xxx;
+   //   x1 = x0 - i1  + 1.0 * C.xxx;
+   //   x2 = x0 - i2  + 2.0 * C.xxx;
+   //   x3 = x0 - 1.0 + 3.0 * C.xxx;
+   vec3 x1 = x0 - i1 + C.xxx;
+   vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
+   vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
+
+   // Permutations
+   i = mod289(i);
+   vec4 p = permute( permute( permute(
+               i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+            + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
+         + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+   // Gradients: 7x7 points over a square, mapped onto an octahedron.
+   // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+   float n_ = 0.142857142857; // 1.0/7.0
+   vec3  ns = n_ * D.wyz - D.xzx;
+
+   vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
+
+   vec4 x_ = floor(j * ns.z);
+   vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+   vec4 x = x_ *ns.x + ns.yyyy;
+   vec4 y = y_ *ns.x + ns.yyyy;
+   vec4 h = 1.0 - abs(x) - abs(y);
+
+   vec4 b0 = vec4( x.xy, y.xy );
+   vec4 b1 = vec4( x.zw, y.zw );
+
+   //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
+   //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
+   vec4 s0 = floor(b0)*2.0 + 1.0;
+   vec4 s1 = floor(b1)*2.0 + 1.0;
+   vec4 sh = -step(h, vec4(0.0));
+
+   vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+   vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+   vec3 p0 = vec3(a0.xy,h.x);
+   vec3 p1 = vec3(a0.zw,h.y);
+   vec3 p2 = vec3(a1.xy,h.z);
+   vec3 p3 = vec3(a1.zw,h.w);
+
+   //Normalise gradients
+   vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+   p0 *= norm.x;
+   p1 *= norm.y;
+   p2 *= norm.z;
+   p3 *= norm.w;
+
+   // Mix final noise value
+   vec4 m = max(0.5 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+   m = m * m;
+   return 105.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
+            dot(p2,x2), dot(p3,x3) ) );
+}
+
+float psrdnoise(vec2 x, vec2 period, float alpha, out vec2 gradient) {
+
+	// Transform to simplex space (axis-aligned hexagonal grid)
+	vec2 uv = vec2(x.x + x.y*0.5, x.y);
+
+	// Determine which simplex we're in, with i0 being the "base"
+	vec2 i0 = floor(uv);
+	vec2 f0 = fract(uv);
+	// o1 is the offset in simplex space to the second corner
+	float cmp = step(f0.y, f0.x);
+	vec2 o1 = vec2(cmp, 1.0-cmp);
+
+	// Enumerate the remaining simplex corners
+	vec2 i1 = i0 + o1;
+	vec2 i2 = i0 + vec2(1.0, 1.0);
+
+	// Transform corners back to texture space
+	vec2 v0 = vec2(i0.x - i0.y * 0.5, i0.y);
+	vec2 v1 = vec2(v0.x + o1.x - o1.y * 0.5, v0.y + o1.y);
+	vec2 v2 = vec2(v0.x + 0.5, v0.y + 1.0);
+
+	// Compute vectors from v to each of the simplex corners
+	vec2 x0 = x - v0;
+	vec2 x1 = x - v1;
+	vec2 x2 = x - v2;
+
+	vec3 iu, iv;
+	vec3 xw, yw;
+
+	// Wrap to periods, if desired
+	if(any(greaterThan(period, vec2(0.0)))) {
+		xw = vec3(v0.x, v1.x, v2.x);
+		yw = vec3(v0.y, v1.y, v2.y);
+		if(period.x > 0.0)
+			xw = mod(vec3(v0.x, v1.x, v2.x), period.x);
+		if(period.y > 0.0)
+			yw = mod(vec3(v0.y, v1.y, v2.y), period.y);
+		// Transform back to simplex space and fix rounding errors
+		iu = floor(xw + 0.5*yw + 0.5);
+		iv = floor(yw + 0.5);
+	} else { // Shortcut if neither x nor y periods are specified
+		iu = vec3(i0.x, i1.x, i2.x);
+		iv = vec3(i0.y, i1.y, i2.y);
+	}
+
+	// Compute one pseudo-random hash value for each corner
+	vec3 hash = mod(iu, 289.0);
+	hash = mod((hash*51.0 + 2.0)*hash + iv, 289.0);
+	hash = mod((hash*34.0 + 10.0)*hash, 289.0);
+
+	// Pick a pseudo-random angle and add the desired rotation
+	vec3 psi = hash * 0.07482 + alpha;
+	vec3 gx = cos(psi);
+	vec3 gy = sin(psi);
+
+	// Reorganize for dot products below
+	vec2 g0 = vec2(gx.x,gy.x);
+	vec2 g1 = vec2(gx.y,gy.y);
+	vec2 g2 = vec2(gx.z,gy.z);
+
+	// Radial decay with distance from each simplex corner
+	vec3 w = 0.8 - vec3(dot(x0, x0), dot(x1, x1), dot(x2, x2));
+	w = max(w, 0.0);
+	vec3 w2 = w * w;
+	vec3 w4 = w2 * w2;
+
+	// The value of the linear ramp from each of the corners
+	vec3 gdotx = vec3(dot(g0, x0), dot(g1, x1), dot(g2, x2));
+
+	// Multiply by the radial decay and sum up the noise value
+	float n = dot(w4, gdotx);
+
+	// Compute the first order partial derivatives
+	vec3 w3 = w2 * w;
+	vec3 dw = -8.0 * w3 * gdotx;
+	vec2 dn0 = w4.x * g0 + dw.x * x0;
+	vec2 dn1 = w4.y * g1 + dw.y * x1;
+	vec2 dn2 = w4.z * g2 + dw.z * x2;
+	gradient = 10.9 * (dn0 + dn1 + dn2);
+
+	// Scale the return value to fit nicely into the range [-1,1]
+	return 10.9 * n;
+}
+
 float smin( float a, float b, float k )
 {
    float h = max( k-abs(a-b), 0.0 )/k;
@@ -137,6 +403,14 @@ float sdArc( in vec2 p, in vec2 sca, in vec2 scb, in float ra, in float rb )
    p.x = abs(p.x);
    float k = (scb.y*p.x>scb.x*p.y) ? dot(p.xy,scb) : length(p);
    return sqrt( max(0.0, dot(p,p) + ra*ra - 2.0*ra*k) ) - rb;
+}
+
+float sdArc2( in vec2 p, in vec2 sc, in float ra, float rb )
+{
+    // sc is the sin/cos of the arc's aperture
+    p.x = abs(p.x);
+    return ((sc.y*p.x>sc.x*p.y) ? length(p-sc*ra) :
+                                  abs(length(p)-ra)) - rb;
 }
 
 float sdCircle( in vec2 p, in float r )
@@ -616,6 +890,59 @@ vec4 sm( vec4 colour, Image tex, vec2 texture_coords, vec2 screen_coords )
    return colour;
 }
 
+vec4 electric( vec4 colour, Image tex, vec2 texture_coords, vec2 screen_coords )
+{
+   float m = 1.0 / dimensions.x;
+   float progress = fract( u_time*1.3 );
+   vec2 uv = texture_coords*2.0-1.0;
+   float u_r = floor(u_time*1.3);//floor(2.0*u_time);
+
+   colour = vec4( 0.1, 0.7, 0.9, 1.0 );
+
+   float angle = 2.0*M_PI*random(u_r);
+   float shear = 0.5*random(10.0*u_r+10.0);
+   float c = cos(angle);
+   float s = sin(angle);
+
+   mat2 R = mat2(c,-s,s,c);
+   mat2 S = mat2(1.0,shear,0.0,1.0);
+   uv = S * R * uv;
+
+   float r = length(uv);
+
+   vec3 nuv = vec3( 3.0*uv, u_time );
+   float n = abs(snoise( nuv ));
+   n += 0.5*abs(snoise(2.0*nuv));
+   n += 0.25*abs(snoise(4.0*nuv));
+   n = pow(1.0-n,2.0);
+   colour.rgb += 0.3*n;
+
+   const float UP = 0.7;
+   const float MAX = 0.65;
+
+   float t = 0.5*smoothstep( 0.0, 1.0, progress / UP );
+   float mixr = smoothstep( 0.0, 1.0, (progress-UP) / (1.0-UP) );
+   float w = 0.3;
+   float d, d2;
+   if (mixr <= 0.0)
+      d = abs(sdCircle( uv, t ))-w;
+   else
+      d = abs(sdCircle( uv, MAX - 2.0*mixr ))-w-mixr;
+   colour.a *= smoothstep( 0.0, w-0.1, -d ) * n;
+   //colour.a *= smoothstep( 0.0, w-0.1, -d );
+
+   w = 0.1;
+   if (mixr <= 0.0)
+      d = abs(sdCircle( uv, t ))-w;
+   else
+      d = abs(sdCircle( uv, MAX - 2.0*mixr ))-w-mixr;
+   colour.a += smoothstep( 0.0, w, -d );
+
+   colour.a = pow( colour.a, 2.0 );
+
+   return colour;
+}
+
 vec4 effect( vec4 colour, Image tex, vec2 uv, vec2 px )
 {
    vec4 col_out;
@@ -643,7 +970,8 @@ vec4 effect( vec4 colour, Image tex, vec2 uv, vec2 px )
    //col_out = sdf_vndone( colour, uv_rel );
    //col_out = sdf_notemarker( colour, uv_rel );
    //col_out = sm_highlight( colour, tex, uv, px );
-   col_out = sm( colour, tex, uv, px );
+   //col_out = sm( colour, tex, uv, px );
+   col_out = electric( colour, tex, uv, px );
 
    return mix( bg(uv), col_out, col_out.a );
 }
