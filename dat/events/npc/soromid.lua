1b9e58a0c3c67c0c590c319ee5b86ae185e8ee3b
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-22 09:11:34 +0900

    Fixed typos reported by Isshiki Kotonashi on discord.

--- dat/events/npc/soromid.lua
+++ dat/events/npc/soromid.lua
@@ -45,7 +45,7 @@ local msg_lore = {
    _([["We never meant to become new humans, but when you are faced with the annihilation of your people, you have to take decisive action to save as many as you can."]]),
    _([["We do not easily forget the treatment the Empire gave us during the Great Quarantine. We will make sure that does not happen again."]]),
    _([["We have many stories about the Great Quarantine. It is important to not forget our past as we strive for a better future."]]),
-   _([["Some thing that making the Soromid inhabit the barely inhabitable is a sign of inferiority to the Great Houses. However, we see it as a pride and accomplishment. Let the Great Houses fight over their few worlds while we live harmonious with nature!"]]),
+   _([["Some think that making the Soromid inhabit the barely inhabitable is a sign of inferiority to the Great Houses. However, we see it as our pride and accomplishments. Let the Great Houses fight over their few worlds while we live harmonious with nature!"]]),
 }
 
 local msg_tip = {
