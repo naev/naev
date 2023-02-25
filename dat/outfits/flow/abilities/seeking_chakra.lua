0a31a500a0d97a69dd5abd64166d80cf7cdf6dd3
Edgar Simo-Serra <bobbens@gmail.com>
2023-02-25 23:18:05 +0900

    Tweaked seeking chakra effects and description a bit more.

--- dat/outfits/flow/abilities/seeking_chakra.lua
+++ dat/outfits/flow/abilities/seeking_chakra.lua
@@ -2,32 +2,38 @@ local flow = require "ships.lua.lib.flow"
 local fmt = require "format"
 
 local function getStats( p )
-   local flow_cost, cooldown, ref
+   local flow_cost, cooldown, ref, strength, duration
    local size = flow.size( p )
    if size == 1 then
       flow_cost   = 25
       cooldown    = 3
       ref         = outfit.get("Seeking Chakra Small")
+      duration    = 5
+      strength    = 1
    elseif size == 2 then
       flow_cost   = 50
       cooldown    = 4
       ref         = outfit.get("Seeking Chakra Small")
+      duration    = 8
+      strength    = 1.25
    else
       flow_cost   = 100
       cooldown    = 5
       ref         = outfit.get("Seeking Chakra Small")
+      duration    = 15
+      strength    = 1.5
    end
-   return flow_cost, cooldown, ref
+   return flow_cost, cooldown, ref, strength, duration
 end
 
 function descextra( p, _o )
    -- Generic description
    if p==nil then
-      return "#y".._("Uses flow to create a seeking energy orb that deals damage and disable with a cooldown. Strength varies depending on the flow amplifier.").."#0"
+      return "#y".._("Uses flow to create a seeking energy orb that deals damage and disable with a cooldown. Affected ships suffer from decreased movement and firerate. Strength varies depending on the flow amplifier.").."#0"
    end
-   local cost, cooldown, ref = getStats( p )
+   local cost, cooldown, ref, strength, duration = getStats( p )
    local refstats = ref:specificstats()
-   local damage, disable = refstats.damage, refstats.disable
+   local damage, disable, penetration = refstats.damage, refstats.disable, refstats.penetration
    local size = flow.size( p )
    local prefix = ""
    if size==1 then
@@ -35,12 +41,12 @@ function descextra( p, _o )
    elseif size==3 then
       prefix = _("(Greater)")
    end
-   return fmt.f("#y".._("{prefix} Uses {cost} flow to create a seeking energy orb that deals {damage} ion damage and {disable} disable with a {cooldown} second cooldown. Strength varies depending on the flow amplifier."),
-      {prefix=prefix, cost=cost, damage=damage, disable=disable, cooldown=cooldown}).."#0"
+   return fmt.f("#y".._("{prefix} Uses {cost} flow to create a seeking energy orb that deals {damage} ion damage and {disable} disable {penetration}% penetration and a {cooldown} second cooldown. Affected ships suffer from -{strength}% movement and firerate for {duration} seconds. Strength varies depending on the flow amplifier."),
+      {prefix=prefix, cost=cost, damage=damage, disable=disable, penetration=100*penetration, cooldown=cooldown, strength=25*strength, duration=duration}).."#0"
 end
 
 function init( p, po )
-   mem.flow_cost, mem.cooldown, mem.ref = getStats( p )
+   mem.flow_cost, mem.cooldown, mem.ref, mem.strength, mem.duration = getStats( p )
 
    mem.timer = 0
    po:state("off")
@@ -80,5 +86,5 @@ function ontoggle( p, po, on )
 end
 
 function onimpact( _p, target )
-   target:effectAdd("Chakra Corruption")
+   target:effectAdd( "Chakra Corruption", mem.duration, mem.strength )
 end
