ad7138f14774ff938d81be887e5850eeb73c3e28
Edgar Simo-Serra <bobbens@gmail.com>
2023-03-02 12:34:00 +0900

    WIP cleansing flames stuff

--- dat/outfits/flow/abilities/cleansing_flames.lua
+++ dat/outfits/flow/abilities/cleansing_flames.lua
@@ -1,20 +1,44 @@
 local flow = require "ships.lua.lib.flow"
 local fmt = require "format"
 
-function onadd( _p, po )
-   local size = po:slot().size
-   if size=="Small" then
-      mem.flow_cost   = 40
-      mem.ref         = nil
-      mem.range       = 200
-      mem.cooldown    = 5
-   else
-      error(fmt.f(_("Flow ability '{outfit}' put into slot of unknown size '{size}'!"),
-         {outfit=po:outfit(),size=size}))
+local function getStats( p, size )
+   local flow_cost, cooldown, ref, range
+   size = size or flow.size( p )
+   if size == 1 then
+      flow_cost   = 40
+      ref         = nil
+      range       = 200
+      cooldown    = 5
+   --elseif size == 2 then
+   --else
    end
+   return flow_cost, ref, range, cooldown
 end
 
-function init( _p, po )
+function descextra( p, _o )
+   -- Generic description
+   local size
+   if p then
+      size = flow.size( p )
+   else
+      size = 0
+   end
+   local s = "#y".._([[TODO]]).."#0"
+   for i=1,3 do
+      local cost, _ref, range, cooldown = getStats( nil, i )
+      local pfx = flow.prefix(i)
+      if i==size then
+         pfx = "#b"..pfx.."#n"
+      end
+      s = s.."\n"..fmt.f(_("#n{prefix}:#0 {cost} flow, {cooldown} s cooldown, {range} range"),
+         {prefix=pfx, cost=cost, range=range, cooldown=cooldown}).."#0"
+   end
+   return s
+end
+
+function init( p, po )
+   mem.flow_cost, mem.ref, mem.range, mem.cooldown = getStats( p )
+
    mem.timer = 0
    po:state("off")
 end
