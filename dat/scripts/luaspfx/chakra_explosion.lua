67fc6dfb71bd6c67eecc6bac9e24470c5f7a0fa3
Edgar Simo-Serra <bobbens@gmail.com>
2023-03-22 00:36:18 +0900

    chakra_explosion: renamed internals and return the spfx

--- dat/scripts/luaspfx/chakra_explosion.lua
+++ dat/scripts/luaspfx/chakra_explosion.lua
@@ -3,7 +3,7 @@ local lf = require 'love.filesystem'
 --local audio = require 'love.audio'
 local love_shaders = require 'love_shaders'
 
-local emp_shader, emp_sfx
+local exp_shader, exp_sfx
 
 local function update( s, dt )
    local d = s:data()
@@ -12,14 +12,14 @@ end
 
 local function render( sp, x, y, z )
    local d = sp:data()
-   emp_shader:send( "u_time",  d.timer )
-   emp_shader:send( "u_speed",  d.speed )
-   emp_shader:send( "u_grain",  d.grain )
-   emp_shader:send( "u_r", d.r )
+   exp_shader:send( "u_time",  d.timer )
+   exp_shader:send( "u_speed",  d.speed )
+   exp_shader:send( "u_grain",  d.grain )
+   exp_shader:send( "u_r", d.r )
 
    local s = d.size * z
    local old_shader = lg.getShader()
-   lg.setShader( emp_shader )
+   lg.setShader( exp_shader )
    love_shaders.img:draw( x-s*0.5, y-s*0.5, 0, s )
    lg.setShader( old_shader )
 end
@@ -29,7 +29,7 @@ local function spfx_chakra( pos, vel, size, params )
    local speed = params.speed or math.max(1.5-(size/350)^0.5, 0.4)
    local sfx
    if not params.silent then
-      sfx = emp_sfx[ rnd.rnd(1,#emp_sfx) ]
+      sfx = exp_sfx[ rnd.rnd(1,#exp_sfx) ]
    end
    local s  = spfx.new( 1/speed, update, nil, nil, render, pos, vel, sfx, size*0.5 )
    local d  = s:data()
@@ -42,23 +42,24 @@ local function spfx_chakra( pos, vel, size, params )
       local ss = s:sfx()
       ss:setVolume( params.volume )
    end
+   return s
 end
 
-local function emp( pos, vel, radius, params )
+local function exp( pos, vel, radius, params )
    params = params or {}
 
    -- Lazy loading shader / sound
-   if not emp_shader then
-      local emp_shader_frag = lf.read( "glsl/chakra_exp.frag" )
-      emp_shader = lg.newShader( emp_shader_frag )
-      emp_sfx = {
+   if not exp_shader then
+      local exp_shader_frag = lf.read( "glsl/chakra_exp.frag" )
+      exp_shader = lg.newShader( exp_shader_frag )
+      exp_sfx = {
          -- TODO sound
          audio.new( "snd/sounds/empexplode.ogg" ),
       }
    end
 
-   -- Create the emps
-   spfx_chakra( pos, vel, radius, params )
+   -- Create the exps
+   return spfx_chakra( pos, vel, radius, params )
 end
 
-return emp
+return exp
