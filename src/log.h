c9cca1ceb1be9eaf56dc03951d777eef6afb2f89
Justin Blanchard <UncombedCoconut@gmail.com>
2023-05-12 08:45:47 -0400

    Use C99 syntax for variadic macros. Note: this is theoretically less flexible where we'd want 0 extra args; C20 adds a __VA_OPT__ syntax without this limitation.

--- src/log.h
+++ src/log.h
@@ -12,22 +12,22 @@
 
 #include "nstring.h"
 
-#define LOG(str, args...)  (logprintf(stdout, 1, str, ## args))
-#define LOGERR(str, args...)  (logprintf(stderr, 1, str, ## args))
+#define LOG(str, ...)  (logprintf(stdout, 1, str, ## __VA_ARGS__))
+#define LOGERR(str, ...)  (logprintf(stderr, 1, str, ## __VA_ARGS__))
 #ifdef DEBUG_PARANOID /* Will cause WARNs to blow up */
-#define WARN(str, args...) (logprintf(stderr, 0, _("WARNING %s:%d [%s]: "), __FILE__, __LINE__, __func__), logprintf( stderr, 1, str, ## args), raise(SIGINT))
+#define WARN(str, ...) (logprintf(stderr, 0, _("WARNING %s:%d [%s]: "), __FILE__, __LINE__, __func__), logprintf( stderr, 1, str, ## __VA_ARGS__), raise(SIGINT))
 #else /* DEBUG_PARANOID */
-#define WARN(str, args...) (logprintf(stderr, 0, _("Warning: [%s] "), __func__), logprintf( stderr, 1, str, ## args))
+#define WARN(str, ...) (logprintf(stderr, 0, _("Warning: [%s] "), __func__), logprintf( stderr, 1, str, ## __VA_ARGS__))
 #endif /* DEBUG_PARANOID */
-#define ERR(str, args...)  (logprintf(stderr, 0, _("ERROR %s:%d [%s]: "), __FILE__, __LINE__, __func__), logprintf( stderr, 1, str, ## args), abort())
+#define ERR(str, ...)  (logprintf(stderr, 0, _("ERROR %s:%d [%s]: "), __FILE__, __LINE__, __func__), logprintf( stderr, 1, str, ## __VA_ARGS__), abort())
 #ifdef DEBUG
 #  undef DEBUG
-#  define DEBUG(str, args...) LOG(str, ## args)
+#  define DEBUG(str, ...) LOG(str, ## __VA_ARGS__)
 #  ifndef DEBUGGING
 #    define DEBUGGING 1
 #  endif /* DEBUGGING */
 #else /* DEBUG */
-#  define DEBUG(str, args...) do {;} while (0)
+#  define DEBUG(str, ...) do {;} while (0)
 #endif /* DEBUG */
 #define DEBUG_BLANK() DEBUG("%s", "")
 
