cc57485d52fcd461fedd90a7466caede2b9f9e4f
Edgar Simo-Serra <bobbens@gmail.com>
2023-06-17 21:20:13 +0900

    More work on Minerva Judgement.

--- dat/missions/minerva/judgement.lua
+++ dat/missions/minerva/judgement.lua
@@ -18,10 +18,15 @@
 --]]
 local minerva = require "common.minerva"
 local vn = require 'vn'
---local vni = require 'vnimage'
+local vni = require 'vnimage'
 local fmt = require "format"
 --local lmisn = require "lmisn"
 --local love_shaders = require "love_shaders"
+
+local zalek_image = "zalek_thug1.png"
+local dvaered_image = "dvaered_thug1.png"
+local zalek_colour = {1, 0.4, 0.4}
+local dvaered_colour = {1, 0.7, 0.3}
 
 local trialspb, trialsys = spob.getS("Jade Court")
 
@@ -110,6 +115,7 @@ end
 
 function trial_start ()
 --[[
+- minerva_altercation_helped ("dvaered", "zalek", nil)
 - maikki_gave_drink (true, nil)
 - maikki_response ("yes", "no", nil)
 - maikki_scavengers_alive (true, nil)
@@ -118,9 +124,18 @@ function trial_start ()
 --]]
    local didtrial = false
 
+   -- Main characters
+   local zuri = minerva.vn_zuri()
+   local kex = minerva.vn_kex()
+   local maikki = minerva.vn_maikki()
+   local ceo = minerva.vn_ceo()
+   local judge = vn.Characters.new( _("Judge Holmes"), {image=vni.genericFemale()} ) -- TODO replace graphics
+   local zlk = vn.Character.new( _("Za'lek Lawyer"), { image=zalek_image, color=zalek_colour } )
+   local dvd = vn.Character.new( _("Dvaered Representative"), { image=dvaered_image, color=dvaered_colour } )
+
    vn.clear()
    vn.scene()
-   local zuri = vn.newCharacter( minerva.vn_zuri() )
+   vn.newCharacter( zuri )
    vn.music( minerva.loops.pirate )
    vn.transition()
 
@@ -137,8 +152,88 @@ function trial_start ()
 
    vn.label("01_start")
    vn.func( function () didtrial = true end )
+   vn.na(_([[The station seems to be crowded with the many simultaneous trials and ongoings. Watching people bustling around with unpredictable patterns is surprisingly entertaining, however, you remember your purpose and follow Zuri to the assigned courtroom.]]))
    -- vn.music( ) -- TODO trial music
    -- TODO background?
+   vn.na(_([[After getting lost in the corridors overflowing with people several times, you make it to the courtroom. It seems to be quite packed and the ambience electric, with uncertainty about how the upcoming legal drama would unfold.]]))
+   zuri(_([[Zuri turns to you.
+"I'll be right back, I have to prepare some things. Take a set and hold on tight! I have a feeling this trial is going to be wild."]]))
+   vn.na(_([[Zuri leaves you and not having anything better to do in the clamorous room, you find an empty seat and sit down.]]))
+   vn.na(_([[Without anything better to do, you begin to observe your surroundings. Looking around the public you could make out quite a few figures you know.]]))
+
+   vn.scene()
+   vn.newCharacter( ceo )
+   vn.transition( "slideleft" )
+   vn.na(_([[The Minerva CEO is near the front of the crowd, he seems to be quite pale and looking very well.]]))
+
+   vn.scene()
+   vn.newCharacter( zlk )
+   vn.transition( "slideleft" )
+   vn.na(_([[To the left is the Za'lek crowd. They seem to be intently comparing notes and discussing amongst themselves.]]))
+
+   vn.scene()
+   vn.newCharacter( dvd )
+   vn.transition( "slideleft" )
+   vn.na(_([[To the right are the Dvaered officials. They seem to boisterously arguing with each other.]]))
+
+   vn.scene()
+   vn.newCharacter( maikki )
+   vn.transition( "slideleft" )
+   vn.na(_([[Wait, what is Maikki doing here?]]))
+
+   vn.scene()
+   vn.newCharacter( kex )
+   vn.transition( "slideleft" )
+   vn.na(_([[Even weirder, it seems like Kex is also at the court... How did they get here?]]))
+
+   vn.scene()
+   vn.transition( "slideleft" )
+   vn.na(_([[Before you can try to do anything, suddenly a hush fills the room. It seems like the show is about to start.]]))
+
+   vn.scene()
+   vn.newCharacter( judge )
+   vn.transition()
+   judge(_([[A judge floats up to the center of the room.
+"Order! Order!"
+She hits her gavel.]]))
+   vn.na(_([[Except for the odd cough, silence envelopes the room.]]))
+   judge(_([["We are gathered here to day to deliberate over the future of Minerva Station, which has been accused of falling into debauchery and lawlessness."]]))
+
+   vn.func( function ()
+      zlk.pos = "farleft"
+      dvd.pos = "farright"
+      ceo.pos = "farleft"
+      maikki.pos = "farleft"
+   end )
+   judge(_([["The three parties involved are House Za'lek..."]]))
+   vn.appear( zlk, "slideup" )
+   zlk(_([[The Za'lek lawyer does a short concise bow.
+"Your honour."]]))
+   vn.disappear( zlk, "slideup" )
+   judge(_([["...House Dvaered..."]]))
+   vn.appear( dvd, "slideup" )
+   dvd(_([[The Dvaered representative does an exaggerated deep bow.
+"Your honour."]]))
+   vn.disappear( dvd, "slideup" )
+   judge(_([["..and the CEO of Minerva Station."]]))
+   vn.appear( ceo, "slideup" )
+   ceo(_([[A pale feverishly sweating Minerva CEO stands up.
+"Your h..."
+...and promptly collapses in a puddle of their sweat.]]))
+   vn.na(_([[A collective gasp is let out in the courtroom. Not the best way to start the deliberations.]]))
+   vn.disappear( ceo, "slideup" )
+   judge(_([[The judge clicks her tongue in annoyance.
+"Passing out without filing an EJ-2891? Unheard of."]]))
+   vn.appear( maikki, "slideup" )
+   maikki(_([[Suddenly, Maikki stands up and raises a wad of papers.]]))
+   judge(_([["Is that the EJ-2891? Go on."
+The judge nods towards Maikki, giving her permission to speak.]]))
+   maikki(_([["Your honour."
+She gives an impeccable formal bow.]]))
+   maikki(_([["I would like to submit an EJ-7777 motion to substitute the Minera CEO." ]]))
+   judge(_([[The judge makes a curious expression and hovers down to collect the papers from Maikki. She quickly skims over it, and gives a sly grin.]]))
+   judge(_([["Well it seems like the deliberations will be able to proceed today. Replacing the previous individual, we have Maisie McPherson, who will be representing the independent interests of Minerva Station. Please sit down."]]))
+   vn.disappear( maikki, "slideup" )
 
    vn.run()
 
