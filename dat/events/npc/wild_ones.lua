159c576068e2ae3e9693ecac3718f0c81c84f529
Edgar Simo-Serra <bobbens@gmail.com>
2023-06-20 00:48:56 +0900

    More wild ones npc descriptions.

--- dat/events/npc/wild_ones.lua
+++ dat/events/npc/wild_ones.lua
@@ -11,6 +11,11 @@ desc_list["generic"] = {
    _("An unruly Wild Ones clansperson."),
    _("You see a rough looking Wild Ones clansperson."),
    _("The Wild Ones clansperson looks like they've seen some real shit."),
+   _("A scarred  Wild Ones clansperson, leaning back in their chair with an air of nonchalant confidence."),
+   _("A weathered Wild Ones clansperson with a permanent sneer etched on their face."),
+   _("A gruff Wild Ones clansman quietly nursing their drink."),
+   _("A wiry Wild Ones clansman, fingers tapping restlessly on the bar counter"),
+   _("A burly Wild Ones clansman, arms crossed and scowling"),
 }
 
 local msg_lore = {
