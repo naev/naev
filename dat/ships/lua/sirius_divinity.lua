d4dcc56780f994a4f5b655153dae02eca53cb03b
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-04 00:49:12 +0900

    Gave Sirius Divinity bonus damage to fighters.

---
+++ dat/ships/lua/sirius_divinity.lua
@@ -0,0 +1,28 @@
+local flow = require "ships.lua.lib.flow"
+require "ships.lua.sirius"
+
+local DT    = 1
+local RANGE = 3000
+
+local super_init = init
+function init( p )
+   super_init( p )
+   mem.t = 0
+end
+
+function update( p, dt )
+   mem.t = mem.t - dt
+   if mem.t > 0 then
+      return
+   end
+   mem.t = mem.t + DT
+
+   local f = flow.get( p )
+   local mod = f*0.08-20
+   local rmod = p:shipstat("ew_detect",true)
+   for k,v in ipairs(p:getAllies( RANGE*rmod )) do
+      if v:leader()==p and v:memory().carried then
+         v:effectAdd( "Psychic Divinity", nil, mod )
+      end
+   end
+end
--- dat/ships/lua/sirius_dogma.lua
+++ dat/ships/lua/sirius_dogma.lua
@@ -1,11 +1,6 @@
 local flow = require "ships.lua.lib.flow"
 require "ships.lua.sirius"
 
-local super_init = init
-function init( p )
-   super_init( p )
-end
-
 function update( p, _dt )
    local f = flow.get( p )
    local mod = f*0.04-10
