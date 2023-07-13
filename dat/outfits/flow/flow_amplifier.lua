89ff2916024790cb443b0f6c7019b43c24b26803
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-13 17:10:04 +0900

    Flow amplifiers aren't active outfits and use lua to do desc_extra.

--- dat/outfits/flow/flow_amplifier.lua
+++ dat/outfits/flow/flow_amplifier.lua
@@ -1,6 +1,17 @@
-local flow = require "ships.lua.lib.flow"
+notactive = true -- Doesnt' become active
 
-notactive = false
+local flow = require "ships.lua.lib.flow"
+local fmt = require "format"
+local srs = require "common.sirius"
+
+function descextra( _p, o )
+   local powers
+   if not srs.playerIsPsychic() then
+      powers = "\n#r".._("Requires psychic powers to use.").."#0"
+   end
+   return fmt.f(_("Provides {flow} maximum flow capacity and allows a ship to use flow and allows gaining 10% of damage received as flow.{powers}"),
+      { flow=flow.list_base[o:nameRaw()], powers=powers })
+end
 
 function init( p )
    flow.recalculate( p )
