32619e9ff27aca6d8763cf767d112b51f7941db3
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-14 22:26:32 +0900

    Reorganized the player_autonav header.

--- src/player_autonav.h
+++ src/player_autonav.h
@@ -11,18 +11,24 @@ enum {
    AUTONAV_PILOT, /**< Player is going to a pilot. */
 };
 
+/* Initialization of the internal autonav stuff. */
 int player_autonavInit (void);
 
+/* Updating routines. */
 void player_thinkAutonav( Pilot *pplayer, double dt );
 void player_updateAutonav( double dt );
+
+/* Control routines. */
 void player_autonavResetSpeed (void);
 void player_autonavStart (void);
 void player_autonavEnd (void);
 void player_autonavAbort( const char *reason );
+void player_autonavReset( double s );
+void player_autonavEnter (void);
+
+/* Starting routines, for initializing autonav. */
 void player_autonavStartWindow( unsigned int wid, const char *str );
 void player_autonavPos( double x, double y );
 void player_autonavSpob( const char *name, int tryland );
 void player_autonavPil( unsigned int p );
 void player_autonavBoard( unsigned int p );
-void player_autonavReset( double s );
-void player_autonavEnter (void);
