e3f15f6e52bcb7fb2b578a286bab5e0eca7e2044
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-15 01:08:59 +0900

    Add headers to po/credits.pot to fix meson compile naev-pot.

--- po/credits_pot.py
+++ po/credits_pot.py
@@ -4,11 +4,33 @@ import os
 import re
 import sys
 
-for fn in sys.argv[1:]:
-    with open(fn) as f:
-        fn = re.sub('.*/dat', 'dat', fn)
-        for i, line in enumerate(f):
-            line = line.rstrip('\r\n')
-            if line and not line.startswith('['):
-                quoted_escaped_line = json.dumps(line, ensure_ascii=False)
-                print('#: {}:{}\nmsgid {}\nmsgstr ""\n'.format(fn, i+1, quoted_escaped_line))
+HEADER = r"""# Naev Credits
+# Copyright (C) YEAR Naev Dev Team
+# This file is distributed under the same license as the naev package.
+# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
+#
+#, fuzzy
+msgid ""
+msgstr ""
+"Project-Id-Version: PACKAGE VERSION\n"
+"Report-Msgid-Bugs-To: https://github.com/naev/naev/issues\n"
+"POT-Creation-Date: YEAR-MO-DA HO:MI+ZONE\n"
+"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
+"Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
+"Language-Team: LANGUAGE <LL@li.org>\n"
+"Language: \n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"""
+
+with open(sys.argv[1],"w") as fout:
+    for fn in sys.argv[2:]:
+        fout.write(HEADER)
+        with open(fn) as f:
+            fn = re.sub('.*/dat', 'dat', fn)
+            for i, line in enumerate(f):
+                line = line.rstrip('\r\n')
+                if line and not line.startswith('['):
+                    quoted_escaped_line = json.dumps(line, ensure_ascii=False)
+                    fout.write('#: {}:{}\nmsgid {}\nmsgstr ""\n'.format(fn, i+1, quoted_escaped_line))
