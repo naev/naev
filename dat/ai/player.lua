b96b989a37790fac1f85dc07c1fb8931703dfec7
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-15 12:37:24 +0900

    Added lanes.clearCache and made player AI use neutral lanes by default.

--- dat/ai/player.lua
+++ dat/ai/player.lua
@@ -1,6 +1,10 @@
+--[[
+   AI for the player, basically meant for escorts and other functionality.
+--]]
 require 'ai.core.core'
 mem.aggressive = true -- Player is an asshole
 mem.atk_kill = false -- Should propagate to fighters
+mem.lanes_useneutral = true -- Uses neutral lanes
 
 function should_attack( enemy, _si, _aggressor )
    if not enemy or not enemy:exists() then
