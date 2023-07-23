244a014a3a4da774245afb4cd2152643a5d0fdf9
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-23 13:15:36 +0900

    Added missing target.h file.

---
+++ src/target.h
@@ -0,0 +1,28 @@
+/*
+ * See Licensing and Copyright notice in naev.h
+ */
+#pragma once
+
+/**
+ * @brief Used to generalize what the weapon is targetting.
+ */
+typedef enum TargetType_ {
+   TARGET_NONE,
+   TARGET_PILOT,
+   TARGET_WEAPON,
+   TARGET_ASTEROID,
+} TargetType;
+
+/**
+ * @brief Represents a weapon target.
+ */
+typedef struct Target_ {
+   TargetType type; /* Target type. */
+   union {
+      unsigned int id;   /* For pilot/weapons. */
+      struct {
+         int anchor;
+         int asteroid;
+      } ast; /* For asteroids. */
+   } u;
+} Target;
