83284938b4b04ff97e2a725a9ddc17d6adf1d6ed
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-23 15:59:44 +0900

    Point defense only fires at enemies it can track.

--- dat/outfits/pointdefense/pointdefense.lua
+++ dat/outfits/pointdefense/pointdefense.lua
@@ -1,8 +1,8 @@
-local range, range2, hitships
+local range, range2, hitships, trackmin
 
 function onload( o )
-   local _dps, _disps, _eps, _tmin, _tmax
-   _dps, _disps, _eps, range, _tmin, _tmax = o:weapstats()
+   local _dps, _disps, _eps, _trackmax
+   _dps, _disps, _eps, range, trackmin, _trackmax = o:weapstats()
    range2 = range*range
    hitships = not o:missShips()
 end
@@ -28,7 +28,7 @@ function update( p, po, _dt )
       m = nil
    end
    -- See if we want to retarget
-   if not (m  and pos:dist2( m:pos() ) < range2) or mem.tpilot then
+   if not (m and pos:dist2( m:pos() ) < range2) then
 
       -- Try to prioritize munitions
       local mall = munition.getInrange( pos, range, p )
@@ -42,10 +42,29 @@ function update( p, po, _dt )
       if not m and hitships then
          local pall = p:getEnemies( range )
          if #pall > 0 then
-            m = pall[ rnd.rnd(1,#mall) ]
-            mem.target = m
-            mem.tpilot = true
+            local ptarget = {}
+            for k,e in ipairs(pall) do
+               if e:evasion() < trackmin then
+                  table.insert( ptarget, e )
+               end
+            end
+            if #ptarget > 0 then
+               m = ptarget[ rnd.rnd(1,#mall) ]
+               mem.target = m
+               mem.tpilot = true
+            end
          end
+      end
+   end
+
+   -- Prefer munitions over pilots
+   if mem.tpilot then
+      -- Try to prioritize munitions
+      local mall = munition.getInrange( pos, range, p )
+      if #mall > 0 then
+         m = mall[ rnd.rnd(1,#mall) ] -- Just get a random one
+         mem.target = m
+         mem.tpilot = false
       end
    end
 
