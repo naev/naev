0d1013ba880b3d8f5a39fe213c371e18fa74a103
Edgar Simo-Serra <bobbens@gmail.com>
2023-07-30 14:23:48 +0900

    Start to move more autonav settings to the player.

--- dat/events/settings.lua
+++ dat/events/settings.lua
@@ -7,11 +7,15 @@
 </event>
 --]]
 --[[
-   General settings.
+   General per-pilot settings.
 --]]
 local luatk = require "luatk"
+local fmt = require "format"
 
 local settings, uselanes_jump, uselanes_spob, pick_gui
+local reset_dist, reset_shield
+
+local AUTONAV_MAX_DIST = 10e3
 
 function create ()
    -- Load variables
@@ -22,12 +26,15 @@ function create ()
       uselanes_jump = true
       var.push( "autonav_uselanes_jump", true )
    end
+   reset_shield = var.peek("autonav_reset_shield") or 1
+   reset_dist = var.peek("autonav_reset_dist") or 5e3
 
    -- Set an info button up
    player.infoButtonRegister( _("Settings"), settings, 1, "A" )
 end
 
 function settings ()
+   local txt_autonav, fad_autonav
    local chk_uselanes_jump, chk_uselanes_spob
 
    local w, h = 600, 420
@@ -35,9 +42,42 @@ function settings ()
    wdw:setCancel( luatk.close )
    luatk.newText( wdw, 0, 10, w, 20, _("Player Settings"), nil, "center" )
 
+   local function update_autonav( _fad, value )
+      local msg = _("Stop Speedup At: ")
+      if value > 1 then
+         reset_dist = math.huge
+         reset_shield = 1
+         msg = msg.._("Enemy Presence")
+      elseif value > 0.5 then
+         reset_dist = (value-0.5) / 0.5 * AUTONAV_MAX_DIST
+         reset_shield = 1
+         msg = msg..fmt.f(_("Enemy within {dist} distance"),{dist=fmt.number(reset_dist)})
+      elseif value > 0 then
+         reset_dist = -1
+         reset_shield = value / 0.5
+         msg = msg..fmt.f(_("{shield:.0f}% Shield"),{shield=reset_shield*100})
+      else
+         reset_dist = -1
+         reset_shield = 0
+         msg = msg.._("Armour Damage")
+      end
+      txt_autonav:set( msg )
+   end
+   local autonav_value
+   if reset_dist > 0 then
+      autonav_value = math.min( 1, 0.5 + 0.5 * reset_dist / AUTONAV_MAX_DIST )
+   else
+      autonav_value = math.max( 0, 0.5 * reset_shield )
+   end
+
    local y = 40
    luatk.newText( wdw, 20, y, w-40, 20, "#n".._("Autonav") )
    y = y + 20
+   txt_autonav = luatk.newText( wdw, 20, y, w-40, 20 )
+   y = y + 20
+   fad_autonav = luatk.newFader( wdw, 20, y, w-40, 20, 0, 1, nil, update_autonav )
+   fad_autonav:set( autonav_value )
+   y = y + 40
    chk_uselanes_jump = luatk.newCheckbox( wdw, 20, y, w-40, 20, _("Use patrol lanes when jumping"), nil, uselanes_jump )
    y = y + 30
    chk_uselanes_spob = luatk.newCheckbox( wdw, 20, y, w-40, 20, _("Use patrol lanes when travelling to a space object"), nil, uselanes_spob )
@@ -55,6 +95,8 @@ function settings ()
    uselanes_spob = chk_uselanes_spob:get()
    var.push( "autonav_uselanes_jump", uselanes_jump )
    var.push( "autonav_uselanes_spob", uselanes_spob )
+   var.push( "autonav_reset_dist", reset_dist )
+   var.push( "autonav_reset_shield", reset_shield )
 end
 
 function pick_gui ()
