0dc152c3426d1cc8a9cf685033e3f2a7f763e720
Edgar Simo-Serra <bobbens@gmail.com>
2023-10-06 11:52:34 +0900

    Made rackham's razor give double effect for pirate ships.

---
+++ dat/outfits/accessory/rackham_razor.lua
@@ -0,0 +1,9 @@
+local pir = require "common.pirate"
+
+function init( p, po )
+   if pir.isPirateShip( p ) then
+      -- It's multiplicative so we add a bonus such that multiplied by the base bonus we get double the original amount
+      po:set( "loot_mod", 120/1.1-100 )
+      po:set( "jam_chance", 120/1.1/100 )
+   end
+end
