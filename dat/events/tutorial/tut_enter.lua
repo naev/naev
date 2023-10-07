3c91b8cde03ab24940e8800ecc1995a50aa15a55
Edgar Simo-Serra <bobbens@gmail.com>
2023-10-07 21:50:15 +0900

    Added small tutorial for when the player takes off with something not assigned to a weapon set.

--- dat/events/tutorial/tut_enter.lua
+++ dat/events/tutorial/tut_enter.lua
@@ -3,6 +3,10 @@
 <event name="Enter Tutorial Event">
  <location>enter</location>
  <chance>100</chance>
+ <cond>
+   local tut = require "common.tutorial"
+   return not tut.isDisabled()
+ </cond>
 </event>
 --]]
 --[[
@@ -15,16 +19,35 @@ local tut = require "common.tutorial"
 local vn  = require "vn"
 
 function create ()
-   if tut.isDisabled() then evt.finish() end
-
+   local pp = player.pilot()
    local enter_delay = 5
 
    hook.land( "evt_done" )
 
    -- TODO we should probably allow looking at any faction not just empire
-   if not var.peek("tut_illegal") and player.pilot():hasIllegal("Empire") then
+   if not var.peek("tut_illegal") and pp:hasIllegal("Empire") then
       hook.timer( enter_delay, "tut_illegal" )
       return
+   end
+
+   --  If the player has not set up an outfit in a weapon set we have to warn them
+   if not var.peek("tut_weapset") then
+      local o = {}
+      for k,v in ipairs(pp:outfits()) do
+         if v and v:toggleable() then
+            o[k] = true -- mark slot as needs checking
+         end
+      end
+      for wid=1,10 do
+         for k,v in ipairs(pp:weapsetList(wid)) do
+            o[v] = nil -- unmark as it is in weapon set
+         end
+      end
+      -- Warn about remaining ones
+      local hasunused = (next(o)~=nil)
+      if hasunused then
+         hook.timer( enter_delay, "tut_weapset", o )
+      end
    end
 
    local sys = system.cur()
@@ -63,7 +86,7 @@ function tut_illegal ()
    local sai = vn.newCharacter( tut.vn_shipai() )
    vn.transition( tut.shipai.transition )
    sai(fmt.f(_([[Just as after taking off, {ainame} materializes in front of you.
-   "I don't know how to say this to you, but it seems like you have acquired some illegal items. Your {item} is on the rather dubious side of the law, and you will run into trouble if you are scanned by patrols. Sometimes, right after you are discovered, you may get away with paying a small fine in the form of a bribe, but that may not always be the case."]]),
+"I don't know how to say this to you, but it seems like you have acquired some illegal items. Your {item} is on the rather dubious side of the law, and you will run into trouble if you are scanned by patrols. Sometimes, right after you are discovered, you may get away with paying a small fine in the form of a bribe, but that may not always be the case."]]),
       {item=badstuff[ rnd.rnd(1,#badstuff) ], ainame=tut.ainame()} ))
    sai(fmt.f(_([["If you have to deal with illegal goods, I would recommend you to try to maximize the stealth functionality of your ship, that you can enable with {stealthkey}. As ship detection in general is tied to ship mass, you will most likely have best result using small and agile ships to avoid detection. Remember to stay off patrol routes and avoid crowded systems for highest chance of success."]]),
       {stealthkey=tut.getKey("stealth")} ))
@@ -74,6 +97,79 @@ function tut_illegal ()
    vn.run()
 
    var.push( "tut_illegal", true )
+   evt.finish()
+end
+
+function tut_weapset( unused )
+   local unused_outfit, skipped
+   for k,v in ipairs(unused) do
+      unused_outfit = player.pilot():outfitGet(k)
+   end
+
+   vn.clear()
+   vn.scene()
+   local sai = vn.newCharacter( tut.vn_shipai() )
+   vn.transition( tut.shipai.transition )
+   sai(fmt.f(_([[Just as after taking off, {ainame} materializes in front of you.
+"I was reviewing the ship loadout to keep the urges away, and noticed that even though {item} is equipped on the ship, it is not part of any weapon set!"]]),
+      {item=unused_outfit, ainame=tut.ainame()} ))
+   vn.menu{
+      {_([[Continue]]), "cont01"},
+      {_([["Urges?!"]]), "cont01_urges"},
+      {_([[Skip tutorial]]), "skip"},
+   }
+
+   vn.label("cont01_urges")
+   sai(fmt.f(_([["Um, that's not important right now, what is important is that if you do not set {item} in a weapon set, you won't be able to use it when it becomes necessary!"]]),
+      {item=unused_outfit}))
+   vn.jump("cont01")
+
+   vn.label("cont01_cont")
+   sai(fmt.f(_([["If you do not set {item} in a weapon set, you won't be able to use it when it becomes necessary!"]]),
+      {item=unused_outfit}))
+   vn.jump("cont01")
+
+   vn.label("cont01")
+   sai(fmt.f(_([["To be able to use your outfits, you have to open the info menu with {infokey}, and from there access the weapons tab. From there you will be able to configure how you use the different outfits. Let me open it up for you."]]),
+      {infokey=tut.getKey("info")}))
+   vn.done( tut.shipai.transition )
+
+   vn.label("skip")
+   vn.na(fmt.f(_([[You tell {shipai} that you will solve it out yourself.]]),
+      {shipai=tut.ainame()}))
+   vn.func( function () skipped = true end )
+   vn.done( tut.shipai.transition )
+   vn.run()
+
+   if skipped then
+      var.push( "tut_weapset", true )
+      evt.finish()
+   else
+      hook.safe( "tut_weapset_info", unused_outfit )
+   end
+end
+
+function tut_weapset_info( unused_outfit )
+   naev.menuInfo( "weapons" )
+
+   vn.clear()
+   vn.scene()
+   vn.transition( tut.shipai.transition )
+   local sai = vn.newCharacter( tut.ainame(), {
+      color=tut.shipai.colour
+   } )
+   sai(_([["A large part of combat is decided ahead of time by the ship classes and their load out. However, good piloting can turn the tables easily. It is important to assign weapon sets to be easy to use. You can set weapon sets from the '#oWeapons#0' tab of the information window. You have 10 different weapon sets that can be configured separately for each ship."]]))
+   sai(_([["There are three different types of weapon sets:
+- #oSwitch#0: activating the hotkey will set your primary and secondary weapons
+- #oToggle#0: activating the hotkey will toggle the outfits between on/off states
+- #oHold#0: holding the hotkey will turn the outfits on]]))
+   sai(_([["By default, the weapon sets will be automatically managed by me, with forward bolts in set 1 (switch), turret weapons in set 2 (switch), and both turret and forward weapons in set 3 (switch). Seeker weapons are in set 4 (hold), and fighter bays in set 5 (hold). However, you can override this and set them however you prefer. Just remember to update them whenever you change your outfits."]]))
+   sai(fmt.f(_([["Go ahead and try to add {item} to a weapon set!"]]),
+      {item=unused_outfit}))
+   vn.done( tut.shipai.transition )
+   vn.run()
+
+   var.push( "tut_weapset", true )
    evt.finish()
 end
 
