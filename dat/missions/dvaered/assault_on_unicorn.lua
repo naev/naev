84dd8489cb46ed6231a0cf50b3fa50e153149349
Edgar Simo-Serra <bobbens@gmail.com>
2023-10-08 10:41:01 +0900

    assault_on_unicorn: include return system in OSD

--- dat/missions/dvaered/assault_on_unicorn.lua
+++ dat/missions/dvaered/assault_on_unicorn.lua
@@ -46,10 +46,11 @@ local function update_osd()
    local osd_msg = {}
    osd_msg[1] = _("Fly to the Unicorn system.")
    if mem.bounty_earned == mem.max_payment then
-      osd_msg[2] = fmt.f(_("You have reached your maximum payment. Return to {pnt}."), {pnt=mem.planet_start})
+      osd_msg[2] = fmt.f(_("You have reached your maximum payment. Return to {pnt} ({sys} system)."),
+         {pnt=mem.planet_start, sys=mem.system_start})
    else
-      osd_msg[2] = fmt.f(_("Destroy some pirates! You have killed {n} and have earned {credits}. If finished, return to {pnt}."),
-                         {n=mem.pirates_killed, credits=fmt.credits(mem.bounty_earned), pnt=mem.planet_start})
+      osd_msg[2] = fmt.f(_("Destroy some pirates! You have killed {n} and have earned {credits}. If finished, return to {pnt} ({sys} system)."),
+         {n=mem.pirates_killed, credits=fmt.credits(mem.bounty_earned), pnt=mem.planet_start, sys=mem.system_start})
    end
    misn.osdCreate(_("Assault on Unicorn"), osd_msg)
 end
@@ -60,7 +61,8 @@ function create ()
    mem.max_payment = rep * 50e3
    misn.setTitle(dv.prefix.._("Assault on Unicorn"))
    misn.setReward(_("Variable"))
-   misn.setDesc(fmt.f(_("It is time to put a dent in the pirates' forces. We have detected a strong pirate presence in the system of Unicorn. We are offering a small sum for each pirate killed. The maximum we will pay you is {credits}."), {credits=fmt.credits(mem.max_payment)} ))
+   misn.setDesc(fmt.f(_("It is time to put a dent in the pirates' forces. We have detected a strong pirate presence in the system of Unicorn. We are offering a small sum for each pirate killed. The maximum we will pay you is {credits}."),
+      {credits=fmt.credits(mem.max_payment)} ))
 
    mem.marker = misn.markerAdd( misn_target_sys, "computer" )
 end
@@ -69,7 +71,7 @@ function accept ()
    -- This mission makes no system claims.
    if misn.accept() then
       mem.pirates_killed = 0
-      mem.planet_start = spob.cur()
+      mem.planet_start, mem.system_start = spob.cur()
       mem.marker2 = misn.markerAdd( mem.planet_start, "low" )
       mem.pirates_killed = 0
       mem.bounty_earned = 0
