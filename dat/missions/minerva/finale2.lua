6bee14f59ccc4fcb634cc0732dc775e0f19e7788
Edgar Simo-Serra <bobbens@gmail.com>
2023-10-13 21:19:59 +0900

    minerva/finale2: more WIP stuff

--- dat/missions/minerva/finale2.lua
+++ dat/missions/minerva/finale2.lua
@@ -171,7 +171,7 @@ Maikki gives you a weird two finger salute and takes off to her ship, leaving yo
    hook.enter("enter")
 end
 
-local boss, hailhook
+local boss, guards, hailhook, bosshailed
 function enter ()
    local scur = system.cur()
    if scur==mainsys and mem.state==1 then
@@ -181,12 +181,19 @@ function enter ()
 
       local pos = mainspb:pos() + vec2.new( 100+200*rnd.rnd(), rnd.angle() )
       boss = pilot.add( "Empire Pacifier", "Empire", pos, nil, {ai="guard"} )
+      guards = { boss }
       for k,v in ipairs{"Empire Lancelot", "Empire Lancelot"} do
          local p = pilot.add( v, "Empire", pos+rnd.rnd(50,rnd.angle()), nil, {ai="guard"} )
          p:setLeader( boss )
+         table.insert( guards, p )
       end
 
+      bosshailed = false
+      mainspb:landDeny(true,_("Special authorization needed."))
+
       hailhook = hook.hail_spob( "comm_minerva" )
+      hook.pilot( boss, "hail", "comm_boss" )
+      hook.pilot( boss, "death", "boss_death" )
    elseif scur==mainsys and mem.state==2 then
       -- Set up fight
       pilot.clear()
@@ -200,16 +207,169 @@ function enter ()
    end
 end
 
-function comm_minerva( commspb )
-   if commspb ~= mainspb then return end
+local landack, timetodie
+local function talk_boss( fromspob )
+   if timetodie then
+      player.msg(_("Communication channel is closed."))
+      return
+   elseif landack then
+      if boss and boss:exists() then
+         boss:comm(_("Proceed to land."))
+      end
+      return
+   end
+
+   local askwhy, left01, left02, left03
 
    vn.clear()
    vn.scene()
-   local spb = ccomm.newCharacterSpob( vn, mainspb )
+   local p = ccomm.newCharacter( vn, boss )
    vn.transition()
-   spb(_([[]]))
+   if fromspob then
+      vn.na(fmt.f(_([[You try to open a communication channel with {spb}, but get rerouted to an Imperial ship.]]),
+         {spb=mainspb}))
+   end
+   if bosshailed then
+      p(_([["What do you want again? I told you Minerva Station is locked down until further notice."]]))
+   else
+      p(_([["What do you want? Minerva Station is locked down until further notice."]]))
+      vn.func( function () bosshailed = true end )
+   end
+
+   vn.label("menu")
+   vn.menu( function ()
+      local opts = {
+         {_([["Why is it locked down?"]]), "01_why"},
+         {_([[Leave.]]), "leave"},
+      }
+      table.insert( opts, 2, {_([["I'm here to do cleaning."]]), "01_contractor"} )
+      if left03 then
+         table.insert( opts, 2, {_([["Please, let me land!"]]), "01_left4"} )
+      elseif left02 then
+         table.insert( opts, 2, {_([["It's a matter of life and death!"]]), "01_left3"} )
+      elseif left01 then
+         table.insert( opts, 2, {_([["What I left is really important!"]]), "01_left2"} )
+      else
+         table.insert( opts, 2, {_([["I left something at the station."]]), "01_left"} )
+      end
+      if askwhy then
+         table.insert( opts, 2, {_([["I brought Imperial clearance."]]), "01_clearance"} )
+      end
+      return opts
+   end )
+
+   vn.label("01_why")
+   p(_([["Imperial Decree 28701-289 is why. No access without proper Imperial clearance. No exceptions."]]))
+   vn.func( function () askwhy = true end )
+   vn.jump("menu")
+
+   vn.label("01_left")
+   p(_([["Tough luck buddy. Judge's orders. Not going to risk a pay cut for someone I don't know. File an EB-89212-9 with information on it and you may be able to get it back."]]))
+   vn.func( function () left01 = true end )
+   vn.jump("menu")
+
+   vn.label("01_left2")
+   p(_([["Everyone thinks everything is important to them, buddy. The richest man is he who needs nothing, but just go file an EB-89212-9 if you want it back."]]))
+   vn.func( function () left02 = true end )
+   vn.jump("menu")
+
+   vn.label("01_left3")
+   p(_([["Why, you should have have said so at the beginning! No, seriously, buddy. File the EB-89312-9. What, or was it EB-89213-9? Anyway, you're not getting through. Don't push your luck."]]))
+   vn.func( function () left03 = true end )
+   vn.jump("menu")
+
+   vn.label("01_left4")
+   p(_([["Would have preferred to have an office job instead of having to deal with punks..."
+They let out a sigh.]]))
+   vn.jump("timetodie")
+
+   vn.label("01_contractor")
+   vn.func( function ()
+      local s = player.pilot():ship()
+      local t = s:tags()
+      if t.standard and t.transport then
+         vn.jump("contractor_ok")
+      else
+         vn.jump("contractor_bad")
+      end
+   end )
+   vn.label("contractor_ok")
+   p(_([["Great! We were expecting you, it's a mess down there. Proceed to land."]]))
+   vn.jump("landack")
+   vn.label("contractor_bad")
+   p(fmt.f(_([["Wait, why are you in a {ship}? Cleaning crew usually comes in a Koala or Mule? Show me your credentials!"]]),
+      {ship=player.pilot():ship()}))
+   vn.menu{
+      {_([["Company is doing a fleet renewal."]]),"contractor_bad_renewal"},
+      {_([["Left it at the office."]]),"contractor_bad_left"}
+   }
+   vn.label("contractor_bad_renewal")
+   vn.func( function ()
+      local pp = player.pilot()
+      local weaps = pp:outfitsList("weapon")
+      if #weaps >= 2 then
+         vn.jump("contractor_bad_renewal_fight")
+      else
+         vn.jump("contractor_bad_left")
+      end
+   end )
+   vn.label("contractor_bad_renewal_fight")
+   p(_([["What's the deal with all the weapons you're sporting then? You look like you're looking for trouble, but the Empire always takes out the trash!"]]))
+   vn.jump("timetodie")
+   vn.label("contractor_bad_left")
+   p(_([["Go get them then. You are not getting landing access without your credentials."]]))
+   vn.na(_([[The communication channel cuts off.]]))
+   vn.done()
+
+   vn.label("01_clearance")
+   vn.jump("menu")
+
+   vn.label("timetodie")
+   vn.func( function () timetodie = true end )
+   vn.na(_([[The communication channel cuts off as your sensors pick up signals of weapons powering up.]]))
+   vn.done()
+
+   vn.label("landack")
+   vn.na(_([[Your navigation system lights up green as you receive a confirmation of landing access to Minerva Station.]]))
+   vn.func( function () landack=true end )
+   vn.done()
+
+   vn.menu("leave")
+   vn.na(_([[You close the communication channel.]]))
+   vn.done()
+
+   if landack then
+      mainspb:landAllow(true)
+   elseif timetodie then
+      for k,v in ipairs(guards) do
+         v:setHostile(true)
+      end
+      if hailhook then
+         hook.rm( hailhook )
+         hailhook = nil
+      end
+   end
 
    vn.run()
+end
 
+function comm_boss()
+   talk_boss( false )
    player.commClose()
+end
+
+function comm_minerva( commspb )
+   if commspb ~= mainspb then return end
+   talk_boss( true )
+   player.commClose()
+end
+
+function boss_death ()
+   if hailhook then
+      hook.rm( hailhook )
+      hailhook = nil
+   end
+
+   mainspb:landAllow(true)
+   player.msg(fmt.f(_("With the blocking ship out of the way, you should be able to land on {spb} now."), {spb=mainspb}))
 end
