cef28c0f16abcd2292e1f50051f3b27c40b8d6ea
Edgar Simo-Serra <bobbens@gmail.com>
2023-10-30 21:25:35 +0900

    Switch order of weapon set cycle to switch -> hold -> toggle

--- src/info.c
+++ src/info.c
@@ -721,13 +721,13 @@ static void weapons_toggleList( unsigned int wid, const char *str )
    t = pilot_weapSetTypeCheck( player.p, info_eq_weaps.weapons );
    switch (t) {
       case WEAPSET_TYPE_SWITCH:
-         c = WEAPSET_TYPE_TOGGLE;
+         c = WEAPSET_TYPE_HOLD;
          break;
       case WEAPSET_TYPE_HOLD:
-         c = WEAPSET_TYPE_SWITCH;
+         c = WEAPSET_TYPE_TOGGLE;
          break;
       case WEAPSET_TYPE_TOGGLE:
-         c = WEAPSET_TYPE_HOLD;
+         c = WEAPSET_TYPE_SWITCH;
          break;
       default:
          /* Shouldn't happen... but shuts up GCC */
