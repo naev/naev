049cb1a92bd3e01277122ac0ee5102e0b7b4278b
Justin Blanchard <UncombedCoconut@gmail.com>
2023-11-01 18:48:38 -0400

    Bump subproject to SuiteSparse 7.3.1.beta1, reverting a harmless workaround for 7.3.0.

--- src/safelanes.c
+++ src/safelanes.c
@@ -10,12 +10,6 @@
 /** @cond */
 #include <math.h>
 
-#if HAVE_SUITESPARSE_CHOLMOD_H
-#include <suitesparse/cholmod.h>
-#else /* HAVE_SUITESPARSE_CHOLMOD_H */
-#include <cholmod.h>
-#endif /* HAVE_SUITESPARSE_CHOLMOD_H */
-
 #if HAVE_OPENBLAS_CBLAS_H
 #   include <openblas/cblas.h>
 #elif HAVE_CBLAS_OPENBLAS_H
@@ -33,6 +27,12 @@
 #   include <openblas/f77blas.h>
 #   define I_LOVE_FORTRAN 1
 #endif
+
+#if HAVE_SUITESPARSE_CHOLMOD_H
+#include <suitesparse/cholmod.h>
+#else /* HAVE_SUITESPARSE_CHOLMOD_H */
+#include <cholmod.h>
+#endif /* HAVE_SUITESPARSE_CHOLMOD_H */
 
 #include "naev.h"
 /** @endcond */
