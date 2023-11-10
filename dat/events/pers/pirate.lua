c3f80ffc3322c63f802cb6eaa216e80ab61dcab7
Edgar Simo-Serra <bobbens@gmail.com>
2023-11-10 14:38:30 +0900

    Some more pirate pers.

--- dat/events/pers/pirate.lua
+++ dat/events/pers/pirate.lua
@@ -133,5 +133,119 @@ return function ()
       end
    end
 
+   local dreamerclan = pres["Dreamer Clan"] or 0
+   if dreamerclan > 100 then
+      for k,v in ipairs{
+         {
+            spawn = function ()
+               local p = pilot.add("Pirate Starbridge", "Raven Clan", nil, _("Shroomancer"), {naked=true, ai="pers_pirate"})
+               equipopt.sirius( p, {
+                  prefer={
+                     ["Large Flow Amplifier"] = 100,
+                     ["Medium Flow Resonator"] = 100,
+                  },
+                  type_range = {
+                     ["Flow Modifier"] = { max=1 },
+                  },
+                  flow_ability=outfit.get("House of Mirrors"),
+               } )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _([["Did you know that there are over 5,000 types of psychedelic mushrooms grown on Sanchez alone?"]])
+               m.taunt = _("You're ruining my vibes, soul!")
+               return p
+            end,
+         }
+      } do
+         table.insert( pers, v )
+      end
+   end
+
+   local blacklotus = pres["Black Lotus"] or 0
+   if blacklotus > 50 then
+      for k,v in ipairs{
+         {
+            spawn = function ()
+               local p = pilot.add("Pirate Shark", "Black Lotus", nil, _("Feldspar"), {naked=true, ai="pers_pirate"})
+               equipopt.sirius( p, {noflow=true} )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _([["I've been working overtime to get my rank up in the Black Lotus. I won't be the underdog forever!"]])
+               m.taunt = _("You're my ticket to a promotion. Die!")
+               return p
+            end,
+         }, {
+            spawn = function ()
+               local p = pilot.add("Pirate Starbridge", "Black Lotus", nil, _("Lapis Lazuli"), {naked=true, ai="pers_pirate"})
+               equipopt.pirate( p, {
+                  launcher = 2,
+                  ["Launcher"] = { max=4 },
+               } )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _([["With so many careless merchants flying around, business has never been better. I always like to get my hands dirty."]])
+               m.taunt = _("Let me add you to my trophy collection!")
+               return p
+            end,
+         }, {
+            spawn = function ()
+               local p = pilot.add("Pirate Starbridge", "Black Lotus", nil, _("Opal"), {naked=true, ai="pers_pirate"})
+               equipopt.dvaered( p, {} )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _([["It's hard being a rising star in the Black Lotus. They expect so much from you. Good thing I'm an overachiever."]])
+               m.taunt = _("It's my time to shine!")
+               return p
+            end,
+         }
+      } do
+         table.insert( pers, v )
+      end
+   end
+   if blacklotus > 125 then
+      for k,v in ipairs{
+         {
+            spawn = function ()
+               local p = pilot.add("Pirate Kestrel", "Black Lotus", nil, _("Emerald"), {naked=true, ai="pers_pirate"})
+               equipopt.zalek( p, {fighterbay=5} )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _([["Paperwork is such a bore. I escaped from the Empire to join the Black Lotus only to find I have to fill out more and more paperwork as I rise in rank. Maybe I should have joined the Wild Ones instead?"]])
+               m.taunt = _("Time for some fun!")
+               return p
+            end,
+         }, {
+            spawn = function ()
+               local p = pilot.add("Pirate Kestrel", "Black Lotus", nil, _("Topaz"), {naked=true, ai="pers_pirate"})
+               equipopt.soromid( p, {fighterbay=5} )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _("(ΦωΦ)")
+               m.taunt = _([["(｢・ω・)｢"]])
+               return p
+            end,
+         },
+      } do
+         table.insert( pers, v )
+      end
+   end
+   if blacklotus > 200 then
+      for k,v in ipairs{
+         {
+            spawn = function ()
+               local p = pilot.add("Pirate Zebra", "Black Lotus", nil, _("Diamond"), {naked=true, ai="pers_pirate"})
+               equipopt.pirate( p, {fighterbay=5, bolt=5} )
+               p:intrinsicSet( "shield_mod", 25 )
+               local m = p:memory()
+               m.comm_greet = _([["It's nice being near the top of the pecking order. It's fun to give contradictory orders to your subordinates and see them run around in chaos."]])
+               m.taunt = _("Time to snare another bonus!")
+               return p
+            end,
+         },
+      } do
+         table.insert( pers, v )
+      end
+   end
+
    return pers
 end
