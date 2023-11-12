7bee8a0ef831a0350242111f29fe3ffbe3f440a3
Edgar Simo-Serra <bobbens@gmail.com>
2023-11-12 11:41:17 +0900

    Update shadow_comm to vn.

--- dat/events/neutral/shadow_comm.lua
+++ dat/events/neutral/shadow_comm.lua
@@ -8,16 +8,18 @@
   <done_misn name="Shadowrun"/>
   <campaign>Shadow</campaign>
   <tier>3</tier>
+  <priority>9</priority><!-- Since it tests a claim we have to run later. -->
  </notes>
 </event>
 --]]
 --[[
    Comm Event for the Shadow missions
 --]]
-
 require "proximity"
 local fmt = require "format"
 local shadow = require "common.shadow"
+local vn = require "vn"
+local ccomm = require "common.comm"
 
 local vendetta, hailhook -- Non-persistent state.
 
@@ -51,23 +53,46 @@ function hailme()
 end
 
 -- Triggered when the player hails the ship
-function hail( _p )
+function hail( p )
+   local accepted = false
    hook.rm(hailhook)
 
    local sys = system.get("Pas")
-   tk.msg(_("An open invitation"), fmt.f(_([["Greetings, {player}," the pilot of the Vendetta says to you as soon as you answer his hail. "I have been looking for you on behalf of an acquaintance of yours. She wishes to meet with you at a place of her choosing, and a time of yours. It involves a proposition that you might find interesting - if you don't mind sticking your neck out."
-    You frown at that, but you ask the pilot where this acquaintance wishes you to go anyway.
-    "Fly to the {sys} system," he replies. "She will meet you there. There's no rush, but I suggest you go see her at the earliest opportunity."
-    The screen blinks out and the Vendetta goes about its business, paying you no more attention. It seems there's someone out there who wants to see you, and there's only one way to find out what about. Perhaps you should make a note of the place you're supposed to meet her: the {sys} system.]]), {player=player.name(), sys=sys}))
+
+   vn.clear()
+   vn.scene()
+   local plt = ccomm.newCharacter( vn, p )
+   plt(fmt.f(_([["Greetings, {player}," the pilot of the Vendetta says to you as soon as you answer his hail. "I have been looking for you on behalf of an acquaintance of yours. She wishes to meet with you at a place of her choosing, and a time of yours. It involves a proposition that you might find interesting - if you don't mind sticking your neck out."]]),
+      {player=player.name()}))
+   vn.na(_([[You frown at that, but you ask the pilot where this acquaintance wishes you to go anyway.]]))
+   plt(fmt.f(_([["Fly to the {sys} system," he replies. "She will meet you there. There's no rush, but I suggest you go see her at the earliest opportunity."]]),
+      {sys=sys}))
+   vn.na(fmt.f(_([[The screen blinks out and the Vendetta goes about its business, paying you no more attention. It seems there's someone out there who wants to see you, and there's only one way to find out what about. Perhaps you should make a note of the place you're supposed to meet her: the {sys} system.]]),
+      {sys=sys}))
+   vn.na(_([[Do you intend to respond to the invitation?]]))
+   vn.menu{
+      {_([[Yes.]]), "yes"},
+      {_([[No.]]), "no"},
+   }
+
+   vn.label("yes")
+   vn.func( function () accepted = true end )
+   vn.na(_([[You decide to follow the offer.]]))
+   vn.done()
+
+   vn.label("no")
+   vn.na(_([[You decide not to pursue the offer for now.]]))
+   vn.run()
+
    player.commClose()
    vendetta:control()
    vendetta:hyperspace()
 
-   if tk.yesno( "", _("Do you intend to respond to the invitation?") ) then
+   if accepted then
       shadow.addLog( fmt.f(_([[Someone has invited you to meet with her in the {sys} system, supposedly an acquaintance of yours. The pilot who told you this said that there's no rush, "but I suggest you go see her at the earliest opportunity".]]), {sys=sys}) )
       naev.missionStart("Shadow Vigil")
-      evt.finish()
    end
+   evt.finish()
 end
 
 -- Clean up
