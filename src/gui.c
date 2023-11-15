09c9de094956185cdfb32343e701c7230d02c63c
Edgar Simo-Serra <bobbens@gmail.com>
2023-11-15 19:49:35 +0900

    Oops, don't always draw scanning icon.

--- src/gui.c
+++ src/gui.c
@@ -1180,8 +1180,7 @@ void gui_renderPilot( const Pilot* p, RadarShape shape, double w, double h, doub
    }
    else {
       /* Draw scanning icon. */
-      //if (scanning) {
-      if (1) {
+      if (scanning) {
          glUseProgram( shaders.pilotscanning.program );
          gl_renderShader( x+scale+3., y+scale+3., 5., 5., 1.5*animation_dt, &shaders.pilotscanning, col, 1 );
       }
