3b09b53204ff400652d13080ad36b465ec76e4a8
Edgar Simo-Serra <bobbens@gmail.com>
2023-11-30 15:35:42 +0900

    Can't be too happy with the Dvaered for FLF rehab.

--- dat/missions/flf/flf_rehab.lua
+++ dat/missions/flf/flf_rehab.lua
@@ -13,7 +13,6 @@
    Rehabilitation Mission
 --]]
 require("common.rehab").init( faction.get("FLF"), {
-   ignoreenemies = true, -- Always available
    desc = _([[Although you have crossed the Frontier Liberation Front, the organization is willing to offer a chance to redeem yourself through a significant donation. If you accept, your reputation will be temporarily changed to neutral until you recover all the lost standing. Any hostile action during this time against the FLF will immediately void the agreement.]]),
    txtbroke = _([[You need to donate at least {credits} for the FLF to temporarily forgive you.]]),
    txtaccept = _([[Thank you for supporting the FLF. Remember that any collaboration with the dirty Dvaered or actions against the FLF will immediately void this agreement. During this time if you gain enough standing with the FLF, your past offenses will be erased from our records.]]),
