f05c835c72177b71f95a26ab4ca9fc544bb25f3e
Edgar Simo-Serra <bobbens@gmail.com>
2023-12-10 23:02:53 +0900

    gettextlanguagetool.py: more robust and use automatic language checking

--- utils/gettextlanguagetool.py
+++ utils/gettextlanguagetool.py
@@ -6,8 +6,14 @@ import subprocess
 import tempfile
 import language_tool_python
 
-# Below is a chunk from an example vimrc of getting this to work with vim
 """
+This scripts extracts all the gettext translatable fields from a file and then
+runs languagetool on each of the outputs. The output follows the same format as
+languagetool while keeping the line numbers and offsets relative to the
+original input file.
+
+Below is a chunk from an example vimrc of getting this to work with vim
+```
 " Set up ALE plugin used plugged
 call plug#begin('~/.vim/plugged')
  Plug 'dense-analysis/ale'
@@ -22,6 +28,7 @@ let g:ale_languagetool_executable = "/PATH/TO/naev/utils/gettextlint.py"
 let g:ale_linters = {
 \  'lua':['languagetool', 'cspell', 'lua_language_server', 'luac', 'luacheck', 'selene'],
 \}
+```
 """
 
 assert(len(sys.argv)>1)
@@ -37,8 +44,8 @@ with open( filename, 'r' ) as f:
 lines = data.splitlines()
 
 tf = tempfile.NamedTemporaryFile( suffix='.po' )
-args = [ "xgettext", "-", '--language=lua', '-d' , tf.name[:-3] ]
-ret = subprocess.run( args, input=bytes(data,'utf-8') )
+args = [ "xgettext", filename, '--from-code=utf-8', '-d' , tf.name[:-3] ]
+ret = subprocess.run( args )
 
 # If we use 'en-GB' it gets a ton of false positives, not sure how to solve it...
 tool = language_tool_python.LanguageTool('en-GB')
@@ -46,15 +53,16 @@ tool = language_tool_python.LanguageTool('en-GB')
 po = polib.pofile( tf.name )
 n = 1
 for entry in po:
-    clist = tool.check( entry.msgid )
-    for c in clist:
-        line = int(entry.occurrences[1][1])
-        f = lines[line-1].find(entry.msgid)
+    for ti,txt in enumerate(entry.msgid.splitlines()):
+        clist = tool.check(txt)
+        line = int(entry.occurrences[0][1])+ti
+        f = lines[line-1].find(txt)
         assert( f>= 0)
-        col = 1+f+c.offset
-        print(f"{n}.) Line {line}, column {col}, Rule ID: {c.ruleId}")
-        print(f"Message: {c.message}")
-        print(f"{c.context}")
-        print(f"{' '*c.offsetInContext}{'^'*c.errorLength}")
-        print()
-        n = n+1
+        for c in clist:
+            col = 1+f+c.offset
+            print(f"{n}.) Line {line}, column {col}, Rule ID: {c.ruleId}")
+            print(f"Message: {c.message}")
+            print(f"{c.context}")
+            print(f"{' '*c.offsetInContext}{'^'*c.errorLength}")
+            print()
+            n = n+1
