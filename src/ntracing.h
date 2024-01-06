73e29552c7333adc164f05db7fa0aed3f5479e4f
Edgar Simo-Serra <bobbens@gmail.com>
2024-01-06 22:24:19 +0900

    Trying to replace all the memory calls was a bit too much, let's keep it to stuff we want to trace.

--- src/ntracing.h
+++ src/ntracing.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #if HAVE_TRACY
+#include "attributes.h"
 #include "tracy/TracyC.h"
 #define _uninitialized_var(x) x = *(&(x))
 #  define NTracingFrameMark            TracyCFrameMark
@@ -13,6 +14,30 @@
 #  define NTracingZoneEnd( ctx )       TracyCZoneEnd( ctx )
 #  define NTracingAlloc( ptr, size )   do { _uninitialized_var(ptr); TracyCAlloc( ptr, size ) } while (0)
 #  define NTracingFree( ptr )          do { if (ptr!=NULL) { TracyCFree( ptr ); }; } while (0)
+ALWAYS_INLINE static inline void *nmalloc(size_t size)
+{
+   void *ptr = malloc(size);
+   NTracingAlloc( ptr, size );
+   return ptr;
+}
+ALWAYS_INLINE static inline void nfree(void *ptr)
+{
+   NTracingFree( ptr );
+   free( ptr );
+}
+ALWAYS_INLINE static inline void *ncalloc(size_t nmemb, size_t size)
+{
+   void *ptr = calloc( nmemb, size );
+   NTracingAlloc( ptr, nmemb*size );
+   return ptr;
+}
+ALWAYS_INLINE static inline void *nrealloc(void *ptr, size_t size)
+{
+   NTracingFree( ptr );
+   void *newptr = realloc( ptr, size );
+   NTracingAlloc( newptr, size );
+   return newptr;
+}
 #else
 #  define NTracingFrameMark
 #  define NTracingFrameMarkStart( name )
@@ -21,4 +46,8 @@
 #  define NTracingZoneEnd( ctx )
 #  define NTracingAlloc( ptr, size )
 #  define NTracingFree( ptr )
+#  define nmalloc(size)      malloc(size)
+#  define ncalloc(nmemb,size) calloc(nmemb,size)
+#  define nfree(ptr)         free(ptr)
+#  define nrealloc(ptr,size) realloc(ptr,size)
 #endif /* HAVE_TRACY */
