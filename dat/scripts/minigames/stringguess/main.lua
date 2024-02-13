9125f615c3ba8fe75a0215ff8f67c9cdf9178ad8
Edgar Simo-Serra <bobbens@gmail.com>
2024-02-13 11:17:57 +0900

    Stringguess minigame is now playable with the mouse. Closes #2526

--- dat/scripts/minigames/stringguess/main.lua
+++ dat/scripts/minigames/stringguess/main.lua
@@ -2,5 +2,7 @@ local mg = require "core"
 
 love.load         = mg.load
 love.keypressed   = mg.keypressed
+love.mousemoved   = mg.mousemoved
+love.mousepressed = mg.mousepressed
 love.draw         = mg.draw
 love.update       = mg.update
