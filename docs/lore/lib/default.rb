ee922a285c57c805e148917373b5f33d3f2f163b
Edgar Simo-Serra <bobbens@gmail.com>
2024-04-01 22:02:46 +0900

    Simplified rules and tucked stuff away into functions.

--- docs/lore/lib/default.rb
+++ docs/lore/lib/default.rb
@@ -7,140 +7,138 @@ include Nanoc::Helpers::XMLSitemap
 include Nanoc::Helpers::ChildParent
 include Nanoc::Helpers::Blogging
 
-require 'nanoc/filters/javascript_concatenator'
-require 'image_size'
+def card_spob( s )
+  cls = ""
+  cls += " fct-"+s[:faction]
+  cls += " cls-"+s[:spobclass]
+  s[:services].each do |s|
+      cls += " srv-"+s.to_s
+  end
+  s[:tags].each do |t|
+      cls += " tag-"+t
+  end
 
-def image_with_size(item, alt='', class_code='', html_code='')
-   begin
-      if item.class == Nanoc::CompilationItemView and not item.attributes[:content_filename].nil?
-         img = ImageSize.new(IO.read(item.attributes[:content_filename]))
-         "<img src='#{item.path}' height='#{img.height}' width='#{img.width}' alt=\"#{alt}\" class='img-fluid #{class_code}' #{html_code}/>"
-      else
-         img = ImageSize.new(IO.read("output/#{item.path}"))
-         "<img src='#{item.path}' height='#{img.height}' width='#{img.width}' alt=\"#{alt}\" class='img-fluid #{class_code}' #{html_code}/>"
-      end
-   rescue
-      "<img src='#{item.path}' alt=\"#{alt}\" class='img-fluid #{class_code}' #{html_code}/>"
-   end
-end
-
-class Integer
-  def ordinalize
-    if (11..13).include?(self % 100)
-      "#{self}th"
-    else
-      case self % 10
-        when 1; "#{self}st"
-        when 2; "#{self}nd"
-        when 3; "#{self}rd"
-        else    "#{self}th"
-      end
+  services = ""
+  if s[:services].length() > 0
+    services += "<div>"
+    s[:services].each do |srv|
+      services += "<span class='badge rounded-pill text-bg-secondary'>#{srv}</span>"
     end
+    services += "</div>"
   end
-end
+  tags = ""
+  if s[:tags].length() > 0
+    tags = "<div>"
+    s[:tags].each do |t|
+        tags += "<span class='badge rounded-pill text-bg-info'>#{t}</span>"
+    end
+    tags += "</div>"
+  end
 
-def video( item, caption, class_code='', aspectratio='16by9' )
-  return <<-EOS
-<figure class="figure embed-responsive #{class_code}">
- <div class="embed-responsive embed-responsive-#{aspectratio} figure-img">
-  <video controls class="embed-responsive-item">
-   <source src="#{item.path}" type='video/webm;codecs="vp9, opus"'>
-  </video>
- </div>
- <figcaption class="figure-caption">#{caption}</figcaption>
-</figure>
-EOS
-end
-
-def pdf( item, width="100%", height="900" )
-  return <<-EOS
-<object width="#{width}" height="#{height}" type="application/pdf" data="#{item}?#zoom=85&scrollbar=0&toolbar=0&navpanes=0">
-<a href="#{item}">Could not load PDF, Click here to download.</a>
-</object>
-EOS
-end
-
-def youtube( video_id, caption, class_code='', aspectratio='16by9' )
-  return <<-EOS
-<figure class="figure embed-responsive #{class_code}">
- <div class="embed-responsive embed-responsive-#{aspectratio} figure-img">
-  <embed src="https://www.youtube.com/embed/#{video_id}">
- </div>
- <figcaption class="figure-caption">#{caption}</figcaption>
-</figure>
-EOS
-end
-
-def image( item, caption, alignment='center', extra_html='' )
-  return <<-EOS
-<figure class="figure embed-responsive" style="text-align:#{alignment};">
-  <div class="embed-responsive figure-img">
-    <img class='img-fluid' alt='#{caption}' src="#{item.path}" #{extra_html} />
+  if not s[:spob][:GFX].nil? and not s[:spob][:GFX][:space].nil?
+      gfxpath = relative_path_to(@items["/gfx/spob/space/"+s[:spob][:GFX][:space]])
+  end
+  gfx = ""
+  if not gfxpath.nil?
+    gfx += "<img src='#{gfxpath}' class='card-img-top' alt='#{s[:spob][:GFX][:space]}'>"
+  end
+<<-EOF
+ <div class="col #{cls}" data-Name="#{s[:name]}" data-Faction="#{s[:faction]}" data-Class="#{s[:spobclass]}" data-Population="#{s[:population]}" >
+  <div class="card bg-black" data-bs-toggle="modal" data-bs-target="#modal-#{s[:id]}" >
+   #{gfx}
+   <div class="card-body">
+    <h5 class="card-title">#{s[:name]}</h5>
+    <div class="card-text">
+     <div>
+      <span class="badge rounded-pill text-bg-primary">#{s[:faction]}</span>
+      <span class="badge rounded-pill text-bg-primary">Class #{s[:spobclass]}</span>
+     </div>
+     #{services}
+     #{tags}
+    </div>
+   </div>
   </div>
-  <figcaption class="figure-caption">#{caption}</figcaption>
-</figure>
-EOS
+ </div>
+EOF
 end
 
-# Embeds a badge
-# Populate this case with additional badges as the need arises.
-# Badges should be stored in imgs/badges/<badge_name>.svg
-
-def badge( type, version='latest' )
-
-  if version == 'latest'
-    version=version
-  else
-    version="v#{version}"
+def modal_spob( s )
+  header = s[:name]
+  if not s[:ssys].nil?
+    header += " (#{s[:ssys][:name]} system)"
   end
 
-  case type
-    when 'steam';
-      link = "https://store.steampowered.com/app/598530/Naev"
-      badge_image = @items['/imgs/badges/steam.svg'].path
-      alt = "Get - Steam"
-    when 'flathub';
-      link = "https://flathub.org/apps/details/org.naev.Naev"
-      badge_image = @items['/imgs/badges/flathub.svg'].path
-      alt = "Get - Flathub"
-    when 'itchio';
-      link = "https://naev.itch.io/naev"
-      badge_image = @items['/imgs/badges/itchio.svg'].path
-      alt = "Get - Itch.io"
-    when 'repology';
-      link = "https://repology.org/project/naev/versions"
-      badge_image = "https://repology.org/badge/latest-versions/naev.svg"
-      alt = "latest packaged version(s)"
-    when 'github';
-      link = "https://github.com/naev/naev/releases/#{version}"
-      badge_image = @items['/imgs/badges/github.svg'].path
-      alt = "Get - Github"
-    when 'matrix';
-      link = "https://matrix.to/#/#naev-community:matrix.org"
-      badge_image = @items['/imgs/badges/matrix.svg'].path
-      alt = "Join - Matrix"
-    when 'discord';
-      link = "https://discord.com/invite/nd2M5BR"
-      badge_image = @items['/imgs/badges/discord.svg'].path
-      alt = "Join - Discord"
-    when 'steamdisc';
-      link = "https://steamcommunity.com/app/598530/discussions"
-      badge_image = @items['/imgs/badges/steamdisc.svg'].path
-      alt = "Discussions - Steam"
-    when 'githubdisc';
-      link = "https://github.com/naev/naev/discussions"
-      badge_image = @items['/imgs/badges/githubdisc.svg'].path
-      alt = "Discussions - Github"
-    when 'issues';
-      link = "https://github.com/naev/naev/issues"
-      badge_image = @items['/imgs/badges/issues.svg'].path
-      alt = "Issue Tracker - Github"
-    else
-      link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-      badge_image = @items['/imgs/badges/error.svg'].path
-      alt = "Error - Bad"
+  gfx = ""
+  if not s[:spob][:GFX].nil? and not s[:spob][:GFX][:exterior].nil?
+      gfxpath = relative_path_to(@items["/gfx/spob/exterior/"+s[:spob][:GFX][:exterior]])
+      gfx += "<img src='#{gfxpath}' class='rounded col-md-6 float-md-end mb-3 ms-md-3' alt='#{s[:spob][:GFX][:space]}'>"
+  elsif not s[:spob][:GFX].nil? and not s[:spob][:GFX][:space].nil?
+      gfxpath = relative_path_to(@items["/gfx/spob/space/"+s[:spob][:GFX][:space]])
+      gfx += "<img src='#{gfxpath}' class='col-md-6 float-md-end mb-3 ms-md-3' alt='#{s[:spob][:GFX][:space]}'>"
   end
-  return <<-EOS
-[![#{alt}](#{badge_image})](#{link})
-EOS
+
+  services = ""
+  if s[:services].length() > 0
+    services += "<div class='m-1'>"
+    s[:services].each do |srv|
+      services += "<span class='badge rounded-pill text-bg-secondary'>#{srv}</span>"
+    end
+    services += "</div>"
+  end
+
+  tags = ""
+  if s[:tags].length() > 0
+    tags = "<div class='m-1'>"
+    s[:tags].each do |t|
+        tags += "<span class='badge rounded-pill text-bg-info'>#{t}</span>"
+    end
+    tags += "</div>"
+  end
+
+  description = ""
+  if not s[:description].nil?
+    description += <<-EOF
+    <div>
+     <h5>Description:</h5>
+     <p>#{s[:description]}</p>
+    </div>
+    EOF
+  end
+
+  bar = ""
+  if not s[:bar].nil?
+     bar += <<-EOF
+     <div>
+      <h5>Spaceport Bar:</h5>
+      <p>#{s[:bar]}</p>
+     </div>
+    EOF
+  end
+
+<<-EOF
+ <div class="modal fade" id="modal-#{s[:id]}" tabindex="-1" aria-labelledby="modal-label-#{s[:id]}" aria-hidden="true">
+  <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+   <div class="modal-content">
+    <div class="modal-header">
+     <h1 class="modal-title fs-5" id="modal-label-#{s[:id]}">#{header}</h1>
+     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body clearfix">
+     #{gfx}
+     <div class='m-1'>
+      <span class="badge rounded-pill text-bg-primary">#{s[:faction]}</span>
+      <span class="badge rounded-pill text-bg-primary">Class #{s[:spobclass]}</span>
+     </div>
+     #{services}
+     #{tags}
+     #{description}
+     #{bar}
+    </div>
+    <div class="modal-footer">
+     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
+   </div>
+  </div>
+ </div>
+EOF
 end
