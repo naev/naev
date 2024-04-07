8c7e8862b0145a079cab91459bca66e1c5e8a397
Edgar Simo-Serra <bobbens@gmail.com>
2024-04-07 21:08:52 +0900

    lore: added factions and such

--- docs/lore/lib/default.rb
+++ docs/lore/lib/default.rb
@@ -41,6 +41,73 @@ def listmisnevt( header, lst )
   return out
 end
 
+def fcts_card( f )
+  tags = ""
+  if f[:tags].length() > 0
+    tags = "<div class='m-1'>"
+    f[:tags].each do |t|
+        tags += "<span class='badge rounded-pill text-bg-info'>#{t}</span>"
+    end
+    tags += "</div>"
+  end
+
+  gfx = ""
+  if not f[:logo].nil?
+      gfxpath = relative_path_to(@items[f[:logo]])
+      gfx = "<img src='#{gfxpath}' class='card-img-top' alt=\"#{f[:longname]} Logo\">"
+  end
+<<-EOF
+ <div class="col" data-Name="#{f[:name]}">
+  <div class="card bg-black" data-bs-toggle="modal" data-bs-target="#modal-fcts-#{f[:id]}" >
+   #{gfx}
+   <div class="card-body">
+    <h5 class="card-title">#{f[:name]}</h5>
+    <div class="card-text">
+     #{tags}
+    </div>
+   </div>
+  </div>
+ </div>
+EOF
+end
+
+def fcts_modal( f )
+  gfx = ""
+  if not f[:logo].nil?
+      gfxpath = relative_path_to(f[:logo])
+      gfx = "<img src='#{gfxpath}' class='col-md-6 float-md-end mb-3 ms-md-3' alt=\"#{f[:longname]} Logo\">"
+  end
+
+  tags = ""
+  if f[:tags].length() > 0
+    tags = "<div class='m-1'>"
+    f[:tags].each do |t|
+        tags += "<span class='badge rounded-pill text-bg-info'>#{t}</span>"
+    end
+    tags += "</div>"
+  end
+
+<<-EOF
+ <div class="modal fade fcts" id="modal-fcts-#{f[:id]}" tabindex="-1" aria-labelledby="modal-fcts-label-#{f[:id]}" data-fcts-modal="#{f[:name]}" aria-hidden="true">
+  <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+   <div class="modal-content">
+    <div class="modal-header">
+     <h1 class="modal-title fs-5" id="modal-fcts-label-#{f[:id]}">#{f[:longname]}</h1>
+     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body clearfix">
+     #{gfx}
+     <p>#{f[:description]}</p>
+    </div>
+    <div class="modal-footer">
+     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
+   </div>
+  </div>
+ </div>
+EOF
+end
+
 def spob_card( s )
   cls = ""
   cls += " fct-"+s[:faction]
@@ -286,11 +353,14 @@ end
 
 def modal_addAll
   out = ""
-  @items.find_all('/spob/*.md').sort{ |a,b| a[:name]<=>b[:name] }.each do |s|
+  @items.find_all('/spob/*.md').each do |s|
     out += spob_modal( s )
   end
-  @items.find_all('/ssys/*.md').sort{ |a,b| a[:name]<=>b[:name] }.each do |s|
+  @items.find_all('/ssys/*.md').each do |s|
     out += ssys_modal( s )
+  end
+  @items.find_all('/fcts/*.md').each do |f|
+    out += fcts_modal( f )
   end
   out
 end
