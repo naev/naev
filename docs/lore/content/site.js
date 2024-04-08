610b79705fc483bbbbf27f578263c650e04a13f4
Edgar Simo-Serra <bobbens@gmail.com>
2024-04-08 13:59:16 +0900

    lore: support for spoilers

--- docs/lore/content/site.js
+++ docs/lore/content/site.js
@@ -3,6 +3,30 @@
 //= require js/_debounce.js
 //= require js/_fluidbox.js
 
+function spoilers () {
+   return localStorage.getItem('spoilers') === "true";
+}
+
+function spoilers_update () {
+   if (spoilers()) {
+      $('.spoiler').show();
+      $('.nospoiler').hide();
+   }
+   else {
+      $('.spoiler').hide();
+      $('.nospoiler').show();
+   }
+}
+
 $(document).ready( function() {
    $(".use-fluidbox").fluidbox();
+
+   $('input#spoilers').attr('checked', spoilers());
+   spoilers_update();
+
+   $('input#spoilers').change( function () {
+      var checked = $(this).is(':checked');
+      localStorage.setItem('spoilers', checked);
+      spoilers_update();
+   } );
 });
