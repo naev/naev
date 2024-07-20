35787a9964b0a7334a040c171541cc19bf091e4b
Edgar Simo-Serra <bobbens@gmail.com>
2024-07-21 01:22:24 +0900

    Simplify canvas creation lua code.

--- src/nlua_canvas.c
+++ src/nlua_canvas.c
@@ -160,8 +160,7 @@ static int canvasL_eq( lua_State *L )
  */
 int canvas_new( LuaCanvas_t *lc, int w, int h )
 {
-   GLenum status;
-   char  *name;
+   char *name;
 
    memset( lc, 0, sizeof( LuaCanvas_t ) );
 
@@ -175,25 +174,8 @@ int canvas_new( LuaCanvas_t *lc, int w, int h )
    free( name );
 
    /* Create the frame buffer. */
-   glGenFramebuffers( 1, &lc->fbo );
-   glBindFramebuffer( GL_FRAMEBUFFER, lc->fbo );
+   gl_fboCreate( &lc->fbo, &lc->tex->texture, w, h );
 
-   /* Attach the colour buffer. */
-   glFramebufferTexture2D( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-                           lc->tex->texture, 0 );
-
-   /* Check status. */
-   status = glCheckFramebufferStatus( GL_FRAMEBUFFER );
-   if ( status != GL_FRAMEBUFFER_COMPLETE )
-      return -1;
-
-   /* Clear the canvas. */
-   glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
-   /* Restore state. */
-   glBindFramebuffer( GL_FRAMEBUFFER, gl_screen.current_fbo );
-
-   gl_checkErr();
    return 0;
 }
 
