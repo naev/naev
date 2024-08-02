9baef2a1a94a29b5038db7cbaeb115988dd0bed4
Edgar Simo-Serra <bobbens@gmail.com>
2024-08-02 20:49:50 +0900

    Barebones naevpedia ship thing.

--- dat/naevpedia/ships/ships.py
+++ dat/naevpedia/ships/ships.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python
+
+import xml.etree.ElementTree as ET
+import argparse
+
+parser = argparse.ArgumentParser( description='Script to generate naevpedia markdown from ship XML files.')
+parser.add_argument('path', metavar='PATH', type=str, help='Name of the ship XML file.')
+parser.add_argument('-o', type=str, help='Output path.' )
+
+args, unknown = parser.parse_known_args()
+
+tree = ET.parse( args.path )
+root = tree.getroot()
+name = root.get('name')
+
+outstr = f"""---
+title: "{name}"
+cond: "return ship.get(\\\"{name}\\\"):known()"
+---
+## {name}
+"""
+
+with open( args.o, 'w' ) as f:
+    f.write( outstr )
