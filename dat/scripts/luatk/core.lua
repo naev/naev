8a36029780519afd33b0bbfe30cda950dcc520e2
Edgar Simo-Serra <bobbens@gmail.com>
2024-08-11 14:45:39 +0900

    luatk: fixed release offset being off

--- dat/scripts/luatk/core.lua
+++ dat/scripts/luatk/core.lua
@@ -270,7 +270,7 @@ function luatk.mousereleased( mx, my, button )
       end
       wgt._pressed = false
       if wgt.released then
-         wgt:released( mx, my, button )
+         wgt:released( mx-wgt.x, my-wgt.y, button )
          luatk._dirty = true
       end
       if inbounds then
