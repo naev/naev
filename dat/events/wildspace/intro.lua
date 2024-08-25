fc17ee2fbb7a91ad6466a20cf617e9aaf708cc33
Edgar Simo-Serra <bobbens@gmail.com>
2024-08-25 13:38:58 +0900

    More work on wild space intro.

--- dat/events/wildspace/intro.lua
+++ dat/events/wildspace/intro.lua
@@ -9,6 +9,7 @@
 --]]
 local vn = require "vn"
 local vni = require "vnimage"
+local der = require "common.derelict"
 
 local mainspb, mainsys = spob.getS("Hypergate Protera")
 
@@ -16,32 +17,37 @@ local derelict
 function create ()
    evt.finish(false)
 
+   -- Create the random derelict
    derelict = pilot.add( "Koala", "Derelict", vec2.newP( system.cur():radius()*0.3, rnd.angle() ), p_("ship", "Derelict"), {ai="dummy", naked=true})
    derelict:disable()
    derelict:intrinsicSet( "ew_hide", 300 ) -- Much more visible
    derelict:intrinsicSet( "nebu_absorb", 100 ) -- Immune to nebula
 
+   -- Don't let the player land untiil the hail, we could do a diff, but it's more complex and likely not worth it
+   player.landAllow( false, _("You do not see any place to land here.") )
+
+   -- Set up our hooks
    hook.timer( 25, "msg" )
    hook.enter( "enter" )
    hook.land( "land" )
 end
 
 function msg ()
+   -- Shouldn't be dead, but just in case
    if not derelict:exists() then
       return
    end
    derelict:setVisplayer()
    derelict:hailPlayer()
    hook.pilot(derelict, "hail", "hail")
-
-   player.landAllow( false, _("You do not see any place to land here.") )
 end
 
 function hail ()
    vn.clear()
    vn.scene()
-
    local c = vn.newCharacter( vni.soundonly( _("character","C") ) )
+   vn.transition()
+
    vn.na(_([[You respond to the mysterious hail from a derelict, and strangely enough are able to open a sound-only channel.]]))
    c(_([["You aren't one of them, are you?"]]))
    vn.menu{
@@ -58,6 +64,7 @@ function hail ()
 
    vn.run()
 
+   -- Not a mission so use a system marker
    system.markerAdd( mainspb:pos(), _("Land here") )
    player.landAllow()
 end
@@ -65,12 +72,19 @@ end
 function land ()
    vn.clear()
    vn.scene()
+   vn.transition()
 
+   vn.sfx( der.sfx.board )
+   vn.na(_([[You slowly approach the location you were given with your ship avoiding the thick structural debris. After a long time of searching, you eventually find a crusty old docking port among the sprawling wreck that seemingly is in working condition. Weapon drawn and in full EVA gear you prepare to go into the rubbish.]]))
+   vn.na(_([[You work your way through the crushed structure]]))
+
+   vn.scene()
    local c = vn.newCharacter( vni.soundonly( _("character","C") ) )
-   vn.na(_([[]]))
    c(_([[]]))
 
    vn.run()
+
+   faction.get("Lost"):setKnown(true)
 end
 
 function enter ()
