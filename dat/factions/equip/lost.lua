b1dd1562a4beb4c186e9f8d09bfe3ae0dbb7e6a4
Edgar Simo-Serra <bobbens@gmail.com>
2024-09-21 01:32:54 +0900

    Lost ships have wild space taint.

--- dat/factions/equip/lost.lua
+++ dat/factions/equip/lost.lua
@@ -1,4 +1,7 @@
 local equipopt = require 'equipopt'
+
+local intrinsic_taint = outfit.get("Wild Space Taint")
+
 --[[
 -- @brief Does Lost pilot equipping
 --
@@ -8,5 +11,6 @@ function equip( p )
    local params = {
       disable = 0, -- THEY WANT BLOOD
    }
+   p:outfitAddIntrinsic( intrinsic_taint ) -- So the player can capture it
    return equipopt.pirate( p, params )
 end
