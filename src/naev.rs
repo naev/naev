d9dbff008049652057976574d4f2b5ee8d0da9a1
Edgar Simo-Serra <bobbens@gmail.com>
2024-10-05 21:39:25 +0900

    rustfmt pre-commit hook.

--- src/naev.rs
+++ src/naev.rs
@@ -1,8 +1,8 @@
-use std::os::raw::{c_int, c_char};
+use std::os::raw::{c_char, c_int};
 
 #[link(name = "naev")]
 extern "C" {
-    pub fn naev_main( argc: c_int, argv: *mut *mut c_char ) -> c_int;
+    pub fn naev_main(argc: c_int, argv: *mut *mut c_char) -> c_int;
 }
 
 use std::env;
@@ -13,19 +13,16 @@ pub fn naev() {
     let args: Vec<String> = env::args().collect();
     let mut cargs = vec![];
     for a in args {
-        cargs.push( CString::new(a).unwrap() )
+        cargs.push(CString::new(a).unwrap())
     }
-    let mut argv = cargs
-        .into_iter()
-        .map(|s| s.into_raw() )
-        .collect::<Vec<_>>();
+    let mut argv = cargs.into_iter().map(|s| s.into_raw()).collect::<Vec<_>>();
     argv.shrink_to_fit();
 
     println!("Starting Rust!");
-    println!("SDL_log(16) is {}", unsafe{naevc::SDL_log(16.0)} );
+    println!("SDL_log(16) is {}", unsafe { naevc::SDL_log(16.0) });
     println!("Rust done, starting C main loop!");
 
-    unsafe{
-        naev_main( argv.len() as c_int, argv.as_mut_ptr() );
+    unsafe {
+        naev_main(argv.len() as c_int, argv.as_mut_ptr());
     }
 }
