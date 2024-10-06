3c3464f7697d6deee7e24b86855df06d8ca684f2
Edgar Simo-Serra <bobbens@gmail.com>
2024-10-06 20:20:34 +0900

    Binary search for slot properties.

--- src/slots.rs
+++ src/slots.rs
@@ -1,4 +1,5 @@
 use std::ffi::{CStr, CString};
+use std::io::Result;
 use std::os::raw::{c_char, c_int};
 
 use crate::array;
@@ -15,6 +16,36 @@ pub struct SlotProperty {
     pub visible: bool,
     pub icon: *mut naevc::glTexture,
 }
+impl Default for SlotProperty {
+    fn default() -> Self {
+        SlotProperty {
+            name: CString::default(),
+            display: CString::default(),
+            description: CString::default(),
+            required: false,
+            exclusive: false,
+            locked: false,
+            visible: false,
+            icon: std::ptr::null_mut(),
+        }
+    }
+}
+impl Ord for SlotProperty {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (&self.name).cmp(&(&other.name))
+    }
+}
+impl PartialOrd for SlotProperty {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl PartialEq for SlotProperty {
+    fn eq(&self, other: &Self) -> bool {
+        &self.name == &other.name
+    }
+}
+impl Eq for SlotProperty {}
 
 static mut SLOT_PROPERTIES: Vec<SlotProperty> = Vec::new();
 
@@ -34,14 +65,15 @@ pub extern "C" fn sp_get(name: *const c_char) -> c_int {
     unsafe {
         let ptr = CStr::from_ptr(name);
         let sname = CString::new(ptr.to_str().unwrap()).unwrap();
-        for (i, sp) in SLOT_PROPERTIES.iter().enumerate() {
-            if sp.name == sname {
-                return (i + 1) as c_int;
-            }
+        let query = SlotProperty {
+            name: sname,
+            ..SlotProperty::default()
+        };
+        match SLOT_PROPERTIES.binary_search(&query) {
+            Ok(i) => (i + 1) as c_int,
+            Err(_) => 0,
         }
     }
-    // WARN( _( "Slot property '%s' not found in array." ), name );
-    0
 }
 
 #[no_mangle]
@@ -101,7 +133,7 @@ pub extern "C" fn sp_locked(sp: c_int) -> c_int {
 }
 
 // Assume static here, because it doesn't really change after loading
-pub fn get_c(sp: c_int) -> std::io::Result<&'static SlotProperty> {
+pub fn get_c(sp: c_int) -> Result<&'static SlotProperty> {
     unsafe {
         match SLOT_PROPERTIES.get((sp - 1) as usize) {
             Some(sp) => Ok(sp),
@@ -110,7 +142,22 @@ pub fn get_c(sp: c_int) -> std::io::Result<&'static SlotProperty> {
     }
 }
 
-pub fn load() -> std::io::Result<()> {
+#[allow(dead_code)]
+pub fn get(name: CString) -> Result<&'static SlotProperty> {
+    unsafe {
+        let query = SlotProperty {
+            name: name,
+            ..SlotProperty::default()
+        };
+        match SLOT_PROPERTIES.binary_search(&query) {
+            Ok(i) => Ok(&SLOT_PROPERTIES[i]),
+            Err(_) => Err(std::io::Error::new(std::io::ErrorKind::Other, "")),
+        }
+    }
+    // WARN( _( "Slot property '%s' not found in array." ), name );
+}
+
+pub fn load() -> Result<()> {
     let sp_files = unsafe { naevc::ndata_listRecursive(naevc::SP_DATA_PATH.as_ptr().cast()) };
     let sp_array = array::to_vec(sp_files)?;
     let mut files: Vec<String> = Vec::new();
@@ -124,14 +171,8 @@ pub fn load() -> std::io::Result<()> {
         let root = doc.root_element();
         let name = CString::new(root.attribute("name").unwrap())?;
         let mut sp = SlotProperty {
-            name,
-            display: CString::new("")?,
-            description: CString::new("")?,
-            required: false,
-            exclusive: false,
-            locked: false,
-            visible: false,
-            icon: std::ptr::null_mut(),
+            name: name,
+            ..SlotProperty::default()
         };
         for node in root.children() {
             if !node.is_element() {
@@ -154,5 +195,6 @@ pub fn load() -> std::io::Result<()> {
         println!("{:?}", sp);
         unsafe { SLOT_PROPERTIES.push(sp) };
     }
+    unsafe { SLOT_PROPERTIES.sort() };
     Ok(())
 }
