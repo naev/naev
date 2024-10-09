9bfc17944b4664249037fdda5974043c6b9979fb
Edgar Simo-Serra <bobbens@gmail.com>
2024-10-10 00:27:30 +0900

    Tried something else for gettext.rs, seems to work but is a tad inefficient.

--- src/gettext.rs
+++ src/gettext.rs
@@ -1,16 +1,28 @@
 use std::ffi::{CStr, CString};
-use std::os::raw::c_char;
+use std::os::raw::c_ulong;
 
 #[allow(dead_code)]
-pub fn gettext<'a>(msg_id: &'a str) -> &'static str {
-    let cmsg_id = CString::new(msg_id).unwrap();
+/*
+pub fn gettext<'a>(msgid: &'a str) -> &'static str {
+    let msgid = CString::new(msgid).unwrap();
     unsafe {
-        let t = naevc::gettext_rust(cmsg_id.as_ptr() as *const c_char);
+        let t = naevc::gettext_rust(msgid.as_ptr() as *const c_char);
         CStr::from_ptr(t).to_str().unwrap()
+    }
+}
+*/
+pub fn gettext<T: Into<String>>(msgid: T) -> String {
+    let msgid = CString::new(msgid.into()).expect("`msgid` contains an internal 0 byte");
+    unsafe {
+        CStr::from_ptr(naevc::gettext_rust(msgid.as_ptr()))
+            .to_str()
+            .expect("gettext() returned invalid UTF-8")
+            .to_owned()
     }
 }
 
 #[allow(dead_code)]
+/*
 pub fn ngettext<'a>(msg_id: &'a str, msg_id_plural: &'a str, n: u64) -> &'static str {
     let cmsg_id = CString::new(msg_id).unwrap();
     let cmsg_id_plural = CString::new(msg_id_plural).unwrap();
@@ -23,8 +35,29 @@ pub fn ngettext<'a>(msg_id: &'a str, msg_id_plural: &'a str, n: u64) -> &'static
         CStr::from_ptr(t).to_str().unwrap()
     }
 }
+*/
+pub fn ngettext<T, S>(msgid: T, msgid_plural: S, n: u32) -> String
+where
+    T: Into<String>,
+    S: Into<String>,
+{
+    let msgid = CString::new(msgid.into()).expect("`msgid` contains an internal 0 byte");
+    let msgid_plural =
+        CString::new(msgid_plural.into()).expect("`msgid_plural` contains an internal 0 byte");
+    unsafe {
+        CStr::from_ptr(naevc::gettext_ngettext(
+            msgid.as_ptr(),
+            msgid_plural.as_ptr(),
+            n as c_ulong,
+        ))
+        .to_str()
+        .expect("ngettext() returned invalid UTF-8")
+        .to_owned()
+    }
+}
 
 #[allow(dead_code)]
+/*
 pub fn pgettext<'a>(msg_context: &'a str, msg_id: &'a str) -> &'static str {
     let cmsg_context = CString::new(msg_context).unwrap();
     let cmsg_id = CString::new(msg_id).unwrap();
@@ -34,5 +67,20 @@ pub fn pgettext<'a>(msg_context: &'a str, msg_id: &'a str) -> &'static str {
             cmsg_id.as_ptr() as *const c_char,
         );
         CStr::from_ptr(t).to_str().unwrap()
+    }
+}
+*/
+pub fn pgettext<T, U>(msgctxt: T, msgid: U) -> String
+where
+    T: Into<String>,
+    U: Into<String>,
+{
+    let msgctxt = CString::new(msgctxt.into()).expect("`msgctxt` contains an internal 0 byte");
+    let msgid = CString::new(msgid.into()).expect("`msgid` contains an internal 0 byte");
+    unsafe {
+        CStr::from_ptr(naevc::pgettext_var(msgctxt.as_ptr(), msgid.as_ptr()))
+            .to_str()
+            .expect("ngettext() returned invalid UTF-8")
+            .to_owned()
     }
 }
