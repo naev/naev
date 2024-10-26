320977b026ff06780d091e30c3c8f6727dce9418
Edgar Simo-Serra <bobbens@gmail.com>
2024-10-26 23:40:05 +0900

    No secondary hits for proteron, for now.

--- dat/scripts/factions/proteron.lua
+++ dat/scripts/factions/proteron.lua
@@ -2,5 +2,6 @@ return {
    fct            = faction.get("Proteron"),
    rep_max        = 70, --30, -- TODO lower as missions get added
    rep_max_var    = "_fcap_proteron",
+   secondary_default = 0, -- Unaffected by secondary hits, oblivious to the universe (for now...)
    rep_max_tags   = {},
 }
