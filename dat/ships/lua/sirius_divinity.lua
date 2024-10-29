e8265c9215eab4fbc0e2f1184421f3663b00ec7a
Edgar Simo-Serra <bobbens@gmail.com>
2024-10-29 13:39:03 +0900

    Gave somewhat arbitrary limits to all Sirius ships as players keep on breaking them :P.

--- dat/ships/lua/sirius_divinity.lua
+++ dat/ships/lua/sirius_divinity.lua
@@ -1,12 +1,18 @@
 local flow = require "ships.lua.lib.flow"
+local fmt = require "format"
 require "ships.lua.sirius"
 
 update_dt = 1 -- Update once per second
 local RANGE = 3000
 
+function descextra( _p, _s )
+   return "#y"..fmt.f(_("Every second applies a buff to all fighters within {range} range that increases damage by {inc}% per {over} flow over {min} flow up to {max}%. Range is affected by detection."),
+      {range=fmt.number(RANGE), inc=8, over=100, min=250, max=50}).."#0"
+end
+
 function update( p )
    local f = flow.get( p, mem )
-   local mod = math.max( (f-250)*0.08, 0 )
+   local mod = math.min( 50, math.max( (f-250)*0.08, 0 ) )
    if mod > 0 then
       local rmod = p:shipstat("ew_detect",true)
       for k,v in ipairs(p:getAllies( RANGE*rmod )) do
