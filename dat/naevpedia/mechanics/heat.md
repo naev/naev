7106152147ef782fbd8df940342aa12307cf1740
Edgar Simo-Serra <bobbens@gmail.com>
2024-11-09 14:58:41 +0900

    archives: Added launchers description and heat stub.

---
+++ dat/naevpedia/mechanics/heat.md
@@ -0,0 +1,6 @@
+---
+title: "Heat Management"
+---
+# Heat
+
+TODO
--- src/outfit.c
+++ src/outfit.c
@@ -147,7 +147,7 @@ static os_opts duration_opts = { N_( "Duration" ), _UNIT_TIME, 0, 0, 1, 1 };
 static os_opts cooldown_opts = { N_( "Cooldown" ), _UNIT_TIME, 0, 0, 1, 1 };
 static os_opts lockon_opts   = { N_( "Lock-On" ), _UNIT_TIME, 0, 0, 1, 0 };
 static os_opts inflight_calib_opts = {
-   N_( "Inflight Calibration" ), _UNIT_TIME, 0, 0, 1, 1 };
+   N_( "In-flight Calibration" ), _UNIT_TIME, 0, 0, 1, 1 };
 static os_opts initial_speed_opts = {
    N_( "Launch Speed" ), _UNIT_SPEED, 0, 0, 1, 0 };
 static os_opts accel_opts     = { N_( "Accel" ), _UNIT_ACCEL, 0, 0, 1, 0 };
@@ -165,9 +165,8 @@ static os_opts shots_delay_opts = {
 
 static int outfit_cmp( const void *p1, const void *p2 )
 {
-   const Outfit *o1, *o2;
-   o1 = (const Outfit *)p1;
-   o2 = (const Outfit *)p2;
+   const Outfit *o1 = p1;
+   const Outfit *o2 = p2;
    return strcmp( o1->name, o2->name );
 }
 
