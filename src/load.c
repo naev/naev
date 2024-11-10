435172a7c4db7f73b41f333a2c503d350a9e0562
Edgar Simo-Serra <bobbens@gmail.com>
2024-11-10 22:36:48 +0900

    Fixed save detection.

--- src/load.c
+++ src/load.c
@@ -331,6 +331,11 @@ static int load_enumerateCallbackPlayer( void *data, const char *origdir,
    size_t      dir_len;
    PHYSFS_Stat stat;
 
+   /* Want .ns extensions. */
+   size_t name_len = strlen( fname );
+   if ( name_len < 4 || strcmp( &fname[name_len - 3], ".ns" ) )
+      return PHYSFS_ENUM_OK;
+
    dir_len = strlen( origdir );
 
    fmt = dir_len && origdir[dir_len - 1] == '/' ? "%s%s" : "%s/%s";
@@ -340,13 +345,6 @@ static int load_enumerateCallbackPlayer( void *data, const char *origdir,
             _( PHYSFS_getErrorByCode( PHYSFS_getLastErrorCode() ) ) );
       free( path );
    } else if ( stat.filetype == PHYSFS_FILETYPE_REGULAR ) {
-      size_t path_len = strlen( path );
-      /* Want .ns extensions. */
-      if ( path_len < 4 || strcmp( &fname[path_len - 3], ".ns" ) ) {
-         free( path );
-         return PHYSFS_ENUM_OK;
-      }
-
       player_saves_t *ps = (player_saves_t *)data;
       nsave_t         ns;
       memset( &ns, 0, sizeof( ns ) );
