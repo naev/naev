8b77cb3af123f8a495bd9e993eae511e6041ed0e
Edgar Simo-Serra <bobbens@gmail.com>
2024-11-11 23:58:29 +0900

    Added thurion prefix.

--- dat/scripts/common/thurion.lua
+++ dat/scripts/common/thurion.lua
@@ -5,6 +5,8 @@ local vn = require "vn"
 
 local thurion = {}
 
+thurion.prefix = "#n".._("THURION: ").."#0" -- Repeatable mission prefix
+
 function thurion.vn_drone( name, params )
    return vn.Character.new( name, tmerge( {
       image="thurion_drone.webp",
