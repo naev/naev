f3858f2b3689727960bae7d0a3d0d5145f72119e
Edgar Simo-Serra <bobbens@gmail.com>
2025-01-09 13:34:07 +0900

    Trivial optimization.

--- dat/scripts/common/census.lua
+++ dat/scripts/common/census.lua
@@ -48,9 +48,10 @@ end
 -- Remark: the list `detected` contains pilots that no longer exist.
 -- This is normal behaviour as they have been counted
 function cens.testInRange( detected, fact_list )
-   local visibles = player.pilot():getVisible() -- codespell:ignore visibles
+   local pp = player.pilot()
+   local visibles = pp:getVisible() -- codespell:ignore visibles
    for i, p in ipairs(visibles) do -- Has to be visible -- codespell:ignore visibles
-      local _d, s = player.pilot():inrange(p)
+      local _d, s = pp:inrange(p)
       if s then -- Fuzzy not allowed
          if inlist( fact_list, p:faction() ) then -- Has to be in factions list
             if not inlist( detected, p ) then -- Has not to be in pilots list
