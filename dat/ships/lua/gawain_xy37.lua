cfdc19fabc209d6d4eba12b215652ba1041820e2
Edgar Simo-Serra <bobbens@gmail.com>
2025-01-09 13:36:18 +0900

    Nerfed Gawain XY-37 bonus to a max of +30%.

--- dat/ships/lua/gawain_xy37.lua
+++ dat/ships/lua/gawain_xy37.lua
@@ -1,12 +1,14 @@
 local fmt = require "format"
 
+local MAXBONUS = 30 / 100
+
 function descextra( _p, _s )
-   return "#b"..fmt.f(_("Gains action speed and lowers time constant the lower shields are, to a maximum of a {bonus}% bonus."), {bonus=50}).."#0"
+   return "#b"..fmt.f(_("Gains action speed and lowers time constant the lower shields are, to a maximum of a {bonus}% bonus."), {bonus=MAXBONUS*100}).."#0"
 end
 
 function update( p, _dt )
    local s = p:shield()
-   local bonus = (100-s) * 0.5
+   local bonus = (100-s) * MAXBONUS
    local last = mem.last or 1
    if last ~= s then
       p:shippropSet{ -- Should overwrite existing values
