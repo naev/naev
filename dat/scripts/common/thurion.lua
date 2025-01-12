22919e4bc31ff5d168aa888f7f3dc8f0a71063ba
Edgar Simo-Serra <bobbens@gmail.com>
2025-01-12 11:26:42 +0900

    Updated artwork and add new portraits.

--- dat/scripts/common/thurion.lua
+++ dat/scripts/common/thurion.lua
@@ -7,9 +7,24 @@ local thurion = {}
 
 thurion.prefix = "#n".._("THURION: ").."#0" -- Repeatable mission prefix
 
+thurion.liao = {
+   portrait = "liao.webp",
+   image = "liao.webp",
+   name = _("Liao"),
+   colour = nil,
+   transition = "hexagon",
+}
+
 function thurion.vn_drone( name, params )
    return vn.Character.new( name, tmerge( {
       image="thurion_drone.webp",
+   }, params) )
+end
+
+function thurion.vn_liao( params )
+   return vn.Character.new( thurion.liao.name, tmerge( {
+      image = thurion.liao.image,
+      colour = thurion.liao.colour,
    }, params) )
 end
 
