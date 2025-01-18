8fd651f6b0f8ba35a3964aa8a285647243c34898
hypersoop <hypersoop@proton.me>
2025-01-18 11:52:24 +0000

    Improve a few of the new strings

--- dat/events/news/orez.lua
+++ dat/events/news/orez.lua
@@ -6,12 +6,8 @@ local greeting = {
 }
 local articles = {
    {
-      head = N_([[More War Crimes by House O'rez]]),
-      body = _([[New war crimes have been captured on footage by brave informants. Evidence is to be presented to the Imperial court to strengthen the case for the swift elimination of House O'rez.]]),
-   },
-   {
       head = N_([[More Fabrications and Lies by House Yetmer]]),
-      body = _([[New sham campaign by House Yetmer to cover the fact they are losing the war. The traitors have made up claims regarding bloodlines, which will be soon squashed by the House O'rez elite lawyer team working hard at Jade Court.]]),
+      body = _([[Light has been shed on a new sham campaign by House Yetmer, attempting to cover up the fact they are losing the war. The traitors are making false claims regarding bloodlines, which will soon be squashed by the House O'rez elite lawyer team working hard at Jade Court.]]),
    },
 }
 
