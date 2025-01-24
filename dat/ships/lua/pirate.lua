888365331b2986dfb361d3ff29600ed64b614513
Edgar Simo-Serra <bobbens@gmail.com>
2025-01-25 00:08:40 +0900

    Removed left-in debug print. Gave Soromid ships 3x fuel regen bonus in stellar winds.

--- dat/ships/lua/pirate.lua
+++ dat/ships/lua/pirate.lua
@@ -1,11 +1,8 @@
-
 function init( p )
    -- Pirates get twice the effect of stellar winds
    if system.cur():tags().stellarwind then
-      print("fuel regen!")
       p:shippropSet( "fuel_regen", 1 )
    else
-      print("no fuel regen!")
       p:shippropSet( "fuel_regen", 0 )
    end
 end
--- dat/ships/lua/pirate_revenant.lua
+++
@@ -1,3 +0,0 @@
--- Pirate Revenant gets the Pirate bonuses but Soromid explosions
-require "ships.lua.pirate"
-require "ships.lua.soromid"
