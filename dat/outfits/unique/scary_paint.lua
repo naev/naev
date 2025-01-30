61cf729463069c06bf3eaff897436ca1711e92d6
Edgar Simo-Serra <bobbens@gmail.com>
2025-01-30 13:37:52 +0900

    Fixes for consistency.

--- dat/outfits/unique/scary_paint.lua
+++ dat/outfits/unique/scary_paint.lua
@@ -4,18 +4,19 @@
    Makes the player feared by pirates and bounty hunters.
 --]]
 local pir
+notactive = true
 
 function onload( _o )
    pir = require "common.pirate"
 end
 
 function init( p, _po )
-   p:effectAdd("Paint", math.huge)
+   p:effectAdd("Scary Paint")
+   mem.isp = (p==player.pilot())
 end
 
 function onremove( p, _po )
---   p:effectRm("Paint")
-	p:effectClear()
+   p:effectRm("Scary Paint")
 end
 
 -- TODONOTE: when independents start scanning, change the else branch!
@@ -27,13 +28,8 @@ function onscanned( p, _po, scanner )
          scanner:setHostile(p, false)
       end
    else
-	-- unfortunately, bounty hunters don't scan yet I think
-	-- but there is an edge case here if the scanner is the
-	-- player's main ship piloted by a AI and the player is in an auxiliary ship
-      local c = naev.cache()
-      if c.player_mothership ~= scanner:name() then
+      if mem.isp and not scanner:withPlayer() then
          scanner:setHostile(p)
-	  end
+      end
    end
 end
-
