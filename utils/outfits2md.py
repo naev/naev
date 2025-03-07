b534873e1d01ee92a648be6e8aa0a9c2a7121c8e
johann1764 <johann.brault_baron@yahoo.com>
2025-03-07 13:37:31 +0100

    outfits2md - better conformity with github md specs & misc improvements

--- utils/outfits2md.py
+++ utils/outfits2md.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-from sys import argv
+from sys import argv,stderr
 import xml.etree.ElementTree as ET
 
 
-def main(args):
+def main(args,gith=False,ter=False):
    names=['']*len(args)
    L=[dict() for a in args]
    rang=dict()
@@ -14,7 +14,7 @@ def main(args):
       T=ET.parse(args[i]).getroot()
       for t in T.iter():
          try:
-            n=float(t.text)
+            n=abs(float(t.text))
             L[i][t.tag]=t.text
             if not rang.has_key(t.tag):
                rang[t.tag]=(n,n)
@@ -34,35 +34,62 @@ def main(args):
 
    Res=[[k]+[l[k] if k in l else '' for l in L] for k in acc]
    names=['']+names
-   length=[1]*len(names)
-   length=[max(n,len(s)) for (n,s) in zip(length,names)]
+   length=[4]*len(names) # :--- at least 3 - required
 
-   for r in Res:
-      length=[max(n,len(s)) for (n,s) in zip(length,r)]
+   if not gith:
+      length=[max(n,len(s)) for (n,s) in zip(length,names)]
 
-   mklin=lambda L:'| '+' | '.join(L)+' |'
-   fmt=lambda (s,n):(n-len(s))*' '+s
-   lfmt=lambda (s,n):s+(n-len(s))*' '
+      for r in Res:
+         length=[max(n,len(s)) for (n,s) in zip(length,r)]
+
+   if ter:
+      Sep,Lm,Rm,LM,RM='\033[34m|\033[37m',"\033[31m","\033[37m","\033[32m","\033[37m"
+      mk_pad =lambda i,n:"\033[34m"+n*'-'+"\033[0m"
+      leng=lambda x:len(x)- (10 if x!='' and x[0]=='\033' else 0)
+   else:
+      Sep,Lm,Rm,LM,RM='|',"_","_","**","**"
+      mk_pad =lambda i,n:'-'*(n-1)+('-' if i==0 else ':')
+      leng=len
+
+   mklin=lambda L:Sep+' '+(' '+Sep+' ').join(L)+' '+Sep
+   fmt=lambda (s,n):(n-leng(s))*' '+s
+   lfmt=lambda (s,n):s+(n-leng(s))*' '
 
    def emph(s,(mi,ma)):
       if s=='':
          return "_"
-      if mi!=ma:
+      elif mi!=ma:
+         if mi<0 and ma<0: #if all are negative, assume the lower the better
+            mi,ma=ma,mi
          if float(s)==mi:
-            return "_"+s+"_"
+            return Lm+s+Rm
          elif float(s)==ma:
-            return "**"+s+"**"
+            return LM+s+RM
       return s
 
+   print
    print mklin(map(fmt,zip(names,length)))
-   print mklin(['-'*(n-1)+ ('-' if i==0 else ':') for i,n in enumerate(length)])
+   print mklin([mk_pad(i,n) for i,n in enumerate(length)])
    for r in Res:
       r=[r[0].replace("_"," ")]+[emph(k,rang[r[0]]) for k in r[1:]]
       print mklin([fmt(x) if i>0 else lfmt(x) for i,x in enumerate(zip(r,length))])
 
 if __name__ == '__main__':
    if '-h' in argv[1:] or '--help' in argv[1:] or len(argv)<2:
-      print "usage:",argv[0],'<outfitname1.xml> ...'
+      print "usage:",argv[0],'[-g|-t] <outfitname1.xml> ...'
+      print "By default, outputs text aligned markdown table comparing the outfits resp. values."
+      print "The options improve your confort in certain use cases:"
+      print "  -g  unaligned (therefore smaller) valid github md, for use in posts."
+      print "  -c  colored terminal output."
    else:
-      main([f for f in argv[1:] if f.endswith(".xml")])
+      gith,ter="-g" in argv[1:],"-c" in argv[1:]
+      if gith and ter:
+         gith=ter=False
+         print >>stderr,"Ignored incompatible -g and -c."
+
+      ign=[f for f in argv[1:] if f not in ["-g","-c"] and not f.endswith(".xml")]
+      if ign!=[]:
+         print >>stderr,'Ignored: "'+'", "'.join(ign)+'"'
+
+      main([f for f in argv[1:] if f.endswith(".xml")],gith,ter)
 
