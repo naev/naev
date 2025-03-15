4e58e22e329c6a0d0c99ea8373dc89ea6e19cdef
johann1764 <johann.brault_baron@yahoo.com>
2025-03-15 03:52:12 +0100

    multicore -- utils/outfits/

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!
-./utils/multicore.py dat/outfits/core_system/small/milspec_orion_*.xml | ./utils/multicore2lua.py res
+./utils/outfits/multicore.py dat/outfits/core_system/small/milspec_orion_*.xml | ./utils/outfits/multicore2lua.py res
