6ae1e5680426b474041996b096edcf87323f1122
johann1764 <johann.brault_baron@yahoo.com>
2025-03-27 13:15:18 +0100

    engines reworked - engine_stats->python3 + help

--- utils/outfits/engine_stats.py
+++ utils/outfits/engine_stats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from outfit import outfit
 
@@ -31,11 +31,9 @@ fullsptime=lambda a:maxspeed(a)/accel(a) if accel(a) else 0.0
 radius=lambda a:round(maxspeed(a)/(turn(a)/180.0*math.pi))
 fullspdist=lambda a:round(maxspeed(a)*fullsptime(a)/2.0)
 
-def my_cmp((a,_),(b,_2)):
-   d=maxspeed(a)-maxspeed(b)
-   if d==0.0:
-      d=accel(a)-accel(b)
-   return -(-1 if d<0.0 else 1 if d>0.0 else 0)
+def key(A):
+   (a,_)=A
+   return (-maxspeed(a),-accel(a))
 
 def fmt(f):
    return round(f,2)
@@ -52,17 +50,24 @@ def l(s):
    a,b=tuple(s.split('.',1))
    return '|'+(3-len(a))*' '+a+m+b+(2-len(b))*' '
 
-L=[(a.to_dict(),a.shortname()) for a in map(outfit,argv[1:]) if a is not None]
-L.sort(my_cmp)
-C=['speed','max speed','accel','fullsp time','fullsp dist','turn','turn radius']
-N=max([len(n) for (_,n) in L])
-print '|'+'  '+'|',' | '.join(C)
-print '|  ---'+(N-3)*' '+len(C)*'|  ---  '
-for k,n in L:
-   if accel(k)!=0:
-      nam=n+(N-len(n))*' '
-      print '|',nam,l(speed(k)),l(fmt(maxspeed(k))),l(accel(k)),
-      print l(fmt(fullsptime(k))),l(fmt(fullspdist(k))),
-      print l(turn(k)),l(radius(k))
+if '-h' in argv or '--help' in argv:
+   print("Usage: "+argv[0]+" <file1> <file2> ...")
+   print("Will only process the files in the list that have .xml or .mvx extension.")
+   print("Outputs a MD table with some new derived fields.")
+   print("\nTypical usage (from naev root dir) :")
+   print("> ./utils/outfits/apply_engines.py `find dat/outfits/core_engine/`")
+else:
+   L=[(a.to_dict(),a.shortname()) for a in map(outfit,argv[1:]) if a is not None]
+   L.sort(key=key)
+   C=['speed','max speed','accel','fullsp time','fullsp dist','turn','turn radius']
+   N=max([len(n) for (_,n) in L])
+   print('|'+'  '+'|',' | '.join(C))
+   print('|  ---'+(N-3)*' '+len(C)*'|  ---  ')
+   for k,n in L:
+      if accel(k)!=0:
+         nam=n+(N-len(n))*' '
+         acc='| '+nam+l(speed(k))+l(fmt(maxspeed(k)))+l(accel(k))
+         acc+=l(fmt(fullsptime(k)))+l(fmt(fullspdist(k)))+l(turn(k))+l(radius(k))
+         print(acc)
 
 
