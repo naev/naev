3764653c8838d1bbd55ab72df7cdad4e7abaa27f
Edgar Simo-Serra <bobbens@gmail.com>
2025-03-28 10:01:28 +0900

    Support for more complicated constant expressions.

--- utils/outfits/getconst.py
+++ utils/outfits/getconst.py
@@ -2,37 +2,22 @@
 
 import os
 import sys
+import re
 
 script_dir = os.path.dirname( __file__ )
 const_dir = os.path.join( script_dir, '..', '..' , 'dat' , 'constants.lua' )
 
 try:
-   fp=open(const_dir,"rt")
-   s=fp.read().split("PHYSICS_SPEED_DAMP",1)
-   fp.close()
+   with open(const_dir,"rt") as fp:
+      p = re.compile("PHYSICS_SPEED_DAMP *=(.*),")
+      m = p.search(fp.read())
+      s = m.group(1)
 except:
    sys.stderr.write('Err: '+str(const_dir)+' not found!\n')
    sys.exit(1)
 
-if len(s)!=2:
-   sys.stderr.write("Can't find \"PHYSICS_SPEED_DAMP\" in "+str(const_dir)+'.\n')
-   sys.exit(1)
-
-s=s[1].strip()
-if s[0]=='=':
-   s=s[1:]
-
-if s[0] not in "012345679+-.":
-   sys.stderr.write("Can't find value after \"PHYSICS_SPEED_DAMP =\" in "+str(const_dir)+'.\n')
-   sys.exit(1)
-
-for i in range(len(s)):
-   if s[i] not in "012345679+-.eE":
-      s=s[:i]
-      break
-
 try:
-   f=float(s)
+   f = eval(s)
 except:
    sys.stderr.write("Can't read value after \"PHYSICS_SPEED_DAMP=\" in "+str(const_dir)+'.\n')
    sys.exit(1)
@@ -41,4 +26,3 @@ PHYSICS_SPEED_DAMP=f
 
 if __name__=="__main__":
    sys.stderr.write("PHYSICS_SPEED_DAMP="+str(f)+'\n')
-
