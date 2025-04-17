363ad1dd1dc0d6497a4199f125604879834ff498
johann1764 <johann.brault_baron@yahoo.com>
2025-04-17 10:59:21 +0200

    multicore_3 - alone/twin draft

--- dat/outfits/lib/alone.lua
+++ dat/outfits/lib/alone.lua
@@ -1,6 +1,21 @@
---init = require "alone"
---onoutfitchange = require "alone"
 
-return function ( _p, _po )
-   print ("Hello")
+return function ( p, po )
+   if false and p and po then
+      local s = p:ship()
+      if s then
+         local slot
+         for i, v in ipairs( ship.getSlots( s ) ) do
+            if v['name'] == 'engines_secondary' then
+               slot = i
+               break
+            end
+         end
+         if slot then
+            local o = outfitSlot (p, slot)
+            if po:outfitRmSlot(p,'engines_secondary') then
+               print 'You cannot do that.'
+            end
+         end
+      end
+   end
 end
--- dat/outfits/lib/twins.lua
+++ dat/outfits/lib/twins.lua
@@ -1,6 +1,21 @@
---init = require "alone"
---onoutfitchange = require "alone"
 
-return function ( _p, _po )
-   print ("Hello")
+return function ( p, po )
+   if false and p and po then
+      local count = 0
+      local mismatch = false
+      for k,o in ipairs(p:outfits()) do
+         if o and o:type() == "Core Systems (Engine)" then
+            if o:name() == po:name() then
+               count = count+1
+            else
+               mismatch = true
+            end
+         end
+      end
+      if count == 2 and not mismatch then
+         print ("Ok")
+      else
+         print ("Not ok")
+      end
+   end
 end
