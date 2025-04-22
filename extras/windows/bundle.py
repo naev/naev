c3c066bf3d6ce92ad4d0d76a3b48e76ddf457e94
Jack Greiner <jack@emoss.org>
2025-04-21 20:20:09 -0400

    bundle.py: Add hack to include SDL3
    For some reason when you check what DLLs are dependencies of SDL2-compat using objdump, SDL3 doesn't make the list.

--- extras/windows/bundle.py
+++ extras/windows/bundle.py
@@ -16,6 +16,22 @@ def usage():
     print(f"usage: {os.path.basename(sys.argv[0])} [-d] (Verbose output)")
     sys.exit(1)
 
+# Yoink helper function from mingw-bundledlls to help find DLL in given search paths
+def find_full_path(filename, path_prefixes):
+    for path_prefix in path_prefixes:
+        path = os.path.join(path_prefix, filename)
+        path_low = os.path.join(path_prefix, filename.lower())
+        if os.path.exists(path):
+            return path
+        if os.path.exists(path_low):
+            return path_low
+    else:
+        raise RuntimeError(
+            "Can't find " + filename + ". If it is an inbuilt Windows DLL, "
+            "please add it to the blacklist variable in the script and send "
+            "a pull request!"
+        )
+
 verbose = False
 
 # Parse command-line arguments
@@ -59,8 +75,22 @@ if verbose or dll_list_proc.returncode != 0:
 
 dll_list_proc.check_returncode()
 
-# Copy DLLs to installation directory
+# HACK: Add SDL3.dll if it exists
+# This is a workaround for the fact that objdump doesnm't show SDL3.dll as a dependency for SDL2-compat
+# So lets include it in the list of DLLs to copy
+
 dll_list = dll_list_out.decode().splitlines()
+search_paths = os.environ['MINGW_BUNDLEDLLS_SEARCH_PATH'].split(os.pathsep)
+try:
+    sdl3_path = find_full_path('SDL3.dll', search_paths)
+    dll_list.append(sdl3_path)
+    if verbose:
+        print(f"Found SDL3.dll at {sdl3_path} and added to dll_list")
+except RuntimeError:
+    if verbose:
+        print("SDL3.dll not found. Skipping.")
+
+# Copy DLLs to installation directory
 for dll in dll_list:
     dll_path = os.path.join(os.getenv('MESON_INSTALL_DESTDIR_PREFIX'), os.path.basename(dll))
     shutil.copy(dll, dll_path)
