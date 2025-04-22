30e78f1e6d364fba66e71f35dfa9a6ec69cbe5ea
Edgar Simo-Serra <bobbens@gmail.com>
2025-04-22 21:58:27 +0900

    Simplify things a bit more.

--- src/nlua.rs
+++ src/nlua.rs
@@ -72,49 +72,6 @@ fn open_gettext(lua: &mlua::Lua) -> mlua::Result<()> {
     Ok(())
 }
 
-fn sandbox(lua: &mlua::Lua) -> mlua::Result<()> {
-    let globals = lua.globals();
-
-    /* Override built-ins to use Naev for I/O. */
-    /*
-    lua_register( L, "print", cli_print );
-    lua_register( L, "printRaw", cli_printRaw );
-    lua_register( L, "warn", cli_warn );
-    */
-
-    // move [un]pack to table.[un]pack as in Lua5.2.
-    let table: mlua::Table = globals.get("table")?;
-    let unpack: mlua::Value = globals.get("unpack")?;
-    table.set("unpack", unpack)?;
-    globals.set("unpack", mlua::Nil)?;
-
-    // Some overrides.
-    globals.set("dofile", mlua::Nil)?;
-    globals.set("getfenv", mlua::Nil)?;
-    globals.set("load", mlua::Nil)?;
-    globals.set("loadfile", mlua::Nil)?;
-
-    // Sandbox "io" and "os".
-    globals.set("io", mlua::Nil)?;
-    let os_table = lua.create_table()?;
-    os_table.set(
-        "getenv",
-        lua.create_function(|_lua, _s: String| -> mlua::Result<String> {
-            // Fake a $HOME
-            Ok(String::from("lua_home"))
-        })?,
-    )?;
-    globals.set("os", os_table)?;
-
-    // Special math.
-    let math_table: mlua::Table = globals.get("math")?;
-    math_table.set("mod", mlua::Nil)?; /* Get rid of math.mod */
-
-    //
-
-    Ok(())
-}
-
 #[allow(dead_code)]
 fn require(lua: &mlua::Lua, filename: mlua::String) -> mlua::Result<mlua::Value> {
     let globals = lua.globals();
@@ -164,15 +121,41 @@ impl NLua {
         // Load base libraries NOT SUFFICIENT
         //lua.load_std_libs( mlua::StdLib::ALL_SAFE ).unwrap();
 
-        // Minor sandboxing
-        sandbox(&lua)?;
-
         // Set up gettext stuff
         open_gettext(&lua)?;
 
-        // Add some mor functions.
+        // Add some more functions.
         let globals = lua.globals();
         globals.set("require", lua.create_function(require)?)?;
+
+        // move [un]pack to table.[un]pack as in Lua5.2.
+        let table: mlua::Table = globals.get("table")?;
+        let unpack: mlua::Value = globals.get("unpack")?;
+        table.set("unpack", unpack)?;
+        globals.set("unpack", mlua::Nil)?;
+
+        // Some unsafe overrides.
+        globals.set("dofile", mlua::Nil)?;
+        globals.set("getfenv", mlua::Nil)?;
+        globals.set("load", mlua::Nil)?;
+        globals.set("loadfile", mlua::Nil)?;
+
+        // Sandbox "io" and "os".
+        globals.set("io", mlua::Nil)?;
+        let os_table = lua.create_table()?;
+        os_table.set(
+            "getenv",
+            lua.create_function(|_lua, _s: String| -> mlua::Result<String> {
+                // Fake a $HOME
+                Ok(String::from("lua_home"))
+            })?,
+        )?;
+        globals.set("os", os_table)?;
+
+        // Special math.
+        let math_table: mlua::Table = globals.get("math")?;
+        math_table.set("mod", mlua::Nil)?; /* Get rid of math.mod */
+
         unsafe {
             /*
             globals.set(
