56445761e524fb801a62da1550e380f1430a0366
johann1764 <johann.brault_baron@yahoo.com>
2025-04-28 12:43:13 +0200

    improved luafunc_dup using uniq

--- utils/luafunc_dup.sh
+++ utils/luafunc_dup.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/bash
 
-TMP=`tempfile`
 LST=`tempfile`
 res=0
-trap 'rm -f $TMP $LST ; exit $res' EXIT
+trap 'rm -f $LST ; exit $res' EXIT
 
 for arg in $@ ; do
-   grep '^ \* @luafunc' $arg | sort > $TMP
-   sort -u $TMP | diff $TMP - | sort -u - | grep '<' | cut '-d<' -f2- > $LST
+   grep '^ \* @luafunc' $arg | sort | uniq -c | grep -v "^ *1" > $LST
 
    if [ -s "$LST" ] ; then
       echo "$arg:"
