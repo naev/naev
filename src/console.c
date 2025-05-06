728c7ea51f83c16df3845c9ddb130ec4082b5780
johann1764 <johann.brault_baron@yahoo.com>
2025-05-06 05:55:39 +0200

    oops

--- src/console.c
+++ src/console.c
@@ -121,12 +121,12 @@ static char *fontcol_toTermEscapeString(const char*s){
    char*buf;
    int i, wi=0, count=0;
 
-   for(i=0; s[i++]; i++)
+   for(i=0; s[i]; i++)
       if(s[i] == FONT_COLOUR_CODE)
          count++;
 
    buf=calloc(i+count*7+1,sizeof(char));
-   for(i=0; i<len; i++){
+   for(i=0; s[i]; i++){
       if(s[i] == FONT_COLOUR_CODE){
          const char*es=seq[(s[i+1]>='0' && s[i+1]<='z')? _CID(s[i+1]):0];
 
@@ -195,7 +195,7 @@ static int cli_printCore( lua_State *L, int cli_only, int escape )
    const char *s = lua_tostring( L, -1 );
 
    if ( !cli_only ){
-      char*tolog=fontcol_toTermEscapeString(s)
+      char*tolog=fontcol_toTermEscapeString(s);
       LOG( "%s", s );
       free(tolog);
    }
