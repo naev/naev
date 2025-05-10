861fb20743b59d3665c16c3a00e50def4fbb456f
johann1764 <johann.brault_baron@yahoo.com>
2025-05-10 07:07:35 +0200

    mc_polish - better path management in scripts

--- utils/outfits/update_mvx_from_xmllua.sh
+++ utils/outfits/update_mvx_from_xmllua.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DST="${SCRIPT_DIR}/../../dat/outfits"
-#DST="${SCRIPT_DIR}/../../dat/outfits/core_engine"
+DST=$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/outfits")
 
 echo "$DST"
 for i in `grep -rl 'lua_inline' ${DST} | grep '\.xml$'` ;
