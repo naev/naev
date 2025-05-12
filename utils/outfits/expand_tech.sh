f8b22a98b9815e17928a3de9ebe1df194d5a4058
johann1764 <johann.brault_baron@yahoo.com>
2025-05-12 14:11:22 +0200

    mvx_dep - update expand_tech.sh

--- utils/outfits/expand_tech.sh
+++ utils/outfits/expand_tech.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 
-LST=$(mktemp)
-res=0
-trap 'rm -f $LST ; exit $res' EXIT
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DST=$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/outfits")
-grep '<item>.*</item>' "$@" | sed 's/^ *<item>\(.*\)<\/item> *$/\1/' | sed 's/\&amp;/\&/' | sort -du | sed 's/^\(.*\)$/outfit name="\1"/' | grep -f - -rl "${DST}" | sort -du > "$LST"
-grep "\.\(xml\)\|\(mvx\)$" "$LST" | rev | cut -b 5- | rev | uniq -d | sed 's/$/.xml/' | diff - "$LST" | grep '>' | cut -b 2-
+grep '<item>.*</item>' "$@" | sed 's/^ *<item>\(.*\)<\/item> *$/\1/' | sed 's/\&amp;/\&/' | \
+sort -du | sed 's/^\(.*\)$/outfit name="\1"/' | grep -f - -rl "${DST}" | grep "\.xml" | \
+sort -du
