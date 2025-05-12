64b88253a24b2c61d86979bb37d20f8ec0864f46
johann1764 <johann.brault_baron@yahoo.com>
2025-05-12 12:26:16 +0200

    mvx_dep - updated sh scripts

--- utils/outfits/gen_mvx_from_xml.sh
+++ utils/outfits/gen_mvx_from_xml.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DST=$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/outfits")
 
-echo "$DST"
+echo Generate from "$DST" >&2
 for i in `grep -rl 'lua_inline' ${DST} | grep '\.xml$'` ;
-   do ${SCRIPT_DIR}/xmllua2mvx.py "$i" > "${i%xml}mvx"
+   do ${SCRIPT_DIR}/xmllua2mvx.py "$i" > $(dirname "$i")/.$(basename "$i" xml)mvx
 done
--- utils/outfits/update_xmllua_from_mvx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DST=$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/outfits")
-
-echo "$DST"
-for i in `find ${DST} | grep '\.mvx$'` ;
-   do ${SCRIPT_DIR}/mvx2xmllua.py "$i" > "${i%mvx}xml"
-done
