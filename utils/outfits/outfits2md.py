e7857ffa5f03356172c12a07ce10653bc8deb5e7
johann1764 <johann.brault_baron@yahoo.com>
2025-05-12 17:07:09 +0200

    mvx_dep - adapt outfits2md.py & fmt

--- utils/outfits/outfits2md.py
+++ utils/outfits/outfits2md.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from sys import stderr
-from outfit import outfit
+from core_outfit import some_outfit
 from outfit import LOWER_BETTER
 
 def transpose( M ):
@@ -9,7 +9,7 @@ def transpose( M ):
    M = [t+['']*(N-len(t)) for t in M]
    return list(zip(*(tuple(M))))
 
-getfloat = lambda s:float(s.split('/')[0])
+getfloat = lambda s: float(s.split('/')[0])
 
 def keyfunc( s ):
    def key( o ):
@@ -37,7 +37,7 @@ def main( args, gith = False, ter = False, noext = False, sortit = False, autost
          elif '+' in i:
             stderr.write('"+"')
          else:
-            if outfit(i).can_sec():
+            if some_outfit(i).can_sec():
                sec_args.append(i)
             continue
          stderr.write(' incompatible with -A/-C options\n')
@@ -45,16 +45,16 @@ def main( args, gith = False, ter = False, noext = False, sortit = False, autost
 
       if comb:
          sec_args = set(sec_args)
-         tmp = [t for t in args]
+         tmp = ['1x'+t for t in args]
          for i in range(len(args)):
             for j in range(i, len(args)):
                if args[j] in sec_args:
-                  tmp.append(args[i]+'+'+args[j])
+                  tmp.append(args[i] + '+' + args[j])
                if i != j and args[i] in sec_args:
-                  tmp.append(args[j]+'+'+args[i])
+                  tmp.append(args[j] + '+' + args[i])
          args = tmp
       elif autostack:
-         args = args+['2x'+i for i in sec_args]
+         args = ['1x'+t for t in args] + ['2x'+i for i in sec_args]
 
    rang = dict()
    L = []
@@ -64,24 +64,29 @@ def main( args, gith = False, ter = False, noext = False, sortit = False, autost
    for i in range(len(args)):
       if args[i][:2] == '2x' or args[i][-2:] == 'x2':
          args[i] = args[i][2:] if args[i][:2] == '2x' else args[i][:-2]
-         args[i] = args[i]+'+'+args[i]
+         args[i] = args[i] + '+' + args[i]
 
       if len(args[i].split('+')) == 2:
          o, o2 = args[i].split('+')
-         o, o2 = outfit(o.strip()), outfit(o2.strip())
+         o, o2 = some_outfit(o.strip()), some_outfit(o2.strip())
          if not o.can_stack(o2):
             continue
          o.stack(o2)
       else:
-         o = outfit(args[i])
+         autos = args[i][:2] == '1x' or args[i][-2:] == 'x1'
+         if autos:
+            args[i] = args[i][2:] if args[i][:2] == '1x' else args[i][:-2]
+         o = some_outfit(args[i])
          if not o.can_alone():
             continue
+         if autos:
+            o.autostack(False)
 
       d = o.to_dict()
       for k, v in d.items():
          if type(v) == type((1.0,)):
             (val, _) = v
-            s = nfmt(v[0])+'/'+nfmt(v[1])
+            s = nfmt(v[0]) + '/' + nfmt(v[1])
          elif type(v) == type(1.0):
             val = v
             s = nfmt(v)
@@ -119,11 +124,11 @@ def main( args, gith = False, ter = False, noext = False, sortit = False, autost
 
    if ter:
       Sep, SepAlt, Lm, Rm, LM, RM = '\033[30;1m|\033[0m', '\033[34m|\033[0m', '\033[31m', '\033[37m', '\033[32m', '\033[37m'
-      mk_pad = lambda i, n:'\033[34m'+n*'-'+'\033[0m'
-      leng = lambda x:len(x)- (10 if x != '' and x[0]=='\033' else 0)
+      mk_pad = lambda i, n: '\033[34m'+n*'-'+'\033[0m'
+      leng = lambda x: len(x) - (10 if x != '' and x[0]=='\033' else 0)
    else:
       Sep, SepAlt, Lm, Rm, LM, RM = '|', '|', '_', '_', '**', '**'
-      mk_pad = lambda i, n:'-'*(n-1)+('-' if i == 0 else ':')
+      mk_pad = lambda i, n: '-'*(n-1)+('-' if i == 0 else ':')
       leng = len
 
    if ter:
@@ -138,11 +143,11 @@ def main( args, gith = False, ter = False, noext = False, sortit = False, autost
       for t in head:
          length = [max(n, len(s)) for (n, s) in zip(length, t)]
 
-   mklinsep = lambda sep:lambda L:sep+' '+(' '+sep+' ').join(L)+' '+sep
-   mklinalt = lambda alt:mklinsep(SepAlt) if alt else mklinsep(Sep)
+   mklinsep = lambda sep: lambda L:sep+' '+(' '+sep+' ').join(L)+' '+sep
+   mklinalt = lambda alt: mklinsep(SepAlt) if alt else mklinsep(Sep)
    mklin = mklinalt(False)
-   fmt = lambda t:(t[1]-leng(t[0]))*' '+t[0]
-   lfmt = lambda t:t[0]+(t[1]-leng(t[0]))*' '
+   fmt = lambda t: (t[1]-leng(t[0]))*' '+t[0]
+   lfmt = lambda t: t[0]+(t[1]-leng(t[0]))*' '
 
    def emph( s, m ):
       (mi, ma) = m
@@ -165,7 +170,7 @@ def main( args, gith = False, ter = False, noext = False, sortit = False, autost
 
    count = 0
    for r in Res:
-      r = [r[0].replace('_', ' ')]+[emph(k, rang[r[0]]) for k in r[1:]]
+      r = [r[0].replace('_', ' ')] + [emph(k, rang[r[0]]) for k in r[1:]]
       print(mklinalt(count%3 == 2)([fmt(x) if i>0 else lfmt(x) for i, x in enumerate(zip(r, length))]))
       count += 1
    return 0
@@ -186,7 +191,8 @@ if __name__ == '__main__':
    parser.add_argument('-A', '--autostack', action = 'store_true', help = 'Adds 2x outfits' )
    parser.add_argument('-C', '--combinations', action = 'store_true', help = 'Does all the combinations.' )
    parser.add_argument('filename', nargs = '*', help = """An outfit with ".xml" or ".mvx" extension, else will be ignored.
-Can also be two outfits separated by \'+\', or an outfit prefixed with \'2x\' or suffixed with \'x2\'.""")
+Can also be two outfits separated by \'+\', or an outfit prefixed with \'2x\'
+(or \'1x\') or suffixed with \'x2\' (or \'x1\'.""")
 
    args = parser.parse_args()
    if args.github and args.color:
