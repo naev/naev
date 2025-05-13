3ee2300d9d8fbee485ee78675adb7352379a6194
johann1764 <johann.brault_baron@yahoo.com>
2025-05-13 13:01:25 +0200

    mc_details - silenced generate.py mvx generation messages

--- utils/outfits/core_outfit.py
+++ utils/outfits/core_outfit.py
@@ -11,29 +11,29 @@ from mvx2xmllua import mvx2xmllua
 def mvx_nam(xml):
    return path.join(path.dirname(xml), '.' + path.basename(xml)[:-3] + 'mvx')
 
-def _gen_if_needed( xml, force = False ):
+def _gen_if_needed( xml, force, quiet ):
    mvx = mvx_nam(xml)
    exists = (not force) and Path(mvx).is_file()
    uptodate = exists and not path.getmtime(mvx) < path.getmtime(xml)
    if not uptodate:
-      return xmllua2mvx(xml, mvx, quiet = False)
+      return xmllua2mvx(xml, mvx, quiet)
    else:
       return outfit(mvx)
 
-def core_outfit( nam, try_again = False ):
+def core_outfit( nam, try_again = False, quiet = False ):
    if nam[-4:] == '.xml':
       try:
-         o = _gen_if_needed(nam)
+         o = _gen_if_needed(nam, False, quiet)
       except:
          o = None
       if o is None and try_again:
-         o = _gen_if_needed(nam, True)
+         o = _gen_if_needed(nam, True, quiet)
       if not (o is None):
          o.fil = nam
       return o
 
-def some_outfit( nam ):
-   o = core_outfit(nam)
+def some_outfit( nam, quiet = False ):
+   o = core_outfit(nam, quiet)
    return o if not (o is None) else outfit(nam)
 
 def core_write( o, fil ):
