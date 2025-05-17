0bfb0663ab67f6b9b2c924fac642ee559905a78a
Edgar Simo-Serra <bobbens@gmail.com>
2025-05-17 16:52:56 +0900

    Fix event name.

--- dat/events/neutral/nougat_ii_astral_orchids.lua
+++ dat/events/neutral/nougat_ii_astral_orchids.lua
@@ -1,9 +1,9 @@
 --[[
 <?xml version='1.0' encoding='utf8'?>
-<event name="Unicorn II Astral Orchids">
+<event name="Nougat II Astral Orchids">
  <location>land</location>
  <chance>100</chance>
- <spob>Unicorn II</spob>
+ <spob>Nougat II</spob>
  <tags>
   <tag>astral_orchids</tag>
  </tags>
