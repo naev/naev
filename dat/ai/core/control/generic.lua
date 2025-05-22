0dd120a243512e0f5c77575f452a8188209237c2
Edgar Simo-Serra <bobbens@gmail.com>
2025-05-22 23:39:26 +0900

    Added loiter_compensate_vel to AI memory.

--- dat/ai/core/control/generic.lua
+++ dat/ai/core/control/generic.lua
@@ -56,6 +56,7 @@ mem.leadermaxdist = nil -- Distance from leader to run back to leader
 mem.gather_range  = 800 -- Radius in which the pilot looks for gatherables
 mem.lanes_useneutral = false -- Whether or not to use neutral lanes
 mem.uselanes       = true -- Try to use lanes
+mem.loiter_compensate_vel = true -- Try to compensate velocity when loitering
 
 --[[Control parameters: mem.radius and mem.angle are the polar coordinates
 of the point the pilot has to follow when using follow_accurate.
