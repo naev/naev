318ecf1e78e4c29ac82aee4793715383b53aaa43
Edgar Simo-Serra <ess@waseda.jp>
2025-05-28 12:39:14 +0900

    Launch escape pod when capturing ships.

--- dat/outfits/intrinsic/escape_pod.lua
+++ dat/outfits/intrinsic/escape_pod.lua
@@ -1,22 +1,12 @@
+local escapepod = require "outfits.lib.escape_pod"
+
 function init ()
    mem.launched = false
 end
 
 function ondeath( p )
    if not mem.launched then
-      -- TODO for some reason it doesn't like setting the ai first, gives an
-      -- error that we can't really trace atm
-      --local ep = pilot.add("Escape Pod", p:faction(), p:pos(), nil, {ai="escape_pod"} )
-      local ep = pilot.add("Escape Pod", p:faction(), p:pos(), nil, {naked=true} )
-      ep:changeAI("escape_pod")
-      ep:setInvincible(true)
-      ep:setInvisible(true)
-      ep:setVel( p:vel() )
-      if p:ship():size() < 3 then
-         ep:setDir( p:dir() )
-      else
-         ep:setDir( rnd.angle() )
-      end
+      escapepod.launch( p )
       mem.launched = true
    end
 end
