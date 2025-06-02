6b740bfdd82b6054faa05b088375bacd343c5269
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-02 12:34:57 +0900

    Tweaked and nerfed reaver cannon a bit.

--- dat/outfits/py/reaver.py
+++ dat/outfits/py/reaver.py
@@ -7,21 +7,21 @@ data['outfit']['@name'] = N_('Reaver Cannon')
 
 general = data['outfit']['general']
 general['rarity'] = '2'
-general['description'] = N_("TODO")
+general['description'] = N_("The complex engineering specifications and difficulty of mass production doomed the Reaver Cannon from the start. However, in the right hands, given the extreme long range and high penetration for a weapon of its size, it can be very deadly.")
 h.add_i(general, 'price', 130e3 )
-h.add_i(general, 'mass', -1 )
+h.add_i(general, 'mass', 2 )
 h.mul_i(general, 'cpu', 1.1 )
 
 specific = data['outfit']['specific']
 h.mul_f(specific, 'range', 1.5 )
 h.mul_f(specific, 'falloff', 1.2 )
-h.mul_f(specific, 'delay', 2.5 )
+h.mul_f(specific, 'delay', 2 )
 h.mul_f(specific, 'energy', 2.0 )
 h.mul_f(specific, 'swivel', 1.5 )
 h.mul_i(specific, 'trackmax', 0.8333333333 )
 
 damage = specific['damage']
-h.mul_f(damage, 'physical', 3.0 )
-h.mul_i(damage, 'penetrate', 1.5 )
+h.mul_f(damage, 'physical', 2.0 )
+h.mul_i(damage, 'penetrate', 1.3 )
 
 h.write( data )
