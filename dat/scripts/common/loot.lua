a3c5d3bca8938f9c28b4c1711244ba4dd9fbe728
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-04 00:30:44 +0900

    Start to work on tiered loot script.

---
+++ dat/scripts/common/loot.lua
@@ -0,0 +1,38 @@
+local lib = {}
+
+local tier1_nonweapon, tier1_weapon
+
+function lib.tier1_nonweapon ()
+   if not tier1_nonweapon then
+      tier1_nonweapon = {
+         outfit.get("Emergency Stasis Inducer"),
+         outfit.get("Combat Hologram Projector"),
+         outfit.get("Berserk Chip"),
+         outfit.get("Hellburner"),
+         outfit.get("Nanofiber Structural Enhancement"),
+         outfit.get("Biometal Armour"),
+      }
+   end
+   return tier1_nonweapon[ rnd.rnd(1,#tier1_nonweapon) ]
+end
+
+function lib.tier1_weapon ()
+   if not tier1_weapon then
+      tier1_weapon = {
+         outfit.get("Flak Gun"),
+         outfit.get("Neutralizer"),
+         outfit.get("Reaver Cannon"),
+         outfit.get("Pincushion Battery"),
+      }
+   end
+   return tier1_weapon[ rnd.rnd(1,#tier1_nonweapon) ]
+end
+
+function lib.tier1()
+   if rnd.rnd() < 0.5 then
+      return lib.tier1_nonweapon()
+   end
+   return lib.tier1_weapon()
+end
+
+return lib
