4302862e026280b649061c9f4025807a5ec4aa46
johann1764 <johann.brault_baron@yahoo.com>
2025-06-03 12:39:39 +0200

    shellcheck utils/outfits/*.sh

--- utils/outfits/gen_mvx_from_xml.sh
+++ utils/outfits/gen_mvx_from_xml.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DST=$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/outfits")
 
 echo Generate from "$DST" >&2
-for i in $(grep -rl 'lua_inline' "${DST}" | grep '\.xml$') ; do
+grep -rl 'lua_inline' "${DST}" | grep '\.xml$' | while read -r i ; do
    "${SCRIPT_DIR}/xmllua2mvx.py" "$i" > "$(dirname "$i")/.$(basename "$i" xml)mvx"
 done
