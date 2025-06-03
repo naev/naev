ae94e07224218c97a7e4b5e40a9d4463b3c307e0
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-03 18:37:37 +0900

    [pre-commit run -a]

--- src/dev_uniedit.c
+++ src/dev_uniedit.c
@@ -1639,30 +1639,32 @@ static int uniedit_checkName( const char *name )
 }
 
 // s/_\([xvi][xvi]*\)-\([a-z]\)$/_\1\2/;
-static size_t _parse_it(const char*s){
+static size_t _parse_it( const char *s )
+{
    size_t out;
 
-   for(out = 0 ; s[out]=='x' || s[out]=='v' || s[out]=='i'; out++);
-   if(s[out]=='-' && s[out+1]>='a' && s[out+1]<='z'
-         && (!s[out+2] || s[out+2]=='\n')
-   )
-      return out+1;
+   for ( out = 0; s[out] == 'x' || s[out] == 'v' || s[out] == 'i'; out++ )
+      ;
+   if ( s[out] == '-' && s[out + 1] >= 'a' && s[out + 1] <= 'z' &&
+        ( !s[out + 2] || s[out + 2] == '\n' ) )
+      return out + 1;
    else
       return 0;
 }
 
-char *uniedit_nameFilter( const char *name ){
+char *uniedit_nameFilter( const char *name )
+{
    size_t len = strlen( name ) + 1;
    char  *out = malloc( len );
    size_t r, w = 0;
-   int res;
+   int    res;
 
    // s/ /_/g;
-   for(r = 0; r <= len ; r++)
-      if (name[r]==' ')
+   for ( r = 0; r <= len; r++ )
+      if ( name[r] == ' ' )
          out[r] = '_';
-      else if (name[r]>='A' && name[r]<='Z')
-         out[r] = name[r] + ('a'-'A');
+      else if ( name[r] >= 'A' && name[r] <= 'Z' )
+         out[r] = name[r] + ( 'a' - 'A' );
       else
          out[r] = name[r];
 
@@ -1670,36 +1672,34 @@ char *uniedit_nameFilter( const char *name ){
    // s/&amp;/_and_/g;
    // s/-\([0-9]\)/\1/g;
    // s/_\([xvi][xvi]*\)-\([a-z]\)$/_\1\2/;
-   for(r = 0; r < len ; )
-      if (strchr("':.()?", out[r]))
+   for ( r = 0; r < len; )
+      if ( strchr( "':.()?", out[r] ) )
          r++;
-      else if (!strncmp(out+r, "&amp;", 5)){
-         memcpy(out+w, "_and_", 5);
+      else if ( !strncmp( out + r, "&amp;", 5 ) ) {
+         memcpy( out + w, "_and_", 5 );
          w += 5;
          r += 5;
-      }else if (out[r]=='-' && out[r+1]>='0' && out[r+1]<='9' )
+      } else if ( out[r] == '-' && out[r + 1] >= '0' && out[r + 1] <= '9' )
          r++;
-      else if (out[r]=='_' && (res = _parse_it(out+r+1))){
-         memmove(out+w, out+r, res);
+      else if ( out[r] == '_' && ( res = _parse_it( out + r + 1 ) ) ) {
+         memmove( out + w, out + r, res );
          w += res;
          r += res + 1;
-      }else
+      } else
          out[w++] = out[r++];
 
    out[w] = '\0';
-   len = w;
-   w = 0;
+   len    = w;
+   w      = 0;
    // s/_-/-/g;
    // s/-_/-/g;
    // s/\(._\)_*/\1/g;
-   for(r=0; r<len; r++)
-      if (out[r]=='_' && out[r+1]=='-')
-         {}
-      else if (out[r]=='-' && out[r+1]=='_')
+   for ( r = 0; r < len; r++ )
+      if ( out[r] == '_' && out[r + 1] == '-' ) {
+      } else if ( out[r] == '-' && out[r + 1] == '_' )
          out[w++] = out[r++];
-      else if(r && out[r] == '_' && out[r+1] == '_')
-         {}
-      else
+      else if ( r && out[r] == '_' && out[r + 1] == '_' ) {
+      } else
          out[w++] = out[r];
    out[w] = '\0';
    return out;
--- utils/apply_renaming_suggestion.sh
+++ utils/apply_renaming_suggestion.sh
@@ -7,15 +7,14 @@ tmp=$(mktemp)
 trap 'rm -f $tmp' EXIT
 
 sed 's/^\(.*\){\(.*\) => \(.*\)\}.xml$/\1 \2.xml \3.xml/' |
-while read line; do
+while read -r line; do
    path=$(echo "$line" | cut '-d ' -f 1)
    from=$(echo "$line" | cut '-d ' -f 2)
    to=$(echo "$line" | cut '-d ' -f 3)
    git mv "$path$from" "$path$to" &&
    echo "s/\/$from/\/$to/" >> "$tmp" &&
    echo "/$from"
-done | for i in $(grep -rlf - $DST/po $DST/dat 2>/dev/null) ; do
+done | for i in $(grep -rlf - "$DST"/po "$DST"/dat 2>/dev/null) ; do
    echo "$i" >&2
    sed -f "$tmp" -i "$i"
 done
-
