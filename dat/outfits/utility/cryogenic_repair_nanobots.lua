0c7b03dabe8a6f46ba8ab42cbd034b69d9b908b7
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-07 00:43:03 +0900

    Cryogenic repair nanobots don't give 100% armour at the end of normal cycle, and also regenerate based on total armour instead of missing armour.

--- dat/outfits/utility/cryogenic_repair_nanobots.lua
+++ dat/outfits/utility/cryogenic_repair_nanobots.lua
@@ -1,21 +1,26 @@
 notactive = true
 
 function init( _p, po )
+   mem.a = nil
    po:clear()
 end
 
 function cooldown( p, po, done, opt )
    if not done then
-      -- Initialize repairs, we calculate how much is needed to repair to full
-      local a = p:health(true)
+      -- Initialize repairs, we calculate how much is needed to repair to full from zero
+      mem.a = p:armour(true)
       local ps = p:stats()
-      local regen = (ps.armour - a) / opt
+      local regen = ps.armour / opt
       po:set( "armour_regen_malus", -regen )
    else
       -- We force set it to 100% here in the case of outfits that trigger full cycles
       if opt then
-         local _a, s = p:health()
-         p:setHealth( 100, s )
+         -- Really bad way of checking, but if starting health is the same as ending health,
+         -- we assume it's run in row and thus doing single cycle and needs full heal.
+         local a, s, stress = p:health(true)
+         if mem.a==a then
+            p:setHealthAbs( a, s, stress )
+         end
       end
       -- Stop repairing
       po:clear()
