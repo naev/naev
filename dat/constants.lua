ce3d271d546049be2e1c5cea9976a544c9e0da31
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-07 15:26:33 +0900

    Simplify constant definition.

--- dat/constants.lua
+++ dat/constants.lua
@@ -11,9 +11,7 @@ return {
    PILOT_SHIELD_DOWN_TIME = 5, -- Time shield is down after being knocked down to 0
    PILOT_DISABLED_ARMOUR= 0.1, -- Armour rate at which the ship is disabled (was 0 before 0.13.0)
 
-   -- Constants used by Lua scripts
-   --BITE_ACCEL_MOD = 500,
-   --BITE_SPEED_MOD = 80,
-   BITE_ACCEL_MOD = 390, -- like hades_torch
-   BITE_SPEED_MOD = 110, -- like adrenal_glands_ii
+   -- Constants used by multiple Lua scripts
+   BITE_ACCEL_MOD = 390, -- like hades_torch (was 500 before 0.13.0)
+   BITE_SPEED_MOD = 110, -- like adrenal_glands_ii (was 80 before 0.13.0)
 }
