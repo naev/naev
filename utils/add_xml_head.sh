3dc90104c83bb3e3259b26ef790d93727289c5a7
johann1764 <johann.brault_baron@yahoo.com>
2025-06-08 14:27:07 +0200

    fixed pre-commit hook + rem_xml_head now cleans spob & ssys by default

--- utils/add_xml_head.sh
+++ utils/add_xml_head.sh
@@ -8,6 +8,6 @@ if [ "$*" = "" ] ; then
    args+=("$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../dat")")
 fi
 
-grep -L '^<?xml\ version' "${args[@]}" --include "*.xml" | while read -r i; do
+grep -L '^<?xml version' "${args[@]}" --include "*.xml" | while read -r i; do
    sed -i '1 i\<?xml version="1.0" encoding="UTF-8"?>' "$i"
 done
--- utils/rem_xml_head.sh
+++ utils/rem_xml_head.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 args=("$@")
 if [ "$*" = "" ] ; then
    args+=("-r")
-   args+=("$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../dat")")
+   path=$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../dat")
+   args+=("$path/spob")
+   args+=("$path/ssys")
 fi
 
-grep -l '^<?xml\ version' "${args[@]}" --include "*.xml" | while read -r i; do
+grep -l '^<?xml version' "${args[@]}" --include "*.xml" | while read -r i; do
    sed '0,/^<?xml\ version=\(["'\'']\)1\.0\1\ encoding=\(["'\'']\)\(\(utf\)\|\(UTF\)\)-\?8\2?>$/{//d}' -i "$i"
 done
