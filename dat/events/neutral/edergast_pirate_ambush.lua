c2f14dcf75393fd2868adca956adac63a0a15d49
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-09 01:32:34 +0900

    Erdegast pirate ambush: just need a proper reward and testing.

--- dat/events/neutral/edergast_pirate_ambush.lua
+++ dat/events/neutral/edergast_pirate_ambush.lua
@@ -177,6 +177,29 @@ function edergast_pirate_ambush_found( aborted )
       return
    end
 
+   -- TODO better reward
+   local REWARD = outfit.get("Berserk Chip")
+
+   vn.reset()
+   vn.scene()
+   vn.transition()
+   vn.na(_([[As you approach what you believe to be the location marked in the treasure map, you scanners pick up something in low orbit.]]))
+   vn.na(fmt.f(_([[You slowly approach the object while hoping it's not another ambush. Luckily enough, you pick up nothing strange as you near what seems to be some sort of capsule orbiting {spob}.]]),
+      {spob=spob.cur()}))
+   vn.na(fmt.f(_([[You manage to pry open the capsule, and inside you find some weird pirate king fanfics, but more importantly, you see a {reward}. You flip through some of the fanfic, it doesn't seem to be too original or interesting, so you leave that, the {reward} on the other hand seems like something you should come with you.]]),
+      {reward=REWARD}))
+   vn.na(fmt.f(_([[You take the {reward}, but leave the fanfic for the next person to find the capsule. You can imagine they'll be very happy.]]),
+      {reward=REWARD}))
+   vn.sfxVictory()
+   vn.func( function ()
+      player.outfitAdd(REWARD)
+   end )
+   vn.na(fmt.reward(REWARD))
+   vn.run()
+
+   th.log(fmt.f(_([[You followed a treasure map you found on {start} to {goal}, and found a nice reward of {reward}.]]),
+      {start=SPB, goal=system.cur(), reward=REWARD}))
+
    -- And done here :D
    evt.finish(true)
 end
