3ff9a1b78bd28021bbfed02c3749c21d8fa3e925
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-10 13:18:37 +0900

    Added outfit helper support for manipulating core outfit data.

--- dat/outfits/py/helper.py
+++ dat/outfits/py/helper.py
@@ -1,5 +1,10 @@
 import sys
 import xmltodict
+from os import path
+script_dir = path.join(path.dirname(__file__), '..', '..', '..', 'utils', 'outfits')
+sys.path.append(path.realpath(script_dir))
+from core_outfit import core_outfit
+from outfit import outfit
 
 INPUT = sys.argv[1]
 OUTPUT = sys.argv[2]
@@ -10,7 +15,10 @@ def read():
 
 def write( data ):
     with open(OUTPUT,'w') as f:
-        f.write( xmltodict.unparse( data, pretty=True ) )
+        f.write( tostring(data) )
+
+def tostring( data ):
+    return xmltodict.unparse( data, pretty=True )
 
 def mul_i( d, f, v ):
     d[f] = str(round(float(d[f])*v))
@@ -20,3 +28,29 @@ def add_i( d, f, v ):
 
 def mul_f( d, f, v ):
     d[f] = str(float(d[f])*v)
+
+def get_outfit_dict( name, core = False ):
+   try:
+      if core:
+         o = core_outfit(name, try_again = True, quiet = True)
+      else:
+         o = outfit(name)
+   except:
+      raise Exception('Could not read "' + path.basename(name) + '"')
+   return o.to_dict()
+
+def to_multicore_lua( ref, pri_only = True ):
+    out = 'require("outfits.lib.multicore").init{'
+    specific = False
+    for r in ref:
+        if not specific:
+            if r=='specific':
+                specific = True
+        else:
+            v = ref[r]
+            if not pri_only and len(v) > 1:
+                out += f'\n   {{"{r}", {v[0]}, {v[1]} }},'
+            else:
+                out += f'\n   {{"{r}", {v[0]} }},'
+    out += '\n}'
+    return out
