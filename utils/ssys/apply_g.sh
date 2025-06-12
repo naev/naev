d57d7c4b543fa026ea78851795e672dc0b6574c4
johann1764 <johann.brault_baron@yahoo.com>
2025-06-12 07:11:39 +0200

    fixed apply_g.sh

--- utils/ssys/apply_g.sh
+++ utils/ssys/apply_g.sh
@@ -19,4 +19,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ ! -f "$SCRIPT_DIR"/gravity ] || [ ! "$SCRIPT_DIR"/gravity -nt "$SCRIPT_DIR"/gravity.c ] ; then
    gcc -Wall -Wextra -Ofast "$SCRIPT_DIR"/gravity.c -o "$SCRIPT_DIR"/gravity -lm || exit 1
 fi
-"$SCRIPT_DIR"/ssys_graph.py | repiper "$N" "$SCRIPT_DIR"/gravity -a | "$SCRIPT_DIR"ssys_graph.py -w "$RESCALE"
+"$SCRIPT_DIR"/ssys_graph.py | repiper "$N" "$SCRIPT_DIR"/gravity -a | "$SCRIPT_DIR"/ssys_graph.py -w "$RESCALE"
