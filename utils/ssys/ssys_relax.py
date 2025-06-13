33e4ef1951bccff23348aa9b339b082f9453153c
johann1764 <johann.brault_baron@yahoo.com>
2025-06-13 15:43:40 +0200

    ssys_relax: also try flipping when 2 jumps && warnings

--- utils/ssys/ssys_relax.py
+++ utils/ssys/ssys_relax.py
@@ -43,40 +43,51 @@ def ssys_relax( sys, quiet = True, graph = False ):
       flip = nop
       pi1, pi2 = mk_p(mapvs), mk_p(sysvs)
       # more than max possible cost <= 2.0
-      uf_cost = 3.0
       eps = 0.0001
       out = sys.replace('.xml', '') if graph else None
-      wrn = ''
 
-      if pi1 == pi2:
-         # same permutation -> everything is fine
-         pass
+      wrn = []
+
+      if pi1 == pi2: # same permutation -> everything is fine
+         # If only two jumps, we can flip to try improving the result.
+         try_flipped, try_unflipped = (len(pi1) == 2), True
       else:
-         flip = lambda sysv: vec(-sysv[0], sysv[1])
          if pi1 == pi2[:1] + pi2[1:][::-1]:
-            wrn += '\033[32m"' + basename(sys) + '" flipped !\033[0m\n'
+            wrn += [('2', 'flipped')]
+            try_flipped, try_unflipped = True, False
          else:
-            wrn += '\033[33m"' + basename(sys) + '" crossed : '
             pi1 = [names[i] for i in pi1]
             pi2 = [names[i] for i in pi2]
-            wrn += ', '.join(pi1) + ' -> ' + ', '.join(pi2) + '\033[0m'
-            wrn += '\n' if not quiet else ''
-            outu = None if out is None else (out + '_u')
-            uf_alpha, uf_cost = relax_dir(sysvs, mapvs, eps = eps/10.0, debug = outu, quiet = quiet)
+            wrn += [('3', 'crossed: ' + ', '.join(pi1) + ' -> ' + ', '.join(pi2))]
+            try_flipped, try_unflipped = True, True
 
-      alpha, cost = relax_dir([flip(v) for v in sysvs], mapvs, eps = eps/10.0, debug = out, quiet = quiet )
-      if uf_cost < 3.0:
-         if cost > uf_cost:
-            cost, alpha, flip = uf_cost, uf_alpha, nop
-            wrn += ' \033[33m[better unflipped]\033[0m\n'
-         else:
-            wrn += ' \033[33m[better flipped]\033[0m\n'
+      if try_unflipped:
+         alpha, cost = relax_dir(sysvs, mapvs,
+            eps = eps/10.0, debug = out, quiet = quiet)
+      else:
+         alpha, cost = 360.0, 3.0
+
+      if try_flipped:
+         outf = None if out is None else (out + '_f')
+         _flip = lambda v: vec(-v[0], v[1])
+         alpha_f, cost_f = relax_dir([_flip(v) for v in sysvs], mapvs,
+            eps = eps/10.0, debug = outf, quiet = quiet)
+
+         if cost_f < cost:
+            alpha, cost, flip = alpha_f, cost_f, _flip
+
+      if try_flipped and try_unflipped:
+         op = 'unflipped' if flip is nop else 'flipped'
+         col = '3' if len(pi1) > 2 else '2'
+         if op == "flipped" or col == 3:
+            wrn += [(col, '[better ' + op + ']')]
 
       if cost > 0.5:
-         wrn += '\033[33m"' + basename(sys) + '" : badness ' + str((int)((100*cost)/2)) + '%\033[0m\n'
+         wrn += [('3', 'badness ' + str((int)((100*cost)/2)) + '%')]
 
-      if wrn != '':
-         stderr.write(wrn)
+      if wrn != []:
+         wrn[0] = (wrn[0][0], '"' + basename(sys) + '": ' + wrn[0][1])
+         stderr.write(''.join(['\033[3'+str(i)+'m' + j + '\033[0m\n' for i, j in wrn]))
          stderr.flush()
 
       if abs(alpha) > eps or flip != nop:
