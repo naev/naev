6f179de1d81dae010dc68d41e23dc82eb8ca13b6
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-14 02:16:03 +0900

    More fleshed out with boss fight and such.

--- dat/events/neutral/haven_curse.lua
+++ dat/events/neutral/haven_curse.lua
@@ -11,12 +11,15 @@ local pp_shaders = require 'pp_shaders'
 local lmisn = require "lmisn"
 local pilotai = require "pilotai"
 local lg = require "love.graphics"
+local equipopt = require "equipopt"
+local luaspfx = require "luaspfx"
+local vn = require "vn"
+local fmt = require "format"
 
 local spb, sys = spob.getS("Old Man Jack")
 local pos = sys:waypoints("haven_curse_spawn")
 
 function create ()
-   evt.finish(false) -- Disabled for now
    if not evt.claim( sys, true ) then return evt.finish(false) end
 
    hook.timer( 1, "heartbeat" )
@@ -68,12 +71,12 @@ function heartbeat ()
          else
             spin_elapsed = spin_elapsed + diff
             spin_last = a
-            if spin_elapsed > math.pi * 6 then
+            if spin_elapsed > 0 then --math.pi * 6 then
                local p = pilot.add( "Pirate Hyena", "Derelict", pos, _("Mysterious Derelict"), {naked=true} )
                p:setHealth( 37, 0 )
                p:setDisable(true)
                p:effectAdd("Fade-In")
-               p:setHighlight()
+               p:setHilight()
                hook.pilot( p, "exploded", "der_destroyed" )
                hook.pilot( p, "board", "der_boarded" )
                if noise_shader then
@@ -82,6 +85,7 @@ function heartbeat ()
                derelict = p
                lmisn.sfxEerie()
                pilotai.clear()
+               pilot.toggleSpawn(false)
                return -- done
             elseif spin_elapsed > math.pi * 4 then
                if noise_shader then
@@ -103,6 +107,7 @@ function heartbeat ()
    hook.timer( 0.1, "heartbeat" )
 end
 
+local boss_music
 local boss
 local fade_factor = 0
 local fade_growth = 1/9
@@ -113,6 +118,13 @@ function spawn_final ()
    boss:effectAdd("Fade-In Black")
    boss:control(false)
    boss:setHostile(true)
+   boss:setInvisible(false)
+   shader.rmPPShader( noise_shader )
+
+   -- Proper boss music
+   boss_music = audio.new( "snd/music/blackmoor_tides.ogg", "stream" )
+   boss_music:play()
+   boss_music:setLooping(true)
 end
 
 function spawn_flash2 ()
@@ -134,10 +146,26 @@ function spawn_start3 ()
    shader.addPPShader( noise_shader )
    hook.timer( 1, "spawn_flash1" )
 
-   boss = pilot.add( "Pirate Kestrel", "Marauder", pos, _("Defiance"), {ai="baddie", naked=true} )
-   boss:control()
-   boss:setInvisible( true )
-   boss:effectAdd("Black")
+   local p = pilot.add( "Pirate Kestrel", "Marauder", pos, _("Defiance"), {ai="baddie", naked=true} )
+   p:control()
+   p:setInvisible( true )
+   p:setNoDeath()
+   p:effectAdd("Black")
+   p:intrinsicSet( "shield_mod", 80 )
+   p:intrinsicSet( "armour_mod", 60 )
+   p:intrinsicSet( "shield_regen_mod", 50 )
+   p:intrinsicSet( "tur_damage", 25 )
+   p:intrinsicSet( "fbay_rate", 500 )
+   p:intrinsicSet( "fbay_capacity", 100 )
+   equipopt.pirate( p, {
+      fighterbay = 10,
+   } ) -- So intrinsics affect
+   local m = p:memory()
+   m.comm_greet = _([[You hear the sound of oceans and wild over the communication channel.]])
+   m.taunt = nil
+   m.bribe_no = _([[The wind is howling over the communication channel.]])
+
+   boss = p
 end
 
 function spawn_start2 ()
@@ -165,6 +193,7 @@ local function spawn_start ()
    hook.update( "update" )
    fade_factor = 0
    fade_growth = 1/9
+   music.stop(true)
 end
 
 function fade ()
@@ -175,8 +204,79 @@ function fade ()
    end
 end
 
+local boss_stage = 0
 function update( dt )
    fade_factor = math.min( 1, fade_factor + dt * fade_growth )
+   if boss and boss:exists() then
+      if boss:shield() <= 0 or boss:disabled() then
+         if boss_stage < 2 then
+            local bpos = boss:pos()
+            boss:setHealth( 100-35*boss_stage, 100 )
+            luaspfx.blink( boss, bpos )
+            boss:effectAdd("Blink")
+            boss:outfitRm("all")
+            local ships
+            if boss_stage==0 then
+               ships = {
+                  "Pirate Shark",
+                  "Pirate Vendetta",
+                  "Pirate Ancestor",
+               }
+               equipopt.pirate( boss, {
+                  beam = 10,
+                  turret = 10,
+               } ) -- So intrinsics affect
+            elseif boss_stage==1 then
+               ships = {
+                  "Pirate Admonisher",
+                  "Pirate Vendetta",
+                  "Pirate Phalanx",
+               }
+               equipopt.pirate( boss, {
+                  launcher = 10,
+               } ) -- So intrinsics affect
+            end
+            -- "Launch" some new fighters that sort of "pop" out
+            for i,s in ipairs(ships) do
+               local p = pilot.add( s, "Marauder", boss:pos() )
+               p:setHostile(true)
+               p:setLeader(boss)
+               local a = rnd.angle()
+               p:setDir( a )
+               p:setVel( vec2.newP( 500, a ) )
+               p:effectAdd( "Fade-In" )
+            end
+            -- Behind player
+            boss:setPos( player.pos() + vec2.newP( 2000, player.pilot():dir()+math.pi ) )
+         else
+            -- Player won
+            boss:setDisable(true)
+            boss:setInvisible(true)
+            -- Get rid of followers
+            for k,f in ipairs(boss:followers()) do
+               if f:exists() then
+                  f:effectAdd( "Fade-Out" )
+               end
+            end
+            hook.pilot( boss, "board", "boss_board" )
+            pilot.toggleSpawn(true)
+            hook.update("music_fadeout")
+         end
+      end
+   end
+end
+
+local music_fade = 1
+function music_fadeout( dt )
+   if boss_music then
+      music_fade = music_fade - dt*0.5
+      if music_fade < 0 then
+         boss_music:stop()
+         boss_music = nil
+      else
+         boss_music:setVolume( music_fade )
+      end
+   end
 end
 
 function der_destroyed ()
@@ -187,4 +287,20 @@ function der_boarded ()
    player.unboard()
    derelict:setInvisible(true)
    derelict:effectAdd("Fade-Out")
+   player.msg(_([[The ship seems to disappear as you board it.]]))
+   spawn_start()
+end
+
+function boss_board ()
+   player.unboard()
+
+   vn.reset()
+   vn.scene()
+   vn.transition()
+   vn.na(fmt.f(_([[Against your better judgement, you board the mysterious {shipname}.]]),
+      {shipname=boss:name()}))
+   vn.run()
+
+   spb:landDeny(false)
+   evt.finish(true)
 end
