28b7f835b338581ef8105790e23ec1160a4fe3ea
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-14 17:57:12 +0900

    Added fadeout_norender effect.

---
+++ dat/effects/lua/fadeout_norender.lua
@@ -0,0 +1,14 @@
+function add( p )
+   p:setInvincible(true)
+   p:setInvisible(true)
+end
+
+function remove( p )
+   if player.pilot()~=p then -- and p:exists() then
+p:rm()
+      p:setNoRender()
+   else
+      p:setInvincible(false)
+      p:setInvisible(false)
+   end
+end
