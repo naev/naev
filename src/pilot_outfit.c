4cd2f8be7557cdcdec4652d036f3db21837496b3
Edgar Simo-Serra <ess@waseda.jp>
2025-06-16 16:52:26 +0900

    Run calcstats after doing a Lua message.

--- src/pilot_outfit.c
+++ src/pilot_outfit.c
@@ -2482,12 +2482,14 @@ void pilot_outfitLCleanup( Pilot *pilot )
 int pilot_outfitLMessage( Pilot *pilot, PilotOutfitSlot *po, const char *msg,
                           int data )
 {
-   int           oldmem;
+   int           oldmem, modified;
    const Outfit *o = po->outfit;
    if ( o->lua_message == LUA_NOREF )
       return 0;
 
-   nlua_env env = o->lua_env;
+   nlua_env env         = o->lua_env;
+   modified             = pilotoutfit_modified;
+   pilotoutfit_modified = 0;
 
    /* Set the memory. */
    oldmem = pilot_outfitLmem( po, env );
@@ -2508,6 +2510,14 @@ int pilot_outfitLMessage( Pilot *pilot, PilotOutfitSlot *po, const char *msg,
       ret = 0;
    }
    pilot_outfitLunmem( env, oldmem );
+
+   /* Recalculate if anything changed. */
+   if ( pilotoutfit_modified ) {
+      /* TODO pilot_calcStats can be called twice here. */
+      pilot_weapSetUpdateOutfitState( pilot );
+      pilot_calcStats( pilot );
+   }
+   pilotoutfit_modified = modified;
    return ret;
 }
 
