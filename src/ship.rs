f1b35ada4b32f1114cf8e4859140de0237ec8e7f
Edgar Simo-Serra <bobbens@gmail.com>
2025-06-20 23:08:03 +0900

    Simplify things a bit.

--- src/ship.rs
+++ src/ship.rs
@@ -5,16 +5,26 @@ struct ShipWrapper(naevc::Ship);
 //unsafe impl Sync for ShipWrapper {}
 unsafe impl Send for ShipWrapper {}
 
+#[allow(dead_code)]
+fn get() -> &'static [ShipWrapper] {
+    unsafe {
+        let ships = naevc::ship_getAll();
+        let n = naevc::array_size_rust(ships as *const c_void) as usize;
+        std::slice::from_raw_parts(ships as *const ShipWrapper, n)
+    }
+}
+
+fn get_mut() -> &'static mut [ShipWrapper] {
+    unsafe {
+        let ships = naevc::ship_getAll();
+        let n = naevc::array_size_rust(ships as *const c_void) as usize;
+        std::slice::from_raw_parts_mut(ships as *mut ShipWrapper, n)
+    }
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn ship_gfxLoadNeeded_() {
-    let (ships, n) = unsafe {
-        let ss = naevc::ship_getAll();
-        (ss, naevc::array_size_rust(ss as *const c_void) as usize)
-    };
-
-    let ss: &mut [ShipWrapper] =
-        unsafe { std::slice::from_raw_parts_mut(ships as *mut ShipWrapper, n) };
-    ss.par_iter_mut().for_each(|ptr| {
+    get_mut().par_iter_mut().for_each(|ptr| {
         let s = &mut ptr.0;
         if s.flags & naevc::SHIP_NEEDSGFX > 0 {
             // TODO oad graphics here
