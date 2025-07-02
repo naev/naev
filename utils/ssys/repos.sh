a3a79fd520f4248aa989adf340b78fe622dfa258
johann1764 <johann.brault_baron@yahoo.com>
2025-07-02 11:50:55 +0200

    repos.sh && app_pot.sh - expressed as graph mod & simplified

--- utils/ssys/repos.sh
+++ utils/ssys/repos.sh
@@ -4,12 +4,14 @@
 FLAGS=("-Ofast")
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ] ; then
-   echo "usage:  $(basename "$0") [<#iterations>|-C] [reposition args]" >&2
-   echo "  If -C is set, just compile reposition." >&2
-   echo "  Applies reposition <#iterations> times (or once if not provided)" >&2
-   echo "  <#iterations> might be 0 (just output current ssys positions).">&2
-   echo "  Use reposition -h to get info on reposition args." >&2
-   echo "  reposition args -e and -o are managed even if <#iterations> is 0." >&2
+   (
+      echo "usage:  $(basename "$0") [<#iterations>|-C] [reposition args]"
+      echo "  If -C is set, just compile reposition."
+      echo "  Applies reposition <#iterations> times (or once if not provided)"
+      echo "  <#iterations> might be 0 (just output current ssys positions)."
+      echo "  Use reposition -h to get info on reposition args."
+      echo "  reposition arg -o is managed even if <#iterations> is 0."
+   ) >&2
    exit 0
 fi
 
@@ -27,11 +29,11 @@ else
 fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-read -ra FLG <<< "$(pkg-config glib-2.0 --cflags)"
-read -ra LIB <<< "$(pkg-config glib-2.0 --libs)"
-
 if [ ! -f "$SCRIPT_DIR"/reposition ] || [ ! "$SCRIPT_DIR"/reposition -nt "$SCRIPT_DIR"/reposition.c ] || [ ! "$SCRIPT_DIR"/reposition -nt "$SCRIPT_DIR"/repos.sh ] ; then
    echo -n 'Compile.. ' >&2
+
+   read -ra FLG <<< "$(pkg-config glib-2.0 --cflags)"
+   read -ra LIB <<< "$(pkg-config glib-2.0 --libs)"
    gcc -Wall -Wextra -Werror -Og "${FLAGS[@]}" "$SCRIPT_DIR"/reposition.c -o "$SCRIPT_DIR"/reposition\
       -I/usr/include/glib-2.0 \
       "${FLG[@]}"             \
@@ -45,33 +47,15 @@ if [ "$N" = "C" ] ; then
 fi
 echo "Apply reposition $N time(s)." >&2
 
-TMP=$(mktemp)
-trap 'rm -f "$TMP"' EXIT
-
 repeat() {
    local -i n="$1";
-   n="$((n-1))"
    shift;
-   cat - "$TMP" | "$SCRIPT_DIR"/reposition -q "$@" |
-   (
-      if (( n )); then
-         repeat "$n" "$@"
-      else
-         cat
-      fi
-   )
-}
-
-"$SCRIPT_DIR"/ssys2graph.sh -v |
-(
-   if [ "$N" -gt "0" ]; then
-      cat - <("$SCRIPT_DIR"/ssys2graph.sh -e | tee "$TMP") |
-      "$SCRIPT_DIR"/reposition -q "$@" |
-      (if (( N-1 )); then repeat "$((N-1))" "$@"; else cat; fi)
+   if [ ! "$n" = "0" ] ; then
+      n="$((n - 1))"
+      "$SCRIPT_DIR"/reposition "$@" | repeat "$n" "$@"
    elif [ ! "$1" = "-o" ] ; then
       cat
-      if [ "$1" = "-e" ] ; then
-         "$SCRIPT_DIR"/ssys2graph.sh -e
-      fi
    fi
-)
+}
+
+repeat "$N" -e -q "$@"
