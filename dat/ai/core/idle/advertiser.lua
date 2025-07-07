7779375e93cb6132371046631a65f728061cb274
Edgar Simo-Serra <bobbens@gmail.com>
2025-07-07 23:50:01 +0900

    Advertisers support advertising functions.

--- dat/ai/core/idle/advertiser.lua
+++ dat/ai/core/idle/advertiser.lua
@@ -14,15 +14,21 @@ control_funcs.loiter = function ()
    local delay = mem.adspamdelay or (mem.adspamdelayalpha + mem.adspamdelaybeta*rnd.rnd())
 
    if curtime - lastspammed > delay then
+      local p = ai.pilot()
       local ad = mem.ad
-      if type(ad) == "table" then -- If table we just cycle through them from random start
+      local tad = type(ad)
+      if tad == "table" then -- If table we just cycle through them from random start
          mem.adid = mem.adid or rnd.rnd(1,#ad)
          mem.adid = math.fmod( mem.adid, #ad )+1
          ad = ad[ mem.adid ]
+      elseif tad == "function" then
+         ad = ad( p )
       end
 
       -- Broadcast for all to hear!
-      ai.pilot():broadcast( ad )
+      if ad then
+         p:broadcast( ad )
+      end
 
       -- Delay again
       mem.adspamlast = curtime
