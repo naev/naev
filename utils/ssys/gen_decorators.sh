33fd7dd357de0445f8e0fd66a306da12cdb5ae4f
johann1764 <johann.brault_baron@yahoo.com>
2025-07-09 10:16:59 +0200

    process_sys -v -H -n, graph2pov -d -H -n, graphmod catches broken pipe / intr.

--- utils/ssys/gen_decorators.sh
+++ utils/ssys/gen_decorators.sh
@@ -2,30 +2,35 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PIC="$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../artwork/gfx/map")"
-#MAP="$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/map_decorator")"
+MAP="$(realpath --relative-to="$PWD" "${SCRIPT_DIR}/../../dat/map_decorator")"
 
 if [ -f "decorators.inc" ] ; then
    echo '"decorators.inc" already exists!'
    exit 0
 fi
 
-for i in "$PIC"/*.webp; do
-   res=$(identify -verbose "$i" | grep -m 1 'geometry:')
+mkdir -p "decorators"
+
+grep "<image>" "$MAP"/*.xml | sed 's/^.*<image>\(.*\)<\/image>.*$/\1/' |
+while read -r picnam ; do
+   pic="$PIC"/"$picnam"
+   bas="${picnam%.webp}"
+   OUT=decorators/$bas.png
+   res=$(identify -verbose "$pic" | grep -m 1 'geometry:')
    if [ "$res" != "" ] ; then
-      #xml="$MAP/$(basename "${i%.webp}")".xml
-      bas="$(basename "${i%.webp}")"
       # shellcheck disable=SC2001
       geom=$(sed "s/^.*geometry: \([0-9]*x[0-9]*\).*$/\1/" <<< "$res")
       W="$(echo "$geom" | cut -dx -f1)"
       H="$(echo "$geom" | cut -dx -f2)"
-      echo "$bas.png $W $H" >&2
-      if [ ! -f "$bas".png ] || [ "$i" -nt "$bas.png" ] ; then
-         convert "$i" "$bas".png
+      echo -n "$OUT $W $H" >&2
+      if [ ! -f "$OUT" ] || [ "$pic" -nt "$OUT" ] ; then
+         convert "$pic" "$OUT"
+      else
+         echo -n "[already here]" >&2
       fi
+      echo "" >&2
       echo -e "#declare $bas = box{\n\t<0,0,0>\n\t<1,1,-1>"
-      echo -e "\tpigment{image_map{\"$bas.png\"}}"
+      echo -e "\tpigment{image_map{\"$OUT\"}}"
       echo -e "\ttranslate <-0.5,-0.5,0>\n\tscale <-$W,-$H,1>\n}"
    fi
 done > "decorators.inc"
-
-
--- utils/ssys/graphmod.py
+++ utils/ssys/graphmod.py
@@ -37,31 +37,47 @@ class _graph():
 
    def __del__( self ):
       if not self.quiet:
-         self.pos.output()
-         self.jmp.output()
-         for i in stdin:
+         try:
+            self.pos.output()
+            self.jmp.output()
+            for i in stdin:
+               pass
+            print()
+         except KeyboardInterrupt:
+            exit(0)
+         except BrokenPipeError:
+            stderr.write('Broken output pipe. Bye !\n')
+            exit(0)
+         except:
             pass
-         print()
 
 graph = _graph()
 sys_pos = graph.pos
 sys_jmp = graph.jmp
 no_graph_out = lambda:graph.silence()
 
-for inp in stdin:
-   if (line := inp.strip()) != '':
-      l = line.split(' ')
-      try:
-         bname, x, y = tuple((l[:3]))
-         sys_pos[bname] = vec(x, y)
-         sys_pos.aux[bname] = l[3:]
-         continue
-      except:
-         pass
-      bname1, bname2 = tuple(l[:2])
-      sys_jmp[bname1].append((bname2, l[2:]))
-   else:
-      break
+try:
+   for inp in stdin:
+      if (line := inp.strip()) != '':
+         l = line.split(' ')
+         try:
+            bname, x, y = tuple((l[:3]))
+            sys_pos[bname] = vec(x, y)
+            sys_pos.aux[bname] = l[3:]
+            success = True
+         except:
+            success = False
+
+         if not success:
+            bname1, bname2 = tuple(l[:2])
+            sys_jmp[bname1].append((bname2, l[2:]))
+      else:
+         break
+except KeyboardInterrupt:
+   exit(0)
+except BrokenPipeError:
+   stderr.write('Broken input pipe. Bye !\n')
+   exit(0)
 
 if len(sys_pos) == 0 or len(sys_jmp) == 0:
    stderr.write('Empty input ! Bye !\n')
