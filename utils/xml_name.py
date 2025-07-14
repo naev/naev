c501c8a79ef426f31151a6c82c94d76b83de61ab
johann1764 <johann.brault_baron@yahoo.com>
2025-07-14 12:05:32 +0200

    xml_name.py removed PythonSed dep. + **much** faster

--- utils/xml_name.py
+++ utils/xml_name.py
@@ -1,14 +1,10 @@
 # python3
 
-from PythonSed import Sed
-from io import StringIO
-
-
-sed = Sed()
-#sed.regexp_extended = False
-sed.load_script(__file__.replace('.py', '.sed'))
-
+from subprocess import Popen, PIPE
+proc = Popen([__file__.replace('.py', '.sed'), '-u'], stdin=PIPE, stdout=PIPE)
 
 def xml_name( s ):
-   input_f = StringIO(s.lower())
-   return "\n".join(sed.apply(input_f, output = None)).rstrip('\n')
+   line = s.strip().lower() + '\n'
+   proc.stdin.write(line.encode('utf-8'))
+   proc.stdin.flush()
+   return proc.stdout.readline().decode('utf-8').strip()
