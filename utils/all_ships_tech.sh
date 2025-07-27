2532b91a07f3b373ad673126fc5eb4f080deb15e
johann1764 <johann.brault_baron@yahoo.com>
2025-07-27 19:08:19 +0200

    filter out stations from ship list && removed tech/all_ships.xml

--- utils/all_ships_tech.sh
+++ utils/all_ships_tech.sh
@@ -2,18 +2,17 @@
 
 
 # forbid '<tag>__excl_tag__</tag>' in file
-#EXCLUDED_TAGS=()
-EXCLUDED_TAGS=('noplayer')
+EXCLUDED_TAGS=('station')
+
 # forbid '__excl_nam_pattern__' in outfit name
-EXCLUDED_NAM_PAT=()
-#EXCLUDED_NAM_PAT=('Astral')
+EXCLUDED_NAM_PAT=('Astral')
 
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DST=$(realpath --relative-to="$PWD" "$DIR"/../dat/ships)
 (
    echo -n '<tech name="All Ships">'
 
-   grep -rho '\(<ship name=\"[^"]*\"\)\|\(<tag>[^<]*</tag>\)' "$DST" |
+   grep -rho '\(<ship name=\"[^"]*\"\)\|\(<tag>[^<]*</tag>\)' "$DST"|
    sed -e 's/^[^"]*"/!/' |
    tr -d $'\n' | tr '!' $'\n' |
    grep -v -f <(
@@ -21,6 +20,7 @@ DST=$(realpath --relative-to="$PWD" "$DIR"/../dat/ships)
          echo '<tag>'"$k"'</tag>$'
       done
    ) |
+   grep -v -f <(IFS=$'\n'; echo -n "${EXCLUDED_NAM_PAT[*]}") |
    sed 's/^\([^"]*\)\".*$/ <item>\1<\/item>/'
 
    echo
