f7101918e3f12223bb21915e3f51ced75caf8ecd
Jack Greiner <jack@emoss.org>
2025-07-28 15:17:39 -0400

    lua_lldb_helpers.py: misc fixes and improve readability a bit
    Should work a bit better, contributions welcome!

--- utils/build/lua_lldb_helpers.py
+++ utils/build/lua_lldb_helpers.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
 # Lua LLDB Helpers
-# Original GDB version by Michal Kottman
+# LLDB port based on the original GDB version by Michal Kottman
 # License: MIT
 
 import lldb
 
+# Utility functions
+def get_uint(expr):
+    return int(expr.GetValueAsUnsigned())
+
+def get_str(expr):
+    return expr.GetSummary().strip('"')
+
+# Command functions
 def luavalue(debugger, command, result, internal_dict):
     args = command.split()
     if len(args) == 0:
@@ -16,9 +24,6 @@ def luavalue(debugger, command, result, internal_dict):
         debugger.HandleCommand(f"expr void* $L = (void*)({L})")
         debugger.HandleCommand(f"expr void* $obj = (void*)index2adr($L, {index})")
 
-lldb.debugger.HandleCommand('command script add -f __main__.luavalue luavalue')
-lldb.debugger.HandleCommand('''command script add -o -h "luavalue <index> [L]\nProvides a pointer to a TValue from a stack index. By default, uses the current variable L as a lua_State pointer, but can be specified as the second argument. The return value is passed in variable $obj." luavalue''')
-
 def luaprint(debugger, command, result, internal_dict):
     args = command.split()
     if len(args) == 0:
@@ -27,49 +32,48 @@ def luaprint(debugger, command, result, internal_dict):
         v = args[0]
         verbose = int(args[1]) if len(args) > 1 else 0
         debugger.HandleCommand(f"expr int $type = ((TValue*){v})->tt")
-        type = int(debugger.GetSelectedTarget().EvaluateExpression("$type").GetValue())
-        if type == 0:
+        type_val = get_uint(debugger.GetSelectedTarget().EvaluateExpression("$type"))
+        if type_val == 0:
             result.AppendMessage("nil")
-        elif type == 1:
-            debugger.HandleCommand(f"expr bool $val = (bool)(((TValue*){v})->value.b)")
-            val = bool(debugger.GetSelectedTarget().EvaluateExpression("$val").GetValue())
+        elif type_val == 1:
+            debugger.HandleCommand(f"expr bool $val = ((TValue*){v})->value.b")
+            val = bool(get_uint(debugger.GetSelectedTarget().EvaluateExpression("$val")))
             result.AppendMessage("true" if val else "false")
-        elif type == 2:
+        elif type_val == 2:
             debugger.HandleCommand(f"expr void* $p = ((TValue*){v})->value.p")
-            p = debugger.GetSelectedTarget().EvaluateExpression("$p").GetValueAsUnsigned()
+            p = get_uint(debugger.GetSelectedTarget().EvaluateExpression("$p"))
             result.AppendMessage(f"<ludata>{p}")
-        elif type == 3:
+        elif type_val == 3:
             debugger.HandleCommand(f"expr double $n = ((TValue*){v})->value.n")
-            n = debugger.GetSelectedTarget().EvaluateExpression("$n").GetValue()
+            n = get_str(debugger.GetSelectedTarget().EvaluateExpression("$n"))
             result.AppendMessage(f"{n}")
-        elif type == 4:
+        elif type_val == 4:
             debugger.HandleCommand(f"expr TString* $ts = (TString*)(((TValue*){v})->value.gc->ts)")
-            ts = debugger.GetSelectedTarget().EvaluateExpression("$ts").GetValue()
+            ts = get_str(debugger.GetSelectedTarget().EvaluateExpression("$ts"))
             result.AppendMessage(f"'{ts}'")
-        elif type == 5:
+        elif type_val == 5:
             debugger.HandleCommand(f"expr Table* $tab = (Table*)(((TValue*){v})->value.gc->h)")
-            tab = debugger.GetSelectedTarget().EvaluateExpression("$tab").GetValue()
+            tab = get_str(debugger.GetSelectedTarget().EvaluateExpression("$tab"))
             result.AppendMessage(f"<tab> {tab}")
             if verbose:
-                luaprinttable(debugger, tab, result, internal_dict)
-        elif type == 7:
+                luaprinttable(debugger, "$tab", result, internal_dict)
+        elif type_val == 7:
             debugger.HandleCommand(f"expr auto $uv = (((TValue*){v})->value.gc.u->uv)")
-            uv = debugger.GetSelectedTarget().EvaluateExpression("$uv").GetValue()
+            uv = get_str(debugger.GetSelectedTarget().EvaluateExpression("$uv"))
             result.AppendMessage(f"<udata> {uv}")
-            if verbose and debugger.GetSelectedTarget().EvaluateExpression("$uv.metatable").IsValid():
-                result.AppendMessage("metatable=")
+            uv_metatable = debugger.GetSelectedTarget().EvaluateExpression("$uv.metatable")
+            if verbose and uv_metatable.IsValid():
+                result.AppendMessage(" metatable=")
                 luaprinttable(debugger, "$uv.metatable", result, internal_dict)
-            if verbose and debugger.GetSelectedTarget().EvaluateExpression("$uv.env").IsValid():
-                result.AppendMessage("env=")
+            uv_env = debugger.GetSelectedTarget().EvaluateExpression("$uv.env")
+            if verbose and uv_env.IsValid():
+                result.AppendMessage(" env=")
                 luaprinttable(debugger, "$uv.env", result, internal_dict)
         else:
-            debugger.HandleCommand(f"expr const char* $typename = lua_typename(L, $type)")
-            typename = debugger.GetSelectedTarget().EvaluateExpression("$typename").GetSummary()
-            val = debugger.GetSelectedTarget().EvaluateExpression(f"((TValue*){v})->value").GetSummary()
+            debugger.HandleCommand("expr const char* $typename = lua_typename(L, $type)")
+            typename = get_str(debugger.GetSelectedTarget().EvaluateExpression("$typename"))
+            val = get_str(debugger.GetSelectedTarget().EvaluateExpression(f"((TValue*){v})->value"))
             result.AppendMessage(f"<{typename}> {val}")
-
-lldb.debugger.HandleCommand('command script add -f __main__.luaprint luaprint')
-lldb.debugger.HandleCommand('''command script add -o -h "luaprint <value> [verbose]\nPretty-prints a TValue passed as argument. Expects a pointer to a TValue. When verbose is 1, expands tables, metatables, and userdata environments." luaprint''')
 
 def luaprinttable(debugger, command, result, internal_dict):
     args = command.split()
@@ -79,46 +83,66 @@ def luaprinttable(debugger, command, result, internal_dict):
         t = args[0]
         result.AppendMessage(" { ")
         debugger.HandleCommand(f"expr auto $node = ((Table*){t})->node")
+        lsizenode_expr = debugger.GetSelectedTarget().EvaluateExpression(f"((Table*){t})->lsizenode")
+        lsizenode = get_uint(lsizenode_expr)
+        last = 1 << lsizenode
         i = 0
-        last = 1 << int(debugger.GetSelectedTarget().EvaluateExpression(f"((Table*){t})->lsizenode").GetValue())
         while i < last:
             debugger.HandleCommand(f"expr auto $node_i = $node + {i}")
-            debugger.HandleCommand(f"expr auto $key = $node_i->i_key")
-            tvk_tt = int(debugger.GetSelectedTarget().EvaluateExpression(f"((TValue*)$key).tt").GetValue())
+            debugger.HandleCommand("expr auto $key = $node_i->i_key")
+            tvk_expr = debugger.GetSelectedTarget().EvaluateExpression("((TValue*)$key)->tt")
+            tvk_tt = get_uint(tvk_expr)
             if tvk_tt > 0:
                 if tvk_tt == 4:
-                    debugger.HandleCommand(f"expr TString* $ts = (TString*)(((TValue*)$key).value.gc->ts)")
-                    ts = debugger.GetSelectedTarget().EvaluateExpression("$ts").GetValue()
+                    debugger.HandleCommand("expr TString* $ts = (TString*)(((TValue*)$key)->value.gc->ts)")
+                    ts = get_str(debugger.GetSelectedTarget().EvaluateExpression("$ts"))
                     result.AppendMessage(f"{ts} = ")
                 else:
                     debugger.HandleCommand(f"expr const char* $typename = lua_typename(L, {tvk_tt})")
-                    typename = debugger.GetSelectedTarget().EvaluateExpression("$typename").GetSummary()
+                    typename = get_str(debugger.GetSelectedTarget().EvaluateExpression("$typename"))
                     result.AppendMessage(f"<{typename}> = ")
-                debugger.HandleCommand(f"expr auto $val = $node_i->i_val")
-                luaprint(debugger, f"$val", result, internal_dict)
+                debugger.HandleCommand("expr auto $val = $node_i->i_val")
+                luaprint(debugger, "$val", result, internal_dict)
                 result.AppendMessage(",\n")
             i += 1
         i = 0
-        sizearray = int(debugger.GetSelectedTarget().EvaluateExpression(f"((Table*){t})->sizearray").GetValue())
+        sizearray_expr = debugger.GetSelectedTarget().EvaluateExpression(f"((Table*){t})->sizearray")
+        sizearray = get_uint(sizearray_expr)
         while i < sizearray:
             debugger.HandleCommand(f"expr auto $val = ((Table*){t})->array + {i}")
-            luaprint(debugger, f"$val", result, internal_dict)
+            luaprint(debugger, "$val", result, internal_dict)
             result.AppendMessage(", ")
             i += 1
         result.AppendMessage(" } ")
-
-lldb.debugger.HandleCommand('command script add -f __main__.luaprinttable luaprinttable')
-lldb.debugger.HandleCommand('''command script add -o -h "luaprinttable <table>\nPretty-prints a Lua Table. Expects a pointer to Table." luaprinttable''')
 
 def luastack(debugger, command, result, internal_dict):
     args = command.split()
     L = args[0] if len(args) > 0 else "L"
     debugger.HandleCommand(f"expr int $stack_size = lua_gettop({L})")
-    stack_size = int(debugger.GetSelectedTarget().EvaluateExpression("$stack_size").GetValue())
+    stack_size = get_uint(debugger.GetSelectedTarget().EvaluateExpression("$stack_size"))
     result.AppendMessage(f"Lua stack trace: {stack_size} items")
     debugger.HandleCommand(f"expr auto $ptr = {L}->base")
     idx = 1
     while True:
         debugger.HandleCommand(f"expr bool $is_less = $ptr < {L}->top")
-        if not bool(debugger.GetSelectedTarget().EvaluateExpression("$is_less").GetValue()):
+        is_less_expr = debugger.GetSelectedTarget().EvaluateExpression("$is_less")
+        if is_less_expr.GetValue().strip() != "true":
             break
+        result.AppendMessage(f"{idx:03d}: ")
+        luaprint(debugger, "$ptr", result, internal_dict)
+        result.AppendMessage("\n")
+        debugger.HandleCommand("expr $ptr = $ptr + 1")
+        idx += 1
+
+# Registration function to add all commands
+def register_commands():
+    lldb.debugger.HandleCommand('command script add -f __main__.luavalue luavalue')
+    lldb.debugger.HandleCommand('''command script add -o -h "luavalue <index> [L]\nProvides a pointer to a TValue from a stack index. By default, uses the current variable L as a lua_State pointer, but can be specified as the second argument. The return value is passed in variable $obj." luavalue''')
+    lldb.debugger.HandleCommand('command script add -f __main__.luaprint luaprint')
+    lldb.debugger.HandleCommand('''command script add -o -h "luaprint <value> [verbose]\nPretty-prints a TValue passed as argument. Expects a pointer to a TValue. When verbose is 1, expands tables, metatables, and userdata environments." luaprint''')
+    lldb.debugger.HandleCommand('command script add -f __main__.luaprinttable luaprinttable')
+    lldb.debugger.HandleCommand('''command script add -o -h "luaprinttable <table>\nPretty-prints a Lua Table. Expects a pointer to Table." luaprinttable''')
+    lldb.debugger.HandleCommand('command script add -f __main__.luastack luastack')
+
+# Register commands immediately
+register_commands()
