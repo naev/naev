e4cfcd75a490cb7e587f28417af83dede18e0d76
Edgar Simo-Serra <bobbens@gmail.com>
2025-07-28 18:02:36 +0900

    Lower razor energy drain to 6x.

--- dat/outfits/weapons/razor.lua
+++ dat/outfits/weapons/razor.lua
@@ -7,7 +7,7 @@ function onload( o )
    -- The effect applies a fixed ratio of energy vs disable and is normalized
    -- around disable.
    disable     = s.disable*0.5
-   energy      = s.disable*4
+   energy      = s.disable*3
    penetration = s.penetration
    isturret    = s.isturret
 end
