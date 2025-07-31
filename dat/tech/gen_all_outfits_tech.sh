c2c0bd273aa6ef4f86f2c0a43ab4608e85fa8ca8
johann1764 <johann.brault_baron@yahoo.com>
2025-07-31 10:05:42 +0200

    grep warnings in gen_all_..._tech.sh

--- dat/tech/gen_all_outfits_tech.sh
+++ dat/tech/gen_all_outfits_tech.sh
@@ -4,7 +4,7 @@ ARG=$1
 shift
 (
    echo  '<tech name="All Outfits">'
-   grep -m1 -ho '<outfit name=\"[^"]*\"' "$@" |
-   sed 's/^<outfit name=\"\([^"]*\)\"$/ <item>\1<\/item>/'
+   grep -m1 -ho '<outfit name="[^"]*"' "$@" |
+   sed 's/^<outfit name="\([^"]*\)"$/ <item>\1<\/item>/'
    echo '</tech>'
 ) >"$ARG"
--- dat/tech/gen_all_ships_tech.sh
+++ dat/tech/gen_all_ships_tech.sh
@@ -4,7 +4,7 @@ ARG=$1
 shift
 (
    echo '<tech name="All Ships">'
-   grep -m1 -ho '<ship name=\"[^"]*\"' "$@" |
-   sed 's/^<ship name=\"\([^"]*\)\"$/ <item>\1<\/item>/'
+   grep -m1 -ho '<ship name="[^"]*"' "$@" |
+   sed 's/^<ship name="\([^"]*\)"$/ <item>\1<\/item>/'
    echo '</tech>'
 ) >"$ARG"
