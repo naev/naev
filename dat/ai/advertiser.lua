e0d69be74b09523526f2448038694ead5583ad95
mjpenders <max.penders@gmail.com>
2025-08-03 21:08:01 -0400

    Added advertiser locating brands

--- dat/ai/advertiser.lua
+++ dat/ai/advertiser.lua
@@ -10,6 +10,61 @@ mem.atk_skill = 0
 function create ()
    create_pre()
 
+   -- Advertiser can give you the location for popular brands
+   -- This is how you make custom menu items!
+   mem.comm_custom = mem.comm_custom or {}
+   table.insert(mem.comm_custom, {
+      menu = _("Locate a brand"),
+      setup = function(vn, _p, _plt)
+         vn.na(fmt.f(_('"I\'d be happy to help you out! Which company are you interested in finding?"')))
+         vn.label("companies")
+         vn.menu({
+            { _([[Krain Industries]]), "starbridge" },
+            { _([[Unicorp]]), "unicorp" },
+            { _([[Milspec]]), "milspec" },
+            { _([[Tricon]]), "tricon" },
+            { _([[Teracom]]), "teracom" },
+            { _([[Nexus]]), "nexus" },
+            { _([[Enygma]]), "enygma" },
+            { _([[Back]]), "back" },
+         })
+         vn.label("back")
+         vn.jump("menu")
+
+         vn.label("starbridge")
+         vn.na(_('"Head to New Xantusia in Lynx to get the incredible Krain Industries Starbridge."'))
+         vn.jump("companies")
+
+         vn.label("unicorp")
+         vn.na(_('"You can find everything Unicorp at the Unicorp Orbital in Botarn."'))
+         vn.jump("companies")
+
+         vn.label("milspec")
+         vn.na(_('"Check out the smart and safe Milspec ships at Mining Vrata Guildhouse in Holly."'))
+         vn.jump("companies")
+
+         vn.label("tricon")
+         vn.na(_('"Tricon\'s market-leading engines can be conveniently found on Bolero in Delta Polaris."'))
+         vn.jump("companies")
+
+         vn.label("teracom")
+         vn.na(_('"Look for the whole selection of Teracom rockets on Dundrop Monastery in Botarn."'))
+         vn.jump("companies")
+
+         vn.label("nexus")
+         vn.na(_('"Nexus\' elite line of ships can be found at their very own Nexus Shipyards HQ in Valerie."'))
+         vn.jump("companies")
+
+         vn.label("enygma")
+         vn.na(
+            _(
+               '"Um, one second, let me check my notes. *ruffling of papers* You can find the Enygma System Turreted Launchers on Sharak in Opal."'
+            )
+         )
+         vn.jump("companies")
+      end,
+   })
+
    -- Credits.
    local price = ai.pilot():ship():price()
    ai.setcredits( rnd.rnd(price/180, price/40) )
