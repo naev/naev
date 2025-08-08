b42976a466e28e3e009b44fc6e6876b49349a386
johann1764 <johann.brault_baron@yahoo.com>
2025-08-08 23:21:45 +0200

    ported decorators & graph2ssysmap to ssys, ssys.py cleanup

--- utils/ssys/graph2ssysmap.py
+++ utils/ssys/graph2ssysmap.py
@@ -3,8 +3,7 @@
 
 import os
 from sys import stderr, exit
-import xml.etree.ElementTree as ET
-from ssys import nam2base, getpath, PATH, naev_xml
+from ssys import nam2base, getpath, PATH, naev_xml, ssys_xml, vec_to_pos
 from graph_vaux import ssys_others, ssys_nam
 
 
@@ -25,7 +24,7 @@ if help_f or (argv[1:] and do_write):
       msg(l)
    exit(0 if help_f else 1)
 
-def new_ssys(name, basenam, pos, ssys_pos, jmp):
+def new_ssys(name, basenam, ssys_pos, jmp):
    xml = naev_xml()
    xml.save_as(name)
    Nam = ssys_nam(ssys_pos, basenam)
@@ -40,7 +39,7 @@ def new_ssys(name, basenam, pos, ssys_pos, jmp):
    xml['ssys'] = {
       '@name': Nam or ' '.join([s[0].upper() + s[1:] for s in basenam.split('_')]),
       'general': {'radius': 5000, 'spacedust': 300, 'interference': 0},
-      'pos': {'@x': pos[0], '@y': pos[1]},
+      'pos': vec_to_pos(v),
       'spobs': {},
       'jumps': {'jump': [mk_jump(k, v) for k, v in jmp.items()]},
       'asteroids': {},
@@ -52,19 +51,17 @@ def new_ssys(name, basenam, pos, ssys_pos, jmp):
         'map_shader': 'stellarwind_map.frag',
         'features': '#bStellar Wind (1 fuel regen)#0'
       }
-   xml.save()
+   return xml
 
 from graphmod import ssys_pos, ssys_jmp, no_graph_out
 
 no_graph_out()
 
-from ssys import fil_ET
-for n, (x, y) in ssys_pos.items():
+for n, v in ssys_pos.items():
    name = os.path.join(PATH, 'ssys', n + '.xml')
    try:
-      T = fil_ET(name)
-      e = T.getroot().find('pos')
-      e.attrib['x'], e.attrib['y'] = str(x), str(y)
-      T.write(name)
+      xml = ssys_xml(name)
    except FileNotFoundError:
-      new_ssys(name, n, (x, y), ssys_pos, ssys_jmp[n])
+      xml = new_ssys(name, n, ssys_pos, ssys_jmp[n])
+   xml['ssys']['pos'] = vec_to_pos(v)
+   xml.save()
--- utils/ssys/ssys.py
+++ utils/ssys/ssys.py
@@ -2,14 +2,13 @@
 
 
 import os
-import sys
 import xml.etree.ElementTree as ET
-from sys import argv, stderr
+from sys import argv, stderr, path
 
 getpath = lambda *x: os.path.realpath(os.path.join(*x))
 
 script_dir = os.path.dirname(__file__)
-sys.path.append(getpath(script_dir, '..'))
+path.append(getpath(script_dir, '..'))
 
 from xml_name import xml_name as nam2base
 from naev_xml import naev_xml, xml_node
@@ -22,7 +21,6 @@ from geometry import vec
 _fil =lambda folder: lambda nam : getpath(PATH, folder, nam + '.xml')
 spob_fil = _fil('spob')
 ssys_fil = _fil('ssys')
-
 
 class ssys_xml(naev_xml):
    def __init__(self, *args):
@@ -41,28 +39,6 @@ class ssys_xml(naev_xml):
             dict.__setitem__(s[fs], f, [s[fs][f]])
       self._uptodate = True
 
-import subprocess
-cmd = getpath(script_dir, '..', 'repair_xml.sh')
-need_repair = []
-from atexit import register
-
-def fil_ET( name ):
-   T = ET.parse(name)
-   oldw = T.write
-   def write( nam ):
-      global need_repair
-      oldw(nam)
-      if need_repair == []:
-         def _repair_ET():
-            global need_repair
-            if need_repair:
-               subprocess.run([cmd] + need_repair)
-            need_repair = []
-         register(_repair_ET)
-      need_repair.append(nam)
-   T.write = write
-   return T
-
 class starmap(dict):
    def __missing__( self, key ):
       name = ssys_fil(key)
@@ -75,27 +51,8 @@ class starmap(dict):
             self[key] = None
       return self[key]
 
-def ssysneigh( sys ):
-   T = ET.parse(ssys_fil(sys)).getroot()
-   acc = []
-   count = 1
-   for e in T.findall('./jumps/jump'):
-      try:
-         acc.append((nam2base(e.attrib['target']), e.find('hidden') is not None))
-      except:
-         stderr.write('no target defined in "'+sys+'" jump '+str(count)+'\n')
-      count += 1
-   return acc
-
 def pos_to_vec( e ):
    return vec(e['$@x'], e['$@y'])
 
 def vec_to_pos( v ):
    return {'$@x': v[0], '$@y': v[1] }
-
-def vec_from_element( e ):
-   return vec(float(e.attrib['x']), float(e.attrib['y']))
-
-def vec_to_element( e, v ):
-   e.set('x', str(v[0]))
-   e.set('y', str(v[1]))
