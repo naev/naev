370d71b0f440a3e9d4125f0d551e58e2667d9676
johann1764 <johann.brault_baron@yahoo.com>
2025-08-09 00:19:26 +0200

    disable shellcheck warning && update Readme

--- utils/xml_format.sh
+++ utils/xml_format.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
+#shellcheck disable=SC2207
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#shellcheck disable=SC2207
 NON_COMMENTED=($(grep -L "<!--" "$@"))
 
-#shellcheck disable=SC2207
 OUTFITS=($(grep -l '^[[:space:]]*<outfit ' "${NON_COMMENTED[@]}"))
 "$SCRIPT_DIR"/outfits/outfit.py "${OUTFITS[@]}"
 
-#shellcheck disable=SC2207
 NON_OUTFITS=($(grep -L '^<outfit ' "${NON_COMMENTED[@]}"))
 SSYS=($(grep -l '^[[:space:]]*<ssys ' "${NON_OUTFITS[@]}"))
 "$SCRIPT_DIR"/ssys/ssys.py "${SSYS[@]}"
 
-#shellcheck disable=SC2207
 NON_SSYS=($(grep -L '^[[:space:]]*<ssys ' "${NON_OUTFITS[@]}"))
 "$SCRIPT_DIR"/naev_xml.py "${NON_SSYS[@]}"
