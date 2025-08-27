b612c34d617153a290438f1d7d1f12532da81d87
johann1764 <johann.brault_baron@yahoo.com>
2025-08-27 17:54:41 +0200

    utils/check_luadoc_funcs.sh

--- utils/check_luadoc_funcs.sh
+++ utils/check_luadoc_funcs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
 TMP=$(mktemp -u)
 mkfifo "$TMP"
 TMP2=$(mktemp)
@@ -7,8 +10,9 @@ TMP2=$(mktemp)
 res=0
 trap 'rm -f "$TMP" "$TMP2" ; exit $res' EXIT
 
+doxt="$("$SCRIPT_DIR"/get_doxtractor.sh)"
 for arg in "$@" ; do
-   docs/lua/c2luadoc.sh "$arg" "$TMP" &
+   "$SCRIPT_DIR/../docs/lua/c2luadoc.sh" "$doxt" "$arg" "$TMP" &
    sed ':a;N;$!ba;s/\n--/\\n--/g' < "$TMP" | grep -v '\(^$\)\|\(@module\)\|\(@function\)' > "$TMP2"
    if [ -s "$TMP2" ] ; then
       echo "$arg:"
