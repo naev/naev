f574472efa9ed5efa2a0cfbb38a9bb359da5df1b
Edgar Simo-Serra <bobbens@gmail.com>
2025-08-31 10:21:02 +0900

    Added AtomicF32.

--- src/core/src/utils.rs
+++ src/core/src/utils.rs
@@ -17,3 +17,25 @@ where
 {
     a.binary_search_by(|x| key(x).cmp(b))
 }
+
+use std::sync::atomic::{AtomicU32, Ordering};
+/// Small wrapper for atomic f32 operations.
+pub struct AtomicF32 {
+    storage: AtomicU32,
+}
+impl AtomicF32 {
+    pub const fn new(value: f32) -> Self {
+        let as_u32 = value.to_bits();
+        Self {
+            storage: AtomicU32::new(as_u32),
+        }
+    }
+    pub fn store(&self, value: f32, ordering: Ordering) {
+        let as_u32 = value.to_bits();
+        self.storage.store(as_u32, ordering)
+    }
+    pub fn load(&self, ordering: Ordering) -> f32 {
+        let as_u32 = self.storage.load(ordering);
+        f32::from_bits(as_u32)
+    }
+}
