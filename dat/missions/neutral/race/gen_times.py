238286781cf70b720e495962c6aca5dc9df9d9c9
johann1764 <johann.brault_baron@yahoo.com>
2025-08-31 19:10:25 +0200

    gen_times.py

---
+++ dat/missions/neutral/race/gen_times.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import sys
+from os import path
+dat_dir = path.realpath(path.join(path.dirname(__file__), '..', '..', '..'))
+script_dir = path.realpath(path.join(dat_dir, '..', 'utils'))
+sys.path.append(script_dir)
+sys.path.append(path.realpath(path.join(script_dir, 'outfits')))
+from outfit import outfit, naev_xml
+from getconst import PHYSICS_SPEED_DAMP
+
+# Peninsula 40 25 20
+# SM
+# QT
+
+# These are computed by the game and displayed in the race menu
+names = ["Peninsula", "Smiling Man", "Qex tour"]
+length = [23745, 40554, 36886]
+#the lengths are overestimated by approx 15%
+length = [n / 1.15 for n in length]
+
+configs = [
+   {
+      'ship': 'neutral/hyena', 'engine': 'small/nexus_dart_160_engine',
+   },{
+      'ship': 'neutral/hyena', 'engine': 'small/tricon_zephyr_engine',
+      'AB': 'unicorp_light_afterburner',
+   },{
+      'ship': 'neutral/hyena', 'engine': 'small/tricon_zephyr_engine',
+      'structure': "engine_reroute", # expects a reactor
+      'AB': 'unicorp_light_afterburner',
+   },{
+      'ship': 'soromid/soromid_reaver', 'engines': 'small/tricon_zephyr_engine',
+      'accessory': 'racing_trophy_bronze',
+      'structure': "compact_lightsail",
+      'AG': 'adrenal_gland_ii',
+      'skill': 'wanderer'
+   },{
+      'ship': 'soromid/soromid_reaver', 'engines': 'small/tricon_zephyr_engine',
+      'accessory': 'racing_trophy_bronze',
+      'structure': "compact_lightsail",
+      'AG': 'adrenal_gland_ii',
+      'skill': 'wanderer'
+   },{
+      'ship': 'soromid/soromid_reaver', 'engines': 'small/tricon_zephyr_engine',
+      'accessory': 'racing_trophy_bronze',
+      'structure': "compact_lightsail",
+      'AG': 'adrenal_gland_ii',
+      'skill': 'wanderer'
+   },{
+      'ship': 'pirate/pirate_revenant', 'engine': 'medium/tricon_cyclone_engine',
+      'accessory': 'racing_trophy_silver',
+      'structure': ("compact_lightsail", 2),
+      'AG': 'adrenal_gland_iii',
+      'skill': 'wanderer'
+   },{
+      'ship': 'pirate/pirate_revenant', 'engine': 'medium/tricon_cyclone_engine',
+      'accessory': 'racing_trophy_silver',
+      'structure': ("compact_lightsail", 2),
+      'AG': 'adrenal_gland_iii',
+      'skill': 'wanderer'
+   },{
+      'ship': 'pirate/pirate_revenant', 'engine': 'medium/tricon_cyclone_engine',
+      'accessory': 'racing_trophy_silver',
+      'structure': ("compact_lightsail", 2),
+      'AG': 'adrenal_gland_iii',
+      'skill': 'wanderer'
+   }
+]
+
+ab_ratio = [0.0, 0.15, 0.2, 0.6, 0.6, 0.65, 0.9, 0.9, 0.9]
+
+where = {
+   'ship': 'ships/',
+   'engine': 'outfits/core_engine/',
+   'engines': 'outfits/core_engine/',
+   'accessory': 'outfits/accessory/',
+   'structure': 'outfits/structure/',
+   'AB': 'outfits/utility',
+   'AG': 'outfits/bioship/skills/',
+   'skill': 'outfits/bioship/skills/',
+}
+
+
+def config_to_str(c):
+   s = ''
+   for t in c.values():
+      if not isinstance(t, tuple):
+         t = (t, 1)
+      s += t[0].split('/')[-1]
+      if t[1] != 1:
+         s += ' x' + str(t[1])
+      s += ' '
+   return s
+
+def fmt_tim(f):
+   if m := int(f//60):
+      return str(m) + "'" + str(round(f-m*60, 1))
+   else:
+      return str(round(f, 1))
+
+race = [names[i] + ' ' + j for j in ['bronze', 'silver', 'gold'] for i in range(3)]
+for ra, l, c, ab in zip(race, length*3, configs, ab_ratio):
+   total = {
+      'speed': 0, 'accel': 0,
+      'speed_fact': 1.0, 'accel_fact': 1.0,
+      'speed_AB': 1.0, 'accel_AB': 1.0, 'time_speedup_AB': 1.0}
+   for k, v in c.items():
+      if isinstance(v,tuple):
+         v, count = v
+      else:
+         count = 1
+      fnam = path.join(dat_dir, where[k], v + '.xml')
+      if k == 'engine':
+         o = outfit(fnam)
+         o.stack()
+      elif k == 'engines':
+         o = outfit(fnam)
+         o.stack(o)
+      else:
+         o = naev_xml(fnam)
+      for f in ['speed', 'speed_mod', 'accel', 'accel_mod', 'time_speedup']:
+         if r := o.find(f):
+            r *= count
+            if k in {'AB', 'AG'}:
+               total[f + '_AB'] *= 1.0 + (r/100.0)
+            elif (w:= f.find('_mod')) != -1:
+               total[f[:w] + '_fact'] *= 1.0 + (r/100.0)
+            else:
+               total[f] += r
+   for f in ['speed', 'accel']:
+      total[f] *= total[f + '_fact']
+      del total[f + '_fact']
+   in_AB = {k:total[k]*total[k + '_AB'] for k in ['speed', 'accel']}
+   final = total['speed'] + total['accel']/PHYSICS_SPEED_DAMP
+   final_AB = (in_AB['speed'] + in_AB['accel']/PHYSICS_SPEED_DAMP) * total['time_speedup_AB']
+
+   vals = [(a[0].split('/')[-1], a[1]) if isinstance(a, tuple) else a.split('/')[-1] for a in c.values()]
+   s = 'normal: ' + str(round(final))
+   s += '  ' + 'AB (\033[36m' + str(round(100.0*ab)) + '%\033[m' + ' of track) ' + str(round(final_AB))
+   print('\n' + config_to_str(c))
+   final = ab*final_AB + (1 - ab)*final
+   print(s, '->', '\033[36m' + str(round(final)) + '\033[m')
+   print(ra + ':','\033[36;1m' + fmt_tim(l/final) + '\033[m')
