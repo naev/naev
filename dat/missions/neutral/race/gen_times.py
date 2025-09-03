33d5666fbed45bf5bd848a51305a4cffa9d70952
johann1764 <johann.brault_baron@yahoo.com>
2025-09-03 02:50:52 +0200

    generate lua race times

--- dat/missions/neutral/race/gen_times.py
+++ dat/missions/neutral/race/gen_times.py
@@ -9,6 +9,21 @@ sys.path.append(path.realpath(path.join(script_dir, 'outfits')))
 from outfit import outfit, naev_xml
 from getconst import PHYSICS_SPEED_DAMP
 
+if __name__ != '__main__':
+   raise Exception('This is intended to be used as main')
+
+from sys import argv
+
+if len(argv) == 1:
+   argv += ['-']
+
+if argv[1] == '-q':
+   argv.remove('-q')
+   def info(*t):
+      pass
+else:
+   info = lambda *t: sys.stderr.write(' '.join(t) + '\n')
+
 # Peninsula 40 25 20
 # SM
 # QT
@@ -32,19 +47,16 @@ configs = [
    },{
       'ship': 'soromid/soromid_reaver', 'engines': 'small/tricon_zephyr_engine',
       'accessory': 'racing_trophy_bronze',
-      'structure': "compact_lightsail",
       'AG': 'adrenal_gland_ii',
       'skill': 'wanderer'
    },{
       'ship': 'soromid/soromid_reaver', 'engines': 'small/tricon_zephyr_engine',
       'accessory': 'racing_trophy_bronze',
-      'structure': "compact_lightsail",
       'AG': 'adrenal_gland_ii',
       'skill': 'wanderer'
    },{
       'ship': 'soromid/soromid_reaver', 'engines': 'small/tricon_zephyr_engine',
       'accessory': 'racing_trophy_bronze',
-      'structure': "compact_lightsail",
       'AG': 'adrenal_gland_ii',
       'skill': 'wanderer'
    },{
@@ -68,7 +80,7 @@ configs = [
    }
 ]
 
-ab_ratio = [0.0, 0.15, 0.2, 0.6, 0.6, 0.65, 0.9, 0.9, 0.9]
+ab_ratio = [0.0, 0.15, 0.2, 0.65, 0.65, 0.7, 0.9, 0.9, 0.9]
 
 where = {
    'ship': 'ships/',
@@ -84,9 +96,9 @@ where = {
 
 def config_to_str(c):
    s = ''
-   for t in c.values():
+   for k, t in c.items():
       if not isinstance(t, tuple):
-         t = (t, 1)
+         t = (t, 1 if k != 'engines' else 2)
       s += t[0].split('/')[-1]
       if t[1] != 1:
          s += ' x' + str(t[1])
@@ -99,46 +111,64 @@ def fmt_tim(f):
    else:
       return str(round(f, 1))
 
+info = lambda *t: sys.stderr.write(' '.join(t)+'\n')
 race = [names[i] + ' ' + j for j in ['bronze', 'silver', 'gold'] for i in range(3)]
-for ra, l, c, ab in zip(race, length*3, configs, ab_ratio):
-   total = {
-      'speed': 0, 'accel': 0,
-      'speed_fact': 1.0, 'accel_fact': 1.0,
-      'speed_AB': 1.0, 'accel_AB': 1.0, 'time_speedup_AB': 1.0}
-   for k, v in c.items():
-      if isinstance(v,tuple):
-         v, count = v
-      else:
-         count = 1
-      fnam = path.join(dat_dir, where[k], v + '.xml')
-      if k == 'engine':
-         o = outfit(fnam)
-         o.stack()
-      elif k == 'engines':
-         o = outfit(fnam)
-         o.stack(o)
-      else:
-         o = naev_xml(fnam)
-      for f in ['speed', 'speed_mod', 'accel', 'accel_mod', 'time_speedup']:
-         if r := o.find(f):
-            r *= count
-            if k in {'AB', 'AG'}:
-               total[f + '_AB'] *= 1.0 + (r/100.0)
-            elif (w:= f.find('_mod')) != -1:
-               total[f[:w] + '_fact'] *= 1.0 + (r/100.0)
-            else:
-               total[f] += r
-   for f in ['speed', 'accel']:
-      total[f] *= total[f + '_fact']
-      del total[f + '_fact']
-   in_AB = {k:total[k]*total[k + '_AB'] for k in ['speed', 'accel']}
-   final = total['speed'] + total['accel']/PHYSICS_SPEED_DAMP
-   final_AB = (in_AB['speed'] + in_AB['accel']/PHYSICS_SPEED_DAMP) * total['time_speedup_AB']
+out={}
+for nj, j in enumerate(['bronze', 'silver', 'gold']):
+   for ni, i in enumerate(names):
+      ra = j + ' ' + i
+      l = length[ni]
+      ij = nj*3 + ni
+      c = configs[ij]
+      ab = ab_ratio[ij]
+      total = {
+         'speed': 0, 'accel': 0,
+         'speed_fact': 1.0, 'accel_fact': 1.0,
+         'speed_AB': 1.0, 'accel_AB': 1.0, 'time_speedup_AB': 1.0}
+      for k, v in c.items():
+         if isinstance(v,tuple):
+            v, count = v
+         else:
+            count = 1
+         fnam = path.join(dat_dir, where[k], v + '.xml')
+         if k == 'engine':
+            o = outfit(fnam)
+            o.stack()
+         elif k == 'engines':
+            o = outfit(fnam)
+            o.stack(o)
+         else:
+            o = naev_xml(fnam)
+         for f in ['speed', 'speed_mod', 'accel', 'accel_mod', 'time_speedup']:
+            if r := o.find(f):
+               r *= count
+               if k in {'AB', 'AG'}:
+                  total[f + '_AB'] *= 1.0 + (r/100.0)
+               elif (w:= f.find('_mod')) != -1:
+                  total[f[:w] + '_fact'] *= 1.0 + (r/100.0)
+               else:
+                  total[f] += r
+      for f in ['speed', 'accel']:
+         total[f] *= total[f + '_fact']
+         del total[f + '_fact']
+      in_AB = {k:total[k]*total[k + '_AB'] for k in ['speed', 'accel']}
+      final = total['speed'] + total['accel']/PHYSICS_SPEED_DAMP
+      final_AB = (in_AB['speed'] + in_AB['accel']/PHYSICS_SPEED_DAMP) * total['time_speedup_AB']
 
-   vals = [(a[0].split('/')[-1], a[1]) if isinstance(a, tuple) else a.split('/')[-1] for a in c.values()]
-   s = 'normal: ' + str(round(final))
-   s += '  ' + 'AB (\033[36m' + str(round(100.0*ab)) + '%\033[m' + ' of track) ' + str(round(final_AB))
-   print('\n' + config_to_str(c))
-   final = ab*final_AB + (1 - ab)*final
-   print(s, '->', '\033[36m' + str(round(final)) + '\033[m')
-   print(ra + ':','\033[36;1m' + fmt_tim(l/final) + '\033[m')
+      vals = [(a[0].split('/')[-1], a[1]) if isinstance(a, tuple) else a.split('/')[-1] for a in c.values()]
+      s = 'normal: ' + str(round(final))
+      s += '  ' + 'AB (\033[36m' + str(round(100.0*ab)) + '%\033[m' + ' of track) ' + str(round(final_AB))
+      info('\n' + config_to_str(c))
+      final = ab*final_AB + (1 - ab)*final
+      info(s, '->', '\033[36m' + str(round(final)) + '\033[m')
+      info(ra + ':','\033[36;1m' + fmt_tim(l/final) + '\033[m')
+      out[i] = out.get(i, {})
+      out[i][j] = round(l/final, 1)
+info()
+
+with open(argv[1], 'wt') if argv[1] != '-' else sys.stdout as fp:
+   fp.write('return {\n')
+   for track, times in out.items():
+      fp.write(' '*3 + '[' + repr(track) + ']= ')
+      fp.write(str(times).replace(':', '=').replace("'",'') + ',\n')
+   fp.write('}\n')
