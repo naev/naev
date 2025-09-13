b8ee09c7ee17ff5eeebbeb74ae097ae2f15e222c
johann1764 <johann.brault_baron@yahoo.com>
2025-09-13 12:22:07 +0200

    Updated Qex race dialog + added advisor character

--- dat/missions/neutral/race_qex.lua
+++ dat/missions/neutral/race_qex.lua
@@ -24,7 +24,7 @@ local lmisn = require "lmisn"
 local DEFAULT_REWARD = 100e3
 
 goal_times = require 'missions.neutral.race.times_qex'
-local medal_col = {Bronze= '#o', Silver='#w', Gold= '#y'}
+local medal_col = {Bronze= '#o', Silver= '#w', Gold= '#y'}
 
 local elapsed_time, race_done
 
@@ -84,6 +84,11 @@ function upgrade_trophies(to)
    end
 end
 
+local function add_advice_npc()
+   misn.npcAdd( "qex_race_advice", _("Laid Back Person"), laid_back_portrait,
+      _("This person will be happy to answer any questions about racing."), 6)
+end
+
 function create ()
    mem.race_spob = spob.cur()
    if not misn.claim( system.cur() ) then
@@ -111,6 +116,47 @@ function create ()
    end
 
    misn.npcAdd( "approach_terminal", _("Racing Terminal"), npc_portrait, npc_description )
+   if var.peek( "racing_intro" ) then
+      add_advice_npc()
+   end
+end
+
+function qex_race_advice()
+   vn.clear()
+   vn.scene()
+   local lbp = vn.newCharacter(_("Laid Back Person"), {image=laid_back_portrait})
+   vn.transition()
+   lbp(_([["Hiya there! How is the racing going ?"]]))
+   vn.label("start")
+   lbp(_([["Do you need help?]]))
+   vn.menu{
+      {_([[difficulty levels]]), "cont_difficulty"},
+      {_([[general strategy]]), "cont_strategy"},
+      {_([[the bronze series]]), "cont_bronze"},
+      {_([[the silver series]]), "cont_silver"},
+      {_([[the gold series]]), "cont_gold"},
+      {_([[quit]]), "cont_quit"},
+   }
+   vn.label("cont_difficulty")
+   lbp(_([["The tracks are increasingly difficult. Peninsula is the easiest, while Qex Tour is the hardest. That also means that you get more rewards for harder races.
+  The three levels of difficulty Bronze, Silver and Gold change drastically how hard it is to beat a race, so, Peninsula - Silver is harder than Qex Tour - Bronze and Peninsula - Gold is harder than Qex Tour - Silver"]]))
+   lbp(_([["Therefore, it is advised to beat all Bronze times before attempting to beat Silver times. Furthermore, the Bronze price will make it easier to beat the Silver times, and similarly for the Gold times."]]))
+   vn.jump("start")
+   vn.label("cont_strategy")
+   lbp(_([["Qex Races allow you -- and expects you -- to use afterburners.
+  The two crucial aspects of your ships are therefore how long it can maintain afterburner and how fast does it go while in afterburner."]]))
+   vn.jump("start")
+   vn.label("cont_bronze")
+   lbp(_([["The Bronze series is the easy one. It is doable with limited ressources and outfits and ships available here."]]))
+   vn.jump("start")
+   vn.label("cont_silver")
+   lbp(_([["The Silver series is more challenging. It will require better than the basic Unicorp afterburner, and finding the right ship and outfit to do this is a challenge in itself. It is a challenge for seasoned captains who can afford it."]]))
+   vn.jump("start")
+   vn.label("cont_gold")
+   lbp(_([["The Gold series is even harder than the Silver series. Only very few manage to do it and get the prestigious Gold Trophy. This is all about prestige, as the Gold Trophy in itself is not worth the effort, if you ask me."]]))
+   vn.jump("start")
+   vn.label("cont_quit")
+   vn.run()
 end
 
 local function display_time( time )
@@ -141,15 +187,19 @@ function approach_terminal ()
       }
 
       vn.label("cont01_yes")
-      lbp(_([["Racing is quite simple, you have to go through all the gates in order until you reach the final goal. There are no limitations on what ship and outfits you can use, but you're best sticking to Yacht or Interceptor-class ships if you want to make good times."]]))
-      lbp(_([["Each track has a goal time to beat. If you beat the time, you will get a nice credit reward. Furthermore, if you beat all the tracks here, a Melendez Corporation representative will give you a nice trophy to commemorate!"]]))
+      lbp(_([["Racing is quite simple, you have to go through all the gates in order until you reach the final goal. There are no limitations on what ship and outfits you can use, but you're best sticking to light or medium-light ships and using an afterburner if you want to make good times."]]))
+      lbp(_([["Each track has three goal times to beat: Bronze, Silver and Gold corresponding to three levels of difficulty.
+  If you beat one of the times, you will get a nice credit reward depending on the track and on the difficulty level of the beaten time. In addition of that, you will get an early finish bonus dependepending on the improvement over the reference time.
+  If you beat all the tracks here for a given difficulty level, a Melendez Corporation representative will give you a nice trophy to commemorate!"]]))
       lbp(_([["You can try as many times as you want with no penalty. Your best time will also be saved so you can try to beat it again."]]))
       lbp(_([["It's really fun, just be careful not to get addicted. He he."]]))
 
       vn.label("cont01_no")
-      lbp(_([["OK, look forward to seeing your racing skills!"]]))
+      lbp(_([["OK, look forward to seeing your racing skills!
+  I will be around if you have any questions."]]))
       vn.run()
       var.push("racing_intro", true)
+      add_advice_npc()
    end
 
    local w, h = 480, 420
