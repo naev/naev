74bae54efd39a28b533ea45ab80334bda0e76e51
Edgar Simo-Serra <bobbens@gmail.com>
2025-10-09 10:27:37 +0900

    Nuked obsoleted code.

--- src/console.c
+++ src/console.c
@@ -30,13 +30,11 @@
 #include "nlua_bkg.h"
 #include "nlua_camera.h"
 #include "nlua_cli.h"
-#include "nlua_colour.h"
 #include "nlua_linopt.h"
 #include "nlua_music.h"
 #include "nlua_tex.h"
 #include "nlua_tk.h"
 #include "nluadef.h"
-#include "nstring.h"
 #include "toolkit.h"
 
 #define BUTTON_WIDTH 50  /**< Button width. */
@@ -83,7 +81,6 @@ static const luaL_Reg cli_methods[] = {
 static int  cli_keyhandler( unsigned int wid, SDL_Keycode key, SDL_Keymod mod,
                             int isrepeat );
 static void cli_render( double bx, double by, double w, double h, void *data );
-static int  cli_printCore( lua_State *L, int cli_only, int escape );
 static void cli_addMessage( const char *msg );
 static void cli_addMessageMax( const char *msg, const int l );
 void        cli_tabComplete( unsigned int wid );
@@ -102,47 +99,6 @@ static char *cli_escapeString( int *len_out, const char *s, int len )
    *len_out = b;
    return buf;
 }
-
-#define _CID( c ) ( ( (int)c ) - (int)'0' + 1 )
-static char *fontcol_toTermEscapeString( const char *s )
-{
-   static const char *const seq[_CID( 'z' ) + 1] = {
-      [_CID( '0' )] = "\e[0m",    // default
-      [_CID( 'b' )] = "\e[0;34m", // blue
-      [_CID( 'g' )] = "\e[0;32m", // green
-      [_CID( 'n' )] = "\e[1;30m", // grey
-      [_CID( 'o' )] = "\e[0;33m", // orange -> brown
-      [_CID( 'p' )] = "\e[0;35m", // purple or pink? -> magenta
-      [_CID( 'r' )] = "\e[0;31m", // red
-      [_CID( 'w' )] = "\e[0;37m", // white
-      [_CID( 'y' )] = "\e[1;33m"  // yellow
-   };
-   char       *buf;
-   const char *es = seq[_CID( '0' )];
-   int         i, wi = 0, count = 0;
-
-   for ( i = 0; s[i]; i++ )
-      if ( s[i] == FONT_COLOUR_CODE )
-         count++;
-
-   buf = calloc( i + count * 7 + 4 + 1, sizeof( char ) );
-
-   for ( i = 0; s[i]; i++ )
-      if ( s[i] == FONT_COLOUR_CODE &&
-           ( ( es =
-                  seq[( s[i + 1] >= '0' && s[i + 1] <= 'z' ) ? _CID( s[i + 1] )
-                                                             : 0] ) ) ) {
-         wi += sprintf( buf + wi, "%s", es );
-         i++;
-      } else
-         buf[wi++] = s[i];
-
-   if ( es != seq[_CID( '0' )] )
-      sprintf( buf + wi, "%s", seq[_CID( '0' )] );
-
-   return buf;
-}
-#undef _CID
 
 /**
  * @brief Prints a string.
