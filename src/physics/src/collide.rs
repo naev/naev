8a4e65b1ba9de5f7768477e75c5a57201057548a
Edgar Simo-Serra <bobbens@gmail.com>
2025-10-10 17:35:47 +0900

    Drop parry2d dependency and use a custom line-circle collision implementation based on our C code.

--- src/physics/src/collide.rs
+++ src/physics/src/collide.rs
@@ -1,5 +1,6 @@
-use nalgebra::{Isometry2, Vector2};
-use parry2d_f64 as parry2d;
+use nalgebra::Vector2;
+
+const TOLERANCE: f64 = 1e-6;
 
 pub enum Collision<T> {
     Single(Vector2<T>),
@@ -47,35 +48,103 @@ pub fn line_line(
     }
 }
 
-// Computes collision between a line and a circle
+#[allow(non_snake_case)]
+/// Computes collision between a line and a circle
 pub fn line_circle(
     p1: Vector2<f64>,
     p2: Vector2<f64>,
-    center: Vector2<f64>,
-    radius: f64,
+    cc: Vector2<f64>,
+    cr: f64,
 ) -> Option<Collision<f64>> {
-    let line = parry2d::shape::Segment::new(p1.into(), p2.into());
-    let ident = Isometry2::identity();
-    let circle = parry2d::shape::Ball::new(radius);
-    let center = Isometry2::translation(center.x, center.y);
-    match parry2d::query::contact(&ident, &line, &center, &circle, 0.0).unwrap() {
-        /*
-        // The point on the line gives the center collision, while the sphere gives surface
-        Some(c) => Some(Collision::Double(
-            c.point1.coords.into(),
-            c.point2.coords.into(),
-        )),
-        */
-        Some(c1) => {
-            let line = parry2d::shape::Segment::new(p2.into(), p1.into());
-            let c2 = parry2d::query::contact(&ident, &line, &center, &circle, 0.0)
-                .unwrap()
-                .unwrap();
-            Some(Collision::Double(
-                c1.point2.coords.into(),
-                c2.point2.coords.into(),
-            ))
+    fn fx(A: f64, B: f64, C: f64, x: f64) -> f64 {
+        -(A * x + C) / B
+    }
+    fn fy(A: f64, B: f64, C: f64, y: f64) -> f64 {
+        -(B * y + C) / A
+    }
+    fn line_point_on_segment(p1: Vector2<f64>, p2: Vector2<f64>, d1: f64, x: f64, y: f64) -> bool {
+        let v = Vector2::new(x, y);
+        let d2 = (v - p1).norm();
+        let d3 = (v - p2).norm();
+        (d1 - d2 - d3).abs() < TOLERANCE
+    }
+
+    /* Case line in circle. */
+    let r2 = cr * cr;
+    if ((p1 - cc).norm_squared() < r2) && ((p2 - cc).norm_squared() < r2) {
+        return Some(Collision::Single((p1 + p2) * 0.5));
+    }
+
+    let A = p2.y - p1.y;
+    let B = p1.x - p2.x;
+    let C = p2.x * p1.y - p1.x * p2.y;
+
+    let a = A * A + B * B;
+
+    // Non-vertical case.
+    let (b, c, bnz) = if B.abs() >= TOLERANCE {
+        let b = 2. * (A * C + A * B * cc.y - B * B * cc.x);
+        let c = C * C + 2. * B * C * cc.y - B * B * (cr * cr - cc.norm_squared());
+        (b, c, true)
+    // Have to have special care when line is vertical.
+    } else {
+        let b = 2. * (B * C + A * B * cc.x - A * A * cc.y);
+        let c = C * C + 2. * A * C * cc.x - A * A * (cr * cr - cc.norm_squared());
+        (b, c, false)
+    };
+
+    // Discriminant
+    let d = b * b - 4. * a * c;
+    if d < 0. {
+        return None;
+    }
+
+    let d1 = (p2 - p1).norm();
+    if d.abs() < TOLERANCE {
+        let (x, y) = if bnz {
+            let x = -b / (2. * a);
+            let y = fx(A, B, C, x);
+            (x, y)
+        } else {
+            let y = -b / (2. * a);
+            let x = fy(A, B, C, y);
+            (x, y)
+        };
+        if line_point_on_segment(p1, p2, d1, x, y) {
+            Some(Collision::Single(Vector2::new(x, y)))
+        } else {
+            None
         }
-        None => None,
+
+    // Two potential intersections
+    } else {
+        let d = d.sqrt();
+        let (x1, y1, x2, y2) = if bnz {
+            let x1 = (-b + d) / (2. * a);
+            let y1 = fx(A, B, C, x1);
+            let x2 = (-b - d) / (2. * a);
+            let y2 = fx(A, B, C, x2);
+            (x1, y1, x2, y2)
+        } else {
+            let y1 = (-b + d) / (2. * a);
+            let x1 = fx(A, B, C, y1);
+            let y2 = (-b - d) / (2. * a);
+            let x2 = fx(A, B, C, y2);
+            (x1, y1, x2, y2)
+        };
+        let on1 = line_point_on_segment(p1, p2, d1, x1, y1);
+        let on2 = line_point_on_segment(p1, p2, d1, x2, y2);
+        if on1 && on2 {
+            Some(Collision::Double(
+                Vector2::new(x1, y1),
+                Vector2::new(x2, y2),
+            ))
+        } else if on1 {
+            Some(Collision::Single(Vector2::new(x1, y1)))
+        } else if on2 {
+            Some(Collision::Single(Vector2::new(x2, y2)))
+        } else {
+            None
+        }
     }
 }
