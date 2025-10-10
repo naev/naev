9146b4b6b745399a3a0ba8b650f7066ab201b9dd
Edgar Simo-Serra <bobbens@gmail.com>
2025-10-10 18:41:02 +0900

    More vec2 unit tests that pass.

--- src/physics/src/vec2.rs
+++ src/physics/src/vec2.rs
@@ -546,56 +546,71 @@ end
 
 local v = vec2.new( 10, 5 )
 assert( close_enough( v:mod(), 11.180339887498949 ), "v:mod() failed" )
+v:set( 5, 10 )
+assert( close_enough_vec( v, vec2.new( 5, 10 ) ), "v:set( 5, 10 )" )
+assert( close_enough_vec( -v, vec2.new( -5, -10 ) ), "-v" )
 
+local v = vec2.new( 10, 5 )
 local a = vec2.new( 8, 3 )
 assert( close_enough( v:dist(a), 2*1.4142135623730951 ), "v:dist(a)" )
+assert( close_enough( v:dist2(a), (2*1.4142135623730951)^2 ), "v:dist2(a)" )
+local b = vec2.new( 3, 2 )
+assert( close_enough_vec( a+b, vec2.new(11,5) ), "a+b" )
+assert( close_enough_vec( a-b, vec2.new(5,1) ), "a-b" )
+assert( close_enough_vec( a*5, vec2.new(40,15) ), "a*5" )
+assert( close_enough_vec( a*b, vec2.new(24,6) ), "a*b" )
+assert( close_enough_vec( a/b, vec2.new(8/3, 3/2) ), "a/b" )
 
 local p = vec2.newP( 10, 3 )
 assert( close_enough( p:mod(), 10 ) and close_enough( p:angle(), 3 ), "p:newP()" )
+
+local v = vec2.new( 57, 13 )
+v:normalize()
+assert( close_enough( v:mod(), 1 ), "v:normalize()" )
 
 local s1 = vec2.new( 2, 2 )
 local e1 = vec2.new( 4, 2 )
 local s2 = vec2.new( 3, 1 )
 local e2 = vec2.new( 3, 3 )
-assert( close_enough_vec( vec2.collideLineLine( s1, e1, s2, e2 ), vec2.new(3,2) ) )
+assert( close_enough_vec( vec2.collideLineLine( s1, e1, s2, e2 ), vec2.new(3,2) ), "line segment collision 1" )
 
 local off = vec2.new( 3, 0 )
-assert( vec2.collideLineLine( s1+off, e1+off, s2, e2 )==nil )
+assert( vec2.collideLineLine( s1+off, e1+off, s2, e2 )==nil, "line segment collision 2" )
 
-assert( test_collision( table.pack(vec2.collideCircleLine(
+assert( test_collision( {vec2.collideCircleLine(
    vec2.new( 2, 2 ),
    1,
    vec2.new( 2, 0 ),
    vec2.new( 2, 4 )
-)), {vec2.new(2,1), vec2.new(2,3)} ))
+)}, {vec2.new(2,1), vec2.new(2,3)} ), "circle line collision 1")
 
-assert( test_collision( table.pack(vec2.collideCircleLine(
+assert( test_collision( {vec2.collideCircleLine(
    vec2.new( 2, 2 ),
    1,
    vec2.new( 0, 2 ),
    vec2.new( 4, 2 )
-)), {vec2.new(1,2), vec2.new(3,2)} ))
+)}, {vec2.new(1,2), vec2.new(3,2)} ), "circle line collision 2")
 
-assert( test_collision( table.pack(vec2.collideCircleLine(
+assert( test_collision( {vec2.collideCircleLine(
    vec2.new( 3, 3 ),
    1,
    vec2.new( 0, 2 ),
    vec2.new( 4, 2 )
-)), {vec2.new(3,2), nil} ))
+)}, {vec2.new(3,2), nil} ), "circle line collision 3")
 
-assert( test_collision( table.pack(vec2.collideCircleLine(
+assert( test_collision( {vec2.collideCircleLine(
    vec2.new( 4, 4 ),
    1,
    vec2.new( 0, 2 ),
    vec2.new( 4, 2 )
-)), {nil, nil} ))
+)}, {nil, nil} ), "circle line collision 4")
 
-assert( test_collision( table.pack(vec2.collideCircleLine(
+assert( test_collision( {vec2.collideCircleLine(
    vec2.new( 4, 4 ),
    10,
    vec2.new( 0, 2 ),
    vec2.new( 4, 2 )
-)), {vec2.new(0,2), vec2.new(4,2)} ))
+)}, {vec2.new(0,2), vec2.new(4,2)} ), "circle line collision 5")
         "#,
     )
     .set_name("mlua Vec2 test")
