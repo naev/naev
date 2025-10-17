fa7880ef9a45e4f7aa8de2cc80c4eef086653268
Edgar Simo-Serra <bobbens@gmail.com>
2025-10-17 11:36:26 +0900

    Fixed luacheckrc_extractor.sh to find the AI module.

--- utils/luacheckrc_extractor.sh
+++ utils/luacheckrc_extractor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-readarray -t FILES <<< "$(git ls-files "*/nlua_*.c")"
+readarray -t FILES <<< "$(git ls-files "src/nlua_*.c" "src/ai.c" )"
 ./utils/luacheckrc_extractor.py "${FILES[@]}" --output utils/luacheckrc_gen.lua
