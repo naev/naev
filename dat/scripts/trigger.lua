6b5e6f09127a94015fcf2bf4ace42998d406e987
Edgar Simo-Serra <bobbens@gmail.com>
2025-10-25 21:48:58 +0900

    trigger: Allow passing parameters to the callback functions.

--- dat/scripts/trigger.lua
+++ dat/scripts/trigger.lua
@@ -11,24 +11,26 @@ local lib = {}
 
 function _hook_distance( params )
    if player.pos():dist2( params.target ) <= params.dist2 then
-      params.fn()
+      params.fn( params.fnparams )
    else
       hook.timer( 1, "_hook_distance", params )
    end
 end
 
 --[[--
-Runs a function 'fn' when the player is within a distance of a target position.
+Runs a function 'fn' once when the player is within a distance of a target position.
 
    @tparam vec2 target Target position.
    @tparam number distance Distance from the target position to trigger.
    @tparam function fn Function to run when within the distance.
+   @param fnparams Parameter to pass to fn.
 --]]
-function lib.distance_player( target, distance, fn )
+function lib.distance_player( target, distance, fn, fnparams )
    local params = {
       target   = target,
       fn       = fn,
       dist2    = distance^2,
+      fnparams = fnparams,
    }
    hook.timer( 1, "_hook_distance", params )
 end
@@ -50,7 +52,7 @@ local function pilot_defeated( plt, params )
 
    -- Run hook if necessary
    if #params.pilots <= 0 then
-      params.fn()
+      params.fn( params.fnparams )
    end
 end
 function _hook_pilot_defeated( plt, _killer, params )
@@ -69,12 +71,14 @@ Use pilot:setNoDeath() or pilot:setNoDisable() if you want the pilots to either 
 
    @tparam {Pilot} pilots Pilots to check when they are all defeated, which is disabled or destroyed.
    @tparam function fn Function to call when all the pilots are defeated.
+   @param fnparams Parameter to pass to fn.
 --]]
-function lib.pilots_defeated( pilots, fn )
+function lib.pilots_defeated( pilots, fn, fnparams )
    local params = {
       pilots      = pilots,
       fn          = fn,
       permdisable = true,
+      fnparams    = fnparams,
    }
    for k,p in ipairs(pilots) do
       hook.pilot( p, "death", "_hook_pilot_defeated", params )
