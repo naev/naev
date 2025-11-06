4ed8453ff731446a30234f0332d7f6bb6e59be24
Edgar Simo-Serra <bobbens@gmail.com>
2025-11-06 20:29:58 +0900

    Started to work on more Rust SDF shaders.

--- src/renderer/src/sdf.rs
+++ src/renderer/src/sdf.rs
@@ -15,6 +15,14 @@ pub struct CrossUniform {
     pub radius: f32,
 }
 
+#[repr(C)]
+#[derive(Debug, Copy, Clone, ShaderType)]
+pub struct CircleUniform {
+    pub transform: Matrix3<f32>,
+    pub colour: Vector4<f32>,
+    pub radius: f32,
+}
+
 macro_rules! draw_sdf_func_ex {
     ($funcname: ident, $uniform: ty, $program: tt, $buffer: tt) => {
         pub fn $funcname(&self, ctx: &Context, uniform: &$uniform) -> Result<()> {
@@ -35,6 +43,8 @@ macro_rules! draw_sdf_func_ex {
 pub struct SdfRenderer {
     program_cross: Shader,
     buffer_cross: Buffer,
+    program_circle: Shader,
+    buffer_circle: Buffer,
 }
 impl SdfRenderer {
     pub fn new(gl: &glow::Context) -> Result<Self> {
@@ -48,9 +58,21 @@ impl SdfRenderer {
             .data(&TextureUniform::default().buffer()?)
             .build(gl)?;
 
+        let program_circle = ProgramBuilder::new(Some("Circle Shader"))
+            .uniform_buffer("circledata", 0)
+            .wgsl_file("circle.wgsl")
+            .build(gl)?;
+        let buffer_circle = BufferBuilder::new(Some("Circle Buffer"))
+            .target(BufferTarget::Uniform)
+            .usage(BufferUsage::Dynamic)
+            .data(&TextureUniform::default().buffer()?)
+            .build(gl)?;
+
         Ok(Self {
             program_cross,
             buffer_cross,
+            program_circle,
+            buffer_circle,
         })
     }
 
@@ -76,11 +98,34 @@ impl SdfRenderer {
         };
         self.draw_cross_ex(ctx, &uniform)
     }
-
     draw_sdf_func_ex!(draw_cross_ex, CrossUniform, program_cross, buffer_cross);
+
+    pub fn draw_circle(
+        &self,
+        ctx: &Context,
+        x: f32,
+        y: f32,
+        r: f32,
+        colour: Vector4<f32>,
+    ) -> Result<()> {
+        let dims = ctx.dimensions.read().unwrap();
+        #[rustfmt::skip]
+        let transform: Matrix3<f32> = dims.projection * Matrix3::new(
+             r,  0.0,  x,
+            0.0,  r,   y,
+            0.0, 0.0, 1.0,
+        );
+        let uniform = CircleUniform {
+            transform,
+            colour,
+            radius: r,
+        };
+        self.draw_circle_ex(ctx, &uniform)
+    }
+    draw_sdf_func_ex!(draw_circle_ex, CircleUniform, program_circle, buffer_circle);
 }
 
-use std::ffi::c_double;
+use std::ffi::c_double; //{c_double, c_int};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn gl_renderCross(x: c_double, y: c_double, r: c_double, c: *const Vector4<f32>) {
@@ -93,3 +138,23 @@ pub extern "C" fn gl_renderCross(x: c_double, y: c_double, r: c_double, c: *cons
         .sdf
         .draw_cross(&ctx, x as f32, y as f32, r as f32, colour);
 }
+
+/*
+#[unsafe(no_mangle)]
+pub extern "C" fn gl_renderCircle(
+    x: c_double,
+    y: c_double,
+    r: c_double,
+    c: *const Vector4<f32>,
+    _filled: c_int,
+) {
+    let ctx = Context::get();
+    let colour = match c.is_null() {
+        true => Vector4::<f32>::from([1.0, 1.0, 1.0, 1.0]),
+        false => unsafe { *c },
+    };
+    let _ = ctx
+        .sdf
+        .draw_circle(&ctx, x as f32, y as f32, r as f32, colour);
+}
+*/
