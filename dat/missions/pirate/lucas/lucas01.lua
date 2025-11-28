2ba0c354c5bfa762904591b5392b9d72ed65dd35
Jack Greiner <jack@emoss.org>
2025-11-28 01:16:21 -0500

    lucas01.lua: Some grammar and wording improvements.

--- dat/missions/pirate/lucas/lucas01.lua
+++ dat/missions/pirate/lucas/lucas01.lua
@@ -82,8 +82,8 @@ function accept ()
    local lucas = vn.newCharacter( lcs.vn_lucas() )
    vn.transition( lcs.lucas.transition )
 
-   vn.na(_([[You approach the stressed out individual gnawing at their nails at the bar.]]))
-   lucas(_([["Say, you wouldn't be able to help me find my family? I'll pay, I promise!"]]))
+   vn.na(_([[You approach the stressed-out individual gnawing at their nails at the bar.]]))
+   lucas(_([["Any chance you could help me find my family? I'll pay, I promise!"]]))
    vn.menu{
       {_([[Accept]]), "accept"},
       {_([[Refuse]]), "refuse"},
@@ -97,15 +97,15 @@ function accept ()
    vn.func( function ()
       accepted = true
    end )
-   lucas(_([["My name is Lucas, I was a Nebula refugee. When I was very young, my family and I escaped the Incident, but I don't know what happened and I got separated."]]))
-   lucas(_([["I guess I never really thought about it too much, just did what I was told and went with the flow, but then one day I couldn't. You understand?"]]))
-   lucas(_([[The pause and tighten their fists.
-"I guess there was always something missing in me, like something fundamental was taken away by the Incident. However, I just didn't realize it, didn't understand."]]))
-   lucas(_([["One day it snapped, and I had a dream remembering stuff I had forgotten about: my family. I guess I had repressed it for so long. Survival instincts maybe?"]]))
-   lucas(_([["Since then I've been trying to find my family with the credits I was able to scrounge up working. I don't think they had as much luck as I had, they are probably stuck on some refugee world. The governments don't care for us at all. They just toss refugees into barely habitable planets and turn their eyes away. We are humans too!"]]))
-   lucas(fmt.f(_([["I've narrowed it down a bit, I think they should be on {spb} in the {sys} system. Please try to find them! I'll give you all the information I have. Here take this locket, it is the only thing I have from them."]]),
+   lucas(_([["My name is Lucas. I'm a Nebula refugee. When I was very young, my family and I escaped the Incident, but I don't know what happened-we got separated."]]))
+   lucas(_([["I never really thought about it; I just did what I was told and went with the flow, until one day I couldn't anymore. You understand?"]]))
+   lucas(_([[They pause and clench their fists.
+"There was always something missing in me, like something fundamental the Incident took away, but I didn't realize it, didn't understand."]]))
+   lucas(_([["Then something snapped. I had a dream that brought back what I'd buried—my family. I guess I'd repressed it for so long. Survival instincts maybe?"]]))
+   lucas(_([["Since then I've been trying to find them with the credits I've managed to scrape together. I don't think they were as lucky; they're probably stuck on some refugee world. The governments don't care for us at all. They just toss refugees onto barely habitable planets and look away. We're human too!"]]))
+   lucas(fmt.f(_([["I've narrowed it down a bit: I think they're on {spb} in the {sys} system. Please try to find them! I'll give you all the information I have. Here, take this locket—it's the only thing I have from them."]]),
       {spb=mem.first_spob, sys=mem.first_sys}))
-   vn.na(_([[They hand you an old locket that looks like is missing half of it. It is fairly simple and made of some resistant metal alloy, but bears signs of heavy use.]]))
+   vn.na(_([[They hand you an old locket that looks like it's missing half. It's fairly simple and made of some rugged metal alloy, but bears signs of heavy use.]]))
    lucas(_([["I'll be waiting here."]]))
    vn.done( lcs.lucas.transition )
    vn.run()
@@ -138,14 +138,14 @@ function land ()
       vn.scene()
       vn.transition()
 
-      vn.na(_([[You land and begin to ask around to see if there are any traces of Lucas family.]]))
-      vn.na(fmt.f(_([[As you converse with many of the refugees at {spb}, you begin to appreciate the magnitude of the Incident calamity. Many refugees are missing or searching for family members, with deep psychological scars that are unable to heal.]]),
+      vn.na(_([[You land and begin to ask around to see if there are any traces of Lucas' family.]]))
+      vn.na(fmt.f(_([[As you converse with many of the refugees at {spb}, you begin to appreciate the calamity of the Incident. Many refugees are missing or searching for family members, carrying deep psychological scars that won't heal.]]),
          {spb=spb}))
-      vn.na(fmt.f(_([[Feeling like searching for a needle in a haystack, you are almost about to give up when you find an older one-armed refugee. They take a close look at the locket and mention that they used to share a cell with someone using the same locket on {spb} in the {sys} system.]]),
+      vn.na(fmt.f(_([[Feeling like you're searching for a needle in a haystack, you're almost ready to give up when you find an older one-armed refugee. They take a close look at the locket and mention they used to share a cell with someone who had the same locket on {spb} in the {sys} system.]]),
          {spb=mem.last_spob, sys=mem.last_sys}))
-      vn.na(fmt.f(_([[From their story, it seems like {spb} is a horrible traumatic place, a refugee limbo where atrocities are commonplace. It also seems like the planet is locked down, you'll likely have to bribe the authorities to access it.]]),
+      vn.na(fmt.f(_([[From their story, {spb} sounds horribly traumatic, a refugee limbo where atrocities are commonplace. The planet is locked down; you'll likely have to bribe the authorities to access it.]]),
          {spb=mem.last_spob}))
-      vn.na(_([[They give you the location of the cell where they were and wish you luck. Looks like you have a lead.]]))
+      vn.na(_([[They give you the location of the cell where they were and wish you luck. Looks like you finally have a lead.]]))
 
       vn.run()
 
@@ -163,9 +163,9 @@ function land ()
       vn.scene()
       vn.transition()
 
-      vn.na(fmt.f(_([[You manage to land on {spb}. Although things are quite quiet and clean around the spaceport, it all takes a turn for the worse once you pass the checkpoints and head into the slums towards the location you were given.]]),
+      vn.na(fmt.f(_([[You manage to land on {spb}. Although things are quite quiet and clean around the spaceport, it all takes a turn for the worse once you pass the checkpoints and head into the slums toward the location you were given.]]),
          {spb=spb}))
-      vn.na(_([[The atmosphere is dark, almost oppressive, and you feel like you are given many stares by the locals. It seems likely that they do not get many visitors.]]))
+      vn.na(_([[The atmosphere is dark, almost oppressive, and you feel the locals' stares. They clearly don't get many visitors.]]))
       vn.na(_([[Eventually you get to the cell you were told about. It seems to be locked. You take a look to see if you can override it.]]))
       mg.vn()
       vn.func( function ()
@@ -180,10 +180,10 @@ function land ()
             return
          end
       end )
-      vn.na(_([[You open the cell and find yourself with a very weakened old man. At first, they are scared to death, believing you have come to take their meagre possessions or their life, however, you are able to calm them down when you mention that Lucas sent you, and pass out.]]))
-      vn.na(_([[You quickly look around to see if there is anyone else there. Once you confirm that the man is the only person you pick them up and make your way back to the spaceport. Likely due to malnourishment, the old man is a much lighter load than you had expected. On the way back, surprisingly enough, people seem to take less notice of you than when you came in.]]))
+      vn.na(_([[You open the cell to find a very weakened old man. At first they are terrified, thinking you've come to take their meagre possessions or their life, but when you mention Lucas sent you they ease for a moment before collapsing from shock.]]))
+      vn.na(_([[You quickly look around to see if there is anyone else there. Once you confirm the man is alone, you pick him up and make your way back to the spaceport. Likely due to malnourishment, the old man is a much lighter load than you had expected. On the way back, surprisingly enough, people seem to take less notice of you than when you came in.]]))
       vn.na(_([[Eventually you reach the spaceport checkpoint. The guards raise an eyebrow at the old man you're carrying, but once you show them the receipt of your spaceship, they let you through.]]))
-      vn.na(_([[The old man is in not very good shape and seems to fall into a deep slumber when you set them aboard the ship, would be best to try to head back to Lucas as soon as possible.]]))
+      vn.na(_([[The old man is in rough shape and falls into a deep slumber when you set him aboard the ship. It would be best to head back to Lucas as soon as possible.]]))
       vn.done()
 
       vn.label("failed")
@@ -199,11 +199,11 @@ function land ()
       vn.transition( lcs.lucas. transition )
 
       vn.na(_([[You land and quickly go find Lucas, who then follows you back to your ship.]]))
-      lucas(_([[When Lucas sees the old man, his eyes tear up, and he kneels to take a closer look.
-"Dear old man, what have they done to you?"]]))
-      lucas(_([[Still kneeling, Lucas shuffles forward and emotively hugs the old man. Probably not the reunion they were looking for, but better than nothing nonetheless.]]))
+      lucas(_([[When Lucas sees the old man, his eyes well up and he kneels to take a closer look.
+"Dear old man, what did they do to you?"]]))
+      lucas(_([[Still kneeling, Lucas shuffles forward and hugs the old man tightly. Probably not the reunion he was looking for, but better than nothing nonetheless.]]))
       lucas(_([[After a solemn while, Lucas kisses the old man on the cheek and turns to you.
-"Thank you for what you've done. I think I can take care of it from now on. I have to see what happened to the others."]]))
+"Thank you for everything. I can take it from here. I have to see what happened to the others."]]))
       vn.na(_([[You help Lucas take the old man off the ship, and also return the locket. Lucas thanks you fervently for all you've done and hands you a credit chip. He then heads off with his father towards the nearest medical centre.]]))
 
       vn.sfxVictory()
