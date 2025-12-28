570f84f99052c46e5b6d9d37b5eca4ea4193b6bd
Edgar Simo-Serra <bobbens@gmail.com>
2025-12-28 15:49:03 +0900

    Moved AudioData Lua into its own file.

--- src/nlua.rs
+++ src/nlua.rs
@@ -547,7 +547,7 @@ impl LuaEnv {
         open_lib("colour", renderer::colour::open_colour)?;
         open_lib("rnd", crate::rng::open_rnd)?;
         open_lib("spfx", crate::spfx::open_spfx)?;
-        open_lib("audiodata", audio::open_audiodata)?;
+        open_lib("audiodata", audio::lua::open_audiodata)?;
         open_lib("audio", audio::open_audio)?;
         open_lib("data", ndata::data::open_data)?;
         open_lib("file", ndata::lua::open_file)?;
