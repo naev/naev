feb74cee4019c04a3b0a6116cf08da2f5fa90720
Edgar Simo-Serra <bobbens@gmail.com>
2025-12-29 21:11:38 +0900

    Fixed find_nlua.sh to work without readarray.

--- utils/find_nlua.sh
+++ utils/find_nlua.sh
@@ -3,8 +3,13 @@
 cd "$1" || exit
 if [ -d .git ]; then
    unset GIT_INDEX_FILE  # Don't let whatever meson does fuck up the results.
-   readarray -t LSFILES <<< "$(git ls-files "src/*.c" "src/**/*.c" "src/*.rs" "src/**/*.rs")"
+   while read -r -a line; do
+      LSFILES+=("${line[0]}")
+   done < <(git ls-files "src/*.c" "src/**/*.c" "src/*.rs" "src/**/*.rs")
 else
-   readarray -t LSFILES <<< "$(find src/ -name "*.c" -or -name "*.rs")"
+   while read -r -a line; do
+      LSFILES+=("${line[0]}")
+   done < <(find src/ -name "*.c" -or -name "*.rs")
 fi
+
 grep -l '@luamod' "${LSFILES[@]}"
