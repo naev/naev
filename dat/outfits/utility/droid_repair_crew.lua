7edd92b41b72694290920587a572a7424f1da0d8
Edgar Simo-Serra <bobbens@gmail.com>
2026-01-05 22:21:50 +0900

    Define droid repair crew descextra in the Lua.

--- dat/outfits/utility/droid_repair_crew.lua
+++ dat/outfits/utility/droid_repair_crew.lua
@@ -1,6 +1,15 @@
+local fmt = require "format"
+
+local THRESHOLD = 50
+
+function descextra( _p, _o, _po )
+   return fmt.f(_("Can only restore armour when below {threshold}%."),
+      {threshold = THRESHOLD})
+end
+
 function update( p, po )
    local a = p:health()
-   if a > 50 then
+   if a > THRESHOLD then
       po:state( "off" )
    else
       po:state( "on" )
