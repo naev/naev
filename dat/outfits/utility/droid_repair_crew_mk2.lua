defd86b1309ba917d5583e459f4ae3f36a698e56
Edgar Simo-Serra <bobbens@gmail.com>
2026-01-06 14:09:22 +0900

    Added Droid Repair Crew MK2.

---
+++ dat/outfits/utility/droid_repair_crew_mk2.lua
@@ -0,0 +1,17 @@
+local fmt = require "format"
+
+local THRESHOLD = 35
+
+function descextra( _p, _o, _po )
+   return fmt.f(_("Can only restore armour when below {threshold}%."),
+      {threshold = THRESHOLD})
+end
+
+function update( p, po )
+   local a = p:health()
+   if a > THRESHOLD then
+      po:state( "off" )
+   else
+      po:state( "on" )
+   end
+end
