29da8b3443a8a8828579617158b74b5f6bf924f8
Edgar Simo-Serra <bobbens@gmail.com>
2026-01-10 16:54:04 +0900

    Update bioplasma and razor to use the new API instead of duplicating code.

--- dat/outfits/weapons/razor.lua
+++ dat/outfits/weapons/razor.lua
@@ -1,6 +1,6 @@
 local fmt = require "format"
 
-local disable, penetration, isturret, duration, energy
+local disable, duration, energy
 duration = 3
 function onload( o )
    local s     = o:specificstats()
@@ -8,8 +8,6 @@ function onload( o )
    -- around disable.
    disable     = s.disable*0.5
    energy      = s.disable*1.5
-   penetration = s.penetration
-   isturret    = s.isturret
 end
 
 function descextra( _p )
@@ -17,20 +15,7 @@ function descextra( _p )
       {disable=disable, energy=energy, duration=duration})
 end
 
-function onimpact( p, target )
-   local ts = target:stats()
-   local dmg = disable * (1 - math.min( 1, math.max( 0, ts.absorb - penetration ) ))
-
-   -- Modify by damage
-   if p:exists() then
-      local mod
-      if isturret then
-         mod = p:shipstat("tur_damage",true)
-      else
-         mod = p:shipstat("fwd_damage",true)
-      end
-      dmg = dmg * mod * p:shipstat("weapon_damage",true)
-   end
-
+function onimpact( p, target, _pos, _vel, _o, armour, shield )
+   local dmg = armour + shield
    target:effectAdd( "Ionization", duration, dmg, p )
 end
