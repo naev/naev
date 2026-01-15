cbfaa4ba0ecb7f7f32cdaa01470c9935101bc8ae
hsza <hsza@noreply.codeberg.org>
2026-01-15 10:56:32 +0000

    add new cool dvaered ancestor variant

--- dat/factions/spawn/dvaered/default.lua
+++ dat/factions/spawn/dvaered/default.lua
@@ -1,7 +1,7 @@
 local scom = require "factions.spawn.lib.common"
+local var = require "shipvariants"
 
 local svendetta   = ship.get("Dvaered Vendetta")
-local sancestor   = ship.get("Dvaered Ancestor")
 local sphalanx    = ship.get("Dvaered Phalanx")
 local svigilance  = ship.get("Dvaered Vigilance")
 local sretribution= ship.get("Dvaered Retribution")
@@ -11,17 +11,17 @@ local sarsenal    = ship.get("Dvaered Arsenal")
 -- @brief Spawns a small patrol fleet.
 local function spawn_patrol ()
    return scom.doTable( { __doscans = true }, {
-      { w=0.5, svendetta, sancestor },
-      { w=0.8, svendetta, svendetta, sancestor },
-      { sphalanx, svendetta, sancestor },
+      { w=0.5, svendetta, var.dvaered_ancestor },
+      { w=0.8, svendetta, svendetta, var.dvaered_ancestor },
+      { sphalanx, svendetta, var.dvaered_ancestor },
    } )
 end
 
 -- @brief Spawns a medium sized squadron.
 local function spawn_squad ()
    return scom.doTable( { __doscans = (rnd.rnd() < 0.5) }, {
-      { w=0.5, svigilance, svendetta, sancestor },
-      { w=0.8, svigilance, svendetta, svendetta, sancestor },
+      { w=0.5, svigilance, svendetta, var.dvaered_ancestor },
+      { w=0.8, svigilance, svendetta, svendetta, var.dvaered_ancestor },
       { svigilance, sphalanx, svendetta },
    } )
 end
@@ -37,8 +37,8 @@ local function spawn_capship ()
 
    -- Generate the escorts
    return scom.doTable( pilots, {
-      { w=0.5, svendetta, svendetta, sancestor },
-      { w=0.8, sphalanx, svendetta, sancestor },
+      { w=0.5, svendetta, svendetta, var.dvaered_ancestor },
+      { w=0.8, sphalanx, svendetta, var.dvaered_ancestor },
       { svigilance, svendetta, svendetta },
    } )
 end
