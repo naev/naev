ac56f66cb86c08f43d79967eade1e280cf9ad765
Edgar Simo-Serra <bobbens@gmail.com>
2026-01-18 16:55:32 +0900

    More clarification about plugin identifiers.

--- src/pluginmgr/src/plugin.rs
+++ src/pluginmgr/src/plugin.rs
@@ -39,7 +39,7 @@ impl<'de> de::Deserialize<'de> for Identifier {
             } else if !inner.chars().all(|c| c.is_ascii_alphanumeric()) {
                 Err(de::Error::invalid_value(
                     de::Unexpected::Str(&inner),
-                    &"identifier contains non-ascii alphanumeric characters (whitespaces included)",
+                    &"&identifier contains non-alphanumeric or non-ascii characters",
                 ))
             } else if inner.starts_with(ID_ERROR) {
                 Err(de::Error::invalid_value(
