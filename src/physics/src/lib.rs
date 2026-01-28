69a3e43c199faaa7b1ea8f9aa83a1981ad3c2fca
Edgar Simo-Serra <bobbens@gmail.com>
2026-01-28 22:40:58 +0900

    Use TAU.

--- src/physics/src/lib.rs
+++ src/physics/src/lib.rs
@@ -1,25 +1,26 @@
-use std::f64::consts::PI;
+use std::f64::consts::{PI, TAU};
 
 pub mod collide;
+pub mod polygon;
 pub mod transform2;
 pub mod vec2;
 
 /// Converts an angle to the [0, 2*PI] range.
 pub fn angle_clean(a: f64) -> f64 {
     let mut a = a;
-    if a.abs() > 2. * PI {
-        a %= 2. * PI;
+    if a.abs() > TAU {
+        a %= TAU;
     }
-    if a < 0. { a + 2. * PI } else { a }
+    if a < 0. { a + TAU } else { a }
 }
 
 /// Gets the difference between two angles.
 pub fn angle_diff(a: f64, b: f64) -> f64 {
     let d = angle_clean(b) - angle_clean(a);
     if d > PI {
-        d - 2. * PI
+        d - TAU
     } else if d < -PI {
-        d + 2. * PI
+        d + TAU
     } else {
         d
     }
