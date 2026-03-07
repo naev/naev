3ff6db64e86ebdf4f0875ddff7c3be968147e4fc
Edgar Simo-Serra <bobbens@gmail.com>
2026-03-07 12:18:43 +0900

    Start to flesh out trader societ scripts a bit.

---
+++ dat/scripts/factions/trader_society.lua
@@ -0,0 +1,11 @@
+return {
+   fct            = faction.get("Traders Society"),
+   rep_max        = 30,
+   rep_max_var    = "_fcap_trader",
+   destroy_max    = 10,
+   distress_mod   = 0.1, -- They lose from distressing
+   board_mod      = 0.5, -- Dislike being stolen from
+   rep_max_tags   = {
+      ["trader_cap_medium"] = { val=10, max=100 },
+   }
+}
