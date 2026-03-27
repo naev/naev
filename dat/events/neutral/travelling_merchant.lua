01588e7740d625baf04517076615fb9a8061eb7c
hsza <hsza@noreply.codeberg.org>
2026-03-27 06:09:10 +0000

    add accessory to Misi for 2 matrices

--- dat/events/neutral/travelling_merchant.lua
+++ dat/events/neutral/travelling_merchant.lua
@@ -193,6 +193,7 @@ function gen_outfits ()
       { "Terraforming Antlejos 10", "Commemorative Stein" },
       { "DIY Nerds",                "Homebrew Processing Unit" },
       { "Dvaered Shopping",         "Fancy Key Chain" },
+      { "Antivirus",                "AD-USB" },
    }
    local event_rewards = {
    }
