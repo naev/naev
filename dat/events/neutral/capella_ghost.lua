83c7c9ab7294b66c2e1724edee6c8cc6a27a317a
hsza <hsza@noreply.codeberg.org>
2026-03-28 14:06:17 +0000

    add some dialogue on first visit to the system

--- dat/events/neutral/capella_ghost.lua
+++ dat/events/neutral/capella_ghost.lua
@@ -22,6 +22,8 @@
 --]]
 local pilotai = require "pilotai"
 local ai_setup = require "ai.core.setup"
+local vn = require "vn"
+local tut = require "common.tutorial"
 local ghost
 function create ()
    local fct = faction.dynAdd("Dummy", "metaphysical", _("Metaphysical"))
@@ -44,12 +46,27 @@ function create ()
    ghost:setHostile()
    hook.update("update")
    hook.enter("enter")
-   hook.timer(5, "timer")
+   hk_timer = hook.timer(4, "timer")
 end
-local intrinsicd = {}
 local timer_done = false
 function timer()
    timer_done = true
+   if not var.peek("capella_visited") then
+      local sai = tut.vn_shipai()
+      vn.clear()
+      vn.scene()
+      vn.transition()
+      
+      vn.na(_([[As you're getting your bearings after the hyperspace jump, an eerie feeling washes over you. There's something peculiar about this place, and the sensor feeds confirm it is anything but mundane.]]))
+      vn.appear( sai, tut.shipai.transition )
+      sai(_([["There are several abnormalities in the ambient radiation and gravitational readings. Also, a system with this many stars would not be statistically possible under ordinary physics."]]))
+      sai(_([["Something almost like another ship is nearby, but I am unable to pinpoint the exact location, nor assess how much danger we could be in. I would suggest flying in stealth mode and exercising heavy caution as long as we are here.]]))
+      vn.na(_([[You can feel a presence start to stir against your very being, as if you were being watched. It may not be wise to linger.]]))
+      vn.disappear( sai, tut.shipai.transition )
+      
+      vn.run()
+      var.push("capella_visited", true)
+   end
 end
 function update( dt )
    if not ghost:exists() then return end
@@ -58,18 +75,10 @@ function update( dt )
       p:damage( dmg * 5.5, dmg * 3.5, 5., "energy", ghost )
    end
    if timer_done then
-      ghost:intrinsicSet("ew_detect", ghost:intrinsicGet("ew_detect") + 0.155 * dt, true) -- some risk/reward over time: being unstealthed/getting to mine more
-      for k,p in ipairs(pilot.get()) do
-         p:intrinsicSet("mining_bonus", p:intrinsicGet("mining_bonus") + 0.155 * dt, true)
-         p:intrinsicSet("jump_warmup", p:intrinsicGet("jump_warmup") + 0.310 * dt, true) -- ...and a tiny sprinkle of evil
-         if not inlist(intrinsicd, p) then table.insert(intrinsicd, p) end
-      end
+      ghost:intrinsicSet("ew_detect", ghost:intrinsicGet("ew_detect") + 0.160 * dt, true) -- some risk/reward over time: being unstealthed/getting to mine more
    end
 end
 function enter ()
-   for k,p in ipairs(intrinsicd) do if p:exists() then 
-   p:intrinsicSet("mining_bonus", 0, true) 
-   p:intrinsicSet("jump_warmup", 0, true) 
-   end end
+   hook.rm(hk_timer)
    evt.finish()
 end
