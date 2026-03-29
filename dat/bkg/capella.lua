e70f08042dbb4f5bf1cdf7f9165e256532ed113b
Edgar Simo-Serra <bobbens@gmail.com>
2026-03-29 21:24:01 +0900

    [pre-commit run -a]

--- dat/bkg/capella.lua
+++ dat/bkg/capella.lua
@@ -6,12 +6,12 @@ function background ()
    local c = time.sub(time.cur(), time.new( 593, 3726, 4663 )):split() -- ""Fake"" stars
    local n = 4 -- Base stars
    starfield.init( { num_stars = n } )
-   
+
    local added = {}
    for i = 1, c do
       local num = starfield.star_add( added, n+i, true )
       added[ num ] = true
    end
-   
+
 end
 renderbg = starfield.render
