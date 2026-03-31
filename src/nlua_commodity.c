d42250b5d6b42a4639d4c2d3c265c2c02e0c4ed5
Edgar Simo-Serra <bobbens@gmail.com>
2026-03-31 15:56:18 +0900

    More removing unnecessary code.

--- src/nlua_commodity.c
+++ src/nlua_commodity.c
@@ -1,73 +1,8 @@
 /*
  * See Licensing and Copyright notice in naev.h
  */
-/**
- * @file nlua_commodity.c
- *
- * @brief Handles the Lua commodity bindings.
- */
-/** @cond */
-#include <lauxlib.h>
 
-#include "naev.h"
-/** @endcond */
-
-#include "nlua_commodity.h"
-
-#include "array.h"
-#include "ndata.h"
-#include "nlua_faction.h"
-#include "nlua_spob.h"
-#include "nlua_system.h"
-#include "nlua_tex.h"
-#include "nlua_time.h"
-#include "nluadef.h"
-
-/* Commodity metatable methods. */
-static int commodityL_eq( lua_State *L );
-static int commodityL_get( lua_State *L );
-static int commodityL_getAll( lua_State *L );
-static int commodityL_exists( lua_State *L );
-static int commodityL_getStandard( lua_State *L );
-static int commodityL_flags( lua_State *L );
-static int commodityL_name( lua_State *L );
-static int commodityL_nameRaw( lua_State *L );
-static int commodityL_price( lua_State *L );
-static int commodityL_priceAt( lua_State *L );
-static int commodityL_priceAtTime( lua_State *L );
-static int commodityL_canSell( lua_State *L );
-static int commodityL_canBuy( lua_State *L );
-static int commodityL_icon( lua_State *L );
-static int commodityL_description( lua_State *L );
-static int commodityL_new( lua_State *L );
-static int commodityL_illegalto( lua_State *L );
-static int commodityL_illegality( lua_State *L );
-static int commodityL_tags( lua_State *L );
-
-static const luaL_Reg commodityL_methods[] = {
-   { "__tostring", commodityL_name },
-   { "__eq", commodityL_eq },
-   { "get", commodityL_get },
-   { "getAll", commodityL_getAll },
-   { "exists", commodityL_exists },
-   { "getStandard", commodityL_getStandard },
-   { "flags", commodityL_flags },
-   { "name", commodityL_name },
-   { "nameRaw", commodityL_nameRaw },
-   { "price", commodityL_price },
-   { "priceAt", commodityL_priceAt },
-   { "priceAtTime", commodityL_priceAtTime },
-   { "canSell", commodityL_canSell },
-   { "canBuy", commodityL_canBuy },
-   { "icon", commodityL_icon },
-   { "description", commodityL_description },
-   { "new", commodityL_new },
-   { "illegalto", commodityL_illegalto },
-   { "illegality", commodityL_illegality },
-   { "tags", commodityL_tags },
-   { 0, 0 } }; /**< Commodity metatable methods. */
-
-/**
+/*
  * @brief Gets the flags that are set for a commodity.
  *
  *    @luatreturn table A table containing the flags as key and value as
@@ -91,74 +26,6 @@ static int commodityL_flags( lua_State *L )
    return 1;
 }
 
-/**
- * @brief Gets the base price of an commodity on a certain spob.
- *
- * @usage if c:priceAt( spob.get("Polaris Prime") ) > 100 then -- Checks price
- * of a commodity at Polaris Prime
- *
- *    @luatparam Commodity c Commodity to get information of.
- *    @luatparam Spob p Spob to get price at.
- *    @luatreturn number The price of the commodity at the spob.
- * @luafunc priceAt
- */
-static int commodityL_priceAt( lua_State *L )
-{
-   CommodityRef      c;
-   const Spob       *p;
-   const StarSystem *sys;
-   const char       *sysname;
-
-   c       = luaL_validcommodity( L, 1 );
-   p       = luaL_validspob( L, 2 );
-   sysname = spob_getSystemName( p->name );
-   if ( sysname == NULL )
-      return NLUA_ERROR( L, _( "Spob '%s' does not belong to a system." ),
-                         p->name );
-   sys = system_get( sysname );
-   if ( sys == NULL )
-      return NLUA_ERROR( L, _( "Spob '%s' can not find its system '%s'." ),
-                         p->name, sysname );
-
-   lua_pushnumber( L, spob_commodityPrice( p, c ) );
-   return 1;
-}
-
-/**
- * @brief Gets the price of an commodity on a certain spob at a certain time.
- *
- * @usage if c:priceAtTime( spob.get("Polaris Prime"), time ) > 100 then --
- * Checks price of a commodity at Polaris Prime
- *
- *    @luatparam Commodity c Commodity to get information of.
- *    @luatparam Spob p Spob to get price at.
- *    @luatparam Time t Time to get the price at.
- *    @luatreturn number The price of the commodity at the spob.
- * @luafunc priceAtTime
- */
-static int commodityL_priceAtTime( lua_State *L )
-{
-   CommodityRef      c;
-   const Spob       *p;
-   const StarSystem *sys;
-   const char       *sysname;
-   ntime_t           t;
-   c       = luaL_validcommodity( L, 1 );
-   p       = luaL_validspob( L, 2 );
-   t       = luaL_validtime( L, 3 );
-   sysname = spob_getSystemName( p->name );
-   if ( sysname == NULL )
-      return NLUA_ERROR( L, _( "Spob '%s' does not belong to a system." ),
-                         p->name );
-   sys = system_get( sysname );
-   if ( sys == NULL )
-      return NLUA_ERROR( L, _( "Spob '%s' can not find its system '%s'." ),
-                         p->name, sysname );
-
-   lua_pushnumber( L, spob_commodityPriceAtTime( p, c, t ) );
-   return 1;
-}
-
 static int spob_hasCommodity( CommodityRef c, const Spob *s )
 {
    for ( int i = 0; i < array_size( s->commodities ); i++ ) {
@@ -170,7 +37,7 @@ static int spob_hasCommodity( CommodityRef c, const Spob *s )
    return 0;
 }
 
-/**
+/*
  * @brief Sees if a commodity can be sold at either a spob or system.
  *
  * It does not check faction standings, only if it is possible to sell at a spob
@@ -208,7 +75,7 @@ static int commodityL_canSell( lua_State *L )
    return 1;
 }
 
-/**
+/*
  * @brief Sees if a commodity can be bought at either a spob or system.
  *
  * It does not check faction standings, only if it is possible to buy at a spob
