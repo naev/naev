136bf44bcdfc03ef2faac7b873ad981e3b5bbbee
johann1764 <johann.brault_baron@yahoo.com>
2026-04-03 23:00:37 +0200

    bugfix

--- dat/utils/ssys/fmt_numbers.sh
+++ dat/utils/ssys/fmt_numbers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DAT=$(realpath --relative-to="$PWD" "${DIR}/../../")
 
 sed -e 's/\([0-9]*[1-9][0-9]*\.[0-9]\{6\}\)[0-9]*/\1/g' \
