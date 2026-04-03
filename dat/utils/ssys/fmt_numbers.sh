52c19c15242a35340eee6e22acbcc36fae30317f
johann1764 <johann.brault_baron@yahoo.com>
2026-04-03 02:51:53 +0200

    oopsie

--- dat/utils/ssys/fmt_numbers.sh
+++ dat/utils/ssys/fmt_numbers.sh
@@ -5,4 +5,4 @@ DAT=$(realpath --relative-to="$PWD" "${DIR}/../../")
 sed -e 's/\([0-9]*[1-9][0-9]*\.[0-9]\{6\}\)[0-9]*/\1/g' \
     -e 's/\([0-9]\+\.[0-9]*[1-9]\)0*\([^0-9]\)/\1\2/g' \
     -e 's/\([0-9]\+\)\.0*\([^0-9]\)/\1\2/g' \
-    -i ssys/*.xml spob/*.xml map_decorator/*.xml
+    -i "$DAT"/ssys/*.xml "$DAT"/spob/*.xml "$DAT"/map_decorator/*.xml
