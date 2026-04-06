e0c1fe5360541bbd5aa8d1b4a154e36746b0f018
Edgar Simo-Serra <bobbens@gmail.com>
2026-04-06 18:31:34 +0900

    Fix pirate lua when pilot not initialized.

--- dat/ships/lua/pirate.lua
+++ dat/ships/lua/pirate.lua
@@ -1,8 +1,10 @@
 function init( p )
-   -- Pirates get twice the effect of stellar winds
-   if system.cur():tags().stellarwind then
-      p:shippropSet( "fuel_regen", 1 )
-   else
-      p:shippropSet( "fuel_regen", 0 )
+   if p then
+      -- Pirates get twice the effect of stellar winds
+      if system.cur():tags().stellarwind then
+         p:shippropSet( "fuel_regen", 1 )
+      else
+         p:shippropSet( "fuel_regen", 0 )
+      end
    end
 end
