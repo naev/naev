819c5822bf833718d7fef98b888a4b589256e65c
Edgar Simo-Serra <bobbens@gmail.com>
2026-04-07 15:33:40 +0900

    Added missing file.

---
+++ src/asteroid.rs
@@ -0,0 +1,96 @@
+#![allow(dead_code)]
+use crate::commodity::CommodityRef;
+use collide::polygon::SpinPolygon;
+use renderer::texture::Texture;
+use std::sync::Arc;
+
+struct Material {
+   material: CommodityRef,
+   quantity: i32,
+   rarity: i32,
+}
+
+enum Gfx {
+   Single(Texture),
+   Sprite(Texture),
+}
+
+pub struct Type {
+   name: String,
+   scanned_msg: String,
+   gfx: Vec<Gfx>,
+   material: Vec<Material>,
+   armour_min: f64,
+   armour_max: f64,
+   absorb: f64,
+   damage: f64,
+   disable: f64,
+   penetration: f64,
+   exp_radius: f64,
+   alert_range: f64,
+}
+
+pub struct TypeGroup {
+   name: String,
+   types: Vec<(Arc<Type>, f64)>,
+   wtotal: f64,
+}
+
+enum State {
+   Xx,
+   XxToBg,
+   Xb,
+   BgToFg,
+   Fg,
+   FgToBg,
+   Bx,
+   BgToXx,
+}
+/*
+impl State {
+   fn from_c(c: naevc::AsteroidState) -> Self {
+      match c {
+         AsteroidState_ASTEROID_XX => State::Xx,
+         AsteroidState_ASTEROID_XX_TO_BG => State::XxToBg,
+         AsteroidState_ASTEROID_XB => State::Xb,
+         AsteroidState_ASTEROID_BG_TO_FG => State::BgToFg,
+         AsteroidState_ASTEROID_FG => State::Fg,
+         AsteroidState_ASTEROID_FG_TO_BG => State::FgToBg,
+         AsteroidState_ASTEROID_BX => State::Bx,
+         AsteroidState_ASTEROID_BG_TO_XX => State::BgToXx,
+      }
+   }
+
+   fn to_c(&self) -> naevc::AsteroidState {
+      match self {
+         State::Xx => AsteroidState_ASTEROID_XX,
+         State::XxToBg => AsteroidState_ASTEROID_XX,
+         State::Xb => AsteroidState_ASTEROID_XX,
+         State::BgToFg => AsteroidState_ASTEROID_XX,
+         State::Fg => AsteroidState_ASTEROID_XX,
+         State::FgToBg => AsteroidState_ASTEROID_XX,
+         State::Bx => AsteroidState_ASTEROID_XX,
+         State::BgToXx => AsteroidState_ASTEROID_XX,
+      }
+   }
+}
+*/
+
+pub struct Asteroid {
+   id: i32,
+   parent: i32,
+   state: State,
+   atype: Arc<Type>,
+   gfx: usize,
+   poly: SpinPolygon,
+   armour: f64,
+
+   sol: naevc::Solid,
+   ang: f64,
+   spin: f64,
+
+   timer: f64,
+   timer_max: f64,
+   scan_alpha: f64,
+   scanned: bool,
+}
