b7ccb96dc54ee42c47eb60d1bc37740a1f4f32c6
Edgar Simo-Serra <bobbens@gmail.com>
2026-04-13 21:09:56 +0900

    Added support for getting pilots from Lua.

--- src/pilot.rs
+++ src/pilot.rs
@@ -41,3 +41,45 @@ pub fn get_mut() -> &'static mut [PilotWrapper] {
       array::array_as_slice_mut(pilots as *mut PilotWrapper)
    }
 }
+
+unsafe extern "C-unwind" fn lua_pilot(lua: *mut mlua::ffi::lua_State) -> i32 {
+   unsafe {
+      if mlua::ffi::lua_isinteger(lua, 1) != 0 {
+         let plt = mlua::ffi::lua_tointeger(lua, 1);
+         naevc::lua_pushsystem(lua as *mut naevc::lua_State, plt as i32);
+      } else {
+         mlua::ffi::lua_pushstring(lua, c"lua_pilot recived a non-integer value!".as_ptr());
+         mlua::ffi::lua_error(lua);
+      }
+   }
+   1
+}
+
+unsafe extern "C-unwind" fn lua_pilot_index(lua: *mut mlua::ffi::lua_State) -> i32 {
+   unsafe {
+      let pltid = naevc::luaL_checkpilot(lua as *mut naevc::lua_State, 1);
+      mlua::ffi::lua_pushinteger(lua, pltid.into());
+   }
+   1
+}
+pub fn from_lua_index(lua: &mlua::Lua, value: &mlua::Value) -> mlua::Result<i64> {
+   let f = match lua.named_registry_value::<mlua::Function>("lua_pilot_index") {
+      Ok(f) => f,
+      Err(_e) => {
+         let f = unsafe { lua.create_c_function(lua_pilot_index)? };
+         lua.set_named_registry_value("lua_pilot_index", f.clone())?;
+         f
+      }
+   };
+   f.call::<i64>(value)
+}
+pub fn from_lua(lua: &mlua::Lua, value: &mlua::Value) -> mlua::Result<PilotWrapper> {
+   let id = from_lua_index(lua, value)?;
+   let plt = unsafe { naevc::pilot_get(id as u32) };
+   let plt = if plt.is_null() {
+      return Err(mlua::Error::RuntimeError("Pilot not found".to_string()));
+   } else {
+      PilotWrapper(NonNull::new(plt).unwrap())
+   };
+   Ok(plt)
+}
