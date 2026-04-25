2b6c4a1d7bbe10238be24f643f0e69b71f8893a7
Edgar Simo-Serra <bobbens@gmail.com>
2026-04-25 09:16:25 +0900

    Autoequip black diamond fighters.

---
+++ dat/scripts/equipopt/fighterbays/black_diamond.lua
@@ -0,0 +1,26 @@
+local fb = require "equipopt.fighterbays"
+local o -- Lazy load the outfits
+return {
+   priority = 10,
+   ship = ship.get("Black Diamond"),
+   equip = function ( p )
+      -- Lazy loading
+      if not o then
+         o = {
+            -- Cores
+            systems = outfit.get("Unicorp PT-16 Core System"),
+            systems_secondary = outfit.get("Milspec Orion 2301 Core System"),
+            engines = outfit.get("Nexus Dart 160 Engine"),
+            engines_secondary = outfit.get("Nexus Dart 160 Engine"),
+            hull = outfit.get("S&K Skirmish Plating"),
+            hull_secondary = outfit.get("Unicorp D-2 Light Plating"),
+            -- Other outfits
+            outfit.get("TeraCom Banshee Launcher"),
+            outfit.get("Gauss Gun"),
+            outfit.get("Gauss Gun"),
+         }
+      end
+      fb.equip( p, o )
+      return true
+   end,
+}
