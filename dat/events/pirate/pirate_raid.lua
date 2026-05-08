53b2be61a0993529f0faccdb7c0468ac9c50aaa7
Edgar Simo-Serra <bobbens@gmail.com>
2026-05-08 17:45:12 +0900

    Added pirate raid events.

---
+++ dat/events/pirate/pirate_raid.lua
@@ -0,0 +1,83 @@
+--[[
+<?xml version='1.0' encoding='utf8'?>
+<event name="Pirate System Raid">
+ <location>none</location>
+ <chance>0</chance>
+ <unique />
+</event>
+--]]
+
+--[[
+   Manages pirate raids in the background.
+--]]
+local fmt = require "format"
+
+local RAID_LENGTH       = time.new( 0, 60, 0 )
+local UNIDIFF           = [[
+<unidiff name="{name}">
+ <system name="{sysname}">
+  <spob_virtual_add>Pirate Raid</spob_virtual_add>
+ </system>
+</unidiff>
+]]
+
+local function diff_name ()
+   return mem.sys:nameRaw().." Pirate Raid"
+end
+
+-- Need to regenerate the diff on start
+function initialize ()
+   local sys = mem.sys:nameRaw()
+   diff.newDynamic( fmt.f( UNIDIFF, {
+      name     = diff_name(),
+      sysname  = sys,
+   } ))
+
+   local nc = naev.cache()
+   local t = nc._pirate_raid_active or {}
+   t[ sys ] = true
+   nc._pirate_raid_active = t
+end
+
+function create ()
+   evt.save(true)
+
+   local nc = naev.cache()
+   local pr = nc._pirate_raid
+   mem.sys  = pr.sys
+   mem.start= time.cur()
+   mem.finish= mem.start + RAID_LENGTH
+
+   -- Inclusive claim, should be tested already
+   evt.claim( {mem.sys}, true )
+   initialize()
+
+   news.add{ {
+      faction  = "Generic",
+      head     = fmt.f(_("Pirate Raids in {sys}"), {sys=mem.sys}),
+      body     = fmt.f(_("Reports say there seems to be a significant increase of pirate activity in the {sys} system. Local authorities request civilians to avoid the system until the increased hostilities cease, which are expected to continue until {date}."), {
+         sys   = mem.sys,
+         date  = mem.finish,
+      }),
+      date_to_rm = mem.finish,
+   } }
+
+   hook.load("initialize")
+   hook.date( time.new( 0, 1, 0 ), "date" )
+
+   nc._pirate_raid = nil
+end
+
+function date ()
+   if time.cur() < mem.finish then return end
+
+   if system.cur() == mem.sys then
+      player.msg(fmt.f(_("Pirates have stopped raiding {sys}."), {
+         sys = mem.sys,
+      } ) )
+   end
+
+   diff.remove( diff_name() )
+   naev.cache()._pirate_raid_active[ mem.sys:nameRaw() ] = nil
+   evt.finish(false) -- Don't want it to register as finished
+end
---
+++ dat/events/pirate/pirate_raid_manager.lua
@@ -0,0 +1,65 @@
+--[[
+<?xml version='1.0' encoding='utf8'?>
+<event name="Pirate System Raid Manager">
+ <location>load</location>
+ <chance>100</chance>
+ <unique />
+ <priority>99</priority>
+</event>
+--]]
+
+--[[
+   Manages pirate raids in the background.
+--]]
+--local fmt = require "format"
+local lmisn = require "lmisn"
+
+local PIRATE            = faction.get("Pirate")
+local CHANCE_PER_PERIOD = 5
+
+function create ()
+   mem.time    = time.cur()
+   hook.date( time.new( 0, 3, 0 ), "date" )
+end
+
+local function should_be_active ()
+   return player.chapter() ~= "0"
+end
+
+local function has_inhabited_generic_spob( sys )
+   for k,v in ipairs(sys:spobs()) do
+      local f = v:faction()
+      if f and f:tags().generic then
+         return true
+      end
+   end
+   return false
+end
+
+function date ()
+   if not should_be_active() then return end
+
+   if rnd.rnd() > CHANCE_PER_PERIOD then return end
+
+   local nc    = naev.cache()
+   local pra   = nc._pirate_raid_active or {}
+
+   -- Centred around the player somewhat
+   local candidates = lmisn.getSysAtDistance( nil, 0, 9, function ( sys )
+      -- No active raid
+      if pra[ sys:nameRaw() ] then return false end
+      -- Has some pirate presence
+      if sys:presence( PIRATE ) <= 0 then return false end
+      -- Claimable
+      if not naev.claimTest( sys, true ) then return false end
+      -- Has an inhabited "generic" spob
+      if not has_inhabited_generic_spob( sys ) then return false end
+      return true
+   end )
+
+   -- Set up and run the sub-event
+   nc._pirate_raid = {
+      sys = candidates[ rnd.rnd(#candidates) ],
+   }
+   naev.eventStart("Pirate System Raid")
+end
