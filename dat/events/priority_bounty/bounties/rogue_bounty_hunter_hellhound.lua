9cf2091c3e85e9091b289addb72992673beb6eeb
Edgar Simo-Serra <bobbens@gmail.com>
2026-05-10 11:34:53 +0900

    Fleshed out hellhound a bit more.

--- dat/events/priority_bounty/bounties/rogue_bounty_hunter_hellhound.lua
+++ dat/events/priority_bounty/bounties/rogue_bounty_hunter_hellhound.lua
@@ -1,9 +1,10 @@
-local bhelp = require "events.priority_bounty.helpers"
-local bounty = require "common.bounty"
+local bhelp    = require "events.priority_bounty.helpers"
+local bounty   = require "common.bounty"
+local equipopt = require "equipopt"
 return {
    var            = "bounty_hellhound", -- To control whether or not the player did it
-   title          = _("To Hell and Back"),
-   desc           = _("An ex-Astra Vigilis guild member has gone rogue after betraying a high priority bounty target. They are flying the Hellhound and believed to be accompanied by other rogue members. The Astra Vigilis wants them captured alive."),
+   title          = _("A Bark in the Dark"),
+   desc           = _("An ex-Astra Vigilis guild member known as Hellhound has gone rogue after betraying a high priority bounty target. They are flying the Hellhound and believed to be accompanied by other rogue members. The Astra Vigilis wants them captured alive."),
    escorts        = _("with heavy escorts"),
    reward         = 1.3e6,
    system         = system.get("Alteris"),
@@ -15,12 +16,20 @@ return {
    ships          = { ship.get("Starbridge Sigma") },
    spawnfunc      = function( b, params )
       local fct = bounty.get_faction()
-      local p = pilot.add( b.targetship[1], fct, params, b.targetname )
+      local p = pilot.add( b.targetship[1], fct, params, b.targetname, { naked = true } )
+      equipopt.soromid( p )
+      p:outfitAddIntrinsic("Escape Pod")
+      local m = p:memory()
+      m.capturable = true
+      local saying = _("What? Astra Vigilis sticking bounty hunters on me?")
+      m.taunt = saying
+      m.comm_greet = saying
       for k,s in ipairs(bhelp.choose_ships_from_points_and_capship( p:ship(), bhelp.ships.mercenary, 200 )) do
          local e = pilot.add( s, fct, params )
+         e:memory().capturable = true
          e:setLeader(p)
       end
       return p
    end,
-   cond = bhelp.cond_bounty_points( 200 ),
+   cond = bhelp.cond_bounty_points( 300 ),
 }
