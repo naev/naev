d3473db71526601a59052a26d92367942d31e631
Edgar Simo-Serra <bobbens@gmail.com>
2026-06-11 10:42:15 +0900

    Outfit price should give a proper broke message now.

--- dat/outfits/lib/custom_price.lua
+++ dat/outfits/lib/custom_price.lua
@@ -1,18 +1,24 @@
 local fmt = require "format"
 local lib = {}
+
+local REASON_BROKE = _("You do not have enough credits to purchase this item.")
+
 function lib.setup( costfunc )
    notactive = true -- Doesn't become active
 
    function price( _q )
       local c = costfunc()
       local canbuy = (c <= player.credits())
+      if not canbuy then
+         canbuy = REASON_BROKE
+      end
       return fmt.credits(c), canbuy, false
    end
 
    function buy( _q )
       local c = costfunc()
       if c > player.credits() then
-         return false, _("You do not have enough credits to purchase this item.")
+         return false, REASON_BROKE
       end
       player.pay( -c )
       return true, 1 -- Can only buy one at a time
