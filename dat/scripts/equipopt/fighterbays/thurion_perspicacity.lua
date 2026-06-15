02c31c9f5035eda6a32afbe7316873392264afbb
hsza <hsza@noreply.codeberg.org>
2026-06-15 07:42:23 +0000

    more thurion adjustments

--- dat/scripts/equipopt/fighterbays/thurion_perspicacity.lua
+++ dat/scripts/equipopt/fighterbays/thurion_perspicacity.lua
@@ -1,24 +1,26 @@
 local fb = require "equipopt.fighterbays"
-local o -- Lazy load the outfits
 return {
    priority = 10,
    ship = ship.get("Thurion Perspicacity"),
    equip = function ( p )
-      -- Lazy loading
-      if not o then
-         o = {
-            -- Cores
-            systems = outfit.get("Milspec Thalos 2202 Core System"),
-            engines = outfit.get("Nexus Dart 160 Engine"),
-            hull = outfit.get("Unicorp D-2 Light Plating"),
-            -- Other outfits
-            outfit.get("Laser Cannon MK1"),
-            outfit.get("Laser Cannon MK1"),
-            outfit.get("Nebula Resistant Coating"),
-            outfit.get("Unicorp Scrambler"),
-            outfit.get("Flicker Drive"),
-            outfit.get("Reactor Class I"),
-         }
+      local o = {
+         systems = outfit.get("Milspec Thalos 2202 Core System"),
+         engines = outfit.get("Nexus Dart 160 Engine"),
+         hull = outfit.get("Unicorp D-2 Light Plating"),
+         outfit.get("Laser Cannon MK2"),
+         outfit.get("Unicorp Scrambler"),
+         outfit.get("Reactor Class I"),
+      }
+      local nebu = select(2, system.cur():nebula())
+      if nebu >= 9 then
+         table.insert(o, outfit.get("Nebula Resistant Coating"))
+      else
+         table.insert(o, outfit.get("Nexus Concealment Coating"))
+      end
+      if nebu >= 16 then
+         table.insert(o, outfit.get("Small Shield Booster"))
+      else
+         table.insert(o, outfit.get("Flicker Drive"))
       end
       fb.equip( p, o )
       return true
