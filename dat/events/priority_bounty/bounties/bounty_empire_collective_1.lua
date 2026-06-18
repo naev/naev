37905c5228b8a55ae2c9adeec56f6a90b7873deb
solardawning <solardawning@gmail.com>
2026-06-18 11:49:45 -0700

    Add Empire bounties on Collective Drone ships

--- dat/events/priority_bounty/bounties/bounty_empire_collective_1.lua
+++ dat/events/priority_bounty/bounties/bounty_empire_collective_1.lua
@@ -3,8 +3,8 @@ local bounty   = require "common.bounty"
 local equipopt = require "equipopt"
 return {
    var            = "bounty_empire_collective_1",
-   title          = _("Haywire Drones"),
-   desc           = _("SECRET: An experimental drone fighter has been misplaced, and the Empire would like it destroyed quietly and without any fuss. Your prompt attention to and silence on this matter is appreciated."),
+   title          = _("Abominable Intelligence"),
+   desc           = _("SECRET: An experimental wing of drone fightercraft have been misplaced, and the Empire would like them destroyed quietly and without any fuss. Your prompt attention to and silence on this matter is appreciated."),
    escorts        = _("with a few light drone escorts."),
    reward         = 0.8e6,
    system         = system.get("Chloe"),
@@ -16,8 +16,7 @@ return {
    ships          = { ship.get("Heavy Drone") },
    spawnfunc      = function( b, params )
       local fct = bounty.get_faction()
-      local p = pilot.add( b.targetship[1], fct, params, b.targetname, {ai="baddie_norun", naked = true } )
-      equipopt.collective( p )
+      local p = pilot.add( b.targetship[1], fct, params, b.targetname, { ai = "baddie_norun" } )
       p:outfitAddIntrinsic("Escape Pod")
       local m = p:memory()
       if not m.lootables then
@@ -25,7 +24,7 @@ return {
       end
       m.lootables["encrypted_data_matrix"] = 1
       m.capturable = true
-      local saying = _("BEEP. BEEP. BEEP.")
+      local saying = _("IFF_ERR[37601]: PARAM UNDEFINED. HOSTILES DETECTED. ENGAGING.")
       m.taunt = saying
       m.comm_greet = saying
       local enemies = {p}
@@ -37,8 +36,12 @@ return {
       end
       return enemies
    end,
+   boardfunc = function( p )
+      p:rename( _("the ship's AI core") )
+      return true
+   end,
    cond = function ()
-      return not diff.isApplied("collective_dead")
+      return not diff.isApplied("collective_dead") --Only available pre-Collective plot. Serves as worldbuilding, Empire rep, and to guide players gently to finding it.
          and (var.peek("astra_vigilis_points") or 0) > 100
    end,
 }
