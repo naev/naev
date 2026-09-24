de92ba67302076d4c65ce66dc683a44901616a36
AvianGeneticist <64111606+AvianGeneticist@users.noreply.github.com>
2026-09-24 12:37:16 -0600

    Change Bulwarks into a variant and add some needed nerfs

--- dat/scripts/equipopt/fighterbays/soromid_brigand.lua
+++ dat/scripts/equipopt/fighterbays/soromid_brigand.lua
@@ -1,6 +1,7 @@
 local fb = require "equipopt.fighterbays"
 local bioship = require "bioship"
 local o -- Lazy load the outfits
+local NAVIUMNAL = outfit.get("Naviumnal Brigand Bay")
 return {
    priority = 10,
    ship = ship.get("Soromid Brigand"),
@@ -11,13 +12,23 @@ return {
             outfit.get("Plasma Blaster MK1"),
          }
       end
-      bioship.simulate( p, bioship.maxstage( p ), {
-         "bite1",
-         "bite2",
-         "bite3",
-         "stealth1",
-         "plasma1",
-      } )
+      if fb.spawnerOutfit(p) == NAVIUMNAL then
+         bioship.simulate( p, bioship.maxstage( p ), {
+            "plasma1",
+            "plasma2",
+            "plasma3",
+            "health1",
+            "health2",
+         } )
+      else
+         bioship.simulate( p, bioship.maxstage( p ), {
+            "bite1",
+            "bite2",
+            "bite3",
+            "stealth1",
+            "plasma1",
+         } )
+      end
       fb.equip( p, o )
       return true
    end,
